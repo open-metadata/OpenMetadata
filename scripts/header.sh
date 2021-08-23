23db2a062992daa4f60d141ba8745dc232f78ed3
Suresh Srinivas <srini30005@gmail.com>
2021-08-22 22:34:16 -0700

    Fix #265 - Remove scripts related to JsonSchema2md documentation that … (#266)
    * Fix #265 - Remove script related to JsonSchema2md documentation that are no longer necessary
    
    * Delete schema-scripts.yml
    
    It is not required.
    
    Co-authored-by: parthp2107 <83201188+parthp2107@users.noreply.github.com>

--- scripts/header.sh
+++
@@ -1,9 +0,0 @@
-file=/home/runner/work/OpenMetadata/OpenMetadata/docs/openmetadata-apis/schemas/SchemaMarkdown/*.md
-sed -i '1d' $file
-cd /home/runner/work/OpenMetadata/OpenMetadata/docs/openmetadata-apis/schemas/SchemaMarkdown/
-for file in *.md
-do
-  sed -i "1i\
-# ${file%%.*}" "$file"
-done
-
