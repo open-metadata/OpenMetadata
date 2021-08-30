76d5e7d0ca0608ccce9920d8c60200804996b45e
Suresh Srinivas <srini30005@gmail.com>
2021-08-30 10:29:16 -0700

    Es doc fix (#335)
    * Minor: Search Api doc fix
    
    Co-authored-by: Suresh Srinivas <srini3005@gmail.com>

--- catalog-rest-service/src/main/java/org/openmetadata/catalog/resources/search/SearchResource.java
+++ catalog-rest-service/src/main/java/org/openmetadata/catalog/resources/search/SearchResource.java
@@ -85,8 +85,8 @@ public class SearchResource {
                          @Context SecurityContext securityContext,
                          @Parameter(description = "Search Query Text, Pass *text* for substring match; " +
                                  "Pass without wildcards for exact match. <br/> " +
-                                 "1. For listing all tables pass q=* <br/>" +
-                                 "2. For search tables pass q=*search_term* <br/>" +
+                                 "1. For listing all tables or topics pass q=* <br/>" +
+                                 "2. For search tables or topics pass q=*search_term* <br/>" +
                                  "3. For searching field names such as search by column_name " +
                                  "pass q=column_names:address <br/>" +
                                  "4. For searching by tag names pass q=tags:user.email <br/>" +
@@ -98,7 +98,7 @@ public class SearchResource {
                          @javax.ws.rs.QueryParam("q") String query,
                          @Parameter(description = "ElasticSearch Index name, defaults to table_search_index")
                            @DefaultValue("table_search_index") @QueryParam("index") String index,
-                         @Parameter(description = "From field to paginate the results, defaults to 1")
+                         @Parameter(description = "From field to paginate the results, defaults to 0")
                            @DefaultValue("0") @QueryParam("from") int from,
                          @Parameter(description = "Size field to limit the no.of results returned, defaults to 10")
                            @DefaultValue("10") @QueryParam("size") int size,
