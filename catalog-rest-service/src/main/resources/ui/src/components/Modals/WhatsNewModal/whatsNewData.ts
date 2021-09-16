724e2b3f82168c94fbd868a9081b7af406a8b1f3
darth-coder00 <86726556+darth-coder00@users.noreply.github.com>
2021-09-17 01:03:36 +0530

    Whats new changes (#512)
    * Whats new modal auto visible for New and Returning users post new release
    
    * Minor fix
    
    * Minor change for video
    
    * Minor changes

--- catalog-rest-service/src/main/resources/ui/src/components/Modals/WhatsNewModal/whatsNewData.ts
+++ catalog-rest-service/src/main/resources/ui/src/components/Modals/WhatsNewModal/whatsNewData.ts
@@ -21,10 +21,10 @@ export const LATEST_VERSION_ID = 0;
 
 export const COOKIE_VERSION = 'VERSION_0_4_0';
 
-const dummyImg = 'https://via.placeholder.com/725x278';
+export const dummyImg = 'https://via.placeholder.com/725x278';
 
 // for youtube video make isImage = false and path = {video embed id}
-// embed:- youtube video => share => click on embed and take id of it
+// embed:- youtube video => share => click on embed and take {url with id} from it
 
 export const WHATS_NEW = [
   {
@@ -44,7 +44,7 @@ export const WHATS_NEW = [
         description:
           "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. ",
         isImage: false,
-        path: 'jssO8-5qmag',
+        path: 'https://www.youtube.com/embed/jssO8-5qmag',
       },
       {
         title: 'UI implements',
