cc6a4de72fd06e86859f94bafdfd572878a2a026
darth-coder00 <86726556+darth-coder00@users.noreply.github.com>
2021-09-17 01:12:00 +0530

    Whats new changes (#514)
    * Whats new modal auto visible for New and Returning users post new release
    
    * Minor fix
    
    * Minor change for video
    
    * Minor changes
    
    * making path optional for what's new modal features

--- catalog-rest-service/src/main/resources/ui/src/components/Modals/WhatsNewModal/whatsNewData.ts
+++ catalog-rest-service/src/main/resources/ui/src/components/Modals/WhatsNewModal/whatsNewData.ts
@@ -51,7 +51,7 @@ export const WHATS_NEW = [
         description:
           "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. ",
         isImage: true,
-        path: dummyImg,
+        path: '',
       },
     ],
     changeLogs: {
