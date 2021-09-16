579c5aa66dfdecf856c7dfe8f346720abc28518a
Sachin-chaurasiya <sachinchaurasiyachotey87@gmail.com>
2021-09-16 16:47:56 +0530

    added externallink to need help options

--- catalog-rest-service/src/main/resources/ui/src/components/app-bar/Appbar.tsx
+++ catalog-rest-service/src/main/resources/ui/src/components/app-bar/Appbar.tsx
@@ -98,7 +98,7 @@ const Appbar: React.FC = (): JSX.Element => {
       ),
     },
     {
-      name: `Slack Channel`,
+      name: `Slack`,
       to: 'https://openmetadata.slack.com/join/shared_invite/zt-udl8ris3-Egq~YtJU_yJgJTtROo00dQ#/shared-invite/email',
       disabled: false,
       isOpenNewTab: true,
