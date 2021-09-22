1298c73e7b1d60e1efd49c1849ca5150fd76cb75
Shailesh Parmar <shailesh.parmar.webdev@gmail.com>
2021-09-22 19:39:29 +0530

    fixed popOver test

--- catalog-rest-service/src/main/resources/ui/src/components/common/popover/PopOver.test.js
+++ catalog-rest-service/src/main/resources/ui/src/components/common/popover/PopOver.test.js
@@ -1,15 +1,23 @@
-import { queryByText, render } from '@testing-library/react';
+import {
+  fireEvent,
+  getByText,
+  queryByText,
+  render,
+  screen,
+} from '@testing-library/react';
 import React from 'react';
 import PopOver from './PopOver';
 
-// global.document.createRange = () => ({
-//   setStart: jest.fn(),
-//   setEnd: jest.fn(),
-//   commonAncestorContainer: {
-//     nodeName: 'BODY',
-//     ownerDocument: document,
-//   },
-// });
+let global;
+
+global.document.createRange = () => ({
+  setStart: jest.fn(),
+  setEnd: jest.fn(),
+  commonAncestorContainer: {
+    nodeName: 'BODY',
+    ownerDocument: document,
+  },
+});
 
 describe('Test Popover Component', () => {
   it('Component should render', () => {
@@ -24,40 +32,40 @@ describe('Test Popover Component', () => {
     expect(popover).toBeInTheDocument();
   });
 
-  // it('Onclick popover should display title', () => {
-  //   const { container } = render(
-  //     <PopOver position="bottom" title="test popover" trigger="click">
-  //       <span>Hello World</span>
-  //     </PopOver>
-  //   );
+  it('Onclick popover should display title', () => {
+    const { container } = render(
+      <PopOver position="bottom" title="test popover" trigger="click">
+        <span>Hello World</span>
+      </PopOver>
+    );
 
-  //   fireEvent(
-  //     getByText(container, /Hello World/i),
-  //     new MouseEvent('click', {
-  //       bubbles: true,
-  //       cancelable: true,
-  //     })
-  //   );
+    fireEvent(
+      getByText(container, /Hello World/i),
+      new MouseEvent('click', {
+        bubbles: true,
+        cancelable: true,
+      })
+    );
 
-  //   expect(screen.getByText(/test popover/i)).toBeInTheDocument();
-  // });
+    expect(screen.getByText(/test popover/i)).toBeInTheDocument();
+  });
 
-  // it('Onclick popover should display html', () => {
-  //   const html = <p>test popover</p>;
-  //   const { container } = render(
-  //     <PopOver html={html} position="bottom" trigger="click">
-  //       <span>Hello World</span>
-  //     </PopOver>
-  //   );
+  it('Onclick popover should display html', () => {
+    const html = <p>test popover</p>;
+    const { container } = render(
+      <PopOver html={html} position="bottom" trigger="click">
+        <span>Hello World</span>
+      </PopOver>
+    );
 
-  //   fireEvent(
-  //     getByText(container, /Hello World/i),
-  //     new MouseEvent('click', {
-  //       bubbles: true,
-  //       cancelable: true,
-  //     })
-  //   );
+    fireEvent(
+      getByText(container, /Hello World/i),
+      new MouseEvent('click', {
+        bubbles: true,
+        cancelable: true,
+      })
+    );
 
-  //   expect(screen.getByText(/test popover/i)).toBeInTheDocument();
-  // });
+    expect(screen.getByText(/test popover/i)).toBeInTheDocument();
+  });
 });
