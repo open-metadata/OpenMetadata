ec73f274ce345feb3bb091d74d13f9fc5d2dcd37
Sriharsha Chintalapani <sriharsha@apache.org>
2021-09-26 11:18:44 -0700

    Fix #587: Ingestion: Add standalone report process to generate datasets, usage & profile and serve from standalone server

---
+++ ingestion/src/metadata_server/manage.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+"""OpenMetadata Server for generated reports."""
+import os
+import sys
+
+
+def main():
+    """Run OpenMetadata report server."""
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'openmetadata.settings')
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(sys.argv)
+
+
+if __name__ == '__main__':
+    main()
