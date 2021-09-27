a351cf3af57911c5f660b5773c90375e4135b3fc
darth-coder00 <86726556+darth-coder00@users.noreply.github.com>
2021-09-27 19:23:58 +0530

    Added Licensing to generated types (#593)

--- catalog-rest-service/src/main/resources/ui/src/generated/entity/data/pipeline.ts
+++ catalog-rest-service/src/main/resources/ui/src/generated/entity/data/pipeline.ts
@@ -1,3 +1,21 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 /**
  * This schema defines the Pipeline entity. A pipeline enables the flow of data from source
  * to destination through a series of processing steps. ETL is a type of pipeline where the
@@ -5,9 +23,18 @@
  */
 export interface Pipeline {
   /**
-   * Description of this pipeline.
+   * Description of this Pipeline.
    */
   description?: string;
+  /**
+   * Display Name that identifies this Pipeline. It could be title or label from the source
+   * services.
+   */
+  displayName?: string;
+  /**
+   * Followers of this Pipeline.
+   */
+  followers?: EntityReference[];
   /**
    * A unique name that identifies a pipeline in the format 'ServiceName.PipelineName'.
    */
@@ -29,18 +56,32 @@ export interface Pipeline {
    */
   owner?: EntityReference;
   /**
+   * Pipeline  URL to visit/manage. This URL points to respective pipeline service UI
+   */
+  pipelineUrl?: string;
+  /**
    * Link to service where this pipeline is hosted in.
    */
   service: EntityReference;
+  /**
+   * Tags for this Pipeline.
+   */
+  tags?: TagLabel[];
+  /**
+   * All the tasks that are part of pipeline.
+   */
+  tasks?: EntityReference[];
 }
 
 /**
- * Owner of this pipeline.
+ * Followers of this Pipeline.
  *
  * This schema defines the EntityReference type used for referencing an entity.
  * EntityReference is used for capturing relationships from one entity to another. For
  * example, a table has an attribute called database of type EntityReference that captures
  * the relationship of a table `belongs to a` database.
+ *
+ * Owner of this pipeline.
  *
  * Link to service where this pipeline is hosted in.
  */
@@ -67,4 +108,51 @@ export interface EntityReference {
    * `bigquery`, `snowflake`...
    */
   type: string;
+}
+
+/**
+ * This schema defines the type for labeling an entity with a Tag.
+ */
+export interface TagLabel {
+  /**
+   * Link to the tag resource.
+   */
+  href?: string;
+  /**
+   * Label type describes how a tag label was applied. 'Manual' indicates the tag label was
+   * applied by a person. 'Derived' indicates a tag label was derived using the associated tag
+   * relationship (see TagCategory.json for more details). 'Propagated` indicates a tag label
+   * was propagated from upstream based on lineage. 'Automated' is used when a tool was used
+   * to determine the tag label.
+   */
+  labelType?: LabelType;
+  /**
+   * 'Suggested' state is used when a tag label is suggested by users or tools. Owner of the
+   * entity must confirm the suggested labels before it is marked as 'Confirmed'.
+   */
+  state?: State;
+  tagFQN?: string;
+}
+
+/**
+ * Label type describes how a tag label was applied. 'Manual' indicates the tag label was
+ * applied by a person. 'Derived' indicates a tag label was derived using the associated tag
+ * relationship (see TagCategory.json for more details). 'Propagated` indicates a tag label
+ * was propagated from upstream based on lineage. 'Automated' is used when a tool was used
+ * to determine the tag label.
+ */
+export enum LabelType {
+  Automated = 'Automated',
+  Derived = 'Derived',
+  Manual = 'Manual',
+  Propagated = 'Propagated',
+}
+
+/**
+ * 'Suggested' state is used when a tag label is suggested by users or tools. Owner of the
+ * entity must confirm the suggested labels before it is marked as 'Confirmed'.
+ */
+export enum State {
+  Confirmed = 'Confirmed',
+  Suggested = 'Suggested',
 }
--- catalog-rest-service/src/main/resources/ui/src/generated/type/basic.ts
+++ catalog-rest-service/src/main/resources/ui/src/generated/type/basic.ts
@@ -15,30 +15,3 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-/**
- * This schema defines Bot entity. A bot automates tasks, such as adding description,
- * identifying the importance of data. It runs as a special user in the system.
- */
-export interface Bots {
-  /**
-   * Description of the bot.
-   */
-  description?: string;
-  /**
-   * Name used for display purposes. Example 'FirstName LastName'.
-   */
-  displayName?: string;
-  /**
-   * Link to the resource corresponding to this bot.
-   */
-  href?: string;
-  /**
-   * Unique identifier of a bot instance.
-   */
-  id?: string;
-  /**
-   * Name of the bot.
-   */
-  name?: string;
-}
