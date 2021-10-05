f255ba1538a6e99cdb4d7b69b49f4e2778dd1e7a
Sachin-chaurasiya <sachinchaurasiyachotey87@gmail.com>
2021-10-05 12:47:07 +0530

    added check for id tableProfiler is available or not.

--- catalog-rest-service/src/main/resources/ui/src/components/my-data-details/ProfilerTable.tsx
+++ catalog-rest-service/src/main/resources/ui/src/components/my-data-details/ProfilerTable.tsx
@@ -47,146 +47,154 @@ const ProfilerTable = ({ tableProfiles, columns }: Props) => {
   });
 
   return (
-    <table className="tw-w-full" data-testid="schema-table">
-      <thead>
-        <tr className="tableHead-row">
-          <th className="tableHead-cell">Column Name</th>
-          <th className="tableHead-cell">Rows</th>
-          <th className="tableHead-cell">Distinct Ratio (Percentage)</th>
-          <th className="tableHead-cell">Null Ratio (Percentage)</th>
-          <th className="tableHead-cell">Min</th>
-          <th className="tableHead-cell">Max</th>
-          <th className="tableHead-cell">Median</th>
-          <th className="tableHead-cell">Standard Deviation</th>
-        </tr>
-      </thead>
-      {columnSpecificData.map((col, colIndex) => {
-        return (
-          <Fragment key={colIndex}>
-            <tbody className="tableBody">
-              <tr className={classNames('tableBody-row')}>
-                <td className="tw-relative tableBody-cell">
-                  <span
-                    className="tw-mr-2 tw-cursor-pointer"
-                    onClick={() =>
-                      setExpandedColumn((prevState) => ({
-                        name: col.name,
-                        isExpanded:
-                          prevState.name === col.name
-                            ? !prevState.isExpanded
-                            : true,
-                      }))
-                    }>
-                    {expandedColumn.name === col.name ? (
-                      expandedColumn.isExpanded ? (
-                        <i className="fas fa-caret-down" />
-                      ) : (
-                        <i className="fas fa-caret-right" />
-                      )
-                    ) : (
-                      <i className="fas fa-caret-right" />
-                    )}
-                  </span>
-                  {col.name}
-                </td>
-                <td className="tw-relative tableBody-cell">
-                  <ProfilerGraph
-                    data={
-                      col.data
-                        ?.map((d) => ({
-                          date: d.profilDate,
-                          value: d.rows ?? 0,
-                        }))
-                        .reverse() as ProfilerGraphData
-                    }
-                  />
-                </td>
-                <td className="tw-relative tableBody-cell">
-                  <ProfilerGraph
-                    data={
-                      col.data
-                        ?.map((d) => ({
-                          date: d.profilDate,
-                          value: d.uniqueProportion ?? 0,
-                        }))
-                        .reverse() as ProfilerGraphData
-                    }
-                  />
-                </td>
-                <td className="tw-relative tableBody-cell">
-                  <ProfilerGraph
-                    data={
-                      col.data
-                        ?.map((d) => ({
-                          date: d.profilDate,
-                          value: d.nullProportion ?? 0,
-                        }))
-                        .reverse() as ProfilerGraphData
-                    }
-                  />
-                </td>
-                <td className="tw-relative tableBody-cell">{col.min}</td>
-                <td className="tw-relative tableBody-cell">{col.max}</td>
-                <td className="tw-relative tableBody-cell">{col.median}</td>
-                <td className="tw-relative tableBody-cell">
-                  <ProfilerGraph
-                    data={
-                      col.data
-                        ?.map((d) => ({
-                          date: d.profilDate,
-                          value: d.stddev ?? 0,
-                        }))
-                        .reverse() as ProfilerGraphData
-                    }
-                  />
-                </td>
-              </tr>
-            </tbody>
-            {expandedColumn.name === col.name && expandedColumn.isExpanded && (
-              <tbody>
-                {col.data?.map((colData, index) => (
-                  <tr
-                    className={classNames(
-                      'tableBody-row tw-border-0 tw-border-l tw-border-r',
-                      {
-                        'tw-border-b':
-                          columnSpecificData.length - 1 === colIndex &&
-                          col.data?.length === index + 1,
-                      }
-                    )}
-                    key={index}>
+    <>
+      {tableProfiles?.length ? (
+        <table className="tw-w-full" data-testid="schema-table">
+          <thead>
+            <tr className="tableHead-row">
+              <th className="tableHead-cell">Column Name</th>
+              <th className="tableHead-cell">Rows</th>
+              <th className="tableHead-cell">Distinct Ratio (Percentage)</th>
+              <th className="tableHead-cell">Null Ratio (Percentage)</th>
+              <th className="tableHead-cell">Min</th>
+              <th className="tableHead-cell">Max</th>
+              <th className="tableHead-cell">Median</th>
+              <th className="tableHead-cell">Standard Deviation</th>
+            </tr>
+          </thead>
+          {columnSpecificData.map((col, colIndex) => {
+            return (
+              <Fragment key={colIndex}>
+                <tbody className="tableBody">
+                  <tr className={classNames('tableBody-row')}>
                     <td className="tw-relative tableBody-cell">
-                      <span className="tw-pl-6">{colData.profilDate}</span>
+                      <span
+                        className="tw-mr-2 tw-cursor-pointer"
+                        onClick={() =>
+                          setExpandedColumn((prevState) => ({
+                            name: col.name,
+                            isExpanded:
+                              prevState.name === col.name
+                                ? !prevState.isExpanded
+                                : true,
+                          }))
+                        }>
+                        {expandedColumn.name === col.name ? (
+                          expandedColumn.isExpanded ? (
+                            <i className="fas fa-caret-down" />
+                          ) : (
+                            <i className="fas fa-caret-right" />
+                          )
+                        ) : (
+                          <i className="fas fa-caret-right" />
+                        )}
+                      </span>
+                      {col.name}
                     </td>
                     <td className="tw-relative tableBody-cell">
-                      <span className="tw-pl-6">{colData.rows}</span>
+                      <ProfilerGraph
+                        data={
+                          col.data
+                            ?.map((d) => ({
+                              date: d.profilDate,
+                              value: d.rows ?? 0,
+                            }))
+                            .reverse() as ProfilerGraphData
+                        }
+                      />
                     </td>
                     <td className="tw-relative tableBody-cell">
-                      {colData.uniqueProportion ?? 0}
+                      <ProfilerGraph
+                        data={
+                          col.data
+                            ?.map((d) => ({
+                              date: d.profilDate,
+                              value: d.uniqueProportion ?? 0,
+                            }))
+                            .reverse() as ProfilerGraphData
+                        }
+                      />
                     </td>
                     <td className="tw-relative tableBody-cell">
-                      {colData.nullProportion ?? 0}
+                      <ProfilerGraph
+                        data={
+                          col.data
+                            ?.map((d) => ({
+                              date: d.profilDate,
+                              value: d.nullProportion ?? 0,
+                            }))
+                            .reverse() as ProfilerGraphData
+                        }
+                      />
                     </td>
+                    <td className="tw-relative tableBody-cell">{col.min}</td>
+                    <td className="tw-relative tableBody-cell">{col.max}</td>
+                    <td className="tw-relative tableBody-cell">{col.median}</td>
                     <td className="tw-relative tableBody-cell">
-                      {colData.min ?? 0}
-                    </td>
-                    <td className="tw-relative tableBody-cell">
-                      {colData.max ?? 0}
-                    </td>
-                    <td className="tw-relative tableBody-cell">
-                      {colData.median ?? 0}
-                    </td>
-                    <td className="tw-relative tableBody-cell">
-                      {colData.stddev ?? 0}
+                      <ProfilerGraph
+                        data={
+                          col.data
+                            ?.map((d) => ({
+                              date: d.profilDate,
+                              value: d.stddev ?? 0,
+                            }))
+                            .reverse() as ProfilerGraphData
+                        }
+                      />
                     </td>
                   </tr>
-                ))}
-              </tbody>
-            )}
-          </Fragment>
-        );
-      })}
-    </table>
+                </tbody>
+                {expandedColumn.name === col.name && expandedColumn.isExpanded && (
+                  <tbody>
+                    {col.data?.map((colData, index) => (
+                      <tr
+                        className={classNames(
+                          'tableBody-row tw-border-0 tw-border-l tw-border-r',
+                          {
+                            'tw-border-b':
+                              columnSpecificData.length - 1 === colIndex &&
+                              col.data?.length === index + 1,
+                          }
+                        )}
+                        key={index}>
+                        <td className="tw-relative tableBody-cell">
+                          <span className="tw-pl-6">{colData.profilDate}</span>
+                        </td>
+                        <td className="tw-relative tableBody-cell">
+                          <span className="tw-pl-6">{colData.rows}</span>
+                        </td>
+                        <td className="tw-relative tableBody-cell">
+                          {colData.uniqueProportion ?? 0}
+                        </td>
+                        <td className="tw-relative tableBody-cell">
+                          {colData.nullProportion ?? 0}
+                        </td>
+                        <td className="tw-relative tableBody-cell">
+                          {colData.min ?? 0}
+                        </td>
+                        <td className="tw-relative tableBody-cell">
+                          {colData.max ?? 0}
+                        </td>
+                        <td className="tw-relative tableBody-cell">
+                          {colData.median ?? 0}
+                        </td>
+                        <td className="tw-relative tableBody-cell">
+                          {colData.stddev ?? 0}
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                )}
+              </Fragment>
+            );
+          })}
+        </table>
+      ) : (
+        <div className="tw-flex tw-justify-center tw-font-medium tw-items-center tw-border tw-border-main tw-rounded-md tw-p-8">
+          No profiler data available
+        </div>
+      )}
+    </>
   );
 };
 
