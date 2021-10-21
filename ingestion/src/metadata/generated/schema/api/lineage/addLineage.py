9c5e45dd84e1c9391a9c2ca6d56e1ec69e29eef8
pmbrull <p.brull@stuart.com>
2021-10-21 18:15:44 +0200

    Keep lineage title as addLineage

--- ingestion/src/metadata/generated/schema/api/lineage/addLineage.py
+++ ingestion/src/metadata/generated/schema/api/lineage/addLineage.py
@@ -11,7 +11,7 @@ from pydantic import BaseModel, Field
 from ...type import entityLineage
 
 
-class AddlineageEntityRequest(BaseModel):
+class AddLineage(BaseModel):
     description: Optional[str] = Field(
         None, description='User provided description of the lineage details.'
     )
