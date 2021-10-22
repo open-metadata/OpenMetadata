ac54aed61b8fff2aedfc29b8a702bcb688fec3fa
rong fengliang <1141591465@qq.com>
2021-10-22 14:12:24 +0800

    ISSUE-861: add elasticsearch username & password

--- catalog-rest-service/src/main/java/org/openmetadata/catalog/ElasticSearchConfiguration.java
+++ catalog-rest-service/src/main/java/org/openmetadata/catalog/ElasticSearchConfiguration.java
@@ -26,6 +26,10 @@ public class ElasticSearchConfiguration {
     @NotEmpty
     private Integer port;
 
+    private String username;
+
+    private String password;
+
     public String getHost() {
         return host;
     }
@@ -42,11 +46,29 @@ public class ElasticSearchConfiguration {
         this.port = port;
     }
 
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     @Override
     public String toString() {
-        return "ElasticSearchConfiguration {" +
-                ", host=" +  host +
+        return "ElasticSearchConfiguration{" +
+                "host='" + host + '\'' +
                 ", port=" + port +
-                "}";
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
     }
 }
