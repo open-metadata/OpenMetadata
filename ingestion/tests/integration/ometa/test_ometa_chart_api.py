34ad6ad89819d1822745125bd38860fe675210a5
pmbrull <p.brull@stuart.com>
2021-10-25 17:41:40 +0200

    Add Chart tests

---
+++ ingestion/tests/integration/ometa/test_ometa_chart_api.py
@@ -0,0 +1,192 @@
+"""
+OpenMetadata high-level API Database test
+"""
+import uuid
+from unittest import TestCase
+
+from metadata.generated.schema.api.data.createChart import CreateChartEntityRequest
+from metadata.generated.schema.api.services.createDashboardService import (
+    CreateDashboardServiceEntityRequest,
+)
+from metadata.generated.schema.api.teams.createUser import CreateUserEntityRequest
+from metadata.generated.schema.entity.data.chart import Chart
+from metadata.generated.schema.entity.data.database import Database
+from metadata.generated.schema.entity.services.dashboardService import (
+    DashboardService,
+    DashboardServiceType,
+)
+from metadata.generated.schema.type.entityReference import EntityReference
+from metadata.ingestion.ometa.ometa_api import OMeta
+from metadata.ingestion.ometa.openmetadata_rest import MetadataServerConfig
+
+
+class OMetaChartTest(TestCase):
+    """
+    Run this integration test with the local API available
+    Install the ingestion package before running the tests
+    """
+
+    service_entity_id = None
+
+    server_config = MetadataServerConfig(api_endpoint="http://localhost:8585/api")
+    metadata = OMeta(server_config)
+
+    user = metadata.create_or_update(
+        entity=CreateUserEntityRequest,
+        data=CreateUserEntityRequest(name="random-user", email="random@user.com"),
+    )
+    owner = EntityReference(id=user.id, type="user")
+
+    service = CreateDashboardServiceEntityRequest(
+        name="test-service",
+        serviceType=DashboardServiceType.Superset,
+        dashboardUrl="https://localhost:1000",
+    )
+    service_type = "dashboardService"
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        """
+        Prepare ingredients
+        """
+        cls.service_entity = cls.metadata.create_or_update(
+            entity=CreateDashboardServiceEntityRequest, data=cls.service
+        )
+
+        cls.entity = Chart(
+            id=uuid.uuid4(),
+            name="test",
+            service=EntityReference(id=cls.service_entity.id, type=cls.service_type),
+            fullyQualifiedName="test-service.test",
+        )
+
+        cls.create = CreateChartEntityRequest(
+            name="test",
+            service=EntityReference(id=cls.service_entity.id, type=cls.service_type),
+        )
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        """
+        Clean up
+        """
+        _id = str(
+            cls.metadata.get_by_name(entity=Chart, fqdn="test-service.test").id.__root__
+        )
+
+        service_id = str(
+            cls.metadata.get_by_name(
+                entity=DashboardService, fqdn="test-service"
+            ).id.__root__
+        )
+
+        cls.metadata.delete(entity=Chart, entity_id=_id)
+        cls.metadata.delete(entity=DashboardService, entity_id=service_id)
+
+    def test_create(self):
+        """
+        We can create a Chart and we receive it back as Entity
+        """
+
+        res = self.metadata.create_or_update(
+            entity=CreateChartEntityRequest, data=self.create
+        )
+
+        self.assertEqual(res.name, self.create.name)
+        self.assertEqual(res.service.id, self.create.service.id)
+        self.assertEqual(res.owner, None)
+
+    def test_update(self):
+        """
+        Updating it properly changes its properties
+        """
+
+        res_create = self.metadata.create_or_update(
+            entity=CreateChartEntityRequest, data=self.create
+        )
+
+        updated = self.entity.dict(exclude_unset=True)
+        updated["owner"] = self.owner
+        updated_entity = Database(**updated)
+
+        res = self.metadata.create_or_update(entity=Chart, data=updated_entity)
+
+        # Same ID, updated algorithm
+        self.assertEqual(res.service.id, updated_entity.service.id)
+        self.assertEqual(res_create.id, res.id)
+        self.assertEqual(res.owner.id, self.user.id)
+
+    def test_get_name(self):
+        """
+        We can fetch a Chart by name and get it back as Entity
+        """
+
+        self.metadata.create_or_update(entity=Chart, data=self.entity)
+
+        res = self.metadata.get_by_name(
+            entity=Chart, fqdn=self.entity.fullyQualifiedName
+        )
+        self.assertEqual(res.name, self.entity.name)
+
+    def test_get_id(self):
+        """
+        We can fetch a Chart by ID and get it back as Entity
+        """
+
+        self.metadata.create_or_update(entity=Chart, data=self.entity)
+
+        # First pick up by name
+        res_name = self.metadata.get_by_name(
+            entity=Chart, fqdn=self.entity.fullyQualifiedName
+        )
+        # Then fetch by ID
+        res = self.metadata.get_by_id(entity=Chart, entity_id=str(res_name.id.__root__))
+
+        self.assertEqual(res_name.id, res.id)
+
+    def test_list(self):
+        """
+        We can list all our Charts
+        """
+
+        print(self.entity)
+        self.metadata.create_or_update(entity=Chart, data=self.entity)
+
+        res = self.metadata.list_entities(entity=Chart, limit=100)
+        print(len(res.entities))
+        print(res)
+        for e in res.entities:
+            print("---------")
+            print(e)
+            print(e.name)
+
+        # Fetch our test Database. We have already inserted it, so we should find it
+        data = next(
+            iter(ent for ent in res.entities if ent.name == self.entity.name), None
+        )
+        assert data
+
+    def test_delete(self):
+        """
+        We can delete a Chart by ID
+        """
+
+        self.metadata.create_or_update(entity=Chart, data=self.entity)
+
+        # Find by name
+        res_name = self.metadata.get_by_name(
+            entity=Chart, fqdn=self.entity.fullyQualifiedName
+        )
+        # Then fetch by ID
+        res_id = self.metadata.get_by_id(
+            entity=Chart, entity_id=str(res_name.id.__root__)
+        )
+
+        # Delete
+        self.metadata.delete(entity=Chart, entity_id=str(res_id.id.__root__))
+
+        # Then we should not find it
+        res = self.metadata.list_entities(entity=Chart)
+        assert not next(
+            iter(ent for ent in res.entities if ent.name == self.entity.name), None
+        )
