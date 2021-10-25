4cdbc3a4ba9a5aa5910e96422bd1ce9023bb85e4
pmbrull <p.brull@stuart.com>
2021-10-25 19:44:35 +0200

    Add pipeline tests

---
+++ ingestion/tests/integration/ometa/test_ometa_pipeline_api.py
@@ -0,0 +1,197 @@
+"""
+OpenMetadata high-level API Database test
+"""
+import uuid
+from unittest import TestCase
+
+from metadata.generated.schema.api.data.createPipeline import (
+    CreatePipelineEntityRequest,
+)
+from metadata.generated.schema.api.services.createDashboardService import (
+    CreateDashboardServiceEntityRequest,
+)
+from metadata.generated.schema.api.services.createPipelineService import (
+    CreatePipelineServiceEntityRequest,
+)
+from metadata.generated.schema.api.teams.createUser import CreateUserEntityRequest
+from metadata.generated.schema.entity.data.Pipeline import Pipeline
+from metadata.generated.schema.entity.data.pipeline import Pipeline
+from metadata.generated.schema.entity.services.dashboardService import (
+    DashboardService,
+    DashboardServiceType,
+)
+from metadata.generated.schema.entity.services.pipelineService import (
+    PipelineServiceType,
+)
+from metadata.generated.schema.type.entityReference import EntityReference
+from metadata.ingestion.ometa.ometa_api import OMeta
+from metadata.ingestion.ometa.openmetadata_rest import MetadataServerConfig
+
+
+class OMetaPipelineTest(TestCase):
+    """
+    Run this integration test with the local API available
+    Install the ingestion package before running the tests
+    """
+
+    service_entity_id = None
+
+    server_config = MetadataServerConfig(api_endpoint="http://localhost:8585/api")
+    metadata = OMeta(server_config)
+
+    user = metadata.create_or_update(
+        entity=CreateUserEntityRequest,
+        data=CreateUserEntityRequest(name="random-user", email="random@user.com"),
+    )
+    owner = EntityReference(id=user.id, type="user")
+
+    service = CreatePipelineServiceEntityRequest(
+        name="test-service",
+        serviceType=PipelineServiceType.Airflow,
+        dashboardUrl="https://localhost:1000",
+    )
+    service_type = "pipelineService"
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        """
+        Prepare ingredients
+        """
+        cls.service_entity = cls.metadata.create_or_update(
+            entity=CreatePipelineServiceEntityRequest, data=cls.service
+        )
+
+        cls.entity = Pipeline(
+            id=uuid.uuid4(),
+            name="test",
+            service=EntityReference(id=cls.service_entity.id, type=cls.service_type),
+            fullyQualifiedName="test-service.test",
+        )
+
+        cls.create = CreatePipelineEntityRequest(
+            name="test",
+            service=EntityReference(id=cls.service_entity.id, type=cls.service_type),
+        )
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        """
+        Clean up
+        """
+        _id = str(
+            cls.metadata.get_by_name(
+                entity=Pipeline, fqdn="test-service.test"
+            ).id.__root__
+        )
+
+        service_id = str(
+            cls.metadata.get_by_name(
+                entity=DashboardService, fqdn="test-service"
+            ).id.__root__
+        )
+
+        cls.metadata.delete(entity=Pipeline, entity_id=_id)
+        cls.metadata.delete(entity=DashboardService, entity_id=service_id)
+
+    def test_create(self):
+        """
+        We can create a Pipeline and we receive it back as Entity
+        """
+
+        res = self.metadata.create_or_update(
+            entity=CreatePipelineEntityRequest, data=self.create
+        )
+
+        self.assertEqual(res.name, self.create.name)
+        self.assertEqual(res.service.id, self.create.service.id)
+        self.assertEqual(res.owner, None)
+
+    def test_update(self):
+        """
+        Updating it properly changes its properties
+        """
+
+        res_create = self.metadata.create_or_update(
+            entity=CreatePipelineEntityRequest, data=self.create
+        )
+
+        updated = self.entity.dict(exclude_unset=True)
+        updated["owner"] = self.owner
+        updated_entity = Pipeline(**updated)
+
+        res = self.metadata.create_or_update(entity=Pipeline, data=updated_entity)
+
+        # Same ID, updated algorithm
+        self.assertEqual(res.service.id, updated_entity.service.id)
+        self.assertEqual(res_create.id, res.id)
+        self.assertEqual(res.owner.id, self.user.id)
+
+    def test_get_name(self):
+        """
+        We can fetch a Pipeline by name and get it back as Entity
+        """
+
+        self.metadata.create_or_update(entity=Pipeline, data=self.entity)
+
+        res = self.metadata.get_by_name(
+            entity=Pipeline, fqdn=self.entity.fullyQualifiedName
+        )
+        self.assertEqual(res.name, self.entity.name)
+
+    def test_get_id(self):
+        """
+        We can fetch a Pipeline by ID and get it back as Entity
+        """
+
+        self.metadata.create_or_update(entity=Pipeline, data=self.entity)
+
+        # First pick up by name
+        res_name = self.metadata.get_by_name(
+            entity=Pipeline, fqdn=self.entity.fullyQualifiedName
+        )
+        # Then fetch by ID
+        res = self.metadata.get_by_id(
+            entity=Pipeline, entity_id=str(res_name.id.__root__)
+        )
+
+        self.assertEqual(res_name.id, res.id)
+
+    def test_list(self):
+        """
+        We can list all our Pipelines
+        """
+
+        self.metadata.create_or_update(entity=Pipeline, data=self.entity)
+
+        res = self.metadata.list_entities(entity=Pipeline, limit=100)
+
+        # Fetch our test Database. We have already inserted it, so we should find it
+        data = next(
+            iter(ent for ent in res.entities if ent.name == self.entity.name), None
+        )
+        assert data
+
+    def test_delete(self):
+        """
+        We can delete a Pipeline by ID
+        """
+
+        self.metadata.create_or_update(entity=Pipeline, data=self.entity)
+
+        # Find by name
+        res_name = self.metadata.get_by_name(
+            entity=Pipeline, fqdn=self.entity.fullyQualifiedName
+        )
+        # Then fetch by ID
+        res_id = self.metadata.get_by_id(
+            entity=Pipeline, entity_id=str(res_name.id.__root__)
+        )
+
+        # Delete
+        self.metadata.delete(entity=Pipeline, entity_id=str(res_id.id.__root__))
+
+        # Then we should not find it
+        res = self.metadata.list_entities(entity=Pipeline)
+        assert not next(
+            iter(ent for ent in res.entities if ent.name == self.entity.name), None
+        )
