1e766971bc1e17a38e16fdabf1b4d56da07a144a
Shailesh Parmar <shailesh.parmar.webdev@gmail.com>
2021-11-03 16:46:07 +0530

    Feat: issue-944 add documentation guid for user when ES disconnected and index is missing

--- catalog-rest-service/src/main/resources/ui/src/components/MyData/MyData.interface.ts
+++ catalog-rest-service/src/main/resources/ui/src/components/MyData/MyData.interface.ts
@@ -2,9 +2,10 @@ import { EntityCounts, SearchDataFunctionType, SearchResponse } from 'Models';
 import { User } from '../../generated/entity/teams/user';
 
 export interface MyDataProps {
+  error: string;
   countServices: number;
   userDetails: User;
-  searchResult: SearchResponse | undefined;
+  searchResult: SearchResponse[] | undefined;
   fetchData: (value: SearchDataFunctionType) => void;
   entityCounts: EntityCounts;
 }
