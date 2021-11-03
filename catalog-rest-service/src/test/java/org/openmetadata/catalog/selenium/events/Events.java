6fa28a54b38b455e45c0f369e92be96270eafb29
Parth Panchal <parth.panchal@deuexsolutions.com>
2021-11-03 18:34:42 +0530

    addressing reviewdog finding

--- catalog-rest-service/src/test/java/org/openmetadata/catalog/selenium/events/Events.java
+++ catalog-rest-service/src/test/java/org/openmetadata/catalog/selenium/events/Events.java
@@ -21,7 +21,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class Events {
+public final class Events {
 
     @SuppressWarnings("unused")
     private Events() {
