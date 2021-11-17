6cb628c8ebf73f13afb6db2e356e64d322bb7f80
Shailesh Parmar <shailesh.parmar.webdev@gmail.com>
2021-11-17 18:02:34 +0530

    Fix: Pagination button hover issue (#1227)

--- catalog-rest-service/src/main/resources/ui/src/components/Pagination/index.tsx
+++ catalog-rest-service/src/main/resources/ui/src/components/Pagination/index.tsx
@@ -32,7 +32,7 @@ const Pagination: React.FC<PaginationProps> = ({
         hideFirstLastPages
         activePage={currentPage}
         disabledClass="tw-opacity-60 disabled"
-        itemClass="tw-border tw-border-primary tw-text-blue-500 tw-rounded tw-px-3 tw-py-1.5 tw-text-sm tw-mx-2 hover:tw-bg-primary "
+        itemClass="tw-border tw-border-primary tw-text-blue-500 tw-rounded tw-px-3 tw-py-1.5 tw-text-sm tw-mx-2 hover:tw-bg-primary button-text"
         itemsCountPerPage={sizePerPage}
         linkClass="tw-text-primary tw-font-medium tw-link"
         nextPageText={
