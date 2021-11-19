eea60a86963f5115f650c7eb471ec9453f11a37c
Sriharsha Chintalapani <harshach@users.noreply.github.com>
2021-11-18 16:27:43 -0800

    Fix #1264: Pipelines: remove 64 character constraint in names (#1265)
    * Fix #1264: Pipelines: remove 64 character constraint in names

--- bootstrap/sql/mysql/v001__create_db_connection_info.sql
+++ bootstrap/sql/mysql/v001__create_db_connection_info.sql
@@ -212,7 +212,7 @@ CREATE TABLE IF NOT EXISTS ml_model_entity (
 
 CREATE TABLE IF NOT EXISTS pipeline_entity (
     id VARCHAR(36) GENERATED ALWAYS AS (json ->> '$.id') STORED NOT NULL,
-    fullyQualifiedName VARCHAR(256) GENERATED ALWAYS AS (json ->> '$.fullyQualifiedName') NOT NULL,
+    fullyQualifiedName VARCHAR(512) GENERATED ALWAYS AS (json ->> '$.fullyQualifiedName') NOT NULL,
     json JSON NOT NULL,
     updatedAt TIMESTAMP GENERATED ALWAYS AS (TIMESTAMP(STR_TO_DATE(json ->> '$.updatedAt', '%Y-%m-%dT%T.%fZ'))) NOT NULL,
     updatedBy VARCHAR(256) GENERATED ALWAYS AS (json ->> '$.updatedBy') NOT NULL,
