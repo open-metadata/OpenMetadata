2127ef198f7005ce0916638df76649ee4bcad2da
Sriharsha Chintalapani <harsha@getcollate.io>
2021-11-23 14:46:30 -0800

    Fix #1299: Move UI module into its own maven module

---
+++ openmetadata-ui/src/test/java/org/openmetadata/catalog/selenium/pages/ingestion/IngestionPageTest.java
@@ -0,0 +1,136 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements. See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License. You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.openmetadata.catalog.selenium.pages.ingestion;
+
+import com.github.javafaker.Faker;
+import org.openmetadata.catalog.selenium.events.Events;
+import org.openmetadata.catalog.selenium.properties.Property;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.MethodOrderer;
+
+import java.time.Duration;
+import java.util.ArrayList;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class IngestionPageTest {
+
+  static WebDriver webDriver;
+  static String url = Property.getInstance().getURL();
+  static Faker faker = new Faker();
+  static Actions actions;
+  static WebDriverWait wait;
+  Integer waitTime = Property.getInstance().getSleepTime();
+
+  @BeforeEach
+  public void openMetadataWindow() {
+    System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/linux/chromedriver");
+    ChromeOptions options = new ChromeOptions();
+    options.addArguments("--headless");
+    options.addArguments("--window-size=1280,800");
+    webDriver = new ChromeDriver(options);
+    actions = new Actions(webDriver);
+    wait = new WebDriverWait(webDriver, Duration.ofSeconds(30));
+    webDriver.manage().window().maximize();
+    webDriver.get(url);
+  }
+
+  @Test
+  @Order(1)
+  public void openIngestionPage() throws InterruptedException {
+    Events.click(webDriver, By.cssSelector("[data-testid='closeWhatsNew']")); // Close What's new
+    Thread.sleep(waitTime);
+    Events.click(webDriver, By.cssSelector(
+        "[data-testid='menu-button'][id='menu-button-Settings']")); // Setting
+    Events.click(webDriver, By.cssSelector("[data-testid='menu-item-Ingestions']")); // Setting/Services
+    Thread.sleep(waitTime);
+  }
+
+  @Test
+  @Order(2)
+  public void addIngestionService() throws InterruptedException {
+    openIngestionPage();
+    Events.click(webDriver, By.cssSelector("[data-testid='add-new-user-button']"));
+    Events.sendKeys(webDriver, By.cssSelector("[id='name'][name='name']"), faker.name().firstName());
+    Events.click(webDriver, By.cssSelector("[value='BigQuery$$bigquery']"));
+    Events.click(webDriver, By.cssSelector("[value='bigquery']"));
+    Events.click(webDriver, By.cssSelector("[data-testid='next-button']"));
+    Events.sendKeys(webDriver, By.cssSelector("[name='username']"), "openmetadata_user");
+    Events.sendKeys(webDriver, By.cssSelector("[name='password']"), "openmetadata_password");
+    Events.sendKeys(webDriver, By.cssSelector("[name='host']"), "localhost:3306");
+    Events.sendKeys(webDriver, By.cssSelector("[name='database']"), "openmetadata_db");
+    Events.click(webDriver, By.cssSelector("[data-testid='next-button']"));
+    Events.click(webDriver, By.cssSelector("[value='week']"));
+    Events.click(webDriver, By.cssSelector("[value='4']"));
+    Events.click(webDriver, By.cssSelector("[value='21']"));
+    Events.sendKeys(webDriver, By.cssSelector("[data-testid='endDate']"), "21072022");
+    Events.click(webDriver, By.cssSelector("[data-testid='next-button']"));
+    Events.click(webDriver, By.cssSelector("[data-testid='save-button']"));
+  }
+
+  @Test
+  @Order(3)
+  public void runIngestionService() throws InterruptedException{
+    openIngestionPage();
+    Events.click(webDriver, By.cssSelector("[data-testid='run']"));
+    webDriver.navigate().refresh();
+  }
+
+  @Test
+  @Order(4)
+  public void editIngestionService() throws InterruptedException{
+    openIngestionPage();
+    Events.click(webDriver, By.cssSelector("[data-testid='edit']"));
+    Events.sendKeys(webDriver, By.cssSelector("[data-testid='includeFilterPattern']"), ",");
+    Events.click(webDriver, By.cssSelector("[data-testid='next-button']"));
+    Events.click(webDriver, By.cssSelector("[value='hour']"));
+    Events.click(webDriver, By.cssSelector("[value='20']"));
+    Events.click(webDriver, By.cssSelector("[data-testid='next-button']"));
+    Events.click(webDriver, By.cssSelector("[data-testid='save-button']"));
+  }
+
+  @Test
+  @Order(5)
+  public void deleteIngestionService() throws InterruptedException {
+    openIngestionPage();
+    Events.click(webDriver, By.cssSelector("[data-testid='delete']"));
+    Events.click(webDriver, By.cssSelector("[data-testid='save-button']"));
+    webDriver.navigate().refresh();
+  }
+
+  @AfterEach
+  public void closeTabs() {
+    ArrayList<String> tabs = new ArrayList<>(webDriver.getWindowHandles());
+    String originalHandle = webDriver.getWindowHandle();
+    for (String handle : webDriver.getWindowHandles()) {
+      if (!handle.equals(originalHandle)) {
+        webDriver.switchTo().window(handle);
+        webDriver.close();
+      }
+    }
+    webDriver.switchTo().window(tabs.get(0)).close();
+  }
+}
---
+++ openmetadata-ui/src/test/java/org/openmetadata/catalog/selenium/pages/myData/MyDataPageTest.java
@@ -0,0 +1,155 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements. See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License. You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.openmetadata.catalog.selenium.pages.myData;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.openmetadata.catalog.selenium.events.Events;
+import org.openmetadata.catalog.selenium.properties.Property;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.ArrayList;
+
+public class MyDataPageTest {
+
+  static WebDriver webDriver;
+  static String url = Property.getInstance().getURL();
+  static Actions actions;
+  static WebDriverWait wait;
+  Integer waitTime = Property.getInstance().getSleepTime();
+
+  @BeforeEach
+  public void openMetadataWindow() {
+    System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/linux/chromedriver");
+    ChromeOptions options = new ChromeOptions();
+    options.addArguments("--headless");
+    webDriver = new ChromeDriver(options);
+    actions = new Actions(webDriver);
+    wait = new WebDriverWait(webDriver, Duration.ofSeconds(30));
+    webDriver.manage().window().maximize();
+    webDriver.get(url);
+  }
+
+  @Test
+  @Order(1)
+  public void checkWhatsNew() {
+    Events.click(webDriver, By.xpath("//ul[@class='slick-dots testid-dots-button']//li[2]")); // What's new page 2
+    Events.click(webDriver, By.cssSelector("[data-testid='WhatsNewModalChangeLogs']")); // Change Logs
+    Events.click(webDriver, By.cssSelector("[data-testid='closeWhatsNew']")); // Close What's new
+  }
+
+  @Test
+  @Order(2)
+  public void checkTabs() {
+    checkWhatsNew();
+    Events.click(webDriver, By.cssSelector("[data-testid='tab'][id='myDataTab']")); // My Data
+    Events.click(webDriver, By.cssSelector("[data-testid='tab'][id='followingTab']")); // Following
+  }
+
+  @Test
+  @Order(3)
+  public void checkOverview() throws InterruptedException {
+    checkWhatsNew();
+    Events.click(webDriver, By.cssSelector("[data-testid='tables']")); // Tables
+    webDriver.navigate().back();
+    Events.click(webDriver, By.cssSelector("[data-testid='topics']")); // Topics
+    webDriver.navigate().back();
+    Events.click(webDriver, By.cssSelector("[data-testid='dashboards']")); // Dashboard
+    webDriver.navigate().back();
+    Events.click(webDriver, By.cssSelector("[data-testid='pipelines']")); // Pipeline
+    webDriver.navigate().back();
+    Events.click(webDriver, By.cssSelector("[data-testid='service']")); // Services
+    webDriver.navigate().back();
+    Events.click(webDriver, By.cssSelector("[data-testid='ingestion']")); // Services
+    webDriver.navigate().back();
+    Events.click(webDriver, By.cssSelector("[data-testid='user']")); // Users
+    webDriver.navigate().back();
+    Events.click(webDriver, By.cssSelector("[data-testid='terms']"));  // Teams
+  }
+
+  @Test
+  @Order(4)
+  public void checkSearchBar() throws InterruptedException {
+    checkWhatsNew();
+    wait.until(ExpectedConditions.elementToBeClickable(
+        webDriver.findElement(By.cssSelector("[id='searchBox']")))); // Search bar/dim
+    Events.sendKeys(webDriver, By.cssSelector("[id='searchBox']"), "dim"); // Search bar/dim
+    Thread.sleep(waitTime);
+    Events.click(webDriver, By.cssSelector("[data-testid='data-name']")); // Search bar/dim
+  }
+
+
+  @Test
+  @Order(5)
+  public void checkHeaders() {
+    checkWhatsNew();
+    ArrayList<String> tabs = new ArrayList<>(webDriver.getWindowHandles());
+    Events.click(webDriver, By.cssSelector("[data-testid='appbar-item'][id='explore']")); // Explore
+    webDriver.navigate().back();
+    Events.click(webDriver, By.cssSelector("[data-testid='menu-button'][id='menu-button-Settings']")); // Setting
+    Events.click(webDriver, By.cssSelector("[data-testid='menu-item-Teams']")); // Setting/Teams
+    webDriver.navigate().back();
+    Events.click(webDriver, By.cssSelector("[data-testid='menu-button'][id='menu-button-Settings']")); // Setting
+    Events.click(webDriver, By.cssSelector("[data-testid='menu-item-Tags']")); // Setting/Tags
+    webDriver.navigate().back();
+    Events.click(webDriver, By.cssSelector("[data-testid='menu-button'][id='menu-button-Settings']")); // Setting
+    Events.click(webDriver, By.cssSelector("[data-testid='menu-item-Services']")); // Setting/Services
+    Events.click(webDriver, By.cssSelector("[data-testid='whatsnew-modal']")); // What's New
+    checkWhatsNew();
+    Events.click(webDriver, By.cssSelector("[data-testid='menu-button'][id='menu-button-Need Help']"));
+    Events.click(webDriver, By.cssSelector("[data-testid='menu-item-Docs']"));
+    webDriver.switchTo().window(tabs.get(0));
+
+    Events.click(webDriver, By.cssSelector("[data-testid='menu-button'][id='menu-button-Need Help']"));
+    Events.click(webDriver, By.cssSelector("[data-testid='menu-item-API']"));
+    webDriver.navigate().back();
+
+    Events.click(webDriver, By.cssSelector("[data-testid='menu-button'][id='menu-button-Need Help']"));
+    Events.click(webDriver, By.cssSelector("[data-testid='menu-item-Slack']"));
+    webDriver.switchTo().window(tabs.get(0));
+  }
+
+  @Test
+  @Order(6)
+  public void checkLogout() {
+    checkWhatsNew();
+    Events.click(webDriver, By.cssSelector("[data-testid='greeting-text']"));
+    Events.click(webDriver, By.cssSelector("[data-testid='menu-item-Logout']"));
+  }
+
+  @AfterEach
+  public void closeTabs() {
+    ArrayList<String> tabs = new ArrayList<>(webDriver.getWindowHandles());
+    String originalHandle = webDriver.getWindowHandle();
+    for (String handle : webDriver.getWindowHandles()) {
+      if (!handle.equals(originalHandle)) {
+        webDriver.switchTo().window(handle);
+        webDriver.close();
+      }
+    }
+    webDriver.switchTo().window(tabs.get(0)).close();
+  }
+}
