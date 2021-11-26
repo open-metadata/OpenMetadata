c74d7612bcd9f2bb4e32646c3de399663da3e2a9
Sachin Chaurasiya <sachinchaurasiyachotey87@gmail.com>
2021-11-26 20:37:33 +0530

    Adding Lineage node expansion to show columns . (#1393)
    * Adding Lineage node expansion to show columns .
    
    * Adding support for getting columns of expanded node.
    
    * Refactoring.
    
    * Reafctoring and style changes.
    
    * Minor changes
    
    * Changing prop type.
    
    * minor style changes.

---
+++ openmetadata-ui/src/main/resources/ui/src/components/EntityLineage/CustomNode.component.jsx
@@ -0,0 +1,77 @@
+import classNames from 'classnames';
+import React, { Fragment } from 'react';
+import { Handle } from 'react-flow-renderer';
+
+const handleStyles = { borderRadius: '50%', position: 'absolute', top: 10 };
+const getHandle = (nodeType, isConnectable) => {
+  if (nodeType === 'output') {
+    return (
+      <Handle
+        isConnectable={isConnectable}
+        position="left"
+        style={{ ...handleStyles, left: '-14px' }}
+        type="target"
+      />
+    );
+  } else if (nodeType === 'input') {
+    return (
+      <Handle
+        isConnectable={isConnectable}
+        position="right"
+        style={{ ...handleStyles, right: '-14px' }}
+        type="source"
+      />
+    );
+  } else {
+    return (
+      <Fragment>
+        <Handle
+          isConnectable={isConnectable}
+          position="left"
+          style={{ ...handleStyles, left: '-14px' }}
+          type="target"
+        />
+        <Handle
+          isConnectable={isConnectable}
+          position="right"
+          style={{ ...handleStyles, right: '-14px' }}
+          type="source"
+        />
+      </Fragment>
+    );
+  }
+};
+
+const CustomNode = (props) => {
+  /* eslint-disable-next-line */
+  const { data, type, isConnectable } = props;
+  /* eslint-disable-next-line */
+  const { label, columns } = data;
+
+  return (
+    <div className="tw-relative nowheel ">
+      {getHandle(type, isConnectable)}
+      {/* Node label could be simple text or reactNode */}
+      <div className={classNames('tw-px-2')}>{label}</div>
+
+      {columns?.length ? <hr className="tw-my-2 tw--mx-3" /> : null}
+      <section
+        className={classNames('tw--mx-3 tw-px-3', {
+          'tw-h-36 tw-overflow-y-auto': columns?.length,
+        })}
+        id="table-columns">
+        <div className="tw-flex tw-flex-col tw-gap-y-1">
+          {columns?.map((c) => (
+            <p
+              className="tw-p-1 tw-rounded tw-border tw-text-primary"
+              key={c.name}>
+              {c.name}
+            </p>
+          ))}
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default CustomNode;
