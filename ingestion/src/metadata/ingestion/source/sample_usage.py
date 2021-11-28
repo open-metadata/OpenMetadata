be9195317b0636cc5272bd310de7d3b0b51ac329
Sriharsha Chintalapani <harshach@users.noreply.github.com>
2021-11-28 14:25:55 -0800

    Fix #1443: sample-usage ingestion is broken (#1444)

--- ingestion/src/metadata/ingestion/source/sample_usage.py
+++ ingestion/src/metadata/ingestion/source/sample_usage.py
@@ -30,9 +30,7 @@ class SampleUsageSource(Source):
         self.query_log_csv = config.sample_data_folder + "/datasets/query_log"
         with open(self.query_log_csv, "r") as fin:
             self.query_logs = [dict(i) for i in csv.DictReader(fin)]
-        self.service = get_database_service_or_create(
-            self.service_json, metadata_config
-        )
+        self.service = get_database_service_or_create(self.config, metadata_config)
 
     @classmethod
     def create(cls, config_dict, metadata_config_dict, ctx):
