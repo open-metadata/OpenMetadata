7009a0bc128f111121d7e2d8e32aaa878b200c0f
Sriharsha Chintalapani <harshach@users.noreply.github.com>
2021-11-29 10:59:15 -0800

    Fix #1073: Apache Druid Support (#1424)
    * Fix #1073: Apache Druid Support

--- ingestion/setup.py
+++ ingestion/setup.py
@@ -79,6 +79,7 @@ plugins: Dict[str, Set[str]] = {
     "bigquery-usage": {"google-cloud-logging", "cachetools"},
     "docker": {"docker==5.0.3"},
     "dbt": {},
+    "druid": {"pydruid>=0.6.2"},
     "elasticsearch": {"elasticsearch~=7.13.1"},
     "glue": {"boto3~=1.19.12"},
     "hive": {
