9c58f104ea33d5e55344245aa15cf0bc5ca3c034
Parth Panchal <parth.panchal@deuexsolutions.com>
2021-11-30 14:20:58 +0530

    added orders to test classes and added minor changes to tests

--- openmetadata-ui/src/test/java/org/openmetadata/catalog/selenium/pages/topicDetails/TopicDetailsPageTest.java
+++ openmetadata-ui/src/test/java/org/openmetadata/catalog/selenium/pages/topicDetails/TopicDetailsPageTest.java
@@ -36,6 +36,7 @@ import org.junit.jupiter.api.MethodOrderer;
 import java.time.Duration;
 import java.util.ArrayList;
 
+@Order(7)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class TopicDetailsPageTest {
   static WebDriver webDriver;
