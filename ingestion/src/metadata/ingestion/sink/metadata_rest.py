dea64524a100cb1ed6750096c13ddeed02a42a08
pmbrull <p.brull@stuart.com>
2021-12-10 18:01:24 +0100

    Use CreateMlModelEntityRequest instead of MlModel for PUT operations

--- ingestion/src/metadata/ingestion/sink/metadata_rest.py
+++ ingestion/src/metadata/ingestion/sink/metadata_rest.py
@@ -127,7 +127,7 @@ class MetadataRestSink(Sink[Entity]):
             self.write_lineage(record)
         elif isinstance(record, User):
             self.write_users(record)
-        elif isinstance(record, MlModel):
+        elif isinstance(record, CreateMlModelEntityRequest):
             self.write_ml_model(record)
         elif isinstance(record, OMetaDatabaseAndModel):
             self.write_dbt_models(record)
@@ -361,16 +361,9 @@ class MetadataRestSink(Sink[Entity]):
             logger.error(err)
             self.status.failure(f"Lineage: {add_lineage}")
 
-    def write_ml_model(self, model: MlModel):
+    def write_ml_model(self, model: CreateMlModelEntityRequest):
         try:
-            model_request = CreateMlModelEntityRequest(
-                name=model.name,
-                displayName=model.displayName,
-                description=model.description,
-                algorithm=model.algorithm,
-                dashboard=model.dashboard,
-            )
-            created_model = self.metadata.create_or_update(model_request)
+            created_model = self.metadata.create_or_update(model)
             logger.info(f"Successfully added Model {created_model.displayName}")
             self.status.records_written(f"Model: {created_model.displayName}")
         except (APIError, ValidationError) as err:
