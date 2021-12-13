2d5aa076b6e5ed61f3ddafa111fef337e4b7f04c
Sachin-chaurasiya <sachinchaurasiyachotey87@gmail.com>
2021-12-13 18:58:16 +0530

    Style : Adding Some style changes according to the feedback in #1710

--- openmetadata-ui/src/main/resources/ui/src/components/dropdown/AnchorDropDownList.tsx
+++ openmetadata-ui/src/main/resources/ui/src/components/dropdown/AnchorDropDownList.tsx
@@ -31,7 +31,7 @@ const AnchorDropDownList = ({ dropDownList, setIsOpen }: DropDownListProp) => {
         aria-labelledby="menu-button"
         aria-orientation="vertical"
         className="tw-origin-top-right tw-absolute tw-z-10
-              tw-right-0 tw-mt-2 tw-w-32 tw-rounded-md tw-shadow-lg
+              tw-right-0 tw-mt-2 tw-w-36 tw-rounded-md tw-shadow-lg
               tw-bg-white tw-ring-1 tw-ring-black tw-ring-opacity-5 focus:tw-outline-none"
         role="menu">
         <div className="py-1" role="none">
