1073ee435ddb20434d283d730435f7179fe72e17
Sriharsha Chintalapani <harshach@users.noreply.github.com>
2021-12-13 18:55:24 -0800

    Fix #1714: isBot field not coming in api response for botPrinciples user (#1715)
    * Fix #1714: isBot field not coming in api response for botPrinciples user

--- catalog-rest-service/src/main/java/org/openmetadata/catalog/security/DefaultCatalogAuthorizer.java
+++ catalog-rest-service/src/main/java/org/openmetadata/catalog/security/DefaultCatalogAuthorizer.java
@@ -75,7 +75,7 @@ public class DefaultCatalogAuthorizer implements CatalogAuthorizer {
                 return true;
               }
             })
-            .forEach(this::addUser);
+            .forEach(this::addAdmin);
   }
 
   private void mayBeAddBotUsers() {
@@ -94,7 +94,7 @@ public class DefaultCatalogAuthorizer implements CatalogAuthorizer {
                 return true;
               }
             })
-            .forEach(this::addUser);
+            .forEach(this::addBot);
   }
 
 
@@ -162,13 +162,31 @@ public class DefaultCatalogAuthorizer implements CatalogAuthorizer {
     }
   }
 
-  private void addUser(String name) {
+  private void addAdmin(String name) {
     User user = new User().withId(UUID.randomUUID())
             .withName(name)
             .withEmail(name + "@" + principalDomain)
             .withIsAdmin(true)
             .withUpdatedBy(name)
             .withUpdatedAt(new Date());
+
+    try {
+      User addedUser = userRepository.create(null, user);
+      LOG.debug("Added admin user entry: {}", addedUser);
+    } catch (DuplicateEntityException | IOException | ParseException exception) {
+      // In HA setup the other server may have already added the user.
+      LOG.debug("Caught exception: " + ExceptionUtils.getStackTrace(exception));
+      LOG.debug("Admin user entry: {} already exists.", user);
+    }
+  }
+
+  private void addBot(String name) {
+    User user = new User().withId(UUID.randomUUID())
+        .withName(name)
+        .withEmail(name + "@" + principalDomain)
+        .withIsBot(true)
+        .withUpdatedBy(name)
+        .withUpdatedAt(new Date());
 
     try {
       User addedUser = userRepository.create(null, user);
