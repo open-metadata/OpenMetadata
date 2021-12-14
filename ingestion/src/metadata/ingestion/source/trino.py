24b19a217d27e6680544a69c7f674a190c3863af
Sriharsha Chintalapani <harsha@getcollate.io>
2021-12-14 09:52:54 -0800

    Fix #1747:  Ingestion: parse connection config method

--- ingestion/src/metadata/ingestion/source/trino.py
+++ ingestion/src/metadata/ingestion/source/trino.py
@@ -10,12 +10,13 @@
 #  limitations under the License.
 
 from typing import Iterable
+from urllib.parse import quote_plus
 
 from sqlalchemy.inspection import inspect
 
 from metadata.ingestion.models.ometa_table_db import OMetaDatabaseAndTable
-from .sql_source import SQLConnectionConfig, SQLSource
-from ..ometa.openmetadata_rest import MetadataServerConfig
+from metadata.ingestion.ometa.openmetadata_rest import MetadataServerConfig
+from metadata.ingestion.source.sql_source import SQLConnectionConfig, SQLSource
 
 
 class TrinoConfig(SQLConnectionConfig):
@@ -28,15 +29,25 @@ class TrinoConfig(SQLConnectionConfig):
     def get_connection_url(self):
         url = f"{self.scheme}://"
         if self.username is not None:
-            url += f"{self.username}"
+            url += f"{quote_plus(self.username)}"
             if self.password is not None:
-                url += f":{self.password.get_secret_value()}"
+                url += f":{quote_plus(self.password.get_secret_value())}"
             url += "@"
         url += f"{self.host_port}"
         if self.catalog is not None:
             url += f"/{self.catalog}"
             if self.database is not None:
                 url += f"/{self.database}"
+
+        if self.options is not None:
+            if self.database is None:
+                url += "/"
+            params = "&".join(
+                f"{key}={quote_plus(value)}"
+                for (key, value) in self.options.items()
+                if value
+            )
+            url = f"{url}?{params}"
         return url
 
 
