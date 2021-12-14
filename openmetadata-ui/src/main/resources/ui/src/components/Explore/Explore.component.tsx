1be2fdbc09f06523e60022bc4eb78eb53d740fd4
darth-coder00 <aashit@getcollate.io>
2021-12-14 19:02:32 +0530

    Wraping fixed for tabs

--- openmetadata-ui/src/main/resources/ui/src/components/Explore/Explore.component.tsx
+++ openmetadata-ui/src/main/resources/ui/src/components/Explore/Explore.component.tsx
@@ -383,7 +383,7 @@ const Explore: React.FC<ExploreProps> = ({
   const getTabs = () => {
     return (
       <div className="tw-mb-5">
-        <nav className="tw-flex tw-flex-row tw-gh-tabs-container tw-mx-9 tw-pl-72 xl:tw-pr-64 lg:tw-pr-20 tw-justify-between">
+        <nav className="tw-flex tw-flex-row tw-gh-tabs-container tw-mx-9 tw-pl-72 xl:tw-pr-64 lg:tw-pr-0 tw-justify-between">
           <div>
             {tabsInfo.map((tabDetail, index) => (
               <button
