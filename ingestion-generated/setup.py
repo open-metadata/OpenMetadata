4fc5be13c4401e8e8443b5039b732945b6b831d2
Alberto Miorin <32617+amiorin@users.noreply.github.com>
2021-12-16 10:44:08 +0100

    Fix #1670: Python generated code is missing when using pip install git+https

---
+++ ingestion-generated/setup.py
@@ -0,0 +1,39 @@
+#  Copyright 2021 Collate
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#  http://www.apache.org/licenses/LICENSE-2.0
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+import os
+
+from setuptools import setup, find_namespace_packages
+
+
+def get_long_description():
+    root = os.path.dirname(__file__)
+    with open(os.path.join(root, "README.md")) as f:
+        description = f.read()
+    return description
+
+
+setup(name="openmetadata-ingestion-generated",
+      version="0.5.0",
+      url="https://open-metadata.org/",
+      author="OpenMetadata Committers",
+      license="Apache License 2.0",
+      description="These are the generated Python classes from JSON Schema",
+      long_description=get_long_description(),
+      long_description_content_type="text/markdown",
+      python_requires=">=3.8",
+      package_dir={"": "src"},
+      zip_safe=False,
+      project_urls={
+          "Documentation": "https://docs.open-metadata.org/",
+          "Source": "https://github.com/open-metadata/OpenMetadata",
+      },
+      packages=find_namespace_packages(where="./src", exclude=["tests*"]),
+      )
