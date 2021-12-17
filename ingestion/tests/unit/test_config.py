f559a7c786a24e46b47157f33641f83f40a84045
pmbrull <p.brull@stuart.com>
2021-12-17 20:24:04 +0100

    Prepare tests for basic config cases

---
+++ ingestion/tests/unit/test_config.py
@@ -0,0 +1,58 @@
+#  Copyright 2021 Collate
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#  http://www.apache.org/licenses/LICENSE-2.0
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+"""
+Test module for loading configs
+"""
+import json
+import os
+from pathlib import Path
+from unittest import TestCase
+
+from metadata.config.common import ConfigurationError, load_config_file
+
+
+class TestConfig(TestCase):
+    """
+    Check config reading
+    """
+
+    basedir = os.path.join("resources", "config")
+
+    def test_basic(self):
+        """
+        Load basic config file
+        """
+        basic_file = Path(os.path.join(self.basedir, "basic.json"))
+        loaded = load_config_file(basic_file)
+
+        with basic_file.open() as file:
+            expected = json.loads(file.read())
+
+        assert loaded == expected
+
+    def test_invalid(self):
+        """
+        Fail with non existent file
+        """
+
+        no_file = Path(os.path.join(self.basedir, "random.json"))
+
+        with self.assertRaises(ConfigurationError):
+            load_config_file(no_file)
+
+    def test_bad_suffix(self):
+        """
+        Fail if not valid suffix
+        """
+        bad_suffix = Path(os.path.join(self.basedir, "basic.random"))
+
+        with self.assertRaises(ConfigurationError):
+            load_config_file(bad_suffix)
