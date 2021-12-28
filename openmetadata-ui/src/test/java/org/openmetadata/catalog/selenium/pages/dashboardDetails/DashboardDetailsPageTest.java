3a7ed94bed42d5bb021e357bef98aebd7e7f728a
Parth Panchal <parth.panchal@deuexsolutions.com>
2021-12-28 18:42:42 +0530

    minor change

--- openmetadata-ui/src/test/java/org/openmetadata/catalog/selenium/pages/dashboardDetails/DashboardDetailsPageTest.java
+++ openmetadata-ui/src/test/java/org/openmetadata/catalog/selenium/pages/dashboardDetails/DashboardDetailsPageTest.java
@@ -46,7 +46,7 @@ public class DashboardDetailsPageTest {
 
   @BeforeEach
   public void openMetadataWindow() {
-    System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/macM1/chromedriver");
+    System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/linux/chromedriver");
     ChromeOptions options = new ChromeOptions();
     options.addArguments("--headless");
     options.addArguments("--window-size=1280,800");
