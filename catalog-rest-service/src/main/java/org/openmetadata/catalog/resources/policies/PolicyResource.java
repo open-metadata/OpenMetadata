5a0ada69ca489eef2964728c681a9c12591a3885
Yasar Shaikh <yasar_shaikh@mail.com>
2022-01-06 00:27:18 +0530

    fix(#2054) : Replaced replace() with replace (#2056)

--- catalog-rest-service/src/main/java/org/openmetadata/catalog/resources/policies/PolicyResource.java
+++ catalog-rest-service/src/main/java/org/openmetadata/catalog/resources/policies/PolicyResource.java
@@ -110,7 +110,7 @@ public class PolicyResource {
   }
 
   static final String FIELDS = "displayName,description,owner,policyUrl,enabled,rules,location";
-  public static final List<String> FIELD_LIST = Arrays.asList(FIELDS.replaceAll("\\s", "").split(","));
+  public static final List<String> FIELD_LIST = Arrays.asList(FIELDS.replace(" ", "").split(","));
 
   @GET
   @Valid
