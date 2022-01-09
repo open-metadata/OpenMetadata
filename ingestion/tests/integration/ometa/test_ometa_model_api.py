81d582a275d8c20e1fa408daa0d2dbbfb0ac3f7b
pmbrull <p.brull@stuart.com>
2022-01-09 12:19:56 +0100

    Add tests for retrieving the entity reference

--- ingestion/tests/integration/ometa/test_ometa_model_api.py
+++ ingestion/tests/integration/ometa/test_ometa_model_api.py
@@ -293,7 +293,9 @@ class OMetaModelTest(TestCase):
             entity=MlModel, fqdn=self.entity.fullyQualifiedName
         )
 
-        res = self.metadata.get_list_entity_versions(entity=MlModel, entity_id=res_name.id.__root__)
+        res = self.metadata.get_list_entity_versions(
+            entity=MlModel, entity_id=res_name.id.__root__
+        )
         assert res
 
     def test_get_entity_version(self):
@@ -307,11 +309,20 @@ class OMetaModelTest(TestCase):
             entity=MlModel, fqdn=self.entity.fullyQualifiedName
         )
         res = self.metadata.get_entity_version(
-            entity=MlModel,
-            entity_id=res_name.id.__root__,
-            version=0.1
+            entity=MlModel, entity_id=res_name.id.__root__, version=0.1
         )
 
         # check we get the correct version requested and the correct entity ID
         assert res.version.__root__ == 0.1
         assert res.id == res_name.id
+
+    def test_get_entity_ref(self):
+        """
+        test get EntityReference
+        """
+        res = self.metadata.create_or_update(data=self.create)
+        entity_ref = self.metadata.get_entity_reference(
+            entity=MlModel, fqdn=res.fullyQualifiedName
+        )
+
+        assert res.id == entity_ref.id
--- ingestion/tests/integration/ometa/test_ometa_table_api.py
+++ ingestion/tests/integration/ometa/test_ometa_table_api.py
@@ -347,7 +347,9 @@ class OMetaTableTest(TestCase):
             entity=Table, fqdn=self.entity.fullyQualifiedName
         )
 
-        res = self.metadata.get_list_entity_versions(entity=Table, entity_id=res_name.id.__root__)
+        res = self.metadata.get_list_entity_versions(
+            entity=Table, entity_id=res_name.id.__root__
+        )
         assert res
 
     def test_get_entity_version(self):
@@ -361,11 +363,20 @@ class OMetaTableTest(TestCase):
             entity=Table, fqdn=self.entity.fullyQualifiedName
         )
         res = self.metadata.get_entity_version(
-            entity=Table,
-            entity_id=res_name.id.__root__,
-            version=0.1
+            entity=Table, entity_id=res_name.id.__root__, version=0.1
         )
 
         # check we get the correct version requested and the correct entity ID
         assert res.version.__root__ == 0.1
         assert res.id == res_name.id
+
+    def test_get_entity_ref(self):
+        """
+        test get EntityReference
+        """
+        res = self.metadata.create_or_update(data=self.create)
+        entity_ref = self.metadata.get_entity_reference(
+            entity=Table, fqdn=res.fullyQualifiedName
+        )
+
+        assert res.id == entity_ref.id
