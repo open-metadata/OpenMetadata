52b3674a14a29b07088ca0c3c18e18ced49a6916
Alberto Miorin <32617+amiorin@users.noreply.github.com>
2022-01-12 15:18:20 +0100

    Add pipelineService

--- catalog-rest-service/src/main/java/org/openmetadata/catalog/resources/services/pipeline/PipelineServiceResource.java
+++ catalog-rest-service/src/main/java/org/openmetadata/catalog/resources/services/pipeline/PipelineServiceResource.java
@@ -54,6 +54,7 @@ import org.openmetadata.catalog.security.Authorizer;
 import org.openmetadata.catalog.security.SecurityUtil;
 import org.openmetadata.catalog.type.EntityHistory;
 import org.openmetadata.catalog.type.EntityReference;
+import org.openmetadata.catalog.type.Include;
 import org.openmetadata.catalog.util.RestUtil;
 import org.openmetadata.catalog.util.RestUtil.PutResponse;
 import org.openmetadata.catalog.util.ResultList;
@@ -116,15 +117,21 @@ public class PipelineServiceResource {
           String before,
       @Parameter(description = "Returns list of services after this cursor", schema = @Schema(type = "string"))
           @QueryParam("after")
-          String after)
+          String after,
+      @Parameter(
+              description = "Include all, deleted, or non-deleted entities.",
+              schema = @Schema(implementation = Include.class))
+          @QueryParam("include")
+          @DefaultValue("non-deleted")
+          Include include)
       throws IOException, GeneralSecurityException, ParseException {
     RestUtil.validateCursors(before, after);
 
     if (before != null) { // Reverse paging
-      return dao.listBefore(uriInfo, null, null, limitParam, before);
+      return dao.listBefore(uriInfo, null, null, limitParam, before, include);
     }
     // Forward paging or first page
-    return dao.listAfter(uriInfo, null, null, limitParam, after);
+    return dao.listAfter(uriInfo, null, null, limitParam, after, include);
   }
 
   @GET
@@ -142,9 +149,17 @@ public class PipelineServiceResource {
         @ApiResponse(responseCode = "404", description = "Pipeline service for instance {id} is not found")
       })
   public PipelineService get(
-      @Context UriInfo uriInfo, @Context SecurityContext securityContext, @PathParam("id") String id)
+      @Context UriInfo uriInfo,
+      @Context SecurityContext securityContext,
+      @PathParam("id") String id,
+      @Parameter(
+              description = "Include all, deleted, or non-deleted entities.",
+              schema = @Schema(implementation = Include.class))
+          @QueryParam("include")
+          @DefaultValue("non-deleted")
+          Include include)
       throws IOException, ParseException {
-    return dao.get(uriInfo, id, null);
+    return dao.get(uriInfo, id, null, include);
   }
 
   @GET
@@ -162,9 +177,17 @@ public class PipelineServiceResource {
         @ApiResponse(responseCode = "404", description = "Pipeline service for instance {id} is not found")
       })
   public PipelineService getByName(
-      @Context UriInfo uriInfo, @Context SecurityContext securityContext, @PathParam("name") String name)
+      @Context UriInfo uriInfo,
+      @Context SecurityContext securityContext,
+      @PathParam("name") String name,
+      @Parameter(
+              description = "Include all, deleted, or non-deleted entities.",
+              schema = @Schema(implementation = Include.class))
+          @QueryParam("include")
+          @DefaultValue("non-deleted")
+          Include include)
       throws IOException, ParseException {
-    return dao.getByName(uriInfo, name, null);
+    return dao.getByName(uriInfo, name, null, include);
   }
 
   @GET
