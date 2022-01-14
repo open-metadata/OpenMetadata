9cb6f3c20049a1b065187e39499ce8ac280bf854
Shailesh Parmar <shailesh.parmar.webdev@gmail.com>
2022-01-14 15:40:12 +0530

    Login page revamp (#2207)
    * rewamp login page
    
    * added carousal for login
    
    * addressing comments

---
+++ openmetadata-ui/src/main/resources/ui/src/constants/login.const.ts
@@ -0,0 +1,47 @@
+/*
+ *  Copyright 2021 Collate
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+import screenShot1 from '../assets/img/screen-shot1.png';
+
+export const LOGIN_SLIDE = [
+  {
+    image: screenShot1,
+    description:
+      'Centralized Metadata Store, Discover, Collaborate and get your Data Right',
+  },
+  {
+    image: screenShot1,
+    description:
+      'Centralized Metadata Store, Discover, Collaborate and get your Data Right',
+  },
+  {
+    image: screenShot1,
+    description:
+      'Centralized Metadata Store, Discover, Collaborate and get your Data Right',
+  },
+  {
+    image: screenShot1,
+    description:
+      'Centralized Metadata Store, Discover, Collaborate and get your Data Right',
+  },
+];
+
+export const LOGIN_SLIDER_SETTINGS = {
+  dots: true,
+  dotsClass: 'login-slider slick-dots',
+  arrows: false,
+  infinite: true,
+  speed: 200,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+};
