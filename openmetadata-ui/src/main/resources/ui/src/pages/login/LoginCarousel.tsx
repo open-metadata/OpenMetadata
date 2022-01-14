e6fb69c59167ed11e731cae355f9e99eee5547aa
Shailesh Parmar <shailesh.parmar.webdev@gmail.com>
2022-01-14 14:59:39 +0530

    addressing comments

--- openmetadata-ui/src/main/resources/ui/src/pages/login/LoginCarousel.tsx
+++ openmetadata-ui/src/main/resources/ui/src/pages/login/LoginCarousel.tsx
@@ -1,22 +1,28 @@
+/*
+ *  Copyright 2021 Collate
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 import { uniqueId } from 'lodash';
 import React from 'react';
 import Slider from 'react-slick';
-import { LOGIN_SLIDE } from '../../constants/login.const';
+import {
+  LOGIN_SLIDE,
+  LOGIN_SLIDER_SETTINGS,
+} from '../../constants/login.const';
 
 const LoginCarousel = () => {
-  const settings = {
-    dots: true,
-    dotsClass: 'login-slider slick-dots',
-    arrows: false,
-    infinite: true,
-    speed: 200,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-  };
-
   return (
     <div style={{ width: '633px' }}>
-      <Slider {...settings}>
+      <Slider {...LOGIN_SLIDER_SETTINGS}>
         {LOGIN_SLIDE.map((data) => (
           <div key={uniqueId()}>
             <div>
