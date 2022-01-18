1d8cd1ad6f193af8e88fffa2ea0f08123b96f9c4
Sachin-chaurasiya <sachinchaurasiyachotey87@gmail.com>
2022-01-18 20:54:24 +0530

    Addressing review comment

--- openmetadata-ui/src/main/resources/ui/src/components/dropdown/DropDownList.tsx
+++ openmetadata-ui/src/main/resources/ui/src/components/dropdown/DropDownList.tsx
@@ -12,7 +12,7 @@
  */
 
 import classNames from 'classnames';
-import { isNil, isUndefined } from 'lodash';
+import { isNil, isUndefined, toLower } from 'lodash';
 import React, { FunctionComponent, useEffect, useRef, useState } from 'react';
 import { useWindowDimensions } from '../../hooks/useWindowDimensions';
 import { getCountBadge } from '../../utils/CommonUtils';
@@ -92,9 +92,7 @@ const DropDownList: FunctionComponent<DropDownListProp> = ({
   useEffect(() => {
     setSearchedList(
       dropDownList.filter((item) => {
-        return (item.name as string)
-          .toLowerCase()
-          .includes(searchText.toLowerCase());
+        return toLower(item.name as string).includes(toLower(searchText));
       })
     );
   }, [searchText, dropDownList]);
