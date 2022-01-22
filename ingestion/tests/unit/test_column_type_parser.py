316dc292ee45d6d32ecb66aa9eb13b75e57ec67a
Ayush Shah <ayush@getcollate.io>
2022-01-22 19:48:04 +0530

    Added Tests

--- ingestion/tests/unit/test_column_type_parser.py
+++ ingestion/tests/unit/test_column_type_parser.py
@@ -7,7 +7,9 @@ COLUMN_TYPE_PARSE = [
     "struct<a:int,b:string>",
     "struct<a:struct<b:array<string>,c:bigint>>",
     "struct<a:array<string>>",
-
+    "struct<bigquerytestdatatype51:array<struct<bigquery_test_datatype_511:array<string>>>>",
+    "struct<record_1:struct<record_2:struct<record_3:struct<record_4:string>>>>",
+    "array<struct<check_datatype:array<string>>>",
 ]
 
 EXPECTED_OUTPUT = [
@@ -56,6 +58,55 @@ EXPECTED_OUTPUT = [
         "dataTypeDisplay": "struct<a:array<string>>",
         "dataType": "STRUCT",
     },
+    {
+        "children": [
+            {
+                "dataType": "ARRAY",
+                "arrayDataType": "STRUCT",
+                "dataTypeDisplay": "array<struct<bigquery_test_datatype_511:array<string>>>",
+                "name": "bigquerytestdatatype51",
+            }
+        ],
+        "dataTypeDisplay": "struct<bigquerytestdatatype51:array<struct<bigquery_test_datatype_511:array<string>>>>",
+        "dataType": "STRUCT",
+    },
+    {
+        "children": [
+            {
+                "children": [
+                    {
+                        "children": [
+                            {
+                                "children": [
+                                    {
+                                        "dataType": "STRING",
+                                        "dataTypeDisplay": "string",
+                                        "name": "record_4",
+                                    }
+                                ],
+                                "dataTypeDisplay": "struct<record_4:string>",
+                                "dataType": "STRUCT",
+                                "name": "record_3",
+                            }
+                        ],
+                        "dataTypeDisplay": "struct<record_3:struct<record_4:string>>",
+                        "dataType": "STRUCT",
+                        "name": "record_2",
+                    }
+                ],
+                "dataTypeDisplay": "struct<record_2:struct<record_3:struct<record_4:string>>>",
+                "dataType": "STRUCT",
+                "name": "record_1",
+            }
+        ],
+        "dataTypeDisplay": "struct<record_1:struct<record_2:struct<record_3:struct<record_4:string>>>>",
+        "dataType": "STRUCT",
+    },
+    {
+        "dataType": "ARRAY",
+        "arrayDataType": "STRUCT",
+        "dataTypeDisplay": "array<struct<check_datatype:array<string>>>",
+    },
 ]
 
 
@@ -63,5 +114,7 @@ class ColumnTypeParseTest(TestCase):
     def test_check_datatype_support(self):
         for index, parse_string in enumerate(COLUMN_TYPE_PARSE):
             parsed_string = ColumnTypeParser._parse_datatype_string(parse_string)
-            print(parsed_string)
-            self.assertTrue(True if parsed_string == EXPECTED_OUTPUT[index] else False,msg=f"{index}: {COLUMN_TYPE_PARSE[index] }")
+            self.assertTrue(
+                True if parsed_string == EXPECTED_OUTPUT[index] else False,
+                msg=f"{index}: {COLUMN_TYPE_PARSE[index]} : {parsed_string}",
+            )
