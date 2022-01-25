1dd9b0fa9c52528a0afdeb763182fc1b636791e2
Akash-Jain <Akash.J@deuexsolutions.com>
2022-01-26 01:29:01 +0530

    updating ingestion version to 0.8.0

--- ingestion/setup.py
+++ ingestion/setup.py
@@ -134,7 +134,7 @@ test = {
 build_options = {"includes": ["_cffi_backend"]}
 setup(
     name="openmetadata-ingestion",
-    version="0.9.0.dev0",
+    version="0.8.0",
     url="https://open-metadata.org/",
     author="OpenMetadata Committers",
     license="Apache License 2.0",
