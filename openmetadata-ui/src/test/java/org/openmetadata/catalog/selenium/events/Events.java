9809f7d00f7f7765bbb1564e025e8432981a084e
Parth Panchal <parth.panchal@deuexsolutions.com>
2022-02-01 13:05:17 +0530

    addressing style-check

--- openmetadata-ui/src/test/java/org/openmetadata/catalog/selenium/events/Events.java
+++ openmetadata-ui/src/test/java/org/openmetadata/catalog/selenium/events/Events.java
@@ -16,7 +16,6 @@ package org.openmetadata.catalog.selenium.events;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
--- openmetadata-ui/src/test/java/org/openmetadata/catalog/selenium/objectRepository/TagsPage.java
+++ openmetadata-ui/src/test/java/org/openmetadata/catalog/selenium/objectRepository/TagsPage.java
@@ -15,7 +15,6 @@ package org.openmetadata.catalog.selenium.objectRepository;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 
 public class TagsPage {
   WebDriver webDriver;
