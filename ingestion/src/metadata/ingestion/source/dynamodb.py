f920e88fe73dcfc347e3fc97ceca941ddd7f0586
codingwithabhi <imabhipro@gmail.com>
2022-02-03 21:40:02 +0530

    run-python-formatter

--- ingestion/src/metadata/ingestion/source/dynamodb.py
+++ ingestion/src/metadata/ingestion/source/dynamodb.py
@@ -1,27 +1,25 @@
 import logging
 import traceback
 import uuid
+from typing import Any, Generic, Iterable, List
 
 from metadata.generated.schema.entity.data.database import Database
 from metadata.generated.schema.entity.data.location import Location, LocationType
 from metadata.generated.schema.entity.data.pipeline import Pipeline, Task
 from metadata.generated.schema.entity.data.table import Column, Table
+from metadata.generated.schema.entity.services.databaseService import (
+    DatabaseServiceType,
+)
+from metadata.generated.schema.type.entityReference import EntityReference
+from metadata.ingestion.api.common import Entity, IncludeFilterPattern
 from metadata.ingestion.api.source import Source, SourceStatus
 from metadata.ingestion.models.ometa_table_db import OMetaDatabaseAndTable
 from metadata.ingestion.ometa.ometa_api import OpenMetadata
 from metadata.ingestion.ometa.openmetadata_rest import MetadataServerConfig
-from metadata.utils.aws_client import AWSClientConfigModel, AWSClient
-from metadata.generated.schema.entity.services.databaseService import (
-    DatabaseServiceType,
-)
-
 from metadata.ingestion.source.sql_source_common import SQLSourceStatus
+from metadata.utils.aws_client import AWSClient, AWSClientConfigModel
 from metadata.utils.column_type_parser import ColumnTypeParser
-from metadata.generated.schema.type.entityReference import EntityReference
-from metadata.ingestion.api.common import Entity, IncludeFilterPattern
 from metadata.utils.helpers import get_database_service_or_create
-
-from typing import Any, Generic, Iterable, List
 
 logger: logging.Logger = logging.getLogger(__name__)
 
