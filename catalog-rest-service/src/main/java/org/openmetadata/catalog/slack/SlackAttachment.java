fb8d5e94c9424a6ffabf6c32d988a3cd01343206
Matt <mithmatt@gmail.com>
2022-02-05 13:46:38 -0800

    Use @Getter @Setter annotations for configurations

--- catalog-rest-service/src/main/java/org/openmetadata/catalog/slack/SlackAttachment.java
+++ catalog-rest-service/src/main/java/org/openmetadata/catalog/slack/SlackAttachment.java
@@ -3,171 +3,61 @@ package org.openmetadata.catalog.slack;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class SlackAttachment {
-  private String fallback;
-  private String color;
-  private String pretext;
+  @Getter @Setter private String fallback;
+  @Getter @Setter private String color;
+  @Getter @Setter private String pretext;
 
   @JsonProperty("author_name")
+  @Getter
+  @Setter
   private String authorName;
 
   @JsonProperty("author_link")
+  @Getter
+  @Setter
   private String authorLink;
 
   @JsonProperty("author_icon")
+  @Getter
+  @Setter
   private String authorIcon;
 
-  private String title;
+  @Getter @Setter private String title;
 
   @JsonProperty("title_link")
+  @Getter
+  @Setter
   private String titleLink;
 
-  private String text;
-  private Field[] fields;
+  @Getter @Setter private String text;
+  @Getter @Setter private Field[] fields;
 
   @JsonProperty("image_url")
+  @Getter
+  @Setter
   private String imageUrl;
 
   @JsonProperty("thumb_url")
+  @Getter
+  @Setter
   private String thumbUrl;
 
-  private String footer;
+  @Getter @Setter private String footer;
 
   @JsonProperty("footer_icon")
+  @Getter
+  @Setter
   private String footerIcon;
 
-  private String ts;
+  @Getter @Setter private String ts;
 
   @JsonProperty("mrkdwn_in")
+  @Getter
+  @Setter
   private List<String> markdownIn;
-
-  public String getFallback() {
-    return fallback;
-  }
-
-  public void setFallback(String fallback) {
-    this.fallback = fallback;
-  }
-
-  public String getColor() {
-    return color;
-  }
-
-  public void setColor(String color) {
-    this.color = color;
-  }
-
-  public String getPretext() {
-    return pretext;
-  }
-
-  public void setPretext(String pretext) {
-    this.pretext = pretext;
-  }
-
-  public String getAuthorName() {
-    return authorName;
-  }
-
-  public void setAuthorName(String authorName) {
-    this.authorName = authorName;
-  }
-
-  public String getAuthorLink() {
-    return authorLink;
-  }
-
-  public void setAuthorLink(String authorLink) {
-    this.authorLink = authorLink;
-  }
-
-  public String getAuthorIcon() {
-    return authorIcon;
-  }
-
-  public void setAuthorIcon(String authorIcon) {
-    this.authorIcon = authorIcon;
-  }
-
-  public String getTitle() {
-    return title;
-  }
-
-  public void setTitle(String title) {
-    this.title = title;
-  }
-
-  public String getTitleLink() {
-    return titleLink;
-  }
-
-  public void setTitleLink(String titleLink) {
-    this.titleLink = titleLink;
-  }
-
-  public String getText() {
-    return text;
-  }
-
-  public void setText(String text) {
-    this.text = text;
-  }
-
-  public Field[] getFields() {
-    return fields;
-  }
-
-  public void setFields(Field[] fields) {
-    this.fields = fields;
-  }
-
-  public String getImageUrl() {
-    return imageUrl;
-  }
-
-  public void setImageUrl(String imageUrl) {
-    this.imageUrl = imageUrl;
-  }
-
-  public String getThumbUrl() {
-    return thumbUrl;
-  }
-
-  public void setThumbUrl(String thumbUrl) {
-    this.thumbUrl = thumbUrl;
-  }
-
-  public String getFooter() {
-    return footer;
-  }
-
-  public void setFooter(String footer) {
-    this.footer = footer;
-  }
-
-  public String getFooterIcon() {
-    return footerIcon;
-  }
-
-  public void setFooterIcon(String footerIcon) {
-    this.footerIcon = footerIcon;
-  }
-
-  public String getTs() {
-    return ts;
-  }
-
-  public void setTs(String ts) {
-    this.ts = ts;
-  }
-
-  public List<String> getMarkdownIn() {
-    return markdownIn;
-  }
-
-  public void setMarkdownIn(List<String> markdownIn) {
-    this.markdownIn = markdownIn;
-  }
 }
