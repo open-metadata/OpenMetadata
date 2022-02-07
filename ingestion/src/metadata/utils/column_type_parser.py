888e225544b335c5b30443af4c8fea31379bf253
codingwithabhi <63392662+codingwithabhi@users.noreply.github.com>
2022-02-07 10:17:21 +0530

    Snowflake ingestion fix (#2622)
    * creds-removed
    
    * fixed-ingestion-fail-and-variant-type-error

--- ingestion/src/metadata/utils/column_type_parser.py
+++ ingestion/src/metadata/utils/column_type_parser.py
@@ -146,7 +146,7 @@ class ColumnTypeParser:
         if isinstance(column_type, types.NullType):
             return "NULL"
         for sql_type in ColumnTypeParser._SOURCE_TYPE_TO_OM_TYPE.keys():
-            if str(column_type) in sql_type:
+            if str(column_type) == sql_type:
                 type_class = ColumnTypeParser._SOURCE_TYPE_TO_OM_TYPE[sql_type]
                 break
         if type_class is None or type_class == "NULL":
