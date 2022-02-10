ecf9e855dca16c5be3fc0f45e54bedd2b3e2774e
kushal <kushalshinde2512@gmail.com>
2022-02-10 18:13:44 +0530

    Revamp pipeline details page

---
+++ openmetadata-ui/src/test/java/org/openmetadata/catalog/selenium/objectRepository/PipelineDetails.java
@@ -0,0 +1,50 @@
+package org.openmetadata.catalog.selenium.objectRepository;
+
+import java.util.List;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class PipelineDetails {
+  WebDriver webDriver;
+
+  public PipelineDetails(WebDriver webDriver) {
+    this.webDriver = webDriver;
+  }
+
+  By pipelines = By.xpath("(//button[@data-testid='tab'])[4]");
+  By selectedTag = By.xpath("//span[@class='tw-no-underline hover:tw-no-underline tw-py-0.5 tw-px-2 tw-pl-2 tw-pr-1']");
+  By editTaskDescription = By.xpath("(//img[@data-testid='image']/parent::button)[3]");
+  By descriptionBox = By.xpath("(//div[@data-testid='description'])[2]");
+  By lineage = By.xpath("//button[@data-testid='tab'][@id='lineage']");;
+  By lineageComponents = By.xpath("//span[@data-testid='lineage-entity']");
+  By sideDrawerLineage = By.xpath("//header[@class=\"tw-flex tw-justify-between\"]");
+
+  public By sideDrawer() {
+    return sideDrawerLineage;
+  }
+
+  public List<WebElement> lineageNodes() {
+    return webDriver.findElements(lineageComponents);
+  }
+
+  public By lineage() {
+    return lineage;
+  }
+
+  public By getPipelines() {
+    return pipelines;
+  }
+
+  public By getSelectedTag() {
+    return selectedTag;
+  }
+
+  public By getEditTaskDescription() {
+    return editTaskDescription;
+  }
+
+  public WebElement getDescriptionBox() {
+    return webDriver.findElement(descriptionBox);
+  }
+}
