ce846c6afcdffc3740ba353136e7b126e0edfc95
Igor Kramer <igor.kramer@gmail.com>
2022-02-10 13:14:28 +0300

    ISSUE-2706: Fix tableau ingestion bugs

--- ingestion/src/metadata/ingestion/source/tableau.py
+++ ingestion/src/metadata/ingestion/source/tableau.py
@@ -115,7 +115,7 @@ class TableauSource(Source[Entity]):
         }
         if self.config.username and self.config.password:
             tableau_server_config[self.config.env]["username"] = self.config.username
-            tableau_server_config[self.config.env]["password"] = self.config.password
+            tableau_server_config[self.config.env]["password"] = self.config.password.get_secret_value()
         elif (
             self.config.personal_access_token_name
             and self.config.personal_access_token_secret
@@ -159,10 +159,13 @@ class TableauSource(Source[Entity]):
         Returns:
             List[DashboardOwner]
         """
+        parts = owner["fullName"].split(" ")
+        first_name = " ".join(parts[:len(owner)//2])
+        last_name = " ".join(parts[len(owner)//2:])
         return [
             DashboardOwner(
-                first_name=owner["fullName"].split(" ")[0],
-                last_name=owner["fullName"].split(" ")[1],
+                first_name=first_name,
+                last_name=last_name,
                 username=owner["name"],
             )
         ]
