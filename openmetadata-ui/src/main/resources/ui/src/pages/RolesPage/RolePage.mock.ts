47aae8994eac04aa5fdf198fb872d42824f536d4
darth-coder00 <aashit@getcollate.io>
2022-02-14 23:21:03 +0530

    Added new Unit tests

---
+++ openmetadata-ui/src/main/resources/ui/src/pages/RolesPage/RolePage.mock.ts
@@ -0,0 +1,126 @@
+export const mockGetRole = {
+  data: [
+    {
+      id: 'c605dad7-a90b-4672-954b-37e9cae2eae9',
+      name: 'DataConsumer',
+      displayName: 'Data Consumer',
+      description:
+        'Users with Data Consumer role use different data assets for their day to day work.',
+      version: 0.1,
+      updatedAt: 1644389269714,
+      updatedBy: 'admin',
+      href: 'http://localhost:8585/api/v1/roles/c605dad7-a90b-4672-954b-37e9cae2eae9',
+      deleted: false,
+      policy: {
+        id: 'noRule',
+        type: 'policy',
+        name: 'DataConsumerRoleAccessControlPolicy',
+        description:
+          'Policy for Data Consumer Role to perform operations on metadata entities',
+        displayName: 'Data Consumer Role Access Control Policy',
+        href: 'http://localhost:8585/api/v1/policies/8d6b23bd-400b-4537-a1f0-29acb4c6f366',
+      },
+      users: [],
+    },
+    {
+      id: 'bff587a4-fcc4-4dd0-aae6-394bef7c3751',
+      name: 'DataSteward',
+      displayName: 'Data Steward',
+      description:
+        'Users with Data Steward role are responsible for ensuring correctness of metadata for data assets',
+      version: 0.1,
+      updatedAt: 1644389269724,
+      updatedBy: 'admin',
+      href: 'http://localhost:8585/api/v1/roles/bff587a4-fcc4-4dd0-aae6-394bef7c3751',
+      deleted: false,
+      policy: {
+        id: 'withRule',
+        type: 'policy',
+        name: 'DataStewardRoleAccessControlPolicy',
+        description:
+          'Policy for Data Steward Role to perform operations on metadata entities',
+        displayName: 'Data Steward Role Access Control Policy',
+        href: 'http://localhost:8585/api/v1/policies/f1c8f8a3-6d76-4352-8686-376fc0c7f86f',
+      },
+      users: [
+        {
+          id: 'dbd39398-f25c-48f4-9bc3-a4687c2d324c',
+          type: 'user',
+          name: 'aaron_johnson0',
+          displayName: 'Aaron Johnson',
+          href: 'http://localhost:8585/api/v1/users/dbd39398-f25c-48f4-9bc3-a4687c2d324c',
+        },
+      ],
+    },
+  ],
+  paging: {
+    total: 2,
+  },
+};
+
+export const mockGetPolicyNoRuleData = {
+  id: '8d6b23bd-400b-4537-a1f0-29acb4c6f366',
+  name: 'DataConsumerRoleAccessControlPolicy',
+  fullyQualifiedName: 'DataConsumerRoleAccessControlPolicy',
+  displayName: 'Data Consumer Role Access Control Policy',
+  description:
+    'Policy for Data Consumer Role to perform operations on metadata entities',
+  href: 'http://localhost:8585/api/v1/policies/8d6b23bd-400b-4537-a1f0-29acb4c6f366',
+  policyType: 'AccessControl',
+  enabled: true,
+  version: 0.1,
+  updatedAt: 1644389269835,
+  updatedBy: 'admin',
+  rules: [],
+  deleted: false,
+};
+
+export const mockGetPolicyWithRuleData = {
+  id: 'f1c8f8a3-6d76-4352-8686-376fc0c7f86f',
+  name: 'DataStewardRoleAccessControlPolicy',
+  fullyQualifiedName: 'DataStewardRoleAccessControlPolicy',
+  displayName: 'Data Steward Role Access Control Policy',
+  description:
+    'Policy for Data Steward Role to perform operations on metadata entities',
+  href: 'http://localhost:8585/api/v1/policies/f1c8f8a3-6d76-4352-8686-376fc0c7f86f',
+  policyType: 'AccessControl',
+  enabled: true,
+  version: 0.1,
+  updatedAt: 1644389269821,
+  updatedBy: 'admin',
+  rules: [
+    {
+      name: 'DataStewardRoleAccessControlPolicy-UpdateDescription',
+      allow: true,
+      enabled: true,
+      priority: 1000,
+      operation: 'UpdateDescription',
+      userRoleAttr: 'DataSteward',
+    },
+    {
+      name: 'DataStewardRoleAccessControlPolicy-UpdateLineage',
+      allow: true,
+      enabled: true,
+      priority: 1000,
+      operation: 'UpdateLineage',
+      userRoleAttr: 'DataSteward',
+    },
+    {
+      name: 'DataStewardRoleAccessControlPolicy-UpdateOwner',
+      allow: true,
+      enabled: true,
+      priority: 1000,
+      operation: 'UpdateOwner',
+      userRoleAttr: 'DataSteward',
+    },
+    {
+      name: 'DataStewardRoleAccessControlPolicy-UpdateTags',
+      allow: true,
+      enabled: true,
+      priority: 1000,
+      operation: 'UpdateTags',
+      userRoleAttr: 'DataSteward',
+    },
+  ],
+  deleted: false,
+};
---
+++ openmetadata-ui/src/main/resources/ui/src/pages/RolesPage/RolesPage.test.tsx
@@ -0,0 +1,210 @@
+import {
+  act,
+  findAllByTestId,
+  findAllByText,
+  findByTestId,
+  findByText,
+  fireEvent,
+  render,
+} from '@testing-library/react';
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import {
+  mockGetPolicyNoRuleData,
+  mockGetPolicyWithRuleData,
+  mockGetRole,
+} from './RolePage.mock';
+import RolesPage from './RolesPage.component';
+
+jest.mock('../../axiosAPIs/rolesAPI', () => ({
+  createRole: jest.fn(),
+  getPolicy: jest.fn().mockImplementation((id: string) => {
+    return Promise.resolve({
+      data:
+        id === 'noRule' ? mockGetPolicyNoRuleData : mockGetPolicyWithRuleData,
+    });
+  }),
+  getRoleByName: jest.fn(),
+  getRoles: jest
+    .fn()
+    .mockImplementation(() => Promise.resolve({ data: mockGetRole })),
+  updatePolicy: jest.fn(),
+  updateRole: jest.fn(),
+}));
+
+jest.mock(
+  '../../components/containers/PageLayout',
+  () =>
+    ({
+      children,
+      leftPanel,
+      rightPanel,
+    }: {
+      children: React.ReactNode;
+      rightPanel: React.ReactNode;
+      leftPanel: React.ReactNode;
+    }) =>
+      (
+        <div data-testid="PageLayout">
+          <div data-testid="left-panel-content">{leftPanel}</div>
+          <div data-testid="right-panel-content">{rightPanel}</div>
+          {children}
+        </div>
+      )
+);
+
+jest.mock('../../components/common/non-admin-action/NonAdminAction', () => {
+  return jest
+    .fn()
+    .mockImplementation(({ children }: { children: React.ReactNode }) => (
+      <span>{children}</span>
+    ));
+});
+
+jest.mock('../../components/containers/PageContainerV1', () => {
+  return jest
+    .fn()
+    .mockImplementation(({ children }: { children: React.ReactNode }) => (
+      <div data-testid="PageContainerV1">{children}</div>
+    ));
+});
+
+jest.mock('../../components/common/description/Description', () => {
+  return jest.fn().mockReturnValue(<p>Description component</p>);
+});
+
+jest.mock('../../components/Modals/ConfirmationModal/ConfirmationModal', () => {
+  return jest.fn().mockReturnValue(<p>ConfirmationModal component</p>);
+});
+
+jest.mock('../../components/Modals/RulesModal/AddRuleModal', () => {
+  return jest.fn().mockReturnValue(<p>AddRuleModal component</p>);
+});
+
+jest.mock('../teams/Form', () => {
+  return jest.fn().mockReturnValue(<p>Form component</p>);
+});
+
+jest.mock('../teams/UserCard', () => {
+  return jest.fn().mockReturnValue(<p>UserCardComponent</p>);
+});
+
+describe('Test RolesPage component', () => {
+  it('RolesPage component should render properly', async () => {
+    const { container } = render(<RolesPage />, {
+      wrapper: MemoryRouter,
+    });
+
+    const PageContainerV1 = await findByTestId(container, 'PageContainerV1');
+    const PageLayout = await findByTestId(container, 'PageLayout');
+    const leftPanelContent = await findByTestId(
+      container,
+      'left-panel-content'
+    );
+    const leftPanelTitle = await findByTestId(container, 'left-panel-title');
+    const leftPanelAddRoleButton = await findByTestId(container, 'add-role');
+    const roleNames = await findAllByTestId(container, 'role-name-container');
+
+    const roleContainer = await findByTestId(container, 'role-container');
+    const header = await findByTestId(container, 'header');
+    const headerTitle = await findByTestId(container, 'header-title');
+    const addNewRuleButton = await findByTestId(
+      container,
+      'add-new-rule-button'
+    );
+    const tabs = await findByTestId(container, 'tabs');
+    const description = await findByText(container, /Description component/i);
+
+    expect(PageContainerV1).toBeInTheDocument();
+    expect(PageLayout).toBeInTheDocument();
+    expect(leftPanelContent).toBeInTheDocument();
+    expect(leftPanelTitle).toBeInTheDocument();
+    expect(leftPanelAddRoleButton).toBeInTheDocument();
+    expect(roleNames.length).toBe(mockGetRole.data.length);
+    expect(roleNames[0]).toHaveClass('activeCategory');
+    expect(roleContainer).toBeInTheDocument();
+    expect(header).toBeInTheDocument();
+    expect(headerTitle).toBeInTheDocument();
+    expect(headerTitle.textContent).toBe(mockGetRole.data[0].displayName);
+    expect(addNewRuleButton).toBeInTheDocument();
+    expect(tabs).toBeInTheDocument();
+    expect(tabs.childElementCount).toBe(2);
+    expect(tabs.children[0].textContent).toBe('Policy');
+    expect(tabs.children[1].textContent).toBe('Users');
+    expect(tabs.children[0]).toHaveClass('active');
+    expect(description).toBeInTheDocument();
+  });
+
+  it('Check no rule and no user behaviour', async () => {
+    const { container } = render(<RolesPage />, {
+      wrapper: MemoryRouter,
+    });
+    // checking No Rules Added. directly as there is no data available on 1st instance
+
+    const usersButton = await findByTestId(container, 'users');
+
+    expect(await findByText(container, /No Rules Added./i)).toBeInTheDocument();
+
+    fireEvent.click(usersButton);
+
+    expect(await findByText(container, /No Users Added./i)).toBeInTheDocument();
+  });
+
+  it('Check behaviour when there is data in policy and user', async () => {
+    const { container } = render(<RolesPage />, {
+      wrapper: MemoryRouter,
+    });
+    const roleNames = await findAllByTestId(container, 'role-name-container');
+    const usersButton = await findByTestId(container, 'users');
+    fireEvent.click(roleNames[1]);
+
+    expect(roleNames[1]).toHaveClass('activeCategory');
+
+    await act(async () => {
+      fireEvent.click(roleNames[1]);
+
+      expect(roleNames[1]).toHaveClass('activeCategory');
+    });
+
+    const headerTitle = await findByTestId(container, 'header-title');
+    const table = await findByTestId(container, 'table');
+    const tableHeading = await findAllByTestId(container, 'table-heading');
+
+    expect(headerTitle).toBeInTheDocument();
+    expect(headerTitle.textContent).toBe(mockGetRole.data[1].displayName);
+    expect(table).toBeInTheDocument();
+    expect(tableHeading.map((heading) => heading.textContent)).toStrictEqual([
+      'Operation',
+      'Access',
+      'Enabled',
+      'Action',
+    ]);
+
+    fireEvent.click(usersButton);
+    const users = await findAllByText(container, /UserCardComponent/);
+
+    expect(usersButton).toHaveClass('active');
+    expect(users.length).toBe(mockGetRole.data[1].users.length);
+  });
+
+  it('CTA should work', async () => {
+    const { container } = render(<RolesPage />, {
+      wrapper: MemoryRouter,
+    });
+
+    const addRoleButton = await findByTestId(container, 'add-role');
+    const addNewRuleButton = await findByTestId(
+      container,
+      'add-new-rule-button'
+    );
+    fireEvent.click(addRoleButton);
+
+    expect(await findByText(container, /Form component/i)).toBeInTheDocument();
+
+    fireEvent.click(addNewRuleButton);
+
+    expect(
+      await findByText(container, /AddRuleModal component/i)
+    ).toBeInTheDocument();
+  });
+});
