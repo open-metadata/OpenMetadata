5a6b755b69ba053d902d9ca8f9a43a79fa4d7ca7
ulixius9 <mayursingal9@gmail.com>
2022-02-14 14:56:39 +0530

    optional db and licence info

--- ingestion/src/metadata/ingestion/source/databricks.py
+++ ingestion/src/metadata/ingestion/source/databricks.py
@@ -1,3 +1,16 @@
+#  Copyright 2021 Collate
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#  http://www.apache.org/licenses/LICENSE-2.0
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+from typing import Optional
+
 from metadata.ingestion.ometa.openmetadata_rest import MetadataServerConfig
 from metadata.ingestion.source.sql_source import SQLSource
 from metadata.ingestion.source.sql_source_common import SQLConnectionConfig
@@ -7,10 +20,14 @@ class DatabricksConfig(SQLConnectionConfig):
     host_port = "localhost:443"
     scheme = "databricks+pyhive"
     service_type = "Databricks"
-    token = "token"
+    token: str
+    database: Optional[str]
 
     def get_connection_url(self):
-        return f"{self.scheme}://token:{self.token}@{self.host_port}/{self.database}"
+        url = f"{self.scheme}://token:{self.token}@{self.host_port}"
+        if self.database:
+            url += f"/{self.database}"
+        return url
 
 
 class DatabricksSource(SQLSource):
