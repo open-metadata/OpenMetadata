785a176d757603b98024e5fcdffb1427b295ba75
pmbrull <peremiquelbrull@gmail.com>
2022-02-24 12:29:33 +0100

    Add np req

--- ingestion/setup.py
+++ ingestion/setup.py
@@ -141,6 +141,8 @@ test = {
     # sklearn integration
     "scikit-learn==1.0.2",
     "pandas==1.3.5",
+    # Metrics sample data gen
+    "numpy==1.22.2",
 }
 
 build_options = {"includes": ["_cffi_backend"]}
