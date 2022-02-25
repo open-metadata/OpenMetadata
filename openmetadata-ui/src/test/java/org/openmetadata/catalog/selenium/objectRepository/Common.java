9018207e955f326c4055907c1f7526767600404c
kushal <kushalshinde2512@gmail.com>
2022-02-25 11:51:12 +0530

    Java checkstyle

--- openmetadata-ui/src/test/java/org/openmetadata/catalog/selenium/objectRepository/Common.java
+++ openmetadata-ui/src/test/java/org/openmetadata/catalog/selenium/objectRepository/Common.java
@@ -86,7 +86,9 @@ public class Common {
   By addIngestion = By.cssSelector("button[data-testid='add-new-ingestion-button']");
   By name = By.cssSelector("[data-testid='name']");
 
-  public By breadcrumbTags() { return breadCrumbTags; }
+  public By breadcrumbTags() {
+    return breadCrumbTags;
+  }
 
   public List<WebElement> versionRadioButton() {
     return webDriver.findElements(versionRadioButton);
