1bfe395c2b159a4870f7886485fe2a14cfc8ef87
pmbrull <peremiquelbrull@gmail.com>
2022-02-27 19:06:25 +0100

    Update test definitions

--- ingestion/src/metadata/orm_profiler/validations/models.py
+++ ingestion/src/metadata/orm_profiler/validations/models.py
@@ -18,77 +18,33 @@ from typing import List
 from parsimonious import ParseError
 from pydantic import BaseModel, validator
 
+from metadata.config.common import ConfigModel
+from metadata.generated.schema.tests.columnTest import ColumnTest
+from metadata.generated.schema.tests.tableTest import TableTest
 from metadata.orm_profiler.utils import logger
 from metadata.orm_profiler.validations.core import Validation
 from metadata.orm_profiler.validations.grammar import ExpVisitor, parse
 
-logger = logger()
-visitor = ExpVisitor()
 
-
-class ExpressionModel(BaseModel):
+class TestDef(ConfigModel):
     """
-    We use this model to convert expression to Validations
-    on the fly while we are parsing the JSON configuration.
+    Table test definition
 
-    It is a comfortable and fast way to know if we'll be
-    able to parse the tests with our grammar.
+    We expect:
+    - table name
+    - List of table tests
+    - List of column tests
     """
 
-    expression: List[Validation]
-    enabled: bool = True
-
-    @validator("expression", pre=True)
-    def convert_expression(cls, value):  # cls as per pydantic docs
-        """
-        Make sure that we can parse the expression to a
-        validation
-        """
-        try:
-            raw_validation = parse(value, visitor)
-        except ParseError as err:
-            logger.error(f"Cannot parse expression properly: {value}")
-            raise err
-
-        return [Validation.create(val) for val in raw_validation]
-
-    class Config:
-        arbitrary_types_allowed = True
-
-
-class TableTest(ExpressionModel):
-    """
-    Incoming table test definition from the workflow
-    """
-
-    name: str
-    table: str  # fullyQualifiedName
-
-
-class ColumnTestExpression(ExpressionModel):
-    """
-    Column test expression definition
-    """
-
-    name: str
-    column: str
-
-
-class ColumnTest(BaseModel):
-    """
-    Incoming column test definition from the workflow
-    """
-
-    table: str  # fullyQualifiedName
-    name: str  # Set of col tests
-    columns: List[ColumnTestExpression]
-
-
-class TestDef(BaseModel):
-    """
-    Base Test definition
-    """
-
-    name: str
+    table: str  # Table FQDN
     table_tests: List[TableTest] = None
     column_tests: List[ColumnTest] = None
+
+
+class TestSuite(ConfigModel):
+    """
+    Config test definition
+    """
+
+    name: str  # Test Suite name
+    tests: List[TestDef]
