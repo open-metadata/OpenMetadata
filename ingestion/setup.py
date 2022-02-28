00552f6d3641ac446ec64e395a3364c810648e87
Pere Miquel Brull <peremiquelbrull@gmail.com>
2022-02-28 17:11:18 +0100

    Fix #3012 - Dependency breaking Airflow Container (#3015)
    Fix #3012 - Dependency breaking Airflow Container

--- ingestion/setup.py
+++ ingestion/setup.py
@@ -38,7 +38,7 @@ base_requirements = {
     "email-validator>=1.0.3",
     "wheel~=0.36.2",
     "python-jose==3.3.0",
-    "sqlalchemy>=1.3.24",
+    "sqlalchemy>=1.4.0",
     "sql-metadata~=2.0.0",
     "requests~=2.26",
     "cryptography",
@@ -63,6 +63,11 @@ base_plugins = {
     "sql-metadata~=2.0.0",
 }
 plugins: Dict[str, Set[str]] = {
+    "airflow-container": {
+        "marshmallow-sqlalchemy>=0.26.0",
+        "SQLAlchemy-Utils>=0.38.0",
+        "pymysql>=1.0.2",
+    },
     "amundsen": {"neo4j~=4.4.0"},
     "athena": {"PyAthena[SQLAlchemy]"},
     "azuresql": {"pyodbc"},
