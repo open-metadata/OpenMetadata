f07e91fccc1edb39693a75f29b7bcb3ebc411490
Matt <mithmatt@gmail.com>
2022-03-05 10:17:56 -0800

    Force register TeamRepository in Authorizer init

--- catalog-rest-service/src/main/java/org/openmetadata/catalog/security/DefaultAuthorizer.java
+++ catalog-rest-service/src/main/java/org/openmetadata/catalog/security/DefaultAuthorizer.java
@@ -32,6 +32,7 @@ import org.openmetadata.catalog.entity.teams.User;
 import org.openmetadata.catalog.exception.EntityNotFoundException;
 import org.openmetadata.catalog.jdbi3.CollectionDAO;
 import org.openmetadata.catalog.jdbi3.RoleRepository;
+import org.openmetadata.catalog.jdbi3.TeamRepository;
 import org.openmetadata.catalog.jdbi3.UserRepository;
 import org.openmetadata.catalog.resources.teams.UserResource;
 import org.openmetadata.catalog.security.policyevaluator.PolicyEvaluator;
@@ -61,10 +62,11 @@ public class DefaultAuthorizer implements Authorizer {
     LOG.debug("Admin users: {}", adminUsers);
     CollectionDAO collectionDAO = dbi.onDemand(CollectionDAO.class);
     this.userRepository = new UserRepository(collectionDAO);
-    // RoleRepository needs to be instantiated for Entity.DAO_MAP to populated.
-    // As we create default admin/bots we need to have RoleRepository available in DAO_MAP.
+    // RoleRepository and TeamRepository needs to be instantiated for Entity.DAO_MAP to populated.
+    // As we create default admin/bots we need to have RoleRepository and TeamRepository available in DAO_MAP.
     // This needs to be handled better in future releases.
     RoleRepository roleRepository = new RoleRepository(collectionDAO);
+    TeamRepository teamRepository = new TeamRepository(collectionDAO);
     mayBeAddAdminUsers();
     mayBeAddBotUsers();
     this.policyEvaluator = PolicyEvaluator.getInstance();
