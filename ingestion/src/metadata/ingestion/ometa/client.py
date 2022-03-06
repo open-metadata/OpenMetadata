0cff248851ed7f8b53166c7bf7feecf9187ae00f
Ayush Shah <ayush@getcollate.io>
2022-03-06 23:12:35 +0530

    Added check for datetime issue concat with float

--- ingestion/src/metadata/ingestion/ometa/client.py
+++ ingestion/src/metadata/ingestion/ometa/client.py
@@ -11,6 +11,7 @@
 """
 Python API REST wrapper and helpers
 """
+import datetime
 import logging
 import time
 from typing import Callable, List, Optional
@@ -117,6 +118,10 @@ class REST:
             self.config.access_token, expiry = self._auth_token()
             if not self.config.access_token == "no_token":
                 self.config.expires_in = time.time() + expiry - 120
+                if isinstance(expiry, datetime.datetime):
+                    self.config.expires_in = expiry.timestamp() - 120
+                else:
+                    self.config.expires_in = time.time() + expiry - 120
         headers[self.config.auth_header] = f"Bearer {self.config.access_token}"
         opts = {
             "headers": headers,
