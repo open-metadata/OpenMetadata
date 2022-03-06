82f7f5c892c1c71092f52c00be02870fb325c42d
pmbrull <peremiquelbrull@gmail.com>
2022-03-06 11:12:10 +0100

    Use ORM profiler for ingestion

--- ingestion/src/metadata/ingestion/source/sql_source.py
+++ ingestion/src/metadata/ingestion/source/sql_source.py
@@ -16,9 +16,9 @@ import logging
 import re
 import traceback
 import uuid
+from datetime import datetime
 from typing import Dict, Iterable, List, Optional, Tuple
 
-from sqlalchemy import create_engine
 from sqlalchemy.engine.reflection import Inspector
 from sqlalchemy.inspection import inspect
 from sqlalchemy.orm import Session
@@ -49,6 +49,8 @@ from metadata.ingestion.source.sql_source_common import (
     SQLConnectionConfig,
     SQLSourceStatus,
 )
+from metadata.orm_profiler.orm.converter import ometa_to_orm
+from metadata.orm_profiler.profiles.default import DefaultProfiler
 from metadata.utils.column_type_parser import ColumnTypeParser
 from metadata.utils.engines import create_and_bind_session, get_engine
 from metadata.utils.helpers import get_database_service_or_create, ingest_lineage
@@ -101,25 +103,25 @@ class SQLSource(Source[OMetaDatabaseAndTable]):
         if self.config.dbt_manifest_file is not None:
             with open(self.config.dbt_manifest_file, "r", encoding="utf-8") as manifest:
                 self.dbt_manifest = json.load(manifest)
+        self.profile_date = datetime.now()
 
-    def _instantiate_profiler(self) -> bool:
+    def run_profiler(self, table: Table, schema: str) -> Optional[TableProfile]:
         """
-        If the profiler is configured, load it and run.
+        Convert the table to an ORM object and run the ORM
+        profiler.
 
-        Return True if the profiling ran correctly
+        :param table: Table Entity to be ingested
+        :param schema: Table schema
+        :return: TableProfile
         """
         try:
-            if self.config.data_profiler_enabled:
-                if self.data_profiler is None:
-                    # pylint: disable=import-outside-toplevel
-                    from metadata.profiler.dataprofiler import DataProfiler
+            orm = ometa_to_orm(table=table, database=schema)
+            profiler = DefaultProfiler(
+                session=self.session, table=orm, profile_date=self.profile_date
+            )
+            profiler.execute()
+            return profiler.get_profile()
 
-                    # pylint: enable=import-outside-toplevel
-
-                    self.data_profiler = DataProfiler(
-                        status=self.status, config=self.config
-                    )
-                return True
         # Catch any errors during profiling init and continue ingestion
         except ModuleNotFoundError as err:
             logger.error(
@@ -129,8 +131,9 @@ class SQLSource(Source[OMetaDatabaseAndTable]):
 
         except Exception as exc:  # pylint: disable=broad-except
             logger.debug(traceback.print_exc())
-            logger.debug(f"Error loading profiler {repr(exc)}")
-        return False
+            logger.debug(f"Error running ingestion profiler {repr(exc)}")
+
+        return None
 
     @property
     def session(self) -> Session:
@@ -138,7 +141,7 @@ class SQLSource(Source[OMetaDatabaseAndTable]):
         Return the SQLAlchemy session from the engine
         """
         if not self._session:
-            session = (create_and_bind_session(self.engine),)
+            self._session = create_and_bind_session(self.engine)
 
         return self._session
 
@@ -245,14 +248,13 @@ class SQLSource(Source[OMetaDatabaseAndTable]):
                     logger.error(err)
 
                 try:
-                    if self._instantiate_profiler():
-                        profile = self.run_data_profiler(table_name, schema)
-                        table_entity.tableProfile = (
-                            [profile] if profile is not None else None
-                        )
+                    if self.config.data_profiler_enabled:
+                        profile = self.run_profiler(table=table_entity, schema=schema)
+                        table_entity.tableProfile = [profile] if profile else None
                 # Catch any errors during the profile runner and continue
                 except Exception as err:
                     logger.error(err)
+
                 # check if we have any model to associate with
                 table_entity.dataModel = self._get_data_model(schema, table_name)
 
@@ -650,32 +652,13 @@ class SQLSource(Source[OMetaDatabaseAndTable]):
             logger.error(f"{repr(err)}: {table} {err}")
             return None
 
-    def _check_col_length(self, datatype, col_raw_type):
+    @staticmethod
+    def _check_col_length(datatype, col_raw_type):
         if datatype and datatype.upper() in {"CHAR", "VARCHAR", "BINARY", "VARBINARY"}:
             try:
                 return col_raw_type.length if col_raw_type.length else 1
             except AttributeError:
                 return 1
-
-    def run_data_profiler(self, table: str, schema: str) -> TableProfile:
-        """
-        Run the profiler for a table in a schema.
-
-        Prepare specific namings for different sources, e.g. bigquery
-        """
-        dataset_name = f"{schema}.{table}"
-        self.status.scanned(f"profile of {dataset_name}")
-        logger.info(f"Running Profiling for {dataset_name}. ")
-        if self.config.scheme == "bigquery":
-            table = dataset_name
-        profile = self.data_profiler.run_profiler(
-            dataset_name=dataset_name,
-            profile_date=self.sql_config.data_profiler_date,
-            schema=schema,
-            table=table,
-        )
-        logger.debug(f"Finished profiling {dataset_name}")
-        return profile
 
     def parse_raw_data_type(self, raw_data_type):
         return raw_data_type
