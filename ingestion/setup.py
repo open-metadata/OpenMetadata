424a267f1e0bdb60257ae885d227bdb7f2cfaada
Akash-Jain <Akash.J@deuexsolutions.com>
2022-03-09 11:23:23 +0530

    prepare dev version for ingestion 0.9.1

--- ingestion/setup.py
+++ ingestion/setup.py
@@ -152,7 +152,7 @@ test = {
 build_options = {"includes": ["_cffi_backend"]}
 setup(
     name="openmetadata-ingestion",
-    version="0.9.0.dev0",
+    version="0.9.1.dev0",
     url="https://open-metadata.org/",
     author="OpenMetadata Committers",
     license="Apache License 2.0",
