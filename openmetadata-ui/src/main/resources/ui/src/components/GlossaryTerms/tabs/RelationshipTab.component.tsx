4d1d25a58b9641e8a23e6f89496dd9596984157e
darth-coder00 <86726556+darth-coder00@users.noreply.github.com>
2022-03-10 21:18:12 +0530

    Fixed No data placeholders for reviewers and related terms (#3359)
    * Fixed No data placeholders for reviewers and related terms

--- openmetadata-ui/src/main/resources/ui/src/components/GlossaryTerms/tabs/RelationshipTab.component.tsx
+++ openmetadata-ui/src/main/resources/ui/src/components/GlossaryTerms/tabs/RelationshipTab.component.tsx
@@ -12,68 +12,69 @@
  */
 
 import classNames from 'classnames';
-import React from 'react';
-import ErrorPlaceHolder from '../../common/error-with-placeholder/ErrorPlaceHolder';
+import { FormatedGlossaryTermData } from 'Models';
+import React, { ReactNode } from 'react';
 import RichTextEditorPreviewer from '../../common/rich-text-editor/RichTextEditorPreviewer';
-/* eslint-disable max-len */
-// need to remove eslint disable once API data comes
-
-type RelationshipTableType = {
-  relatedTerms: string;
-  description: string;
-};
 
 type Props = {
-  data?: RelationshipTableType[];
+  data?: FormatedGlossaryTermData[];
+  addButton?: ReactNode;
 };
 
-const RelationshipTab = ({ data }: Props) => {
-  return data?.length ? (
+const RelationshipTab = ({ data, addButton }: Props) => {
+  return (
     <div className="tw-table-responsive" id="relationship">
       <table className="tw-w-full tw-bg-white">
         <thead>
           <tr className="tableHead-row">
-            <th className="tableHead-cell">Terms</th>
+            <th className="tableHead-cell tw-w-2/12">Terms</th>
             <th className="tableHead-cell">Description</th>
           </tr>
         </thead>
         <tbody>
-          {data.map((row, index) => {
-            return (
-              <tr className={classNames('tableBody-row')} key={index}>
-                <td
-                  className={classNames(
-                    'tableBody-cell tw-group tw-relative tw-align-baseline tw-w-2/12'
-                  )}>
-                  {row.relatedTerms}
-                </td>
-                <td
-                  className={classNames(
-                    'tableBody-cell tw-group tw-relative tw-align-baseline'
-                  )}>
-                  <div
-                    className="description-text"
-                    data-testid="description-text">
-                    {row.description.trim() ? (
-                      <RichTextEditorPreviewer
-                        enableSeeMoreVariant={false}
-                        markdown={row.description}
-                      />
-                    ) : (
-                      <span className="tw-no-description">No description</span>
-                    )}
-                  </div>
-                </td>
-              </tr>
-            );
-          })}
+          {data?.length ? (
+            data.map((row, index) => {
+              return (
+                <tr className={classNames('tableBody-row')} key={index}>
+                  <td
+                    className={classNames(
+                      'tableBody-cell tw-group tw-relative tw-align-baseline'
+                    )}>
+                    {row.displayName || row.name}
+                  </td>
+                  <td
+                    className={classNames(
+                      'tableBody-cell tw-group tw-relative tw-align-baseline'
+                    )}>
+                    <div
+                      className="description-text"
+                      data-testid="description-text">
+                      {row.description?.trim() ? (
+                        <RichTextEditorPreviewer
+                          enableSeeMoreVariant={false}
+                          markdown={row.description}
+                        />
+                      ) : (
+                        <span className="tw-no-description">
+                          No description
+                        </span>
+                      )}
+                    </div>
+                  </td>
+                </tr>
+              );
+            })
+          ) : (
+            <tr className="tableBody-row">
+              <td className="tableBody-cell tw-text-center" colSpan={4}>
+                <p className="tw-mb-3">No related terms available.</p>
+                {addButton}
+              </td>
+            </tr>
+          )}
         </tbody>
       </table>
     </div>
-  ) : (
-    <ErrorPlaceHolder>
-      <p className="tw-text-base tw-text-center">No related terms.</p>
-    </ErrorPlaceHolder>
   );
 };
 
