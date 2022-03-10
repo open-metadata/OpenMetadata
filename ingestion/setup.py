c26c4c03191ff5da67d317575aa35d46fd759237
Ayush Shah <ayush@getcollate.io>
2022-03-11 01:23:15 +0530

    Modified core version

--- ingestion/setup.py
+++ ingestion/setup.py
@@ -23,7 +23,7 @@ def get_long_description():
 
 
 base_requirements = {
-    "openmetadata-ingestion-core==0.8.0",
+    "openmetadata-ingestion-core==0.9.0",
     "commonregex",
     "idna<3,>=2.5",
     "click>=7.1.1",
