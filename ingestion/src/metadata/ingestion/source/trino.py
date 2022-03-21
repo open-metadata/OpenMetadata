ee5104eed0b47b04017939509cb1ad1e1996550a
Alberto Miorin <32617+amiorin@users.noreply.github.com>
2022-03-21 12:53:59 +0100

    override prepare and next_record

--- ingestion/src/metadata/ingestion/source/trino.py
+++ ingestion/src/metadata/ingestion/source/trino.py
@@ -11,14 +11,16 @@
 
 import logging
 import sys
-from typing import Optional
+from typing import Optional, Iterable
 from urllib.parse import quote_plus
+from sqlalchemy.inspection import inspect
 
 import click
 
 from metadata.generated.schema.entity.services.databaseService import (
     DatabaseServiceType,
 )
+from metadata.generated.schema.type.entity import Entity
 from metadata.ingestion.ometa.openmetadata_rest import MetadataServerConfig
 from metadata.ingestion.source.sql_source import SQLSource
 from metadata.ingestion.source.sql_source_common import SQLConnectionConfig
@@ -52,6 +54,8 @@ class TrinoConfig(SQLConnectionConfig):
 
 class TrinoSource(SQLSource):
     def __init__(self, config, metadata_config, ctx):
+        self.schema_names = None
+        self.inspector = None
         try:
             from sqlalchemy_trino import (
                 dbapi,  # pylint: disable=import-outside-toplevel,unused-import
@@ -72,3 +76,26 @@ class TrinoSource(SQLSource):
         config = TrinoConfig.parse_obj(config_dict)
         metadata_config = MetadataServerConfig.parse_obj(metadata_config_dict)
         return cls(config, metadata_config, ctx)
+
+    def prepare(self):
+        self.inspector = inspect(self.engine)
+        self.schema_names = (
+            self.inspector.get_schema_names()
+            if not self.config.database
+            else [self.config.database]
+        )
+        return super().prepare()
+
+    def next_record(self) -> Iterable[Entity]:
+        for schema in self.schema_names:
+            self.database_source_state.clear()
+            if not self.sql_config.schema_filter_pattern.included(schema):
+                self.status.filter(schema, "Schema pattern not allowed")
+                continue
+            if self.config.include_tables:
+                yield from self.fetch_tables(self.inspector, schema)
+            if self.config.include_views:
+                yield from self.fetch_views(self.inspector, schema)
+            if self.config.mark_deleted_tables_as_deleted:
+                schema_fqdn = f"{self.config.service_name}.{schema}"
+                yield from self.delete_tables(schema_fqdn)
