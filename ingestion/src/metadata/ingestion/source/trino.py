204bb2f093c172ace6ce27b5a92271361615e459
Alberto Miorin <32617+amiorin@users.noreply.github.com>
2022-03-21 11:53:34 +0100

    Fix #3507: Trino connector, support optional database field

--- ingestion/src/metadata/ingestion/source/trino.py
+++ ingestion/src/metadata/ingestion/source/trino.py
@@ -8,78 +8,49 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-"""Trino source module"""
 
 import logging
 import sys
-from typing import Iterable
+from typing import Optional
 from urllib.parse import quote_plus
 
 import click
-from sqlalchemy.inspection import inspect
 
 from metadata.generated.schema.entity.services.databaseService import (
     DatabaseServiceType,
 )
-from metadata.ingestion.models.ometa_table_db import OMetaDatabaseAndTable
 from metadata.ingestion.ometa.openmetadata_rest import MetadataServerConfig
 from metadata.ingestion.source.sql_source import SQLSource
 from metadata.ingestion.source.sql_source_common import SQLConnectionConfig
 
 logger = logging.getLogger(__name__)
 
-
 class TrinoConfig(SQLConnectionConfig):
-    """Trinio config class -- extends SQLConnectionConfig class
-
-    Attributes:
-        host_port:
-        scheme:
-        service_type:
-        catalog:
-        database:
-    """
-
     host_port = "localhost:8080"
     scheme = "trino"
     service_type = DatabaseServiceType.Trino.value
     catalog: str
-    database: str
+    include_views = False
+    params: Optional[dict] = None
 
     def get_connection_url(self):
         url = f"{self.scheme}://"
-        if self.username is not None:
-            url += f"{self.username}"
-            if self.password is not None:
+        if self.username:
+            url += f"{quote_plus(self.username)}"
+            if self.password:
                 url += f":{quote_plus(self.password.get_secret_value())}"
             url += "@"
         url += f"{self.host_port}"
-        if self.catalog is not None:
-            url += f"/{self.catalog}"
-            if self.database is not None:
-                url += f"/{self.database}"
-
-        if self.options is not None:
-            if self.database is None:
-                url += "/"
+        url += f"/{self.catalog}"
+        if self.params is not None:
             params = "&".join(
-                f"{key}={quote_plus(value)}"
-                for (key, value) in self.options.items()
-                if value
+                f"{key}={quote_plus(value)}" for (key, value) in self.params.items() if value
             )
             url = f"{url}?{params}"
         return url
 
 
 class TrinoSource(SQLSource):
-    """Trino source -- extends SQLSource
-
-    Args:
-        config:
-        metadata_config:
-        ctx
-    """
-
     def __init__(self, config, metadata_config, ctx):
         try:
             from sqlalchemy_trino import (
@@ -101,10 +72,3 @@ class TrinoSource(SQLSource):
         config = TrinoConfig.parse_obj(config_dict)
         metadata_config = MetadataServerConfig.parse_obj(metadata_config_dict)
         return cls(config, metadata_config, ctx)
-
-    def next_record(self) -> Iterable[OMetaDatabaseAndTable]:
-        inspector = inspect(self.engine)
-        if self.config.include_tables:
-            yield from self.fetch_tables(inspector, self.config.database)
-        if self.config.include_views:
-            yield from self.fetch_views(inspector, self.config.database)
