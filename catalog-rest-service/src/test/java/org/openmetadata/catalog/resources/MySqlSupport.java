402c7b44d2a5f2eca192d2997066f61d89250d05
Alberto Miorin <32617+amiorin@users.noreply.github.com>
2022-03-25 07:59:46 +0100

    Support wix for x86_64 and testcontainers for the rest.

--- catalog-rest-service/src/test/java/org/openmetadata/catalog/resources/MySqlSupport.java
+++ catalog-rest-service/src/test/java/org/openmetadata/catalog/resources/MySqlSupport.java
@@ -13,7 +13,14 @@
 
 package org.openmetadata.catalog.resources;
 
+import static com.wix.mysql.distribution.Version.v8_latest;
+
+import com.wix.mysql.EmbeddedMysql;
+import com.wix.mysql.config.MysqldConfig;
+import com.wix.mysql.config.SchemaConfig;
+import java.time.ZoneId;
 import java.util.List;
+import java.util.TimeZone;
 import lombok.extern.slf4j.Slf4j;
 import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.extension.AfterAllCallback;
@@ -23,11 +30,20 @@ import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @Slf4j
-public class DockerMySqlSupport implements BeforeAllCallback, AfterAllCallback {
+public class MySqlSupport implements BeforeAllCallback, AfterAllCallback {
   static GenericContainer mysqlContainer;
+  static EmbeddedMysql embeddedMysql;
 
   @Override
   public void beforeAll(ExtensionContext extensionContext) throws InterruptedException {
+    if ("x86_64".equals(System.getProperty("os.arch"))) {
+      useWix();
+    } else {
+      useTestcontainers();
+    }
+  }
+
+  private void useTestcontainers() {
     if (mysqlContainer == null) {
       mysqlContainer =
           new GenericContainer(DockerImageName.parse("mysql/mysql-server:latest"))
@@ -39,24 +55,52 @@ public class DockerMySqlSupport implements BeforeAllCallback, AfterAllCallback {
       mysqlContainer.start();
       LOG.info("Docker MySQL is started");
 
-      Flyway flyway =
-          Flyway.configure()
-              // TODO Remove hardcoding
-              .dataSource(
-                  "jdbc:mysql://localhost:3307/openmetadata_test_db?useSSL=false&serverTimezone=UTC", "root", "")
-              .table("DATABASE_CHANGE_LOG")
-              .sqlMigrationPrefix("v")
-              .load();
-      flyway.clean();
-      flyway.migrate();
-      LOG.info("Flyway migration is complete");
+      runFlyway();
     } else {
       LOG.info("Docker MySQL is already running");
     }
   }
 
+  private void useWix() {
+    if (embeddedMysql == null) {
+      MysqldConfig config =
+          MysqldConfig.aMysqldConfig(v8_latest)
+              .withPort(3307)
+              .withTimeZone(TimeZone.getTimeZone(ZoneId.of("UTC")))
+              .withUser("test", "")
+              .build();
+
+      SchemaConfig schemaConfig = SchemaConfig.aSchemaConfig("openmetadata_test_db").build();
+
+      embeddedMysql = EmbeddedMysql.anEmbeddedMysql(config).addSchema(schemaConfig).start();
+      LOG.info("Embedded MySQL is started");
+
+      runFlyway();
+    } else {
+      LOG.info("Embedded MySQL is already running");
+    }
+  }
+
+  private void runFlyway() {
+    Flyway flyway =
+        Flyway.configure()
+            // TODO Remove hardcoding
+            .dataSource("jdbc:mysql://localhost:3307/openmetadata_test_db?useSSL=false&serverTimezone=UTC", "root", "")
+            .table("DATABASE_CHANGE_LOG")
+            .sqlMigrationPrefix("v")
+            .load();
+    flyway.clean();
+    flyway.migrate();
+    LOG.info("Flyway migration is complete");
+  }
+
   @Override
   public void afterAll(ExtensionContext extensionContext) {
+    if (embeddedMysql != null) {
+      LOG.info("Stopping the embedded db");
+      embeddedMysql.stop();
+      embeddedMysql = null;
+    }
     if (mysqlContainer != null) {
       LOG.info("Stopping Docker MySQL");
       mysqlContainer.stop();
