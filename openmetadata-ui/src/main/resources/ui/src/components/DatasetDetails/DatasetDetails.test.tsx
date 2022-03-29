595d11f9a5a0f96ca042c2efcf409b39865c4036
Sachin-chaurasiya <sachinchaurasiyachotey87@gmail.com>
2022-03-29 13:19:05 +0530

    Add unit test for table entity page

--- openmetadata-ui/src/main/resources/ui/src/components/DatasetDetails/DatasetDetails.test.tsx
+++ openmetadata-ui/src/main/resources/ui/src/components/DatasetDetails/DatasetDetails.test.tsx
@@ -11,7 +11,13 @@
  *  limitations under the License.
  */
 
-import { findByText, getByTestId, render } from '@testing-library/react';
+import {
+  findByTestId,
+  findByText,
+  getByTestId,
+  queryByTestId,
+  render,
+} from '@testing-library/react';
 import { LeafNodes, LoadingNodeState } from 'Models';
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
@@ -58,6 +64,40 @@ const mockUserTeam = [
     type: 'type',
   },
 ];
+
+const mockThreads = [
+  {
+    id: '465b2dfb-300e-45f5-a1a6-e19c6225e9e7',
+    href: 'http://localhost:8585/api/v1/feed/465b2dfb-300e-45f5-a1a6-e19c6225e9e7',
+    threadTs: 1647434125848,
+    about: '<#E/table/bigquery_gcp.shopify.raw_product_catalog/description>',
+    entityId: 'f1ebcfdf-d4b8-43bd-add2-1789e25ddde3',
+    createdBy: 'aaron_johnson0',
+    updatedAt: 1647434125848,
+    updatedBy: 'anonymous',
+    resolved: false,
+    message: 'New thread.',
+    postsCount: 0,
+    posts: [],
+    relativeDay: 'Today',
+  },
+  {
+    id: '40c2faec-0159-4d86-9b15-c17f3e1c081b',
+    href: 'http://localhost:8585/api/v1/feed/40c2faec-0159-4d86-9b15-c17f3e1c081b',
+    threadTs: 1647411418056,
+    about: '<#E/table/bigquery_gcp.shopify.raw_product_catalog/description>',
+    entityId: 'f1ebcfdf-d4b8-43bd-add2-1789e25ddde3',
+    createdBy: 'sachin.c',
+    updatedAt: 1647434031435,
+    updatedBy: 'anonymous',
+    resolved: false,
+    message: 'New thread.',
+    postsCount: 0,
+    posts: [],
+    relativeDay: 'Today',
+  },
+];
+
 const DatasetDetailsProps = {
   activeTab: 1,
   columns: [],
@@ -93,7 +133,7 @@ const DatasetDetailsProps = {
   removeLineageHandler: jest.fn(),
   entityLineageHandler: jest.fn(),
   tableQueries: [],
-  entityThread: [],
+  entityThread: mockThreads,
   isentityThreadLoading: false,
   postFeedHandler: jest.fn(),
   feedCount: 0,
@@ -115,15 +155,17 @@ const DatasetDetailsProps = {
   tagUpdateHandler: jest.fn(),
 };
 jest.mock('../ManageTab/ManageTab.component', () => {
-  return jest.fn().mockReturnValue(<p>ManageTab</p>);
+  return jest.fn().mockReturnValue(<p data-testid="manage">ManageTab</p>);
 });
 
 jest.mock('../EntityLineage/EntityLineage.component', () => {
-  return jest.fn().mockReturnValue(<p>Lineage</p>);
+  return jest.fn().mockReturnValue(<p data-testid="lineage">Lineage</p>);
 });
 
 jest.mock('../TableProfiler/TableProfiler.component', () => {
-  return jest.fn().mockReturnValue(<p>ProfilerTable</p>);
+  return jest
+    .fn()
+    .mockReturnValue(<p data-testid="TableProfiler">TableProfiler</p>);
 });
 
 jest.mock('../common/description/Description', () => {
@@ -138,12 +180,8 @@ jest.mock('../common/entityPageInfo/EntityPageInfo', () => {
   return jest.fn().mockReturnValue(<p>EntityPageInfo</p>);
 });
 
-jest.mock('../common/TabsPane/TabsPane', () => {
-  return jest.fn().mockReturnValue(<p>TabsPane</p>);
-});
-
 jest.mock('../ActivityFeed/ActivityFeedList/ActivityFeedList.tsx', () => {
-  return jest.fn().mockReturnValue(<p>FeedCards</p>);
+  return jest.fn().mockReturnValue(<p>ActivityFeedList</p>);
 });
 
 jest.mock('../ActivityFeed/ActivityThreadPanel/ActivityThreadPanel.tsx', () => {
@@ -155,9 +193,9 @@ jest.mock('../ActivityFeed/ActivityFeedEditor/ActivityFeedEditor.tsx', () => {
 
 jest.mock('../../utils/CommonUtils', () => ({
   addToRecentViewed: jest.fn(),
+  getCountBadge: jest.fn(),
   getCurrentUserId: jest.fn().mockReturnValue('CurrentUserId'),
   getPartialNameFromFQN: jest.fn().mockReturnValue('PartialNameFromFQN'),
-  // getTableFQNFromColumnFQN: jest.fn().mockReturnValue('TableFQNFromColumnFQN'),
   getUserTeams: () => mockUserTeam,
 }));
 
@@ -169,10 +207,123 @@ describe('Test MyDataDetailsPage page', () => {
 
     const relatedTables = getByTestId(container, 'related-tables-container');
     const EntityPageInfo = await findByText(container, /EntityPageInfo/i);
-    const TabsPane = await findByText(container, /TabsPane/i);
+    const description = await findByText(container, /Description/i);
+    const tabs = await findByTestId(container, 'tabs');
+    const schemaTab = await findByTestId(tabs, 'Schema');
+    const activityFeedTab = await findByTestId(tabs, 'Activity Feed');
+    const sampleDataTab = await findByTestId(tabs, 'Sample Data');
+    const queriesTab = await findByTestId(tabs, 'Queries');
+    const profilerTab = await findByTestId(tabs, 'Profiler');
+    const dataQualityTab = await findByTestId(tabs, 'Data Quality');
+    const lineageTab = await findByTestId(tabs, 'Lineage');
+    const manageTab = await findByTestId(tabs, 'Manage');
+    const dbtTab = queryByTestId(tabs, 'DBT');
 
     expect(relatedTables).toBeInTheDocument();
     expect(EntityPageInfo).toBeInTheDocument();
-    expect(TabsPane).toBeInTheDocument();
+    expect(description).toBeInTheDocument();
+    expect(tabs).toBeInTheDocument();
+    expect(schemaTab).toBeInTheDocument();
+    expect(activityFeedTab).toBeInTheDocument();
+    expect(sampleDataTab).toBeInTheDocument();
+    expect(queriesTab).toBeInTheDocument();
+    expect(profilerTab).toBeInTheDocument();
+    expect(dataQualityTab).toBeInTheDocument();
+    expect(lineageTab).toBeInTheDocument();
+    expect(manageTab).toBeInTheDocument();
+    expect(dbtTab).not.toBeInTheDocument();
+  });
+
+  it('Check if active tab is schema', async () => {
+    const { container } = render(<DatasetDetails {...DatasetDetailsProps} />, {
+      wrapper: MemoryRouter,
+    });
+    const schema = await findByText(container, /SchemaTab/i);
+
+    expect(schema).toBeInTheDocument();
+  });
+
+  it('Check if active tab is activity feed', async () => {
+    const { container } = render(
+      <DatasetDetails {...DatasetDetailsProps} activeTab={2} />,
+      {
+        wrapper: MemoryRouter,
+      }
+    );
+    const activityFeedList = await findByText(container, /ActivityFeedList/i);
+
+    expect(activityFeedList).toBeInTheDocument();
+  });
+
+  it('Check if active tab is sample data', async () => {
+    const { container } = render(
+      <DatasetDetails {...DatasetDetailsProps} activeTab={3} />,
+      {
+        wrapper: MemoryRouter,
+      }
+    );
+    const sampleData = await findByTestId(container, 'sample-data');
+
+    expect(sampleData).toBeInTheDocument();
+  });
+
+  it('Check if active tab is queries', async () => {
+    const { container } = render(
+      <DatasetDetails {...DatasetDetailsProps} activeTab={4} />,
+      {
+        wrapper: MemoryRouter,
+      }
+    );
+    const tableQueries = await findByTestId(container, 'table-queries');
+
+    expect(tableQueries).toBeInTheDocument();
+  });
+
+  it('Check if active tab is profiler', async () => {
+    const { container } = render(
+      <DatasetDetails {...DatasetDetailsProps} activeTab={5} />,
+      {
+        wrapper: MemoryRouter,
+      }
+    );
+    const tableProfiler = await findByTestId(container, 'TableProfiler');
+
+    expect(tableProfiler).toBeInTheDocument();
+  });
+
+  it('Check if active tab is data quality', async () => {
+    const { container } = render(
+      <DatasetDetails {...DatasetDetailsProps} activeTab={6} />,
+      {
+        wrapper: MemoryRouter,
+      }
+    );
+    const dataQuality = await findByTestId(container, 'data-quality-tab');
+
+    expect(dataQuality).toBeInTheDocument();
+  });
+
+  it('Check if active tab is lineage', async () => {
+    const { container } = render(
+      <DatasetDetails {...DatasetDetailsProps} activeTab={7} />,
+      {
+        wrapper: MemoryRouter,
+      }
+    );
+    const lineage = await findByTestId(container, 'lineage');
+
+    expect(lineage).toBeInTheDocument();
+  });
+
+  it('Check if active tab is manage', async () => {
+    const { container } = render(
+      <DatasetDetails {...DatasetDetailsProps} activeTab={9} />,
+      {
+        wrapper: MemoryRouter,
+      }
+    );
+    const manage = await findByTestId(container, 'manage');
+
+    expect(manage).toBeInTheDocument();
   });
 });
