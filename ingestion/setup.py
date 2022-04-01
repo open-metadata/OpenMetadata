23d68b24d90d1c4c19ac235823a11425b48cf1e2
dvecerdea <92868540+dvecerdea@users.noreply.github.com>
2022-04-01 20:12:03 +0100

    Replace sqlalchemy-trino with trino[sqlalchemy] (#3800)

--- ingestion/setup.py
+++ ingestion/setup.py
@@ -102,7 +102,7 @@ plugins: Dict[str, Set[str]] = {
     "oracle": {"cx_Oracle"},
     "powerbi": {"python-power-bi==0.1.2"},
     "presto": {"pyhive~=0.6.3"},
-    "trino": {"sqlalchemy-trino==0.4.1"},
+    "trino": {"trino[sqlalchemy]"},
     "postgres": {"pymysql>=1.0.2", "psycopg2-binary", "GeoAlchemy2"},
     "redash": {"redash-toolbelt==0.1.8"},
     "redshift": {"sqlalchemy-redshift==0.8.9", "psycopg2-binary", "GeoAlchemy2"},
