e34eb72c883ff1a60c465e9b3b99be7bf9e8eb00
Shailesh Parmar <shailesh.parmar.webdev@gmail.com>
2022-04-04 19:20:11 +0530

    add unit test for the files

--- openmetadata-ui/src/main/resources/ui/src/pages/DatasetDetailsPage/DatasetDetailsPage.test.tsx
+++ openmetadata-ui/src/main/resources/ui/src/pages/DatasetDetailsPage/DatasetDetailsPage.test.tsx
@@ -11,22 +11,127 @@
  *  limitations under the License.
  */
 
-import { getByText, render } from '@testing-library/react';
+import { findByTestId, findByText, render } from '@testing-library/react';
 import React from 'react';
 import { MemoryRouter } from 'react-router';
+import { getTableDetailsByFQN } from '../../axiosAPIs/tableAPI';
 import DatasetDetailsPage from './DatasetDetailsPage.component';
 
-jest.mock('./DatasetDetailsPage.component', () => {
-  return jest.fn().mockReturnValue(<div>DatasetDetails Page</div>);
+const mockUseParams = {
+  datasetFQN: 'bigquery_gcp:shopify:dim_address',
+  tab: 'schema',
+};
+
+jest.mock('../../components/DatasetDetails/DatasetDetails.component', () => {
+  return jest
+    .fn()
+    .mockReturnValue(<div data-testid="datasetdetails-component" />);
 });
+
+jest.mock(
+  '../../components/common/error-with-placeholder/ErrorPlaceHolder',
+  () => {
+    return jest.fn().mockReturnValue(<div>ErrorPlaceHolder.component</div>);
+  }
+);
+
+jest.mock('../../hooks/useToastContext', () => {
+  return jest.fn().mockImplementation(() => jest.fn());
+});
+
+jest.mock('../../axiosAPIs/tableAPI', () => ({
+  addColumnTestCase: jest.fn(),
+  addFollower: jest.fn(),
+  addTableTestCase: jest.fn(),
+  deleteColumnTestCase: jest.fn(),
+  deleteTableTestCase: jest.fn(),
+  getTableDetailsByFQN: jest.fn().mockImplementation(() => Promise.resolve()),
+  patchTableDetails: jest.fn(),
+  removeFollower: jest.fn(),
+}));
+
+jest.mock('../../axiosAPIs/feedsAPI', () => ({
+  getAllFeeds: jest.fn().mockImplementation(() => Promise.resolve()),
+  getFeedCount: jest.fn().mockImplementation(() => Promise.resolve()),
+  postFeedById: jest.fn(),
+  postThread: jest.fn(),
+}));
+
+jest.mock('../../axiosAPIs/lineageAPI', () => ({
+  getLineageByFQN: jest.fn().mockImplementation(() => Promise.resolve()),
+}));
+
+jest.mock('../../axiosAPIs/miscAPI', () => ({
+  addLineage: jest.fn(),
+  deleteLineageEdge: jest.fn(),
+}));
+
+jest.mock('react-router-dom', () => ({
+  useHistory: jest.fn(),
+  useParams: jest.fn().mockImplementation(() => mockUseParams),
+}));
 
 describe('Test DatasetDetails page', () => {
   it('Component should render', async () => {
     const { container } = render(<DatasetDetailsPage />, {
       wrapper: MemoryRouter,
     });
-    const ContainerText = getByText(container, 'DatasetDetails Page');
+    const ContainerText = await findByTestId(
+      container,
+      'datasetdetails-component'
+    );
 
     expect(ContainerText).toBeInTheDocument();
+  });
+
+  describe('Render Sad Paths', () => {
+    it('ErrorPlaceholder should be visible if there is error with status code 404', async () => {
+      (getTableDetailsByFQN as jest.Mock).mockImplementationOnce(() =>
+        Promise.reject({
+          response: { data: { message: 'Error!' }, status: 404 },
+        })
+      );
+      const { container } = render(<DatasetDetailsPage />, {
+        wrapper: MemoryRouter,
+      });
+      const errorPlaceholder = await findByText(
+        container,
+        /ErrorPlaceHolder.component/i
+      );
+
+      expect(errorPlaceholder).toBeInTheDocument();
+    });
+
+    it('ErrorPlaceholder should not visible if status code is other than 404 and message is present', async () => {
+      (getTableDetailsByFQN as jest.Mock).mockImplementationOnce(() =>
+        Promise.reject({
+          response: { data: { message: 'Error!' } },
+        })
+      );
+      const { container } = render(<DatasetDetailsPage />, {
+        wrapper: MemoryRouter,
+      });
+      const ContainerText = await findByTestId(
+        container,
+        'datasetdetails-component'
+      );
+
+      expect(ContainerText).toBeInTheDocument();
+    });
+
+    it('ErrorPlaceholder should not visible if status code is other than 404 and response message is not present', async () => {
+      (getTableDetailsByFQN as jest.Mock).mockImplementationOnce(() =>
+        Promise.reject({ response: { message: '' } })
+      );
+      const { container } = render(<DatasetDetailsPage />, {
+        wrapper: MemoryRouter,
+      });
+      const ContainerText = await findByTestId(
+        container,
+        'datasetdetails-component'
+      );
+
+      expect(ContainerText).toBeInTheDocument();
+    });
   });
 });
