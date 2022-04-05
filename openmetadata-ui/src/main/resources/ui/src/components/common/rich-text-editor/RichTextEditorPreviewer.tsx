b372820e523796723a1b4c5eab1d27e1af8474cf
Sachin-chaurasiya <sachinchaurasiyachotey87@gmail.com>
2022-04-05 17:28:24 +0530

    Add unit tests

--- openmetadata-ui/src/main/resources/ui/src/components/common/rich-text-editor/RichTextEditorPreviewer.tsx
+++ openmetadata-ui/src/main/resources/ui/src/components/common/rich-text-editor/RichTextEditorPreviewer.tsx
@@ -17,10 +17,10 @@ import React, { useEffect, useState } from 'react';
 import MarkdownParser from 'react-markdown';
 import rehypeRaw from 'rehype-raw';
 import remarkGfm from 'remark-gfm';
-import SVGIcons, { Icons } from '../../../utils/SvgUtils';
+import { BlurLayout } from './BlurLayout';
 import { PreviewerProp } from './RichTextEditor.interface';
 
-const MAX_LENGTH = 300;
+export const MAX_LENGTH = 300;
 
 const RichTextEditorPreviewer = ({
   markdown = '',
@@ -40,31 +40,8 @@ const RichTextEditorPreviewer = ({
     setContent(modifiedContent);
   };
 
-  const BlurLayout = () => {
-    return enableSeeMoreVariant && markdown.length > MAX_LENGTH ? (
-      <div
-        className={classNames(
-          'tw-absolute tw-flex tw-h-full tw-w-full tw-inset-x-0 tw-pointer-events-none',
-          !displayMoreText ? blurClasses : null,
-          {
-            'tw-top-0 tw-bottom-0': !displayMoreText,
-            ' tw--bottom-4': displayMoreText,
-          }
-        )}>
-        <p
-          className="tw-cursor-pointer tw-self-end tw-pointer-events-auto tw-text-primary tw-mx-auto"
-          onClick={() => setDisplayMoreText((pre) => !pre)}>
-          <span className="tw-flex tw-items-center tw-gap-2">
-            <SVGIcons
-              alt="expand-collapse"
-              className={classNames({ 'rotate-inverse': displayMoreText })}
-              icon={Icons.CHEVRON_DOWN}
-              width="32"
-            />
-          </span>
-        </p>
-      </div>
-    ) : null;
+  const displayMoreHandler = () => {
+    setDisplayMoreText((pre) => !pre);
   };
 
   useEffect(() => {
@@ -82,7 +59,8 @@ const RichTextEditorPreviewer = ({
         {
           'tw-mb-5': displayMoreText,
         }
-      )}>
+      )}
+      data-testid="viewer-container">
       <MarkdownParser
         sourcePos
         components={{
@@ -134,7 +112,13 @@ const RichTextEditorPreviewer = ({
         remarkPlugins={[remarkGfm]}>
         {content}
       </MarkdownParser>
-      <BlurLayout />
+      <BlurLayout
+        blurClasses={blurClasses}
+        displayMoreHandler={displayMoreHandler}
+        displayMoreText={displayMoreText}
+        enableSeeMoreVariant={enableSeeMoreVariant}
+        markdown={content}
+      />
     </div>
   );
 };
