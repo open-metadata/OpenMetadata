4783405abfdbe8cb38a32eaf42df6de318933479
Sriharsha Chintalapani <harsha@getcollate.io>
2022-04-06 22:53:37 -0700

    Add timers

--- ingestion/src/metadata/ingestion/ometa/utils.py
+++ ingestion/src/metadata/ingestion/ometa/utils.py
@@ -81,11 +81,7 @@ def timer(method):
         ts = time.perf_counter()
         result = method(*args, **kw)
         te = time.perf_counter()
-        if "log_time" in kw:
-            name = kw.get("log_name", method.__name__.upper())
-            kw["log_time"][name] = int((te - ts) * 1000)
-        else:
-            logger.trace("%r  %2.2f ms" % (method.__name__, (te - ts) * 1000))
+        logger.trace("%r  %2.2f ms" % (method.__name__, (te - ts) * 1000))
         return result
 
     return timed
