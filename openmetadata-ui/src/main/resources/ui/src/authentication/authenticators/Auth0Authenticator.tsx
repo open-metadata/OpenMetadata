28a8a40e770bc1598ad2f92a96b31b3a3c7f3894
Vivek Ratnavel Subramanian <vivekratnavel90@gmail.com>
2022-04-08 10:26:10 -0700

    Remove try block to fix code smell

--- openmetadata-ui/src/main/resources/ui/src/authentication/authenticators/Auth0Authenticator.tsx
+++ openmetadata-ui/src/main/resources/ui/src/authentication/authenticators/Auth0Authenticator.tsx
@@ -53,33 +53,27 @@ const Auth0Authenticator = forwardRef<AuthenticatorRef, Props>(
           return new Promise((resolve, reject) => {
             const { provider } = authConfig;
             if (provider === AuthTypes.AUTH0) {
-              try {
-                getAccessTokenSilently()
-                  .then(() => {
-                    getIdTokenClaims()
-                      .then((token) => {
-                        if (token !== undefined) {
-                          idToken = token.__raw;
-                          localStorage.setItem(oidcTokenKey, idToken);
-                          resolve(idToken);
-                        }
-                      })
-                      .catch((err) => {
-                        reject(
-                          `Error while renewing id token from Auth0 SSO: ${err.message}`
-                        );
-                      });
-                  })
-                  .catch((err) => {
-                    reject(
-                      `Error while renewing id token from Auth0 SSO: ${err.message}`
-                    );
-                  });
-              } catch (error) {
-                reject(
-                  `Error while renewing id token from Auth0 SSO: ${error}`
-                );
-              }
+              getAccessTokenSilently()
+                .then(() => {
+                  getIdTokenClaims()
+                    .then((token) => {
+                      if (token !== undefined) {
+                        idToken = token.__raw;
+                        localStorage.setItem(oidcTokenKey, idToken);
+                        resolve(idToken);
+                      }
+                    })
+                    .catch((err) => {
+                      reject(
+                        `Error while renewing id token from Auth0 SSO: ${err.message}`
+                      );
+                    });
+                })
+                .catch((err) => {
+                  reject(
+                    `Error while renewing id token from Auth0 SSO: ${err.message}`
+                  );
+                });
             } else {
               reject(
                 `Auth Provider ${provider} not supported for renewing tokens.`
