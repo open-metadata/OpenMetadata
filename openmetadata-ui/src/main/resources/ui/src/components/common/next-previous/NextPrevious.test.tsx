a2168e9a366b59192c6706f2251d71ede22cbe22
Sachin-chaurasiya <sachinchaurasiyachotey87@gmail.com>
2022-04-09 15:04:16 +0530

    Add unit tests

--- openmetadata-ui/src/main/resources/ui/src/components/common/next-previous/NextPrevious.test.tsx
+++ openmetadata-ui/src/main/resources/ui/src/components/common/next-previous/NextPrevious.test.tsx
@@ -18,6 +18,12 @@ import NextPrevious from './NextPrevious';
 
 const mockCallback = jest.fn();
 
+const computeTotalPages = jest
+  .fn()
+  .mockImplementation((pSize: number, total: number) => {
+    return Math.ceil(total / pSize);
+  });
+
 describe('Test Pagination Component', () => {
   it('Component should render', () => {
     const paging = {
@@ -39,7 +45,7 @@ describe('Test Pagination Component', () => {
     expect(pagination).toBeInTheDocument();
   });
 
-  it('Component should be disabled if pagin is empty', () => {
+  it('Component should be disabled if paging is empty', () => {
     const paging = {
       after: '',
       before: '',
@@ -57,10 +63,13 @@ describe('Test Pagination Component', () => {
     );
     const previous = getByTestId('previous');
     const next = getByTestId('next');
+    const pageIndicator = getByTestId('page-indicator');
 
     expect(previous).toBeDisabled();
 
     expect(next).toBeDisabled();
+
+    expect(pageIndicator).toBeInTheDocument();
   });
 
   it('Left button should be disabled if pagin.before is empty', () => {
@@ -80,8 +89,10 @@ describe('Test Pagination Component', () => {
       />
     );
     const previous = getByTestId('previous');
+    const pageIndicator = getByTestId('page-indicator');
 
     expect(previous).toBeDisabled();
+    expect(pageIndicator).toBeInTheDocument();
   });
 
   it('Next button should be disabled if pagin.after is empty', () => {
@@ -101,7 +112,115 @@ describe('Test Pagination Component', () => {
       />
     );
     const next = getByTestId('next');
+    const pageIndicator = getByTestId('page-indicator');
 
     expect(next).toBeDisabled();
+
+    expect(pageIndicator).toBeInTheDocument();
+  });
+
+  it('Next and Previous should be disable if total is equal to pagesize with isNumberBased as true', () => {
+    const paging = {
+      before: 'test',
+      after: '',
+      total: PAGE_SIZE,
+    };
+
+    const { getByTestId } = render(
+      <NextPrevious
+        isNumberBased
+        currentPage={1}
+        pageSize={PAGE_SIZE}
+        paging={paging}
+        pagingHandler={mockCallback}
+        totalCount={paging.total}
+      />
+    );
+    const next = getByTestId('next');
+    const previous = getByTestId('previous');
+
+    expect(next).toBeDisabled();
+    expect(previous).toBeDisabled();
+  });
+
+  it('Previous should be disable if currentPage is equal to 1 with isNumberBased as true', () => {
+    const paging = {
+      before: 'test',
+      after: '',
+      total: PAGE_SIZE * 2,
+    };
+
+    const { getByTestId } = render(
+      <NextPrevious
+        isNumberBased
+        currentPage={1}
+        pageSize={PAGE_SIZE}
+        paging={paging}
+        pagingHandler={mockCallback}
+        totalCount={paging.total}
+      />
+    );
+    const next = getByTestId('next');
+    const previous = getByTestId('previous');
+
+    expect(next).not.toBeDisabled();
+    expect(previous).toBeDisabled();
+  });
+
+  it('Next should be disable if currentPage is equal to lastpage with isNumberBased as true', () => {
+    const paging = {
+      before: 'test',
+      after: '',
+      total: PAGE_SIZE * 2,
+    };
+
+    const lastPage = computeTotalPages(PAGE_SIZE, PAGE_SIZE * 2);
+
+    const { getByTestId } = render(
+      <NextPrevious
+        isNumberBased
+        currentPage={lastPage}
+        pageSize={PAGE_SIZE}
+        paging={paging}
+        pagingHandler={mockCallback}
+        totalCount={paging.total}
+      />
+    );
+    const next = getByTestId('next');
+    const previous = getByTestId('previous');
+
+    expect(next).toBeDisabled();
+    expect(previous).not.toBeDisabled();
+  });
+
+  it('should render proper page indicator', () => {
+    const paging = {
+      before: 'test',
+      after: '',
+      total: PAGE_SIZE * 2,
+    };
+
+    const totalPage = computeTotalPages(PAGE_SIZE, PAGE_SIZE * 2);
+
+    const { getByTestId } = render(
+      <NextPrevious
+        isNumberBased
+        currentPage={totalPage}
+        pageSize={PAGE_SIZE}
+        paging={paging}
+        pagingHandler={mockCallback}
+        totalCount={paging.total}
+      />
+    );
+    const next = getByTestId('next');
+    const previous = getByTestId('previous');
+    const pageIndicator = getByTestId('page-indicator');
+
+    expect(next).toBeDisabled();
+    expect(previous).not.toBeDisabled();
+
+    expect(pageIndicator).toBeInTheDocument();
+
+    expect(pageIndicator).toHaveTextContent(`${totalPage}/${totalPage} Page`);
   });
 });
