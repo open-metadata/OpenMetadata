1e61b5ad6df5ac732fb3189a8d7c1164803bf05b
pmbrull <peremiquelbrull@gmail.com>
2022-04-11 16:47:26 +0200

    Update examples

--- openmetadata-airflow-apis/examples/test.py
+++
@@ -1,9 +0,0 @@
-from airflow import DAG
-from openmetadata.workflows import workflow_factory
-
-workflow = workflow_factory.WorkflowFactory.create(
-    "/Users/harsha/Code/openmetadata-airflow-apis/examples/test.json"
-)
-workflow.generate_dag(globals())
-dag = workflow.get_dag()
-print(dag)
