cae4094e0965f35831c0a2db21d8c0a736ad8ac3
Stanislav Lysikov <s.lysikov@space307.com>
2022-04-11 18:29:35 +0300

    reformat changes

--- ingestion/src/metadata/ingestion/source/hive.py
+++ ingestion/src/metadata/ingestion/source/hive.py
@@ -53,20 +53,20 @@ def get_columns(self, connection, table_name, schema=None, **kw):
 
 
 def get_table_names(self, connection, schema=None, **kw):
-        query = 'SHOW TABLES'
-        if schema:
-            query += ' IN ' + self.identifier_preparer.quote_identifier(schema)
-        tables_in_schema = connection.execute(query)
-        tables = []
-        for row in tables_in_schema:
-            # check number of columns in result
-            # if it is > 1, we use spark thrift server with 3 columns in the result (schema, table, is_temporary)
-            # else it is hive with 1 column in the result
-            if len(row) > 1:
-                tables.append(row[1])
-            else:
-                tables.append(row[0])
-        return tables
+    query = "SHOW TABLES"
+    if schema:
+        query += " IN " + self.identifier_preparer.quote_identifier(schema)
+    tables_in_schema = connection.execute(query)
+    tables = []
+    for row in tables_in_schema:
+        # check number of columns in result
+        # if it is > 1, we use spark thrift server with 3 columns in the result (schema, table, is_temporary)
+        # else it is hive with 1 column in the result
+        if len(row) > 1:
+            tables.append(row[1])
+        else:
+            tables.append(row[0])
+    return tables
 
 
 HiveDialect.get_columns = get_columns
