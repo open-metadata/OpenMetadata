e08f740b4e3b4cbeb6e97800d59016e0b784ebb3
codingwithabhi <imabhipro@gmail.com>
2022-04-14 00:09:11 +0530

    code-formatted

--- ingestion/src/metadata/ingestion/source/metadata/atlas.py
+++ ingestion/src/metadata/ingestion/source/metadata/atlas.py
@@ -6,9 +6,6 @@ from typing import Any, Dict, Iterable, List
 
 from metadata.generated.schema.api.data.createTopic import CreateTopicRequest
 from metadata.generated.schema.api.lineage.addLineage import AddLineageRequest
-from metadata.generated.schema.api.services.createDatabaseService import (
-    CreateDatabaseServiceRequest,
-)
 from metadata.generated.schema.entity.data.database import Database
 from metadata.generated.schema.entity.data.databaseSchema import DatabaseSchema
 from metadata.generated.schema.entity.data.pipeline import Pipeline
@@ -18,10 +15,6 @@ from metadata.generated.schema.entity.services.connections.metadata.atlasConnect
 )
 from metadata.generated.schema.entity.services.connections.metadata.openMetadataConnection import (
     OpenMetadataConnection,
-)
-from metadata.generated.schema.entity.services.databaseService import (
-    DatabaseService,
-    DatabaseServiceType,
 )
 from metadata.generated.schema.entity.services.messagingService import (
     MessagingServiceType,
