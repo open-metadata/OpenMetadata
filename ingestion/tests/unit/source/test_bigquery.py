55ca117903af28e54c18630e4bc28b428dc43854
Ayush Shah <ayush@getcollate.io>
2022-04-13 14:19:57 +0530

    Fix Python test

--- ingestion/tests/unit/source/test_bigquery.py
+++ ingestion/tests/unit/source/test_bigquery.py
@@ -27,53 +27,48 @@ CONFIG = """
 {
   "source": {
     "type": "bigquery",
-    "config": {
-      "service_name": "test_bigquery",
-      "project_id": "project_id",
-      "host_port": "host_port",
-      "username": "username",
-      "service_name": "gcp_bigquery",
-      "connect_args":{},
-      "partition_query": "select * from {}.{} WHERE DATE({}) = '{}' LIMIT 1000",
-      "enable_policy_tags": "True",
-      "duration":1,
-      "options":{
-        "credentials":{
-          "type": "service_account",
-          "project_id": "project_id",
-          "private_key_id": "private_key_id",
-          "private_key": "",
-          "client_email": "gcpuser@project_id.iam.gserviceaccount.com",
-          "client_id": "",
-          "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-          "token_uri": "https://oauth2.googleapis.com/token",
-          "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-          "client_x509_cert_url": ""
+    "serviceName": "local_bigquery",
+    "serviceConnection": {
+      "config": {
+        "type": "BigQuery",
+        "projectID": "project_id",
+        "enablePolicyTagImport": true,
+        "connectionOptions": {
+          "credentials": {
+            "type": "service_account",
+            "project_id": "project_id",
+            "private_key_id": "private_key_id",
+            "private_key": "private_key",
+            "client_email": "gcpuser@project_id.iam.gserviceaccount.com",
+            "client_id": "client_id",
+            "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+            "token_uri": "https://oauth2.googleapis.com/token",
+            "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+            "client_x509_cert_url": ""
+          }
         }
       }
-    }
+    },
+    "sourceConfig": {"config": {"enableDataProfiler": false}}
   },
   "sink": {
     "type": "file",
     "config": {
       "filename": "/var/tmp/datasets.json"
         }
-    },
-  "metadata_server": {
-    "type": "metadata-server",
-    "config": {
-      "api_endpoint": "http://localhost:8585/api",
-      "auth_provider_type": "no-auth"
+  },
+  "workflowConfig": {
+    "openMetadataServerConfig": {
+      "hostPort": "http://localhost:8585/api",
+      "authProvider": "no-auth"
     }
   }
 }
-
 """
 
-
 MOCK_GET_TABLE_NAMES = [
-    "open_metadata.test1",
-    "open_metadata.cloudaudit_googleapis_com_data_access_20220122",
+    "open_metadata.test_schema.test1",
+    "open_metadata.cloudaudit-googleapis_com_data_access_20220122",
 ]
 
 GET_TABLE_DESCRIPTIONS = {"text": "Test"}
@@ -208,7 +203,6 @@ MOCK_GET_COLUMN = [
         "policy_tags": None,
     },
 ]
-
 
 MOCK_GET_VIEW_NAMES = []
 MOCK_GET_VIEW_DEFINITION = ""
