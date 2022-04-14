3920e2183cce9952944677804285bba412fe2579
Sachin-chaurasiya <sachinchaurasiyachotey87@gmail.com>
2022-04-14 14:30:22 +0530

    Fix failing unit test

--- openmetadata-ui/src/main/resources/ui/src/antlr/generated/FqnListener.js
+++ openmetadata-ui/src/main/resources/ui/src/antlr/generated/FqnListener.js
@@ -4,33 +4,21 @@ import antlr4 from 'antlr4';
 
 // This class defines a complete listener for a parse tree produced by FqnParser.
 export default class FqnListener extends antlr4.tree.ParseTreeListener {
+  // Enter a parse tree produced by FqnParser#fqn.
+  enterFqn(ctx) {}
 
-	// Enter a parse tree produced by FqnParser#fqn.
-	enterFqn(ctx) {
-	}
+  // Exit a parse tree produced by FqnParser#fqn.
+  exitFqn(ctx) {}
 
-	// Exit a parse tree produced by FqnParser#fqn.
-	exitFqn(ctx) {
-	}
+  // Enter a parse tree produced by FqnParser#unquotedName.
+  enterUnquotedName(ctx) {}
 
+  // Exit a parse tree produced by FqnParser#unquotedName.
+  exitUnquotedName(ctx) {}
 
-	// Enter a parse tree produced by FqnParser#unquotedName.
-	enterUnquotedName(ctx) {
-	}
+  // Enter a parse tree produced by FqnParser#quotedName.
+  enterQuotedName(ctx) {}
 
-	// Exit a parse tree produced by FqnParser#unquotedName.
-	exitUnquotedName(ctx) {
-	}
-
-
-	// Enter a parse tree produced by FqnParser#quotedName.
-	enterQuotedName(ctx) {
-	}
-
-	// Exit a parse tree produced by FqnParser#quotedName.
-	exitQuotedName(ctx) {
-	}
-
-
-
+  // Exit a parse tree produced by FqnParser#quotedName.
+  exitQuotedName(ctx) {}
 }
