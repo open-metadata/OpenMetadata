0c55a7ccc176a05ada3343884258f131388025f3
pmbrull <peremiquelbrull@gmail.com>
2022-04-18 17:38:34 +0200

    Add database required fields

--- ingestion/tests/unit/source/test_source_parsing.py
+++ ingestion/tests/unit/source/test_source_parsing.py
@@ -641,6 +641,7 @@ def test_salesforce():
         "serviceConnection": {
             "config": {
                 "type": "Salesforce",
+                "hostPort": "localhost",
                 "username": "username",
                 "password": "password",
                 "securityToken": "securityToken",
