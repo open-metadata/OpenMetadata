4f5c5a9e235d26a5313f2d4c2e148f3f4783a9b3
Ayush Shah <ayush@getcollate.io>
2022-04-18 11:36:21 +0530

    Ingestion - fix pytest workflow errors and modify airflow pipeline (#4178)

--- ingestion/examples/airflow/dags/airflow_metadata_to_es.py
+++ ingestion/examples/airflow/dags/airflow_metadata_to_es.py
@@ -35,12 +35,18 @@ config = """
 {
   "source": {
     "type": "metadata",
-    "config": {
-      "include_tables": "true",
-      "include_topics": "true",
-      "include_dashboards": "true",
-      "limit_records": 10
-    }
+    "serviceName": "openMetadata",
+    "serviceConnection": {
+      "config":{
+        "type":"MetadataES",
+        "includeTables": "true",
+        "includeUsers": "true",
+        "includeTopics": "true",
+        "includeDashboards": "true",
+        "limitRecords": 10
+      } 
+    },
+    "sourceConfig":{"config":{}}
   },
   "sink": {
     "type": "elasticsearch",
@@ -52,11 +58,10 @@ config = """
       "es_port": 9200
     }
   },
-  "metadata_server": {
-    "type": "metadata-server",
-    "config": {
-      "api_endpoint": "http://localhost:8585/api",
-      "auth_provider_type": "no-auth"
+  "workflowConfig": {
+    "openMetadataServerConfig": {
+      "hostPort": "http://localhost:8585/api",
+      "authProvider": "no-auth"
     }
   }
 }
