09f928b01b5d6c0cc30799078182ceb386e741f3
Onkar Ravgan <onkar.10r@gmail.com>
2022-04-22 22:04:07 +0530

    fixes to trino connector (#4377)
    Co-authored-by: Onkar Ravgan <onkarravgan@Onkars-MacBook-Pro.local>

--- ingestion/src/metadata/ingestion/source/trino.py
+++ ingestion/src/metadata/ingestion/source/trino.py
@@ -16,21 +16,23 @@ from typing import Iterable
 import click
 from sqlalchemy.inspection import inspect
 
-from metadata.generated.schema.entity.services.connections.metadata.openMetadataConnection import (
-    OpenMetadataConnection,
-)
-from metadata.ingestion.api.source import InvalidSourceException
-from metadata.ingestion.models.ometa_table_db import OMetaDatabaseAndTable
-from metadata.ingestion.source.sql_source import SQLSource
-
-logger = logging.getLogger(__name__)
-
+from metadata.generated.schema.entity.data.databaseSchema import DatabaseSchema
 from metadata.generated.schema.entity.services.connections.database.trinoConnection import (
     TrinoConnection,
+)
+from metadata.generated.schema.entity.services.connections.metadata.openMetadataConnection import (
+    OpenMetadataConnection,
 )
 from metadata.generated.schema.metadataIngestion.workflow import (
     Source as WorkflowSource,
 )
+from metadata.ingestion.api.source import InvalidSourceException
+from metadata.ingestion.models.ometa_table_db import OMetaDatabaseAndTable
+from metadata.ingestion.source.sql_source import SQLSource
+from metadata.utils.filters import filter_by_schema
+from metadata.utils.fqdn_generator import get_fqdn
+
+logger = logging.getLogger(__name__)
 
 
 class TrinoSource(SQLSource):
@@ -66,21 +68,29 @@ class TrinoSource(SQLSource):
         self.inspector = inspect(self.engine)
         self.schema_names = (
             self.inspector.get_schema_names()
-            if not self.config.database
-            else [self.config.database]
+            if not self.service_connection.database
+            else [self.service_connection.database]
         )
         return super().prepare()
 
     def next_record(self) -> Iterable[OMetaDatabaseAndTable]:
         for schema in self.schema_names:
             self.database_source_state.clear()
-            if not self.sql_config.schema_filter_pattern.included(schema):
+            if filter_by_schema(
+                self.source_config.schemaFilterPattern, schema_name=schema
+            ):
                 self.status.filter(schema, "Schema pattern not allowed")
                 continue
-            if self.config.include_tables:
+
+            if self.source_config.includeTables:
                 yield from self.fetch_tables(self.inspector, schema)
-            if self.config.include_views:
+            if self.source_config.includeViews:
                 yield from self.fetch_views(self.inspector, schema)
-            if self.config.mark_deleted_tables_as_deleted:
-                schema_fqdn = f"{self.config.service_name}.{schema}"
+            if self.source_config.markDeletedTables:
+                schema_fqdn = get_fqdn(
+                    DatabaseSchema,
+                    self.config.serviceName,
+                    self.service_connection.database,
+                    schema,
+                )
                 yield from self.delete_tables(schema_fqdn)
