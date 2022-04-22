dbdd3caf1da92c9e97aa316490cf3a4b8c809102
Pere Miquel Brull <peremiquelbrull@gmail.com>
2022-04-22 16:59:59 +0200

    Fix #4332 - Athena LEN & hist concat cast + disable (#4371)

--- ingestion/tests/unit/profiler/test_profiler.py
+++ ingestion/tests/unit/profiler/test_profiler.py
@@ -102,9 +102,9 @@ class ProfilerTest(TestCase):
             variance=None,
             distinctCount=2.0,
             distinctProportion=1.0,
-            histogram=Histogram(
-                boundaries=["30.0 to 30.25", "31.0 to 31.25"], frequencies=[1, 1]
-            ),
+            # histogram=Histogram(
+            #     boundaries=["30.0 to 30.25", "31.0 to 31.25"], frequencies=[1, 1]
+            # ),
         )
 
     def test_required_metrics(self):
