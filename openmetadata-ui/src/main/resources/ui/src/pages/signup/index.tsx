143268a2fce2cc85a0adaa74bb1deccac7fcff5d
Sachin-chaurasiya <sachinchaurasiyachotey87@gmail.com>
2022-04-22 15:48:36 +0530

    Fix #4317 Default role permissions are not recognised by UI on Manage tab on sign-up

--- openmetadata-ui/src/main/resources/ui/src/pages/signup/index.tsx
+++ openmetadata-ui/src/main/resources/ui/src/pages/signup/index.tsx
@@ -17,6 +17,7 @@ import { UserProfile } from 'Models';
 import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import appState from '../../AppState';
+import { getLoggedInUserPermissions } from '../../axiosAPIs/miscAPI';
 import { getTeams } from '../../axiosAPIs/teamsAPI';
 import { createUser } from '../../axiosAPIs/userAPI';
 import { Button } from '../../components/buttons/Button/Button';
@@ -46,6 +47,19 @@ const Signup = () => {
 
   const history = useHistory();
 
+  const getUserPermissions = () => {
+    getLoggedInUserPermissions()
+      .then((res: AxiosResponse) => {
+        appState.updateUserPermissions(res.data.metadataOperations);
+      })
+      .catch((err: AxiosError) => {
+        showErrorToast(
+          err,
+          jsonData['api-error-messages']['fetch-user-permission-error']
+        );
+      });
+  };
+
   const selectedTeamsHandler = (id?: string) => {
     setSelectedTeams((prevState: Array<string | undefined>) => {
       if (prevState.includes(id as string)) {
@@ -68,6 +82,7 @@ const Signup = () => {
         if (res.data) {
           appState.updateUserDetails(res.data);
           fetchAllUsers();
+          getUserPermissions();
           history.push(ROUTES.HOME);
         } else {
           setLoading(false);
