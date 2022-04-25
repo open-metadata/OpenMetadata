7bfc00f10ea1318753d8fe555c0aaf3dd4a80972
darth-coder00 <86726556+darth-coder00@users.noreply.github.com>
2022-04-26 02:48:02 +0530

    Fixed user profile sync, test connection and toast progressbar issues (#4480)

--- openmetadata-ui/src/main/resources/ui/src/jsons/en.ts
+++ openmetadata-ui/src/main/resources/ui/src/jsons/en.ts
@@ -82,6 +82,7 @@ const jsonData = {
     'fetch-ingestion-error': 'Error while fetching ingestion workflow!',
     'fetch-lineage-error': 'Error while fetching lineage data!',
     'fetch-lineage-node-error': 'Error while fetching lineage node!',
+    'fetch-logged-in-user-error': 'Error while fetching logged-in user!',
     'fetch-pipeline-details-error': 'Error while fetching pipeline details!',
     'fetch-pipeline-status-error': 'Error while fetching pipeline status!',
     'fetch-policy-error': 'Error while fetching policy details!',
