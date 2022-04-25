6ee6d21ec5dd5be8e18d35a79b63372d8a69a7ee
Mayur Singal <39544459+ulixius9@users.noreply.github.com>
2022-04-25 20:17:49 +0530

    Warehosue is optional (#4477)

--- ingestion/src/metadata/utils/source_connections.py
+++ ingestion/src/metadata/utils/source_connections.py
@@ -242,7 +242,8 @@ def _(connection: SnowflakeConnection):
 
     url += connection.account
     url += f"/{connection.database}" if connection.database else ""
-    url += "?warehouse=" + connection.warehouse
+    if connection.warehouse:
+        url += "?warehouse=" + connection.warehouse
     options = (
         connection.connectionOptions.dict()
         if connection.connectionOptions
