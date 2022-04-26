5200834fc461d7f8d8016c8fa6928b19fc986acf
Akash Jain <15995028+akash-jain-10@users.noreply.github.com>
2022-04-26 22:50:17 +0530

    feat: Prepare 0.10.0 release (#4502)
    * mvn version set to 0.10.0
    
    * prepared docker images for 0.10.0 release
    
    * ingestion-core version bump to release 0.10.0
    
    * ingestion version bump to release 0.10.0
    
    * docker compose image tags version bump to 0.10.0

--- ingestion/setup.py
+++ ingestion/setup.py
@@ -23,7 +23,7 @@ def get_long_description():
 
 
 base_requirements = {
-    "openmetadata-ingestion-core==0.9.0",
+    "openmetadata-ingestion-core==0.10.0",
     "commonregex",
     "idna<3,>=2.5",
     "click>=7.1.1,<8",
@@ -156,7 +156,7 @@ test = {
 build_options = {"includes": ["_cffi_backend"]}
 setup(
     name="openmetadata-ingestion",
-    version="0.10.0.dev0",
+    version="0.10.0",
     url="https://open-metadata.org/",
     author="OpenMetadata Committers",
     license="Apache License 2.0",
