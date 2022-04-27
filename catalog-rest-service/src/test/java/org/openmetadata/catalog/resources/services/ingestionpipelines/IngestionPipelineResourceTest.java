55c91b2a27d5e295bdd00c259f14ac4ad99f450f
Sriharsha Chintalapani <harshach@users.noreply.github.com>
2022-04-27 00:00:19 -0700

    Fix #4509: Ingestion Pipeline changes are not getting stored (#4510)

--- catalog-rest-service/src/test/java/org/openmetadata/catalog/resources/services/ingestionpipelines/IngestionPipelineResourceTest.java
+++ catalog-rest-service/src/test/java/org/openmetadata/catalog/resources/services/ingestionpipelines/IngestionPipelineResourceTest.java
@@ -89,6 +89,7 @@ public class IngestionPipelineResourceTest extends EntityResourceTest<IngestionP
         IngestionPipelineResource.IngestionPipelineList.class,
         "services/ingestionPipelines",
         IngestionPipelineResource.FIELDS);
+    this.supportsEmptyDescription = false;
   }
 
   @BeforeAll
