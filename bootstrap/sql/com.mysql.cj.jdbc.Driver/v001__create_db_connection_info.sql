ca701ff0aa1543045642c46704a1cb8982c9e008
Parth Panchal <parth.panchal@deuexsolutions.com>
2022-04-28 10:23:59 +0530

    reverting mysql change

--- bootstrap/sql/com.mysql.cj.jdbc.Driver/v001__create_db_connection_info.sql
+++ bootstrap/sql/com.mysql.cj.jdbc.Driver/v001__create_db_connection_info.sql
@@ -390,7 +390,7 @@ CREATE TABLE IF NOT EXISTS change_event (
 CREATE TABLE IF NOT EXISTS webhook_entity (
     id VARCHAR(36) GENERATED ALWAYS AS (json ->> '$.id') STORED NOT NULL,
     name VARCHAR(256) GENERATED ALWAYS AS (json ->> '$.name') NOT NULL,
-    deleted BOOLEAN GENERATED ALWAYS AS (JSON_EXTRACT(json, '$.deleted')),
+    deleted BOOLEAN GENERATED ALWAYS AS (json -> '$.deleted'),
     status VARCHAR(256) GENERATED ALWAYS AS (json ->> '$.status') NOT NULL,
     json JSON NOT NULL,
     PRIMARY KEY (id),
