d127e1ba91e964a81fcb189b84030d29842553fa
darth-coder00 <aashit@getcollate.io>
2022-05-02 21:22:25 +0530

    Fix #4155: Left panel filter does not reset after

--- openmetadata-ui/src/main/resources/ui/src/utils/RouterUtils.ts
+++ openmetadata-ui/src/main/resources/ui/src/utils/RouterUtils.ts
@@ -15,10 +15,13 @@ import {
   IN_PAGE_SEARCH_ROUTES,
   PLACEHOLDER_ROUTE_INGESTION_FQN,
   PLACEHOLDER_ROUTE_INGESTION_TYPE,
+  PLACEHOLDER_ROUTE_SEARCHQUERY,
   PLACEHOLDER_ROUTE_SERVICE_CAT,
   PLACEHOLDER_ROUTE_SERVICE_FQN,
+  PLACEHOLDER_ROUTE_TAB,
   ROUTES,
 } from '../constants/constants';
+import { initialFilterQS } from '../constants/explore.constants';
 
 export const isDashboard = (pathname: string): boolean => {
   return pathname === ROUTES.FEEDS;
@@ -78,4 +81,27 @@ export const getEditIngestionPath = (
     .replace(PLACEHOLDER_ROUTE_INGESTION_TYPE, ingestionType);
 
   return path;
+};
+
+/**
+ *
+ * @param searchQuery search text
+ * @param tab selected explore result tab
+ * @param filter selected facet filters
+ * @returns
+ */
+export const getExplorePathWithInitFilters = (
+  searchQuery = '',
+  tab = 'tables',
+  filter: string
+) => {
+  let path = ROUTES.EXPLORE_WITH_SEARCH;
+  path = path
+    .replace(PLACEHOLDER_ROUTE_SEARCHQUERY, searchQuery)
+    .replace(PLACEHOLDER_ROUTE_TAB, tab);
+
+  // return filter ? `${path}?${filter}` : path;
+  return filter
+    ? `${path}?${initialFilterQS}=${encodeURIComponent(filter)}`
+    : path;
 };
