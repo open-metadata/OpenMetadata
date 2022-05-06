c9f7bc63e6b6993b159375a7f3ef069b32104c08
Sriharsha Chintalapani <harsha@getcollate.io>
2022-05-05 22:22:35 -0700

    Fix #4758: Add support for yaml configuration in Ingestion connectors

--- ingestion/setup.py
+++ ingestion/setup.py
@@ -30,6 +30,7 @@ base_requirements = {
     "typing_extensions>=3.7.4",
     "mypy_extensions>=0.4.3",
     "typing-inspect",
+    "expandvars>=0.6.5",
     "pydantic>=1.7.4",
     "pydantic[email]>=1.7.2",
     "google>=3.0.0",
--- ingestion/src/metadata/config/common.py
+++ ingestion/src/metadata/config/common.py
@@ -11,11 +11,13 @@
 
 import io
 import json
-import os
 import pathlib
+import re
 from abc import ABC, abstractmethod
 from typing import IO, Any, Optional
 
+import yaml
+from expandvars import UnboundVariable, expandvars
 from pydantic import BaseModel
 
 FQDN_SEPARATOR: str = "."
@@ -45,22 +47,79 @@ class ConfigurationMechanism(ABC):
         pass
 
 
+class YamlConfigurationMechanism(ConfigurationMechanism):
+    """load configuration from yaml files"""
+
+    def load_config(self, config_fp: IO) -> dict:
+        config = yaml.safe_load(config_fp)
+        return config
+
+
+class JsonConfigurationMechanism(ConfigurationMechanism):
+    """load configuration from json files"""
+
+    def load_config(self, config_fp: IO) -> dict:
+        config = json.load(config_fp)
+        return config
+
+
+def resolve_element(element: str) -> str:
+    if re.search("(\$\{).+(\})", element):  # noqa: W605
+        return expandvars(element, nounset=True)
+    elif element.startswith("$"):
+        try:
+            return expandvars(element, nounset=True)
+        except UnboundVariable:
+            return element
+    else:
+        return element
+
+
+def resolve_list(ele_list: list) -> list:
+    new_v = []
+    for ele in ele_list:
+        if isinstance(ele, str):
+            new_v.append(resolve_element(ele))  # type:ignore
+        elif isinstance(ele, list):
+            new_v.append(resolve_list(ele))  # type:ignore
+        elif isinstance(ele, dict):
+            resolve_env_variables(ele)
+            new_v.append(resolve_env_variables(ele))  # type:ignore
+        else:
+            new_v.append(ele)
+    return new_v
+
+
+def resolve_env_variables(config: dict) -> dict:
+    for k, v in config.items():
+        if isinstance(v, dict):
+            resolve_env_variables(v)
+        elif isinstance(v, list):
+            config[k] = resolve_list(v)
+        elif isinstance(v, str):
+            config[k] = resolve_element(v)
+    return config
+
+
 def load_config_file(config_file: pathlib.Path) -> dict:
     if not config_file.is_file():
         raise ConfigurationError(f"Cannot open config file {config_file}")
 
-    if config_file.suffix not in [".json"]:
+    config_mech: ConfigurationMechanism
+    if config_file.suffix in [".yaml", ".yml"]:
+        config_mech = YamlConfigurationMechanism()
+    elif config_file.suffix == ".toml":
+        config_mech = JsonConfigurationMechanism()
+    else:
         raise ConfigurationError(
-            "Only json are supported. Cannot process file type {}".format(
+            "Only .json and .yml are supported. Cannot process file type {}".format(
                 config_file.suffix
             )
         )
-
     with config_file.open() as raw_config_file:
         raw_config = raw_config_file.read()
 
-    expanded_config_file = os.path.expandvars(raw_config)
-    config_fp = io.StringIO(expanded_config_file)
-    config = json.load(config_fp)
-
+    config_fp = io.StringIO(raw_config)
+    config = config_mech.load_config(config_fp)
+    resolve_env_variables(config)
     return config
