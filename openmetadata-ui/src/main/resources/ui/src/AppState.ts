3382c6b911e66775177fc392190e67306457e10a
Shailesh Parmar <shailesh.parmar.webdev@gmail.com>
2022-05-08 20:35:23 +0530

    Fix issue related to teams joining and leaving, name is not getting dispalyed

--- openmetadata-ui/src/main/resources/ui/src/AppState.ts
+++ openmetadata-ui/src/main/resources/ui/src/AppState.ts
@@ -79,6 +79,7 @@ class AppState {
   }
   updateUserDetails(data: User) {
     this.userDetails = data;
+    this.nonSecureUserDetails = data;
   }
   updateNewUser(data: NewUser) {
     this.newUser = data;
