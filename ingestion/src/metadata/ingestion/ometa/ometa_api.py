878602b4f755a18ce8738857e3e29b7587170d2a
Sriharsha Chintalapani <harsha@getcollate.io>
2022-05-09 12:18:14 -0700

    Change health-check to version api call

--- ingestion/src/metadata/ingestion/ometa/ometa_api.py
+++ ingestion/src/metadata/ingestion/ometa/ometa_api.py
@@ -616,9 +616,9 @@ class OpenMetadata(
 
     def health_check(self) -> bool:
         """
-        Run endpoint health-check. Return `true` if OK
+        Run version api call. Return `true` if response is not None
         """
-        return self.client.get("/health-check")["status"] == "healthy"
+        return not self.client.get("/version")["version"]
 
     def close(self):
         """
