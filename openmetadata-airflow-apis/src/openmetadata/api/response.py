505f6137143f8b10f6f02ab2db986f30521a8889
pmbrull <peremiquelbrull@gmail.com>
2022-05-09 12:43:07 +0200

    Check only None

--- openmetadata-airflow-apis/src/openmetadata/api/response.py
+++ openmetadata-airflow-apis/src/openmetadata/api/response.py
@@ -14,7 +14,6 @@ from typing import Optional, Union
 
 from airflow.models import DagRun
 from flask import Response
-
 from metadata.generated.schema.entity.services.ingestionPipelines.ingestionPipeline import (
     PipelineStatus,
 )
@@ -42,7 +41,8 @@ class ApiResponse:
 
     @staticmethod
     def success(response_obj: Union[Optional[dict], Optional[list]] = None):
-        return ApiResponse.standard_response(ApiResponse.STATUS_OK, response_obj or {})
+        response_body = response_obj if response_obj is not None else {}
+        return ApiResponse.standard_response(ApiResponse.STATUS_OK, response_body)
 
     @staticmethod
     def error(status, error):
