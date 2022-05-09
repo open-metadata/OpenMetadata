be37022107cb7edd1635c1732c7d698e6c9a0b85
pmbrull <peremiquelbrull@gmail.com>
2022-05-09 15:17:21 +0200

    Format

--- ingestion/src/metadata/utils/connections.py
+++ ingestion/src/metadata/utils/connections.py
@@ -238,8 +238,8 @@ def _(connection: KafkaConnection, verbose: bool = False) -> KafkaClient:
     """
     Prepare Kafka Admin Client and Schema Registry Client
     """
-    from confluent_kafka.avro import AvroConsumer
     from confluent_kafka.admin import AdminClient, ConfigResource
+    from confluent_kafka.avro import AvroConsumer
     from confluent_kafka.schema_registry.schema_registry_client import (
         Schema,
         SchemaRegistryClient,
