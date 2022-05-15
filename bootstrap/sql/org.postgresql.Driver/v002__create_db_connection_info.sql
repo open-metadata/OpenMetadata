b73665a55d82c0a646070278d2ae8f7347a48c68
sureshms <srini30005@gmail.com>
2022-05-15 11:13:32 -0700

    WIP

--- bootstrap/sql/org.postgresql.Driver/v002__create_db_connection_info.sql
+++ bootstrap/sql/org.postgresql.Driver/v002__create_db_connection_info.sql
@@ -1,13 +1,6 @@
---
--- Table to be used for generic entities that are limited in number. Examples of generic entities are
--- Attribute entity, domain entities etc.
---
--- This reduces need for defining a table per entity.
---
-CREATE TABLE IF NOT EXISTS generic_entity (
+CREATE TABLE IF NOT EXISTS type_entity (
     id VARCHAR(36) GENERATED ALWAYS AS (json ->> 'id') STORED NOT NULL,
-    -- Fully qualified name formed by entityType + "." + entityName
-    fullyQualifiedName VARCHAR(256) GENERATED ALWAYS AS (json ->> 'fullyQualifiedName') STORED NOT NULL,
+    name VARCHAR(256) GENERATED ALWAYS AS (json ->> 'name') STORED NOT NULL,
     json JSONB NOT NULL,
     updatedAt BIGINT GENERATED ALWAYS AS ((json ->> 'updatedAt')::bigint) STORED NOT NULL,
     updatedBy VARCHAR(256) GENERATED ALWAYS AS (json ->> 'updatedBy') STORED NOT NULL,
