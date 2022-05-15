9095d21b5d6b490d7d7ca0c7bc1452c09b640b21
sureshms <srini30005@gmail.com>
2022-05-15 11:50:34 -0700

    amend

--- catalog-rest-service/src/main/java/org/openmetadata/catalog/CatalogApplication.java
+++ catalog-rest-service/src/main/java/org/openmetadata/catalog/CatalogApplication.java
@@ -85,16 +85,16 @@ public class CatalogApplication extends Application<CatalogApplicationConfig> {
         new SqlLogger() {
           @Override
           public void logAfterExecution(StatementContext context) {
-            LOG.debug(
+            LOG.info(
                 "sql {}, parameters {}, timeTaken {} ms",
                 context.getRenderedSql(),
                 context.getBinding(),
                 context.getElapsedTime(ChronoUnit.MILLIS));
           }
         };
-    if (LOG.isDebugEnabled()) {
+//    if (LOG.isDebugEnabled()) {
       jdbi.setSqlLogger(sqlLogger);
-    }
+//    }
 
     // Configure the Fernet instance
     Fernet.getInstance().setFernetKey(catalogConfig);
