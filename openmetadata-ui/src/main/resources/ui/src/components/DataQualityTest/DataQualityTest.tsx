346fc82e76ac1d59586098fea928335de42d96cb
darth-coder00 <86726556+darth-coder00@users.noreply.github.com>
2022-05-18 06:54:14 +0530

    Fix for updated typescript references of table and tests (#4997)

--- openmetadata-ui/src/main/resources/ui/src/components/DataQualityTest/DataQualityTest.tsx
+++ openmetadata-ui/src/main/resources/ui/src/components/DataQualityTest/DataQualityTest.tsx
@@ -14,7 +14,7 @@
 import classNames from 'classnames';
 import React, { useEffect, useState } from 'react';
 import { TITLE_FOR_NON_ADMIN_ACTION } from '../../constants/constants';
-import { ColumnTestType } from '../../enums/columnTest.enum';
+import { ColumnTestType } from '../../generated/entity/data/table';
 import { TableTest, TableTestType } from '../../generated/tests/tableTest';
 import { useAuth } from '../../hooks/authHooks';
 import {
