b8e9a69f0d0bc10972598d06cad1acbe4d29d19b
mohitdeuex <mohit.y@deuexsolutions.com>
2022-05-18 23:18:09 +0530

    [#5025] - [BACKEND] Moving common files to OpenMetadata-core #5025 [WIP], This will require incremental updates until we have completed updated modules relying on the, once we have all the updates

---
+++ openmetadata-core/src/main/java/org/openmetadata/core/jdbi3/ToEntityReferenceMapper.java
@@ -0,0 +1,27 @@
+/*
+ *  Copyright 2021 Collate
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.openmetadata.core.jdbi3;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.UUID;
+import org.jdbi.v3.core.mapper.RowMapper;
+import org.openmetadata.catalog.type.EntityReference;
+
+public class ToEntityReferenceMapper implements RowMapper<EntityReference> {
+  @Override
+  public EntityReference map(ResultSet rs, org.jdbi.v3.core.statement.StatementContext ctx) throws SQLException {
+    return new EntityReference().withId(UUID.fromString(rs.getString("toId"))).withType(rs.getString("toEntity"));
+  }
+}
