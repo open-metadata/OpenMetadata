6036fec2136c10dbac8be000bc2635bdf4baae2f
mohitdeuex <mohit.y@deuexsolutions.com>
2022-05-20 16:20:47 +0530

    [BUG] - Not able to set request body for the creation of Tags #2624 Fixed it the API endpoint consumes application/json

--- catalog-rest-service/src/main/java/org/openmetadata/catalog/resources/tags/TagResource.java
+++ catalog-rest-service/src/main/java/org/openmetadata/catalog/resources/tags/TagResource.java
@@ -30,14 +30,7 @@ import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
 import javax.validation.Valid;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -70,6 +63,7 @@ import org.openmetadata.catalog.util.ResultList;
 @Path("/v1/tags")
 @Api(value = "Tags resources collection", tags = "Tags resources collection")
 @Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 @Collection(name = "tags")
 public class TagResource {
   public static final String TAG_COLLECTION_PATH = "/v1/tags/";
