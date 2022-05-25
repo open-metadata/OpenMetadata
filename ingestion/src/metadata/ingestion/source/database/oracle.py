41ee3a5aaf8c8b9789f9cca3b3f2275446d1a670
Mayur Singal <39544459+ulixius9@users.noreply.github.com>
2022-05-25 19:11:38 +0530

    Fix #3940 : Refractor Sql Source (#5046)
    Fix #3940 : Refractor Sql Source (#5046)

---
+++ ingestion/src/metadata/ingestion/source/database/oracle.py
@@ -0,0 +1,56 @@
+#  Copyright 2021 Collate
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#  http://www.apache.org/licenses/LICENSE-2.0
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+# This import verifies that the dependencies are available.
+from typing import Optional
+
+from metadata.generated.schema.entity.data.database import Database
+from metadata.generated.schema.entity.services.connections.database.oracleConnection import (
+    OracleConnection,
+)
+from metadata.generated.schema.entity.services.connections.metadata.openMetadataConnection import (
+    OpenMetadataConnection,
+)
+from metadata.generated.schema.metadataIngestion.workflow import (
+    Source as WorkflowSource,
+)
+from metadata.generated.schema.type.entityReference import EntityReference
+from metadata.ingestion.api.source import InvalidSourceException
+from metadata.ingestion.source.database.common_db_source import CommonDbSourceService
+
+
+class OracleSource(CommonDbSourceService):
+    def __init__(self, config, metadata_config):
+        super().__init__(config, metadata_config)
+
+    @classmethod
+    def create(cls, config_dict, metadata_config: OpenMetadataConnection):
+        config = WorkflowSource.parse_obj(config_dict)
+        connection: OracleConnection = config.serviceConnection.__root__.config
+        if not isinstance(connection, OracleConnection):
+            raise InvalidSourceException(
+                f"Expected OracleConnection, but got {connection}"
+            )
+        if config.sourceConfig.config.sampleDataQuery == "select * from {}.{} limit 50":
+            config.sourceConfig.config.sampleDataQuery = (
+                "select * from {}.{} where ROWNUM <= 50"
+            )
+        return cls(config, metadata_config)
+
+    def get_database_entity(self, database: Optional[str]) -> Database:
+        if not database:
+            database = self.service_connection.oracleServiceName
+        return Database(
+            name=database,
+            service=EntityReference(
+                id=self.service.id, type=self.service_connection.type.value
+            ),
+        )
