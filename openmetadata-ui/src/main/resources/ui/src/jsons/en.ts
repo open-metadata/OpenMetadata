3704a3b5b5aabc0b6b773ba49add853244ffd82d
darth-coder00 <aashit@getcollate.io>
2022-05-27 19:55:24 +0530

    Fix Users teams apis integration

--- openmetadata-ui/src/main/resources/ui/src/jsons/en.ts
+++ openmetadata-ui/src/main/resources/ui/src/jsons/en.ts
@@ -107,6 +107,7 @@ const jsonData = {
     'fetch-version-error': 'Error while fetching version!',
     'fetch-webhook-error': 'Error while fetching webhooks!',
     'fetch-user-count-error': 'Error while getting users count!',
+    'fetch-users-error': 'Error while fetching users!',
 
     'test-connection-error': 'Error while testing connection!',
 
