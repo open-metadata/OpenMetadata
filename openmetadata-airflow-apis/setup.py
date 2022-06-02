cc987af0f2125a3758d7411d348253b165d7542f
Akash-Jain <Akash.J@deuexsolutions.com>
2022-06-02 22:31:44 +0530

    version bump for 0.10.2 release

--- openmetadata-airflow-apis/setup.py
+++ openmetadata-airflow-apis/setup.py
@@ -22,7 +22,7 @@ def get_long_description():
 
 
 base_requirements = {
-    "openmetadata-ingestion[airflow-container]~=0.10.1.1",
+    "openmetadata-ingestion[airflow-container]~=0.10.2.0",
     "pendulum~=2.1.2",
     "packaging~=21.2",
     "setuptools~=58.3.0",
@@ -42,7 +42,7 @@ dev_requirements = {
 
 setup(
     name="openmetadata-airflow-managed-apis",
-    version="0.10.1.1",
+    version="0.10.2.0",
     url="https://open-metadata.org/",
     author="OpenMetadata Committers",
     license="Apache License 2.0",
