963294f57568063545132b07cfe552c82fb69eb7
Sachin-chaurasiya <sachinchaurasiyachotey87@gmail.com>
2022-06-08 19:29:32 +0530

    ✨ [Feature] : Advance Search

---
+++ openmetadata-ui/src/main/resources/ui/src/components/AdvanceSearch/AdvanceFields.tsx
@@ -0,0 +1,43 @@
+/*
+ *  Copyright 2021 Collate
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { startCase, uniqueId } from 'lodash';
+import React, { FC } from 'react';
+import { getItemLabel } from '../../utils/AdvanceSearchUtils';
+
+interface Props {
+  fields: Array<string>;
+  onFieldRemove: (value: string) => void;
+}
+
+const AdvanceFields: FC<Props> = ({ fields, onFieldRemove }) => {
+  return (
+    <div className="tw-grid tw-grid-cols-4 tw-gap-2 tw-mb-3">
+      {fields.map((field) => (
+        <div
+          className="tw-bg-white tw-border tw-border-main tw-rounded tw-p-1 tw-flex tw-justify-between"
+          key={uniqueId()}>
+          {startCase(getItemLabel(field))}
+          <span
+            className="tw-cursor-pointer"
+            onClick={() => onFieldRemove(field)}>
+            <FontAwesomeIcon className="tw-text-primary" icon="times" />
+          </span>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default AdvanceFields;
---
+++ openmetadata-ui/src/main/resources/ui/src/components/AdvanceSearch/AdvanceSearchDropDown.tsx
@@ -0,0 +1,52 @@
+/*
+ *  Copyright 2021 Collate
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+import { Dropdown, Menu } from 'antd';
+import React, { FC } from 'react';
+import { getDropDownItems } from '../../utils/AdvanceSearchUtils';
+import { normalLink } from '../../utils/styleconstant';
+import { dropdownIcon as DropdownIcon } from '../../utils/svgconstant';
+
+interface Props {
+  index: string;
+  selectedItems: Array<string>;
+  onSelect: (filter: string) => void;
+}
+
+const AdvanceSearchDropDown: FC<Props> = ({
+  index,
+  onSelect,
+  selectedItems,
+}) => {
+  const items = getDropDownItems(index).map((item) => ({
+    ...item,
+    onClick: () => onSelect(item.key),
+    disabled: selectedItems.includes(item.key),
+  }));
+
+  const menu = <Menu items={items} />;
+
+  return (
+    <Dropdown
+      className="tw-self-center tw-mr-2 tw-cursor-pointer"
+      overlay={menu}
+      trigger={['click']}>
+      <div className="tw-text-primary">
+        <span className="tw-mr-2">Advance Search</span>
+        <DropdownIcon style={{ color: normalLink, margin: '0px' }} />
+      </div>
+    </Dropdown>
+  );
+};
+
+export default AdvanceSearchDropDown;
