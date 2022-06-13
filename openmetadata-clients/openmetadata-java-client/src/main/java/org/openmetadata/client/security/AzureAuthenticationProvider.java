515a88fe1f3be30a887eaaa3d5b1b50f811b3122
Parth Panchal <83201188+parthp2107@users.noreply.github.com>
2022-06-13 17:15:19 +0530

    Fixed#5364: added support for azure sso (#5365)

--- openmetadata-clients/openmetadata-java-client/src/main/java/org/openmetadata/client/security/AzureAuthenticationProvider.java
+++ openmetadata-clients/openmetadata-java-client/src/main/java/org/openmetadata/client/security/AzureAuthenticationProvider.java
@@ -13,26 +13,86 @@
 
 package org.openmetadata.client.security;
 
+import com.google.common.collect.ImmutableSet;
+import com.microsoft.aad.msal4j.ClientCredentialFactory;
+import com.microsoft.aad.msal4j.ClientCredentialParameters;
+import com.microsoft.aad.msal4j.ConfidentialClientApplication;
+import com.microsoft.aad.msal4j.IAuthenticationResult;
+import com.microsoft.aad.msal4j.IClientCredential;
 import feign.RequestTemplate;
+import java.io.IOException;
+import java.util.Set;
+import org.openmetadata.catalog.security.client.AzureSSOClientConfig;
 import org.openmetadata.catalog.services.connections.metadata.OpenMetadataServerConnection;
 import org.openmetadata.client.security.interfaces.AuthenticationProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class AzureAuthenticationProvider implements AuthenticationProvider {
-  @Override
-  public AuthenticationProvider create(OpenMetadataServerConnection iConfig) {
-    return null;
+  private static final Logger LOG = LoggerFactory.getLogger(AzureAuthenticationProvider.class);
+  private OpenMetadataServerConnection serverConfig;
+  private final AzureSSOClientConfig securityConfig;
+  private String generatedAuthToken;
+  private Long expirationTimeMillis;
+
+  public AzureAuthenticationProvider(OpenMetadataServerConnection iConfig) {
+    if (!iConfig.getAuthProvider().equals(OpenMetadataServerConnection.AuthProvider.AZURE)) {
+      LOG.error("Required type to invoke is Azure for AzureAuthentication Provider");
+      throw new RuntimeException("Required type to invoke is Azure for AzureAuthentication Provider");
+    }
+    serverConfig = iConfig;
+
+    securityConfig = (AzureSSOClientConfig) iConfig.getSecurityConfig();
+    if (securityConfig == null) {
+      LOG.error("Security Config is missing, it is required");
+      throw new RuntimeException("Security Config is missing, it is required");
+    }
+    generatedAuthToken = "";
   }
 
   @Override
-  public String authToken() {
-    return null;
+  public AuthenticationProvider create(OpenMetadataServerConnection iConfig) {
+    return new AzureAuthenticationProvider(iConfig);
+  }
+
+  @Override
+  public String authToken() throws IOException {
+    IClientCredential credential = ClientCredentialFactory.createFromSecret(securityConfig.getClientSecret());
+    ConfidentialClientApplication cca =
+        ConfidentialClientApplication.builder(securityConfig.getClientId(), credential)
+            .authority(securityConfig.getAuthority())
+            .build();
+    Set<String> scope = ImmutableSet.of("api://" + securityConfig.getClientId() + "/.default");
+
+    ClientCredentialParameters parameters = ClientCredentialParameters.builder(scope).build();
+    IAuthenticationResult result = cca.acquireToken(parameters).join();
+    generatedAuthToken = result.accessToken();
+    return generatedAuthToken;
   }
 
   @Override
   public String getAccessToken() {
-    return null;
+    return generatedAuthToken;
   }
 
   @Override
-  public void apply(RequestTemplate requestTemplate) {}
+  public void apply(RequestTemplate requestTemplate) {
+    if (requestTemplate.url().contains("version")) {
+      return;
+    }
+    if (requestTemplate.headers().containsKey("Authorization")) {
+      return;
+    }
+    // If first time, get the token
+    if (expirationTimeMillis == null || System.currentTimeMillis() >= expirationTimeMillis) {
+      try {
+        this.authToken();
+      } catch (IOException e) {
+        throw new RuntimeException(e);
+      }
+    }
+    if (getAccessToken() != null) {
+      requestTemplate.header("Authorization", "Bearer " + getAccessToken());
+    }
+  }
 }
