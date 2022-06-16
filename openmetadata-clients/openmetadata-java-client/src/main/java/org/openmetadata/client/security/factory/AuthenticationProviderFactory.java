9b93149fd38516957463d1c2d2cb4a9c340630f6
Parth Panchal <83201188+parthp2107@users.noreply.github.com>
2022-06-16 20:31:42 +0530

    Fixed#5490: added support for Custom sso (#5491)

--- openmetadata-clients/openmetadata-java-client/src/main/java/org/openmetadata/client/security/factory/AuthenticationProviderFactory.java
+++ openmetadata-clients/openmetadata-java-client/src/main/java/org/openmetadata/client/security/factory/AuthenticationProviderFactory.java
@@ -16,6 +16,7 @@ package org.openmetadata.client.security.factory;
 import org.openmetadata.catalog.services.connections.metadata.OpenMetadataServerConnection;
 import org.openmetadata.client.security.Auth0AuthenticationProvider;
 import org.openmetadata.client.security.AzureAuthenticationProvider;
+import org.openmetadata.client.security.CustomOIDCAuthenticationProvider;
 import org.openmetadata.client.security.GoogleAuthenticationProvider;
 import org.openmetadata.client.security.NoOpAuthenticationProvider;
 import org.openmetadata.client.security.OktaAuthenticationProvider;
@@ -34,6 +35,7 @@ public class AuthenticationProviderFactory {
       case AUTH_0:
         return new Auth0AuthenticationProvider(serverConfig);
       case CUSTOM_OIDC:
+        return new CustomOIDCAuthenticationProvider(serverConfig);
       case AZURE:
         return new AzureAuthenticationProvider(serverConfig);
       case OPENMETADATA:
