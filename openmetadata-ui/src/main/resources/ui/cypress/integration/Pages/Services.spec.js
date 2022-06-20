3abdbf4c6eb8bcf51f3c6c2d03e0a6703d512032
Shailesh Parmar <shailesh.parmar.webdev@gmail.com>
2022-06-20 18:22:00 +0530

    coverded issue description is getting updated at the time of updating owner

---
+++ openmetadata-ui/src/main/resources/ui/cypress/integration/Pages/Services.spec.js
@@ -0,0 +1,89 @@
+/*
+ *  Copyright 2021 Collate
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+import { service } from '../../constants/constants';
+
+const updateService = () => {
+  cy.get('[data-testid="edit-description"] > [data-testid="image"]')
+    .should('be.visible')
+    .click();
+  cy.get('.toastui-editor-md-container > .toastui-editor > .ProseMirror')
+    .clear()
+    .type(service.newDescription);
+
+  cy.get('[data-testid="save"]').click();
+  cy.get(
+    '[data-testid="description"] > [data-testid="viewer-container"] > p'
+  ).contains(service.newDescription);
+  cy.get(':nth-child(1) > .link-title').click();
+
+  cy.get('[data-testid="viewer-container"] > p').contains(
+    service.newDescription
+  );
+};
+
+const updateOwner = () => {
+  cy.get('[data-testid="Manage"]').should('be.visible').click();
+  cy.get('[data-testid="owner-dropdown"]').should('be.visible').click();
+
+  cy.get('[data-testid="dropdown-list"] > .tw-flex > :nth-child(1)')
+    .should('exist')
+    .should('be.visible')
+    .click();
+
+  cy.get('[data-testid="list-item"]')
+    .contains(service.Owner)
+    .should('be.visible')
+    .click();
+  cy.get('[data-testid="owner-dropdown"]').should('have.text', service.Owner);
+};
+
+describe('Services page should work properly', () => {
+  beforeEach(() => {
+    cy.goToHomePage();
+    //redirecting to services page
+    cy.get(
+      '.tw-ml-5 > [data-testid="dropdown-item"] > div > [data-testid="menu-button"]'
+    )
+      .scrollIntoView()
+      .should('be.visible')
+      .click();
+    cy.get('[data-testid="menu-item-Services"]').should('be.visible').click();
+    // Todo: need to remove below uncaught exception once tree-view error resolves
+    cy.on('uncaught:exception', () => {
+      // return false to prevent the error from
+      // failing this test
+      return false;
+    });
+  });
+
+  it('Update service description', () => {
+    cy.get(`[data-testid="service-name-${service.name}"]`)
+      .should('be.visible')
+      .click();
+    updateService();
+  });
+
+  it('Update owner and check description', () => {
+    cy.get(`[data-testid="service-name-${service.name}"]`)
+      .should('be.visible')
+      .click();
+    updateOwner();
+    //Checking if description exists after assigning the owner
+    cy.get(':nth-child(1) > .link-title').click();
+
+    cy.get('[data-testid="viewer-container"] > p').contains(
+      service.newDescription
+    );
+  });
+});
