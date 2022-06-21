4cb370e8a9f7cc5dd263a10a41c913e0a3dc83e6
darth-coder00 <86726556+darth-coder00@users.noreply.github.com>
2022-06-22 00:30:22 +0530

    Added UI changes for Mode, datalake and Tour fix (#5556)

--- openmetadata-ui/src/main/resources/ui/src/components/MyAssetStats/MyAssetStats.component.tsx
+++ openmetadata-ui/src/main/resources/ui/src/components/MyAssetStats/MyAssetStats.component.tsx
@@ -118,6 +118,7 @@ const MyAssetStats: FunctionComponent<Props> = ({
   return (
     <Card
       data-testid="data-summary-container"
+      id="assetStatsCount"
       style={{
         border: '1px rgb(221, 227, 234) solid',
         borderRadius: '8px',
