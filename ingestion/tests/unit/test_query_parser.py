4f887b56114af56b7963883e107ae8aeac1b7c9c
pmbrull <peremiquelbrull@gmail.com>
2022-06-22 07:50:20 +0200

    simplify tests

--- ingestion/tests/unit/test_query_parser.py
+++ ingestion/tests/unit/test_query_parser.py
@@ -64,22 +64,11 @@ class QueryParserTest(TestCase):
         Check the join count
         """
         expected_result = {
-            "shopify.dim_address": 100,
-            "shopify.dim_shop": 190,
-            "shopify.dim_customer": 125,
-            "dim_customer": 38,
-            "shopify.dim_location": 75,
-            "dim_location.shop_id": 25,
-            "dim_shop.shop_id": 105,
-            "shopify.dim_product": 130,
-            "dim_product.shop_id": 80,
-            "shopify.dim_product_variant": 35,
-            "dim_shop": 5,
-            "shopify.dim_staff": 75,
-            "shopify.fact_line_item": 100,
-            "shopify.fact_order": 185,
-            "shopify.dim_api_client": 85,
-            "shopify.fact_sale": 420,
+            "shopify.raw_product_catalog": 2,
+            "dim_customer": 2,
+            "fact_order": 2,
+            "shopify.fact_sale": 3,
+            "shopify.raw_customer": 10,
         }
         workflow = Workflow.create(json.loads(config))
         workflow.execute()
