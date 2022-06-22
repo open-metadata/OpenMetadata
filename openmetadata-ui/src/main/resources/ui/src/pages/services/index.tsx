950c3a59926c95461f695243e263c69c67a609b1
Shailesh Parmar <shailesh.parmar.webdev@gmail.com>
2022-06-23 02:24:50 +0530

    Fix #5551 Feat: Added support to add ML model service via UI and service details page (#5583)

--- openmetadata-ui/src/main/resources/ui/src/pages/services/index.tsx
+++ openmetadata-ui/src/main/resources/ui/src/pages/services/index.tsx
@@ -45,6 +45,7 @@ import { ServiceCategory } from '../../enums/service.enum';
 import { DashboardService } from '../../generated/entity/services/dashboardService';
 import { DatabaseService } from '../../generated/entity/services/databaseService';
 import { MessagingService } from '../../generated/entity/services/messagingService';
+import { MlmodelService } from '../../generated/entity/services/mlmodelService';
 import { PipelineService } from '../../generated/entity/services/pipelineService';
 import { EntityReference } from '../../generated/type/entityReference';
 import { Paging } from '../../generated/type/paging';
@@ -71,6 +72,7 @@ type ServiceRecord = {
   messagingServices: Array<MessagingService>;
   dashboardServices: Array<DashboardService>;
   pipelineServices: Array<PipelineService>;
+  mlmodelServices: Array<MlmodelService>;
 };
 
 type ServicePagingRecord = {
@@ -78,6 +80,7 @@ type ServicePagingRecord = {
   messagingServices: Paging;
   dashboardServices: Paging;
   pipelineServices: Paging;
+  mlmodelServices: Paging;
 };
 
 export type ApiData = {
@@ -104,12 +107,14 @@ const ServicesPage = () => {
     messagingServices: pagingObject,
     dashboardServices: pagingObject,
     pipelineServices: pagingObject,
+    mlmodelServices: pagingObject,
   });
   const [services, setServices] = useState<ServiceRecord>({
     databaseServices: [],
     messagingServices: [],
     dashboardServices: [],
     pipelineServices: [],
+    mlmodelServices: [],
   });
   const [serviceList, setServiceList] = useState<Array<ServiceDataObj>>([]);
   const [isLoading, setIsLoading] = useState<boolean>(false);
@@ -121,6 +126,7 @@ const ServicesPage = () => {
     messagingServices: 0,
     dashboardServices: 0,
     pipelineServices: 0,
+    mlmodelServices: 0,
   });
 
   const [currentPage, setCurrentPage] = useState(1);
@@ -170,6 +176,7 @@ const ServicesPage = () => {
               messagingServices: servicePaging.messagingServices.total || 0,
               dashboardServices: servicePaging.dashboardServices.total || 0,
               pipelineServices: servicePaging.pipelineServices.total || 0,
+              mlmodelServices: servicePaging.mlmodelServices.total || 0,
             });
             setServiceList(
               serviceRecord[serviceName] as unknown as Array<ServiceDataObj>
@@ -190,14 +197,14 @@ const ServicesPage = () => {
               }
             }
           }
-          setIsLoading(false);
         })
         .catch((err: AxiosError) => {
           showErrorToast(
             err,
             jsonData['api-error-messages']['fetch-services-error']
           );
-        });
+        })
+        .finally(() => setIsLoading(false));
     }
   };
 
@@ -255,7 +262,7 @@ const ServicesPage = () => {
                 onClick={() => {
                   handleTabChange(tab.name);
                 }}>
-                <p className="tw-text-center tw-self-center label-category">
+                <p className="tw-text-center tw-mb-0 tw-self-center label-category">
                   {tab.displayName}
                 </p>
 
@@ -317,6 +324,31 @@ const ServicesPage = () => {
                 className=" tw-ml-1 tw-font-normal tw-text-grey-body"
                 data-testid="pipeline-url">
                 {pipelineService.connection.config?.hostPort}
+              </span>
+            </div>
+          </>
+        );
+      }
+
+      case ServiceCategory.ML_MODAL_SERVICES: {
+        const mlmodel = service as unknown as MlmodelService;
+
+        return (
+          <>
+            <div className="tw-mb-1 tw-truncate" data-testid="additional-field">
+              <label className="tw-mb-0">Registry:</label>
+              <span
+                className=" tw-ml-1 tw-font-normal tw-text-grey-body"
+                data-testid="pipeline-url">
+                {mlmodel.connection.config?.registryUri}
+              </span>
+            </div>
+            <div className="tw-mb-1 tw-truncate" data-testid="additional-field">
+              <label className="tw-mb-0">Tracking:</label>
+              <span
+                className=" tw-ml-1 tw-font-normal tw-text-grey-body"
+                data-testid="pipeline-url">
+                {mlmodel.connection.config?.trackingUri}
               </span>
             </div>
           </>
@@ -416,7 +448,7 @@ const ServicesPage = () => {
           {serviceList.map((service, index) => (
             <Card key={index} style={leftPanelAntCardStyle}>
               <div
-                className="tw-flex tw-py-2 tw-px-3 tw-justify-between tw-text-grey-muted"
+                className="tw-flex tw-justify-between tw-text-grey-muted"
                 data-testid="service-card">
                 <div className="tw-flex tw-flex-col tw-justify-between tw-truncate">
                   <div>
