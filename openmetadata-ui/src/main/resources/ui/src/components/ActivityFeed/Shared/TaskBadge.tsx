49d47524eae8dc49c49463fbb343d2de8944a6e4
Sachin Chaurasiya <sachinchaurasiyachotey87@gmail.com>
2022-06-30 18:05:33 +0530

    ✨ UI : Add task status icon to the task card (#5786)
    * ✨ UI : Add task status icon in task card
    
    * Add tooltip for task badge
    
    * Change icon size

--- openmetadata-ui/src/main/resources/ui/src/components/ActivityFeed/Shared/TaskBadge.tsx
+++ openmetadata-ui/src/main/resources/ui/src/components/ActivityFeed/Shared/TaskBadge.tsx
@@ -11,22 +11,42 @@
  *  limitations under the License.
  */
 
+import { Popover } from 'antd';
+import { isEqual } from 'lodash';
 import React from 'react';
+import { ThreadTaskStatus } from '../../../generated/entity/feed/thread';
+import SVGIcons, { Icons } from '../../../utils/SvgUtils';
 
-const TaskBadge = () => {
+const TaskBadge = ({ status }: { status: ThreadTaskStatus }) => {
+  const isTaskOpen = isEqual(status, ThreadTaskStatus.Open);
+
+  const popoverContent = isTaskOpen ? 'Status: open' : 'Status: closed';
+
   return (
-    <span
-      className="tw-rounded tw-px-2  tw-absolute tw-left-4 tw--top-3"
-      style={{
-        background: '#F1EDFD',
-        border: '1px solid #E3DAFA',
-        boxShadow: '0px 1px 2px rgba(0, 0, 0, 0.06)',
-        borderRadius: '2px',
-        color: '#7147E8',
-        fontSize: '12px',
-      }}>
-      Task
-    </span>
+    <Popover
+      align={{ targetOffset: [0, -15] }}
+      content={popoverContent}
+      overlayClassName="ant-popover-task-status"
+      trigger="hover"
+      zIndex={9999}>
+      <span
+        className="tw-rounded tw-px-2  tw-absolute tw-left-4 tw--top-3 tw-flex"
+        style={{
+          background: '#F1EDFD',
+          border: '1px solid #C6B5F6',
+          boxShadow: '0px 1px 2px rgba(0, 0, 0, 0.06)',
+          borderRadius: '2px',
+          color: '#7147E8',
+          fontSize: '12px',
+        }}>
+        <SVGIcons
+          alt="task-status"
+          icon={isTaskOpen ? Icons.TASK_OPEN : Icons.TASK_CLOSED}
+          width="12px"
+        />
+        <span className="tw-pl-1">Task</span>
+      </span>
+    </Popover>
   );
 };
 
