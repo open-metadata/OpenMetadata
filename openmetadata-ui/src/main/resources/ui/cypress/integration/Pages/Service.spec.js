69b9350be2a8679f2cde790bc1fdb4652464b18b
Shailesh Parmar <shailesh.parmar.webdev@gmail.com>
2022-07-05 14:09:08 +0530

    remove unwanted spacing

--- openmetadata-ui/src/main/resources/ui/cypress/integration/Pages/Service.spec.js
+++ openmetadata-ui/src/main/resources/ui/cypress/integration/Pages/Service.spec.js
@@ -10,18 +10,17 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-​
+
 import { service } from '../../constants/constants';
-​
+
 const updateService = () => {
   cy.get('[data-testid="edit-description"]')
     .should('exist')
     .should('be.visible')
-    .click();
+    .click({ force: true });
   cy.get('.toastui-editor-md-container > .toastui-editor > .ProseMirror')
     .clear()
     .type(service.newDescription);
-​
   cy.get('[data-testid="save"]').click();
   cy.get(
     '[data-testid="description"] > [data-testid="viewer-container"] > p'
@@ -31,27 +30,25 @@ const updateService = () => {
     service.newDescription
   );
 };
-​
+
 const updateOwner = () => {
   cy.get('[data-testid="Manage"]').should('be.visible').click();
-​
+
   cy.get('[data-testid="owner-dropdown"]')
     .should('exist')
     .should('be.visible')
     .click();
-​
   cy.get('[data-testid="dropdown-list"] > .tw-flex > :nth-child(1)')
     .should('exist')
     .should('be.visible')
     .click();
-​
   cy.get('[data-testid="list-item"]')
     .contains(service.Owner)
     .should('be.visible')
     .click();
   cy.get('[data-testid="owner-dropdown"]').should('have.text', service.Owner);
 };
-​
+
 describe('Services page should work properly', () => {
   beforeEach(() => {
     cy.goToHomePage();
@@ -64,7 +61,7 @@ describe('Services page should work properly', () => {
       .click();
     cy.get('[data-testid="menu-item-Services"]').should('be.visible').click();
   });
-​
+
   it('Update service description', () => {
     cy.intercept('GET', '/**').as('serviceApi');
     cy.wait('@serviceApi');
@@ -75,7 +72,7 @@ describe('Services page should work properly', () => {
     //need wait here
     updateService();
   });
-​
+
   it('Update owner and check description', () => {
     cy.get(`[data-testid="service-name-${service.name}"]`)
       .should('be.visible')
