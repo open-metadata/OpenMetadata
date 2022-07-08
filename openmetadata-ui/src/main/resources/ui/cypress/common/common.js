9af785dbd2e0bd1f87bff864e069cef5bd3cb49e
Sachin-chaurasiya <sachinchaurasiyachotey87@gmail.com>
2022-07-08 10:55:18 +0530

    Fix Cypress test

--- openmetadata-ui/src/main/resources/ui/cypress/common/common.js
+++ openmetadata-ui/src/main/resources/ui/cypress/common/common.js
@@ -235,7 +235,7 @@ export const addNewTagToEntity = (entity, term) => {
       .should('be.visible')
       .type(term);
     cy.wait(500);
-    cy.get(`[data-testid*="tag-option-${term}"]`).should('be.visible').click();
+    cy.get(`[data-testid*="${term}"]`).should('be.visible').click();
   cy.get(
     '[data-testid="tags-wrapper"] > [data-testid="tag-container"]'
   ).contains(term);
@@ -255,7 +255,7 @@ export const addNewTagToEntity = (entity, term) => {
       .should('be.visible')
       .type(term);
     cy.wait(500);
-    cy.get(`[data-testid*="tag-option-${term}"]`).should('be.visible').click();
+    cy.get(`[data-testid*="${term}"]`).should('be.visible').click();
   cy.get('[data-testid="saveAssociatedTag"]')
     .scrollIntoView()
     .should('be.visible')
