2da0efb37fe3bdbfff3ad397fb608f44138f5594
Onkar Ravgan <onkarravgan@Onkars-MacBook-Pro.local>
2022-07-19 11:33:05 +0530

    Updated migration files

--- bootstrap/sql/com.mysql.cj.jdbc.Driver/v004__create_db_connection_info.sql
+++ bootstrap/sql/com.mysql.cj.jdbc.Driver/v004__create_db_connection_info.sql
@@ -3,3 +3,8 @@ SET json = JSON_INSERT(json, '$.teamType', 'Department');
 
 ALTER TABLE team_entity
 ADD teamType VARCHAR(64) GENERATED ALWAYS AS (json ->> '$.teamType') NOT NULL;
+
+--For 0.12.0 release
+UPDATE dbservice_entity
+SET json = JSON_REMOVE(json, '$.connection.config.database')
+WHERE serviceType in ('DynamoDB');
--- bootstrap/sql/org.postgresql.Driver/v004__create_db_connection_info.sql
+++ bootstrap/sql/org.postgresql.Driver/v004__create_db_connection_info.sql
@@ -3,3 +3,8 @@ SET json = JSONB_SET(json, '{teamType}', '"Department"', true);
 
 ALTER TABLE team_entity
 ADD teamType VARCHAR(64) GENERATED ALWAYS AS (json ->> 'teamType') STORED NOT NULL;
+
+--For 0.12.0 release
+UPDATE dbservice_entity
+SET json = json::jsonb #- '{connection,config,database}'
+where serviceType in ('DynamoDB');
