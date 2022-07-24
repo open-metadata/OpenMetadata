810e6c7dfc81bd8502198f4b1f546f81812f7d77
Pere Miquel Brull <peremiquelbrull@gmail.com>
2022-07-24 16:02:26 +0200

    Fix #5822 - Remove looker env and rename props (#5700)
    Fix #5822 - Remove looker env and rename props (#5700)

--- bootstrap/sql/com.mysql.cj.jdbc.Driver/v004__create_db_connection_info.sql
+++ bootstrap/sql/com.mysql.cj.jdbc.Driver/v004__create_db_connection_info.sql
@@ -7,3 +7,23 @@ ADD teamType VARCHAR(64) GENERATED ALWAYS AS (json ->> '$.teamType') NOT NULL;
 UPDATE dbservice_entity
 SET json = JSON_REMOVE(json, '$.connection.config.database')
 WHERE serviceType = 'DynamoDB';
+
+UPDATE dashboard_service_entity
+SET json = JSON_INSERT(
+        JSON_REMOVE(json, '$.connection.config.username'),
+        '$.connection.config.clientId',
+        JSON_EXTRACT(json, '$.connection.config.username')
+    )
+WHERE serviceType = 'Looker';
+
+UPDATE dashboard_service_entity
+SET json = JSON_INSERT(
+        JSON_REMOVE(json, '$.connection.config.password'),
+        '$.connection.config.clientSecret',
+        JSON_EXTRACT(json, '$.connection.config.password')
+    )
+WHERE serviceType = 'Looker';
+
+UPDATE dashboard_service_entity
+SET json = JSON_REMOVE(json, '$.connection.config.env')
+WHERE serviceType = 'Looker';
