62877c2e5de55a59ab82fc6c294ce5cd01ed8d2b
Vivek Ratnavel Subramanian <vivekratnavel90@gmail.com>
2022-07-31 21:02:49 -0700

    Fix #6333 Backend: Add API support to create and list announcements (#6446)

--- bootstrap/sql/org.postgresql.Driver/v004__create_db_connection_info.sql
+++ bootstrap/sql/org.postgresql.Driver/v004__create_db_connection_info.sql
@@ -58,3 +58,7 @@ CREATE TABLE IF NOT EXISTS test_case (
 
 UPDATE webhook_entity
 SET json = JSONB_SET(json::jsonb, '{webhookType}', '"generic"', true);
+
+ALTER TABLE thread_entity
+    ADD announcementStart BIGINT GENERATED ALWAYS AS ((json#>'{announcement,startTime}')::bigint) STORED,
+    ADD announcementEnd BIGINT GENERATED ALWAYS AS ((json#>'{announcement,endTime}')::bigint) STORED;
