8f865d790b7b1d2f9183071d6c1beec0ffbabd0a
Pere Miquel Brull <peremiquelbrull@gmail.com>
2022-08-02 07:56:43 +0200

    DeltaLake does not support profiler

--- bootstrap/sql/org.postgresql.Driver/v004__create_db_connection_info.sql
+++ bootstrap/sql/org.postgresql.Driver/v004__create_db_connection_info.sql
@@ -12,6 +12,10 @@ UPDATE dbservice_entity
 SET json = json::jsonb #- '{connection,config,connectionOptions}'
 where serviceType = 'DeltaLake';
 
+UPDATE dbservice_entity
+SET json = json::jsonb #- '{connection,config,supportsProfiler}'
+where serviceType = 'DeltaLake';
+
 UPDATE dashboard_service_entity
 SET json = jsonb_set(json, '{connection,config,clientId}', json#>'{connection,config,username}')
 WHERE serviceType = 'Looker'
