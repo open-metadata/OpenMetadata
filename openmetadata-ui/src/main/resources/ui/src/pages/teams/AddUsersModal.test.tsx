a22135455269f17b8d00206114e7d7672a864c6a
Shailesh Parmar <shailesh.parmar.webdev@gmail.com>
2022-08-03 16:00:42 +0530

    fixed failing test

--- openmetadata-ui/src/main/resources/ui/src/pages/teams/AddUsersModal.test.tsx
+++ openmetadata-ui/src/main/resources/ui/src/pages/teams/AddUsersModal.test.tsx
@@ -23,73 +23,6 @@ import AddUsersModal from './AddUsersModal';
 
 const mockCancel = jest.fn();
 const mockSave = jest.fn();
-const mockAllUsers = [
-  {
-    id: 'b0c9cab3-a5bc-42a8-bcb4-cfa1460e1ef4',
-    name: 'aaron_johnson0',
-    fullyQualifiedName: 'aaron_johnson0',
-    displayName: 'Aaron Johnson',
-    version: 0.3,
-    updatedAt: 1659369350357,
-    updatedBy: 'anonymous',
-    email: 'aaron_johnson0@gmail.com',
-    href: 'http://localhost:8585/api/v1/users/b0c9cab3-a5bc-42a8-bcb4-cfa1460e1ef4',
-    isAdmin: false,
-    deleted: false,
-  },
-  {
-    id: 'd5ba343a-fd34-4af3-8d3e-cb512a323f35',
-    name: 'aaron_singh2',
-    fullyQualifiedName: 'aaron_singh2',
-    displayName: 'Aaron Singh',
-    version: 0.1,
-    updatedAt: 1659332218590,
-    updatedBy: 'anonymous',
-    email: 'aaron_singh2@gmail.com',
-    href: 'http://localhost:8585/api/v1/users/d5ba343a-fd34-4af3-8d3e-cb512a323f35',
-    isAdmin: false,
-    deleted: false,
-  },
-  {
-    id: '189850b8-97f9-4d52-ace8-769c90a48319',
-    name: 'aaron_warren5',
-    fullyQualifiedName: 'aaron_warren5',
-    displayName: 'Aaron Warren',
-    version: 0.1,
-    updatedAt: 1659332218666,
-    updatedBy: 'anonymous',
-    email: 'aaron_warren5@gmail.com',
-    href: 'http://localhost:8585/api/v1/users/189850b8-97f9-4d52-ace8-769c90a48319',
-    isAdmin: false,
-    deleted: false,
-  },
-  {
-    id: '54b0ae87-4ddf-4cb9-85a1-1cd6b28db8ee',
-    name: 'adam_matthews2',
-    fullyQualifiedName: 'adam_matthews2',
-    displayName: 'Adam Matthews',
-    version: 0.1,
-    updatedAt: 1659332218744,
-    updatedBy: 'anonymous',
-    email: 'adam_matthews2@gmail.com',
-    href: 'http://localhost:8585/api/v1/users/54b0ae87-4ddf-4cb9-85a1-1cd6b28db8ee',
-    isAdmin: false,
-    deleted: false,
-  },
-  {
-    id: 'aba1a593-3788-40ce-a725-c2f26663542e',
-    name: 'adam_rodriguez9',
-    fullyQualifiedName: 'adam_rodriguez9',
-    displayName: 'Adam Rodriguez',
-    version: 0.1,
-    updatedAt: 1659332218824,
-    updatedBy: 'anonymous',
-    email: 'adam_rodriguez9@gmail.com',
-    href: 'http://localhost:8585/api/v1/users/aba1a593-3788-40ce-a725-c2f26663542e',
-    isAdmin: false,
-    deleted: false,
-  },
-];
 const mockUserList = [
   {
     description: 'Robert Mitchell',
@@ -113,16 +46,6 @@ jest.mock('../../components/common/searchbar/Searchbar', () => {
 
 jest.mock('./UserCard', () => {
   return jest.fn().mockReturnValue(<p data-testid="user-card">UserCard</p>);
-});
-
-jest.mock('../../axiosAPIs/userAPI', () => {
-  return {
-    getUsers: jest
-      .fn()
-      .mockImplementation(() =>
-        Promise.resolve({ data: { data: mockAllUsers } })
-      ),
-  };
 });
 
 describe('Test AddUsersModal component', () => {
@@ -157,7 +80,7 @@ describe('Test AddUsersModal component', () => {
     );
     const userCard = await findAllByTestId(container, 'user-card');
 
-    expect(userCard.length).toBe(mockAllUsers.length);
+    expect(userCard.length).toBe(mockUserList.length);
   });
 
   it('Onclick of Discard button, onCancel callback should called', async () => {
