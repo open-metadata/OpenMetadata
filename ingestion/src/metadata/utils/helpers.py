ec9b3ffd949174a3aeed3cf3b3009715c656845f
Pere Miquel Brull <peremiquelbrull@gmail.com>
2022-08-03 08:13:15 +0200

    organise

--- ingestion/src/metadata/utils/helpers.py
+++ ingestion/src/metadata/utils/helpers.py
@@ -290,6 +290,7 @@ def list_to_dict(original: Optional[List[str]], sep: str = "=") -> Dict[str, str
     if not original:
         return {}
 
-    split_original = [(elem.split(sep)) for elem in original if sep in elem]
-
-    return {elem[0]: elem[1] for elem in split_original}
+    split_original = [
+        (elem.split(sep)[0], elem.split(sep)[1]) for elem in original if sep in elem
+    ]
+    return dict(split_original)
