40b6b780f6895b9f0207e241a276d40db779bf95
Aniket Katkar <aniketkatkar97@gmail.com>
2022-08-09 11:54:33 +0530

    Fix unit test failing for window.matchmedia not found

--- openmetadata-ui/src/main/resources/ui/src/components/IngestionStepper/IngestionStepper.test.tsx
+++ openmetadata-ui/src/main/resources/ui/src/components/IngestionStepper/IngestionStepper.test.tsx
@@ -16,23 +16,21 @@ import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import IngestionStepper from './IngestionStepper.component';
 
-describe('IngestionStepper Tests', () => {
-  beforeAll(() => {
-    Object.defineProperty(window, 'matchMedia', {
-      writable: true,
-      value: jest.fn().mockImplementation((query) => ({
-        matches: false,
-        media: query,
-        onchange: null,
-        addListener: jest.fn(),
-        removeListener: jest.fn(),
-        addEventListener: jest.fn(),
-        removeEventListener: jest.fn(),
-        dispatchEvent: jest.fn(),
-      })),
-    });
-  });
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(),
+    removeListener: jest.fn(),
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
 
+describe('IngestionStepper Tests', () => {
   it('Component should render properly', () => {
     const { container } = render(
       <IngestionStepper
