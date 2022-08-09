507f92e305692dfadee6bd83a8e714a6b2e7f5e5
Shailesh Parmar <shailesh.parmar.webdev@gmail.com>
2022-08-09 18:46:15 +0530

    updated setting model design

--- openmetadata-ui/src/main/resources/ui/src/components/TableProfiler/Component/ProfilerSettingsModal.tsx
+++ openmetadata-ui/src/main/resources/ui/src/components/TableProfiler/Component/ProfilerSettingsModal.tsx
@@ -11,40 +11,32 @@
  *  limitations under the License.
  */
 
-import { Modal, Select, Slider } from 'antd';
+import { PlusOutlined } from '@ant-design/icons';
+import { Button, Form, Modal, Select, Slider } from 'antd';
 import { AxiosError } from 'axios';
 import 'codemirror/addon/fold/foldgutter.css';
-import { isEmpty, isUndefined } from 'lodash';
+import { isEmpty, isEqual, isUndefined, startCase } from 'lodash';
 import React, { useEffect, useMemo, useState } from 'react';
 import { Controlled as CodeMirror } from 'react-codemirror2';
 import {
   getTableProfilerConfig,
   putTableProfileConfig,
 } from '../../../axiosAPIs/tableAPI';
-import { JSON_TAB_SIZE } from '../../../constants/constants';
-import { CSMode } from '../../../enums/codemirror.enum';
-import { TableProfilerConfig } from '../../../generated/entity/data/table';
+import {
+  codeMirrorOption,
+  DEFAULT_INCLUDE_PROFILE,
+  PROFILER_METRIC,
+} from '../../../constants/entity.constants';
+import {
+  ColumnProfilerConfig,
+  TableProfilerConfig,
+} from '../../../generated/entity/data/table';
 import jsonData from '../../../jsons/en';
+import SVGIcons, { Icons } from '../../../utils/SvgUtils';
 import { showErrorToast, showSuccessToast } from '../../../utils/ToastUtils';
 import Loader from '../../Loader/Loader';
 import { ProfilerSettingsModalProps } from '../TableProfiler.interface';
 import '../tableProfiler.less';
-
-const options = {
-  tabSize: JSON_TAB_SIZE,
-  indentUnit: JSON_TAB_SIZE,
-  indentWithTabs: true,
-  lineNumbers: true,
-  lineWrapping: true,
-  styleActiveLine: true,
-  matchBrackets: true,
-  autoCloseBrackets: true,
-  foldGutter: true,
-  gutters: ['CodeMirror-linenumbers', 'CodeMirror-foldgutter'],
-  mode: {
-    name: CSMode.SQL,
-  },
-};
 
 const ProfilerSettingsModal: React.FC<ProfilerSettingsModalProps> = ({
   tableId,
@@ -56,12 +48,20 @@ const ProfilerSettingsModal: React.FC<ProfilerSettingsModalProps> = ({
   const [sqlQuery, setSqlQuery] = useState<string>('');
   const [profileSample, setProfileSample] = useState<number>();
   const [excludeCol, setExcludeCol] = useState<string[]>([]);
-  const [includeCol, setIncludeCol] = useState<string[]>([]);
+  const [includeCol, setIncludeCol] = useState<ColumnProfilerConfig[]>(
+    DEFAULT_INCLUDE_PROFILE
+  );
 
   const selectOptions = useMemo(() => {
     return columnProfile.map(({ name }) => ({
       label: name,
       value: name,
+    }));
+  }, [columnProfile]);
+  const metricsOptions = useMemo(() => {
+    return PROFILER_METRIC.map((metric) => ({
+      label: startCase(metric),
+      value: metric,
     }));
   }, [columnProfile]);
 
@@ -72,14 +72,12 @@ const ProfilerSettingsModal: React.FC<ProfilerSettingsModalProps> = ({
       if (response) {
         const { tableProfilerConfig } = response;
         if (tableProfilerConfig) {
-          const includeCol =
-            tableProfilerConfig.includeColumns?.map(
-              ({ columnName }) => columnName as string
-            ) || [];
           setSqlQuery(tableProfilerConfig.profileQuery || '');
           setProfileSample(tableProfilerConfig.profileSample);
           setExcludeCol(tableProfilerConfig.excludeColumns || []);
-          setIncludeCol(includeCol);
+          setIncludeCol(
+            tableProfilerConfig.includeColumns || DEFAULT_INCLUDE_PROFILE
+          );
         }
       } else {
         throw jsonData['api-error-messages'][
@@ -100,11 +98,11 @@ const ProfilerSettingsModal: React.FC<ProfilerSettingsModalProps> = ({
       excludeColumns: excludeCol.length > 0 ? excludeCol : undefined,
       profileQuery: !isEmpty(sqlQuery) ? sqlQuery : undefined,
       profileSample: !isUndefined(profileSample) ? profileSample : undefined,
-      includeColumns:
-        includeCol.length > 0
-          ? includeCol.map((col) => ({ columnName: col }))
-          : undefined,
+      includeColumns: !isEqual(includeCol, DEFAULT_INCLUDE_PROFILE)
+        ? includeCol
+        : undefined,
     };
+
     try {
       const data = await putTableProfileConfig(tableId, profileConfig);
       if (data) {
@@ -130,6 +128,9 @@ const ProfilerSettingsModal: React.FC<ProfilerSettingsModalProps> = ({
   return (
     <Modal
       destroyOnClose
+      cancelButtonProps={{
+        type: 'link',
+      }}
       data-testid="profiler-settings-modal"
       maskClosable={false}
       okText="Save"
@@ -144,8 +145,9 @@ const ProfilerSettingsModal: React.FC<ProfilerSettingsModalProps> = ({
         <>
           <div className="tw-pb-4" data-testid="profile-sample-container">
             <p>Profile Sample %</p>
-            <div className="tw-px-2">
+            <div className="tw-px-2 tw-mb-1.5">
               <Slider
+                className="profiler-slider"
                 marks={{ 0: '0%', 100: '100%' }}
                 max={100}
                 min={0}
@@ -157,11 +159,11 @@ const ProfilerSettingsModal: React.FC<ProfilerSettingsModalProps> = ({
             </div>
           </div>
           <div className="tw-pb-4" data-testid="sql-editor-container">
-            <p>Profile Sample Query</p>
+            <p className="tw-mb-1.5">Profile Sample Query</p>
             <CodeMirror
               className="profiler-setting-sql-editor"
               data-testid="profiler-setting-sql-editor"
-              options={options}
+              options={codeMirrorOption}
               value={sqlQuery}
               onBeforeChange={(_Editor, _EditorChange, value) => {
                 setSqlQuery(value);
@@ -171,32 +173,87 @@ const ProfilerSettingsModal: React.FC<ProfilerSettingsModalProps> = ({
               }}
             />
           </div>
-          <p className="tw-mb-1">Enable column profile</p>
-          <div className="tw-pb-4" data-testid="include-column-container">
-            <p className="tw-text-xs">Include:</p>
-            <Select
-              allowClear
-              className="tw-w-full"
-              data-testid="include-column-select"
-              defaultValue={includeCol}
-              mode="tags"
-              options={selectOptions}
-              placeholder="Select columns to include"
-              size="middle"
-              onChange={(value) => setIncludeCol(value)}
-            />
-          </div>
+          <p className="tw-mb-4">Enable column profile</p>
+          <Form
+            autoComplete="off"
+            initialValues={{
+              includeColumns: includeCol,
+            }}
+            layout="vertical"
+            name="dynamic_form_nest_item"
+            onValuesChange={(_, data) => {
+              setIncludeCol(data.includeColumns);
+            }}>
+            <Form.List name="includeColumns">
+              {(fields, { add, remove }) => (
+                <>
+                  <div className="tw-flex tw-items-center tw-mb-1.5">
+                    <p className="w-form-label tw-text-xs tw-mr-3">Include:</p>
+                    <Button
+                      className="include-columns-add-button"
+                      icon={<PlusOutlined />}
+                      size="small"
+                      type="primary"
+                      onClick={() => add()}
+                    />
+                  </div>
+                  {fields.map(({ key, name, ...restField }) => (
+                    <div className="tw-flex tw-gap-2 tw-w-full" key={key}>
+                      <Form.Item
+                        className="tw-w-11/12 tw-mb-4"
+                        {...restField}
+                        name={[name, 'columnName']}>
+                        <Select
+                          className="tw-w-full"
+                          data-testid="exclude-column-select"
+                          options={selectOptions}
+                          placeholder="Select columns to include"
+                          size="middle"
+                        />
+                      </Form.Item>
+                      <Form.Item
+                        className="tw-w-11/12 tw-mb-4"
+                        {...restField}
+                        name={[name, 'metrics']}>
+                        <Select
+                          allowClear
+                          className="tw-w-full"
+                          data-testid="exclude-column-select"
+                          mode="tags"
+                          options={metricsOptions}
+                          placeholder="Select metrics"
+                          size="middle"
+                        />
+                      </Form.Item>
+                      <Button
+                        icon={
+                          <SVGIcons
+                            alt="delete"
+                            className="tw-w-4"
+                            icon={Icons.DELETE}
+                          />
+                        }
+                        type="text"
+                        onClick={() => remove(name)}
+                      />
+                    </div>
+                  ))}
+                </>
+              )}
+            </Form.List>
+          </Form>
+
           <div className="tw-pb-4" data-testid="exclude-column-container">
-            <p className="tw-text-xs">Exclude:</p>
+            <p className="tw-text-xs tw-mb-1.5">Exclude:</p>
             <Select
               allowClear
               className="tw-w-full"
               data-testid="exclude-column-select"
-              defaultValue={excludeCol}
               mode="tags"
               options={selectOptions}
               placeholder="Select columns to exclude"
               size="middle"
+              value={excludeCol}
               onChange={(value) => setExcludeCol(value)}
             />
           </div>
