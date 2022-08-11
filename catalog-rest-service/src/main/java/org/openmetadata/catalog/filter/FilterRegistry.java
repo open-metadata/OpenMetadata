e4b62bf3b20c5eeef2ab10e83bea8babe916f315
mohitdeuex <mohit.y@deuexsolutions.com>
2022-08-11 20:36:27 +0530

    [Backend][Settings] review fix - review wildcard imports

--- catalog-rest-service/src/main/java/org/openmetadata/catalog/filter/FilterRegistry.java
+++ catalog-rest-service/src/main/java/org/openmetadata/catalog/filter/FilterRegistry.java
@@ -1,6 +1,9 @@
 package org.openmetadata.catalog.filter;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import org.openmetadata.catalog.type.EventType;
 
