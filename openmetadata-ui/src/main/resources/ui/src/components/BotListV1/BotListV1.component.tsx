e464d9aae0da45969f488d50df96b3be435031d1
Chirag Madlani <12962843+chirag-madlani@users.noreply.github.com>
2022-08-13 11:26:08 +0530

    fix tests address comments

--- openmetadata-ui/src/main/resources/ui/src/components/BotListV1/BotListV1.component.tsx
+++ openmetadata-ui/src/main/resources/ui/src/components/BotListV1/BotListV1.component.tsx
@@ -1,3 +1,16 @@
+/*
+ *  Copyright 2021 Collate
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 import { Button, Col, Row, Space, Table, Tooltip } from 'antd';
 import { ColumnsType } from 'antd/lib/table';
 import { AxiosError } from 'axios';
@@ -14,7 +27,7 @@ import NextPrevious from '../common/next-previous/NextPrevious';
 import Loader from '../Loader/Loader';
 import { BotListV1Props } from './BotListV1.interfaces';
 
-export const BotListV1 = ({ showDeleted }: BotListV1Props) => {
+const BotListV1 = ({ showDeleted }: BotListV1Props) => {
   const [botUsers, setBotUsers] = useState<Bot[]>([]);
   const [paging, setPaging] = useState<Paging>({} as Paging);
   const [selectedUser, setSelectedUser] = useState<Bot>();
@@ -146,3 +159,5 @@ export const BotListV1 = ({ showDeleted }: BotListV1Props) => {
     </>
   );
 };
+
+export default BotListV1;
--- openmetadata-ui/src/main/resources/ui/src/pages/BotsPageV1/BotsPageV1.component.tsx
+++ openmetadata-ui/src/main/resources/ui/src/pages/BotsPageV1/BotsPageV1.component.tsx
@@ -1,7 +1,20 @@
+/*
+ *  Copyright 2021 Collate
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 import { Button, Col, Row, Space, Switch } from 'antd';
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
-import { BotListV1 } from '../../components/BotListV1/BotListV1.component';
+import BotListV1 from '../../components/BotListV1/BotListV1.component';
 import { getCreateUserPath } from '../../constants/constants';
 
 export const BotsPageV1 = () => {
