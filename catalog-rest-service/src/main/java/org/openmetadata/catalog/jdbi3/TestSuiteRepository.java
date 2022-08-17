0457f05269df60d44b49ce28d271b8800c8151c5
Sriharsha Chintalapani <harsha@getcollate.io>
2022-08-16 23:44:37 -0700

    Fix #5831: Add pipeline to testsuite

--- catalog-rest-service/src/main/java/org/openmetadata/catalog/jdbi3/TestSuiteRepository.java
+++ catalog-rest-service/src/main/java/org/openmetadata/catalog/jdbi3/TestSuiteRepository.java
@@ -3,9 +3,11 @@ package org.openmetadata.catalog.jdbi3;
 import static org.openmetadata.catalog.Entity.TEST_SUITE;
 
 import java.io.IOException;
+import org.openmetadata.catalog.Entity;
 import org.openmetadata.catalog.resources.dqtests.TestSuiteResource;
 import org.openmetadata.catalog.tests.TestSuite;
 import org.openmetadata.catalog.type.EntityReference;
+import org.openmetadata.catalog.type.Relationship;
 import org.openmetadata.catalog.util.EntityUtil;
 
 public class TestSuiteRepository extends EntityRepository<TestSuite> {
@@ -26,6 +28,7 @@ public class TestSuiteRepository extends EntityRepository<TestSuite> {
   @Override
   public TestSuite setFields(TestSuite entity, EntityUtil.Fields fields) throws IOException {
     entity.setOwner(fields.contains("owner") ? getOwner(entity) : null);
+    entity.setPipeline(fields.contains("pipelines") ? getIngestionPipeline(entity) : null);
     return entity;
   }
 
@@ -48,6 +51,10 @@ public class TestSuiteRepository extends EntityRepository<TestSuite> {
   @Override
   public void storeRelationships(TestSuite entity) {
     storeOwner(entity, entity.getOwner());
+  }
+
+  private EntityReference getIngestionPipeline(TestSuite testSuite) throws IOException {
+    return getToEntityRef(testSuite.getId(), Relationship.CONTAINS, Entity.INGESTION_PIPELINE, false);
   }
 
   public class TestSuiteUpdater extends EntityUpdater {
