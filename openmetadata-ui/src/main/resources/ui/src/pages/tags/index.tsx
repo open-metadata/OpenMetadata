c10d40b39f4874bbb7584c47a30e35893200da67
Aniket Katkar <aniketkatkar97@gmail.com>
2022-08-25 23:27:39 +0530

    Fix #6893 : UI improvements and bug fixes

--- openmetadata-ui/src/main/resources/ui/src/pages/tags/index.tsx
+++ openmetadata-ui/src/main/resources/ui/src/pages/tags/index.tsx
@@ -70,6 +70,7 @@ import SVGIcons from '../../utils/SvgUtils';
 import { getTagCategories } from '../../utils/TagsUtils';
 import { showErrorToast } from '../../utils/ToastUtils';
 import Form from './Form';
+import './TagsPage.style.less';
 
 type DeleteTagDetailsType = {
   id: string;
@@ -415,6 +416,7 @@ const TagsPage = () => {
   const fetchLeftPanel = () => {
     return (
       <Card
+        className="tags-page-left-panel"
         data-testid="data-summary-container"
         size="small"
         style={leftPanelAntCardStyle}
