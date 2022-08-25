0650bf43ebec69b5de889cd4e25fe8a7aee708e5
Nahuel Verdugo Revigliono <nahuel@getcollate.io>
2022-08-25 14:27:41 +0200

    Fix logging issues in airflow

--- openmetadata-airflow-apis/openmetadata_managed_apis/utils/logger.py
+++ openmetadata-airflow-apis/openmetadata_managed_apis/utils/logger.py
@@ -8,18 +8,6 @@ BASE_LOGGING_FORMAT = (
     "[%(asctime)s] %(levelname)-8s {%(name)s:%(module)s:%(lineno)d} - %(message)s"
 )
 
-logging.basicConfig(
-    handlers=[
-        RotatingFileHandler(
-            f"{conf.get('logging', 'base_log_folder', fallback='.')}/openmetadata_airflow_api.log",
-            maxBytes=1000000,
-            backupCount=10,
-        )
-    ],
-    format=BASE_LOGGING_FORMAT,
-    level=logging.DEBUG,
-)
-
 
 class Loggers(Enum):
     API_ROUTES = "AirflowAPIRoutes"
@@ -28,17 +16,31 @@ class Loggers(Enum):
     WORKFLOW = "AirflowWorkflow"
 
 
-def routes_logger():
-    return logging.getLogger(Loggers.API_ROUTES.value)
+def build_logger(logger_name: str) -> logging.Logger:
+    logger = logging.getLogger(logger_name)
+    log_format = logging.Formatter(BASE_LOGGING_FORMAT)
+    rotating_log_handler = RotatingFileHandler(
+        f"{conf.get('logging', 'base_log_folder', fallback='')}/openmetadata_airflow_api.log",
+        maxBytes=1000000,
+        backupCount=10,
+    )
+    rotating_log_handler.setFormatter(log_format)
+    logger.addHandler(rotating_log_handler)
+    logger.setLevel(logging.DEBUG)
+    return logger
+
+
+def routes_logger() -> logging.Logger:
+    return build_logger(Loggers.API_ROUTES.value)
 
 
 def api_logger():
-    return logging.getLogger(Loggers.API.value)
+    return build_logger(Loggers.API.value)
 
 
 def operations_logger():
-    return logging.getLogger(Loggers.OPERATIONS.value)
+    return build_logger(Loggers.OPERATIONS.value)
 
 
 def workflow_logger():
-    return logging.getLogger(Loggers.WORKFLOW.value)
+    return build_logger(Loggers.WORKFLOW.value)
