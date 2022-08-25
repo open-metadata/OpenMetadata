5c08101282c80ff1fed1abb5f2226957e75efe6d
Nahuel Verdugo Revigliono <nahuel@getcollate.io>
2022-08-25 12:35:42 +0200

    Remove unused import

--- openmetadata-airflow-apis/openmetadata_managed_apis/workflows/ingestion/common.py
+++ openmetadata-airflow-apis/openmetadata_managed_apis/workflows/ingestion/common.py
@@ -17,8 +17,6 @@ from typing import Callable, Optional, Union
 
 import airflow
 from airflow import DAG
-from openmetadata_managed_apis.api.utils import clean_dag_id
-from openmetadata_managed_apis.workflows import workflow_factory
 
 from metadata.generated.schema.entity.services.dashboardService import DashboardService
 from metadata.generated.schema.entity.services.databaseService import DatabaseService
@@ -30,6 +28,7 @@ from metadata.ingestion.models.encoders import show_secrets_encoder
 from metadata.ingestion.ometa.ometa_api import OpenMetadata
 from metadata.orm_profiler.api.workflow import ProfilerWorkflow
 from metadata.utils.logger import set_loggers_level
+from openmetadata_managed_apis.api.utils import clean_dag_id
 
 try:
     from airflow.operators.python import PythonOperator
