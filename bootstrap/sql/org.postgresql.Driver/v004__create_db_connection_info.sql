482ac05dfb45f845675d72d3e71b512225b0d7f8
Sriharsha Chintalapani <harsha@getcollate.io>
2022-08-25 19:07:56 -0700

    Fix #6936: testPlatforms is required field in testSuite and createTestSuite schema

--- bootstrap/sql/org.postgresql.Driver/v004__create_db_connection_info.sql
+++ bootstrap/sql/org.postgresql.Driver/v004__create_db_connection_info.sql
@@ -91,3 +91,12 @@ WHERE serviceType = 'Athena';
 UPDATE dbservice_entity
 SET json = json::jsonb #- '{connection,config,username}' #- '{connection,config,password}'
 WHERE serviceType in ('Databricks');
+
+CREATE TABLE IF NOT EXISTS openmetadata_settings (
+    id SERIAL NOT NULL ,
+    configType VARCHAR(36) NOT NULL,
+    json JSONB NOT NULL,
+    PRIMARY KEY (id, configType),
+    UNIQUE(configType)
+);
+
