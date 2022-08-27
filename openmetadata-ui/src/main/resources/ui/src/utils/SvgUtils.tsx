629d92660b7140f35c197efbbd23d79e0cbbdddc
Ashish gupta <ag939431@gmail.com>
2022-08-27 23:01:10 +0530

    Ms Teams UI for setting Page

--- openmetadata-ui/src/main/resources/ui/src/utils/SvgUtils.tsx
+++ openmetadata-ui/src/main/resources/ui/src/utils/SvgUtils.tsx
@@ -127,6 +127,7 @@ import Logo from '../assets/svg/logo.svg';
 import IconManageColor from '../assets/svg/manage-color.svg';
 import IconMinus from '../assets/svg/minus.svg';
 import IconMlModal from '../assets/svg/mlmodal.svg';
+import IconMSTeamsGrey from '../assets/svg/ms-teams-grey.svg';
 import IconPaperPlanePrimary from '../assets/svg/paper-plane-primary.svg';
 import IconPaperPlane from '../assets/svg/paper-plane.svg';
 import IconPendingBadge from '../assets/svg/pending-badge.svg';
@@ -337,6 +338,7 @@ export const Icons = {
   INFO_SECONDARY: 'info-secondary',
   ICON_REMOVE: 'icon-remove',
   IC_EDIT_PRIMARY: 'ic-edit-primary',
+  MSTEAMS_GREY: 'msteams-grey',
 };
 
 const SVGIcons: FunctionComponent<Props> = ({
@@ -987,6 +989,11 @@ const SVGIcons: FunctionComponent<Props> = ({
 
     case Icons.INFO_SECONDARY:
       IconComponent = IconInfoSecondary;
+
+      break;
+
+    case Icons.MSTEAMS_GREY:
+      IconComponent = IconMSTeamsGrey;
 
       break;
 
