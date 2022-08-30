814be3f4451f0dfca8f654f1e82035fdd466ea2d
Milan Bariya <milanbariya12@gmail.com>
2022-08-30 18:01:36 +0530

    Fix: add a new property to all JSON Schemas for databases

--- ingestion/src/metadata/utils/connections.py
+++ ingestion/src/metadata/utils/connections.py
@@ -168,7 +168,8 @@ def create_generic_connection(connection, verbose: bool = False) -> Engine:
         echo=verbose,
         max_overflow=-1,
     )
-    listen(engine, "before_cursor_execute", inject_query_header, retval=True)
+    if connection.supportsQueryComment:
+        listen(engine, "before_cursor_execute", inject_query_header, retval=True)
 
     return engine
 
