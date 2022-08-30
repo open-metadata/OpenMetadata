503873a0a45f2f6c0f04ddfb6e1d704a2cc9616e
Sachin Chaurasiya <sachinchaurasiyachotey87@gmail.com>
2022-08-31 01:46:33 +0530

    Update Glossary with new roles and policy (#7067)
    * Update Glossary with new roles and policy
    
    * Fix unit tests
    
    * Fix tests
    
    * Fix tests

--- openmetadata-ui/src/main/resources/ui/src/router/AuthenticatedAppRouter.tsx
+++ openmetadata-ui/src/main/resources/ui/src/router/AuthenticatedAppRouter.tsx
@@ -12,7 +12,7 @@
  */
 
 import { isEmpty } from 'lodash';
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useMemo } from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import AppState from '../AppState';
 import { usePermissionProvider } from '../components/PermissionProvider/PermissionProvider';
@@ -212,6 +212,32 @@ const EditRulePage = withSuspenseFallback(
 const AuthenticatedAppRouter: FunctionComponent = () => {
   const { permissions } = usePermissionProvider();
 
+  const glossaryPermission = useMemo(
+    () =>
+      checkPermission(Operation.ViewAll, ResourceEntity.GLOSSARY, permissions),
+    [permissions]
+  );
+
+  const glossaryTermPermission = useMemo(
+    () =>
+      checkPermission(
+        Operation.ViewAll,
+        ResourceEntity.GLOSSARY_TERM,
+        permissions
+      ),
+    [permissions]
+  );
+
+  const tagCategoryPermission = useMemo(
+    () =>
+      checkPermission(
+        Operation.ViewAll,
+        ResourceEntity.TAG_CATEGORY,
+        permissions
+      ),
+    [permissions]
+  );
+
   return (
     <Switch>
       <Route exact component={MyDataPage} path={ROUTES.MY_DATA} />
@@ -251,8 +277,18 @@ const AuthenticatedAppRouter: FunctionComponent = () => {
         {!isEmpty(AppState.userDetails) && <Redirect to={ROUTES.HOME} />}
       </Route>
       <Route exact component={SwaggerPage} path={ROUTES.SWAGGER} />
-      <Route exact component={TagsPage} path={ROUTES.TAGS} />
-      <Route exact component={TagsPage} path={ROUTES.TAG_DETAILS} />
+      <AdminProtectedRoute
+        exact
+        component={TagsPage}
+        hasPermission={tagCategoryPermission}
+        path={ROUTES.TAGS}
+      />
+      <AdminProtectedRoute
+        exact
+        component={TagsPage}
+        hasPermission={tagCategoryPermission}
+        path={ROUTES.TAG_DETAILS}
+      />
       <Route exact component={DatabaseDetails} path={ROUTES.DATABASE_DETAILS} />
       <Route
         exact
@@ -303,9 +339,24 @@ const AuthenticatedAppRouter: FunctionComponent = () => {
       />
       <Route exact component={EntityVersionPage} path={ROUTES.ENTITY_VERSION} />
       <Route exact component={EditWebhookPage} path={ROUTES.EDIT_WEBHOOK} />
-      <Route exact component={GlossaryPageV1} path={ROUTES.GLOSSARY} />
-      <Route exact component={GlossaryPageV1} path={ROUTES.GLOSSARY_DETAILS} />
-      <Route exact component={GlossaryPageV1} path={ROUTES.GLOSSARY_TERMS} />
+      <AdminProtectedRoute
+        exact
+        component={GlossaryPageV1}
+        hasPermission={glossaryPermission}
+        path={ROUTES.GLOSSARY}
+      />
+      <AdminProtectedRoute
+        exact
+        component={GlossaryPageV1}
+        hasPermission={glossaryPermission}
+        path={ROUTES.GLOSSARY_DETAILS}
+      />
+      <AdminProtectedRoute
+        exact
+        component={GlossaryPageV1}
+        hasPermission={glossaryTermPermission}
+        path={ROUTES.GLOSSARY_TERMS}
+      />
       <Route exact component={UserPage} path={ROUTES.USER_PROFILE} />
       <Route exact component={UserPage} path={ROUTES.USER_PROFILE_WITH_TAB} />
       <Route exact component={MlModelPage} path={ROUTES.MLMODEL_DETAILS} />
