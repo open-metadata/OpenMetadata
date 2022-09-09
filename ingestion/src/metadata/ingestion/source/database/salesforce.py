a2d3c902cffec5ee34c3873a6a3661fab0695c2d
ulixius9 <mayursingal9@gmail.com>
2022-09-09 22:42:17 +0530

    Salseforce Datatype error Fix on 0.11.5

--- ingestion/src/metadata/ingestion/source/database/salesforce.py
+++ ingestion/src/metadata/ingestion/source/database/salesforce.py
@@ -39,6 +39,7 @@ from metadata.ingestion.api.source import InvalidSourceException, Source, Source
 from metadata.ingestion.models.ometa_table_db import OMetaDatabaseAndTable
 from metadata.ingestion.ometa.ometa_api import OpenMetadata
 from metadata.utils.connections import get_connection
+from metadata.utils.filters import filter_by_table
 from metadata.utils.logger import ingestion_logger
 
 logger = ingestion_logger()
@@ -59,7 +60,7 @@ class SalesforceSourceStatus(SourceStatus):
         self, table_name: str, err: str, dataset_name: str = None, col_type: str = None
     ) -> None:
         self.filtered.append(table_name)
-        logger.warning("Dropped Table {} due to {}".format(table_name, err))
+        logger.warning("Filtered Table {} due to {}".format(table_name, err))
 
 
 class SalesforceSource(Source[OMetaDatabaseAndTable]):
@@ -89,81 +90,87 @@ class SalesforceSource(Source[OMetaDatabaseAndTable]):
     def column_type(self, column_type: str):
         if column_type in {"ID", "PHONE", "CURRENCY"}:
             return "INT"
-        if column_type in {
-            "REFERENCE",
-            "PICKLIST",
-            "TEXTAREA",
-            "ADDRESS",
-            "URL",
-            "EMAIL",
-        }:
-            return "VARCHAR"
-
-        return column_type
+        return "VARCHAR"
 
     def next_record(self) -> Iterable[OMetaDatabaseAndTable]:
         yield from self.salesforce_client()
 
     def salesforce_client(self) -> Iterable[OMetaDatabaseAndTable]:
         try:
-
-            row_order = 1
-            table_columns = []
-            md = self.sf.restful(
-                "sobjects/{}/describe/".format(self.service_connection.sobjectName),
-                params=None,
-            )
-
-            for column in md["fields"]:
-                col_constraint = None
-                if column["nillable"]:
-                    col_constraint = Constraint.NULL
-                elif not column["nillable"]:
-                    col_constraint = Constraint.NOT_NULL
-                if column["unique"]:
-                    col_constraint = Constraint.UNIQUE
-
-                table_columns.append(
-                    Column(
-                        name=column["name"],
-                        description=column["label"],
-                        dataType=self.column_type(column["type"].upper()),
-                        constraint=col_constraint,
-                        ordinalPosition=row_order,
-                        dataLength=column["length"],
-                    )
+            object_names = []
+            if self.service_connection.sobjectName:
+                object_names.append(self.service_connection.sobjectName)
+            else:
+                for obj in self.sf.describe()["sobjects"]:
+                    table_name = obj.get("name")
+                    if not table_name:
+                        continue
+                    if filter_by_table(
+                        self.config.sourceConfig.config.tableFilterPattern, table_name
+                    ):
+                        self.status.filter(
+                            "{}".format(table_name),
+                            "Table pattern not allowed",
+                        )
+                        continue
+                    object_names.append(table_name)
+            for object_name in object_names:
+                md = self.sf.restful(
+                    f"sobjects/{object_name}/describe/",
+                    params=None,
                 )
-                row_order += 1
-            logger.info("Successfully Ingested the sample data")
-            table_entity = Table(
-                id=uuid.uuid4(),
-                name=self.service_connection.sobjectName,
-                tableType="Regular",
-                description=" ",
-                columns=table_columns,
-            )
-            self.status.scanned(
-                f"{self.service_connection.scheme}.{self.service_connection.sobjectName}"
-            )
+                row_order = 1
+                table_columns = []
+                for column in md["fields"]:
+                    col_constraint = None
+                    if column["nillable"]:
+                        col_constraint = Constraint.NULL
+                    elif not column["nillable"]:
+                        col_constraint = Constraint.NOT_NULL
+                    if column["unique"]:
+                        col_constraint = Constraint.UNIQUE
 
-            database_entity = Database(
-                id=uuid.uuid4(),
-                name="default",
-                service=EntityReference(id=self.service.id, type="databaseService"),
-            )
-            schema_entity = DatabaseSchema(
-                id=uuid.uuid4(),
-                name=self.service_connection.scheme.name,
-                database=EntityReference(id=database_entity.id, type="databaseSchema"),
-                service=EntityReference(id=self.service.id, type="databaseService"),
-            )
-            table_and_db = OMetaDatabaseAndTable(
-                table=table_entity,
-                database=database_entity,
-                database_schema=schema_entity,
-            )
+                    table_columns.append(
+                        Column(
+                            name=column["name"],
+                            description=column["label"],
+                            dataType=self.column_type(column["type"].upper()),
+                            constraint=col_constraint,
+                            ordinalPosition=row_order,
+                            dataLength=column["length"],
+                        )
+                    )
+                    row_order += 1
+                logger.info("Successfully Ingested the sample data")
+                table_entity = Table(
+                    id=uuid.uuid4(),
+                    name=object_name,
+                    tableType="Regular",
+                    description=" ",
+                    columns=table_columns,
+                )
+                self.status.scanned(f"{self.service_connection.scheme}.{object_name}")
 
-            yield table_and_db
+                database_entity = Database(
+                    id=uuid.uuid4(),
+                    name="default",
+                    service=EntityReference(id=self.service.id, type="databaseService"),
+                )
+                schema_entity = DatabaseSchema(
+                    id=uuid.uuid4(),
+                    name=self.service_connection.scheme.name,
+                    database=EntityReference(
+                        id=database_entity.id, type="databaseSchema"
+                    ),
+                    service=EntityReference(id=self.service.id, type="databaseService"),
+                )
+                table_and_db = OMetaDatabaseAndTable(
+                    table=table_entity,
+                    database=database_entity,
+                    database_schema=schema_entity,
+                )
+
+                yield table_and_db
 
         except ValidationError as err:
             logger.error(err)
