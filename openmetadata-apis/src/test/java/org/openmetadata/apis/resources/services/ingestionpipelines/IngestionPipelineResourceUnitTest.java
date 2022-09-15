af112ccb03f20bcfa0f9d10687059a58f5fe6a73
Sriharsha Chintalapani <harsha@getcollate.io>
2022-09-14 18:45:11 -0700

    Fix #7469: Refactor OpenMetadata code modules

--- openmetadata-apis/src/test/java/org/openmetadata/apis/resources/services/ingestionpipelines/IngestionPipelineResourceUnitTest.java
+++ openmetadata-apis/src/test/java/org/openmetadata/apis/resources/services/ingestionpipelines/IngestionPipelineResourceUnitTest.java
@@ -130,7 +130,7 @@ public class IngestionPipelineResourceUnitTest {
 
   @ParameterizedTest
   @MethodSource(
-      "org.openmetadata.service.resources.services.ingestionpipelines.IngestionPipelineResourceUnitTestParams#params")
+      "org.openmetadata.apis.resources.services.ingestionpipelines.IngestionPipelineResourceUnitTestParams#params")
   void testGetIsEncryptedWhenSecretManagerIsConfigured(
       Object config,
       EntityReference service,
@@ -166,7 +166,7 @@ public class IngestionPipelineResourceUnitTest {
 
   @ParameterizedTest
   @MethodSource(
-      "org.openmetadata.service.resources.services.ingestionpipelines.IngestionPipelineResourceUnitTestParams#params")
+      "org.openmetadata.apis.resources.services.ingestionpipelines.IngestionPipelineResourceUnitTestParams#params")
   void testGetByNameIsEncryptedWhenSecretManagerIsConfigured(
       Object config,
       EntityReference service,
