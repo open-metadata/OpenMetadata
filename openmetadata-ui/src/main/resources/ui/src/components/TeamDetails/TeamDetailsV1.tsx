cfdc50e18f8aa3a340566f5f5fa7056f927ec270
Aniket Katkar <51777795+aniketkatkar97@users.noreply.github.com>
2022-09-26 19:43:34 +0530

    UI: Added cypress tests for soft deleted team checks. (#7718)
    * Added cypress tests for checking the option for permanent delete is available for soft deleted teams
    
    * Added checks in cypress tests for team type select while adding teams
    Added 'cancel deletion' scenario while team deletion

--- openmetadata-ui/src/main/resources/ui/src/components/TeamDetails/TeamDetailsV1.tsx
+++ openmetadata-ui/src/main/resources/ui/src/components/TeamDetails/TeamDetailsV1.tsx
@@ -972,6 +972,7 @@ const TeamDetailsV1 = ({
                       <Space align="center">
                         <Switch
                           checked={showDeletedTeam}
+                          data-testid="show-deleted-switch"
                           onChange={onShowDeletedTeamChange}
                         />
                         <span>Deleted Teams</span>
