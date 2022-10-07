fb341e747f067dae72c623abe40cfa32986dbfb7
Pere Miquel Brull <peremiquelbrull@gmail.com>
2022-10-07 18:44:23 +0200

    Pylint api

--- ingestion/src/metadata/ingestion/api/bulk_sink.py
+++ ingestion/src/metadata/ingestion/api/bulk_sink.py
@@ -8,7 +8,9 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-
+"""
+Abstract BulkSink definition to build a Workflow
+"""
 from abc import ABCMeta, abstractmethod
 from dataclasses import dataclass, field
 from typing import Any, List
