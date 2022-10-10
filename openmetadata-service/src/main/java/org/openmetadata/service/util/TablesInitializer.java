97b94c6ca7e1d8eb28b6c5526fe598d0d9c53474
Nahuel Verdugo Revigliono <nahuel@getcollate.io>
2022-10-10 09:14:44 +0200

    Remove UserRepository constructor which could cause NPE

--- openmetadata-service/src/main/java/org/openmetadata/service/util/TablesInitializer.java
+++ openmetadata-service/src/main/java/org/openmetadata/service/util/TablesInitializer.java
@@ -56,6 +56,8 @@ import org.openmetadata.service.fernet.Fernet;
 import org.openmetadata.service.jdbi3.CollectionDAO;
 import org.openmetadata.service.jdbi3.UserRepository;
 import org.openmetadata.service.jdbi3.locator.ConnectionAwareAnnotationSqlLocator;
+import org.openmetadata.service.secrets.SecretsManager;
+import org.openmetadata.service.secrets.SecretsManagerFactory;
 import org.openmetadata.service.security.jwt.JWTTokenGenerator;
 
 public final class TablesInitializer {
@@ -354,7 +356,7 @@ public final class TablesInitializer {
       user.setAuthenticationMechanism(authenticationMechanism);
     }
     try {
-      addOrUpdateUser(user, jdbi);
+      addOrUpdateUser(user, jdbi, config);
       if (jwtAuthMechanism != null) {
         printToConsoleMandatory(JsonUtils.pojoToJson(user));
       }
@@ -392,7 +394,9 @@ public final class TablesInitializer {
     }
     try {
       CollectionDAO daoObject = jdbi.onDemand(CollectionDAO.class);
-      UserRepository userRepository = new UserRepository(daoObject);
+      SecretsManager secretsManager =
+          SecretsManagerFactory.createSecretsManager(config.getSecretsManagerConfiguration(), config.getClusterName());
+      UserRepository userRepository = new UserRepository(daoObject, secretsManager);
       RestUtil.PutResponse<User> addedUser = userRepository.createOrUpdate(null, user);
       printToConsoleInDebug("Updated user entry: " + addedUser.getEntity());
       if (jwtAuthMechanism != null) {
@@ -403,9 +407,11 @@ public final class TablesInitializer {
     }
   }
 
-  private static void addOrUpdateUser(User user, Jdbi jdbi) throws Exception {
+  private static void addOrUpdateUser(User user, Jdbi jdbi, OpenMetadataApplicationConfig config) throws Exception {
     CollectionDAO daoObject = jdbi.onDemand(CollectionDAO.class);
-    UserRepository userRepository = new UserRepository(daoObject);
+    SecretsManager secretsManager =
+        SecretsManagerFactory.createSecretsManager(config.getSecretsManagerConfiguration(), config.getClusterName());
+    UserRepository userRepository = new UserRepository(daoObject, secretsManager);
     User addedUser = userRepository.create(null, user);
     printToConsoleInDebug("Added user entry: " + addedUser.getName());
   }
