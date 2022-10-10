4e51c70dcc862a16da3c020fd0d91a1ed3570584
Mayur Singal <39544459+ulixius9@users.noreply.github.com>
2022-10-10 10:16:24 +0530

    Messaging & MlModel Source Lint (#8008)
    * Messaging Source Lint
    
    * MlModel Source Lint

--- ingestion/src/metadata/ingestion/source/messaging/kafka.py
+++ ingestion/src/metadata/ingestion/source/messaging/kafka.py
@@ -8,7 +8,9 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-
+"""
+Kafka source ingestion
+"""
 from metadata.generated.schema.entity.services.connections.messaging.kafkaConnection import (
     KafkaConnection,
 )
--- ingestion/src/metadata/ingestion/source/messaging/redpanda.py
+++ ingestion/src/metadata/ingestion/source/messaging/redpanda.py
@@ -8,7 +8,9 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-
+"""
+RedPanda source ingestion
+"""
 from metadata.generated.schema.entity.services.connections.messaging.redpandaConnection import (
     RedpandaConnection,
 )
