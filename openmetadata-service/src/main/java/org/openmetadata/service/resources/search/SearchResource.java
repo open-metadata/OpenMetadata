f48598e1406dcdaf6ea68234b9aadebd02a8f418
Cristian Osiac <mosiac@bloomberg.net>
2022-10-13 20:40:24 +0100

    Change search aggreagation keys

--- openmetadata-service/src/main/java/org/openmetadata/service/resources/search/SearchResource.java
+++ openmetadata-service/src/main/java/org/openmetadata/service/resources/search/SearchResource.java
@@ -379,8 +379,9 @@ public class SearchResource {
     hb.field(highlightColumnDescriptions);
     hb.field(highlightColumnChildren);
     SearchSourceBuilder searchSourceBuilder = searchBuilder(queryStringBuilder, hb, from, size);
-    searchSourceBuilder.aggregation(AggregationBuilders.terms("Database").field("database.name.keyword"));
-    searchSourceBuilder.aggregation(AggregationBuilders.terms("DatabaseSchema").field("databaseSchema.name.keyword"));
+    searchSourceBuilder.aggregation(AggregationBuilders.terms("database.name.keyword").field("database.name.keyword"));
+    searchSourceBuilder.aggregation(
+        AggregationBuilders.terms("databaseSchema.name.keyword").field("databaseSchema.name.keyword"));
 
     return addAggregation(searchSourceBuilder);
   }
@@ -463,12 +464,13 @@ public class SearchResource {
 
   private SearchSourceBuilder addAggregation(SearchSourceBuilder builder) {
     builder
-        .aggregation(AggregationBuilders.terms("Service").field("serviceType").size(MAX_AGGREGATE_SIZE))
-        .aggregation(AggregationBuilders.terms("ServiceName").field("service.name.keyword").size(MAX_AGGREGATE_SIZE))
-        .aggregation(AggregationBuilders.terms("ServiceCategory").field("service.type").size(MAX_AGGREGATE_SIZE))
-        .aggregation(AggregationBuilders.terms("EntityType").field("entityType").size(MAX_AGGREGATE_SIZE))
-        .aggregation(AggregationBuilders.terms("Tier").field("tier.tagFQN"))
-        .aggregation(AggregationBuilders.terms("Tags").field("tags.tagFQN").size(MAX_AGGREGATE_SIZE));
+        .aggregation(AggregationBuilders.terms("serviceType").field("serviceType").size(MAX_AGGREGATE_SIZE))
+        .aggregation(
+            AggregationBuilders.terms("service.name.keyword").field("service.name.keyword").size(MAX_AGGREGATE_SIZE))
+        .aggregation(AggregationBuilders.terms("service.type").field("service.type").size(MAX_AGGREGATE_SIZE))
+        .aggregation(AggregationBuilders.terms("entityType").field("entityType").size(MAX_AGGREGATE_SIZE))
+        .aggregation(AggregationBuilders.terms("tier.tagFQN").field("tier.tagFQN"))
+        .aggregation(AggregationBuilders.terms("tags.tagFQN").field("tags.tagFQN").size(MAX_AGGREGATE_SIZE));
 
     return builder;
   }
