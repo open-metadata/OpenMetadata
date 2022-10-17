e08f25e1923ae1fea27e1223312ccb84e6af4be5
Aniket Katkar <aniketkatkar97@gmail.com>
2022-10-17 11:06:51 +0530

    Fixed failing tests and improved the coverage.

--- openmetadata-ui/src/main/resources/ui/src/pages/signup/index.test.tsx
+++ openmetadata-ui/src/main/resources/ui/src/pages/signup/index.test.tsx
@@ -16,10 +16,68 @@ import React, { ReactNode } from 'react';
 import Signup from '.';
 import * as AppState from '../../AppState';
 import { createUser } from '../../axiosAPIs/userAPI';
+import { getImages } from '../../utils/CommonUtils';
 
-const onChangeHandler = jest.fn();
-const onSubmitHandler = jest.fn();
+const mockChangeHandler = jest.fn();
+const mockSubmitHandler = jest.fn();
 const mockShowErrorToast = jest.fn();
+
+const mockCreateUser = {
+  data: {
+    id: '911d4be4-6ebf-48a0-9016-43a2cf716428',
+    name: 'test.user',
+    fullyQualifiedName: 'test.user',
+    displayName: 'Test User',
+    version: 0.1,
+    updatedAt: 1665145804919,
+    updatedBy: 'test.user',
+    email: 'test.user@test.com',
+    href: 'http://sandbox-beta.open-metadata.org/api/v1/users/911d4be4-6ebf-48a0-9016-43a2cf716428',
+    isAdmin: false,
+    profile: {
+      images: {
+        image:
+          'https://lh3.googleusercontent.com/a/ALm5wu0HwEPhAbyRha16cUHrEum-zxTDzj6KZiqYsT5Y=s96-c',
+        image24:
+          'https://lh3.googleusercontent.com/a/ALm5wu0HwEPhAbyRha16cUHrEum-zxTDzj6KZiqYsT5Y=s24-c',
+        image32:
+          'https://lh3.googleusercontent.com/a/ALm5wu0HwEPhAbyRha16cUHrEum-zxTDzj6KZiqYsT5Y=s32-c',
+        image48:
+          'https://lh3.googleusercontent.com/a/ALm5wu0HwEPhAbyRha16cUHrEum-zxTDzj6KZiqYsT5Y=s48-c',
+        image72:
+          'https://lh3.googleusercontent.com/a/ALm5wu0HwEPhAbyRha16cUHrEum-zxTDzj6KZiqYsT5Y=s72-c',
+        image192:
+          'https://lh3.googleusercontent.com/a/ALm5wu0HwEPhAbyRha16cUHrEum-zxTDzj6KZiqYsT5Y=s192-c',
+        image512:
+          'https://lh3.googleusercontent.com/a/ALm5wu0HwEPhAbyRha16cUHrEum-zxTDzj6KZiqYsT5Y=s512-c',
+      },
+    },
+    teams: [
+      {
+        id: '606c1d33-213b-4626-a619-0e4cef9f5069',
+        type: 'team',
+        name: 'Engineering',
+        fullyQualifiedName: 'Engineering',
+        deleted: false,
+        href: 'http://sandbox-beta.open-metadata.org/api/v1/teams/606c1d33-213b-4626-a619-0e4cef9f5069',
+      },
+    ],
+    deleted: false,
+    inheritedRoles: [
+      {
+        id: '4509b668-2882-45c3-90e1-4551043f8cbd',
+        type: 'role',
+        name: 'DataConsumer',
+        fullyQualifiedName: 'DataConsumer',
+        description:
+          'Users with Data Consumer role use different data assets for their day to day work.',
+        displayName: 'Data Consumer',
+        deleted: false,
+        href: 'http://sandbox-beta.open-metadata.org/api/v1/roles/4509b668-2882-45c3-90e1-4551043f8cbd',
+      },
+    ],
+  },
+};
 
 jest.mock('react-router-dom', () => ({
   useHistory: jest.fn().mockReturnValue({
@@ -54,14 +112,31 @@ jest.mock('../../components/containers/PageContainer', () => {
 });
 
 jest.mock('../../axiosAPIs/userAPI', () => ({
-  createUser: jest.fn().mockResolvedValue({}),
+  createUser: jest.fn().mockResolvedValue(mockCreateUser),
 }));
 
 jest.mock('../../utils/ToastUtils', () => ({
-  showErrorToast: mockShowErrorToast,
+  showErrorToast: jest.fn().mockImplementation(() => mockShowErrorToast),
+}));
+
+jest.mock('../../AppState', () => ({
+  ...jest.requireActual('../../AppState'),
+  updateUserDetails: jest.fn(),
+}));
+
+jest.mock('../../utils/CommonUtils', () => ({
+  getImages: jest
+    .fn()
+    .mockResolvedValue(
+      'https://lh3.googleusercontent.com/a/ALm5wu0HwEPhAbyRha16cUHrEum-zxTDzj6KZiqYsT5Y=s96-c'
+    ),
 }));
 
 describe('Signup page', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
   it('Component should render properly', async () => {
     AppState.default.newUser = {
       name: 'Sample Name',
@@ -103,11 +178,11 @@ describe('Signup page', () => {
     expect(loadingContent).toBeNull();
 
     await act(async () => {
-      form.onsubmit = onSubmitHandler;
+      form.onsubmit = mockSubmitHandler;
 
       fireEvent.submit(form);
 
-      expect(onSubmitHandler).toBeCalledTimes(1);
+      expect(mockSubmitHandler).toBeCalledTimes(1);
     });
   });
 
@@ -136,9 +211,9 @@ describe('Signup page', () => {
     expect(usernameInput).toHaveValue('sample123');
     expect(emailInput).toHaveValue('sample123@sample.com');
 
-    fullNameInput.onchange = onChangeHandler;
-    usernameInput.onchange = onChangeHandler;
-    emailInput.onchange = onChangeHandler;
+    fullNameInput.onchange = mockChangeHandler;
+    usernameInput.onchange = mockChangeHandler;
+    emailInput.onchange = mockChangeHandler;
 
     await act(async () => {
       fireEvent.change(fullNameInput, {
@@ -152,24 +227,80 @@ describe('Signup page', () => {
         target: { name: 'displayName', value: 'sample@sample.com' },
       });
 
-      expect(onChangeHandler).toBeCalledTimes(3);
+      expect(mockChangeHandler).toBeCalledTimes(3);
 
-      form.onsubmit = onSubmitHandler;
+      form.onsubmit = mockSubmitHandler;
 
       fireEvent.submit(form);
 
-      expect(onSubmitHandler).toBeCalledTimes(1);
+      expect(mockSubmitHandler).toBeCalledTimes(1);
     });
   });
 
   it('Error should be thrown if createUser API fails', async () => {
     AppState.default.newUser = {
+      name: 'Sample Name',
+      email: 'sample123@sample.com',
+      picture: 'Profile Picture',
+    };
+    AppState.default.updateUserPermissions = jest.fn();
+    AppState.default.updateUserDetails = jest.fn();
+
+    const { getByTestId } = render(<Signup />);
+
+    const form = getByTestId('create-user-form');
+    const fullNameInput = getByTestId('full-name-input');
+    const usernameInput = getByTestId('username-input');
+    const emailInput = getByTestId('email-input');
+
+    expect(form).toBeInTheDocument();
+    expect(fullNameInput).toHaveValue('Sample Name');
+    expect(usernameInput).toHaveValue('sample123');
+    expect(emailInput).toHaveValue('sample123@sample.com');
+
+    fullNameInput.onchange = mockChangeHandler;
+    usernameInput.onchange = mockChangeHandler;
+    emailInput.onchange = mockChangeHandler;
+
+    await act(async () => {
+      fireEvent.change(fullNameInput, {
+        target: { name: 'displayName', value: 'Fname Mname Lname' },
+      });
+
+      fireEvent.change(usernameInput, {
+        target: { name: 'displayName', value: 'mockUserName' },
+      });
+      fireEvent.change(emailInput, {
+        target: { name: 'displayName', value: '' },
+      });
+    });
+
+    expect(mockChangeHandler).toBeCalledTimes(3);
+
+    form.onsubmit = mockSubmitHandler;
+
+    await act(async () => {
+      (createUser as jest.Mock).mockImplementationOnce(() =>
+        Promise.reject({
+          response: { data: { message: 'error' } },
+        })
+      );
+      fireEvent.submit(form);
+    });
+
+    expect(createUser as jest.Mock).toHaveBeenCalledTimes(1);
+  });
+
+  it('Handlers in form should work if data is empty', async () => {
+    (getImages as jest.Mock).mockImplementationOnce(() => Promise.reject(''));
+
+    AppState.default.newUser = {
       name: '',
       email: '',
       picture: '',
     };
-
-    (createUser as jest.Mock).mockRejectedValueOnce('Test Error');
+    AppState.default.updateUserPermissions = jest.fn();
+    AppState.default.updateUserDetails = jest.fn();
 
     const { getByTestId } = render(<Signup />);
 
@@ -183,13 +314,18 @@ describe('Signup page', () => {
     expect(usernameInput).toHaveValue('');
     expect(emailInput).toHaveValue('');
 
-    form.onsubmit = onSubmitHandler;
+    fullNameInput.onchange = mockChangeHandler;
+    usernameInput.onchange = mockChangeHandler;
+    emailInput.onchange = mockChangeHandler;
+
+    expect(mockChangeHandler).not.toBeCalled();
+
+    form.onsubmit = mockSubmitHandler;
+
     await act(async () => {
       fireEvent.submit(form);
-
-      expect(onSubmitHandler).toBeCalledTimes(1);
     });
 
-    expect(mockShowErrorToast).toBeCalledTimes(1);
+    expect(createUser as jest.Mock).toHaveBeenCalledTimes(0);
   });
 });
