b27f121ebd94526c07d610781480cde02d72230e
Ayush Shah <ayush@getcollate.io>
2022-10-17 18:47:27 +0530

    Fix Pylint

--- ingestion/src/metadata/cmd.py
+++ ingestion/src/metadata/cmd.py
@@ -26,16 +26,43 @@ from metadata.cli.openmetadata_imports_migration import (
 )
 from metadata.cli.profile import run_profiler
 from metadata.cli.restore import run_restore
-from metadata.config.common import load_config_file
-from metadata.test_suite.api.workflow import TestSuiteWorkflow
 from metadata.utils.logger import cli_logger, set_loggers_level
-from metadata.utils.workflow_output_handler import WorkflowType, print_init_error
 
 logger = cli_logger()
 
+OM_IMPORTS_MIGRATION = """
+    Update DAG files generated after creating workflow in 0.11 and before.
+    In 0.12 the airflow managed API package name changed from `openmetadata` to `openmetadata_managed_apis`
+    hence breaking existing DAGs. The `dag_generated_config` folder also changed location in Docker.
+    This small CLI utility allows you to update both elements.
+    """
+
+BACKUP_HELP = """
+    Run a backup for the metadata DB. Uses a custom dump strategy for OpenMetadata tables.
+
+    We can pass as many connection options as required with `-o <opt1>, -o <opt2> [...]`
+    Same with connection arguments `-a <arg1>, -a <arg2> [...]`
+
+    To run the upload, provide the information as
+    `--upload endpoint bucket key` and properly configure the environment
+    variables AWS_ACCESS_KEY_ID & AWS_SECRET_ACCESS_KEY.
+
+    If `-s` or `--schema` is provided, we will trigger a Postgres backup instead
+    of a MySQL backup. This is the value of the schema containing the OpenMetadata
+    tables."""
+RESTORE_HELP = """
+    Run a restore for the metadata DB.
+
+    We can pass as many connection options as required with `-o <opt1>, -o <opt2> [...]`
+    Same with connection arguments `-a <arg1>, -a <arg2> [...]`
+
+    If `-s` or `--schema` is provided, we will trigger a Postgres Restore instead
+    of a MySQL restore. This is the value of the schema containing the OpenMetadata
+    tables.
+    """
+
 
 def create_common_config_parser_args(parser: argparse.ArgumentParser):
-    print(parser, type(parser))
     parser.add_argument(
         "-c",
         "--config",
@@ -62,6 +89,9 @@ def create_openmetadata_imports_migration_args(parser: argparse.ArgumentParser):
 
 
 def docker_args(parser: argparse.ArgumentParser):
+    """
+    Addtional Parser Arguments for Docker
+    """
     parser.add_argument(
         "--start", help="Start release docker containers", default=True, type=bool
     )
@@ -118,10 +148,129 @@ def docker_args(parser: argparse.ArgumentParser):
 
 
 def webhook_args(parser: argparse.ArgumentParser):
-    pass
+    """
+    Addtional Parser Arguments for Webhook
+    """
+    parser.add_argument(
+        "-H", "--host", help="Webserver Host", type=str, default="0.0.0.0"
+    )
+    parser.add_argument("-p", "--port", help="Webserver Port", type=int, default=8000)
+
+
+def backup_args(parser: argparse.ArgumentParser):
+    """
+    Addtional Parser Arguments for Backup
+    """
+    parser.add_argument(
+        "-H", "--host", help="Host that runs the database", required=True
+    )
+    parser.add_argument(
+        "-u",
+        "--user",
+        help="User to run the backup",
+        required=True,
+    )
+    parser.add_argument(
+        "-p",
+        "--password",
+        help="Credentials for the user",
+        required=True,
+    )
+    parser.add_argument(
+        "-d",
+        "--database",
+        help="Database to backup",
+        required=True,
+    )
+    parser.add_argument(
+        "--port",
+        help="Database service port",
+        default="3306",
+        required=False,
+    )
+    parser.add_argument(
+        "--output",
+        help="Local path to store the backup",
+        type=pathlib.Path,
+        default=None,
+    )
+    parser.add_argument(
+        "--upload",
+        help="S3 endpoint, bucket & key to upload the backup file",
+        nargs=3,
+        default=None,
+    )
+    parser.add_argument("-o", "--options", default=None, action="append")
+    parser.add_argument("-a", "--arguments", default=None, action="append")
+    parser.add_argument(
+        "-s",
+        "--schema",
+        default=None,
+        required=False,
+    )
+
+
+def restore_args(parser: argparse.ArgumentParser):
+    """
+    Addtional Parser Arguments for Restore
+    """
+    parser.add_argument(
+        "-H",
+        "--host",
+        help="Host that runs the database",
+        required=True,
+    )
+    parser.add_argument(
+        "-u",
+        "--user",
+        help="User to run the restore backup",
+        required=True,
+    )
+
+    parser.add_argument(
+        "-p",
+        "--password",
+        help="Credentials for the user",
+        required=True,
+    )
+
+    parser.add_argument(
+        "-d",
+        "--database",
+        help="Database to restore",
+        required=True,
+    )
+
+    parser.add_argument(
+        "--port",
+        help="Database service port",
+        default="3306",
+        required=False,
+    )
+
+    parser.add_argument(
+        "--input",
+        help="Local backup file path for restore",
+        type=pathlib.Path,
+        default=None,
+    )
+
+    parser.add_argument("-o", "--options", default=None, action="append")
+
+    parser.add_argument("-a", "--arguments", default=None, action="append")
+
+    parser.add_argument(
+        "-s",
+        "--schema",
+        default=None,
+        required=False,
+    )
 
 
 def add_metadata_args(parser: argparse.ArgumentParser):
+    """
+    Addtional Parser Arguments for Metadata
+    """
     parser.add_argument(
         "-v", "--version", action="version", version=get_metadata_version()
     )
@@ -134,40 +283,54 @@ def add_metadata_args(parser: argparse.ArgumentParser):
     )
 
 
-def metadata(args=None):
+def metadata():
+    """
+    This method implements parsing of the arguments passed from CLI
+    """
     parser = argparse.ArgumentParser(prog="metadata", description="Ingestion Framework")
     sub_parser = parser.add_subparsers(dest="command")
 
-    ingest_parser = sub_parser.add_parser(
-        "ingest", help="Ingestion Workflow", prog="ingest"
+    create_common_config_parser_args(
+        sub_parser.add_parser("ingest", help="Ingestion Workflow", prog="ingest")
     )
-    profile_parser = sub_parser.add_parser(
-        "profile", help="Workflow for profiling Table sources into Metadata"
+    create_common_config_parser_args(
+        sub_parser.add_parser(
+            "profile", help="Workflow for profiling Table sources into Metadata"
+        )
     )
-    test_parser = sub_parser.add_parser("test", help="Workflow for running test suites")
+    create_common_config_parser_args(
+        sub_parser.add_parser("test", help="Workflow for running test suites")
+    )
 
-    create_common_config_parser_args(ingest_parser)
-    create_common_config_parser_args(profile_parser)
-    create_common_config_parser_args(test_parser)
-
-    openmetadata_imports_migration_parser = sub_parser.add_parser(
-        "openmetadata_imports_migration", help="Data Quality Workflow"
+    create_openmetadata_imports_migration_args(
+        sub_parser.add_parser(
+            "openmetadata_imports_migration",
+            help=OM_IMPORTS_MIGRATION,
+        )
     )
-    create_openmetadata_imports_migration_args(openmetadata_imports_migration_parser)
-    docker_parser = sub_parser.add_parser("docker", help="Docker Quickstart")
-    docker_args(docker_parser)
-    webhook_parser = sub_parser.add_parser(
-        "webhook", help="Simple Webserver to test webhook metadata events"
+    docker_args(sub_parser.add_parser("docker", help="Docker Quickstart"))
+    backup_args(
+        sub_parser.add_parser(
+            "backup",
+            help=BACKUP_HELP,
+        )
     )
-    webhook_args(webhook_parser)
+    restore_args(
+        sub_parser.add_parser(
+            "restore",
+            help=RESTORE_HELP,
+        )
+    )
+    webhook_args(
+        sub_parser.add_parser(
+            "webhook", help="Simple Webserver to test webhook metadata events"
+        )
+    )
 
     add_metadata_args(parser)
-
     parser.add_argument("--debug", help="Debug Mode", action="store_true")
 
     contains_args = vars(parser.parse_args())
-    host = contains_args.get("host")
-    port = contains_args.get("port")
     metadata_workflow = contains_args.get("command")
     config_file = contains_args.get("config")
     if contains_args.get("debug"):
@@ -185,11 +348,11 @@ def metadata(args=None):
         run_test(config_path=config_file)
     elif metadata_workflow == "backup":
         run_backup(
-            host,
+            contains_args.get("host"),
             contains_args.get("user"),
             contains_args.get("password"),
             contains_args.get("database"),
-            port,
+            contains_args.get("port"),
             contains_args.get("output"),
             contains_args.get("upload_destination_type"),
             contains_args.get("upload"),
@@ -199,11 +362,11 @@ def metadata(args=None):
         )
     elif metadata_workflow == "restore":
         run_restore(
-            host,
+            contains_args.get("host"),
             contains_args.get("user"),
             contains_args.get("password"),
             contains_args.get("database"),
-            port,
+            contains_args.get("port"),
             contains_args.get("input"),
             contains_args.get("options"),
             contains_args.get("arguments"),
@@ -225,24 +388,26 @@ def metadata(args=None):
     elif metadata_workflow == "webhook":
 
         class WebhookHandler(BaseHTTPRequestHandler):
-            def do_GET(self):
+            def do_GET(self):  # pylint: disable=invalid-name
                 self.send_response(200)
                 self.send_header("Content-type", "text/html")
                 self.end_headers()
+                self.wfile.write(bytes("Hello, World! Here is a GET response", "utf8"))
 
-                message = "Hello, World! Here is a GET response"
-                self.wfile.write(bytes(message, "utf8"))
-
-            def do_POST(self):
+            def do_POST(self):  # pylint: disable=invalid-name
                 content_len = int(self.headers.get("Content-Length"))
-                post_body = self.rfile.read(content_len)
+                self.rfile.read(content_len)
                 self.send_response(200)
                 self.send_header("Content-type", "application/json")
                 self.end_headers()
-                logger.info(post_body)
+                logger.info(self.rfile.read(content_len))
 
-        logger.info(f"Starting server at {host}:{port}")
-        with HTTPServer((host, port), WebhookHandler) as server:
+        logger.info(
+            f"Starting server at {contains_args.get('host')}:{contains_args.get('port')}"
+        )
+        with HTTPServer(
+            (contains_args.get("host"), contains_args.get("port")), WebhookHandler
+        ) as server:
             server.serve_forever()
 
     elif metadata_workflow == "openmetadata_imports_migration":
