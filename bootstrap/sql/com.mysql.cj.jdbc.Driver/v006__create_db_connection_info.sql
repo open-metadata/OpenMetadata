abbd208aa37ee79910d6e4bce1846358e9236bc2
Teddy Crepineau <teddy.crepineau@gmail.com>
2022-10-18 00:10:07 +0200

    Added database migration for web analytic event type

--- bootstrap/sql/com.mysql.cj.jdbc.Driver/v006__create_db_connection_info.sql
+++ bootstrap/sql/com.mysql.cj.jdbc.Driver/v006__create_db_connection_info.sql
@@ -8,3 +8,16 @@ CREATE TABLE IF NOT EXISTS report_definition (
     deleted BOOLEAN GENERATED ALWAYS AS (json -> '$.deleted'),
     UNIQUE (name)
 );
+
+
+CREATE TABLE IF NOT EXISTS web_analytic_event (
+    id VARCHAR(36) GENERATED ALWAYS AS (json ->> '$.id') NOT NULL,
+    name VARCHAR(256) GENERATED ALWAYS AS (json ->> '$.name') NOT NULL,
+    fullyQualifiedName VARCHAR(256) GENERATED ALWAYS AS (json ->> '$.fullyQualifiedName') NOT NULL,
+    eventType VARCHAR(256) GENERATED ALWAYS AS (json ->> '$.eventType') NOT NULL,
+    json JSON NOT NULL,
+    updatedAt BIGINT UNSIGNED GENERATED ALWAYS AS (json ->> '$.updatedAt') NOT NULL,
+    updatedBy VARCHAR(256) GENERATED ALWAYS AS (json ->> '$.updatedBy') NOT NULL,
+    deleted BOOLEAN GENERATED ALWAYS AS (json -> '$.deleted'),
+    UNIQUE (name)
+);
