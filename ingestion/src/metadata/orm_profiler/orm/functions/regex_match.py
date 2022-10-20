8154841e3148683cc3f1f09c513cf80a0fe681a0
dai <Daniel53245@outlook.com>
2022-10-21 10:23:30 +1100

    Implemtn regex_match fro redshift and snowflake dialect

--- ingestion/src/metadata/orm_profiler/orm/functions/regex_match.py
+++ ingestion/src/metadata/orm_profiler/orm/functions/regex_match.py
@@ -1,9 +1,10 @@
 """
 Define match regular expression fucntion
-Given a value(usually a stirng) and a regex
+Given a value(usually a stirng or column neame) and a regex
 Return true if the value contains part that matches the regex
 """
 
+from shutil import copyfile
 from sqlalchemy.ext.compiler import compiles
 from sqlalchemy.sql.functions import ClauseElement
 
@@ -21,7 +22,8 @@ class MatchRegexFn(FunctionElement):
         
         
 """
-matching [regex] colum [namew]
+return true if value matches regex
+return error if the regex is invalid
 """
 @compiles(MatchRegexFn, Dialects.BigQuery)
 def _(element, compiler, **kw):
@@ -29,4 +31,18 @@ def _(element, compiler, **kw):
         compiler.process(element.value, **kw),
         compiler.process(element.regex, **kw)
         )
-    
+
+@compiles(MatchRegexFn,Dialects.Redshift)
+def _(element, compiler, **kw):
+    return "%s SIMILAR TO %s )"%(
+        compiler.process(element.value, **kw),
+        compiler.process(element.regex, **kw)
+    )
+
+
+@copyfile(MatchRegexFn,Dialects.Snowflake)
+def _(element, compiler, **kw):
+    return "%s REGEXP '%s'"%(
+        compiler.process(element.value, **kw),
+        compiler.process(element.regex, **kw)
+    )
