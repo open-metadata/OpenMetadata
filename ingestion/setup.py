75039a03358b5cce4f8b135a518ccb9fd2fce534
Akash-Jain <Akash.J@deuexsolutions.com>
2022-11-15 21:47:01 +0530

    prepare for 0.13.0-preview feature release

--- ingestion/setup.py
+++ ingestion/setup.py
@@ -182,7 +182,7 @@ test = {
 build_options = {"includes": ["_cffi_backend"]}
 setup(
     name="openmetadata-ingestion",
-    version="0.13.0.dev0",
+    version="0.13.0.0.dev0",
     url="https://open-metadata.org/",
     author="OpenMetadata Committers",
     license="Apache License 2.0",
