36e083e41f3cad3f53a250edc841bd3902db231a
Nihar Doshi <nihardoshi16@gmail.com>
2022-11-15 21:38:14 +0530

    Fix py-checkstyle

--- ingestion/src/metadata/ingestion/source/metadata/amundsen.py
+++ ingestion/src/metadata/ingestion/source/metadata/amundsen.py
@@ -67,7 +67,7 @@ from metadata.ingestion.ometa.ometa_api import OpenMetadata
 from metadata.ingestion.source.database.column_type_parser import ColumnTypeParser
 from metadata.utils import fqn
 from metadata.utils.amundsen_helper import SERVICE_TYPE_MAPPER
-from metadata.utils.connections import get_connection, test_connection
+from metadata.utils.connections import get_connection
 from metadata.utils.helpers import get_standard_chart_type
 from metadata.utils.logger import ingestion_logger
 from metadata.utils.sql_queries import (
