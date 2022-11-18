eb55a2a20e573ae352bea95a82f82bef2bed5916
Shailesh Parmar <shailesh.parmar.webdev@gmail.com>
2022-11-18 17:44:51 +0530

    Fixed Remove StartDate on Pipeline UI #8860 (#8862)
    * Fixed Remove StartDate on Pipeline UI #8860
    
    * skip flaky test
    
    * skipping flaky dashboard test

--- openmetadata-ui/src/main/resources/ui/src/components/PipelineDetails/PipelineDetails.component.tsx
+++ openmetadata-ui/src/main/resources/ui/src/components/PipelineDetails/PipelineDetails.component.tsx
@@ -82,7 +82,6 @@ import { getLineageViewPath } from '../../utils/RouterUtils';
 import SVGIcons from '../../utils/SvgUtils';
 import { getTagsWithoutTier, getTierTags } from '../../utils/TableUtils';
 import { fetchTagsAndGlossaryTerms } from '../../utils/TagsUtils';
-import { getDateTimeByTimeStamp } from '../../utils/TimeUtils';
 import { showErrorToast, showSuccessToast } from '../../utils/ToastUtils';
 import ActivityFeedList from '../ActivityFeed/ActivityFeedList/ActivityFeedList';
 import ActivityThreadPanel from '../ActivityFeed/ActivityThreadPanel/ActivityThreadPanel';
@@ -596,14 +595,6 @@ const PipelineDetails = ({
         dataIndex: 'taskType',
         width: 180,
         title: t('label.type'),
-      },
-      {
-        key: t('label.start-date'),
-        dataIndex: 'startDate',
-        width: 180,
-        title: t('label.start-date'),
-        render: (startDate: string) =>
-          getDateTimeByTimeStamp(new Date(startDate).valueOf()),
       },
       {
         key: t('label.description'),
