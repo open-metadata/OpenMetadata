b2c1e42f9bc9252b6215bdf8ea4bd0416b5d3a84
Sachin Chaurasiya <sachinchaurasiyachotey87@gmail.com>
2022-11-22 19:45:07 +0530

    Fix #8901 UI : Show bot user roles on bot detail page (#8922)
    * Fix #8901 UI : Show bot user roles on bot detail page
    
    * Fix edit roles changes
    
    * Address review comments
    
    * Code refactoring
    
    * Addressing comments
    
    * Revert user page change

---
+++ openmetadata-ui/src/main/resources/ui/src/components/common/InheritedRolesCard/InheritedRolesCard.interface.ts
@@ -0,0 +1,5 @@
+import { User } from '../../../generated/entity/teams/user';
+
+export interface InheritedRolesCardProps {
+  userData: User;
+}
---
+++ openmetadata-ui/src/main/resources/ui/src/components/common/RolesElement/RolesElement.interface.ts
@@ -0,0 +1,5 @@
+import { User } from '../../../generated/entity/teams/user';
+
+export interface RolesElementProps {
+  userData: User;
+}
