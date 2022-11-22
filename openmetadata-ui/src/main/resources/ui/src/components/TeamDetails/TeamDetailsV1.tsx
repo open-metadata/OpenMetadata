3540f8e0d0ba860361375cb2a4968dcf2214ee73
Shailesh Parmar <shailesh.parmar.webdev@gmail.com>
2022-11-22 17:22:33 +0530

    UI Fixed: Description alignment issue

--- openmetadata-ui/src/main/resources/ui/src/components/TeamDetails/TeamDetailsV1.tsx
+++ openmetadata-ui/src/main/resources/ui/src/components/TeamDetails/TeamDetailsV1.tsx
@@ -1059,9 +1059,7 @@ const TeamDetailsV1 = ({
               </>
             ))}
           </Space>
-          <div
-            className="tw-mb-3 tw--ml-5 tw-mt-2"
-            data-testid="description-container">
+          <div className="m-b-sm m-t-xs" data-testid="description-container">
             <Description
               description={currentTeam?.description || ''}
               entityName={currentTeam?.displayName ?? currentTeam?.name}
