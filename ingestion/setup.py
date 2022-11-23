ee843b1d784f6ca3a0ffa31fe424e9afad9b02b1
Ayush Shah <ayush@getcollate.io>
2022-11-23 19:12:26 +0530

    Version Bump Up for feature release

--- ingestion/setup.py
+++ ingestion/setup.py
@@ -192,7 +192,7 @@ test = {
 build_options = {"includes": ["_cffi_backend"]}
 setup(
     name="openmetadata-ingestion",
-    version="0.13.0.0.dev0",
+    version="0.13.0.0",
     url="https://open-metadata.org/",
     author="OpenMetadata Committers",
     license="Apache License 2.0",
