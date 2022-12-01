3cad7567d51521ac743dea197c4dac0fb05ef317
Akash-Jain <Akash.J@deuexsolutions.com>
2022-12-01 18:03:28 +0530

    Prepare for 0.13.0.3 Pypi Release

--- ingestion/setup.py
+++ ingestion/setup.py
@@ -192,7 +192,7 @@ test = {
 build_options = {"includes": ["_cffi_backend"]}
 setup(
     name="openmetadata-ingestion",
-    version="0.13.0.2",
+    version="0.13.0.3",
     url="https://open-metadata.org/",
     author="OpenMetadata Committers",
     license="Apache License 2.0",
