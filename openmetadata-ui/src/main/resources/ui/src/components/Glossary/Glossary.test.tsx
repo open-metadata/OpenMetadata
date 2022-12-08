46838542b5092cd611ee04f33834411fc7284a75
Shailesh Parmar <shailesh.parmar.webdev@gmail.com>
2022-12-08 16:59:28 +0530

    fixed failing test

--- openmetadata-ui/src/main/resources/ui/src/components/Glossary/Glossary.test.tsx
+++ openmetadata-ui/src/main/resources/ui/src/components/Glossary/Glossary.test.tsx
@@ -105,6 +105,9 @@ jest.mock('../Modals/EntityDeleteModal/EntityDeleteModal', () =>
 jest.mock('../common/ProfilePicture/ProfilePicture', () =>
   jest.fn().mockReturnValue(<span>U</span>)
 );
+jest.mock('../../utils/TimeUtils', () => ({
+  formatDateTime: jest.fn().mockReturnValue('Jan 15, 1970, 12:26 PM'),
+}));
 
 const mockProps = {
   assetData: mockedAssetData,
