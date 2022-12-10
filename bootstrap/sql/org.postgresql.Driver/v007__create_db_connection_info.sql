4a5cb870eca864b442741d47b25e293819e61a30
Teddy <teddy.crepineau@gmail.com>
2022-12-10 03:53:17 +0100

    Fixes #9192 - Update ES response for DI (#9221)
    * staging commit
    
    * updated terms to french
    
    * fix(dataInsight): updated ES response to get entityType and total non tiered entities
    
    * fix(dataInsight): removed fields=*
    
    * fix(dataInsight): Added entity migration
    
    * cleaned up branch

--- bootstrap/sql/org.postgresql.Driver/v007__create_db_connection_info.sql
+++ bootstrap/sql/org.postgresql.Driver/v007__create_db_connection_info.sql
@@ -1,3 +1,11 @@
 -- Remove markDeletedTablesFromFilterOnly 
 UPDATE ingestion_pipeline_entity
 SET json = json::jsonb #- '{sourceConfig,config,markDeletedTablesFromFilterOnly}';
+
+UPDATE data_insight_chart
+SET json = jsonb_set(
+        json,
+        '{dimensions}',
+        '[{"name":"entityFqn","chartDataType":"STRING"},{"name":"owner","chartDataType":"STRING"},{"name":"owner","entityType":"STRING"}]'
+)
+WHERE name = 'mostViewedEntities';
