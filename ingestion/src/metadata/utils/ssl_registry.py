a7279641afef09313458918ab724a8861f6aabec
Pere Miquel Brull <peremiquelbrull@gmail.com>
2022-12-11 19:05:02 +0100

    Separate SSL config from OpenMetadataConnection

--- ingestion/src/metadata/utils/ssl_registry.py
+++ ingestion/src/metadata/utils/ssl_registry.py
@@ -11,12 +11,12 @@
 """
 Register SSL verification results
 """
-# Complains about same imports
-# pylint: disable=duplicate-code
-from metadata.generated.schema.entity.services.connections.metadata.openMetadataConnection import (
-    OpenMetadataConnection,
-    VerifySSL,
+from typing import Callable, Optional
+
+from metadata.generated.schema.security.ssl.validateSSLClientConfig import (
+    ValidateSSLClientConfig,
 )
+from metadata.generated.schema.security.ssl.verifySSLConfig import VerifySSL
 from metadata.utils.dispatch import enum_register
 
 
@@ -31,15 +31,28 @@ ssl_verification_registry = enum_register()
 
 
 @ssl_verification_registry.add(VerifySSL.no_ssl.value)
-def no_ssl_init(_: OpenMetadataConnection) -> None:
+def no_ssl_init(_: Optional[ValidateSSLClientConfig]) -> None:
     return None
 
 
 @ssl_verification_registry.add(VerifySSL.ignore.value)
-def ignore_ssl_init(_: OpenMetadataConnection) -> bool:
+def ignore_ssl_init(_: Optional[ValidateSSLClientConfig]) -> bool:
     return False
 
 
 @ssl_verification_registry.add(VerifySSL.validate.value)
-def validate_ssl_init(config: OpenMetadataConnection) -> str:
-    return config.sslConfig.certificatePath
+def validate_ssl_init(ssl_config: Optional[ValidateSSLClientConfig]) -> str:
+    return ssl_config.certificatePath
+
+
+def get_verify_ssl_fn(verify_ssl: VerifySSL) -> Callable:
+    """
+    Pick up the right registered function
+    """
+    verify_ssl_fn = ssl_verification_registry.registry.get(verify_ssl.value)
+    if not verify_ssl_fn:
+        raise InvalidSSLVerificationException(
+            f"Cannot find {verify_ssl.value} in {ssl_verification_registry.registry}"
+        )
+
+    return verify_ssl_fn
