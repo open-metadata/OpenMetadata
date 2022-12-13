5dd4d3e94533d4a1a7410071a5df367b6a7902be
Chirag Madlani <12962843+chirag-madlani@users.noreply.github.com>
2022-12-13 13:07:24 +0530

    fix cypress for checkboxes

--- openmetadata-ui/src/main/resources/ui/src/components/common/FilterPattern/FilterPattern.tsx
+++ openmetadata-ui/src/main/resources/ui/src/components/common/FilterPattern/FilterPattern.tsx
@@ -50,10 +50,11 @@ const FilterPattern = ({
   return (
     <div className="m-t-md" data-testid="filter-pattern-container">
       <Row>
-        <Col data-testid={`${type}-filter-pattern-checkbox`}>
+        <Col>
           <Checkbox
             checked={checked}
             className="m-r-sm filter-pattern-checkbox"
+            data-testid={`${type}-filter-pattern-checkbox`}
             id={`${type}FilterPatternCheckbox`}
             name={`${type}FilterPatternCheckbox`}
             onChange={(e) => handleChecked(e.target.checked)}
