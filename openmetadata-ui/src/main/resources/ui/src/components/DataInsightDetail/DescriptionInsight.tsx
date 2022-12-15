33ff2a7397a8c6bfa9eec9f9d0b00c48852fd465
Shailesh Parmar <shailesh.parmar.webdev@gmail.com>
2022-12-15 16:41:28 +0530

    Added legend filter, converted stack bar to line graph and converted most view data to clickable link

--- openmetadata-ui/src/main/resources/ui/src/components/DataInsightDetail/DescriptionInsight.tsx
+++ openmetadata-ui/src/main/resources/ui/src/components/DataInsightDetail/DescriptionInsight.tsx
@@ -13,15 +13,14 @@
 
 import { Card, Typography } from 'antd';
 import { AxiosError } from 'axios';
-import { uniqueId } from 'lodash';
 import React, { FC, useEffect, useMemo, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import {
-  Bar,
-  BarChart,
   CartesianGrid,
   Legend,
   LegendProps,
+  Line,
+  LineChart,
   ResponsiveContainer,
   Tooltip,
   XAxis,
@@ -29,7 +28,6 @@ import {
 import { getAggregateChartData } from '../../axiosAPIs/DataInsightAPI';
 import {
   BAR_CHART_MARGIN,
-  BAR_SIZE,
   ENTITIES_BAR_COLO_MAP,
 } from '../../constants/DataInsight.constants';
 import { DataReportIndex } from '../../generated/dataInsight/dataInsightChart';
@@ -38,6 +36,7 @@ import {
   DataInsightChartType,
 } from '../../generated/dataInsight/dataInsightChartResult';
 import { ChartFilter } from '../../interface/data-insight.interface';
+import { updateActiveChartFilter } from '../../utils/ChartUtils';
 import {
   CustomTooltip,
   getGraphDataByEntityType,
@@ -56,6 +55,7 @@ const DescriptionInsight: FC<Props> = ({ chartFilter }) => {
     useState<DataInsightChartResult>();
 
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [activeKeys, setActiveKeys] = useState<string[]>([]);
 
   const { data, entities, total } = useMemo(() => {
     return getGraphDataByEntityType(
@@ -85,6 +85,12 @@ const DescriptionInsight: FC<Props> = ({ chartFilter }) => {
     }
   };
 
+  const handleLegendClick: LegendProps['onClick'] = (event) => {
+    setActiveKeys((prevActiveKeys) =>
+      updateActiveChartFilter(event.dataKey, prevActiveKeys)
+    );
+  };
+
   useEffect(() => {
     fetchTotalEntitiesDescriptionByType();
   }, [chartFilter]);
@@ -110,29 +116,30 @@ const DescriptionInsight: FC<Props> = ({ chartFilter }) => {
           debounce={1}
           id="description-summary-graph"
           minHeight={400}>
-          <BarChart data={data} margin={BAR_CHART_MARGIN}>
+          <LineChart data={data} margin={BAR_CHART_MARGIN}>
             <CartesianGrid strokeDasharray="3 3" />
             <XAxis dataKey="timestamp" />
             <Tooltip content={<CustomTooltip isPercentage />} />
             <Legend
               align="left"
               content={(props) =>
-                renderLegend(props as LegendProps, `${total}%`)
+                renderLegend(props as LegendProps, `${total}%`, activeKeys)
               }
               layout="vertical"
               verticalAlign="top"
-              wrapperStyle={{ left: '0px' }}
+              wrapperStyle={{ left: '0px', top: '0px' }}
+              onClick={handleLegendClick}
             />
             {entities.map((entity) => (
-              <Bar
-                barSize={BAR_SIZE}
+              <Line
                 dataKey={entity}
-                fill={ENTITIES_BAR_COLO_MAP[entity]}
-                key={uniqueId()}
-                stackId="description"
+                hide={activeKeys.length ? !activeKeys.includes(entity) : false}
+                key={entity}
+                stroke={ENTITIES_BAR_COLO_MAP[entity]}
+                type="monotone"
               />
             ))}
-          </BarChart>
+          </LineChart>
         </ResponsiveContainer>
       ) : (
         <EmptyGraphPlaceholder />
