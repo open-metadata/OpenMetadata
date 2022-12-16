4e783e7f9c499cb6c4e54359bfb855e29d7c1e4c
Sachin Chaurasiya <sachinchaurasiyachotey87@gmail.com>
2022-12-16 16:59:01 +0530

    UI : Clean up data insights summary and KPIs (#9335)

--- openmetadata-ui/src/main/resources/ui/src/components/DataInsightDetail/DataInsightSummary.test.tsx
+++ openmetadata-ui/src/main/resources/ui/src/components/DataInsightDetail/DataInsightSummary.test.tsx
@@ -13,15 +13,13 @@
 
 import { render, screen } from '@testing-library/react';
 import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
 import { act } from 'react-test-renderer';
 import { DataInsightChartType } from '../../generated/dataInsight/dataInsightChartResult';
+import { DataInsightTabs } from '../../interface/data-insight.interface';
 import DataInsightSummary from './DataInsightSummary';
 
-jest.mock('react-i18next', () => ({
-  useTranslation: jest.fn().mockReturnValue({
-    t: (label: string) => label,
-  }),
-}));
+let activeTab = DataInsightTabs.DATA_ASSETS;
 
 const mockFilter = {
   startTs: 1667952000000,
@@ -29,6 +27,15 @@ const mockFilter = {
 };
 
 const mockScrollFunction = jest.fn();
+
+jest.mock('react-router-dom', () => ({
+  Link: jest
+    .fn()
+    .mockImplementation(({ children, ...rest }) => <a {...rest}>{children}</a>),
+  useParams: jest.fn().mockImplementation(() => ({
+    tab: activeTab,
+  })),
+}));
 
 jest.mock('../../axiosAPIs/DataInsightAPI', () => ({
   getAggregateChartData: jest.fn().mockImplementation(() => Promise.resolve()),
@@ -41,7 +48,8 @@ describe('Test DataInsightSummary Component', () => {
         <DataInsightSummary
           chartFilter={mockFilter}
           onScrollToChart={mockScrollFunction}
-        />
+        />,
+        { wrapper: MemoryRouter }
       );
     });
 
@@ -53,5 +61,47 @@ describe('Test DataInsightSummary Component', () => {
 
     expect(summaryCard).toBeInTheDocument();
     expect(totalEntitiesByType).toBeInTheDocument();
+  });
+
+  it('Should render only the data assets summary', async () => {
+    await act(async () => {
+      render(
+        <DataInsightSummary
+          chartFilter={mockFilter}
+          onScrollToChart={mockScrollFunction}
+        />,
+        { wrapper: MemoryRouter }
+      );
+    });
+
+    const dataAssetSummary = await screen.findByTestId('data-assets-summary');
+
+    expect(dataAssetSummary).toBeInTheDocument();
+
+    // should not render the app analytics summary
+    expect(screen.queryByTestId('app-analytics-summary')).toBeNull();
+  });
+
+  it('Should render only the app analytics summary', async () => {
+    activeTab = DataInsightTabs.APP_ANALYTICS;
+
+    await act(async () => {
+      render(
+        <DataInsightSummary
+          chartFilter={mockFilter}
+          onScrollToChart={mockScrollFunction}
+        />,
+        { wrapper: MemoryRouter }
+      );
+    });
+
+    const appAnalyticsSummary = await screen.findByTestId(
+      'app-analytics-summary'
+    );
+
+    expect(appAnalyticsSummary).toBeInTheDocument();
+
+    // should not render the data assets summary
+    expect(screen.queryByTestId('data-assets-summary')).toBeNull();
   });
 });
