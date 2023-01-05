89cf05e5c2ceef8fd5c72922a2c6b0d4641cacbe
Mayur Singal <39544459+ulixius9@users.noreply.github.com>
2023-01-05 23:14:49 +0530

    Fix table comment not being ingested (#9627)
    * Fix table comment not being ingested
    
    * lint fix
    
    * Fixed DB2 comments
    
    Co-authored-by: Onkar Ravgan <onkar.10r@gmail.com>

--- ingestion/src/metadata/ingestion/source/database/common_db_source.py
+++ ingestion/src/metadata/ingestion/source/database/common_db_source.py
@@ -175,11 +175,7 @@ class CommonDbSourceService(
                 f"Table description error for table [{schema_name}.{table_name}]: {exc}"
             )
         else:
-            if hasattr(table_info, "text"):
-                description = table_info["text"]
-                # DB2 connector does not return a str type
-                if isinstance(description, list):
-                    description = description[0]
+            description = table_info.get("text")
         return description
 
     def get_tables_name_and_type(self) -> Optional[Iterable[Tuple[str, str]]]:
