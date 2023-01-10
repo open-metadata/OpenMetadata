6bfb70bc01c2eadb4f35d5ecbbb86942aece9809
Pere Miquel Brull <peremiquelbrull@gmail.com>
2023-01-10 11:23:55 +0100

    Update and organize versions

--- ingestion/setup.py
+++ ingestion/setup.py
@@ -22,6 +22,42 @@ def get_long_description():
     return description
 
 
+# Add here versions required for multiple plugins
+VERSIONS = {
+    "airflow": "apache-airflow==2.3.3",
+    "boto3": "boto3~=1.26",  # No need to add botocore separately. It's a dep from boto3
+    "pydomo": "pydomo~=0.3",
+    "google-cloud-storage": "google-cloud-storage==1.43.0",
+    "pyarrow": "pyarrow~=8.0",
+    "geoalchemy2": "GeoAlchemy2~=0.12",
+    "great-expectations": "great-expectations~=0.15.0",
+    "pyodbc": "pyodbc>=4.0.35,<5",
+    "msal": "msal~=1.2",
+    "pymysql": "pymysql>=1.0.2",
+    "scikit-learn": "scikit-learn~=1.2",
+    "pandas": "pandas==1.3.5",
+    "neo4j": "neo4j~=4.4.0",
+}
+
+COMMONS = {
+    "datalake": {VERSIONS["pandas"], VERSIONS["pyarrow"], VERSIONS["boto3"]},
+    "hive": {
+        "pyhive~=0.6",
+        "presto-types-parser>=0.0.2",
+    },
+    "kafka": {
+        # Due to https://github.com/grpc/grpc/issues/30843#issuecomment-1303816925
+        # we use v1.47.2 https://github.com/grpc/grpc/blob/v1.47.2/tools/distrib/python/grpcio_tools/grpc_version.py#L17
+        "grpcio-tools==1.47.2",  # grpcio-tools already depends on grpcio. No need to add separately
+        "confluent_kafka~=1.9",
+        "fastavro>=1.2.0",
+        "avro-python3~=1.10",
+        "avro~=1.11",
+        "protobuf",
+    },
+}
+
+
 base_requirements = {
     "commonregex",
     "idna<3,>=2.5",
@@ -34,7 +70,7 @@ base_requirements = {
     "python-dateutil>=2.8.1",
     "wheel~=0.38.4",
     "setuptools~=65.6.3",
-    "python-jose==3.3.0",
+    "python-jose~=3.3",
     "sqlalchemy>=1.4.0",
     "requests>=2.23",
     "cryptography",
@@ -43,161 +79,143 @@ base_requirements = {
     "jsonschema",
     "sqllineage==1.3.7",
     "antlr4-python3-runtime==4.9.2",
-    "boto3~=1.19.12",
-    "botocore==1.22.12",
-    "avro-python3==1.10.2",
-    # https://github.com/grpc/grpc/issues/30843#issuecomment-1303816925
-    "grpcio<1.48.1",
-    "grpcio-tools==1.47.2",
-    # compatibility requirements for 3.7
-    "typing-compat~=0.1.0",
+    "typing-compat~=0.1.0",  # compatibility requirements for 3.7
     "importlib-metadata~=4.12.0",  # From airflow constraints
     "croniter~=1.3.0",
-    "requests-aws4auth==1.1.2",
-    "pymysql>=1.0.2",
+    "requests-aws4auth~=1.1",  # Does only depends on requests as external package. Leaving as base.
+    VERSIONS["pymysql"],
     "cached-property==1.5.2",
-    "pandas==1.3.5",
+    VERSIONS["pandas"],  # to be removed from base
     "chardet==4.0.0",
 }
 
-
-datalake_common = {
-    "pandas==1.3.5",
-    "pyarrow==6.0.1",
-}
 
 plugins: Dict[str, Set[str]] = {
     "airflow": {
         "apache-airflow==2.3.3"
     },  # Same as ingestion container. For development.
-    "airflow-container-1.10.15": {"markupsafe==2.0.1 ", "requests==2.23.0"},
-    "amundsen": {"neo4j~=4.4.0"},
+    "amundsen": {VERSIONS["neo4j"]},
     "athena": {"PyAthena[SQLAlchemy]"},
     "atlas": {},
-    "azuresql": {"pyodbc"},
+    "azuresql": {VERSIONS["pyodbc"]},
     "bigquery": {
         "sqlalchemy-bigquery>=1.2.2",
-        "pyarrow~=6.0.1",
+        VERSIONS["pyarrow"],
         "google-cloud-datacatalog==3.6.2",
+        "google-cloud-logging",
+        "cachetools",
     },
-    "bigquery-usage": {"google-cloud-logging", "cachetools"},
     "docker": {"python_on_whales==0.55.0"},
-    "backup": {"boto3~=1.19.12", "azure-identity", "azure-storage-blob"},
-    "dagster": {"pymysql>=1.0.2", "psycopg2-binary", "GeoAlchemy2", "dagster_graphql"},
+    "backup": {VERSIONS["boto3"], "azure-identity", "azure-storage-blob"},
+    "dagster": {
+        VERSIONS["pymysql"],
+        "psycopg2-binary",
+        VERSIONS["geoalchemy2"],
+        "dagster_graphql~=1.1",
+    },
     "datalake-s3": {
+        # requires aiobotocore
+        # https://github.com/fsspec/s3fs/blob/9bf99f763edaf7026318e150c4bd3a8d18bb3a00/requirements.txt#L1
+        # however, the latest version of `s3fs` conflicts its `aiobotocore` dep with `boto3`'s dep on `botocore`.
+        # Leaving this marked to the automatic resolution to speed up installation.
         "s3fs==0.4.2",
-        "boto3~=1.19.12",
-        *datalake_common,
+        *COMMONS["datalake"],
     },
     "datalake-gcs": {
-        "google-cloud-storage==1.43.0",
-        "gcsfs==2022.5.0",
-        *datalake_common,
+        VERSIONS["google-cloud-storage"],
+        "gcsfs==2022.11.0",
+        *COMMONS["datalake"],
     },
-    "dbt": {"google-cloud", "boto3", "google-cloud-storage==1.43.0"},
-    "druid": {"pydruid>=0.6.2"},
-    "elasticsearch": {"elasticsearch==7.13.1", "requests-aws4auth==1.1.2"},
-    "glue": {"boto3~=1.19.12"},
-    "dynamodb": {"boto3~=1.19.12"},
-    "sagemaker": {"boto3~=1.19.12"},
+    "dbt": {"google-cloud", VERSIONS["boto3"], VERSIONS["google-cloud-storage"]},
+    "druid": {"pydruid>=0.6.5"},
+    "elasticsearch": {
+        "elasticsearch>=7.17,<8"
+    },  # also requires requests-aws4auth which is in base
+    "glue": {VERSIONS["boto3"]},
+    "dynamodb": {VERSIONS["boto3"]},
+    "sagemaker": {VERSIONS["boto3"]},
     "hive": {
-        "pyhive~=0.6.5",
-        "thrift~=0.13.0",
-        "sasl==0.3.1",
-        "thrift-sasl==0.4.3",
-        "presto-types-parser==0.0.2",
+        *COMMONS["hive"],
+        "thrift>=0.13,<1",
+        "sasl~=0.3",
+        "thrift-sasl~=0.4",
     },
-    "kafka": {
-        "confluent_kafka==1.8.2",
-        "fastavro>=1.2.0",
-        "avro-python3==1.10.2",
-        "avro==1.11.1",
-        "grpcio-tools",
-        "protobuf",
-    },
-    "kinesis": {"boto3~=1.19.12"},
-    "redpanda": {
-        "confluent_kafka==1.8.2",
-        "fastavro>=1.2.0",
-        "avro-python3==1.10.2",
-        "avro==1.11.1",
-        "grpcio-tools",
-        "protobuf",
-    },
+    "kafka": {*COMMONS["kafka"]},
+    "kinesis": {VERSIONS["boto3"]},
+    "redpanda": {*COMMONS["kafka"]},
     "ldap-users": {"ldap3==2.9.1"},
     "looker": {"looker-sdk>=22.20.0"},
-    "mssql": {"sqlalchemy-pytds>=0.3"},
-    "pymssql": {"pymssql==2.2.5"},
-    "mssql-odbc": {"pyodbc"},
-    "mysql": {"pymysql>=1.0.2"},
-    "oracle": {"cx_Oracle", "oracledb==1.0.3"},
-    "powerbi": {"msal==1.17.0"},
-    "presto": {"pyhive~=0.6.3"},
-    "trino": {"trino[sqlalchemy]"},
-    "postgres": {"pymysql>=1.0.2", "psycopg2-binary", "GeoAlchemy2"},
-    "redash": {"redash-toolbelt==0.1.8"},
-    "redshift": {"sqlalchemy-redshift==0.8.9", "psycopg2-binary", "GeoAlchemy2"},
-    "redshift-usage": {
-        "sqlalchemy-redshift==0.8.9",
-        "psycopg2-binary",
-        "GeoAlchemy2",
+    "mssql": {"sqlalchemy-pytds~=0.3"},
+    "pymssql": {"pymssql~=2.2"},
+    "mssql-odbc": {VERSIONS["pyodbc"]},
+    "mysql": {
+        VERSIONS["pymysql"],
     },
-    "snowflake": {"snowflake-sqlalchemy~=1.4.3"},
-    "snowflake-usage": {"snowflake-sqlalchemy~=1.4.3"},
-    "superset": {},
-    "tableau": {"tableau-api-lib==0.1.50"},
+    "oracle": {"cx_Oracle>=8.3.0,<9", "oracledb~=1.2"},
+    "powerbi": {VERSIONS["msal"]},
+    "presto": {*COMMONS["hive"]},
+    "trino": {"trino[sqlalchemy]"},
+    "postgres": {VERSIONS["pymysql"], "psycopg2-binary", VERSIONS["geoalchemy2"]},
+    "redash": {"redash-toolbelt~=0.1"},
+    "redshift": {
+        "sqlalchemy-redshift~=0.8",
+        "psycopg2-binary",
+        VERSIONS["geoalchemy2"],
+    },
+    "snowflake": {"snowflake-sqlalchemy~=1.4"},
+    "superset": {},  # uses requests
+    "tableau": {"tableau-api-lib~=0.1"},
     "vertica": {"sqlalchemy-vertica[vertica-python]>=0.0.5"},
-    "webhook-server": {},
-    "salesforce": {"simple_salesforce~=1.11.4"},
-    "okta": {"okta~=2.3.0"},
-    "mlflow": {"mlflow-skinny~=1.26.1"},
-    "sklearn": {"scikit-learn==1.0.2"},
-    "db2": {"ibm-db-sa==0.3.8"},
-    "clickhouse": {"clickhouse-driver==0.2.5", "clickhouse-sqlalchemy==0.2.3"},
-    "databricks": {"sqlalchemy-databricks==0.1.0"},
-    "singlestore": {"pymysql>=1.0.2"},
-    "azure-sso": {"msal~=1.17.0"},
-    "deltalake": {"delta-spark~=2.2.0"},
-    "great-expectations": {"great-expectations~=0.15.0"},
-    "pinotdb": {"pinotdb~=0.3.11"},
+    "salesforce": {"simple_salesforce==1.11.4"},
+    "okta": {"okta~=2.3"},
+    "mlflow": {"mlflow-skinny~=1.30"},
+    "sklearn": {VERSIONS["scikit-learn"]},
+    "db2": {"ibm-db-sa~=0.3"},
+    "clickhouse": {"clickhouse-driver~=0.2", "clickhouse-sqlalchemy~=0.2"},
+    "databricks": {"sqlalchemy-databricks~=0.2"},
+    "singlestore": {VERSIONS["pymysql"]},
+    "azure-sso": {VERSIONS["msal"]},
+    "deltalake": {"delta-spark~=2.2"},
+    "great-expectations": {VERSIONS["great-expectations"]},
+    "pinotdb": {"pinotdb~=0.3"},
     "nifi": {},
-    "domo": {"pydomo~=0.3.0.5"},
+    "domo": {VERSIONS["pydomo"]},
     "datalake-azure": {
-        "azure-storage-blob~=12.14.1",
-        "azure-identity~=1.12.0",
-        "adlfs==2022.2.0",
-        *datalake_common,
+        "azure-storage-blob~=12.14",
+        "azure-identity~=1.12",
+        "adlfs~=2022.11",
+        *COMMONS["datalake"],
     },
 }
 dev = {
     "datamodel-code-generator==0.15.0",
     "black==22.3.0",
-    "pycln==1.3.2",
+    "pycln",
     "docker",
-    "google-cloud-storage==1.43.0",
     "twine",
-}
-test = {
-    "isort==5.10.1",
+    "isort",
     "pre-commit",
     "pylint",
+}
+test = {
     "pytest==7.0.0",
     "pytest-cov",
     "pytest-order",
     "coverage",
     # sklearn integration
-    "scikit-learn==1.0.2",
-    "pandas==1.3.5",
+    VERSIONS["scikit-learn"],
+    VERSIONS["pandas"],
     # great_expectations tests
-    "great-expectations~=0.15.0",
+    VERSIONS["great-expectations"],
     # Airflow tests
-    "apache-airflow==2.3.3",
+    VERSIONS["airflow"],
     # Domo test
-    "pydomo~=0.3.0.5",
+    VERSIONS["pydomo"],
     # mock boto3 functions
     "moto==4.0.8",
     # amundsen
-    "neo4j~=4.4.0",
+    VERSIONS["neo4j"],
+    VERSIONS["google-cloud-storage"],
 }
 
 build_options = {"includes": ["_cffi_backend"]}
@@ -243,7 +261,6 @@ setup(
                     if plugin
                     not in {
                         "airflow",
-                        "airflow-container-1.10.15",
                         "db2",
                         "great-expectations",
                     }
