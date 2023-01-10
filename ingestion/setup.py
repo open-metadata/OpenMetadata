793f76a52feb2cd70902ed2f00824888e39f2ba7
Pere Miquel Brull <peremiquelbrull@gmail.com>
2023-01-10 11:50:27 +0100

    Fix order

--- ingestion/setup.py
+++ ingestion/setup.py
@@ -26,67 +26,67 @@ def get_long_description():
 VERSIONS = {
     "airflow": "apache-airflow==2.3.3",
     "boto3": "boto3~=1.26",  # No need to add botocore separately. It's a dep from boto3
-    "pydomo": "pydomo~=0.3",
-    "google-cloud-storage": "google-cloud-storage==1.43.0",
-    "pyarrow": "pyarrow~=8.0",
     "geoalchemy2": "GeoAlchemy2~=0.12",
     "great-expectations": "great-expectations~=0.15.0",
-    "pyodbc": "pyodbc>=4.0.35,<5",
+    "google-cloud-storage": "google-cloud-storage==1.43.0",
     "msal": "msal~=1.2",
-    "pymysql": "pymysql>=1.0.2",
-    "scikit-learn": "scikit-learn~=1.2",
-    "pandas": "pandas==1.3.5",
     "neo4j": "neo4j~=4.4.0",
+    "pandas": "pandas==1.3.5",
+    "pyarrow": "pyarrow~=8.0",
+    "pydomo": "pydomo~=0.3",
+    "pymysql": "pymysql>=1.0.2",
+    "pyodbc": "pyodbc>=4.0.35,<5",
+    "scikit-learn": "scikit-learn~=1.0",  # Python 3.7 only goes up to 1.0.2
 }
 
 COMMONS = {
-    "datalake": {VERSIONS["pandas"], VERSIONS["pyarrow"], VERSIONS["boto3"]},
+    "datalake": {VERSIONS["boto3"], VERSIONS["pandas"], VERSIONS["pyarrow"]},
     "hive": {
-        "pyhive~=0.6",
         "presto-types-parser>=0.0.2",
+        "pyhive~=0.6",
     },
     "kafka": {
+        "avro~=1.11",
+        "avro-python3~=1.10",
+        "confluent_kafka~=1.9",
+        "fastavro>=1.2.0",
         # Due to https://github.com/grpc/grpc/issues/30843#issuecomment-1303816925
         # we use v1.47.2 https://github.com/grpc/grpc/blob/v1.47.2/tools/distrib/python/grpcio_tools/grpc_version.py#L17
         "grpcio-tools==1.47.2",  # grpcio-tools already depends on grpcio. No need to add separately
-        "confluent_kafka~=1.9",
-        "fastavro>=1.2.0",
-        "avro-python3~=1.10",
-        "avro~=1.11",
         "protobuf",
     },
 }
 
 
 base_requirements = {
+    "antlr4-python3-runtime==4.9.2",
+    "cached-property==1.5.2",
+    "chardet==4.0.0",
+    "croniter~=1.3.0",
+    "cryptography",
     "commonregex",
-    "idna<3,>=2.5",
-    "mypy_extensions>=0.4.3",
-    "typing-inspect",
-    "pydantic~=1.9.0",
     "email-validator>=1.0.3",
     "google>=3.0.0",
     "google-auth>=1.33.0",
-    "python-dateutil>=2.8.1",
-    "wheel~=0.38.4",
-    "setuptools~=65.6.3",
-    "python-jose~=3.3",
-    "sqlalchemy>=1.4.0",
-    "requests>=2.23",
-    "cryptography",
-    "Jinja2>=2.11.3",
-    "PyYAML",
-    "jsonschema",
-    "sqllineage==1.3.7",
-    "antlr4-python3-runtime==4.9.2",
-    "typing-compat~=0.1.0",  # compatibility requirements for 3.7
+    "idna<3,>=2.5",
     "importlib-metadata~=4.12.0",  # From airflow constraints
-    "croniter~=1.3.0",
-    "requests-aws4auth~=1.1",  # Does only depends on requests as external package. Leaving as base.
-    VERSIONS["pymysql"],
-    "cached-property==1.5.2",
+    "Jinja2>=2.11.3",
+    "jsonschema",
+    "mypy_extensions>=0.4.3",
     VERSIONS["pandas"],  # to be removed from base
-    "chardet==4.0.0",
+    "pydantic~=1.9.0",
+    VERSIONS["pymysql"],
+    "python-dateutil>=2.8.1",
+    "python-jose~=3.3",
+    "PyYAML",
+    "requests>=2.23",
+    "requests-aws4auth~=1.1",  # Does only depends on requests as external package. Leaving as base.
+    "setuptools~=65.6.3",
+    "sqlalchemy>=1.4.0",
+    "sqllineage==1.3.7",
+    "typing-compat~=0.1.0",  # compatibility requirements for 3.7
+    "typing-inspect",
+    "wheel~=0.38.4",
 }
 
 
@@ -98,20 +98,35 @@ plugins: Dict[str, Set[str]] = {
     "athena": {"PyAthena[SQLAlchemy]"},
     "atlas": {},
     "azuresql": {VERSIONS["pyodbc"]},
+    "azure-sso": {VERSIONS["msal"]},
+    "backup": {VERSIONS["boto3"], "azure-identity", "azure-storage-blob"},
     "bigquery": {
-        "sqlalchemy-bigquery>=1.2.2",
-        VERSIONS["pyarrow"],
+        "cachetools",
         "google-cloud-datacatalog==3.6.2",
         "google-cloud-logging",
-        "cachetools",
+        VERSIONS["pyarrow"],
+        "sqlalchemy-bigquery>=1.2.2",
     },
-    "docker": {"python_on_whales==0.55.0"},
-    "backup": {VERSIONS["boto3"], "azure-identity", "azure-storage-blob"},
+    "clickhouse": {"clickhouse-driver~=0.2", "clickhouse-sqlalchemy~=0.2"},
     "dagster": {
         VERSIONS["pymysql"],
         "psycopg2-binary",
         VERSIONS["geoalchemy2"],
         "dagster_graphql~=1.1",
+    },
+    "dbt": {"google-cloud", VERSIONS["boto3"], VERSIONS["google-cloud-storage"]},
+    "db2": {"ibm-db-sa~=0.3"},
+    "databricks": {"sqlalchemy-databricks~=0.2"},
+    "datalake-azure": {
+        "azure-storage-blob~=12.14",
+        "azure-identity~=1.12",
+        "adlfs~=2022.11",
+        *COMMONS["datalake"],
+    },
+    "datalake-gcs": {
+        VERSIONS["google-cloud-storage"],
+        "gcsfs==2022.11.0",
+        *COMMONS["datalake"],
     },
     "datalake-s3": {
         # requires aiobotocore
@@ -121,19 +136,16 @@ plugins: Dict[str, Set[str]] = {
         "s3fs==0.4.2",
         *COMMONS["datalake"],
     },
-    "datalake-gcs": {
-        VERSIONS["google-cloud-storage"],
-        "gcsfs==2022.11.0",
-        *COMMONS["datalake"],
-    },
-    "dbt": {"google-cloud", VERSIONS["boto3"], VERSIONS["google-cloud-storage"]},
+    "deltalake": {"delta-spark~=2.2"},
+    "docker": {"python_on_whales==0.55.0"},
+    "domo": {VERSIONS["pydomo"]},
     "druid": {"pydruid>=0.6.5"},
+    "dynamodb": {VERSIONS["boto3"]},
     "elasticsearch": {
         "elasticsearch>=7.17,<8"
     },  # also requires requests-aws4auth which is in base
     "glue": {VERSIONS["boto3"]},
-    "dynamodb": {VERSIONS["boto3"]},
-    "sagemaker": {VERSIONS["boto3"]},
+    "great-expectations": {VERSIONS["great-expectations"]},
     "hive": {
         *COMMONS["hive"],
         "thrift>=0.13,<1",
@@ -142,80 +154,62 @@ plugins: Dict[str, Set[str]] = {
     },
     "kafka": {*COMMONS["kafka"]},
     "kinesis": {VERSIONS["boto3"]},
-    "redpanda": {*COMMONS["kafka"]},
     "ldap-users": {"ldap3==2.9.1"},
     "looker": {"looker-sdk>=22.20.0"},
+    "mlflow": {"mlflow-skinny~=1.30"},
     "mssql": {"sqlalchemy-pytds~=0.3"},
-    "pymssql": {"pymssql~=2.2"},
     "mssql-odbc": {VERSIONS["pyodbc"]},
-    "mysql": {
-        VERSIONS["pymysql"],
-    },
+    "mysql": {VERSIONS["pymysql"]},
+    "nifi": {},  # uses requests
+    "okta": {"okta~=2.3"},
     "oracle": {"cx_Oracle>=8.3.0,<9", "oracledb~=1.2"},
+    "pinotdb": {"pinotdb~=0.3"},
+    "postgres": {VERSIONS["pymysql"], "psycopg2-binary", VERSIONS["geoalchemy2"]},
     "powerbi": {VERSIONS["msal"]},
     "presto": {*COMMONS["hive"]},
-    "trino": {"trino[sqlalchemy]"},
-    "postgres": {VERSIONS["pymysql"], "psycopg2-binary", VERSIONS["geoalchemy2"]},
+    "pymssql": {"pymssql~=2.2"},
     "redash": {"redash-toolbelt~=0.1"},
+    "redpanda": {*COMMONS["kafka"]},
     "redshift": {
         "sqlalchemy-redshift~=0.8",
         "psycopg2-binary",
         VERSIONS["geoalchemy2"],
     },
+    "sagemaker": {VERSIONS["boto3"]},
+    "salesforce": {"simple_salesforce==1.11.4"},
+    "singlestore": {VERSIONS["pymysql"]},
+    "sklearn": {VERSIONS["scikit-learn"]},
     "snowflake": {"snowflake-sqlalchemy~=1.4"},
     "superset": {},  # uses requests
     "tableau": {"tableau-api-lib~=0.1"},
+    "trino": {"trino[sqlalchemy]"},
     "vertica": {"sqlalchemy-vertica[vertica-python]>=0.0.5"},
-    "salesforce": {"simple_salesforce==1.11.4"},
-    "okta": {"okta~=2.3"},
-    "mlflow": {"mlflow-skinny~=1.30"},
-    "sklearn": {VERSIONS["scikit-learn"]},
-    "db2": {"ibm-db-sa~=0.3"},
-    "clickhouse": {"clickhouse-driver~=0.2", "clickhouse-sqlalchemy~=0.2"},
-    "databricks": {"sqlalchemy-databricks~=0.2"},
-    "singlestore": {VERSIONS["pymysql"]},
-    "azure-sso": {VERSIONS["msal"]},
-    "deltalake": {"delta-spark~=2.2"},
-    "great-expectations": {VERSIONS["great-expectations"]},
-    "pinotdb": {"pinotdb~=0.3"},
-    "nifi": {},
-    "domo": {VERSIONS["pydomo"]},
-    "datalake-azure": {
-        "azure-storage-blob~=12.14",
-        "azure-identity~=1.12",
-        "adlfs~=2022.11",
-        *COMMONS["datalake"],
-    },
 }
+
 dev = {
-    "datamodel-code-generator==0.15.0",
     "black==22.3.0",
-    "pycln",
+    "datamodel-code-generator==0.15.0",
     "docker",
-    "twine",
     "isort",
     "pre-commit",
+    "pycln",
     "pylint",
+    "twine",
 }
+
 test = {
+    VERSIONS["airflow"],
+    "coverage",
+    VERSIONS["google-cloud-storage"],
+    VERSIONS["great-expectations"],
+    "moto==4.0.8",
+    VERSIONS["neo4j"],
+    VERSIONS["pandas"],
+    VERSIONS["pydomo"],
     "pytest==7.0.0",
     "pytest-cov",
     "pytest-order",
-    "coverage",
-    # sklearn integration
     VERSIONS["scikit-learn"],
-    VERSIONS["pandas"],
-    # great_expectations tests
-    VERSIONS["great-expectations"],
-    # Airflow tests
-    VERSIONS["airflow"],
-    # Domo test
-    VERSIONS["pydomo"],
-    # mock boto3 functions
-    "moto==4.0.8",
-    # amundsen
-    VERSIONS["neo4j"],
-    VERSIONS["google-cloud-storage"],
 }
 
 build_options = {"includes": ["_cffi_backend"]}
