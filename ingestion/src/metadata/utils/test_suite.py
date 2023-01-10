483690bf2155f4ceb16b86d0148ba6c610c25e82
Teddy Crepineau <teddy.crepineau@gmail.com>
2023-01-10 13:46:05 +0100

    feat (profiler): Added support for datetime type for sqa profiler

--- ingestion/src/metadata/utils/test_suite.py
+++ ingestion/src/metadata/utils/test_suite.py
@@ -15,11 +15,17 @@ Helper module for test suite functions
 
 from __future__ import annotations
 
+from typing import Callable, Optional
+
 from metadata.generated.schema.tests.testCase import TestCaseParameterValue
 
 
 def get_test_case_param_value(
-    test_case_param_vals: list[TestCaseParameterValue], name: str, type_, default=None
+    test_case_param_vals: list[TestCaseParameterValue],
+    name: str,
+    type_,
+    default=None,
+    pre_processor: Optional[Callable] = None,
 ):
     """Give a column and a type return the value with the appropriate type casting for the
     test case definition.
@@ -29,8 +35,17 @@ def get_test_case_param_value(
         type_ (Union[float, int, str]): type for the value
         name (str): column name
         default (_type_, optional): Default value to return if column is not found
+        pre_processor: pre processor function/type to use against the value before casting to type_
     """
-    return next(
-        (type_(param.value) for param in test_case_param_vals if param.name == name),
-        default,
+    value = next(
+        (param.value for param in test_case_param_vals if param.name == name), None
     )
+
+    if not value:
+        return default
+
+    if not pre_processor:
+        return type_(value)
+
+    pre_processed_value = pre_processor(value)
+    return type_(pre_processed_value)
