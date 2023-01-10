83817ddd0d62b907eb621da691950dbc170b9f8f
Pere Miquel Brull <peremiquelbrull@gmail.com>
2023-01-10 16:46:14 +0100

    Fixing kafka and pymssql versions due to build failure

--- ingestion/setup.py
+++ ingestion/setup.py
@@ -48,7 +48,7 @@ COMMONS = {
     "kafka": {
         "avro~=1.11",
         "avro-python3~=1.10",
-        "confluent_kafka~=1.9",
+        "confluent_kafka==1.8.2",
         "fastavro>=1.2.0",
         # Due to https://github.com/grpc/grpc/issues/30843#issuecomment-1303816925
         # we use v1.47.2 https://github.com/grpc/grpc/blob/v1.47.2/tools/distrib/python/grpcio_tools/grpc_version.py#L17
@@ -167,7 +167,7 @@ plugins: Dict[str, Set[str]] = {
     "postgres": {VERSIONS["pymysql"], "psycopg2-binary", VERSIONS["geoalchemy2"]},
     "powerbi": {VERSIONS["msal"]},
     "presto": {*COMMONS["hive"]},
-    "pymssql": {"pymssql~=2.2"},
+    "pymssql": {"pymssql==2.2.5"},
     "redash": {"redash-toolbelt~=0.1"},
     "redpanda": {*COMMONS["kafka"]},
     "redshift": {
