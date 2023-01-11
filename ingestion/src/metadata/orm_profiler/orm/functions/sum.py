0ad7ac35b7de189c155dc172b3127ffe901a3710
Teddy Crepineau <teddy.crepineau@gmail.com>
2023-01-11 11:13:11 +0100

    fix (profiler): oracle profiler datatype error

--- ingestion/src/metadata/orm_profiler/orm/functions/sum.py
+++ ingestion/src/metadata/orm_profiler/orm/functions/sum.py
@@ -58,3 +58,9 @@ def _(element, compiler, **kw):
     """These database types have all int types as alias for int64 so don't need a cast"""
     proc = compiler.process(element.clauses, **kw)
     return f"SUM({proc})"
+
+@compiles(SumFn, Dialects.Oracle)
+def _(element, compiler, **kw):
+    """Oracle casting"""
+    proc = compiler.process(element.clauses, **kw)
+    return f"SUM(CAST({proc} AS NUMBER))"
