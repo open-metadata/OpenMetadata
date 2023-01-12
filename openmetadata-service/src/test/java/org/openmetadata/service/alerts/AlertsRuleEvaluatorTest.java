c5e7dd0e6fb6173845d2487a36cfa938549410a2
mohitdeuex <mohit.y@deuexsolutions.com>
2023-01-12 23:05:09 +0530

    updated rule evealutor tests

--- openmetadata-service/src/test/java/org/openmetadata/service/alerts/AlertsRuleEvaluatorTest.java
+++ openmetadata-service/src/test/java/org/openmetadata/service/alerts/AlertsRuleEvaluatorTest.java
@@ -3,138 +3,165 @@ package org.openmetadata.service.alerts;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.openmetadata.service.security.policyevaluator.CompiledRule.parseExpression;
+import static org.openmetadata.service.util.TestUtils.ADMIN_AUTH_HEADERS;
 
-import java.util.ArrayList;
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.List;
 import java.util.UUID;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
+import org.junit.jupiter.api.TestInfo;
+import org.openmetadata.schema.api.data.CreateTable;
 import org.openmetadata.schema.entity.data.Table;
-import org.openmetadata.schema.entity.teams.Team;
-import org.openmetadata.schema.entity.teams.User;
+import org.openmetadata.schema.tests.type.TestCaseResult;
+import org.openmetadata.schema.tests.type.TestCaseStatus;
+import org.openmetadata.schema.type.ChangeDescription;
 import org.openmetadata.schema.type.ChangeEvent;
-import org.openmetadata.schema.type.EntityReference;
-import org.openmetadata.schema.type.TagLabel;
+import org.openmetadata.schema.type.Column;
+import org.openmetadata.schema.type.ColumnDataType;
+import org.openmetadata.schema.type.EventType;
+import org.openmetadata.schema.type.FieldChange;
 import org.openmetadata.service.Entity;
-import org.openmetadata.service.jdbi3.CollectionDAO.TeamDAO;
-import org.openmetadata.service.jdbi3.CollectionDAO.UserDAO;
-import org.openmetadata.service.jdbi3.TableRepository;
-import org.openmetadata.service.jdbi3.TeamRepository;
-import org.openmetadata.service.jdbi3.UserRepository;
-import org.openmetadata.service.security.policyevaluator.ResourceContext;
+import org.openmetadata.service.OpenMetadataApplicationTest;
+import org.openmetadata.service.resources.EntityResourceTest;
+import org.openmetadata.service.resources.databases.TableResourceTest;
 import org.springframework.expression.EvaluationContext;
+import org.springframework.expression.spel.support.StandardEvaluationContext;
 
-class AlertsRuleEvaluatorTest {
-  private static Table table;
-  private static User user;
-  private static EvaluationContext evaluationContext;
+class AlertsRuleEvaluatorTest extends OpenMetadataApplicationTest {
+  private static TableResourceTest tableResourceTest;
 
   @BeforeAll
-  public static void setup() {
-    Entity.registerEntity(User.class, Entity.USER, Mockito.mock(UserDAO.class), Mockito.mock(UserRepository.class));
-    Entity.registerEntity(Team.class, Entity.TEAM, Mockito.mock(TeamDAO.class), Mockito.mock(TeamRepository.class));
-    table = new Table().withName("table");
-    user = new User().withId(UUID.randomUUID()).withName("user");
-    ResourceContext resourceContext =
-        ResourceContext.builder()
-            .resource("table")
-            .entity(table)
-            .entityRepository(Mockito.mock(TableRepository.class))
-            .build();
-    // SubjectContext subjectContext = new SubjectContext(user);
-    // RuleEvaluator ruleEvaluator = new RuleEvaluator(null, subjectContext, resourceContext);
-    ChangeEvent event = new ChangeEvent();
-    // evaluationContext = new StandardEvaluationContext(ruleEvaluator);
+  public static void setup(TestInfo test) throws URISyntaxException, IOException {
+    tableResourceTest = new TableResourceTest();
+    tableResourceTest.setup(test);
   }
 
   @Test
-  void test_noOwner() {
-    // Set no owner to the entity and test noOwner method
-    table.setOwner(null);
-    assertTrue(evaluateExpression("noOwner()"));
-    assertFalse(evaluateExpression("!noOwner()"));
-
-    // Set owner to the entity and test noOwner method
-    table.setOwner(new EntityReference().withId(UUID.randomUUID()).withType(Entity.USER));
-    assertFalse(evaluateExpression("noOwner()"));
-    assertTrue(evaluateExpression("!noOwner()"));
+  void test_matchAnySource() {
+    // Create a change Event
+    ChangeEvent changeEvent = new ChangeEvent();
+    changeEvent.setEntityType("alert");
+    AlertsRuleEvaluator alertsRuleEvaluator = new AlertsRuleEvaluator(changeEvent);
+    EvaluationContext evaluationContext = new StandardEvaluationContext(alertsRuleEvaluator);
+    assertTrue(evaluateExpression("matchAnySource('alert')", evaluationContext));
+    assertFalse(evaluateExpression("matchAnySource('bot')", evaluationContext));
   }
 
   @Test
-  void test_isOwner() {
-    // Table owner is a different user (random ID) and hence isOwner returns false
-    table.setOwner(new EntityReference().withId(UUID.randomUUID()).withType(Entity.USER).withName("otherUser"));
-    assertFalse(evaluateExpression("isOwner()"));
-    assertTrue(evaluateExpression("!isOwner()"));
+  void test_matchAnyOwnerName(TestInfo test) throws IOException {
+    // Create Table
+    List<Column> columns = List.of(new Column().withName("c1").withDataType(ColumnDataType.INT));
+    CreateTable create =
+        tableResourceTest.createRequest(test).withColumns(columns).withOwner(EntityResourceTest.USER1_REF);
+    Table createdTable = tableResourceTest.createAndCheckEntity(create, ADMIN_AUTH_HEADERS);
+    // Create a change Event
+    ChangeEvent changeEvent = new ChangeEvent();
+    changeEvent.setEntityType(Entity.TABLE);
+    changeEvent.setEntity(createdTable);
 
-    // Table owner is same as the user in subjectContext and hence isOwner returns true
-    table.setOwner(new EntityReference().withId(user.getId()).withType(Entity.USER).withName(user.getName()));
-    assertTrue(evaluateExpression("isOwner()"));
-    assertFalse(evaluateExpression("!isOwner()"));
-
-    // noOwner() || isOwner() - with noOwner being true and isOwner false
-    table.setOwner(null);
-    assertTrue(evaluateExpression("noOwner() || isOwner()"));
-    assertFalse(evaluateExpression("!noOwner() && !isOwner()"));
-
-    // noOwner() || isOwner() - with noOwner is false and isOwner true
-    table.setOwner(new EntityReference().withId(user.getId()).withType(Entity.USER).withName(user.getName()));
-    assertTrue(evaluateExpression("noOwner() || isOwner()"));
-    assertFalse(evaluateExpression("!noOwner() && !isOwner()"));
+    AlertsRuleEvaluator alertsRuleEvaluator = new AlertsRuleEvaluator(changeEvent);
+    EvaluationContext evaluationContext = new StandardEvaluationContext(alertsRuleEvaluator);
+    assertTrue(
+        evaluateExpression("matchAnyOwnerName('" + EntityResourceTest.USER1.getName() + "')", evaluationContext));
+    assertFalse(evaluateExpression("matchAnyOwnerName('tempName')", evaluationContext));
   }
 
   @Test
-  void test_matchAllTags() {
-    table.withTags(getTags("tag1", "tag2", "tag3"));
+  void test_matchAnyEntityFqn(TestInfo test) throws IOException {
+    // Create Table
+    List<Column> columns = List.of(new Column().withName("c1").withDataType(ColumnDataType.INT));
+    CreateTable create = tableResourceTest.createRequest(test).withColumns(columns);
+    Table createdTable = tableResourceTest.createAndCheckEntity(create, ADMIN_AUTH_HEADERS);
+    // Create a change Event
+    ChangeEvent changeEvent = new ChangeEvent();
+    changeEvent.setEntityType(Entity.TABLE);
+    changeEvent.setEntity(createdTable);
 
-    // All tags present
-    assertTrue(evaluateExpression("matchAllTags('tag1', 'tag2', 'tag3')"));
-    assertFalse(evaluateExpression("!matchAllTags('tag1', 'tag2', 'tag3')"));
-    assertTrue(evaluateExpression("matchAllTags('tag1', 'tag2')"));
-    assertFalse(evaluateExpression("!matchAllTags('tag1', 'tag2')"));
-    assertTrue(evaluateExpression("matchAllTags('tag1')"));
-    assertFalse(evaluateExpression("!matchAllTags('tag1')"));
-
-    // Tag 'tag4' is missing
-    assertFalse(evaluateExpression("matchAllTags('tag1', 'tag2', 'tag3', 'tag4')"));
-    assertTrue(evaluateExpression("!matchAllTags('tag1', 'tag2', 'tag3', 'tag4')"));
-    assertFalse(evaluateExpression("matchAllTags('tag1', 'tag2', 'tag4')"));
-    assertTrue(evaluateExpression("!matchAllTags('tag1', 'tag2', 'tag4')"));
-    assertFalse(evaluateExpression("matchAllTags('tag2', 'tag4')"));
-    assertTrue(evaluateExpression("!matchAllTags('tag2', 'tag4')"));
-    assertFalse(evaluateExpression("matchAllTags('tag4')"));
-    assertTrue(evaluateExpression("!matchAllTags('tag4')"));
+    // Check AlertRuleEvaluator
+    AlertsRuleEvaluator alertsRuleEvaluator = new AlertsRuleEvaluator(changeEvent);
+    EvaluationContext evaluationContext = new StandardEvaluationContext(alertsRuleEvaluator);
+    String fqn = createdTable.getFullyQualifiedName();
+    assertTrue(evaluateExpression("matchAnyEntityFqn('" + fqn + "')", evaluationContext));
+    assertFalse(evaluateExpression("matchAnyEntityFqn('testFQN1')", evaluationContext));
   }
 
   @Test
-  void test_matchAnyTag() {
-    table.withTags(getTags("tag1", "tag2", "tag3"));
+  void test_matchAnyEntityId(TestInfo test) throws IOException {
+    // Create Table
+    List<Column> columns = List.of(new Column().withName("c1").withDataType(ColumnDataType.INT));
+    CreateTable create = tableResourceTest.createRequest(test).withColumns(columns);
+    Table createdTable = tableResourceTest.createAndCheckEntity(create, ADMIN_AUTH_HEADERS);
+    // Create a change Event
+    ChangeEvent changeEvent = new ChangeEvent();
+    changeEvent.setEntityType(Entity.TABLE);
+    changeEvent.setEntity(createdTable);
 
-    // Tag is present
-    assertTrue(evaluateExpression("matchAnyTag('tag1', 'tag2', 'tag3', 'tag4')"));
-    assertFalse(evaluateExpression("!matchAnyTag('tag1', 'tag2', 'tag3', 'tag4')"));
-    assertTrue(evaluateExpression("matchAnyTag('tag1', 'tag2', 'tag4')"));
-    assertFalse(evaluateExpression("!matchAnyTag('tag1', 'tag2', 'tag4')"));
-    assertTrue(evaluateExpression("matchAnyTag('tag1', 'tag2', 'tag4')"));
-    assertFalse(evaluateExpression("!matchAnyTag('tag1', 'tag2', 'tag4')"));
-    assertTrue(evaluateExpression("matchAnyTag('tag1', 'tag4')"));
-    assertFalse(evaluateExpression("!matchAnyTag('tag1', 'tag4')"));
-
-    // Tag `tag4` is not present
-    assertFalse(evaluateExpression("matchAnyTag('tag4')"));
-    assertTrue(evaluateExpression("!matchAnyTag('tag4')"));
+    // Check AlertRuleEvaluator
+    AlertsRuleEvaluator alertsRuleEvaluator = new AlertsRuleEvaluator(changeEvent);
+    EvaluationContext evaluationContext = new StandardEvaluationContext(alertsRuleEvaluator);
+    String id = createdTable.getId().toString();
+    assertTrue(evaluateExpression("matchAnyEntityId('" + id + "')", evaluationContext));
+    assertFalse(evaluateExpression("matchAnyEntityId('" + UUID.randomUUID() + "')", evaluationContext));
   }
 
-  private Boolean evaluateExpression(String condition) {
+  @Test
+  void test_matchAnyEventType() {
+    // Create a change Event
+    ChangeEvent changeEvent = new ChangeEvent();
+    changeEvent.setEventType(EventType.ENTITY_CREATED);
+    AlertsRuleEvaluator alertsRuleEvaluator = new AlertsRuleEvaluator(changeEvent);
+    EvaluationContext evaluationContext = new StandardEvaluationContext(alertsRuleEvaluator);
+    assertTrue(evaluateExpression("matchAnyEventType('entityCreated')", evaluationContext));
+    assertFalse(evaluateExpression("matchAnyEventType('entityUpdated')", evaluationContext));
+  }
+
+  @Test
+  void test_matchTestResult() {
+    // Create a change Event
+    ChangeDescription changeDescription = new ChangeDescription();
+    changeDescription.setFieldsUpdated(
+        List.of(
+            new FieldChange()
+                .withName("testCaseResult")
+                .withOldValue("test1")
+                .withNewValue(new TestCaseResult().withTestCaseStatus(TestCaseStatus.Success))));
+    ChangeEvent changeEvent = new ChangeEvent();
+    changeEvent.setEntityType(Entity.TEST_CASE);
+    changeEvent.setChangeDescription(changeDescription);
+    AlertsRuleEvaluator alertsRuleEvaluator = new AlertsRuleEvaluator(changeEvent);
+    EvaluationContext evaluationContext = new StandardEvaluationContext(alertsRuleEvaluator);
+    assertTrue(evaluateExpression("matchTestResult('Success')", evaluationContext));
+    assertFalse(evaluateExpression("matchTestResult('Failed')", evaluationContext));
+  }
+
+  @Test
+  void test_matchUpdatedBy() {
+    // Create a change Event
+    ChangeEvent changeEvent = new ChangeEvent();
+    changeEvent.setUserName("test");
+    AlertsRuleEvaluator alertsRuleEvaluator = new AlertsRuleEvaluator(changeEvent);
+    EvaluationContext evaluationContext = new StandardEvaluationContext(alertsRuleEvaluator);
+    assertTrue(evaluateExpression("matchUpdatedBy('test')", evaluationContext));
+    assertFalse(evaluateExpression("matchUpdatedBy('test1')", evaluationContext));
+  }
+
+  @Test
+  void test_matchAnyFieldChange() {
+    // Create a change Event
+    ChangeDescription changeDescription = new ChangeDescription();
+    changeDescription.setFieldsUpdated(
+        List.of(new FieldChange().withName("test").withOldValue("test1").withNewValue("test2")));
+    ChangeEvent changeEvent = new ChangeEvent();
+    changeEvent.setChangeDescription(changeDescription);
+    AlertsRuleEvaluator alertsRuleEvaluator = new AlertsRuleEvaluator(changeEvent);
+    EvaluationContext evaluationContext = new StandardEvaluationContext(alertsRuleEvaluator);
+    assertTrue(evaluateExpression("matchAnyFieldChange('test')", evaluationContext));
+    assertFalse(evaluateExpression("matchAnyFieldChange('temp')", evaluationContext));
+  }
+
+  private Boolean evaluateExpression(String condition, EvaluationContext evaluationContext) {
     return parseExpression(condition).getValue(evaluationContext, Boolean.class);
-  }
-
-  private List<TagLabel> getTags(String... tags) {
-    List<TagLabel> tagLabels = new ArrayList<>();
-    for (String tag : tags) {
-      tagLabels.add(new TagLabel().withTagFQN(tag));
-    }
-    return tagLabels;
   }
 }
