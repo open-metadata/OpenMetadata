a8885163e34acea43d2c597c00236acca2c74a91
Shailesh Parmar <shailesh.parmar.webdev@gmail.com>
2023-01-18 16:09:00 +0530

    UI: Glossary ui revamp (#9729)
    * UI: Revamp the glossary UI
    - Change the left panel as per new mock
    - updated folder structure of glossary page
    
    * added glossary folder icon
    
    * Added glossary terms tab
    
    * no data placehoder added
    
    * updated loading experiance and plus icon
    
    * fixed failing unit test
    
    * miner fix
    
    * fixed cypress for glossary
    
    * aded unit test for glossary left panel component
    
    * Added unit test for glossary term tab
    
    * added unit test for glossary term page
    
    * added unit test for glossaryDetails page
    
    * resolved conflict
    
    * fixed id issue for related term
    
    * prevented empty string to be appear in synonyms
    
    * addressing comments
    
    * fixing failing cypress

--- openmetadata-ui/src/main/resources/ui/src/rest/glossaryAPI.ts
+++ openmetadata-ui/src/main/resources/ui/src/rest/glossaryAPI.ts
@@ -14,7 +14,8 @@
 import { AxiosResponse } from 'axios';
 import { Operation } from 'fast-json-patch';
 import { CSVImportResult } from 'generated/type/csvImportResult';
-import { ModifiedGlossaryData } from 'pages/GlossaryPage/GlossaryPageV1.component';
+import { Include } from 'generated/type/include';
+import { PagingResponse } from 'Models';
 import { CreateGlossary } from '../generated/api/data/createGlossary';
 import { CreateGlossaryTerm } from '../generated/api/data/createGlossaryTerm';
 import { Glossary } from '../generated/entity/data/glossary';
@@ -22,17 +23,25 @@ import { GlossaryTerm } from '../generated/entity/data/glossaryTerm';
 import { getURLWithQueryFields } from '../utils/APIUtils';
 import APIClient from './index';
 
-export const getGlossaries = async (
-  paging = '',
-  limit = 10,
-  arrQueryFields: string | string[] = ''
-) => {
-  const qParams = `limit=${limit}`;
-  const url = getURLWithQueryFields(`/glossaries`, arrQueryFields, qParams);
+type Params = {
+  fields?: string;
+  limit?: number;
+  before?: string;
+  after?: string;
+  include?: Include;
+};
 
-  const response = await APIClient.get<{ data: ModifiedGlossaryData[] }>(
-    paging ? `${url}&${paging}` : url
-  );
+export type ListGlossaryTermsParams = Params & {
+  glossary?: string;
+  parent?: string;
+};
+
+const BASE_URL = '/glossaries';
+
+export const getGlossariesList = async (params?: Params) => {
+  const response = await APIClient.get<PagingResponse<Glossary[]>>(BASE_URL, {
+    params,
+  });
 
   return response.data;
 };
@@ -84,16 +93,15 @@ export const getGlossariesByName = async (
   return response.data;
 };
 
-export const getGlossaryTerms = (
-  glossaryId = '',
-  limit = 10,
-  arrQueryFields: string | string[] = ''
-): Promise<AxiosResponse<{ data: GlossaryTerm[] }>> => {
-  let qParams = `limit=${limit}`;
-  qParams += glossaryId ? `&glossary=${glossaryId}` : '';
-  const url = getURLWithQueryFields(`/glossaryTerms`, arrQueryFields, qParams);
+export const getGlossaryTerms = async (params: ListGlossaryTermsParams) => {
+  const response = await APIClient.get<PagingResponse<GlossaryTerm[]>>(
+    '/glossaryTerms',
+    {
+      params,
+    }
+  );
 
-  return APIClient.get(url);
+  return response.data;
 };
 
 export const getGlossaryTermsById = (
