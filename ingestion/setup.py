799eb0cf5442f4e3099994e884a103dba9c95e58
ulixius9 <mayursingal9@gmail.com>
2023-01-19 22:08:32 +0530

    Bump ingestion version

--- ingestion/setup.py
+++ ingestion/setup.py
@@ -201,7 +201,7 @@ test = {
 build_options = {"includes": ["_cffi_backend"]}
 setup(
     name="openmetadata-ingestion",
-    version="0.13.1.5",
+    version="0.13.1.6",
     url="https://open-metadata.org/",
     author="OpenMetadata Committers",
     license="Apache License 2.0",
