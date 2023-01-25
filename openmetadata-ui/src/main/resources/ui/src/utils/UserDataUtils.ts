a78e48130e419ac683ee8e7f4f5a709cbcbe35ab
Sachin Chaurasiya <sachinchaurasiyachotey87@gmail.com>
2023-01-25 19:38:28 +0530

    fix(ui) : owner dropdown for teams with search text is not working

--- openmetadata-ui/src/main/resources/ui/src/utils/UserDataUtils.ts
+++ openmetadata-ui/src/main/resources/ui/src/utils/UserDataUtils.ts
@@ -151,7 +151,9 @@ export const searchFormattedUsersAndTeams = (
   from = 1
 ): Promise<SearchedUsersAndTeams> => {
   return new Promise<SearchedUsersAndTeams>((resolve, reject) => {
-    const teamQuery = `${searchQuery} AND teamType:Group`;
+    const teamQuery = `${
+      searchQuery === WILD_CARD_CHAR ? searchQuery : `*${searchQuery}*`
+    } AND teamType:Group`;
     const promises = [
       getSearchedUsers(searchQuery, from),
       getSearchedTeams(teamQuery, from),
