1b9b94a4bbbda7175b2f2120328a63a4e50322f1
Pere Miquel Brull <peremiquelbrull@gmail.com>
2023-01-25 11:43:48 +0100

    Simplify db usage

--- ingestion/src/metadata/ingestion/source/database/vertica/lineage.py
+++ ingestion/src/metadata/ingestion/source/database/vertica/lineage.py
@@ -11,17 +11,8 @@
 """
 Vertica lineage module
 """
-import traceback
-from copy import deepcopy
-from typing import Iterator, Optional
-
-from metadata.generated.schema.type.tableQuery import TableQuery
 from metadata.ingestion.source.database.lineage_source import LineageSource
-from metadata.ingestion.source.database.vertica.connection import get_connection
-from metadata.ingestion.source.database.vertica.queries import (
-    VERTICA_LIST_DATABASES,
-    VERTICA_SQL_STATEMENT,
-)
+from metadata.ingestion.source.database.vertica.queries import VERTICA_SQL_STATEMENT
 from metadata.ingestion.source.database.vertica.query_parser import (
     VerticaQueryParserSource,
 )
@@ -31,17 +22,6 @@ logger = ingestion_logger()
 
 
 class VerticaLineageSource(VerticaQueryParserSource, LineageSource):
-    """
-    Vertica lineage parser source.
-
-    Vertica V_MONITOR schema changes from database to database.
-    To allow the lineage to happen for all the ingested databases
-    we'll need to iterate over them.
-
-    This is different from other sources such as Snowflake,
-    where the queries can be extracted in a single run
-    for all the databases.
-    """
 
     sql_stmt = VERTICA_SQL_STATEMENT
 
@@ -50,35 +30,3 @@ class VerticaLineageSource(VerticaQueryParserSource, LineageSource):
     database_field = "DBNAME()"
 
     schema_field = ""
-
-    def get_table_query(self) -> Optional[Iterator[TableQuery]]:
-        """
-        If queryLogFilePath available run the parent implementation.
-
-        If the database is informed, run the parent implementation.
-
-        Otherwise, iterate over the databases and prep the engine
-        with them.
-        """
-        if (
-            self.config.sourceConfig.config.queryLogFilePath
-            or self.service_connection.database
-        ):
-            yield from super().get_table_query()
-
-        else:
-            try:
-                results = get_connection(self.service_connection).execute(
-                    VERTICA_LIST_DATABASES
-                )
-                for res in results:
-                    row = list(res)
-                    new_service_connection = deepcopy(self.service_connection)
-                    new_service_connection.database = row[0]
-
-                    engine = get_connection(new_service_connection)
-                    yield from self.yield_table_query(engine)
-
-            except Exception as exc:
-                logger.debug(traceback.format_exc())
-                logger.error(f"Source usage processing error: {exc}")
--- ingestion/src/metadata/ingestion/source/database/vertica/query_parser.py
+++ ingestion/src/metadata/ingestion/source/database/vertica/query_parser.py
@@ -12,6 +12,7 @@
 Vertica usage module
 """
 from abc import ABC
+from typing import Iterable
 
 from metadata.generated.schema.entity.services.connections.database.verticaConnection import (
     VerticaConnection,
@@ -22,13 +23,23 @@ from metadata.generated.schema.entity.services.connections.metadata.openMetadata
 from metadata.generated.schema.metadataIngestion.workflow import (
     Source as WorkflowSource,
 )
+from metadata.generated.schema.type.tableQuery import TableQuery
 from metadata.ingestion.api.source import InvalidSourceException
+from metadata.ingestion.source.connections import get_connection
 from metadata.ingestion.source.database.query_parser_source import QueryParserSource
+from metadata.ingestion.source.database.vertica.queries import VERTICA_LIST_DATABASES
+from metadata.utils.logger import ingestion_logger
+
+logger = ingestion_logger()
 
 
 class VerticaQueryParserSource(QueryParserSource, ABC):
     """
-    MSSQL base for Usage and Lineage
+    Vertica lineage parser source.
+
+    Vertica V_MONITOR schema changes from database to database.
+    To allow the lineage to happen for all the ingested databases
+    we'll need to iterate over them.
     """
 
     filters: str
@@ -43,3 +54,16 @@ class VerticaQueryParserSource(QueryParserSource, ABC):
                 f"Expected VerticaConnection, but got {connection}"
             )
         return cls(config, metadata_config)
+
+    def get_table_query(self) -> Iterable[TableQuery]:
+        database = self.config.serviceConnection.__root__.config.database
+        if database:
+            yield from super().get_table_query()
+        else:
+            results = self.engine.execute(VERTICA_LIST_DATABASES)
+            for res in results:
+                row = list(res)
+                logger.info(f"Ingesting from database: {row[0]}")
+                self.config.serviceConnection.__root__.config.database = row[0]
+                self.engine = get_connection(self.service_connection)
+                yield from super().get_table_query()
