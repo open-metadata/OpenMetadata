4a5676d6173f8ac087f662685e357ed53c596c1c
Sriharsha Chintalapani <harshach@users.noreply.github.com>
2023-01-27 01:34:00 -0800

    Fix #9801: ES: Search api is not returning expected result (#9948)
    * Fix #9801: ES: Search api is not returning expected result
    
    * Fix #9801: ES: Search api is not returning expected result
    
    * Fix #9801: ES: Search api is not returning expected result

--- openmetadata-service/src/main/java/org/openmetadata/service/elasticsearch/UserIndex.java
+++ openmetadata-service/src/main/java/org/openmetadata/service/elasticsearch/UserIndex.java
@@ -9,21 +9,21 @@ import org.openmetadata.service.util.JsonUtils;
 
 public class UserIndex implements ElasticSearchIndex {
   final User user;
-  final List<String> excludeFields = List.of("owns", "changeDescription", "follows");
+  final List<String> excludeFields = List.of("owns", "changeDescription", "follows", "authenticationMechanism");
 
   public UserIndex(User user) {
     this.user = user;
   }
 
   public Map<String, Object> buildESDoc() {
+    if (user.getDisplayName() == null) {
+      user.setDisplayName(user.getName());
+    }
     Map<String, Object> doc = JsonUtils.getMap(user);
     ElasticSearchIndexUtils.removeNonIndexableFields(doc, excludeFields);
     List<ElasticSearchSuggest> suggest = new ArrayList<>();
     suggest.add(ElasticSearchSuggest.builder().input(user.getName()).weight(5).build());
     suggest.add(ElasticSearchSuggest.builder().input(user.getDisplayName()).weight(10).build());
-    if (user.getDisplayName() == null) {
-      doc.put("displayName", user.getName());
-    }
     doc.put("suggest", suggest);
     doc.put("entityType", Entity.USER);
     return doc;
