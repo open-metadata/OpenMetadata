f3d9fc1954a9014de7ce050e1abcccd0a9722d48
Sachin Chaurasiya <sachinchaurasiyachotey87@gmail.com>
2023-01-27 15:01:52 +0530

    fix: cy test

--- openmetadata-ui/src/main/resources/ui/src/rest/miscAPI.ts
+++ openmetadata-ui/src/main/resources/ui/src/rest/miscAPI.ts
@@ -15,6 +15,7 @@ import { AxiosResponse } from 'axios';
 import { Edge } from 'components/EntityLineage/EntityLineage.interface';
 import { ExploreSearchIndex } from 'components/Explore/explore.interface';
 import { WILD_CARD_CHAR } from 'constants/char.constants';
+import { startsWith } from 'lodash';
 import { SearchIndex } from '../enums/search.enum';
 import { AirflowConfiguration } from '../generated/configuration/airflowConfiguration';
 import { AuthenticationConfiguration } from '../generated/configuration/authenticationConfiguration';
@@ -51,7 +52,8 @@ export const searchData = <SI extends SearchIndex>(
     trackTotalHits
   );
 
-  const queryText = q === WILD_CARD_CHAR ? q : `*${q}*`;
+  const queryText =
+    q === WILD_CARD_CHAR || startsWith(`${q}`, '*') ? q : `*${q}*`;
 
   return APIClient.get<SearchResponse<SI>>(`/search/query?q=${queryText}`, {
     params,
