e7cc4ed20af3e539c6f873bbe0a7f266efa20e87
Sachin Chaurasiya <sachinchaurasiyachotey87@gmail.com>
2023-01-27 15:56:27 +0530

    doc : update what's new contents for 0.13.2

--- openmetadata-ui/src/main/resources/ui/src/components/Modals/WhatsNewModal/whatsNewData.ts
+++ openmetadata-ui/src/main/resources/ui/src/components/Modals/WhatsNewModal/whatsNewData.ts
@@ -13,9 +13,9 @@
 
 /* eslint-disable max-len */
 
-export const LATEST_VERSION_ID = 11;
+export const LATEST_VERSION_ID = 12;
 
-export const COOKIE_VERSION = 'VERSION_0_13_1'; // To be changed with each release.
+export const COOKIE_VERSION = 'VERSION_0_13_2'; // To be changed with each release.
 
 // for youtube video make isImage = false and path = {video embed id}
 // embed:- youtube video => share => click on embed and take {url with id} from it
@@ -632,6 +632,34 @@ export const WHATS_NEW = [
       'Advanced Search': `- **Advanced Search** improvements have been made. When a filter is applied, the details of the filter selected are displayed for clarity.`,
       'Explore Page UI': `- **Side Preview** on the **Explore** page UI is now available for all data assets. Previously it was only displayed for tables.`,
       'Airflow Lineage': `- **Airflow Lineage Operator** and the **OpenMetadata Hook** are now part of the ingestion package. Send Airflow metadata from your DAGs and safely store the OpenMetadata server connection directly in Airflow.`,
+    },
+  },
+  {
+    id: 12,
+    version: 'v0.13.2',
+    description: 'Released on 30 January 2023.',
+    features: [],
+    changeLogs: {
+      'Improved SQL Lineage': `- We have collaborated with the [sqllineage](https://github.com/reata/sqllineage) and [sqlfluff](https://www.sqlfluff.com/) communities
+    to improve the parsing capabilities of *sqllineage*. We'll continue to collaborate to ship further improvements in new releases.`,
+      'New Glossary UI': `- Moved from a tree view in the left panel to an easy to navigate list of the terms sorted alphabetically.
+- The term list shows the tags and descriptions in the cards.`,
+      'Glossary Import & Export': `- You can now export your Glossary data as a CSV file.
+- In the same way, you can now bulk upload terms to a Glossary by adding their details in a CSV file.
+- The import utility will validate the file and show you a preview of the elements that are going to be imported to OpenMetadata.`,
+      'Unified Tag Category API': `- Renamed Tag Categories to Classification, a more widely used term.
+- Updated the API to conform with the rest of the specification. More info [here](https://github.com/open-metadata/OpenMetadata/issues/9259).`,
+      'Mutually Exclusive Tags': `- When creating a Classification or a Glossary term, you can now make the tags to be mutually exclusive.
+- If tags are set to be mutually exclusive, you won't be able to set multiple tags from the same category in the same asset.`,
+      EntityName: `- Special characters`,
+      'Ingestion Framework': `- Performance Improvements: We are now getting descriptions in batch, making connectors such as Redshift or Snowflake way faster!
+- The Oracle connector now ships with the Thick mode enabled.
+- AWS QuickSight fixes
+- DB2 constraints and profiler improvements
+- Added support for Postgres Foreign Tables
+- Added support for Datalake profiler row-based sampling
+
+`,
     },
   },
 ];
