b01a107509d0eb6751bd54d0c20454d1b008c5d9
Chirag Madlani <12962843+chirag-madlani@users.noreply.github.com>
2023-01-31 00:47:39 +0530

    fix(ui): validate token expiry on tab focus (#10011)
    * fix(ui): validate token expiry on tab focus
    
    * show toast message if token is expired
    
    (cherry picked from commit d464a8a255fee2f051005148498c081cc2dbe4c8)

--- openmetadata-ui/src/main/resources/ui/src/components/app-bar/Appbar.tsx
+++ openmetadata-ui/src/main/resources/ui/src/components/app-bar/Appbar.tsx
@@ -19,7 +19,9 @@ import { observer } from 'mobx-react';
 import React, { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Link, useHistory, useLocation, useRouteMatch } from 'react-router-dom';
+import { toast } from 'react-toastify';
 import { getVersion } from 'rest/miscAPI';
+import { extractDetailsFromToken } from 'utils/AuthProvider.util';
 import appState from '../../AppState';
 import {
   getExplorePathWithSearch,
@@ -335,6 +337,28 @@ const Appbar: React.FC = (): JSX.Element => {
       }
     }
   }, [appState.userDetails, isAuthDisabled]);
+
+  useEffect(() => {
+    const handleDocumentVisibilityChange = () => {
+      if (
+        isProtectedRoute(location.pathname) &&
+        isTourRoute(location.pathname)
+      ) {
+        return;
+      }
+      const { isExpired, exp } = extractDetailsFromToken();
+      if (!document.hidden && isExpired) {
+        exp && toast.info(t('message.session-expired'));
+        onLogoutHandler();
+      }
+    };
+
+    addEventListener('focus', handleDocumentVisibilityChange);
+
+    return () => {
+      removeEventListener('focus', handleDocumentVisibilityChange);
+    };
+  }, []);
 
   return (
     <>
