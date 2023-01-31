43005461cbfe59c9e14b262d2e01cfc3a051af7d
Nahuel Verdugo Revigliono <nahuel@getcollate.io>
2023-01-31 21:47:01 +0100

    Bump to version 0.13.2

--- ingestion/setup.py
+++ ingestion/setup.py
@@ -230,7 +230,7 @@ test = {
 build_options = {"includes": ["_cffi_backend"]}
 setup(
     name="openmetadata-ingestion",
-    version="0.13.2.0.dev0",
+    version="0.13.2.1",
     url="https://open-metadata.org/",
     author="OpenMetadata Committers",
     license="Apache License 2.0",
