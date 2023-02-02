a3490093df5d1392b92a6329d34d6630007ca72e
Nahuel <nahuel@getcollate.io>
2023-02-02 17:10:35 +0100

    Generate PDF for Snyk security report (#10086)
    * Generate PDF for Snyk security report
    
    * Add missing doc
    
    * Minor change

--- scripts/datamodel_generation.py
+++ scripts/datamodel_generation.py
@@ -1,3 +1,20 @@
+#  Copyright 2021 Collate
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#  http://www.apache.org/licenses/LICENSE-2.0
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+"""
+This script generates the Python models from the JSON Schemas definition. Additionally, it replaces the `SecretStr`
+pydantic class used for the password fields with the `CustomSecretStr` pydantic class which retrieves the secrets
+from a configured secrets' manager.
+"""
+
 import datamodel_code_generator.model.pydantic
 from datamodel_code_generator.imports import Import
 
