91e08614077f6d268f693690a0efd29848e09e88
Shailesh Parmar <shailesh.parmar.webdev@gmail.com>
2023-02-09 22:25:05 +0530

    cypress: rename advanceSearch spec to SearchFlow

--- openmetadata-ui/src/main/resources/ui/cypress/e2e/Flow/SearchFlow.spec.js
+++ openmetadata-ui/src/main/resources/ui/cypress/e2e/Flow/SearchFlow.spec.js
@@ -10,6 +10,9 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
+
+// The spec is related to advance search feature
+
 import {
   addOwner,
   addTag,
