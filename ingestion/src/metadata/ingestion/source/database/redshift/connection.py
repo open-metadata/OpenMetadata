6424b57f519e1b1a87bdd26e2141e997930875f9
Milan Bariya <52292922+MilanBariya@users.noreply.github.com>
2023-02-09 11:33:48 +0530

    Update test connection function for common source (#10116)
    * Update test connection function for common source
    
    * Update test connection function for common source
    
    * Update test connection function
    
    * Update test connection function
    
    * Update test connection function
    
    * Change based on comments

--- ingestion/src/metadata/ingestion/source/database/redshift/connection.py
+++ ingestion/src/metadata/ingestion/source/database/redshift/connection.py
@@ -12,7 +12,10 @@
 """
 Source connection handler
 """
+from functools import partial
+
 from sqlalchemy.engine import Engine
+from sqlalchemy.inspection import inspect
 
 from metadata.generated.schema.entity.services.connections.database.redshiftConnection import (
     RedshiftConnection,
@@ -22,7 +25,16 @@ from metadata.ingestion.connections.builders import (
     get_connection_args_common,
     get_connection_url_common,
 )
-from metadata.ingestion.connections.test_connections import test_connection_db_common
+from metadata.ingestion.connections.test_connections import (
+    TestConnectionStep,
+    test_connection_db_common,
+)
+from metadata.ingestion.source.database.redshift.queries import (
+    REDSHIFT_GET_DATABASE_NAMES,
+    REDSHIFT_GET_STL_QUERY,
+    REDSHIFT_GET_STL_QUERYTEXT,
+    REDSHIFT_GET_SVV_TABLE_INFO,
+)
 
 
 def get_connection(connection: RedshiftConnection) -> Engine:
@@ -40,4 +52,61 @@ def test_connection(engine: Engine) -> None:
     """
     Test connection
     """
-    test_connection_db_common(engine)
+
+    def custom_executor(engine, statement):
+        cursor = engine.execute(statement)
+        return list(cursor.all())
+
+    inspector = inspect(engine)
+    steps = [
+        TestConnectionStep(
+            function=partial(
+                custom_executor,
+                statement=REDSHIFT_GET_DATABASE_NAMES,
+                engine=engine,
+            ),
+            name="Get Databases",
+        ),
+        TestConnectionStep(
+            function=inspector.get_schema_names,
+            name="Get Schemas",
+        ),
+        TestConnectionStep(
+            function=inspector.get_table_names,
+            name="Get Tables",
+        ),
+        TestConnectionStep(
+            function=inspector.get_view_names,
+            name="Get Views",
+            mandatory=False,
+        ),
+        TestConnectionStep(
+            function=partial(
+                custom_executor,
+                statement=REDSHIFT_GET_SVV_TABLE_INFO,
+                engine=engine,
+            ),
+            name="Get Usage and Lineage Svv Table Info",
+            mandatory=False,
+        ),
+        TestConnectionStep(
+            function=partial(
+                custom_executor,
+                statement=REDSHIFT_GET_STL_QUERYTEXT,
+                engine=engine,
+            ),
+            name="Get Usage and Lineage Stl Query Text",
+            mandatory=False,
+        ),
+        TestConnectionStep(
+            function=partial(
+                custom_executor,
+                statement=REDSHIFT_GET_STL_QUERY,
+                engine=engine,
+            ),
+            name="Get Usage and Lineage Stl Query",
+            mandatory=False,
+        ),
+    ]
+
+    test_connection_db_common(engine, steps)
