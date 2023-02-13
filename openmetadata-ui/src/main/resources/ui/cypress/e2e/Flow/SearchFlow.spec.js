8ad27eb0a6ffb6b48c1929ee49747d22653867e8
Shailesh Parmar <shailesh.parmar.webdev@gmail.com>
2023-02-13 18:42:28 +0530

    cypress: rename advanceSearch spec to SearchFlow (#10168)
    * cypress: rename advanceSearch spec to SearchFlow
    
    * updated searchflow spec
    
    * updated cypres.config to run searchflow last
    
    * updated cypress config
    
    * Revert "updated cypress config"
    
    This reverts commit e7b74f7a826f803c3580e951138f930e3627fcc6.
    
    * Revert "updated cypres.config to run searchflow last"
    
    This reverts commit b455e82a831fb44f9c0844a8fd99a8de11056d62.

--- openmetadata-ui/src/main/resources/ui/cypress/e2e/Flow/SearchFlow.spec.js
+++ openmetadata-ui/src/main/resources/ui/cypress/e2e/Flow/SearchFlow.spec.js
@@ -10,6 +10,9 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
+
+// The spec is related to advance search feature
+
 import {
   addOwner,
   addTag,
@@ -37,7 +40,7 @@ import { MYSQL } from '../../constants/service.constants';
 
 const service_name = MYSQL.serviceName;
 
-describe('Advance search should work properly for all fields', () => {
+describe('pre-requests for test case', () => {
   beforeEach(() => {
     cy.login();
   });
@@ -90,6 +93,12 @@ describe('Advance search should work properly for all fields', () => {
       service_name
     );
   });
+});
+
+describe('Single filed search', () => {
+  beforeEach(() => {
+    cy.login();
+  });
 
   Object.values(FIELDS).forEach((field) => {
     it(`Verify advance search results for ${field.name} field and all condition`, () => {
@@ -120,7 +129,7 @@ describe('Advance search should work properly for all fields', () => {
   });
 });
 
-describe('Advance search should work properly for Add Group functionality', () => {
+describe('Group search', () => {
   beforeEach(() => {
     cy.login();
   });
@@ -198,7 +207,7 @@ describe('Advance search should work properly for Add Group functionality', () =
   });
 });
 
-describe('Advance search should work properly for Add Rule functionality', () => {
+describe('Search with additional rule', () => {
   beforeEach(() => {
     cy.login();
   });
