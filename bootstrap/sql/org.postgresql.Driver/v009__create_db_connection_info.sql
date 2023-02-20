785142d86a3d766e365a37f103ab6fa2fe79c123
Ayush Shah <ayush@getcollate.io>
2023-02-21 00:43:45 +0530

    Add policy tags from Bigquery (#10189)

--- bootstrap/sql/org.postgresql.Driver/v009__create_db_connection_info.sql
+++ bootstrap/sql/org.postgresql.Driver/v009__create_db_connection_info.sql
@@ -1,3 +1,7 @@
 -- Unique constraint for user email address
 ALTER TABLE user_entity
 ADD UNIQUE (email);
+
+
+-- Remove classificationName in BigQuery
+UPDATE dbservice_entity SET json = json #- '{connection,config,classificationName}' where serviceType in ('BigQuery');
