14b2389beba6aa8e7d1e96aa85d6ef394b923ebe
Ayush Shah <ayush@getcollate.io>
2023-02-20 14:29:22 +0530

    Add Migrations

--- bootstrap/sql/com.mysql.cj.jdbc.Driver/v009__create_db_connection_info.sql
+++ bootstrap/sql/com.mysql.cj.jdbc.Driver/v009__create_db_connection_info.sql
@@ -1,3 +1,9 @@
 -- Unique constraint for user email address
 ALTER TABLE user_entity
 ADD UNIQUE (email);
+
+
+-- Remove classificationName in BigQuery
+UPDATE dbservice_entity
+SET json = jsonb_set(json, '{connection,config,classificationName}', 'null', false)
+where serviceType in ('BigQuery') and json#>'{connection,config,classificationName}' is not null;
