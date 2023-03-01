8e5122a06cc4a08a47f2939206775c73d0bb7165
“Vijay” <vijay.l@deuexsolutions.com>
2023-03-01 11:45:52 +0530

    Added two different env files for mysql and postgres

--- docker/local-metadata/mysql-script.sql
+++
@@ -1,7 +0,0 @@
-CREATE DATABASE openmetadata_db;
-CREATE DATABASE airflow_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-CREATE USER 'openmetadata_user'@'%' IDENTIFIED BY 'openmetadata_password';
-CREATE USER 'airflow_user'@'%' IDENTIFIED BY 'airflow_pass';
-GRANT ALL PRIVILEGES ON openmetadata_db.* TO 'openmetadata_user'@'%' WITH GRANT OPTION;
-GRANT ALL PRIVILEGES ON airflow_db.* TO 'airflow_user'@'%' WITH GRANT OPTION;
-commit;
