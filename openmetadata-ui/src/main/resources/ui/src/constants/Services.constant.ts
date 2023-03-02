6e5f9b05ddfa8ddcde7b578b286d610e4ef49c5e
Sachin Chaurasiya <sachinchaurasiyachotey87@gmail.com>
2023-03-02 11:22:31 +0530

    chore: update object store service label

--- openmetadata-ui/src/main/resources/ui/src/constants/Services.constant.ts
+++ openmetadata-ui/src/main/resources/ui/src/constants/Services.constant.ts
@@ -193,7 +193,7 @@ export const SERVICE_CATEGORY: { [key: string]: ServiceCategory } = {
   pipelines: ServiceCategory.PIPELINE_SERVICES,
   mlModels: ServiceCategory.ML_MODEL_SERVICES,
   metadata: ServiceCategory.METADATA_SERVICES,
-  objectstores: ServiceCategory.OBJECT_STORE_SERVICES,
+  objectStores: ServiceCategory.OBJECT_STORE_SERVICES,
 };
 
 export const SERVICE_CATEGORY_TYPE = {
@@ -203,7 +203,7 @@ export const SERVICE_CATEGORY_TYPE = {
   pipelineServices: 'pipelines',
   mlmodelServices: 'mlModels',
   metadataServices: 'metadata',
-  objectstoreServices: 'objectstores',
+  objectstoreServices: 'objectStores',
 };
 
 export const servicesDisplayName: { [key: string]: string } = {
