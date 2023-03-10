f4eb6474850a5db344d25c0d21e8361193349bf9
Teddy Crepineau <teddy.crepineau@gmail.com>
2023-03-10 14:16:23 +0100

    bump ingestion package version

--- ingestion/setup.py
+++ ingestion/setup.py
@@ -231,7 +231,7 @@ test = {
 build_options = {"includes": ["_cffi_backend"]}
 setup(
     name="openmetadata-ingestion",
-    version="0.13.2.6",
+    version="0.13.2.7",
     url="https://open-metadata.org/",
     author="OpenMetadata Committers",
     license="Apache License 2.0",
