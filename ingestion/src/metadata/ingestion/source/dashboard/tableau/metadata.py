2c261f9788c9c5283f20f607ac8f91d898878ead
Teddy <teddy.crepineau@gmail.com>
2023-03-10 09:12:14 +0100

    fix: handle None `owner` (#10497)
    (cherry picked from commit cea10e8301ae948393c8346b662a232cee77ff9d)

--- ingestion/src/metadata/ingestion/source/dashboard/tableau/metadata.py
+++ ingestion/src/metadata/ingestion/source/dashboard/tableau/metadata.py
@@ -244,7 +244,7 @@ class TableauSource(DashboardServiceSource):
         Returns:
             Optional[EntityReference]
         """
-        if dashboard_details.owner.email:
+        if dashboard_details.owner and dashboard_details.owner.email:
             user = self.metadata.get_user_by_email(dashboard_details.owner.email)
             if user:
                 return EntityReference(id=user.id.__root__, type="user")
