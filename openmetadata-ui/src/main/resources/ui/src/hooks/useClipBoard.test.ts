d067bdc7046ebcf3225a500e1ca043f82fdc2f59
Sachin Chaurasiya <sachinchaurasiyachotey87@gmail.com>
2023-03-13 00:15:52 +0530

    test: update test imports

--- openmetadata-ui/src/main/resources/ui/src/hooks/useClipBoard.test.ts
+++ openmetadata-ui/src/main/resources/ui/src/hooks/useClipBoard.test.ts
@@ -10,7 +10,8 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-import { act, renderHook } from '@testing-library/react-hooks';
+import { act } from '@testing-library/react';
+import { renderHook } from '@testing-library/react-hooks';
 import { useClipboard } from './useClipBoard';
 
 const clipboardWriteTextMock = jest.fn();
