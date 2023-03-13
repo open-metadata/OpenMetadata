05a8082502ef0792e99c2279b95902634266a528
Ashish Gupta <ashish@getcollate.io>
2023-03-13 19:48:12 +0530

    Shown Errorplaceholder for ES

--- openmetadata-ui/src/main/resources/ui/src/components/Explore/Explore.component.tsx
+++ openmetadata-ui/src/main/resources/ui/src/components/Explore/Explore.component.tsx
@@ -74,6 +74,7 @@ const Explore: React.FC<ExploreProps> = ({
   page = 1,
   onChangePage = noop,
   loading,
+  isElasticSearchIssue = false,
 }) => {
   const { t } = useTranslation();
   const { tab } = useParams<{ tab: string }>();
@@ -329,6 +330,7 @@ const Explore: React.FC<ExploreProps> = ({
                   currentPage={page}
                   data={searchResults?.hits.hits ?? []}
                   handleSummaryPanelDisplay={handleSummaryPanelDisplay}
+                  isElasticSearchIssue={isElasticSearchIssue}
                   isSummaryPanelVisible={showSummaryPanel}
                   paginate={(value) => {
                     if (isNumber(value)) {
