780dcd3a5152cb56aacb82229d9512f2bb33ed5d
Ashish Gupta <ashish@getcollate.io>
2023-03-14 16:47:55 +0530

    Added bot flag in user search api

--- openmetadata-ui/src/main/resources/ui/cypress/e2e/Pages/Users.spec.js
+++ openmetadata-ui/src/main/resources/ui/cypress/e2e/Pages/Users.spec.js
@@ -27,6 +27,27 @@ const userEmail = `${userName}@gmail.com`;
 const adminName = `Admincttest${uuid()}`;
 const adminEmail = `${adminName}@gmail.com`;
 
+const searchBotText = 'bot';
+
+const searchBotUser = () => {
+  interceptURL(
+    'GET',
+    `/api/v1/search/query?q=*${searchBotText}***isBot:false&from=0&size=15&index=user_search_index`,
+    'searchUser'
+  );
+  cy.get('[data-testid="searchbar"]')
+    .should('exist')
+    .should('be.visible')
+    .type(searchBotText);
+
+  verifyResponseStatusCode('@searchUser', 200);
+
+  cy.get('.ant-table-placeholder > .ant-table-cell').should(
+    'contain',
+    searchBotText
+  );
+};
+
 describe('Users flow should work properly', () => {
   beforeEach(() => {
     cy.login();
@@ -74,6 +95,10 @@ describe('Users flow should work properly', () => {
   it('Permanently Delete Soft Deleted User', () => {
     softDeleteUser(userName);
     deleteSoftDeletedUser(userName);
+  });
+
+  it('Search bot user', () => {
+    searchBotUser();
   });
 });
 
