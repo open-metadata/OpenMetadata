c9fb358af0fba37f777f1fddcc68db9271f72b81
ulixius9 <mayursingal9@gmail.com>
2023-03-15 21:47:08 +0530

    handle oracle source

--- ingestion/src/metadata/ingestion/source/database/oracle/metadata.py
+++ ingestion/src/metadata/ingestion/source/database/oracle/metadata.py
@@ -8,9 +8,23 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+
+# pylint: disable=protected-access
 """Oracle source module"""
-from sqlalchemy.dialects.oracle.base import OracleDialect
+import re
+
+from sqlalchemy import sql, util
+from sqlalchemy.dialects.oracle.base import (
+    FLOAT,
+    INTEGER,
+    INTERVAL,
+    NUMBER,
+    TIMESTAMP,
+    OracleDialect,
+    ischema_names,
+)
 from sqlalchemy.engine import reflection
+from sqlalchemy.sql import sqltypes
 
 from metadata.generated.schema.entity.services.connections.database.oracleConnection import (
     OracleConnection,
@@ -22,16 +36,27 @@ from metadata.generated.schema.metadataIngestion.workflow import (
     Source as WorkflowSource,
 )
 from metadata.ingestion.api.source import InvalidSourceException
+from metadata.ingestion.source.database.column_type_parser import create_sqlalchemy_type
 from metadata.ingestion.source.database.common_db_source import CommonDbSourceService
 from metadata.ingestion.source.database.oracle.queries import (
     ORACLE_ALL_TABLE_COMMENTS,
     ORACLE_ALL_VIEW_DEFINITIONS,
+    ORACLE_GET_COLUMNS,
+    ORACLE_IDENTITY_TYPE,
 )
 from metadata.utils.sqlalchemy_utils import (
     get_all_table_comments,
     get_all_view_definitions,
     get_table_comment_wrapper,
     get_view_definition_wrapper,
+)
+
+ischema_names.update(
+    {
+        "ROWID": create_sqlalchemy_type("ROWID"),
+        "XMLTYPE": create_sqlalchemy_type("XMLTYPE"),
+        "INTERVAL YEAR TO MONTH": INTERVAL,
+    }
 )
 
 
@@ -74,7 +99,126 @@ def get_view_definition(
     )
 
 
+def _get_col_type(self, coltype, precision, scale, length, colname):
+    if coltype == "NUMBER":
+        if precision is None and scale == 0:
+            coltype = INTEGER()
+        else:
+            coltype = NUMBER(precision, scale)
+    elif coltype == "FLOAT":
+        # TODO: support "precision" here as "binary_precision"
+        coltype = FLOAT()
+    elif coltype in ("VARCHAR2", "NVARCHAR2", "CHAR", "NCHAR"):
+        coltype = self.ischema_names.get(coltype)(length)
+    elif "WITH TIME ZONE" in coltype or "TIMESTAMP" in coltype:
+        coltype = TIMESTAMP(timezone=True)
+    elif "INTERVAL" in coltype:
+        coltype = INTERVAL()
+    else:
+        coltype = re.sub(r"\(\d+\)", "", coltype)
+        try:
+            coltype = self.ischema_names[coltype]
+        except KeyError:
+            util.warn(f"Did not recognize type '{coltype}' of column '{colname}'")
+            coltype = sqltypes.NULLTYPE
+    return coltype
+
+
+# pylint: disable=too-many-locals
+@reflection.cache
+def get_columns(self, connection, table_name, schema=None, **kw):
+    """
+
+    Dialect method overridden to add raw data type
+
+    kw arguments can be:
+
+        oracle_resolve_synonyms
+
+        dblink
+
+    """
+    resolve_synonyms = kw.get("oracle_resolve_synonyms", False)
+    dblink = kw.get("dblink", "")
+    info_cache = kw.get("info_cache")
+
+    (table_name, schema, dblink, _) = self._prepare_reflection_args(
+        connection,
+        table_name,
+        schema,
+        resolve_synonyms,
+        dblink,
+        info_cache=info_cache,
+    )
+    columns = []
+
+    char_length_col = "data_length"
+    if self._supports_char_length:
+        char_length_col = "char_length"
+
+    identity_cols = "NULL as default_on_null, NULL as identity_options"
+    if self.server_version_info >= (12,):
+        identity_cols = ORACLE_IDENTITY_TYPE.format(dblink=dblink)
+
+    params = {"table_name": table_name}
+
+    text = ORACLE_GET_COLUMNS.format(
+        dblink=dblink, char_length_col=char_length_col, identity_cols=identity_cols
+    )
+    if schema is not None:
+        params["owner"] = schema
+        text += " AND col.owner = :owner "
+    text += " ORDER BY col.column_id"
+
+    cols = connection.execute(sql.text(text), params)
+
+    for row in cols:
+        colname = self.normalize_name(row[0])
+        length = row[2]
+        nullable = row[5] == "Y"
+        default = row[6]
+        generated = row[8]
+        default_on_nul = row[9]
+        identity_options = row[10]
+        raw_coltype = row.data_type
+
+        coltype = self._get_col_type(
+            row.data_type, row.data_precision, row.data_scale, length, colname
+        )
+
+        computed = None
+        if generated == "YES":
+            computed = {"sqltext": default}
+            default = None
+
+        identity = None
+        if identity_options is not None:
+            identity = self._parse_identity_options(identity_options, default_on_nul)
+            default = None
+
+        cdict = {
+            "name": colname,
+            "type": coltype,
+            "nullable": nullable,
+            "default": default,
+            "autoincrement": "auto",
+            "comment": row.comments,
+            "raw_data_type": raw_coltype,
+        }
+        if row.column_name.lower() == row.column_name:
+            cdict["quote"] = True
+        if computed is not None:
+            cdict["computed"] = computed
+        if identity is not None:
+            cdict["identity"] = identity
+
+        columns.append(cdict)
+    return columns
+
+
 OracleDialect.get_table_comment = get_table_comment
+OracleDialect.get_columns = get_columns
+OracleDialect._get_col_type = _get_col_type
 OracleDialect.get_view_definition = get_view_definition
 OracleDialect.get_all_view_definitions = get_all_view_definitions
 OracleDialect.get_all_table_comments = get_all_table_comments
