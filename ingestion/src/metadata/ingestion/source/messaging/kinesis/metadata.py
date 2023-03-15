dbdbd88edf067d4824ae2dd9d7e55e7ccd7c80c5
Onkar Ravgan <onkar.10r@gmail.com>
2023-03-15 23:13:01 +0530

    Fixed typo

--- ingestion/src/metadata/ingestion/source/messaging/kinesis/metadata.py
+++ ingestion/src/metadata/ingestion/source/messaging/kinesis/metadata.py
@@ -9,7 +9,7 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 """
-Kafka source ingestion
+Kinesis source ingestion
 """
 import binascii
 import traceback
