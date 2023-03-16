26c1793744e4173c1d122197da8e556b1d6a8c1e
Onkar Ravgan <onkar.10r@gmail.com>
2023-03-16 17:18:01 +0530

    Added fixes on review comments

--- ingestion/src/metadata/ingestion/source/messaging/kinesis/metadata.py
+++ ingestion/src/metadata/ingestion/source/messaging/kinesis/metadata.py
@@ -14,7 +14,10 @@ Kinesis source ingestion
 import binascii
 import traceback
 from base64 import b64decode
-from typing import Any, Dict, Iterable, List
+from enum import Enum
+from typing import Any, Dict, Iterable, List, Optional
+
+from pydantic import BaseModel
 
 from metadata.generated.schema.api.data.createTopic import CreateTopicRequest
 from metadata.generated.schema.entity.data.topic import TopicSampleData
@@ -39,6 +42,41 @@ from metadata.utils.logger import ingestion_logger
 logger = ingestion_logger()
 
 
+class KinesisTopicModel(BaseModel):
+    """
+    Chart (View) representation from API
+    """
+
+    stream_names: List[str]
+    has_more_streams: bool
+
+
+class KinesisTopicMetadataModel(BaseModel):
+    summary: Optional[dict]
+    partitions: Optional[List[str]]
+
+
+class KinesisEnum(Enum):
+    """
+    Common enum for dbt
+    """
+
+    LIMIT = "Limit"
+    STREAM_NAMES = "StreamNames"
+    STREAM_NAME = "StreamName"
+    HAS_MORE_STREAMS = "HasMoreStreams"
+    EXCLUSIVE_START_STREAM_NAME = "ExclusiveStartStreamName"
+    STREAM_DESCRIPTION_SUMMARY = "StreamDescriptionSummary"
+    RETENTION_PERIOD_HOURS = "RetentionPeriodHours"
+    NEXT_TOKEN = "NextToken"
+    SHARDS = "Shards"
+    SHARD_ID = "ShardId"
+    RECORDS = "Records"
+    DATA = "Data"
+    SHARD_ITERATOR = "ShardIterator"
+    TRIM_HORIZON = "TRIM_HORIZON"
+
+
 class KinesisSource(MessagingServiceSource):
     """
     Implements the necessary methods to extract
@@ -60,40 +98,60 @@ class KinesisSource(MessagingServiceSource):
             )
         return cls(config, metadata_config)
 
-    def get_topic_list(self) -> Iterable[BrokerTopicDetails]:
-        all_topics, has_more_topics, args = [], True, {"Limit": 100}
-        try:
-            while has_more_topics:
+    def get_stream_names_list(self) -> List[str]:
+        """
+        Get the list of all the streams
+        """
+        all_topics, has_more_topics, args = [], True, {KinesisEnum.LIMIT.value: 100}
+        while has_more_topics:
+            try:
                 topics = self.kinesis.list_streams(**args)
-                all_topics.extend(topics["StreamNames"])
-                has_more_topics = topics["HasMoreStreams"]
+                kinesis_topic_model = KinesisTopicModel(
+                    stream_names=topics.get(KinesisEnum.STREAM_NAMES.value),
+                    has_more_streams=topics.get(KinesisEnum.HAS_MORE_STREAMS.value),
+                )
+                all_topics.extend(kinesis_topic_model.stream_names)
+                has_more_topics = kinesis_topic_model.has_more_streams
                 if len(all_topics) > 0:
-                    args["ExclusiveStartStreamName"] = all_topics[-1]
-        except Exception as err:
-            logger.debug(traceback.format_exc())
-            logger.error(f"Failed to fetch models list - {err}")
+                    args[KinesisEnum.EXCLUSIVE_START_STREAM_NAME.value] = all_topics[-1]
+            except Exception as err:
+                logger.debug(traceback.format_exc())
+                logger.error(f"Failed to fetch kinesis stream - {err}")
+        return all_topics
 
+    def get_topic_list(self) -> Iterable[BrokerTopicDetails]:
+        """
+        Method to yeild topic details
+        """
+        all_topics = self.get_stream_names_list()
         for topic_name in all_topics:
-            yield BrokerTopicDetails(
-                topic_name=topic_name,
-                topic_metadata={
-                    "summary": self._get_topic_details(topic_name),
-                    "partitions": self._get_topic_partitions(topic_name),
-                },
-            )
+            try:
+                yield BrokerTopicDetails(
+                    topic_name=topic_name,
+                    topic_metadata=KinesisTopicMetadataModel(
+                        summary=self._get_topic_details(topic_name),
+                        partitions=self._get_topic_partitions(topic_name),
+                    ),
+                )
+            except Exception as err:
+                logger.debug(traceback.format_exc())
+                logger.error(f"Failed to yield kinesis topic - {err}")
 
     def yield_topic(
         self, topic_details: BrokerTopicDetails
     ) -> Iterable[CreateTopicRequest]:
+        """
+        Method to yield the create topic request
+        """
         try:
             logger.info(f"Fetching topic details {topic_details.topic_name}")
             topic = CreateTopicRequest(
                 name=topic_details.topic_name,
                 service=self.context.messaging_service.fullyQualifiedName.__root__,
-                partitions=len(topic_details.topic_metadata["partitions"]),
+                partitions=len(topic_details.topic_metadata.partitions),
                 retentionTime=float(
-                    topic_details.topic_metadata["summary"].get(
-                        "RetentionPeriodHours", 0
+                    topic_details.topic_metadata.summary.get(
+                        KinesisEnum.RETENTION_PERIOD_HOURS.value, 0
                     )
                     * 3600000
                 ),
@@ -118,27 +176,34 @@ class KinesisSource(MessagingServiceSource):
         # max message size supported by Kinesis is 1MB and is not configurable
         return 1000000
 
-    def _get_topic_details(self, topic_name: str) -> Dict[str, Any]:
+    def _get_topic_details(self, topic_name: str) -> Optional[Dict[str, Any]]:
         try:
             topic = self.kinesis.describe_stream_summary(StreamName=topic_name)
-            return topic["StreamDescriptionSummary"]
+            return topic.get(KinesisEnum.STREAM_DESCRIPTION_SUMMARY.value)
         except Exception as err:
             logger.debug(traceback.format_exc())
             logger.warning(
                 f"Error while fetching topic partitions for topic: {topic_name} - {err}"
             )
-        return {}
+        return None
 
     def _get_topic_partitions(self, topic_name: str) -> List[str]:
-        all_partitions, has_more_partitions, args = [], True, {"StreamName": topic_name}
+        all_partitions, has_more_partitions, args = (
+            [],
+            True,
+            {KinesisEnum.STREAM_NAME.value: topic_name},
+        )
         try:
             while has_more_partitions:
                 partitions = self.kinesis.list_shards(**args)
                 all_partitions.extend(
-                    [part["ShardId"] for part in partitions["Shards"]]
+                    [
+                        partition.get(KinesisEnum.SHARD_ID.value)
+                        for partition in partitions.get(KinesisEnum.SHARDS.value) or []
+                    ]
                 )
-                has_more_partitions = partitions.get("NextToken")
-                args["NextToken"] = partitions.get("NextToken")
+                has_more_partitions = partitions.get(KinesisEnum.NEXT_TOKEN.value)
+                args[KinesisEnum.NEXT_TOKEN.value] = has_more_partitions
         except Exception as err:
             logger.debug(traceback.format_exc())
             logger.warning(
@@ -158,7 +223,7 @@ class KinesisSource(MessagingServiceSource):
                     topic=self.context.topic,
                     sample_data=self._get_sample_data(
                         topic_details.topic_name,
-                        topic_details.topic_metadata["partitions"],
+                        topic_details.topic_metadata.partitions,
                     ),
                 )
         except Exception as err:
@@ -174,18 +239,18 @@ class KinesisSource(MessagingServiceSource):
                 shard_iterator = self.kinesis.get_shard_iterator(
                     StreamName=topic_name,
                     ShardId=shard,
-                    ShardIteratorType="TRIM_HORIZON",
-                )["ShardIterator"]
+                    ShardIteratorType=KinesisEnum.TRIM_HORIZON.value,
+                ).get(KinesisEnum.SHARD_ITERATOR.value)
 
-                records = self.kinesis.get_records(ShardIterator=shard_iterator)[
-                    "Records"
-                ]
+                if shard_iterator:
+                    records = (
+                        self.kinesis.get_records(ShardIterator=shard_iterator).get(
+                            KinesisEnum.RECORDS.value
+                        )
+                        or []
+                    )
+                    data.extend(self._get_sample_records(records=records))
 
-                for record in records:
-                    try:
-                        data.append(b64decode(record["Data"]).decode(UTF_8))
-                    except (binascii.Error, UnicodeDecodeError):
-                        data.append(record["Data"].decode(UTF_8))
                 if data:
                     break
         except Exception as err:
@@ -194,3 +259,18 @@ class KinesisSource(MessagingServiceSource):
                 f"Error while fetching sample data for topic: {topic_name} - {err}"
             )
         return TopicSampleData(messages=data)
+
+    def _get_sample_records(self, records: List) -> List:
+        sample_data = []
+        try:
+            for record in records:
+                record_data = record.get(KinesisEnum.DATA.value)
+                if record_data:
+                    try:
+                        sample_data.append(b64decode(record_data).decode(UTF_8))
+                    except (binascii.Error, UnicodeDecodeError):
+                        sample_data.append(record_data.decode(UTF_8))
+        except Exception as err:
+            logger.debug(traceback.format_exc())
+            logger.warning(f"Error while fetching sample records for topics - {err}")
+        return sample_data
