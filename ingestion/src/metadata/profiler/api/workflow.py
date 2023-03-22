c838e69b94faf551273844a4af52736b15ac2942
Teddy Crepineau <teddy.crepineau@gmail.com>
2023-03-22 09:06:20 +0100

    fix: profiler refactor

--- ingestion/src/metadata/profiler/api/workflow.py
+++ ingestion/src/metadata/profiler/api/workflow.py
@@ -362,7 +362,8 @@ class ProfilerWorkflow(WorkflowStatusMixin):
             self.create_profiler(entity, profiler_interface)
             self.profiler = cast(Profiler, self.profiler)  # satisfy type checker
             profile: ProfilerResponse = self.profiler.process(
-                self.source_config.generateSampleData
+                self.source_config.generateSampleData,
+                self.source_config.processPiiSensitive,
             )
         except Exception as exc:  # pylint: disable=broad-except
 
