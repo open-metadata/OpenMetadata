3cf5af2536de6f0b33740e708736f739d90ce7cd
Sriharsha Chintalapani <harsha@getcollate.io>
2023-03-23 11:34:10 -0700

    Add query index

--- openmetadata-service/src/main/java/org/openmetadata/service/elasticsearch/QueryIndex.java
+++ openmetadata-service/src/main/java/org/openmetadata/service/elasticsearch/QueryIndex.java
@@ -18,12 +18,13 @@ public class QueryIndex implements ElasticSearchIndex {
   public Map<String, Object> buildESDoc() {
     Map<String, Object> doc = JsonUtils.getMap(query);
     List<ElasticSearchSuggest> suggest = new ArrayList<>();
-    suggest.add(ElasticSearchSuggest.builder().input(query.getFullyQualifiedName()).weight(5).build());
-    suggest.add(ElasticSearchSuggest.builder().input(query.getName()).weight(10).build());
+    if (query.getDisplayName() != null) {
+      suggest.add(ElasticSearchSuggest.builder().input(query.getName()).weight(10).build());
+    }
     ElasticSearchIndexUtils.removeNonIndexableFields(doc, excludeTopicFields);
 
     ParseTags parseTags = new ParseTags(Entity.getEntityTags(Entity.QUERY, query));
-    doc.put("displayName", query.getDisplayName() != null ? query.getDisplayName() : query.getName());
+    doc.put("displayName", query.getDisplayName() != null ? query.getDisplayName() : "");
     doc.put("tags", parseTags.tags);
     doc.put("tier", parseTags.tierTag);
     doc.put("followers", ElasticSearchIndexUtils.parseFollowers(query.getFollowers()));
