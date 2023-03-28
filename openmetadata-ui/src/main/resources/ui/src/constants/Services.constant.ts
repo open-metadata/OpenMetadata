e3093eb05c10a002e9d4b6467d19d952f4dbe5df
Sachin Chaurasiya <sachinchaurasiyachotey87@gmail.com>
2023-03-28 18:30:31 +0530

    address comments

--- openmetadata-ui/src/main/resources/ui/src/constants/Services.constant.ts
+++ openmetadata-ui/src/main/resources/ui/src/constants/Services.constant.ts
@@ -16,6 +16,7 @@ import gcs from 'assets/img/service-icon-gcs.png';
 import msAzure from 'assets/img/service-icon-ms-azure.png';
 import { WorkflowStatus } from 'generated/entity/automations/workflow';
 import { ObjectStoreServiceType } from 'generated/entity/services/objectstoreService';
+import { ServiceType } from 'generated/entity/services/serviceType';
 import { map, startCase } from 'lodash';
 import { ServiceTypes } from 'Models';
 import i18n from 'utils/i18next/LocalUtil';
@@ -271,3 +272,13 @@ export const WORKFLOW_COMPLETE_STATUS = [
   WorkflowStatus.Failed,
   WorkflowStatus.Successful,
 ];
+
+export const SERVICE_TYPE_MAP = {
+  [ServiceCategory.DASHBOARD_SERVICES]: ServiceType.Dashboard,
+  [ServiceCategory.DATABASE_SERVICES]: ServiceType.Database,
+  [ServiceCategory.MESSAGING_SERVICES]: ServiceType.Messaging,
+  [ServiceCategory.ML_MODEL_SERVICES]: ServiceType.MlModel,
+  [ServiceCategory.METADATA_SERVICES]: ServiceType.Metadata,
+  [ServiceCategory.OBJECT_STORE_SERVICES]: ServiceType.ObjectStore,
+  [ServiceCategory.PIPELINE_SERVICES]: ServiceType.Pipeline,
+};
