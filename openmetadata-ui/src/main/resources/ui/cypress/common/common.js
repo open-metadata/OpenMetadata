c359478aa53dd305fddc71d400125ecc4bff52a8
karanh37 <karanh37@gmail.com>
2023-03-28 20:22:54 +0530

    fix: team cypress tests

--- openmetadata-ui/src/main/resources/ui/cypress/common/common.js
+++ openmetadata-ui/src/main/resources/ui/cypress/common/common.js
@@ -864,8 +864,6 @@ export const updateOwner = () => {
       // Clicking on edit owner button
       cy.get('[data-testid="add-user"]').should('be.visible').click();
 
-      verifyResponseStatusCode('@getUsers', 200);
-
       cy.get('[data-testid="selectable-list"]')
         .find(`[title="${text.trim()}"]`)
         .click();
