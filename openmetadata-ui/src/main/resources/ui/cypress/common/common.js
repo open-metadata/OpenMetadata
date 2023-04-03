57d3b0cc22121e3f05c64f4df4474d976ba0ae50
Shailesh Parmar <shailesh.parmar.webdev@gmail.com>
2023-04-03 21:03:17 +0530

    added API wait for users spec

--- openmetadata-ui/src/main/resources/ui/cypress/common/common.js
+++ openmetadata-ui/src/main/resources/ui/cypress/common/common.js
@@ -503,6 +503,7 @@ export const visitEntityDetailsPage = (term, serviceName, entity) => {
   cy.get('[data-testid="searchBox"]').scrollIntoView().should('be.visible');
   cy.get('[data-testid="searchBox"]').type(term);
   verifyResponseStatusCode('@searchQuery', 200);
+  cy.get('[data-testid="suggestion-overlay"]').should('exist');
   cy.get('body').then(($body) => {
     // checking if requested term is available in search suggestion
     if (
