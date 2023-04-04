3621033111de09ee839607141b4c706ba743cc20
Keith Sirmons <brian.k.sirmons@nasa.gov>
2023-04-04 12:08:31 -0500

    Cleaned up code by pulling check into its own function

--- ingestion/src/metadata/profiler/orm/converter.py
+++ ingestion/src/metadata/profiler/orm/converter.py
@@ -104,6 +104,19 @@ def check_snowflake_case_sensitive(table_service_type, table_or_col) -> Optional
 
     return None
 
+def check_if_should_quote_column_name(table_service_type) -> Optional[bool]:
+    """Check whether column name should be quoted when passed into the sql command build up.
+    This is important when a column name is the same as a reserve word and causes a sql error.
+    
+    Args:
+        table_service_type: the main sql engine to determine if we should always quote.
+    Return: True or False
+    """
+    
+    if table_service_type == databaseService.DatabaseServiceType.Hive:
+        return True
+    
+    return None
 
 def build_orm_col(
     idx: int, col: Column, table_service_type, parent: Optional[str] = None
@@ -119,11 +132,6 @@ def build_orm_col(
     As this is only used for INSERT/UPDATE/DELETE,
     there is no impact for our read-only purposes.
     """
-    if table_service_type == databaseService.DatabaseServiceType.Hive:
-        shouldQuote = True
-    else:
-        shouldQuote = False
-
     if parent:
         name = f"{parent}.{col.name.__root__}"
     else:
@@ -133,7 +141,8 @@ def build_orm_col(
         name=str(name),
         type_=map_types(col, table_service_type),
         primary_key=not bool(idx),  # The first col seen is used as PK
-        quote=shouldQuote or check_snowflake_case_sensitive(table_service_type, col.name.__root__),
+        quote=check_if_should_quote_column_name(table_service_type) 
+            or check_snowflake_case_sensitive(table_service_type, col.name.__root__),
         key=str(
             col.name.__root__
         ).lower(),  # Add lowercase column name as key for snowflake case sensitive columns
