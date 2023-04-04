46c5d4f17bcdd899502cb6bdea4a5c73f372c348
Chirag Madlani <12962843+chirag-madlani@users.noreply.github.com>
2023-04-04 18:09:44 +0530

    update numKeptInMemory flag to minimum

--- openmetadata-ui/src/main/resources/ui/cypress.config.ts
+++ openmetadata-ui/src/main/resources/ui/cypress.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
   defaultCommandTimeout: 5000,
   videoUploadOnPasses: false,
   chromeWebSecurity: false,
-  numTestsKeptInMemory: 10,
+  numTestsKeptInMemory: 1,
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
