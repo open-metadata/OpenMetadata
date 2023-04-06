06b951681abf54e4c29e48a8badb6d59b19f91d9
Keith Sirmons <brian.k.sirmons@nasa.gov>
2023-04-06 09:56:14 -0500

    updated to fix some pylint errors. imported Dialects to remove string compare to "impala" engine

--- ingestion/src/metadata/ingestion/source/database/hive/metadata.py
+++ ingestion/src/metadata/ingestion/source/database/hive/metadata.py
@@ -30,11 +30,9 @@ from metadata.generated.schema.metadataIngestion.workflow import (
     Source as WorkflowSource,
 )
 from metadata.ingestion.api.source import InvalidSourceException
-from metadata.ingestion.source.database.column_helpers import (
-    remove_table_from_column_name,
-)
 from metadata.ingestion.source.database.common_db_source import CommonDbSourceService
 from metadata.ingestion.source.database.hive.queries import HIVE_GET_COMMENTS
+from metadata.profiler.orm.registry import Dialects
 
 complex_data_types = ["struct", "map", "array", "union"]
 
@@ -184,8 +182,12 @@ def get_table_comment(  # pylint: disable=unused-argument
     return {"text": None}
 
 
-def get_impala_table_or_view_names(connection, schema=None, targetType="table"):
-    query = f"show tables"
+def get_impala_table_or_view_names(connection, schema=None, target_type="table"):
+    """
+    Depending on the targetType returns either the Views or Tables
+    since they share the same method for getting their names.
+    """
+    query = "show tables"
     if schema:
         query += " IN " + schema
 
@@ -195,31 +197,39 @@ def get_impala_table_or_view_names(connection, schema=None, targetType="table"):
 
     retvalue = []
 
-    for tv in tables_and_views:
-        full_table_name = f"{schema}.{tv}"
-        query = f"describe formatted `{schema}`.`{tv}`"
+    for table_view in tables_and_views:
+        query = f"describe formatted `{schema}`.`{table_view}`"
         cursor = connection.execute(query)
         results = cursor.fetchall()
 
         for result in list(results):
             data = result
             if data[0].strip() == "Table Type:":
-                if targetType.lower() in data[1].lower():
-                    retvalue.append(tv)
+                if target_type.lower() in data[1].lower():
+                    retvalue.append(table_view)
     return retvalue
 
 
-def get_impala_view_names(self, connection, schema=None, **kw):
+def get_impala_view_names(
+    self, connection, schema=None, **kw
+):  # pylint: disable=unused-argument
     results = get_impala_table_or_view_names(connection, schema, "view")
     return results
 
 
-def get_impala_table_names(self, connection, schema=None, **kw):
+def get_impala_table_names(
+    self, connection, schema=None, **kw
+):  # pylint: disable=unused-argument
     results = get_impala_table_or_view_names(connection, schema, "table")
     return results
 
 
-def get_impala_table_comment(self, connection, table_name, schema_name, **kw):
+def get_impala_table_comment(
+    self, connection, table_name, schema_name, **kw
+):  # pylint: disable=unused-argument
+    """
+    Gets the table comment from the describe formatted query result under the Table Parameters section.
+    """
     full_table_name = (
         f"{schema_name}.{table_name}" if schema_name is not None else table_name
     )
@@ -228,13 +238,13 @@ def get_impala_table_comment(self, connection, table_name, schema_name, **kw):
     cursor = connection.execute(query)
     results = cursor.fetchall()
 
-    foundTableParameters = False
+    found_table_parameters = False
     try:
         for result in list(results):
             data = result
-            if not foundTableParameters and data[0].strip() == "Table Parameters:":
-                foundTableParameters = True
-            if foundTableParameters:
+            if not found_table_parameters and data[0].strip() == "Table Parameters:":
+                found_table_parameters = True
+            if found_table_parameters:
                 coltext = data[1].strip() if data[1] is not None else ""
                 if coltext == "comment":
                     return {"text": data[2]}
@@ -243,7 +253,9 @@ def get_impala_table_comment(self, connection, table_name, schema_name, **kw):
     return {"text": None}
 
 
-def get_impala_columns(self, connection, table_name, schema=None, **kwargs):
+def get_impala_columns(
+    self, connection, table_name, schema=None, **kwargs
+):  # pylint: disable=unused-argument
     """
     Extracted from the Impala Dialect. We'll tune the implementation.
 
@@ -253,12 +265,11 @@ def get_impala_columns(self, connection, table_name, schema=None, **kwargs):
     full_table_name = f"{schema}.{table_name}" if schema is not None else table_name
     split_name = full_table_name.split(".")
     query = f"DESCRIBE `{split_name[0]}`.`{split_name[1]}`"
-    cursor = connection.execute(query)
-    results = cursor.fetchall()
+    describe_table_rows = connection.execute(query)
     column_info = []
-    ordinalPos = 0
-    for col in results:
-        ordinalPos = ordinalPos + 1
+    ordinal_pos = 0
+    for col in describe_table_rows:
+        ordinal_pos = ordinal_pos + 1
         col_raw = col[1]
         attype = re.sub(r"\(.*\)", "", col[1])
         col_type = re.search(r"^\w+", col[1]).group(0)
@@ -276,15 +287,15 @@ def get_impala_columns(self, connection, table_name, schema=None, **kwargs):
             else:
                 args = (int(charlen),)
             coltype = coltype(*args)
-        a = {
+        add_column = {
             "name": col[0],
             "type": coltype,
             "comment": col[2],
             "nullable": True,
             "autoincrement": False,
-            "ordinalPosition": ordinalPos,
+            "ordinalPosition": ordinal_pos,
         }
-        column_info.append(a)
+        column_info.append(add_column)
     return column_info
 
 
@@ -325,7 +336,7 @@ class HiveSource(CommonDbSourceService):
         Fetching views in hive server with query "SHOW VIEWS" was possible
         only after hive 2.2.0 version
         """
-        if self.engine.driver == "impala":
+        if self.engine.driver == Dialects.Impala:
             ImpalaDialect.get_table_names = get_impala_table_names
             ImpalaDialect.get_view_names = get_impala_view_names
             ImpalaDialect.get_table_comment = get_impala_table_comment
