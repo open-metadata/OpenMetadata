ec6ff78b80927fd5e4a832ca8819ddb92ce24f73
Mohit Yadav <105265192+mohityadav766@users.noreply.github.com>
2023-04-06 10:50:08 +0530

    fixes for Reindex (#10948)
    * fixes for Reindex
    
    * review comments

--- openmetadata-service/src/main/java/org/openmetadata/service/workflows/searchIndex/PaginatedEntitiesSource.java
+++ openmetadata-service/src/main/java/org/openmetadata/service/workflows/searchIndex/PaginatedEntitiesSource.java
@@ -24,14 +24,14 @@ import org.openmetadata.schema.EntityInterface;
 import org.openmetadata.schema.system.StepStats;
 import org.openmetadata.schema.type.Include;
 import org.openmetadata.service.Entity;
-import org.openmetadata.service.exception.ReaderException;
+import org.openmetadata.service.exception.SourceException;
 import org.openmetadata.service.jdbi3.EntityRepository;
 import org.openmetadata.service.jdbi3.ListFilter;
 import org.openmetadata.service.util.ResultList;
 import org.openmetadata.service.workflows.interfaces.Source;
 
 @Slf4j
-public class PaginatedEntitiesReader implements Source<ResultList<? extends EntityInterface>> {
+public class PaginatedEntitiesSource implements Source<ResultList<? extends EntityInterface>> {
   @Getter private final int batchSize;
   @Getter private final String entityType;
   @Getter private final List<String> fields;
@@ -39,14 +39,15 @@ public class PaginatedEntitiesReader implements Source<ResultList<? extends Enti
   private String cursor = null;
   @Getter private boolean isDone = false;
 
-  PaginatedEntitiesReader(String entityType, int batchSize, List<String> fields) {
+  PaginatedEntitiesSource(String entityType, int batchSize, List<String> fields) {
     this.entityType = entityType;
     this.batchSize = batchSize;
     this.fields = fields;
+    this.stats.setTotalRecords(Entity.getEntityRepository(entityType).dao.listTotalCount());
   }
 
   @Override
-  public ResultList<? extends EntityInterface> readNext(Map<String, Object> contextData) throws ReaderException {
+  public ResultList<? extends EntityInterface> readNext(Map<String, Object> contextData) throws SourceException {
     if (!isDone) {
       ResultList<? extends EntityInterface> data = read(cursor);
       cursor = data.getPaging().getAfter();
@@ -59,10 +60,10 @@ public class PaginatedEntitiesReader implements Source<ResultList<? extends Enti
     }
   }
 
-  private ResultList<? extends EntityInterface> read(String cursor) throws ReaderException {
-    LOG.debug("[EntitiesReader] Fetching a Batch of Size: {} ", batchSize);
+  private ResultList<? extends EntityInterface> read(String cursor) throws SourceException {
+    LOG.debug("[PaginatedEntitiesSource] Fetching a Batch of Size: {} ", batchSize);
     EntityRepository<?> entityRepository = Entity.getEntityRepository(entityType);
-    ResultList<? extends EntityInterface> result;
+    ResultList<? extends EntityInterface> result = null;
     try {
       result =
           entityRepository.listAfterWithSkipFailure(
@@ -70,20 +71,28 @@ public class PaginatedEntitiesReader implements Source<ResultList<? extends Enti
       if (result.getErrors().size() > 0) {
         result
             .getErrors()
-            .forEach((error) -> LOG.error("[EntitiesReader] Failed in getting Record, RECORD: {}", error.toString()));
+            .forEach(
+                (error) ->
+                    LOG.error("[PaginatedEntitiesSource] Failed in getting Record, RECORD: {}", error.toString()));
       }
 
       LOG.debug(
-          "[EntitiesReader] Batch Stats :- Submitted : {} Success: {} Failed: {}",
+          "[PaginatedEntitiesSource] Batch Stats :- Submitted : {} Success: {} Failed: {}",
           batchSize,
           result.getData().size(),
           result.getErrors().size());
       updateStats(result.getData().size(), result.getErrors().size());
 
     } catch (IOException e) {
-      LOG.debug("[EntitiesReader] Batch Stats :- Submitted : {} Success: {} Failed: {}", batchSize, 0, batchSize);
-      updateStats(0, batchSize);
-      throw new ReaderException("[EntitiesReader] Batch encountered Exception. Failing Completely.", e);
+      LOG.debug(
+          "[PaginatedEntitiesSource] Batch Stats :- Submitted : {} Success: {} Failed: {}", batchSize, 0, batchSize);
+      if (stats.getTotalRecords() - stats.getProcessedRecords() <= batchSize) {
+        isDone = true;
+        updateStats(0, stats.getTotalRecords() - stats.getProcessedRecords());
+      } else {
+        updateStats(0, batchSize);
+      }
+      throw new SourceException("[PaginatedEntitiesSource] Batch encountered Exception. Failing Completely.", e);
     }
 
     return result;
