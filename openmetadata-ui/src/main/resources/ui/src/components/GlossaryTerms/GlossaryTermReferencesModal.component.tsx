51bf8cd7541e5d33a0e49803068df172a09ee85e
karanh37 <33024356+karanh37@users.noreply.github.com>
2023-04-12 16:42:04 +0530

    fix: glossary route path for add term (#11000)
    * fix: glossary route path for add term
    
    * fix: jest test
    
    * fix: improvements on glossary screen
    
    * fix: glossary comments
    
    * fix: update cypress
    
    * fix: cypress tests
    
    * fix: update cypress tests
    
    * fix: update icon
    
    * fix: tag styles
    
    ---------
    
    Co-authored-by: Chirag Madlani <12962843+chirag-madlani@users.noreply.github.com>

--- openmetadata-ui/src/main/resources/ui/src/components/GlossaryTerms/GlossaryTermReferencesModal.component.tsx
+++ openmetadata-ui/src/main/resources/ui/src/components/GlossaryTerms/GlossaryTermReferencesModal.component.tsx
@@ -54,7 +54,7 @@ const GlossaryTermReferencesModal = ({
           {t('label.cancel')}
         </Button>,
         <Button
-          data-testid="save-button"
+          data-testid="save-btn"
           key="save-btn"
           type="primary"
           onClick={() => form.submit()}>
@@ -126,12 +126,11 @@ const GlossaryTermReferencesModal = ({
               <Form.Item>
                 <Button
                   className="text-primary d-flex items-center"
+                  data-testid="add-references-button"
                   icon={<PlusIcon className="anticon" />}
                   size="small"
                   onClick={() => add()}>
-                  {t('label.add-entity', {
-                    entity: t('label.reference-plural'),
-                  })}
+                  {t('label.add')}
                 </Button>
               </Form.Item>
             </>
