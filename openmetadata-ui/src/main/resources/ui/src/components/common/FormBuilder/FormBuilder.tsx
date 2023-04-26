8bea8615d1fbee5c9867fa2c037748b3f4973151
Sachin Chaurasiya <sachinchaurasiyachotey87@gmail.com>
2023-04-27 00:43:54 +0530

    chore(ui): validate required form fields before testing the connection

--- openmetadata-ui/src/main/resources/ui/src/components/common/FormBuilder/FormBuilder.tsx
+++ openmetadata-ui/src/main/resources/ui/src/components/common/FormBuilder/FormBuilder.tsx
@@ -14,6 +14,7 @@
 import { CheckOutlined } from '@ant-design/icons';
 import Form from '@rjsf/antd';
 import CoreForm, { AjvError, FormProps, IChangeEvent } from '@rjsf/core';
+import validateFormData from '@rjsf/core/lib/validate';
 import { Button as AntDButton } from 'antd';
 import classNames from 'classnames';
 import { customFields } from 'components/JSONSchemaTemplate/CustomFields';
@@ -115,6 +116,12 @@ const FormBuilder: FunctionComponent<Props> = ({
       return error;
     });
 
+  const handleRequiredFieldsValidation = () => {
+    const validationObject = validateFormData(localFormData, schema);
+
+    return !validationObject?.errors.some((error) => error.name === 'required');
+  };
+
   return (
     <Form
       ArrayFieldTemplate={ArrayFieldTemplate}
@@ -159,6 +166,7 @@ const FormBuilder: FunctionComponent<Props> = ({
           isTestingDisabled={disableTestConnection}
           serviceCategory={serviceCategory}
           serviceName={serviceName}
+          onValidateFormRequiredFields={handleRequiredFieldsValidation}
         />
       )}
       <div className="tw-mt-6 tw-flex tw-justify-between">
