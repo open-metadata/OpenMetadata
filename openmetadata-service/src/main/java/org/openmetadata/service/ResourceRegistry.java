265fccda0b1e19aaddd058661d15f910ef3eb12f
sureshms <srini30005@gmail.com>
2023-05-05 10:43:28 -0700

    Fixes #11245 - fix test failures

--- openmetadata-service/src/main/java/org/openmetadata/service/ResourceRegistry.java
+++ openmetadata-service/src/main/java/org/openmetadata/service/ResourceRegistry.java
@@ -2,7 +2,6 @@ package org.openmetadata.service;
 
 import static org.openmetadata.common.utils.CommonUtil.listOf;
 import static org.openmetadata.common.utils.CommonUtil.nullOrEmpty;
-import static org.openmetadata.service.Entity.FIELD_OWNER;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -37,7 +36,7 @@ public class ResourceRegistry {
     mapFieldOperation(MetadataOperation.EDIT_DESCRIPTION, Entity.FIELD_DESCRIPTION);
     mapFieldOperation(MetadataOperation.EDIT_DISPLAY_NAME, Entity.FIELD_DISPLAY_NAME);
     mapFieldOperation(MetadataOperation.EDIT_TAGS, Entity.FIELD_TAGS);
-    mapFieldOperation(MetadataOperation.EDIT_OWNER, FIELD_OWNER);
+    mapFieldOperation(MetadataOperation.EDIT_OWNER, Entity.FIELD_OWNER);
     mapFieldOperation(MetadataOperation.EDIT_CUSTOM_FIELDS, "extension");
     mapFieldOperation(MetadataOperation.EDIT_USERS, "users");
     mapFieldOperation(MetadataOperation.EDIT_ROLE, "defaultRoles");
@@ -54,19 +53,43 @@ public class ResourceRegistry {
 
   private ResourceRegistry() {}
 
-  public static void addResource(String resourceName, List<MetadataOperation> entitySpecificOperations) {
+  public static void addResource(
+      String resourceName, List<MetadataOperation> entitySpecificOperations, List<String> entityFields) {
     // If resourceName already exists, then no need to add the resource again
     if (RESOURCE_DESCRIPTORS.stream().anyMatch(d -> d.getName().equals(resourceName))) {
       return;
     }
+    ResourceDescriptor resourceDescriptor =
+        new ResourceDescriptor()
+            .withName(resourceName)
+            .withOperations(getOperations(resourceName, entitySpecificOperations, entityFields));
+    RESOURCE_DESCRIPTORS.sort(Comparator.comparing(ResourceDescriptor::getName));
+    RESOURCE_DESCRIPTORS.add(resourceDescriptor);
+  }
+
+  private static List<MetadataOperation> getOperations(
+      String resourceName, List<MetadataOperation> entitySpecificOperations, List<String> entityFields) {
     Set<MetadataOperation> operations = new TreeSet<>(COMMON_OPERATIONS);
     if (!nullOrEmpty(entitySpecificOperations)) {
       operations.addAll(entitySpecificOperations);
     }
-    ResourceDescriptor resourceDescriptor =
-        new ResourceDescriptor().withName(resourceName).withOperations(new ArrayList<>(operations));
-    RESOURCE_DESCRIPTORS.sort(Comparator.comparing(ResourceDescriptor::getName));
-    RESOURCE_DESCRIPTORS.add(resourceDescriptor);
+    // Set up operations based on common fields present in an entity
+    if (entityFields.contains(Entity.FIELD_TAGS)) {
+      operations.add(MetadataOperation.EDIT_TAGS);
+    }
+    if (entityFields.contains(Entity.FIELD_OWNER)) {
+      operations.add(MetadataOperation.EDIT_OWNER);
+    }
+    if (entityFields.contains(Entity.FIELD_EXTENSION)) {
+      operations.add(MetadataOperation.EDIT_CUSTOM_FIELDS);
+    }
+    if (entityFields.contains("roles") || entityFields.contains("defaultRoles")) {
+      operations.add(MetadataOperation.EDIT_ROLE);
+    }
+    if (entityFields.contains("reviewers")) {
+      operations.add(MetadataOperation.EDIT_REVIEWERS);
+    }
+    return new ArrayList<>(operations);
   }
 
   public static List<ResourceDescriptor> listResourceDescriptors() {
