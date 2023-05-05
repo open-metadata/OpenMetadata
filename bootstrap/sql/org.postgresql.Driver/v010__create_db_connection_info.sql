fd76faf88e2e874fd48357145cab3155af6b3bf1
Nahuel Verdugo Revigliono <nahuel@getcollate.io>
2023-05-05 16:58:47 +0200

    Fix postgres migration

--- bootstrap/sql/org.postgresql.Driver/v010__create_db_connection_info.sql
+++ bootstrap/sql/org.postgresql.Driver/v010__create_db_connection_info.sql
@@ -3,4 +3,4 @@ UPDATE messaging_service_entity
 SET json = JSONB_SET(json::jsonb, '{connection,config,saslMechanism}', '"PLAIN"')
 WHERE (servicetype = 'Kafka' OR serviceType = 'Redpanda')
   AND json#>'{connection,config,saslMechanism}' IS NOT NULL
-  AND json#>'{connection,config,saslMechanism}' NOT IN ('GSSAPI', 'PLAIN', 'SCRAM-SHA-256', 'SCRAM-SHA-512', 'OAUTHBEARER');
+  AND json#>'{connection,config,saslMechanism}' NOT IN ('"GSSAPI"', '"PLAIN"', '"SCRAM-SHA-256"', '"SCRAM-SHA-512"', '"OAUTHBEARER"');
