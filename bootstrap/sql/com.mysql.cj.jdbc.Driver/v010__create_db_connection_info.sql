b22192575d53362f2576ef67ecf89eecc5558825
Nahuel <nahuel@getcollate.io>
2023-05-08 09:35:49 +0200

    Fix#11216: Use Enum for SASL Mechanism for Kafka and Redpanda connections (#11385)
    * Fix#11216: Use ENUM for SASL Mechanism for Kafka and Redpanda connections
    
    * Minor change
    
    * Address PR comments
    
    * Fix postgres migration

---
+++ bootstrap/sql/com.mysql.cj.jdbc.Driver/v010__create_db_connection_info.sql
@@ -0,0 +1,6 @@
+-- Updating the value of SASL Mechanism for Kafka and Redpanda connections
+UPDATE messaging_service_entity
+SET json = JSON_REPLACE(json, '$.connection.config.saslMechanism', 'PLAIN')
+WHERE (serviceType = 'Kafka' OR serviceType = 'Redpanda')
+  AND JSON_EXTRACT(json, '$.connection.config.saslMechanism') IS NOT NULL
+  AND JSON_EXTRACT(json, '$.connection.config.saslMechanism') NOT IN ('GSSAPI', 'PLAIN', 'SCRAM-SHA-256', 'SCRAM-SHA-512', 'OAUTHBEARER');
