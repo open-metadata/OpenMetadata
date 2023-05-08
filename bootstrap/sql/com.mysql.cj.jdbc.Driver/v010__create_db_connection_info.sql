72b64144789ef99bc1e8f823f28fdf3e0742f21d
Ayush Shah <ayush@getcollate.io>
2023-05-08 13:45:06 +0530

    Add dynamodb migration

--- bootstrap/sql/com.mysql.cj.jdbc.Driver/v010__create_db_connection_info.sql
+++ bootstrap/sql/com.mysql.cj.jdbc.Driver/v010__create_db_connection_info.sql
@@ -4,3 +4,9 @@ SET json = JSON_REPLACE(json, '$.connection.config.saslMechanism', 'PLAIN')
 WHERE (serviceType = 'Kafka' OR serviceType = 'Redpanda')
   AND JSON_EXTRACT(json, '$.connection.config.saslMechanism') IS NOT NULL
   AND JSON_EXTRACT(json, '$.connection.config.saslMechanism') NOT IN ('GSSAPI', 'PLAIN', 'SCRAM-SHA-256', 'SCRAM-SHA-512', 'OAUTHBEARER');
+
+
+-- Update DynamoDB Test Connection step
+UPDATE test_connection_definition
+SET json = JSON_SET(json, '$.steps[0].name', 'ListTables')
+WHERE name = "DynamoDB";
--- bootstrap/sql/org.postgresql.Driver/v010__create_db_connection_info.sql
+++ bootstrap/sql/org.postgresql.Driver/v010__create_db_connection_info.sql
@@ -4,3 +4,8 @@ SET json = JSONB_SET(json::jsonb, '{connection,config,saslMechanism}', '"PLAIN"'
 WHERE (servicetype = 'Kafka' OR serviceType = 'Redpanda')
   AND json#>'{connection,config,saslMechanism}' IS NOT NULL
   AND json#>'{connection,config,saslMechanism}' NOT IN ('"GSSAPI"', '"PLAIN"', '"SCRAM-SHA-256"', '"SCRAM-SHA-512"', '"OAUTHBEARER"');
+
+-- Update DynamoDB Test Connection step
+UPDATE test_connection_definition
+SET json = JSONB_SET(json::jsonb, '{steps, 0, name}', '"ListTables"')
+WHERE name = 'DynamoDB';
