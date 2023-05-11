cff403a05a72dce5d461759bd9b9ba800d8676ad
Onkar Ravgan <onkar.10r@gmail.com>
2023-05-11 21:34:55 +0530

    Validate if tags are created before attaching them to CreateRequest (#11554)
    * Added tags validation
    
    * typo fixed

--- ingestion/tests/unit/test_dbt.py
+++ ingestion/tests/unit/test_dbt.py
@@ -296,9 +296,24 @@ class DbtUnitTest(TestCase):
 
     @patch("metadata.ingestion.source.database.dbt.metadata.DbtSource.get_dbt_owner")
     @patch("metadata.ingestion.ometa.mixins.es_mixin.ESMixin.es_search_from_fqn")
-    def test_dbt_manifest_v8(self, es_search_from_fqn, get_dbt_owner):
+    @patch("metadata.utils.tag_utils._get_tag_label")
+    def test_dbt_manifest_v8(self, _get_tag_label, es_search_from_fqn, get_dbt_owner):
         get_dbt_owner.return_value = MOCK_OWNER
         es_search_from_fqn.return_value = MOCK_TABLE_ENTITIES
+        _get_tag_label.side_effect = [
+            TagLabel(
+                tagFQN="dbtTags.model_tag_one",
+                labelType=LabelType.Automated.value,
+                state=State.Suggested.value,
+                source=TagSource.Classification.value,
+            ),
+            TagLabel(
+                tagFQN="dbtTags.model_tag_two",
+                labelType=LabelType.Automated.value,
+                state=State.Suggested.value,
+                source=TagSource.Classification.value,
+            ),
+        ]
         self.execute_test(
             MOCK_SAMPLE_MANIFEST_V8,
             expected_records=2,
@@ -324,7 +339,29 @@ class DbtUnitTest(TestCase):
         self.assertIsNone(self.dbt_source_obj.get_corrected_name(name="null"))
         self.assertIsNotNone(self.dbt_source_obj.get_corrected_name(name="dev"))
 
-    def test_dbt_get_dbt_tag_labels(self):
+    @patch("metadata.utils.tag_utils._get_tag_label")
+    def test_dbt_get_dbt_tag_labels(self, _get_tag_label):
+        _get_tag_label.side_effect = [
+            TagLabel(
+                tagFQN="dbtTags.tag1",
+                labelType=LabelType.Automated.value,
+                state=State.Suggested.value,
+                source=TagSource.Classification.value,
+            ),
+            TagLabel(
+                tagFQN='dbtTags."tag2.name"',
+                labelType=LabelType.Automated.value,
+                state=State.Suggested.value,
+                source=TagSource.Classification.value,
+            ),
+            TagLabel(
+                tagFQN="dbtTags.tag3",
+                labelType=LabelType.Automated.value,
+                state=State.Suggested.value,
+                source=TagSource.Classification.value,
+            ),
+        ]
+
         mocked_metadata = MagicMock()
         result = tag_utils.get_tag_labels(
             metadata=mocked_metadata,
