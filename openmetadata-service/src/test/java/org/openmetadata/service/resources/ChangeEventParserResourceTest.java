8721be8fd4213835ebe9144faeed1a4211ba782b
mohitdeuex <mohit.y@deuexsolutions.com>
2023-05-16 18:30:55 +0530

    Fix Message Decorator type

--- openmetadata-service/src/test/java/org/openmetadata/service/resources/ChangeEventParserResourceTest.java
+++ openmetadata-service/src/test/java/org/openmetadata/service/resources/ChangeEventParserResourceTest.java
@@ -55,8 +55,8 @@ import org.openmetadata.service.util.JsonUtils;
 class ChangeEventParserResourceTest extends OpenMetadataApplicationTest {
   private Table TABLE;
 
-  public static MessageDecorator feedMessageFormatter = new FeedMessageDecorator();
-  public static MessageDecorator slackMessageFormatter = new SlackMessageDecorator();
+  public static MessageDecorator<?> feedMessageFormatter = new FeedMessageDecorator();
+  public static MessageDecorator<?> slackMessageFormatter = new SlackMessageDecorator();
 
   @BeforeAll
   public void setup(TestInfo test) throws IOException, URISyntaxException {
