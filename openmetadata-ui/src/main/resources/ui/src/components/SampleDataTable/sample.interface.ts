fafe54a8f33a62bc703482d9778f2c90dc19e925
Ashish Gupta <ashish@getcollate.io>
2023-05-19 19:15:05 +0530

    fixes #11490 : scrolling issue in Sample data (#11678)
    * fix scrolling issue in Sample data
    
    * chanegs as per comments

--- openmetadata-ui/src/main/resources/ui/src/components/SampleDataTable/sample.interface.ts
+++ openmetadata-ui/src/main/resources/ui/src/components/SampleDataTable/sample.interface.ts
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2022 Collate.
+ *  Copyright 2023 Collate.
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
@@ -11,11 +11,23 @@
  *  limitations under the License.
  */
 
-import { SampleDataType } from 'components/SampleDataTable/sample.interface';
-import { HTMLAttributes } from 'react';
+import { ColumnsType } from 'antd/lib/table';
 
-export interface SchemaModalProp extends HTMLAttributes<HTMLDivElement> {
-  onClose: () => void;
-  data: SampleDataType;
-  visible: boolean;
+export type SampleDataType =
+  | string
+  | number
+  | null
+  | Record<string, unknown>
+  | unknown[];
+
+type RecordProps = Record<string, SampleDataType>;
+
+export interface SampleData {
+  columns?: ColumnsType<RecordProps>;
+  rows?: RecordProps[];
+}
+
+export interface SampleDataProps {
+  isTableDeleted?: boolean;
+  tableId: string;
 }
