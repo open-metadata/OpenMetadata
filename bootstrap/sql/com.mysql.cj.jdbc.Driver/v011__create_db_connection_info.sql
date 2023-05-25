2f071466458d023254700c7ed66f8c42bf1b394c
Onkar Ravgan <onkar.10r@gmail.com>
2023-05-25 20:09:08 +0530

    Added tableau enum migration

--- bootstrap/sql/com.mysql.cj.jdbc.Driver/v011__create_db_connection_info.sql
+++ bootstrap/sql/com.mysql.cj.jdbc.Driver/v011__create_db_connection_info.sql
@@ -2,3 +2,8 @@
 UPDATE metadata_service_entity
 SET json = JSON_REMOVE(json, '$.openMetadataServerConnection.secretsManagerCredentials')
 where name = 'OpenMetadata';
+
+-- Update the tableau data model enum
+UPDATE dashboard_data_model_entity 
+SET json = JSON_SET(json, '$.dataModelType', 'TableauDataModel')
+WHERE JSON_EXTRACT(json, '$.dataModelType') = 'TableauSheet';
--- bootstrap/sql/org.postgresql.Driver/v011__create_db_connection_info.sql
+++ bootstrap/sql/org.postgresql.Driver/v011__create_db_connection_info.sql
@@ -2,3 +2,8 @@
 UPDATE metadata_service_entity
 SET json = json::jsonb #- '{openMetadataServerConnection.secretsManagerCredentials}'
 where name = 'OpenMetadata';
+
+-- Update the tableau data model enum
+UPDATE dashboard_data_model_entity
+SET json = JSONB_SET(json::jsonb, '{dataModelType}', '"TableauDataModel"')
+WHERE json#>'{dataModelType}' = '"TableauSheet"';
