c857a241e38afd95e2891805a693434aa6849f42
Pere Miquel Brull <peremiquelbrull@gmail.com>
2023-05-31 16:06:26 +0200

    Update Credentials

---
+++ openmetadata-service/src/main/java/org/openmetadata/service/secrets/converter/LookerConnectionClassConverter.java
@@ -0,0 +1,25 @@
+package org.openmetadata.service.secrets.converter;
+import org.openmetadata.schema.security.credentials.BitBucketCredentials;
+import org.openmetadata.schema.security.credentials.GitHubCredentials;
+import org.openmetadata.schema.services.connections.dashboard.LookerConnection;
+import org.openmetadata.service.util.JsonUtils;
+
+import java.util.List;
+
+public class LookerConnectionClassConverter extends ClassConverter {
+
+  private static final List<Class<?>> CREDENTIALS_CLASSES = List.of(GitHubCredentials.class, BitBucketCredentials.class);
+
+  public LookerConnectionClassConverter() {
+    super(LookerConnection.class);
+  }
+
+  @Override
+  public Object convert(Object object) {
+    LookerConnection lookerConnection = (LookerConnection) JsonUtils.convertValue(object, this.clazz);
+
+    tryToConvertOrFail(lookerConnection.getGitCredentials(), CREDENTIALS_CLASSES).ifPresent(lookerConnection::setGitCredentials);
+
+    return lookerConnection;
+  }
+}
