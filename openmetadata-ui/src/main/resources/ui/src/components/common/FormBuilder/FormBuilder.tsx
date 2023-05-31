fd36af4d03a33aeff62f7220d57e315698d6f645
Sachin Chaurasiya <sachinchaurasiyachotey87@gmail.com>
2023-05-31 16:22:11 +0530

    chore(ui): hide json schema fields default description

--- openmetadata-ui/src/main/resources/ui/src/components/common/FormBuilder/FormBuilder.tsx
+++ openmetadata-ui/src/main/resources/ui/src/components/common/FormBuilder/FormBuilder.tsx
@@ -16,7 +16,7 @@ import Form, { FormProps, IChangeEvent } from '@rjsf/core';
 import { Button } from 'antd';
 import classNames from 'classnames';
 import { ArrayFieldTemplate } from 'components/JSONSchemaTemplate/ArrayFieldTemplate';
-import { customFields } from 'components/JSONSchemaTemplate/CustomFields';
+import DescriptionFieldTemplate from 'components/JSONSchemaTemplate/DescriptionFieldTemplate';
 import { ObjectFieldTemplate } from 'components/JSONSchemaTemplate/ObjectFieldTemplate';
 import { ServiceCategory } from 'enums/service.enum';
 import { useAirflowStatus } from 'hooks/useAirflowStatus';
@@ -112,7 +112,6 @@ const FormBuilder: FunctionComponent<Props> = ({
       className={classNames('rjsf', props.className, {
         'no-header': !showFormHeader,
       })}
-      fields={customFields}
       formContext={{ handleFocus: onFocus }}
       formData={localFormData}
       idSeparator="/"
@@ -122,6 +121,7 @@ const FormBuilder: FunctionComponent<Props> = ({
       templates={{
         ArrayFieldTemplate: ArrayFieldTemplate,
         ObjectFieldTemplate: ObjectFieldTemplate,
+        DescriptionFieldTemplate: DescriptionFieldTemplate,
       }}
       transformErrors={transformErrors}
       uiSchema={uiSchema}
