b4fa31fbe4ac35c1a50d16821028337ab2786254
Sriharsha Chintalapani <harsha@getcollate.io>
2023-06-02 08:10:42 -0700

    fix rebase

--- openmetadata-service/src/main/java/org/openmetadata/service/util/UserUtil.java
+++ openmetadata-service/src/main/java/org/openmetadata/service/util/UserUtil.java
@@ -89,7 +89,7 @@ public final class UserUtil {
     } catch (EntityNotFoundException e) {
       updatedUser = user(username, domain, username).withIsAdmin(isAdmin).withIsEmailVerified(true);
       // Update Auth Mechanism if not present, and send mail to the user
-      if (providerType.equals(SSOAuthMechanism.SsoServiceType.BASIC.value())) {
+      if (Objects.equals(authProvider.value(), SSOAuthMechanism.SsoServiceType.BASIC.value())) {
         updateUserWithHashedPwd(updatedUser, getPassword());
         EmailUtil.sendInviteMailToAdmin(updatedUser, ADMIN_USER_NAME);
       }
