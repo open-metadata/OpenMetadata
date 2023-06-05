4d25645c727ace1a89c52778a381684cb13c025e
mohitdeuex <mohit.y@deuexsolutions.com>
2023-06-05 16:28:46 +0530

    Change Table data

--- bootstrap/sql/com.mysql.cj.jdbc.Driver/v012__create_db_connection_info.sql
+++ bootstrap/sql/com.mysql.cj.jdbc.Driver/v012__create_db_connection_info.sql
@@ -13,14 +13,13 @@ SET json = JSON_INSERT(
 WHERE serviceType = 'Looker'
   AND JSON_EXTRACT(json, '$.connection.config.githubCredentials') IS NOT NULL;
 
-CREATE TABLE IF NOT EXISTS knowledge_asset_entity (
-    id VARCHAR(36) GENERATED ALWAYS AS (json ->> '$.id') STORED NOT NULL,
-    name VARCHAR(256) GENERATED ALWAYS AS (json ->> '$.name') NOT NULL,
+--
+-- Used for storing additional docs data with Extension and different Schemas
+--
+CREATE TABLE IF NOT EXISTS doc_store (
+    id VARCHAR(36) NOT NULL,                    -- ID of the from entity
+    extension VARCHAR(256) NOT NULL,            -- Extension name same as entity.fieldName
+    jsonSchema VARCHAR(256) NOT NULL,           -- Schema used for generating JSON
     json JSON NOT NULL,
-    updatedAt BIGINT UNSIGNED GENERATED ALWAYS AS (json ->> '$.updatedAt') NOT NULL,
-    updatedBy VARCHAR(256) GENERATED ALWAYS AS (json ->> '$.updatedBy') NOT NULL,
-    deleted BOOLEAN GENERATED ALWAYS AS (json -> '$.deleted'),
-    PRIMARY KEY (id),
-    UNIQUE(name),
-    INDEX name_index (name)
+    PRIMARY KEY (id, extension)
 );
--- bootstrap/sql/org.postgresql.Driver/v012__create_db_connection_info.sql
+++ bootstrap/sql/org.postgresql.Driver/v012__create_db_connection_info.sql
@@ -8,13 +8,14 @@ UPDATE dashboard_service_entity
 SET json = jsonb_set(json, '{connection,config,gitCredentials}', json#>'{connection,config,githubCredentials}')
     where serviceType = 'Looker'
   and json#>'{connection,config,githubCredentials}' is not null;
-CREATE TABLE IF NOT EXISTS knowledge_asset_entity (
-    id VARCHAR(36) GENERATED ALWAYS AS (json ->> 'id') STORED NOT NULL,
-    name VARCHAR(256) GENERATED ALWAYS AS (json ->> 'name') STORED NOT NULL,
+  
+--
+-- Used for storing additional docs data with Extension and different Schemas
+--
+CREATE TABLE IF NOT EXISTS doc_store (
+    id VARCHAR(36) NOT NULL,                    -- ID of the from entity
+    extension VARCHAR(256) NOT NULL,            -- Extension name same as entity.fieldName
+    jsonSchema VARCHAR(256) NOT NULL,           -- Schema used for generating JSON
     json JSONB NOT NULL,
-    updatedAt BIGINT GENERATED ALWAYS AS ((json ->> 'updatedAt')::bigint) STORED NOT NULL,
-    updatedBy VARCHAR(256) GENERATED ALWAYS AS (json ->> 'updatedBy') STORED NOT NULL,
-    deleted BOOLEAN GENERATED ALWAYS AS ((json ->> 'deleted')::boolean) STORED,
-    PRIMARY KEY (id),
-    UNIQUE (name)  
+    PRIMARY KEY (id, extension)
 );
