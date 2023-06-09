452e782db05ffc97cfb2636453f78a632c3da927
Pere Miquel Brull <peremiquelbrull@gmail.com>
2023-06-09 14:49:42 +0200

    Truncate test_connection_definition migration

--- bootstrap/sql/org.postgresql.Driver/v012__create_db_connection_info.sql
+++ bootstrap/sql/org.postgresql.Driver/v012__create_db_connection_info.sql
@@ -2,3 +2,6 @@
 ALTER TABLE test_connection_definition
 ADD fullyQualifiedName VARCHAR(256) GENERATED ALWAYS AS (json ->> 'fullyQualifiedName') STORED NOT NULL,
 DROP COLUMN name;
+
+-- Since we are not deleting the test connection defs anymore, clean it up
+TRUNCATE test_connection_definition;
