1aae06840a05c4fc4d0867eefa6dbc1007b8781e
Pere Miquel Brull <peremiquelbrull@gmail.com>
2023-06-11 03:38:05 +0200

    Align bootstrap after 1.0.4 patch (#11932)
    * Align bootstrap after 1.0.4 patch
    
    * Align v012 script

--- bootstrap/sql/com.mysql.cj.jdbc.Driver/v012__create_db_connection_info.sql
+++ bootstrap/sql/com.mysql.cj.jdbc.Driver/v012__create_db_connection_info.sql
@@ -1,46 +1,7 @@
--- we are not using the secretsManagerCredentials
-UPDATE metadata_service_entity
-SET json = JSON_REMOVE(json, '$.openMetadataServerConnection.secretsManagerCredentials')
-where name = 'OpenMetadata';
-
--- Rename githubCredentials to gitCredentials
-UPDATE dashboard_service_entity
-SET json = JSON_INSERT(
-        JSON_REMOVE(json, '$.connection.config.githubCredentials'),
-        '$.connection.config.gitCredentials',
-        JSON_EXTRACT(json, '$.connection.config.githubCredentials')
-    )
-WHERE serviceType = 'Looker'
-  AND JSON_EXTRACT(json, '$.connection.config.githubCredentials') IS NOT NULL;
-
-
--- Rename gcsConfig in BigQuery to gcpConfig
-UPDATE dbservice_entity
-SET json = JSON_INSERT(
-    JSON_REMOVE(json, '$.connection.config.credentials.gcsConfig'),
-    '$.connection.config.credentials.gcpConfig',
-    JSON_EXTRACT(json, '$.connection.config.credentials.gcsConfig')
-) where serviceType in ('BigQuery');
-
--- Rename gcsConfig in Datalake to gcpConfig
-UPDATE dbservice_entity
-SET json = JSON_INSERT(
-    JSON_REMOVE(json, '$.connection.config.configSource.securityConfig.gcsConfig'),
-    '$.connection.config.configSource.securityConfig.gcpConfig',
-    JSON_EXTRACT(json, '$.connection.config.configSource.securityConfig.gcsConfig')
-) where serviceType in ('Datalake');
-
-
--- Rename gcsConfig in dbt to gcpConfig
-UPDATE ingestion_pipeline_entity
-SET json = JSON_INSERT(
-    JSON_REMOVE(json, '$.sourceConfig.config.dbtConfigSource.dbtSecurityConfig.gcsConfig'),
-    '$.sourceConfig.config.dbtConfigdbtSecurityConfig.gcpConfig',
-    JSON_EXTRACT(json, '$.sourceConfig.config.dbtConfigSource.dbtSecurityConfig.gcsConfig')
-)
-WHERE json -> '$.sourceConfig.config.type' = 'DBT';
-
 -- use FQN instead of name for Test Connection Definition
 ALTER TABLE test_connection_definition
 ADD fullyQualifiedName VARCHAR(256) GENERATED ALWAYS AS (json ->> '$.fullyQualifiedName') NOT NULL,
 DROP COLUMN name;
+
+-- Since we are not deleting the test connection defs anymore, clean it up
+TRUNCATE TABLE test_connection_definition;
