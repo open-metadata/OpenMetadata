a2b6de34f6b773adb72d6f13ad0b21f2402fec2b
Ayush Shah <ayush@getcollate.io>
2023-06-14 14:18:00 +0530

    Add Test

---
+++ ingestion/tests/unit/test.py
@@ -0,0 +1,23 @@
+
+from unittest import TestCase
+from os import walk
+from metadata.ingestion.api.parser import parse_workflow_config_gracefully
+try:
+    import importlib.resources as pkg_resources
+except ImportError:
+    import pkg_resources
+import yaml
+class TestWorkflowParse(TestCase):
+    """
+    Test parsing scenarios of JSON Schemas
+    """
+
+    def test_get_service_type(self):
+        package_path = f'{pkg_resources.files("metadata")}/examples/workflows'
+        workflow_files = [files for _,_,files in walk(package_path)]
+        for yaml_file in workflow_files[0]:
+            with self.subTest(file_name=yaml_file):
+                with open(f"{package_path}/{yaml_file}","r") as file:
+                    file_content = file.read()
+                    self.assertTrue(parse_workflow_config_gracefully(yaml.safe_load(file_content)))
+                    file.close()
