3f01ee938fe1b025cf3ff14e31013da5ac85fbd0
Teddy <teddy.crepineau@gmail.com>
2023-06-15 21:27:54 +0200

    Fixes #11895 - Add Indexing and Search logic for TestCases (#11989)
    * feat: added logic to delete logical test suite + added check to prevent adding existing testCases to executable test suite
    
    * feat: added elasticsearch index logic for testCases
    
    * feat: added deletion logic from index logic when deleting test suites
    
    * feat: added test case index search to  endpoint
    
    * feat: add executable/logical filter in list testSuite + filterOut tables without tests in Table resource
    
    * feat: added summary field to testSuite
    
    * feat: added executionSummary endpoint for test cases
    
    * feat: removed tick marks around timestamp
    
    * feat: addressed test failures
    
    * feat: ran python linting
    
    * feat: add limit to fetch all tables in TableResource testSuite test
    
    * feat: fix conflict
    
    * feat: ran java checkstyle
    
    * feat: fixed mongo linting + disabled mongo failing tests
    
    * feat: removed mongo test skip
    
    * feat: removed unsued pytest import

---
+++ openmetadata-service/src/main/java/org/openmetadata/service/elasticsearch/TestCaseIndex.java
@@ -0,0 +1,48 @@
+package org.openmetadata.service.elasticsearch;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import org.openmetadata.schema.tests.TestCase;
+import org.openmetadata.schema.tests.TestSuite;
+import org.openmetadata.schema.type.EntityReference;
+import org.openmetadata.schema.type.Include;
+import org.openmetadata.service.Entity;
+import org.openmetadata.service.util.JsonUtils;
+
+public class TestCaseIndex implements ElasticSearchIndex {
+  TestCase testCase;
+
+  public TestCaseIndex(TestCase testCase) {
+    this.testCase = testCase;
+  }
+
+  public Map<String, Object> buildESDoc() {
+    return JsonUtils.getMap(testCase);
+  }
+
+  public Map<String, Object> buildESDocForCreate() throws IOException {
+    EntityReference testSuiteEntityReference = testCase.getTestSuite();
+    TestSuite testSuite = getTestSuite(testSuiteEntityReference.getId());
+    List<TestSuite> testSuiteArray = new ArrayList<>();
+    testSuiteArray.add(testSuite);
+    Map<String, Object> doc = JsonUtils.getMap(testCase);
+    doc.put("testSuite", testSuiteArray);
+    return doc;
+  }
+
+  private TestSuite getTestSuite(UUID testSuiteId) throws IOException {
+    TestSuite testSuite = Entity.getEntity(Entity.TEST_SUITE, testSuiteId, "", Include.ALL);
+    return new TestSuite()
+        .withId(testSuite.getId())
+        .withName(testSuite.getName())
+        .withDisplayName(testSuite.getDisplayName())
+        .withDescription(testSuite.getDescription())
+        .withFullyQualifiedName(testSuite.getFullyQualifiedName())
+        .withDeleted(testSuite.getDeleted())
+        .withHref(testSuite.getHref())
+        .withExecutable(testSuite.getExecutable());
+  }
+}
