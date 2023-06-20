46f86fbbe4064be1b498327dc311b40f3e794621
Pere Miquel Brull <peremiquelbrull@gmail.com>
2023-06-20 16:26:06 +0200

    Align tests with mysql auth changes

--- openmetadata-service/src/test/java/org/openmetadata/service/secrets/SecretsManagerLifecycleTest.java
+++ openmetadata-service/src/test/java/org/openmetadata/service/secrets/SecretsManagerLifecycleTest.java
@@ -19,8 +19,10 @@ import org.openmetadata.schema.entity.automations.Workflow;
 import org.openmetadata.schema.entity.automations.WorkflowType;
 import org.openmetadata.schema.entity.services.ServiceType;
 import org.openmetadata.schema.services.connections.database.MysqlConnection;
+import org.openmetadata.schema.services.connections.database.common.basicAuth;
 import org.openmetadata.service.exception.SecretsManagerException;
 import org.openmetadata.service.fernet.Fernet;
+import org.openmetadata.service.util.JsonUtils;
 
 @ExtendWith(MockitoExtension.class)
 public class SecretsManagerLifecycleTest {
@@ -44,22 +46,23 @@ public class SecretsManagerLifecycleTest {
   @Test
   void testDatabaseServiceConnectionConfigLifecycle() {
     String password = "openmetadata-test";
-    String secretName = "/openmetadata/database/test/password";
+    String secretName = "/openmetadata/database/test/authtype/password";
     String connectionName = "test";
-    Map<String, String> mysqlConnection = Map.of("password", password);
+    Map<String, Map<String, String>> mysqlConnection = Map.of("authType", Map.of("password", password));
 
     // Ensure encrypted service connection config encrypts the password
     MysqlConnection actualConnection =
         (MysqlConnection)
             secretsManager.encryptServiceConnectionConfig(
                 mysqlConnection, Mysql.value(), connectionName, ServiceType.DATABASE);
-    assertNotEquals(password, actualConnection.getPassword());
+    assertNotEquals(password, JsonUtils.convertValue(actualConnection.getAuthType(), basicAuth.class).getPassword());
 
     // Decrypt the encrypted password and validate
     actualConnection =
         (MysqlConnection)
             secretsManager.decryptServiceConnectionConfig(actualConnection, Mysql.value(), ServiceType.DATABASE);
-    assertEquals(DECRYPTED_VALUE, actualConnection.getPassword());
+    assertEquals(
+        DECRYPTED_VALUE, JsonUtils.convertValue(actualConnection.getAuthType(), basicAuth.class).getPassword());
 
     // SM will have the key stored
     String secretValue = secretsManager.getSecret(secretName);
@@ -79,7 +82,7 @@ public class SecretsManagerLifecycleTest {
   @Test
   void testWorkflowLifecycle() {
     String password = "openmetadata_password";
-    String secretName = "/openmetadata/workflow/test-connection/request/connection/config/password";
+    String secretName = "/openmetadata/workflow/test-connection/request/connection/config/authtype/password";
 
     Workflow workflow =
         new Workflow()
@@ -95,19 +98,19 @@ public class SecretsManagerLifecycleTest {
                                 new MysqlConnection()
                                     .withHostPort("mysql:3306")
                                     .withUsername("openmetadata_user")
-                                    .withPassword(password))));
+                                    .withAuthType(new basicAuth().withPassword(password)))));
 
     Workflow encrypted = secretsManager.encryptWorkflow(workflow);
     TestServiceConnectionRequest encryptedRequest = (TestServiceConnectionRequest) encrypted.getRequest();
     DatabaseConnection encryptedConnection = (DatabaseConnection) encryptedRequest.getConnection();
     MysqlConnection encryptedConfig = (MysqlConnection) encryptedConnection.getConfig();
-    assertNotEquals(password, encryptedConfig.getPassword());
+    assertNotEquals(password, JsonUtils.convertValue(encryptedConfig.getAuthType(), basicAuth.class).getPassword());
 
     Workflow decrypted = secretsManager.decryptWorkflow(encrypted);
     TestServiceConnectionRequest decryptedRequest = (TestServiceConnectionRequest) decrypted.getRequest();
     DatabaseConnection decryptedConnection = (DatabaseConnection) decryptedRequest.getConnection();
     MysqlConnection decryptedConfig = (MysqlConnection) decryptedConnection.getConfig();
-    assertEquals(DECRYPTED_VALUE, decryptedConfig.getPassword());
+    assertEquals(DECRYPTED_VALUE, JsonUtils.convertValue(decryptedConfig.getAuthType(), basicAuth.class).getPassword());
 
     // SM will have the key stored
     String secretValue = secretsManager.getSecret(secretName);
