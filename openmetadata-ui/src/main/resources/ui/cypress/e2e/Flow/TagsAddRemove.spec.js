547719906a7795fc8de6e4efe2664229882ec9a0
Ashish Gupta <ashish@getcollate.io>
2023-06-21 23:03:15 +0530

    fix cypress issue

--- openmetadata-ui/src/main/resources/ui/cypress/e2e/Flow/TagsAddRemove.spec.js
+++ openmetadata-ui/src/main/resources/ui/cypress/e2e/Flow/TagsAddRemove.spec.js
@@ -38,8 +38,9 @@ const addTags = (tag, parent) => {
 const checkTags = (tag, checkForParentEntity) => {
   if (checkForParentEntity) {
     cy.get(
-      '[data-testid="entity-right-panel"]  [data-testid="tag-container"] [data-testid="entity-tags"] '
+      '[data-testid="entity-right-panel"]  [data-testid="tags-container"] [data-testid="entity-tags"] '
     )
+
       .scrollIntoView()
       .should('be.visible')
       .contains(tag);
@@ -48,9 +49,9 @@ const checkTags = (tag, checkForParentEntity) => {
   }
 };
 
-const removeTags = (checkForParentEntity, separate) => {
+const removeTags = (checkForParentEntity) => {
   if (checkForParentEntity) {
-    cy.get('[data-testid="entity-right-panel"] [data-testid="edit-button"] ')
+    cy.get('[data-testid="entity-right-panel"] [data-testid="edit-button"]')
       .scrollIntoView()
       .should('be.visible')
       .click();
