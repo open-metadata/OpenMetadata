33c752d2f5a73d02e758aa87a2e24ae4fe5d8942
Sriharsha Chintalapani <harshach@users.noreply.github.com>
2023-06-26 00:33:44 -0700

    interfaces for db migrations (#12057)
    * interfaces for db migrations
    
    * Add Data Migration for entities to 1.1
    
    * Migration for FQN hash
    
    * Added Tag Usage And Fixed some transaction issue in testing
    
    * Added SERVER_CHANGE_LOG validations
    
    * Update Path for files
    
    * Added Postgres Migration
    
    * Test Suite Migration [WIP]
    
    * remove ingestion pipeline
    
    * moved Migration to Table Initializer
    
    * init Data source config in TableInitializer
    
    * make checksum ignore configurable
    
    ---------
    
    Co-authored-by: mohitdeuex <mohit.y@deuexsolutions.com>

---
+++ openmetadata-service/src/main/java/org/openmetadata/service/migration/MigrationFile.java
@@ -0,0 +1,12 @@
+package org.openmetadata.service.migration;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.CONSTRUCTOR})
+public @interface MigrationFile {
+  String name();
+}
