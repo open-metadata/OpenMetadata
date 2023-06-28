df7f5a7309eb33866c053397d83df5808ac3ff4a
Sachin Chaurasiya <sachinchaurasiyachotey87@gmail.com>
2023-06-28 14:18:24 +0530

    feat(ui): show table constraint on table details page (#12163)
    * feat(ui): show table constraint on table details page
    
    * chore: sync locale
    
    ---------
    
    Co-authored-by: Chirag Madlani <12962843+chirag-madlani@users.noreply.github.com>

--- openmetadata-ui/src/main/resources/ui/src/pages/TableDetailsPageV1/TableDetailsPageV1.tsx
+++ openmetadata-ui/src/main/resources/ui/src/pages/TableDetailsPageV1/TableDetailsPageV1.tsx
@@ -87,6 +87,7 @@ import { getTagsWithoutTier, getTierTags } from 'utils/TableUtils';
 import { showErrorToast, showSuccessToast } from 'utils/ToastUtils';
 import { FrequentlyJoinedTables } from './FrequentlyJoinedTables/FrequentlyJoinedTables.component';
 import './table-details-page-v1.less';
+import TableConstraints from './TableConstraints/TableConstraints';
 
 const TableDetailsPageV1 = () => {
   const { isTourOpen, activeTabForTourDatasetPage, isTourPage } =
@@ -519,6 +520,7 @@ const TableDetailsPageV1 = () => {
               onSelectionChange={handleTagSelection}
               onThreadLinkSelect={onThreadLinkSelect}
             />
+            <TableConstraints constraints={tableDetails?.tableConstraints} />
           </Space>
         </Col>
       </Row>
