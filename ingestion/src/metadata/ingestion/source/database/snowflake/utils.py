fef45cfeb16c6d4b95b589f078e423cdc88231fc
Vanshika Kabra <vanshikakabra@Vanshikas-MacBook-Pro.local>
2023-07-05 12:29:43 +0530

    Clean the code

--- ingestion/src/metadata/ingestion/source/database/snowflake/utils.py
+++ ingestion/src/metadata/ingestion/source/database/snowflake/utils.py
@@ -66,7 +66,6 @@ def get_table_names_reflection(self, schema=None, **kw):
 def get_table_names(self, connection, schema, **kw):
     query = SNOWFLAKE_GET_WITHOUT_TRANSIENT_TABLE_NAMES
     if kw.get("include_temp_tables"):
-        # query = SNOWFLAKE_GET_TABLE_NAMES
         query = SNOWFLAKE_GET_TRANSIENT_NAMES
 
     if kw.get("external_tables"):
