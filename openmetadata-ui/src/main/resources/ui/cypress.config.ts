ec765ce2e3e3883a6677b2ee6a9167e7212a2dd7
Chirag Madlani <12962843+chirag-madlani@users.noreply.github.com>
2023-07-19 15:01:30 +0530

    test(ui): e2e tests for recently viewed

--- openmetadata-ui/src/main/resources/ui/cypress.config.ts
+++ openmetadata-ui/src/main/resources/ui/cypress.config.ts
@@ -23,6 +23,7 @@ export default defineConfig({
   chromeWebSecurity: false,
   numTestsKeptInMemory: 0,
   e2e: {
+    testIsolation: false,
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
