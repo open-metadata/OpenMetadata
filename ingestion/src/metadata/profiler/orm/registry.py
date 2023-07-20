ab1ec50c2c7ac276d632cce9d3f234eedcfa6995
Ayush Shah <ayush@getcollate.io>
2023-07-20 13:34:35 +0530

    Fixes Mssql Ntext, text and Image (#12490)

--- ingestion/src/metadata/profiler/orm/registry.py
+++ ingestion/src/metadata/profiler/orm/registry.py
@@ -21,6 +21,7 @@ from metadata.generated.schema.entity.data.table import DataType
 from metadata.ingestion.source import sqa_types
 from metadata.profiler.orm.types.bytea_to_string import ByteaToHex
 from metadata.profiler.orm.types.custom_array import CustomArray
+from metadata.profiler.orm.types.custom_image import CustomImage
 from metadata.profiler.orm.types.custom_timestamp import CustomTimestamp
 from metadata.profiler.orm.types.hex_byte_string import HexByteString
 from metadata.profiler.orm.types.uuid import UUIDString
@@ -34,6 +35,7 @@ class CustomTypes(TypeRegistry):
     BYTEA = ByteaToHex
     ARRAY = CustomArray
     TIMESTAMP = CustomTimestamp
+    IMAGE = CustomImage
 
 
 class Dialects(Enum):
