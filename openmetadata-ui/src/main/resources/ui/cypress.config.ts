16af647931819d0dca2453398ceb0a4e3fe44dc0
Aniket Katkar <aniketkatkar97@gmail.com>
2023-07-28 09:40:12 +0530

    reverted the cypress config changes

--- openmetadata-ui/src/main/resources/ui/cypress.config.ts
+++ openmetadata-ui/src/main/resources/ui/cypress.config.ts
@@ -22,7 +22,6 @@ export default defineConfig({
   videoUploadOnPasses: false,
   chromeWebSecurity: false,
   numTestsKeptInMemory: 0,
-  experimentalMemoryManagement: true,
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
