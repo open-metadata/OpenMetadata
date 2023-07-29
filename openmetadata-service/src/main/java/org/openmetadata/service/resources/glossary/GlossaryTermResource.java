b7e32429115a5a34c7f6e3de9d7c1aa2945888c3
Suresh Srinivas <srini30005@gmail.com>
2023-07-29 11:42:28 -0700

    Make LoadingCache members final variables (#12648)
    * Make LoadingCache members final variables
    
    * Simplify singleton classes
    
    * Reuse Entity fields instead of declaring them again

--- openmetadata-service/src/main/java/org/openmetadata/service/resources/glossary/GlossaryTermResource.java
+++ openmetadata-service/src/main/java/org/openmetadata/service/resources/glossary/GlossaryTermResource.java
@@ -56,14 +56,12 @@ import org.openmetadata.schema.type.Include;
 import org.openmetadata.schema.type.MetadataOperation;
 import org.openmetadata.schema.utils.EntityInterfaceUtil;
 import org.openmetadata.service.Entity;
-import org.openmetadata.service.OpenMetadataApplicationConfig;
 import org.openmetadata.service.exception.CatalogExceptionMessage;
 import org.openmetadata.service.jdbi3.CollectionDAO;
 import org.openmetadata.service.jdbi3.GlossaryTermRepository;
 import org.openmetadata.service.jdbi3.ListFilter;
 import org.openmetadata.service.resources.Collection;
 import org.openmetadata.service.resources.EntityResource;
-import org.openmetadata.service.resources.tags.TagLabelCache;
 import org.openmetadata.service.security.Authorizer;
 import org.openmetadata.service.util.EntityUtil.Fields;
 import org.openmetadata.service.util.RestUtil;
@@ -89,11 +87,6 @@ public class GlossaryTermResource extends EntityResource<GlossaryTerm, GlossaryT
     Entity.withHref(uriInfo, term.getOwner());
     Entity.withHref(uriInfo, term.getDomain());
     return term;
-  }
-
-  @Override
-  public void initialize(OpenMetadataApplicationConfig config) {
-    TagLabelCache.initialize();
   }
 
   public GlossaryTermResource(CollectionDAO dao, Authorizer authorizer) {
--- openmetadata-service/src/main/java/org/openmetadata/service/search/EntityBuilderConstant.java
+++ openmetadata-service/src/main/java/org/openmetadata/service/search/EntityBuilderConstant.java
@@ -1,22 +1,10 @@
 package org.openmetadata.service.search;
 
 public class EntityBuilderConstant {
-  public static final String FIELD_OWNER = "owner";
-  public static final String FIELD_NAME = "name";
-  public static final String FIELD_DESCRIPTION = "description";
-  public static final String FIELD_FOLLOWERS = "followers";
-  public static final String FIELD_VOTES = "votes";
-  public static final String FIELD_TAGS = "tags";
-  public static final String FIELD_DELETED = "deleted";
-  public static final String FIELD_PIPELINE_STATUS = "pipelineStatus";
-  public static final String FIELD_DISPLAY_NAME = "displayName";
-  public static final String FIELD_EXTENSION = "extension";
-  public static final String FIELD_USAGE_SUMMARY = "usageSummary";
   public static final String UNIFIED = "unified";
   public static final String ES_MESSAGE_SCHEMA_FIELD = "messageSchema.schemaFields.name";
   public static final String ES_TAG_FQN_FIELD = "tags.tagFQN";
   public static final String NAME_KEYWORD = "name.keyword";
-  public static final String DISPLAY_NAME = "displayName";
   public static final String FIELD_NAME_NGRAM = "name.ngram";
   public static final String DISPLAY_NAME_KEYWORD = "displayName.keyword";
   public static final String FIELD_DISPLAY_NAME_NGRAM = "displayName.ngram";
@@ -25,7 +13,6 @@ public class EntityBuilderConstant {
   public static final String POST_TAG = "</span>";
   public static final Integer MAX_AGGREGATE_SIZE = 50;
   public static final Integer MAX_RESULT_HITS = 10000;
-  public static final String DESCRIPTION = "description";
   public static final String QUERY = "query";
   public static final String QUERY_NGRAM = "query.ngram";
 
