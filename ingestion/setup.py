6579d9fd48d5e9d140eabe5f87d6a4510aa079e6
Onkar Ravgan <onkar.10r@gmail.com>
2023-07-31 13:05:48 +0530

    Athena fixes

--- ingestion/setup.py
+++ ingestion/setup.py
@@ -120,7 +120,7 @@ base_requirements = {
 plugins: Dict[str, Set[str]] = {
     "airflow": {VERSIONS["airflow"]},  # Same as ingestion container. For development.
     "amundsen": {VERSIONS["neo4j"]},
-    "athena": {"PyAthena[SQLAlchemy]"},
+    "athena": {"pyathena==3.0.6"},
     "atlas": {},
     "azuresql": {VERSIONS["pyodbc"]},
     "azure-sso": {VERSIONS["msal"]},
