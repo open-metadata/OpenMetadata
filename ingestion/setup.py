27d89f3e8a6b127fc52b090e262226706c3a605e
Onkar Ravgan <onkar.10r@gmail.com>
2023-08-03 16:44:12 +0530

    bkp fix version bump

--- ingestion/setup.py
+++ ingestion/setup.py
@@ -264,7 +264,7 @@ test = {
 build_options = {"includes": ["_cffi_backend"]}
 setup(
     name="openmetadata-ingestion",
-    version="1.1.0.3",
+    version="1.1.0.5",
     url="https://open-metadata.org/",
     author="OpenMetadata Committers",
     license="Apache License 2.0",
