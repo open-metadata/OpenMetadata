6675d7ac662297400737dfa4a17a2f511ea79dcf
Sriharsha Chintalapani <harsha@getcollate.io>
2023-08-04 13:18:38 -0700

    Remove Domains/DataProducts table creation

--- bootstrap/sql/org.postgresql.Driver/v014__create_db_connection_info.sql
+++ bootstrap/sql/org.postgresql.Driver/v014__create_db_connection_info.sql
@@ -1,28 +1,4 @@
--- create domain entity table
-CREATE TABLE IF NOT EXISTS domain_entity (
-    id VARCHAR(36) GENERATED ALWAYS AS (json ->> 'id') STORED NOT NULL,
-    name VARCHAR(256) GENERATED ALWAYS AS (json ->> 'name') STORED NOT NULL,
-    fqnHash VARCHAR(256) NOT NULL,
-    json JSONB NOT NULL,
-    updatedAt BIGINT GENERATED ALWAYS AS ((json ->> 'updatedAt')::bigint) STORED NOT NULL,
-    updatedBy VARCHAR(256) GENERATED ALWAYS AS (json ->> 'updatedBy') STORED NOT NULL,
-    PRIMARY KEY (id),
-    UNIQUE (fqnHash)
-);
-
--- create data product entity table
-CREATE TABLE IF NOT EXISTS data_product_entity (
-    id VARCHAR(36) GENERATED ALWAYS AS (json ->> 'id') STORED NOT NULL,
-    name VARCHAR(256) GENERATED ALWAYS AS (json ->> 'name') STORED NOT NULL,
-    fqnHash VARCHAR(256) NOT NULL,
-    json JSONB NOT NULL,
-    updatedAt BIGINT GENERATED ALWAYS AS ((json ->> 'updatedAt')::bigint) STORED NOT NULL,
-    updatedBy VARCHAR(256) GENERATED ALWAYS AS (json ->> 'updatedBy') STORED NOT NULL,
-    PRIMARY KEY (id),
-    UNIQUE (fqnHash)
-);
-
--- Rename includeTempTables in snowflake to includeTransientTables 
+-- Rename includeTempTables in snowflake to includeTransientTables
 
 UPDATE dbservice_entity
 SET json = jsonb_set(json::jsonb #- '{connection,config,includeTempTables}', '{connection,config,includeTransientTables}',
