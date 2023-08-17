1a23fbd180d3d10924efdfe64aba2c53423a0273
Sriharsha Chintalapani <harsha@getcollate.io>
2023-08-17 06:50:39 -0700

    Move upstream migrations to new path

--- bootstrap/sql/migrations/native/1.1.2/mysql/schemaChanges.sql
+++ bootstrap/sql/migrations/native/1.1.2/mysql/schemaChanges.sql
@@ -44,3 +44,27 @@ ALTER TABLE query_entity MODIFY COLUMN nameHash VARCHAR(256) COLLATE ascii_bin;
 ALTER TABLE report_entity MODIFY COLUMN fqnHash VARCHAR(768) COLLATE ascii_bin;
 ALTER TABLE storage_container_entity MODIFY COLUMN fqnHash VARCHAR(768) COLLATE ascii_bin;
 ALTER TABLE topic_entity MODIFY COLUMN fqnHash VARCHAR(768) COLLATE ascii_bin;
+
+
+UPDATE dbservice_entity
+SET json = JSON_SET(
+        JSON_REMOVE(json, '$.connection.config.connectionOptions'),
+        '$.connection.config.connectionOptions',
+        JSON_EXTRACT(json, '$.connection.config.params')
+    )
+WHERE serviceType = 'Trino';
+
+UPDATE dbservice_entity
+SET json = JSON_REMOVE(json, '$.connection.config.params')
+WHERE serviceType = 'Trino';
+
+-- Modify migrations for service connection of trino to move password under authType
+UPDATE dbservice_entity
+SET json = JSON_INSERT(
+        JSON_REMOVE(json, '$.connection.config.password'),
+        '$.connection.config.authType',
+        JSON_OBJECT(),
+        '$.connection.config.authType.password',
+        JSON_EXTRACT(json, '$.connection.config.password'))
+where serviceType = 'Trino'
+  AND JSON_EXTRACT(json, '$.connection.config.password') IS NOT NULL;
--- bootstrap/sql/migrations/native/1.1.2/postgres/schemaChanges.sql
+++ bootstrap/sql/migrations/native/1.1.2/postgres/schemaChanges.sql
@@ -5,3 +5,25 @@ ALTER TABLE thread_entity ALTER COLUMN entityLink TYPE VARCHAR(3072);
 ALTER TABLE tag_usage ALTER COLUMN tagFQNHash TYPE VARCHAR(768), ALTER COLUMN targetFQNHash TYPE VARCHAR(768);
 ALTER TABLE test_suite ALTER COLUMN fqnHash TYPE VARCHAR(768);
 
+UPDATE dbservice_entity
+SET json = JSON_SET(
+        JSON_REMOVE(json, '$.connection.config.connectionOptions'),
+        '$.connection.config.connectionOptions',
+        JSON_EXTRACT(json, '$.connection.config.params')
+    )
+WHERE serviceType = 'Trino';
+
+UPDATE dbservice_entity
+SET json = JSON_REMOVE(json, '$.connection.config.params')
+WHERE serviceType = 'Trino';
+
+-- Modify migrations for service connection of trino to move password under authType
+UPDATE dbservice_entity
+SET json = JSON_INSERT(
+        JSON_REMOVE(json, '$.connection.config.password'),
+        '$.connection.config.authType',
+        JSON_OBJECT(),
+        '$.connection.config.authType.password',
+        JSON_EXTRACT(json, '$.connection.config.password'))
+where serviceType = 'Trino'
+  AND JSON_EXTRACT(json, '$.connection.config.password') IS NOT NULL;
