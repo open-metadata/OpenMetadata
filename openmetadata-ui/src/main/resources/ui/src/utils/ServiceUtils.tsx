7b8244b59e531b26d597861b08a560036ef6bae8
Sachin Chaurasiya <sachinchaurasiyachotey87@gmail.com>
2023-08-22 13:13:45 +0530

    ui: add icon for lightdash service

--- openmetadata-ui/src/main/resources/ui/src/utils/ServiceUtils.tsx
+++ openmetadata-ui/src/main/resources/ui/src/utils/ServiceUtils.tsx
@@ -50,6 +50,7 @@ import {
   IMPALA,
   KAFKA,
   KINESIS,
+  LIGHT_DASH,
   LOGO,
   LOOKER,
   MARIADB,
@@ -244,11 +245,15 @@ export const serviceTypeLogo = (type: string) => {
 
     case DashboardServiceType.DomoDashboard:
       return DOMO;
+
     case DashboardServiceType.Mode:
       return MODE;
 
     case DashboardServiceType.QlikSense:
       return QLIK_SENSE;
+
+    case DashboardServiceType.Lightdash:
+      return LIGHT_DASH;
 
     case PipelineServiceType.Airflow:
       return AIRFLOW;
