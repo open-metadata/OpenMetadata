fe25c75bbf445c30b7071efd30b5719ab58b2b2e
Aniket Katkar <aniketkatkar97@gmail.com>
2023-08-24 11:38:17 +0530

    removed the unnecessary prop used in DataModelVersion component

--- openmetadata-ui/src/main/resources/ui/src/components/DataModelVersion/DataModelVersion.component.tsx
+++ openmetadata-ui/src/main/resources/ui/src/components/DataModelVersion/DataModelVersion.component.tsx
@@ -184,7 +184,6 @@ const DataModelVersion: FC<DataModelVersionProp> = ({
         )}
 
         <EntityVersionTimeLine
-          show
           currentVersion={version}
           versionHandler={versionHandler}
           versionList={versionList}
