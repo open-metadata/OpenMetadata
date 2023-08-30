c0c135dafcc3ecc43be039c79662e2d5fa0afa6c
Pere Miquel Brull <peremiquelbrull@gmail.com>
2023-08-30 08:41:43 +0200

    Bump version to 1.1.4

--- ingestion/setup.py
+++ ingestion/setup.py
@@ -266,7 +266,7 @@ test = {
 build_options = {"includes": ["_cffi_backend"]}
 setup(
     name="openmetadata-ingestion",
-    version="1.1.3.0",
+    version="1.1.4.0",
     url="https://open-metadata.org/",
     author="OpenMetadata Committers",
     license="Apache License 2.0",
