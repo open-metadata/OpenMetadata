701f274bf1cb018a66b425e4e968ec0ebf0df007
Pere Miquel Brull <peremiquelbrull@gmail.com>
2023-08-31 07:30:13 +0200

    Fix import

--- ingestion/tests/cli_e2e/base/test_cli_dashboard.py
+++ ingestion/tests/cli_e2e/base/test_cli_dashboard.py
@@ -19,7 +19,7 @@ from unittest import TestCase
 import pytest
 
 from metadata.ingestion.api.sink import SinkStatus
-from metadata.ingestion.api.source import SourceStatus
+from metadata.ingestion.api.status import Status
 
 from .e2e_types import E2EType
 from .test_cli import CliBase
@@ -95,21 +95,17 @@ class CliDashboardBase(TestCase):
             raise NotImplementedError()
 
         @abstractmethod
-        def assert_not_including(
-            self, source_status: SourceStatus, sink_status: SinkStatus
-        ):
+        def assert_not_including(self, source_status: Status, sink_status: SinkStatus):
             raise NotImplementedError()
 
         @abstractmethod
         def assert_for_vanilla_ingestion(
-            self, source_status: SourceStatus, sink_status: SinkStatus
+            self, source_status: Status, sink_status: SinkStatus
         ) -> None:
             raise NotImplementedError()
 
         @abstractmethod
-        def assert_filtered_mix(
-            self, source_status: SourceStatus, sink_status: SinkStatus
-        ):
+        def assert_filtered_mix(self, source_status: Status, sink_status: SinkStatus):
             raise NotImplementedError()
 
         @staticmethod
