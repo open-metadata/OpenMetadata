56788b7b54b92c86c58143476619478ac69ee393
Ashish Gupta <ashish@getcollate.io>
2023-09-01 14:34:35 +0530

    added support for task functionality

--- openmetadata-ui/src/main/resources/ui/src/pages/TasksPage/TasksPage.interface.ts
+++ openmetadata-ui/src/main/resources/ui/src/pages/TasksPage/TasksPage.interface.ts
@@ -15,6 +15,7 @@ import { Container } from 'generated/entity/data/container';
 import { DashboardDataModel } from 'generated/entity/data/dashboardDataModel';
 import { Database } from 'generated/entity/data/database';
 import { DatabaseSchema } from 'generated/entity/data/databaseSchema';
+import { StoredProcedure } from 'generated/entity/data/storedProcedure';
 import { Dashboard } from '../../generated/entity/data/dashboard';
 import { Mlmodel } from '../../generated/entity/data/mlmodel';
 import { Pipeline } from '../../generated/entity/data/pipeline';
@@ -28,6 +29,7 @@ export type EntityData =
   | Pipeline
   | Mlmodel
   | Container
+  | StoredProcedure
   | Database
   | DatabaseSchema
   | DashboardDataModel;
