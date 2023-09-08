61203414a2fb90c382c3d00b9bcd79b92bd95943
mohitdeuex <mohit.y@deuexsolutions.com>
2023-09-08 14:03:18 +0530

    Add a lookup for avoiding transaction that are not started and should not be rolled back

--- openmetadata-service/src/main/java/org/openmetadata/service/jdbi3/unitofwork/JdbiTransactionAspect.java
+++ openmetadata-service/src/main/java/org/openmetadata/service/jdbi3/unitofwork/JdbiTransactionAspect.java
@@ -1,5 +1,8 @@
 package org.openmetadata.service.jdbi3.unitofwork;
 
+import java.util.Collections;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 import lombok.extern.slf4j.Slf4j;
 import org.jdbi.v3.core.Handle;
 import org.jdbi.v3.core.Handles;
@@ -7,6 +10,7 @@ import org.jdbi.v3.core.Handles;
 @Slf4j
 public class JdbiTransactionAspect {
   private final JdbiHandleManager handleManager;
+  private final Set<Integer> IN_TRANSACTION_HANDLES = Collections.newSetFromMap(new ConcurrentHashMap<>());
 
   public JdbiTransactionAspect(JdbiHandleManager handleManager) {
     this.handleManager = handleManager;
@@ -18,6 +22,7 @@ public class JdbiTransactionAspect {
       handle.getConnection().setAutoCommit(autoCommit);
       handle.getConfig(Handles.class).setForceEndTransactions(false);
       handle.begin();
+      IN_TRANSACTION_HANDLES.add(handle.hashCode());
       LOG.debug(
           "Begin Transaction Thread Id [{}] has handle id [{}] Transaction {} Level {}",
           Thread.currentThread().getId(),
@@ -25,8 +30,7 @@ public class JdbiTransactionAspect {
           handle.isInTransaction(),
           handle.getTransactionIsolationLevel());
     } catch (Exception ex) {
-      handleManager.clear();
-      throw new RuntimeException(ex.getMessage());
+      terminateHandle();
     }
   }
 
@@ -40,7 +44,6 @@ public class JdbiTransactionAspect {
     }
     try {
       handle.getConnection().commit();
-      // handle.commit();
       LOG.debug(
           "Performing commit Thread Id [{}] has handle id [{}] Transaction {} Level {}",
           Thread.currentThread().getId(),
@@ -48,35 +51,35 @@ public class JdbiTransactionAspect {
           handle.isInTransaction(),
           handle.getTransactionIsolationLevel());
     } catch (Exception ex) {
-      handle.rollback();
-    } finally {
-      terminateHandle();
+      rollback();
     }
   }
 
   public void rollback() {
-    Handle handle = handleManager.get();
-    if (handle == null) {
-      LOG.debug("Handle was found to be null during rollback for [{}]", Thread.currentThread().getId());
-      return;
-    }
-    try {
-      handle.getConnection().rollback();
-      // handle.rollback();
-      LOG.debug(
-          "Performed rollback on Thread Id [{}] has handle id [{}] Transaction {} Level {}",
-          Thread.currentThread().getId(),
-          handle.hashCode(),
-          handle.isInTransaction(),
-          handle.getTransactionIsolationLevel());
-    } catch (Exception e) {
-      LOG.debug("Failed to rollback transaction due to {}", e);
-    } finally {
-      terminateHandle();
+    if (IN_TRANSACTION_HANDLES.contains(handleManager.get().hashCode())) {
+      Handle handle = handleManager.get();
+      if (handle == null) {
+        LOG.debug("Handle was found to be null during rollback for [{}]", Thread.currentThread().getId());
+        return;
+      }
+      try {
+        handle.getConnection().rollback();
+        LOG.debug(
+            "Performed rollback on Thread Id [{}] has handle id [{}] Transaction {} Level {}",
+            Thread.currentThread().getId(),
+            handle.hashCode(),
+            handle.isInTransaction(),
+            handle.getTransactionIsolationLevel());
+      } catch (Exception e) {
+        LOG.debug("Failed to rollback transaction due to", e);
+      } finally {
+        terminateHandle();
+      }
     }
   }
 
   public void terminateHandle() {
+    IN_TRANSACTION_HANDLES.remove(handleManager.get().hashCode());
     handleManager.clear();
   }
 }
