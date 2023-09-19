18a4513ccc5c11e51cd028389b471ad9bf4f11d7
Pere Miquel Brull <peremiquelbrull@gmail.com>
2023-09-19 14:14:04 +0200

    Fix #13237 - Rename to instanceDomain and test DomoDashboard charts (#13247)
    * Rename sandboxDomain to instanceDomain
    
    * Test Get Charts in DomoDashboard
    
    * Fix schemas
    
    * Fix test
    
    * Fix test
    
    * Rename to Auto Tag PII
    
    * Fix query test
    
    * Fix query test
    
    * Fix query test

--- openmetadata-service/src/test/java/org/openmetadata/service/resources/query/QueryResourceTest.java
+++ openmetadata-service/src/test/java/org/openmetadata/service/resources/query/QueryResourceTest.java
@@ -119,7 +119,11 @@ public class QueryResourceTest extends EntityResourceTest<Query, CreateQuery> {
 
   @Test
   void post_without_query_400() {
-    CreateQuery create = new CreateQuery().withDuration(0.0).withQueryDate(1673857635064L);
+    CreateQuery create =
+        new CreateQuery()
+            .withDuration(0.0)
+            .withQueryDate(1673857635064L)
+            .withService(SNOWFLAKE_REFERENCE.getFullyQualifiedName());
     assertResponse(
         () -> createEntity(create, ADMIN_AUTH_HEADERS), Response.Status.BAD_REQUEST, "[query must not be null]");
   }
