04760177f693ce726623f2754918595cdacd4c18
Ayush Shah <ayush@getcollate.io>
2023-09-25 15:17:38 +0530

    Fixes 13321: Fix Test Connection timeout (#13323)

--- ingestion/src/metadata/ingestion/source/database/bigquery/metadata.py
+++ ingestion/src/metadata/ingestion/source/database/bigquery/metadata.py
@@ -53,6 +53,7 @@ from metadata.generated.schema.type.tagLabel import TagLabel
 from metadata.ingestion.api.models import Either, StackTraceError
 from metadata.ingestion.api.steps import InvalidSourceException
 from metadata.ingestion.models.ometa_classification import OMetaTagAndClassification
+from metadata.ingestion.source.connections import get_test_connection_fn
 from metadata.ingestion.source.database.bigquery.helper import get_inspector_details
 from metadata.ingestion.source.database.bigquery.models import (
     STORED_PROC_LANGUAGE_MAP,
@@ -217,6 +218,12 @@ class BigquerySource(StoredProcedureMixin, CommonDbSourceService):
     def set_project_id() -> List[str]:
         _, project_ids = auth.default()
         return project_ids if isinstance(project_ids, list) else [project_ids]
+
+    def test_connection(self) -> None:
+        for project_id in self.set_project_id():
+            self.set_inspector(project_id)
+            test_connection_fn = get_test_connection_fn(self.service_connection)
+            test_connection_fn(self.metadata, self.engine, self.service_connection)
 
     def query_table_names_and_types(
         self, schema_name: str
