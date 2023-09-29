54ba1616a3dd14e65650265edeb1923651e5d33a
Teddy Crepineau <teddy.crepineau@gmail.com>
2023-09-29 10:30:48 +0200

    feat: updated status reporter

--- ingestion/src/metadata/workflow/workflow_output_handler.py
+++ ingestion/src/metadata/workflow/workflow_output_handler.py
@@ -258,7 +258,7 @@ def print_data_insight_status(workflow) -> None:
     print_workflow_summary_legacy(
         workflow,
         processor=True,
-        processor_status=workflow.status,
+        processor_status=workflow.source.get_status(),
     )
 
     if workflow.source.get_status().source_start_time:
@@ -270,7 +270,6 @@ def print_data_insight_status(workflow) -> None:
         log_ansi_encoded_string(message=WORKFLOW_FAILURE_MESSAGE)
     elif (
         workflow.source.get_status().warnings
-        or workflow.status.warnings
         or (hasattr(workflow, "sink") and workflow.sink.get_status().warnings)
     ):
         log_ansi_encoded_string(message=WORKFLOW_WARNING_MESSAGE)
