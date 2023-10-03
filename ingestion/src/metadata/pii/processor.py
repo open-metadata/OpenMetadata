6bed1d5857bbe445f4752f51a7703c3116b43b42
Pere Miquel Brull <peremiquelbrull@gmail.com>
2023-10-03 10:26:43 +0200

    Format

--- ingestion/src/metadata/pii/processor.py
+++ ingestion/src/metadata/pii/processor.py
@@ -32,8 +32,8 @@ from metadata.ingestion.api.models import Either, StackTraceError
 from metadata.ingestion.api.parser import parse_workflow_config_gracefully
 from metadata.ingestion.api.step import Step
 from metadata.ingestion.api.steps import Processor
-from metadata.ingestion.ometa.ometa_api import OpenMetadata
 from metadata.ingestion.models.table_metadata import ColumnTag
+from metadata.ingestion.ometa.ometa_api import OpenMetadata
 from metadata.pii.constants import PII
 from metadata.pii.scanners.column_name_scanner import ColumnNameScanner
 from metadata.pii.scanners.ner_scanner import NERScanner
