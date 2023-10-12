5d39af5a68e1b85432581a6301825f9313ff1e76
Sriharsha Chintalapani <harshach@users.noreply.github.com>
2023-10-12 16:11:24 -0700

    Disable tags when the classification is disabled (#13532)
    * Disable tags when the classification is disabled
    
    * remove unnecessary constants
    
    ---------
    
    Co-authored-by: 07Himank <112613760+07Himank@users.noreply.github.com>

--- openmetadata-service/src/main/java/org/openmetadata/service/Entity.java
+++ openmetadata-service/src/main/java/org/openmetadata/service/Entity.java
@@ -108,6 +108,8 @@ public final class Entity {
 
   public static final String FIELD_LIFE_CYCLE = "lifeCycle";
 
+  public static final String FIELD_DISABLED = "disabled";
+
   //
   // Service entities
   //
