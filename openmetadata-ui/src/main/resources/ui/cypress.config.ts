fd4bfa837edac3a43bd3eee05cc62a51b65b3069
Chirag Madlani <12962843+chirag-madlani@users.noreply.github.com>
2023-10-13 16:34:02 +0530

    revert cypress config

--- openmetadata-ui/src/main/resources/ui/cypress.config.ts
+++ openmetadata-ui/src/main/resources/ui/cypress.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
   viewportWidth: 1240,
   viewportHeight: 660,
   watchForFileChanges: false,
-  defaultCommandTimeout: 5000,
+  defaultCommandTimeout: 15000,
   chromeWebSecurity: false,
   numTestsKeptInMemory: 0,
   experimentalMemoryManagement: true,
@@ -28,11 +28,6 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       return plugins(on, config);
     },
-    baseUrl: 'http://localhost:3000/',
-    specPattern: [
-      'cypress/e2e/Features/*.{js,jsx,ts,tsx}',
-      'cypress/e2e/Flow/*.{js,jsx,ts,tsx}',
-      'cypress/e2e/Pages/*.{js,jsx,ts,tsx}',
-    ],
+    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
   },
 });
