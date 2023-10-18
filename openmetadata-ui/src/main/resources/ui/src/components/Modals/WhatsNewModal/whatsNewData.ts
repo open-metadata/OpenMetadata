cb27e66e1451469a0eea9e8f35a7c43abae22f7b
Sachin Chaurasiya <sachinchaurasiyachotey87@gmail.com>
2023-10-18 13:22:23 +0530

    chore(ui): add what's new content for 1.2.0

--- openmetadata-ui/src/main/resources/ui/src/components/Modals/WhatsNewModal/whatsNewData.ts
+++ openmetadata-ui/src/main/resources/ui/src/components/Modals/WhatsNewModal/whatsNewData.ts
@@ -18,9 +18,9 @@ import sqlLineageImg from '../../../assets/img/ImprovedSQLLineage.png';
 import ingestionFramework from '../../../assets/img/IngestionFramework.png';
 import tagCategoryImg from '../../../assets/img/TagCategory.png';
 
-export const LATEST_VERSION_ID = 18;
+export const LATEST_VERSION_ID = 19;
 
-export const COOKIE_VERSION = 'VERSION_1_1_2'; // To be changed with each release.
+export const COOKIE_VERSION = 'VERSION_1_2_0'; // To be changed with each release.
 
 // for youtube video make isImage = false and path = {video embed id}
 // embed:- youtube video => share => click on embed and take {url with id} from it
@@ -989,6 +989,45 @@ export const WHATS_NEW = [
 - Deprecated Flyway and using native migrations.
 - Improved Test Suite UI performance.
       `,
+    },
+  },
+  {
+    id: 19,
+    version: 'v1.2.0',
+    description: 'Released on 23rd Oct 2023.',
+    features: [],
+    changeLogs: {
+      'Domains and Data Products': `- Added support for Domains and Data Products.
+- Assets can be added to a Domain, and users can scope their discovery experience to one Domain.
+- Assets can also be added as Data Products in a Domain.
+`,
+      'Search Index': `- Elasticsearch or Open Search connectors can now bring in the search index metadata into OpenMetadata.
+- The connector will populate the index's mapping, settings, and sample data.`,
+      'Stored Procedures': `- Added support for Stored Procedures.
+- Snowflake, Redshift, and BigQuery connectors are updated to bring stored procedure metadata into OpenMetadata.
+- The metadata workflow will bring the Stored Procedures and parse their executions to extract lineage information.`,
+      'OpenMetadata Browser Extension': `- Updated the Chrome browser extension for OpenMetadata with the new UI.
+- Added support for Databases, Database Schemas, Tables, Dashboards, Charts, Pipelines, and Topics.
+`,
+      'Build Automation Applications': `- Added Applications into OpenMetadata, giving users a unique view of processes that can be scheduled and run in the platform.
+- Search Indexing and Data Insights Report have been converted into Applications.
+- UI displays all the available applications, which Admins can add or schedule.
+- We will continue to add new Applications in upcoming releases.
+      `,
+      Lineage: `- Performance improvements made for lineage based on the new release of SQLfluff.
+- Added support for UPDATE ... FROM Snowflake queries
+- Added column-level lineage support for SELECT * queries`,
+      Connectors: `- Greenplum connector is now supported.
+- Couchbase connector is now supported.`,
+      'Knowledge Center ([Exclusively for Collate](https://www.getcollate.io/))': `- Backend APIs support creating, editing, and listing knowledge articles (with external links).
+- Knowledge articles and links can be associated with a Domain, Team, or an Entity.
+- UI support to build a Knowledge Center and expand the documentation of your company.`,
+      'Customizable Landing Page ([Exclusively for Collate](https://www.getcollate.io/))': `- Admins can create Personas to group individuals in their company, such as Data Engineers, Data Stewards, or Data Scientists.
+- Admins can customize the landing page for each Persona with a set of supported widgets: Activity Feed, Announcements, Knowledge Center, etc.
+- We will add support for more widgets in upcoming releases.`,
+      'Cost Analysis Report ([Exclusively for Collate](https://www.getcollate.io/))': `- The Usage Workflow will now also track how tables are Accessed and Updated.
+- This information will be used in the Data Insights workflow to show the evolution of your used and used assets and compare them by size.
+- Support has been added for Snowflake, and we will continue to add more sources in upcoming releases.`,
     },
   },
 ];
