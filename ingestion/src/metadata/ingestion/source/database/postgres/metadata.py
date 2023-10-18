a918797951eddc6a37e9dde395afe94ba1e18376
Vanshika Kabra <vanshikakabra@Vanshikas-MacBook-Pro.local>
2023-10-18 17:29:05 +0530

    remove pylint error

--- ingestion/src/metadata/ingestion/source/database/postgres/metadata.py
+++ ingestion/src/metadata/ingestion/source/database/postgres/metadata.py
@@ -239,9 +239,7 @@ class PostgresSource(CommonDbSourceService):
                 )
             )
 
-    def get_table_owner(
-        self, table_name, schema=None
-    ):  # pylint: disable=unused-argument
+    def get_table_owner(self, table_name, schema=None):
 
         result = self.connection.execute(POSTGRES_TABLE_OWNERS)
         for table in result:
