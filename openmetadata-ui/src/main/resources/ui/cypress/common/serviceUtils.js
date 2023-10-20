1bd833d9a2709833acc4c4d8ce19db1f2fcbf459
Shailesh Parmar <shailesh.parmar.webdev@gmail.com>
2023-10-20 10:07:51 +0530

    cypress: fixed failing cypress of main and downgraded cypress version to 12.17.4 (#13656)

--- openmetadata-ui/src/main/resources/ui/cypress/common/serviceUtils.js
+++ openmetadata-ui/src/main/resources/ui/cypress/common/serviceUtils.js
@@ -30,15 +30,16 @@ export const visitServiceDetailsPage = (service, verifyHeader = true) => {
     .should('be.visible')
     .click();
 
-  cy.wait('@getServices').then((interception) => {
-    const responseBody = interception.response.body;
-    // check if service name is received on the first page otherwise, click on Next button
-    const item = responseBody.data.find((item) => item.name === service.name);
-    if (!item && responseBody.paging.after) {
-      cy.get('[data-testid="next"]').click();
-      verifyResponseStatusCode('@getServices', 200);
-    }
-  });
+  cy.wait('@getServices');
+
+  interceptURL(
+    'GET',
+    'api/v1/search/query?q=*&from=0&size=*&index=*',
+    'searchService'
+  );
+  cy.get('[data-testid="searchbar"]').type(service.name);
+
+  verifyResponseStatusCode('@searchService', 200);
 
   // click on created service
   cy.get(`[data-testid="service-name-${service.name}"]`)
