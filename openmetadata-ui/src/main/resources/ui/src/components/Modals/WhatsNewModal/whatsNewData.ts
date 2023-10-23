5fcca09df9196676bfe34ef720dd040a3824a094
Chirag Madlani <12962843+chirag-madlani@users.noreply.github.com>
2023-10-23 12:16:23 +0530

    update whats knew content

--- openmetadata-ui/src/main/resources/ui/src/components/Modals/WhatsNewModal/whatsNewData.ts
+++ openmetadata-ui/src/main/resources/ui/src/components/Modals/WhatsNewModal/whatsNewData.ts
@@ -1045,36 +1045,39 @@ export const WHATS_NEW = [
     changeLogs: {
       'Domains and Data Products': `- Added support for Domains and Data Products.
 - Assets can be added to a Domain, and users can scope their discovery experience to one Domain.
-- Assets can also be added as Data Products in a Domain.
-`,
+- Assets can also be added as Data Products in a Domain.`,
       'Search Index': `- Elasticsearch or Open Search connectors can now bring in the search index metadata into OpenMetadata.
 - The connector will populate the index's mapping, settings, and sample data.`,
       'Stored Procedures': `- Added support for Stored Procedures.
 - Snowflake, Redshift, and BigQuery connectors are updated to bring stored procedure metadata into OpenMetadata.
 - The metadata workflow will bring the Stored Procedures and parse their executions to extract lineage information.`,
+      'Glossary Approval Workflow & Glossary Styling': `- Introduced a glossary approval workflow. An approval workflow is created if Reviewers are added to a glossary.
+- A task is added for reviewers to approve or reject the glossary term. The terms will show up in Draft status.
+- Only the reviewers can approve or reject the term.
+- Conversations are supported to discuss further about the terms.
+- If no reviewer is added, then the glossary terms are approved by default.
+- Introduced styling for glossary terms. Now you can add icons and color code the glossary terms for easy identification.
+- Color coding helps to visually differentiate and identify the data assets, when glossary terms are added to them.`,
       'OpenMetadata Browser Extension': `- Updated the Chrome browser extension for OpenMetadata with the new UI.
-- Added support for Databases, Database Schemas, Tables, Dashboards, Charts, Pipelines, and Topics.
-`,
+- Added support for Databases, Database Schemas, Tables, Dashboards, Charts, Pipelines, and Topics.`,
       'Build Automation Applications': `- Added Applications into OpenMetadata, giving users a unique view of processes that can be scheduled and run in the platform.
 - Search Indexing and Data Insights Report have been converted into Applications.
 - UI displays all the available applications, which Admins can add or schedule.
-- We will continue to add new Applications in upcoming releases.
-      `,
+- We will continue to add new Applications in upcoming releases.`,
       Lineage: `- Performance improvements made for lineage based on the new release of SQLfluff.
 - Added support for **UPDATE ... FROM** Snowflake queries
 - Added column-level lineage support for **SELECT * queries**`,
       Connectors: `- Greenplum connector is now supported.
-- Couchbase connector is now supported.`,
+- Couchbase connector is now supported.
+- Azure Data Lake Storage connector is supported ${CollateIconWithLinkMD}`,
       'Customizable Landing Page': `- Admins can create Personas to group individuals in their company, such as Data Engineers, Data Stewards, or Data Scientists.
 - Admins can customize the landing page for each Persona with a set of supported widgets: Activity Feed, Announcements, Knowledge Center, etc.
 - We will add support for more widgets in upcoming releases.`,
-      Glossary: `- Supports Glossary approval workflow.
-- Support Glossary categorision for easy distingusion.`,
       [`Knowledge Center ${CollateIconWithLinkMD}`]: `- Backend APIs support creating, editing, and listing knowledge articles (with external links).
 - Knowledge articles and links can be associated with a Domain, Team, or an Entity.
 - UI support to build a Knowledge Center and expand the documentation of your company.`,
       [`Cost Analysis Report ${CollateIconWithLinkMD}`]: `- The Usage Workflow will now also track how tables are Accessed and Updated.
-- This information will be used in the Data Insights workflow to show the evolution of your used and used assets and compare them by size.
+- This information will be used in the Data Insights workflow to show the evolution of your used and unused assets and compare them by size.
 - Support has been added for Snowflake, and we will continue to add more sources in upcoming releases.`,
     },
   },
