a1e0e4282a02afa08980b3d4550e5786fd40f272
Mayur Singal <39544459+ulixius9@users.noreply.github.com>
2023-10-26 19:56:45 +0530

    Bump ingestion version to 1.2.0.1 (#13730)

--- ingestion/setup.py
+++ ingestion/setup.py
@@ -309,7 +309,7 @@ e2e_test = {
 build_options = {"includes": ["_cffi_backend"]}
 setup(
     name="openmetadata-ingestion",
-    version="1.2.0.0",
+    version="1.2.0.1",
     url="https://open-metadata.org/",
     author="OpenMetadata Committers",
     license="Apache License 2.0",
