fec51a16a1aca6c555c6f2a45409ea4aa6a4cd93
Teddy <teddy.crepineau@gmail.com>
2023-11-01 17:46:31 +0100

    fix: handle null aggregation bucket (#13791)
    (cherry picked from commit 5318646526d68df5cc5809e7d2341a4d799fef10)

--- openmetadata-service/src/main/java/org/openmetadata/service/search/elasticsearch/dataInsightAggregators/ElasticSearchTotalEntitiesByTierAggregator.java
+++ openmetadata-service/src/main/java/org/openmetadata/service/search/elasticsearch/dataInsightAggregators/ElasticSearchTotalEntitiesByTierAggregator.java
@@ -14,7 +14,7 @@ public class ElasticSearchTotalEntitiesByTierAggregator
 
   @Override
   protected Double getValue(Sum key) {
-    return key.getValue();
+    return key != null ? key.getValue() : 0.0;
   }
 
   @Override
