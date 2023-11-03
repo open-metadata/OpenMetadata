7d93b73834c8830ade9e96a35ff609c7003e63e2
07Himank <himank07mehta@gmail.com>
2023-11-03 18:02:30 +0530

    checkstyle

--- openmetadata-service/src/main/java/org/openmetadata/service/jdbi3/DataProductRepository.java
+++ openmetadata-service/src/main/java/org/openmetadata/service/jdbi3/DataProductRepository.java
@@ -13,6 +13,13 @@
 
 package org.openmetadata.service.jdbi3;
 
+import static org.openmetadata.common.utils.CommonUtil.listOrEmpty;
+import static org.openmetadata.service.Entity.DATA_PRODUCT;
+import static org.openmetadata.service.Entity.FIELD_ASSETS;
+import static org.openmetadata.service.util.EntityUtil.entityReferenceMatch;
+
+import java.util.ArrayList;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.jdbi.v3.sqlobject.transaction.Transaction;
 import org.openmetadata.schema.EntityInterface;
@@ -24,14 +31,6 @@ import org.openmetadata.service.Entity;
 import org.openmetadata.service.resources.domains.DataProductResource;
 import org.openmetadata.service.util.EntityUtil;
 import org.openmetadata.service.util.EntityUtil.Fields;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.openmetadata.common.utils.CommonUtil.listOrEmpty;
-import static org.openmetadata.service.Entity.DATA_PRODUCT;
-import static org.openmetadata.service.Entity.FIELD_ASSETS;
-import static org.openmetadata.service.util.EntityUtil.entityReferenceMatch;
 
 @Slf4j
 public class DataProductRepository extends EntityRepository<DataProduct> {
