0a3473d8f5ddfd5ff7b9215219ec00eef4d6fd3b
Onkar Ravgan <onkar.10r@gmail.com>
2023-11-06 18:13:30 +0530

    added 121 migration (#13837)

---
+++ bootstrap/sql/migrations/native/1.2.1/postgres/schemaChanges.sql
@@ -0,0 +1,11 @@
+
+--fixed Query for updating viewParsingTimeoutLimit
+UPDATE ingestion_pipeline_entity
+SET json = jsonb_set(
+  json::jsonb #- '{sourceConfig,config,viewParsingTimeoutLimit}',
+  '{sourceConfig,config,queryParsingTimeoutLimit}',
+  (json #> '{sourceConfig,config,viewParsingTimeoutLimit}')::jsonb,
+  true
+)
+WHERE json #>> '{pipelineType}' = 'metadata'
+AND json #>> '{sourceConfig,config,type}' = 'DatabaseMetadata';
