5b37a71e705ecac1bbedc15960ec396b9d273e98
Aniket Katkar <aniketkatkar97@gmail.com>
2023-11-08 21:15:17 +0530

    fixed unit tests

--- openmetadata-ui/src/main/resources/ui/src/utils/SearchIndexUtils.tsx
+++ openmetadata-ui/src/main/resources/ui/src/utils/SearchIndexUtils.tsx
@@ -22,8 +22,8 @@ import { EntityTabs, TabSpecificField } from '../enums/entity.enum';
 import { SearchIndexField } from '../generated/entity/data/searchIndex';
 import { sortTagsCaseInsensitive } from './CommonUtils';
 
-export const defaultFields = `${TabSpecificField.FIELDS},${TabSpecificField.FOLLOWERS},${TabSpecificField.TAGS},
-${TabSpecificField.OWNER},${TabSpecificField.DOMAIN},${TabSpecificField.VOTES}`;
+// eslint-disable-next-line max-len
+export const defaultFields = `${TabSpecificField.FIELDS},${TabSpecificField.FOLLOWERS},${TabSpecificField.TAGS},${TabSpecificField.OWNER},${TabSpecificField.DOMAIN},${TabSpecificField.VOTES}`;
 
 export const makeRow = (column: SearchIndexField) => {
   return {
