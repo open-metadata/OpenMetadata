1d7e8720341e55ececf3f8bf9ff5224c726a531d
Teddy Crepineau <teddy.crepineau@gmail.com>
2023-11-17 15:06:58 +0100

    feat: wrapped trino import in __init__

--- ingestion/src/metadata/profiler/processor/sampler/sqlalchemy/trino/sampler.py
+++ ingestion/src/metadata/profiler/processor/sampler/sqlalchemy/trino/sampler.py
@@ -13,7 +13,6 @@ Helper module to handle data sampling
 for the profiler
 """
 from sqlalchemy import inspect, or_, text
-from trino.sqlalchemy.dialect import TrinoDialect
 
 from metadata.profiler.orm.registry import FLOAT_SET
 from metadata.profiler.processor.handle_partition import RANDOM_LABEL
@@ -25,8 +24,11 @@ class TrinoSampler(SQASampler):
     Generates a sample of the data to not
     run the query in the whole table.
     """
+    def __init__(self, *args, **kwargs):
+        from trino.sqlalchemy.dialect import TrinoDialect
+        TrinoDialect._json_deserializer = None  # pylint: disable=protected-access
 
-    TrinoDialect._json_deserializer = None  # pylint: disable=protected-access
+        super().__init__(*args, **kwargs)
 
     def _base_sample_query(self, label=None):
         sqa_columns = [col for col in inspect(self.table).c if col.name != RANDOM_LABEL]
