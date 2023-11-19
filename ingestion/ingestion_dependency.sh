1a47d4dc8dab460078c9bf0f4966fee9bc76d19e
Harpreet <allenkallz@gmail.com>
2023-11-19 23:29:41 +0100

    fix the sed command issue in ingestion dep script

--- ingestion/ingestion_dependency.sh
+++ ingestion/ingestion_dependency.sh
@@ -25,7 +25,7 @@ AIRFLOW_ADMIN_PASSWORD=${AIRFLOW_ADMIN_PASSWORD:-admin}
 
 OPENMETADATA_SERVER=${OPENMETADATA_SERVER:-"http://openmetadata-server:8585"}
 
-sed -i "s#\(sql_alchemy_conn = \).*#\1${DB_CONN}#" /opt/airflow/airflow.cfg
+sed -i 's,^#*[ ]*\(sql_alchemy_conn[ ]*=\).*,\1 '${DB_CONN}',g' /opt/airflow/airflow.cfg
 
 airflow db init
 
