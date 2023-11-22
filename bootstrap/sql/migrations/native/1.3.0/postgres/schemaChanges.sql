fae63045844b820f04a405fb05e2b18d077926e6
Pere Miquel Brull <peremiquelbrull@gmail.com>
2023-11-22 08:50:45 +0100

    Add pg migrations

--- bootstrap/sql/migrations/native/1.3.0/postgres/schemaChanges.sql
+++ bootstrap/sql/migrations/native/1.3.0/postgres/schemaChanges.sql
@@ -1,2 +1,16 @@
 -- DataInsightsApplication should not allow configuration
--- TODO MIGRATIONS FOR POSTGRES
+UPDATE apps_marketplace
+SET json = jsonb_set(
+	json::jsonb,
+	'{allowConfiguration}',
+	to_jsonb(false)
+)
+where name = 'DataInsightsApplication';
+
+UPDATE installed_apps
+SET json = jsonb_set(
+	json::jsonb,
+	'{allowConfiguration}',
+	to_jsonb(false)
+)
+where name = 'DataInsightsApplication';
