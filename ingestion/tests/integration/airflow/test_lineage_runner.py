5474f438a147d817c4cdb8cda7ed40c9d866fa96
Pere Miquel Brull <peremiquelbrull@gmail.com>
2023-12-01 06:29:44 +0100

    #11626 & #14131 - Lineage with other Entities & attr-based xlets (#14191)
    * Add OMEntity model
    
    * Test OMEntity
    
    * Update repr
    
    * Fix __str__
    
    * Add entity ref map
    
    * Test serializer for backend
    
    * Fix tests
    
    * Fix serializer
    
    * Test runner
    
    * Add runner tests
    
    * Update docs
    
    * Format

---
+++ ingestion/tests/integration/airflow/test_lineage_runner.py
@@ -0,0 +1,218 @@
+#  Copyright 2021 Collate
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#  http://www.apache.org/licenses/LICENSE-2.0
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+"""
+Test lineage parser to get inlets and outlets information
+"""
+from datetime import datetime
+from unittest import TestCase
+from unittest.mock import patch
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
+from airflow_provider_openmetadata.lineage.runner import AirflowLineageRunner
+from metadata.generated.schema.api.data.createDatabase import CreateDatabaseRequest
+from metadata.generated.schema.api.data.createDatabaseSchema import (
+    CreateDatabaseSchemaRequest,
+)
+from metadata.generated.schema.api.data.createTable import CreateTableRequest
+from metadata.generated.schema.api.services.createDatabaseService import (
+    CreateDatabaseServiceRequest,
+)
+from metadata.generated.schema.entity.data.table import Column, DataType, Table
+from metadata.generated.schema.entity.services.connections.database.common.basicAuth import (
+    BasicAuth,
+)
+from metadata.generated.schema.entity.services.connections.database.mysqlConnection import (
+    MysqlConnection,
+)
+from metadata.generated.schema.entity.services.connections.metadata.openMetadataConnection import (
+    OpenMetadataConnection,
+)
+from metadata.generated.schema.entity.services.databaseService import (
+    DatabaseConnection,
+    DatabaseService,
+    DatabaseServiceType,
+)
+from metadata.generated.schema.entity.services.pipelineService import PipelineService
+from metadata.generated.schema.security.client.openMetadataJWTClientConfig import (
+    OpenMetadataJWTClientConfig,
+)
+from metadata.ingestion.ometa.ometa_api import OpenMetadata
+from metadata.ingestion.source.pipeline.airflow.lineage_parser import (
+    OMEntity,
+    get_xlets_from_dag,
+)
+
+SLEEP = "sleep 1"
+PIPELINE_SERVICE_NAME = "test-lineage-runner"
+DB_SERVICE_NAME = "test-service-lineage-runner"
+OM_JWT = "eyJraWQiOiJHYjM4OWEtOWY3Ni1nZGpzLWE5MmotMDI0MmJrOTQzNTYiLCJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJhZG1pbiIsImlzQm90IjpmYWxzZSwiaXNzIjoib3Blbi1tZXRhZGF0YS5vcmciLCJpYXQiOjE2NjM5Mzg0NjIsImVtYWlsIjoiYWRtaW5Ab3Blbm1ldGFkYXRhLm9yZyJ9.tS8um_5DKu7HgzGBzS1VTA5uUjKWOCU0B_j08WXBiEC0mr0zNREkqVfwFDD-d24HlNEbrqioLsBuFRiwIWKc1m_ZlVQbG7P36RUxhuv2vbSp80FKyNM-Tj93FDzq91jsyNmsQhyNv_fNr3TXfzzSPjHt8Go0FMMP66weoKMgW2PbXlhVKwEuXUHyakLLzewm9UMeQaEiRzhiTMU3UkLXcKbYEJJvfNFcLwSl9W8JCO_l0Yj3ud-qt_nQYEZwqW6u5nfdQllN133iikV4fM5QZsMCnm8Rq1mvLR0y9bmJiD7fwM1tmJ791TUWqmKaTnP49U493VanKpUAfzIiOiIbhg"
+
+
+class TestAirflowLineageRuner(TestCase):
+    """
+    Validate AirflowLineageRunner
+    """
+
+    server_config = OpenMetadataConnection(
+        hostPort="http://localhost:8585/api",
+        authProvider="openmetadata",
+        securityConfig=OpenMetadataJWTClientConfig(jwtToken=OM_JWT),
+    )
+    metadata = OpenMetadata(server_config)
+
+    assert metadata.health_check()
+
+    service = CreateDatabaseServiceRequest(
+        name=DB_SERVICE_NAME,
+        serviceType=DatabaseServiceType.Mysql,
+        connection=DatabaseConnection(
+            config=MysqlConnection(
+                username="username",
+                authType=BasicAuth(password="password"),
+                hostPort="http://localhost:1234",
+            )
+        ),
+    )
+    service_type = "databaseService"
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        """
+        Prepare ingredients: Table Entity + DAG
+        """
+
+        service_entity = cls.metadata.create_or_update(data=cls.service)
+
+        create_db = CreateDatabaseRequest(
+            name="test-db",
+            service=service_entity.fullyQualifiedName,
+        )
+
+        create_db_entity = cls.metadata.create_or_update(data=create_db)
+
+        create_schema = CreateDatabaseSchemaRequest(
+            name="test-schema",
+            database=create_db_entity.fullyQualifiedName,
+        )
+
+        create_schema_entity = cls.metadata.create_or_update(data=create_schema)
+
+        create_inlet = CreateTableRequest(
+            name="lineage-test-inlet",
+            databaseSchema=create_schema_entity.fullyQualifiedName,
+            columns=[Column(name="id", dataType=DataType.BIGINT)],
+        )
+
+        create_inlet_2 = CreateTableRequest(
+            name="lineage-test-inlet2",
+            databaseSchema=create_schema_entity.fullyQualifiedName,
+            columns=[Column(name="id", dataType=DataType.BIGINT)],
+        )
+
+        create_outlet = CreateTableRequest(
+            name="lineage-test-outlet",
+            databaseSchema=create_schema_entity.fullyQualifiedName,
+            columns=[Column(name="id", dataType=DataType.BIGINT)],
+        )
+
+        cls.table_inlet1: Table = cls.metadata.create_or_update(data=create_inlet)
+        cls.table_inlet2: Table = cls.metadata.create_or_update(data=create_inlet_2)
+        cls.table_outlet: Table = cls.metadata.create_or_update(data=create_outlet)
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        """
+        Clean up
+        """
+
+        service_id = str(
+            cls.metadata.get_by_name(
+                entity=DatabaseService, fqn=DB_SERVICE_NAME
+            ).id.__root__
+        )
+
+        cls.metadata.delete(
+            entity=DatabaseService,
+            entity_id=service_id,
+            recursive=True,
+            hard_delete=True,
+        )
+
+        # Service ID created from the Airflow Lineage Operator in the
+        # example DAG
+        pipeline_service_id = str(
+            cls.metadata.get_by_name(
+                entity=PipelineService, fqn=PIPELINE_SERVICE_NAME
+            ).id.__root__
+        )
+
+        cls.metadata.delete(
+            entity=PipelineService,
+            entity_id=pipeline_service_id,
+            recursive=True,
+            hard_delete=True,
+        )
+
+    def test_lineage_runner(self):
+
+        with DAG("test_runner", start_date=datetime(2021, 1, 1)) as dag:
+            BashOperator(
+                task_id="print_date",
+                bash_command="date",
+                inlets=[
+                    OMEntity(
+                        entity=Table,
+                        fqn="test-service-lineage-runner.test-db.test-schema.lineage-test-inlet",
+                    ),
+                    OMEntity(
+                        entity=Table,
+                        fqn="test-service-lineage-runner.test-db.test-schema.lineage-test-inlet2",
+                    ),
+                ],
+            )
+
+            BashOperator(
+                task_id="sleep",
+                bash_command=SLEEP,
+                outlets=[
+                    OMEntity(
+                        entity=Table,
+                        fqn="test-service-lineage-runner.test-db.test-schema.lineage-test-outlet",
+                    )
+                ],
+            )
+
+        # skip the statuses since they require getting data from airflow's db
+        with patch.object(
+            AirflowLineageRunner, "add_all_pipeline_status", return_value=None
+        ):
+            runner = AirflowLineageRunner(
+                metadata=self.metadata,
+                service_name=PIPELINE_SERVICE_NAME,
+                dag=dag,
+                xlets=get_xlets_from_dag(dag),
+                only_keep_dag_lineage=True,
+            )
+
+            runner.execute()
+
+        lineage_data = self.metadata.get_lineage_by_name(
+            entity=Table,
+            fqn=self.table_outlet.fullyQualifiedName.__root__,
+            up_depth=1,
+            down_depth=1,
+        )
+
+        upstream_ids = [edge["fromEntity"] for edge in lineage_data["upstreamEdges"]]
+        self.assertIn(str(self.table_inlet1.id.__root__), upstream_ids)
+        self.assertIn(str(self.table_inlet2.id.__root__), upstream_ids)
--- openmetadata-docs/content/v1.2.x/connectors/pipeline/airflow/configuring-lineage.md
+++ openmetadata-docs/content/v1.2.x/connectors/pipeline/airflow/configuring-lineage.md
@@ -9,6 +9,116 @@ Regardless of the Airflow ingestion process you follow ([Workflow](/connectors/p
 [Lineage Backend](/connectors/pipeline/airflow/lineage-backend) or [Lineage Operator](/connectors/pipeline/airflow/lineage-operator)),
 OpenMetadata will try to extract the lineage information based on the tasks `inlets` and `outlets`.
 
+What it's important to consider here is that when we are ingesting Airflow lineage, we are actually building a graph:
+
+```
+Table A (node) -> DAG (edge) -> Table B (node)
+```
+
+Where tables are nodes and DAGs (Pipelines) are considered edges. This means that the correct way of setting these
+parameters is by making sure that we are informing both `inlets` and `outlets`, so that we have the nodes to build
+the relationship.
+
+## Configuring Lineage
+
+{% note %}
+
+This lineage configuration method is available for OpenMetadata release 1.2.3 or higher.
+
+{% /note %}
+
+Let's take a look at the following example:
+
+```python
+from datetime import timedelta
+
+from airflow import DAG
+from airflow.operators.dummy import DummyOperator
+from airflow.utils.dates import days_ago
+
+from metadata.generated.schema.entity.data.container import Container
+from metadata.generated.schema.entity.data.table import Table
+from metadata.ingestion.source.pipeline.airflow.lineage_parser import OMEntity
+
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(seconds=1),
+}
+
+
+with DAG(
+    "test-lineage",
+    default_args=default_args,
+    description="An example DAG which runs a lineage test",
+    start_date=days_ago(1),
+    is_paused_upon_creation=False,
+    catchup=False,
+) as dag:
+    
+
+    t0 = DummyOperator(
+        task_id='task0',
+        inlets=[
+            OMEntity(entity=Container, fqn="Container A", key="group_A"),
+            OMEntity(entity=Table, fqn="Table X", key="group_B"),
+        ]
+    )
+    
+    t1 = DummyOperator(
+        task_id='task10',
+        outlets=[
+            OMEntity(entity=Table, fqn="Table B", key="group_A"),
+            OMEntity(entity=Table, fqn="Table Y", key="group_B"),
+        ]
+    )
+
+    t0 >> t1
+```
+
+We are passing inlets and outlets as a list of the `OMEntity` class, that lets us specify:
+1. The type of the asset we are using: Table, Container,... following our SDK
+2. The FQN of the asset, which is the unique name of each asset in OpenMetadata, e.g., `serviceName.databaseName.schemaName.tableName`.
+3. The key to group the lineage if needed.
+
+This `OMEntity` class is defined following the example of Airflow's internal lineage 
+[models](https://github.com/apache/airflow/blob/main/airflow/lineage/entities.py).
+
+## Keys
+
+We can inform the lineage dependencies among different groups of tables. In the example above, we are not building the
+lineage from all inlets to all outlets, but rather grouping the tables by key (`group_A` and `group_B`).
+This means that after this lineage is processed, the relationship will be:
+
+```
+Container A (node) -> DAG (edge) -> Table B (node)
+```
+
+and
+
+```
+Table X (node) -> DAG (edge) -> Table Y (node)
+```
+
+It does not matter in which task of the DAG these inlet/outlet information is specified. During the ingestion process we
+group all these details at the DAG level.
+
+
+## Configuring Lineage between Tables
+
+{% note %}
+
+Note that this method only allows lineage between Tables.
+
+We will deprecate it in OpenMetadata 1.4
+
+{% /note %}
+
 Let's take a look at the following example:
 
 ```python
@@ -33,7 +143,7 @@ default_args = {
 with DAG(
     "test-multiple-inlet-keys",
     default_args=default_args,
-    description="An example DAG which runs a a task group lineage test",
+        description="An example DAG which runs a lineage test",
     start_date=days_ago(1),
     is_paused_upon_creation=False,
     catchup=False,
@@ -43,25 +153,22 @@ with DAG(
     t0 = DummyOperator(
         task_id='task0',
         inlets={
-            "tables": ["Table A"],
-            "more_tables": ["Table X"]
+            "group_A": ["Table A"],
+            "group_B": ["Table X"]
         }
     )
     
     t1 = DummyOperator(
         task_id='task10',
         outlets={
-            "tables": ["Table B"],
-            "more_tables": ["Table Y"]
+            "group_A": ["Table B"],
+            "group_B": ["Table Y"]
         }
     )
 
     t0 >> t1
 ```
 
-Note how we have two tasks:
-- `t0`: Informing the `inlets`, with keys `tables` and `more_tables`.
-- `t1`: Informing the `outlets` with keys `tables` and `more_tables`.
 
 {% note %}
 
@@ -70,32 +177,3 @@ Make sure to add the table Fully Qualified Name (FQN), which is the unique name 
 This name is composed as `serviceName.databaseName.schemaName.tableName`.
 
 {% /note %}
-
-What it's important to consider here is that when we are ingesting Airflow lineage, we are actually building a graph:
-
-```
-Table A (node) -> DAG (edge) -> Table B (node)
-```
-
-Where tables are nodes and DAGs (Pipelines) are considered edges. This means that the correct way of setting this
-parameters is by making sure that we are informing both `inlets` and `outlets`, so that we have the nodes to build
-the relationship.
-
-## Keys
-
-We can inform the lineage dependencies among different groups of tables. In the example above, we are not building the
-lineage from all inlets to all outlets, but rather grouping the tables by the dictionary key (`tables` and `more_tables`).
-This means that after this lineage is processed, the relationship will be:
-
-```
-Table A (node) -> DAG (edge) -> Table B (node)
-```
-
-and
-
-```
-Table X (node) -> DAG (edge) -> Table Y (node)
-```
-
-It does not matter in which task of the DAG these inlet/outlet information is specified. During the ingestion process we
-group all these details at the DAG level.
