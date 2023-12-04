3884b4e25d3d9796a121a76e2a6a2946b55ca93a
Mohit Yadav <105265192+mohityadav766@users.noreply.github.com>
2023-12-04 19:06:16 +0530

    Add Bulk Assets for Domain , Data product and Team (#14222)
    * Add Bulk Assets for Domain , Data product and Team
    
    * repetitive code

--- openmetadata-service/src/main/java/org/openmetadata/service/jdbi3/DomainRepository.java
+++ openmetadata-service/src/main/java/org/openmetadata/service/jdbi3/DomainRepository.java
@@ -24,6 +24,8 @@ import org.openmetadata.schema.entity.domains.Domain;
 import org.openmetadata.schema.type.EntityReference;
 import org.openmetadata.schema.type.Include;
 import org.openmetadata.schema.type.Relationship;
+import org.openmetadata.schema.type.api.BulkAssets;
+import org.openmetadata.schema.type.api.BulkOperationResult;
 import org.openmetadata.service.Entity;
 import org.openmetadata.service.resources.domains.DomainResource;
 import org.openmetadata.service.util.EntityUtil.Fields;
@@ -88,6 +90,16 @@ public class DomainRepository extends EntityRepository<Domain> {
       inheritExperts(domain, fields, parent);
     }
     return domain;
+  }
+
+  public BulkOperationResult bulkAddAssets(String domainName, BulkAssets request) {
+    Domain domain = getByName(null, domainName, getFields("id"));
+    return bulkAssetsOperation(domain.getId(), DOMAIN, Relationship.HAS, request, true);
+  }
+
+  public BulkOperationResult bulkRemoveAssets(String domainName, BulkAssets request) {
+    Domain domain = getByName(null, domainName, getFields("id"));
+    return bulkAssetsOperation(domain.getId(), DOMAIN, Relationship.HAS, request, false);
   }
 
   @Override
