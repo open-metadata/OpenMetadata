c7e32c8abf7455d53bfb1d521e933d9aabc74db9
Onkar Ravgan <onkar.10r@gmail.com>
2023-12-06 19:00:31 +0530

    Fixes: 14240 and Fixes 13365: Bulk patch descriptions from dbt (#13999)

--- openmetadata-service/src/main/java/org/openmetadata/service/jdbi3/DatabaseSchemaRepository.java
+++ openmetadata-service/src/main/java/org/openmetadata/service/jdbi3/DatabaseSchemaRepository.java
@@ -158,6 +158,7 @@ public class DatabaseSchemaRepository extends EntityRepository<DatabaseSchema> {
     public void entitySpecificUpdate() {
       recordChange("retentionPeriod", original.getRetentionPeriod(), updated.getRetentionPeriod());
       recordChange("sourceUrl", original.getSourceUrl(), updated.getSourceUrl());
+      recordChange("sourceHash", original.getSourceHash(), updated.getSourceHash());
     }
   }
 
