a6679cb363c0f3dc0d64895972b17df5984f80cc
Sachin Chaurasiya <sachinchaurasiyachotey87@gmail.com>
2023-12-07 12:10:39 +0530

    minor(#13715): add table support in block editor

---
+++ openmetadata-ui/src/main/resources/ui/src/components/BlockEditor/TableMenu/TableMenu.tsx
@@ -0,0 +1,135 @@
+/*
+ *  Copyright 2023 Collate.
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+import { Editor } from '@tiptap/react';
+import { Button, Space, Tooltip } from 'antd';
+import React, { useCallback, useEffect, useRef } from 'react';
+import tippy, { Instance } from 'tippy.js';
+import { ReactComponent as IconDeleteTable } from '../../../assets/svg/ic-delete.svg';
+import { ReactComponent as IconAddColumnAfter } from '../../../assets/svg/ic-format-add-column-after.svg';
+import { ReactComponent as IconAddRowAfter } from '../../../assets/svg/ic-format-add-row-after.svg';
+import { ReactComponent as IconDeleteColumn } from '../../../assets/svg/ic-format-delete-column.svg';
+import { ReactComponent as IconDeleteRow } from '../../../assets/svg/ic-format-delete-row.svg';
+
+interface TableMenuProps {
+  editor: Editor;
+}
+
+const TableMenu = (props: TableMenuProps) => {
+  const { editor } = props;
+  const { view } = editor;
+  const menuRef = useRef<HTMLDivElement>(null);
+  const popup = useRef<Instance | null>(null);
+
+  const handleMouseDown = useCallback((event: MouseEvent) => {
+    const target = event.target as HTMLElement;
+    const table = target.closest('[data-om-table]');
+
+    if (table && table.contains(target)) {
+      popup.current?.setProps({
+        getReferenceClientRect: () => table.getBoundingClientRect(),
+      });
+
+      popup.current?.show();
+    }
+  }, []);
+
+  useEffect(() => {
+    if (menuRef.current) {
+      menuRef.current.remove();
+      menuRef.current.style.visibility = 'visible';
+
+      popup.current = tippy(view.dom, {
+        getReferenceClientRect: null,
+        content: menuRef.current,
+        appendTo: 'parent',
+        trigger: 'manual',
+        interactive: true,
+        arrow: false,
+        placement: 'top',
+        hideOnClick: true,
+        onShown: () => {
+          menuRef.current?.focus();
+        },
+      });
+    }
+
+    return () => {
+      popup.current?.destroy();
+      popup.current = null;
+    };
+  }, []);
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleMouseDown);
+
+    return () => {
+      document.removeEventListener('mousedown', handleMouseDown);
+    };
+  }, [handleMouseDown]);
+
+  return (
+    <div className="table-menu" ref={menuRef}>
+      <Space size="middle">
+        <Tooltip showArrow={false} title="Add row after current row">
+          <Button
+            data-testid="Add row after current row"
+            type="text"
+            onClick={() => editor.chain().focus().addRowAfter().run()}>
+            <IconAddRowAfter style={{ verticalAlign: 'middle' }} />
+          </Button>
+        </Tooltip>
+
+        <Tooltip showArrow={false} title="Add column after current column">
+          <Button
+            data-testid="Add column after current column"
+            type="text"
+            onClick={() => editor.chain().focus().addColumnAfter().run()}>
+            <IconAddColumnAfter style={{ verticalAlign: 'middle' }} />
+          </Button>
+        </Tooltip>
+
+        <Tooltip showArrow={false} title="Delete current row">
+          <Button
+            data-testid="Delete current row"
+            type="text"
+            onClick={() => editor.chain().focus().deleteRow().run()}>
+            <IconDeleteRow style={{ verticalAlign: 'middle' }} />
+          </Button>
+        </Tooltip>
+
+        <Tooltip showArrow={false} title="Delete current column">
+          <Button
+            data-testid="Delete current col"
+            type="text"
+            onClick={() => editor.chain().focus().deleteColumn().run()}>
+            <IconDeleteColumn style={{ verticalAlign: 'middle' }} />
+          </Button>
+        </Tooltip>
+
+        <Tooltip showArrow={false} title="Delete table">
+          <Button
+            data-testid="Delete table"
+            type="text"
+            onClick={() => {
+              editor.chain().focus().deleteTable().run();
+              popup.current?.hide();
+            }}>
+            <IconDeleteTable style={{ verticalAlign: 'middle' }} width={14} />
+          </Button>
+        </Tooltip>
+      </Space>
+    </div>
+  );
+};
+
+export default TableMenu;
