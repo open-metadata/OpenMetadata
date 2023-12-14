77bb2e637b537c00a67aa6e631a7e5e1ab032828
Shailesh Parmar <shailesh.parmar.webdev@gmail.com>
2023-12-14 12:21:55 +0530

    updated icon

--- openmetadata-ui/src/main/resources/ui/src/constants/LeftSidebar.constants.ts
+++ openmetadata-ui/src/main/resources/ui/src/constants/LeftSidebar.constants.ts
@@ -16,6 +16,8 @@ import { ReactComponent as GovernIcon } from '../assets/svg/bank.svg';
 import { ReactComponent as ClassificationIcon } from '../assets/svg/classification.svg';
 import { ReactComponent as ExploreIcon } from '../assets/svg/globalsearch.svg';
 import { ReactComponent as GlossaryIcon } from '../assets/svg/glossary.svg';
+import { ReactComponent as IncidentMangerIcon } from '../assets/svg/ic-bulb.svg';
+import { ReactComponent as DataContractIcon } from '../assets/svg/ic-data-contract.svg';
 import { ReactComponent as DomainsIcon } from '../assets/svg/ic-domain.svg';
 import { ReactComponent as QualityIcon } from '../assets/svg/ic-quality-v1.svg';
 import { ReactComponent as SettingsIcon } from '../assets/svg/ic-settings-v1.svg';
@@ -41,14 +43,14 @@ export const SIDEBAR_LIST = [
         key: 'data-contract',
         label: i18next.t('label.data-contract-plural'),
         redirect_url: ROUTES.DATA_QUALITY,
-        icon: QualityIcon,
+        icon: DataContractIcon,
         dataTestId: 'app-bar-item-data-contract',
       },
       {
         key: 'incident-manager',
         label: i18next.t('label.incident-manager'),
         redirect_url: ROUTES.INCIDENT_MANAGER,
-        icon: QualityIcon,
+        icon: IncidentMangerIcon,
         dataTestId: 'app-bar-item-incident-manager',
       },
     ],
