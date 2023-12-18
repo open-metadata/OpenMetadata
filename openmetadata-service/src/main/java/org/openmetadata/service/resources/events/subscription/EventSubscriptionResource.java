6cae3cec5739d07dd51e41d7283db52f2ffdf4b6
Sriharsha Chintalapani <harsha@getcollate.io>
2023-12-17 21:17:44 -0800

    Fix EventSubscription tests

--- openmetadata-service/src/main/java/org/openmetadata/service/resources/events/subscription/EventSubscriptionResource.java
+++ openmetadata-service/src/main/java/org/openmetadata/service/resources/events/subscription/EventSubscriptionResource.java
@@ -58,7 +58,6 @@ import org.openmetadata.schema.entity.events.EventSubscription;
 import org.openmetadata.schema.entity.events.SubscriptionStatus;
 import org.openmetadata.schema.type.EntityHistory;
 import org.openmetadata.schema.type.Function;
-import org.openmetadata.schema.type.Include;
 import org.openmetadata.schema.type.MetadataOperation;
 import org.openmetadata.schema.type.SubscriptionResourceDescriptor;
 import org.openmetadata.service.Entity;
@@ -304,27 +303,6 @@ public class EventSubscriptionResource extends EntityResource<EventSubscription,
     return response;
   }
 
-  @PUT
-  @Path("/trigger/{id}")
-  @Operation(
-      operationId = "triggerDataInsightJob",
-      summary = "Trigger a existing Data Insight Report Job to run",
-      description = "Trigger a existing Data Insight Report Job to run",
-      responses = {
-        @ApiResponse(responseCode = "200", description = "Trigger a Data Insight Job"),
-        @ApiResponse(responseCode = "400", description = "Bad request")
-      })
-  public Response triggerDataInsightJob(
-      @Context UriInfo uriInfo,
-      @Context SecurityContext securityContext,
-      @Parameter(description = "Id of the event Subscription", schema = @Schema(type = "UUID")) @PathParam("id")
-          UUID id)
-      throws SchedulerException {
-    authorizer.authorizeAdmin(securityContext);
-    EventSubscription eventSub = repository.find(id, Include.NON_DELETED);
-    return ReportsHandler.getInstance().triggerExistingDataInsightJob(eventSub);
-  }
-
   @PATCH
   @Path("/{id}")
   @Operation(
@@ -367,7 +345,6 @@ public class EventSubscriptionResource extends EntityResource<EventSubscription,
       @Context SecurityContext securityContext,
       @Parameter(description = "Id of the Event Subscription", schema = @Schema(type = "UUID")) @PathParam("id")
           UUID id) {
-    authorizer.authorizeAdmin(securityContext);
     return super.listVersionsInternal(securityContext, id);
   }
 
@@ -397,7 +374,6 @@ public class EventSubscriptionResource extends EntityResource<EventSubscription,
               schema = @Schema(type = "string", example = "0.1 or 1.1"))
           @PathParam("version")
           String version) {
-    authorizer.authorizeAdmin(securityContext);
     return super.getVersionInternal(securityContext, id, version);
   }
 
