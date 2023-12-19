d9dbe7a518fe28a94680d8e1d33533a1001e738b
dhruvinmaniar123 <maniardhruvin21@gmail.com>
2023-12-19 20:24:20 +0530

    Minor: Bump ingestion package

--- ingestion/setup.py
+++ ingestion/setup.py
@@ -266,7 +266,7 @@ test = {
 build_options = {"includes": ["_cffi_backend"]}
 setup(
     name="openmetadata-ingestion",
-    version="1.1.7.3",
+    version="1.1.7.4",
     url="https://open-metadata.org/",
     author="OpenMetadata Committers",
     license="Apache License 2.0",
