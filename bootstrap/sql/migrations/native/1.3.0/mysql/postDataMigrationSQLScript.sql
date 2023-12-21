4749f87c574ad9858f34367e7700509d9dd4c688
Teddy Crepineau <teddy.crepineau@gmail.com>
2023-12-21 09:57:40 +0100

    chore: add test definition migration

--- bootstrap/sql/migrations/native/1.3.0/mysql/postDataMigrationSQLScript.sql
+++ bootstrap/sql/migrations/native/1.3.0/mysql/postDataMigrationSQLScript.sql
@@ -4,7 +4,7 @@ SET json = REPLACE(json, '"customMetricsProfile"', '"customMetrics"');
 
 -- Delete customMetricsProfile from entity_extension
 -- This was not supported on the processing side before 1.3.
-DELETE FROM openmetadata_db.entity_extension ee   
+DELETE FROM entity_extension ee   
 where extension  like '%customMetrics';
 
 -- BEGIN: Incident Manager Migration
@@ -18,3 +18,18 @@ WHERE json -> '$.testCaseResult.testCaseFailureStatus' IS NOT NULL;
 UPDATE data_quality_data_time_series d
 SET json = JSON_REMOVE(json, '$.testCaseFailureStatus');
 -- END: Incident Manager Migration
+
+
+-- Test Case passed/failed row level migration
+UPDATE test_definition
+SET json = JSON_SET(json, '$.supportsRowLevelPassedFailed', true)
+WHERE name IN (
+		'columnValuesToBeUnique',
+		'columnValueLengthsToBeBetween',
+		'columnValuesToBeBetween',
+		'columnValuesToBeInSet',
+		'columnValuesToBeNotInSet',
+		'columnValuesToBeNotNull',
+		'columnValuesToMatchRegex',
+		'columnValuesToNotMatchRegex'
+	)
