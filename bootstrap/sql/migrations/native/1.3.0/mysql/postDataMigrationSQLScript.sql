f83c1a7e044b0b885a6aef33f89566ec9d38ae29
Teddy Crepineau <teddy.crepineau@gmail.com>
2023-12-21 11:02:47 +0100

    chore: clean up code

--- bootstrap/sql/migrations/native/1.3.0/mysql/postDataMigrationSQLScript.sql
+++ bootstrap/sql/migrations/native/1.3.0/mysql/postDataMigrationSQLScript.sql
@@ -4,7 +4,7 @@ SET json = REPLACE(json, '"customMetricsProfile"', '"customMetrics"');
 
 -- Delete customMetricsProfile from entity_extension
 -- This was not supported on the processing side before 1.3.
-DELETE FROM openmetadata_db.entity_extension ee   
+DELETE FROM entity_extension ee   
 where extension  like '%customMetrics';
 
 -- BEGIN: Incident Manager Migration
