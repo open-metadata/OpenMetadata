a7c9435368b069f6312c33d02ca16422c6509a8b
Pere Miquel Brull <peremiquelbrull@gmail.com>
2023-12-22 07:56:14 +0100

    Print

--- ingestion/tests/integration/lineage/airflow/test_airflow_lineage.py
+++ ingestion/tests/integration/lineage/airflow/test_airflow_lineage.py
@@ -55,6 +55,7 @@ from metadata.generated.schema.security.client.openMetadataJWTClientConfig impor
 )
 from metadata.ingestion.ometa.ometa_api import OpenMetadata
 
+# These variables are just here to validate elements in the local deployment
 OM_HOST_PORT = "http://localhost:8585/api"
 OM_JWT = "eyJraWQiOiJHYjM4OWEtOWY3Ni1nZGpzLWE5MmotMDI0MmJrOTQzNTYiLCJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJhZG1pbiIsImlzQm90IjpmYWxzZSwiaXNzIjoib3Blbi1tZXRhZGF0YS5vcmciLCJpYXQiOjE2NjM5Mzg0NjIsImVtYWlsIjoiYWRtaW5Ab3Blbm1ldGFkYXRhLm9yZyJ9.tS8um_5DKu7HgzGBzS1VTA5uUjKWOCU0B_j08WXBiEC0mr0zNREkqVfwFDD-d24HlNEbrqioLsBuFRiwIWKc1m_ZlVQbG7P36RUxhuv2vbSp80FKyNM-Tj93FDzq91jsyNmsQhyNv_fNr3TXfzzSPjHt8Go0FMMP66weoKMgW2PbXlhVKwEuXUHyakLLzewm9UMeQaEiRzhiTMU3UkLXcKbYEJJvfNFcLwSl9W8JCO_l0Yj3ud-qt_nQYEZwqW6u5nfdQllN133iikV4fM5QZsMCnm8Rq1mvLR0y9bmJiD7fwM1tmJ791TUWqmKaTnP49U493VanKpUAfzIiOiIbhg"
 AIRFLOW_HOST_API_ROOT = "http://localhost:8080/api/v1/"
@@ -248,6 +249,14 @@ class AirflowLineageTest(TestCase):
 
         if state != "success":
             raise RuntimeError(f"DAG {OM_LINEAGE_DAG_NAME} has not finished on time.")
+
+        # curl -XGET http://localhost:8080/api/v1/openmetadata/last_dag_logs?dag_id=lineage_tutorial_operator&task_id=lineage_op
+        res = requests.get(
+            AIRFLOW_HOST_API_ROOT + f"openmetadata/last_dag_logs?dag_id=lineage_tutorial_operator&task_id=lineage_op",
+            headers=DEFAULT_AIRFLOW_HEADERS,
+        )
+        print("EXECUTION LOG")
+        print(res.text)
 
     @pytest.mark.order(2)
     def test_pipeline_created(self) -> None:
