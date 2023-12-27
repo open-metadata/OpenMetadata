6d34d5ebccc9f900403ab5328b1a7de36721a694
Sriharsha Chintalapani <harsha@getcollate.io>
2023-12-27 12:07:44 -0800

    add subcommands

--- openmetadata-service/src/main/java/org/openmetadata/service/util/OpenMetadataOperations.java
+++ openmetadata-service/src/main/java/org/openmetadata/service/util/OpenMetadataOperations.java
@@ -15,7 +15,6 @@ import io.dropwizard.jackson.Jackson;
 import io.dropwizard.jersey.validation.Validators;
 import java.io.File;
 import java.nio.charset.StandardCharsets;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
@@ -58,7 +57,6 @@ import org.openmetadata.service.search.SearchRepository;
 import org.openmetadata.service.secrets.SecretsManager;
 import org.openmetadata.service.secrets.SecretsManagerFactory;
 import org.openmetadata.service.util.jdbi.DatabaseAuthenticationProviderFactory;
-import org.quartz.SchedulerException;
 import org.slf4j.LoggerFactory;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
@@ -80,196 +78,238 @@ public class OpenMetadataOperations implements Callable<Integer> {
   private SearchRepository searchRepository;
   private String nativeSQLScriptRootPath;
   private String extensionSQLScriptRootPath;
-  private String flywayRootPath;
   private SecretsManager secretsManager;
   private CollectionDAO collectionDAO;
 
-  @CommandLine.ArgGroup(exclusive = false, multiplicity = "1", order = 1, heading = "Config%n")
-  DefaultConfig defaultConfig;
+  @Option(
+      names = {"-d", "--debug"},
+      defaultValue = "false")
+  private boolean debug;
 
-  @CommandLine.ArgGroup(multiplicity = "1", order = 2, heading = "OpenMetadata Operations %n")
-  OpenMetadataOptions options;
-
-  static class DefaultConfig {
-    @Option(
-        names = {"-c", "--config"},
-        description = "OpenMetadata config file",
-        required = true)
-    String configFilePath;
-
-    @Option(
-        names = {"-debug", "--debug"},
-        description = "Prints Debug logs",
-        required = false)
-    boolean debug;
-
-    @Option(
-        names = {"-force", "--force"},
-        description =
-            "Forces migrate to re-run the previously ran SQL statements. "
-                + "Only use this option, if the migrate option didn't worked.",
-        required = false)
-    boolean force;
-  }
-
-  static class OpenMetadataOptions {
-
-    @Option(
-        names = {"-dc", "--drop-create"},
-        description =
-            "Deletes any tables in configured database and creates a new tables "
-                + "based on current version of OpenMetadata. This command also re-creates the search indexes.",
-        required = true)
-    boolean dropCreate;
-
-    @Option(
-        names = {"-m", "--migrate"},
-        description = "Migrates the OpenMetadata database schema and search index mappings.",
-        required = true)
-    boolean migrate;
-
-    @Option(
-        names = {"-ri", "--re-index"},
-        description = "Re Indexes data into search engine from command line.",
-        required = true)
-    boolean reIndex;
-
-    @Option(
-        names = {"-dp", "--deploy-pipelines"},
-        description = "Deploy all the service pipelines.",
-        required = true)
-    boolean deployPipelines;
-
-    @Option(
-        names = {"-validate", "--validate"},
-        description =
-            "Checks if the all the migrations haven been applied on " + "the target database.",
-        required = false)
-    boolean validate;
-
-    @Option(
-        names = {"-info", "--info"},
-        description =
-            "Shows the list of migrations applied and the pending migration "
-                + "waiting to be applied on the target database",
-        required = false)
-    boolean info;
-
-    @Option(
-        names = {"-repair", "--repair"},
-        description =
-            "Repairs the DATABASE_CHANGE_LOG table which is used to track"
-                + "all the migrations on the target database This involves removing entries for the failed migrations and update"
-                + "the checksum of migrations already applied on the target database",
-        required = false)
-    boolean repair;
-
-    @Option(
-        names = {"-check-connection", "--check-connection"},
-        description =
-            "Checks if a connection can be " + "successfully obtained for the target database",
-        required = false)
-    boolean checkConnection;
-
-    @Option(
-        names = {"-rotate-fernet-key", "--rotate-fernet-key"},
-        description = "Rotate the Fernet Key defined in " + "$FERNET_KEY",
-        required = false)
-    boolean rotate;
-  }
+  @Option(
+      names = {"-c", "--config"},
+      required = true)
+  private String configFilePath;
 
   @Override
-  public Integer call() throws Exception {
+  public Integer call() {
+    LOG.info(
+        "Subcommand needed: 'info', 'validate', 'repair', 'check-connection', "
+            + "'drop-create', 'migrate', 'reindex', 'deploy-pipelines'");
+    return 0;
+  }
+
+  @Command(
+      name = "info",
+      description =
+          "Shows the list of migrations applied and the pending migration "
+              + "waiting to be applied on the target database")
+  public Integer info() {
     try {
-      if (defaultConfig.debug) {
-        Logger root = (Logger) LoggerFactory.getLogger(org.slf4j.Logger.ROOT_LOGGER_NAME);
-        root.setLevel(Level.DEBUG);
-      }
       parseConfig();
-      if (options.dropCreate) {
-        dropCreate();
-      } else if (options.migrate) {
-        migrate();
-      } else if (options.reIndex) {
-        reIndex();
-      } else if (options.deployPipelines) {
-        deployPipelines();
-      } else if (options.info) {
-        info();
-      } else if (options.validate) {
-        validate();
-      } else if (options.repair) {
-        repair();
-      } else if (options.checkConnection) {
-        checkConnection();
-      }
+      LOG.info(dumpToAsciiTable(flyway.info().all()));
       return 0;
     } catch (Exception e) {
-      LOG.error("Failed due to", e);
+      LOG.error("Failed due to ", e);
       return 1;
     }
   }
 
-  private void info() {
-    LOG.info(dumpToAsciiTable(flyway.info().all()));
+  @Command(
+      name = "validate",
+      description =
+          "Checks if the all the migrations haven been applied " + "on the target database.")
+  public Integer validate() {
+    try {
+      parseConfig();
+      flyway.validate();
+      return 0;
+    } catch (Exception e) {
+      LOG.error("Database migration validation failed due to ", e);
+      return 1;
+    }
   }
 
-  private void validate() {
-    flyway.validate();
+  @Command(
+      name = "repair",
+      description =
+          "Repairs the DATABASE_CHANGE_LOG table which is used to track"
+              + "all the migrations on the target database This involves removing entries for the failed migrations and update"
+              + "the checksum of migrations already applied on the target database")
+  public Integer repair() {
+    try {
+      parseConfig();
+      flyway.repair();
+      return 0;
+    } catch (Exception e) {
+      LOG.error("Repair of CHANGE_LOG failed due to ", e);
+      return 1;
+    }
   }
 
-  private void repair() {
-    flyway.repair();
+  @Command(
+      name = "check-connection",
+      description =
+          "Checks if a connection can be successfully " + "obtained for the target database")
+  public Integer checkConnection() {
+    try {
+      parseConfig();
+      flyway.getConfiguration().getDataSource().getConnection();
+      return 0;
+    } catch (Exception e) {
+      LOG.error("Failed to check connection due to ", e);
+      return 1;
+    }
   }
 
-  private void checkConnection() throws SQLException {
-    flyway.getConfiguration().getDataSource().getConnection();
+  @Command(
+      name = "drop-create",
+      description =
+          "Deletes any tables in configured database and creates a new tables "
+              + "based on current version of OpenMetadata. This command also re-creates the search indexes.")
+  public Integer dropCreate() {
+    try {
+      promptUserForDelete();
+      parseConfig();
+      LOG.info("Deleting all the OpenMetadata tables.");
+      flyway.clean();
+      LOG.info("Creating the OpenMetadata Schema.");
+      flyway.migrate();
+      validateAndRunSystemDataMigrations(true);
+      LOG.info("OpenMetadata Database Schema is Updated.");
+      return 0;
+    } catch (Exception e) {
+      LOG.error("Failed to drop create due to ", e);
+      return 1;
+    }
   }
 
-  private void dropCreate() {
-    promptUserForDelete();
-    LOG.info("Deleting all the OpenMetadata tables.");
-    flyway.clean();
-    LOG.info("Creating the OpenMetadata Schema.");
-    flyway.migrate();
-    validateAndRunSystemDataMigrations();
-    LOG.info("OpenMetadata Database Schema is Updated.");
-    LOG.info("Deleting Search Indexes.");
-    searchRepository.dropIndexes();
-    LOG.info("Creating Search Indexes.");
-    searchRepository.createIndexes();
-    LOG.info("OpenMetadata DropCreate operation is completed.");
+  @Command(
+      name = "migrate",
+      description = "Migrates the OpenMetadata database schema and search index mappings.")
+  public Integer migrate(
+      @Option(
+              names = {"--force"},
+              description = "Forces migrations to be run again, even if they have ran previously",
+              defaultValue = "false")
+          boolean force) {
+    try {
+      LOG.info("Migrating the OpenMetadata Schema.");
+      parseConfig();
+      flyway.migrate();
+      validateAndRunSystemDataMigrations(force);
+      return 0;
+    } catch (Exception e) {
+      LOG.error("Failed to db migration due to ", e);
+      return 1;
+    }
   }
 
-  private void migrate() {
-    LOG.info("Migrating the OpenMetadata Schema.");
-    flyway.migrate();
-    validateAndRunSystemDataMigrations();
-    LOG.info("Migrating the Search Indexes.");
-    searchRepository.updateIndexes();
-    LOG.info("OpenMetadata Migrate operation is completed.");
+  @Command(name = "reindex", description = "Re Indexes data into search engine from command line.")
+  public Integer reIndex(
+      @Option(
+              names = {"-b", "--batch-size"},
+              defaultValue = "100")
+          int batchSize,
+      @Option(
+              names = {"--recreate-indexes"},
+              defaultValue = "true")
+          boolean recreateIndexes) {
+    try {
+      parseConfig();
+      AppScheduler.initialize(collectionDAO, searchRepository);
+      App searchIndexApp =
+          new App()
+              .withId(UUID.randomUUID())
+              .withName("SearchIndexApp")
+              .withClassName("org.openmetadata.service.apps.bundles.searchIndex.SearchIndexApp")
+              .withAppSchedule(
+                  new AppSchedule().withScheduleType(AppSchedule.ScheduleTimeline.DAILY))
+              .withAppConfiguration(
+                  new EventPublisherJob()
+                      .withEntities(new HashSet<>(List.of("all")))
+                      .withRecreateIndex(recreateIndexes)
+                      .withBatchSize(batchSize)
+                      .withSearchIndexMappingLanguage(
+                          config.getElasticSearchConfiguration().getSearchIndexMappingLanguage()))
+              .withRuntime(new ScheduledExecutionContext().withEnabled(true));
+      AppScheduler.getInstance().triggerOnDemandApplication(searchIndexApp);
+      return 0;
+    } catch (Exception e) {
+      LOG.error("Failed to reindex due to ", e);
+      return 1;
+    }
   }
 
-  private void reIndex() throws SchedulerException {
-    AppScheduler.initialize(collectionDAO, searchRepository);
-    App searchIndexApp =
-        new App()
-            .withId(UUID.randomUUID())
-            .withName("SearchIndexApp")
-            .withClassName("org.openmetadata.service.apps.bundles.searchIndex.SearchIndexApp")
-            .withAppSchedule(new AppSchedule().withScheduleType(AppSchedule.ScheduleTimeline.DAILY))
-            .withAppConfiguration(
-                new EventPublisherJob()
-                    .withEntities(new HashSet<>(List.of("all")))
-                    .withRecreateIndex(true)
-                    .withBatchSize(100)
-                    .withSearchIndexMappingLanguage(
-                        config.getElasticSearchConfiguration().getSearchIndexMappingLanguage()))
-            .withRuntime(new ScheduledExecutionContext().withEnabled(true));
-    AppScheduler.getInstance().triggerOnDemandApplication(searchIndexApp);
+  @Command(name = "deploy-pipelines", description = "Deploy all the service pipelines.")
+  public Integer deployPipelines() {
+    try {
+      LOG.info("Deploying Pipelines");
+      parseConfig();
+      PipelineServiceClient pipelineServiceClient =
+          PipelineServiceClientFactory.createPipelineServiceClient(
+              config.getPipelineServiceClientConfiguration());
+      IngestionPipelineRepository pipelineRepository =
+          (IngestionPipelineRepository) Entity.getEntityRepository(Entity.INGESTION_PIPELINE);
+      List<IngestionPipeline> pipelines =
+          pipelineRepository.listAll(
+              new EntityUtil.Fields(Set.of(FIELD_OWNER, "service")),
+              new ListFilter(Include.NON_DELETED));
+      LOG.debug(String.format("Pipelines %d", pipelines.size()));
+      List<String> columns = Arrays.asList("Name", "Type", "Service Name", "Status");
+      List<List<String>> pipelineStatuses = new ArrayList<>();
+      for (IngestionPipeline pipeline : pipelines) {
+        deployPipeline(pipeline, pipelineServiceClient, pipelineStatuses);
+      }
+      printToAsciiTable(columns, pipelineStatuses, "No Pipelines Found");
+      return 0;
+    } catch (Exception e) {
+      LOG.error("Failed to deploy pipelines due to ", e);
+      return 1;
+    }
+  }
+
+  private void deployPipeline(
+      IngestionPipeline pipeline,
+      PipelineServiceClient pipelineServiceClient,
+      List<List<String>> pipelineStatuses) {
+    try {
+      LOG.debug(String.format("deploying pipeline %s", pipeline.getName()));
+      pipeline.setOpenMetadataServerConnection(new OpenMetadataConnectionBuilder(config).build());
+      secretsManager.decryptIngestionPipeline(pipeline);
+      OpenMetadataConnection openMetadataServerConnection =
+          new OpenMetadataConnectionBuilder(config).build();
+      pipeline.setOpenMetadataServerConnection(
+          secretsManager.encryptOpenMetadataConnection(openMetadataServerConnection, false));
+      ServiceEntityInterface service =
+          Entity.getEntity(pipeline.getService(), "", Include.NON_DELETED);
+      pipelineServiceClient.deployPipeline(pipeline, service);
+    } catch (Exception e) {
+      LOG.error(
+          String.format(
+              "Failed to deploy pipeline %s of type %s for service %s",
+              pipeline.getName(),
+              pipeline.getPipelineType().value(),
+              pipeline.getService().getName()),
+          e);
+      pipeline.setDeployed(false);
+    } finally {
+      LOG.debug("update the pipeline");
+      collectionDAO.ingestionPipelineDAO().update(pipeline);
+      pipelineStatuses.add(
+          Arrays.asList(
+              pipeline.getName(),
+              pipeline.getPipelineType().value(),
+              pipeline.getService().getName(),
+              pipeline.getDeployed().toString()));
+    }
   }
 
   private void parseConfig() throws Exception {
+    if (debug) {
+      Logger root = (Logger) LoggerFactory.getLogger(org.slf4j.Logger.ROOT_LOGGER_NAME);
+      root.setLevel(Level.DEBUG);
+    }
     ObjectMapper objectMapper = Jackson.newObjectMapper();
     Validator validator = Validators.newValidator();
     YamlConfigurationFactory<OpenMetadataApplicationConfig> factory =
@@ -279,7 +319,7 @@ public class OpenMetadataOperations implements Callable<Integer> {
         factory.build(
             new SubstitutingSourceProvider(
                 new FileConfigurationSourceProvider(), new EnvironmentVariableSubstitutor(false)),
-            defaultConfig.configFilePath);
+            configFilePath);
     Fernet.getInstance().setFernetKey(config);
     DataSourceFactory dataSourceFactory = config.getDataSourceFactory();
     if (dataSourceFactory == null) {
@@ -300,8 +340,9 @@ public class OpenMetadataOperations implements Callable<Integer> {
     String jdbcUrl = dataSourceFactory.getUrl();
     String user = dataSourceFactory.getUser();
     String password = dataSourceFactory.getPassword();
+    assert user != null && password != null;
 
-    flywayRootPath = config.getMigrationConfiguration().getFlywayPath();
+    String flywayRootPath = config.getMigrationConfiguration().getFlywayPath();
     String location =
         "filesystem:"
             + flywayRootPath
@@ -343,54 +384,6 @@ public class OpenMetadataOperations implements Callable<Integer> {
     Entity.initializeRepositories(config, jdbi);
   }
 
-  private void deployPipelines() {
-    PipelineServiceClient pipelineServiceClient =
-        PipelineServiceClientFactory.createPipelineServiceClient(
-            config.getPipelineServiceClientConfiguration());
-    IngestionPipelineRepository pipelineRepository =
-        (IngestionPipelineRepository) Entity.getEntityRepository(Entity.INGESTION_PIPELINE);
-    List<IngestionPipeline> pipelines =
-        pipelineRepository.listAll(
-            new EntityUtil.Fields(Set.of(FIELD_OWNER, "service")),
-            new ListFilter(Include.NON_DELETED));
-    LOG.debug(String.format("Pipelines %d", pipelines.size()));
-    List<String> columns = Arrays.asList("Name", "Type", "Service Name", "Status");
-    List<List<String>> pipelineStatuses = new ArrayList<>();
-    for (IngestionPipeline pipeline : pipelines) {
-      try {
-        LOG.debug(String.format("deploying pipeline %s", pipeline.getName()));
-        pipeline.setOpenMetadataServerConnection(new OpenMetadataConnectionBuilder(config).build());
-        secretsManager.decryptIngestionPipeline(pipeline);
-        OpenMetadataConnection openMetadataServerConnection =
-            new OpenMetadataConnectionBuilder(config).build();
-        pipeline.setOpenMetadataServerConnection(
-            secretsManager.encryptOpenMetadataConnection(openMetadataServerConnection, false));
-        ServiceEntityInterface service =
-            Entity.getEntity(pipeline.getService(), "", Include.NON_DELETED);
-        pipelineServiceClient.deployPipeline(pipeline, service);
-      } catch (Exception e) {
-        LOG.error(
-            String.format(
-                "Failed to deploy pipeline %s of type %s for service %s",
-                pipeline.getName(),
-                pipeline.getPipelineType().value(),
-                pipeline.getService().getName()),
-            e);
-        pipeline.setDeployed(false);
-      } finally {
-        LOG.debug("update the pipeline");
-        collectionDAO.ingestionPipelineDAO().update(pipeline);
-        pipelineStatuses.add(
-            Arrays.asList(
-                pipeline.getName(),
-                pipeline.getPipelineType().value(),
-                pipeline.getService().getName(),
-                pipeline.getDeployed().toString()));
-      }
-    }
-    printToAsciiTable(columns, pipelineStatuses, "No Pipelines Found");
-  }
-
   private void promptUserForDelete() {
     LOG.info(
         """
@@ -410,16 +403,12 @@ public class OpenMetadataOperations implements Callable<Integer> {
     }
   }
 
-  private void validateAndRunSystemDataMigrations() {
+  private void validateAndRunSystemDataMigrations(boolean force) {
     ConnectionType connType = ConnectionType.from(config.getDataSourceFactory().getDriverClass());
     DatasourceConfig.initialize(connType.label);
     MigrationWorkflow workflow =
         new MigrationWorkflow(
-            jdbi,
-            nativeSQLScriptRootPath,
-            connType,
-            extensionSQLScriptRootPath,
-            defaultConfig.force);
+            jdbi, nativeSQLScriptRootPath, connType, extensionSQLScriptRootPath, force);
     workflow.loadMigrations();
     workflow.runMigrationWorkflows();
     Entity.cleanup();
