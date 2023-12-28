04a6ef4042932851e961d91d6576c86e237caf67
sureshms <srini30005@gmail.com>
2023-12-28 13:09:44 -0800

    14492 part3 - Add import export support for Table

--- openmetadata-service/src/main/java/org/openmetadata/service/jdbi3/DatabaseSchemaRepository.java
+++ openmetadata-service/src/main/java/org/openmetadata/service/jdbi3/DatabaseSchemaRepository.java
@@ -24,7 +24,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.Iterator;
 import java.util.List;
 import java.util.UUID;
 import lombok.extern.slf4j.Slf4j;
@@ -261,8 +260,7 @@ public class DatabaseSchemaRepository extends EntityRepository<DatabaseSchema> {
     }
 
     @Override
-    protected void createEntity(CSVPrinter printer, Iterator<CSVRecord> csvRecords)
-        throws IOException {
+    protected void createEntity(CSVPrinter printer, List<CSVRecord> csvRecords) throws IOException {
       CSVRecord csvRecord = getNextRecord(printer, csvRecords);
       String tableFqn = FullyQualifiedName.add(schema.getFullyQualifiedName(), csvRecord.get(0));
       Table table;
