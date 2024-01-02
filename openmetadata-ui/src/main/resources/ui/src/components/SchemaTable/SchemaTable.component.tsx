ec2095943c4ce12806a95fbed6d54f6cfc4f4e1b
Chirag Madlani <12962843+chirag-madlani@users.noreply.github.com>
2024-01-02 17:32:32 +0530

    fix(ui): pass encoded fqn to fetch table permission (#14538)
    (cherry picked from commit a457e7a6641a9846508be7865faf93ef2f13a194)

--- openmetadata-ui/src/main/resources/ui/src/components/SchemaTable/SchemaTable.component.tsx
+++ openmetadata-ui/src/main/resources/ui/src/components/SchemaTable/SchemaTable.component.tsx
@@ -28,6 +28,7 @@ import {
 import { EntityTags, TagFilterOptions } from 'Models';
 import React, { useEffect, useMemo, useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { useParams } from 'react-router-dom';
 import { ReactComponent as IconEdit } from '../../assets/svg/edit-new.svg';
 import FilterTablePlaceHolder from '../../components/common/ErrorWithPlaceholder/FilterTablePlaceHolder';
 import EntityNameModal from '../../components/Modals/EntityNameModal/EntityNameModal.component';
@@ -48,6 +49,7 @@ import {
   getEntityName,
   getFrequentlyJoinedColumns,
 } from '../../utils/EntityUtils';
+import { getDecodedFqn } from '../../utils/StringsUtils';
 import {
   getAllTags,
   searchTagInData,
@@ -79,7 +81,6 @@ const SchemaTable = ({
   joins,
   isReadOnly = false,
   onThreadLinkSelect,
-  entityFqn,
   tableConstraints,
 }: SchemaTableProps) => {
   const { t } = useTranslation();
@@ -89,6 +90,8 @@ const SchemaTable = ({
   const [tablePermissions, setTablePermissions] =
     useState<OperationPermission>();
   const [editColumn, setEditColumn] = useState<Column>();
+  const { fqn: entityFqn } = useParams<{ fqn: string }>();
+  const decodedEntityFqn = getDecodedFqn(entityFqn);
 
   const [editColumnDisplayName, setEditColumnDisplayName] = useState<Column>();
   const { getEntityPermissionByFqn } = usePermissionProvider();
@@ -113,13 +116,16 @@ const SchemaTable = ({
       );
     }
   };
+
   const data = React.useMemo(
     () => makeData(searchedColumns),
     [searchedColumns]
   );
+
   useEffect(() => {
     fetchResourcePermission(entityFqn);
   }, [entityFqn]);
+
   const handleEditColumn = (column: Column): void => {
     setEditColumn(column);
   };
@@ -252,7 +258,7 @@ const SchemaTable = ({
             fqn: record.fullyQualifiedName ?? '',
             field: record.description,
           }}
-          entityFqn={entityFqn}
+          entityFqn={decodedEntityFqn}
           entityType={EntityType.TABLE}
           hasEditPermission={hasDescriptionEditAccess}
           index={index}
@@ -410,7 +416,7 @@ const SchemaTable = ({
         filterIcon: getFilterIcon('tag-filter'),
         render: (tags: TagLabel[], record: Column, index: number) => (
           <TableTags<Column>
-            entityFqn={entityFqn}
+            entityFqn={decodedEntityFqn}
             entityType={EntityType.TABLE}
             handleTagSelection={handleTagSelection}
             hasTagEditAccess={hasTagEditAccess}
@@ -435,7 +441,7 @@ const SchemaTable = ({
         filterIcon: getFilterIcon('glossary-filter'),
         render: (tags: TagLabel[], record: Column, index: number) => (
           <TableTags<Column>
-            entityFqn={entityFqn}
+            entityFqn={decodedEntityFqn}
             entityType={EntityType.TABLE}
             handleTagSelection={handleTagSelection}
             hasTagEditAccess={hasTagEditAccess}
@@ -453,7 +459,7 @@ const SchemaTable = ({
       },
     ],
     [
-      entityFqn,
+      decodedEntityFqn,
       isReadOnly,
       tableConstraints,
       hasTagEditAccess,
