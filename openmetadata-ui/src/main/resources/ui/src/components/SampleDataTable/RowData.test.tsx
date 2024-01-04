831c0869e38d06843128b8e084f7afd2d03e5323
Ashish Gupta <ashish@getcollate.io>
2024-01-04 11:32:55 +0530

    fix the sample data value displayed on UI

--- openmetadata-ui/src/main/resources/ui/src/components/SampleDataTable/RowData.test.tsx
+++ openmetadata-ui/src/main/resources/ui/src/components/SampleDataTable/RowData.test.tsx
@@ -41,8 +41,28 @@ describe('Test RowData Component', () => {
     expect(await findByTestId('schema-modal')).toBeInTheDocument();
   });
 
-  it('Should render string data if data is not a tyeopf object', async () => {
+  it('Should render string data if data is not a typeof object', async () => {
     const { findByTestId } = render(<RowData data="data" />, {
+      wrapper: MemoryRouter,
+    });
+
+    const stringData = await findByTestId('string-data');
+
+    expect(stringData).toBeInTheDocument();
+  });
+
+  it('Should render string data if data value is 0', async () => {
+    const { findByTestId } = render(<RowData data={0} />, {
+      wrapper: MemoryRouter,
+    });
+
+    const stringData = await findByTestId('string-data');
+
+    expect(stringData).toBeInTheDocument();
+  });
+
+  it('Should render string data if data value is empty string is passed', async () => {
+    const { findByTestId } = render(<RowData data="" />, {
       wrapper: MemoryRouter,
     });
 
