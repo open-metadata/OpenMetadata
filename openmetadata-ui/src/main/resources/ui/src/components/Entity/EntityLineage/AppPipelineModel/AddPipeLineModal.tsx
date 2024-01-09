44af8b3a479ddff47e524a187134bace2339fb18
07Himank <112613760+07Himank@users.noreply.github.com>
2024-01-09 22:04:17 +0530

    working on lineage es integration (#14181)
    * working on lineage es integration
    
    * os change
    
    * working
    
    * working on lineage es integration
    
    * add curremt lineage, special character handling, delete lineage
    
    * remove test code
    
    * update lineage index work done
    
    * fix: add lineage ui changes
    
    * fix: lineage add edit
    
    * fix: center on load
    
    * fix: add remove edges
    
    * fix: column lineage issues
    
    * use reactflow controls
    
    * add collapse
    
    * added upstream and downstream depth diffrently
    
    * depth and collapse
    
    * fix loading of dialog
    
    * add includeDeleted in API
    
    * cleanup
    
    * cleanup
    
    * cleanup
    
    * initial unit tests
    
    * fix tests
    
    * cleanup
    
    * update filter query
    
    * fix: allow sql query to update
    
    * use input instead of select
    
    * unit tests
    
    * unit tests
    
    * fix edge removal
    
    * cleanup
    
    * description and deleted
    
    * fix: initial lineage tests
    
    * fix unit tests
    
    * fix: review comments
    
    * added edgedescription
    
    * refactor custom nodes
    
    * refactor condition
    
    * add pipeline status color
    
    * fix unit tests
    
    * cleanup
    
    * cleanup
    
    * fix: lineage url
    
    * depth issue resolved
    
    ---------
    
    Co-authored-by: karanh37 <33024356+karanh37@users.noreply.github.com>
    Co-authored-by: karanh37 <karanh37@gmail.com>

--- openmetadata-ui/src/main/resources/ui/src/components/Entity/EntityLineage/AppPipelineModel/AddPipeLineModal.tsx
+++ openmetadata-ui/src/main/resources/ui/src/components/Entity/EntityLineage/AppPipelineModel/AddPipeLineModal.tsx
@@ -12,44 +12,78 @@
  */
 
 import { Button, Input, Modal } from 'antd';
+import { AxiosError } from 'axios';
 import classNames from 'classnames';
 import { t } from 'i18next';
-import { isEmpty, isUndefined } from 'lodash';
-import React, { useMemo } from 'react';
+import { isUndefined } from 'lodash';
+import React, { useEffect, useMemo, useState } from 'react';
+import { Edge } from 'reactflow';
+import { PAGE_SIZE } from '../../../../constants/constants';
 import { ERROR_PLACEHOLDER_TYPE, SIZE } from '../../../../enums/common.enum';
+import { EntityType } from '../../../../enums/entity.enum';
+import { SearchIndex } from '../../../../enums/search.enum';
 import { EntityReference } from '../../../../generated/entity/type';
-import { getEntityName } from '../../../../utils/EntityUtils';
+import { searchData } from '../../../../rest/miscAPI';
+import {
+  getEntityName,
+  getEntityReferenceFromEntity,
+} from '../../../../utils/EntityUtils';
 import Fqn from '../../../../utils/Fqn';
 import { getEntityIcon } from '../../../../utils/TableUtils';
+import { showErrorToast } from '../../../../utils/ToastUtils';
 import ErrorPlaceHolder from '../../../common/ErrorWithPlaceholder/ErrorPlaceHolder';
 import '../../../FeedEditor/feed-editor.less';
 import './add-pipeline-modal.less';
 
 interface AddPipeLineModalType {
   showAddEdgeModal: boolean;
-  edgeSearchValue: string;
-  selectedEdgeId: string | undefined;
-  edgeOptions: EntityReference[];
+  selectedEdge?: Edge;
   onModalCancel: () => void;
-  onSave: () => void;
+  onSave: (value?: EntityReference) => void;
   onRemoveEdgeClick: (evt: React.MouseEvent<HTMLButtonElement>) => void;
-  onSearch: (value: string) => void;
-  onSelect: (value: string) => void;
 }
 
 const AddPipeLineModal = ({
   showAddEdgeModal,
-  edgeOptions,
-  edgeSearchValue,
-  selectedEdgeId,
+  selectedEdge,
   onRemoveEdgeClick,
   onModalCancel,
   onSave,
-  onSearch,
-  onSelect,
 }: AddPipeLineModalType) => {
+  const currentPipeline = selectedEdge?.data.edge.pipeline;
+  const [edgeSearchValue, setEdgeSearchValue] = useState<string>('');
+  const [edgeSelection, setEdgeSelection] = useState<EntityReference>(
+    currentPipeline ?? {}
+  );
+  const [edgeOptions, setEdgeOptions] = useState<EntityReference[]>([]);
+
+  const getSearchResults = async (value = '*') => {
+    try {
+      const data = await searchData(value, 1, PAGE_SIZE, '', '', '', [
+        SearchIndex.PIPELINE,
+        SearchIndex.STORED_PROCEDURE,
+      ]);
+
+      const edgeOptions = data.data.hits.hits.map((hit) =>
+        getEntityReferenceFromEntity(
+          hit._source,
+          hit._source.entityType as EntityType
+        )
+      );
+
+      setEdgeOptions(edgeOptions);
+    } catch (error) {
+      showErrorToast(
+        error as AxiosError,
+        t('server.entity-fetch-error', {
+          entity: t('label.suggestion-lowercase-plural'),
+        })
+      );
+    }
+  };
+
   const errorPlaceholderEdge = useMemo(() => {
-    if (isEmpty(edgeOptions)) {
+    if (isUndefined(selectedEdge)) {
       if (edgeSearchValue) {
         return (
           <ErrorPlaceHolder
@@ -64,7 +98,11 @@ const AddPipeLineModal = ({
     }
 
     return;
-  }, [edgeOptions, edgeSearchValue]);
+  }, [selectedEdge, edgeSearchValue]);
+
+  useEffect(() => {
+    getSearchResults(edgeSearchValue);
+  }, [edgeSearchValue]);
 
   return (
     <Modal
@@ -84,13 +122,13 @@ const AddPipeLineModal = ({
           data-testid="save-button"
           key="save-btn"
           type="primary"
-          onClick={onSave}>
+          onClick={() => onSave(edgeSelection)}>
           {t('label.save')}
         </Button>,
       ]}
       maskClosable={false}
       open={showAddEdgeModal}
-      title={t(`label.${isUndefined(selectedEdgeId) ? 'add' : 'edit'}-entity`, {
+      title={t(`label.${isUndefined(selectedEdge) ? 'add' : 'edit'}-entity`, {
         entity: t('label.edge'),
       })}
       onCancel={onModalCancel}>
@@ -98,7 +136,7 @@ const AddPipeLineModal = ({
         data-testid="field-input"
         placeholder={t('message.search-for-edge')}
         value={edgeSearchValue}
-        onChange={(e) => onSearch(e.target.value)}
+        onChange={(e) => setEdgeSearchValue(e.target.value)}
       />
 
       <div className="edge-option-container">
@@ -109,10 +147,10 @@ const AddPipeLineModal = ({
           return (
             <div
               className={classNames('edge-option-item gap-2', {
-                active: selectedEdgeId === item.id,
+                active: edgeSelection?.id === item.id,
               })}
               key={item.id}
-              onClick={() => onSelect(item.id)}>
+              onClick={() => setEdgeSelection(item)}>
               <div className="flex-center mention-icon-image">{icon}</div>
               <div>
                 <div className="d-flex flex-wrap">
