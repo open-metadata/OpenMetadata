bc7d4d4a4d5d954cb415dfbc9cb1bd45fab8eb97
ulixius9 <mayursingal9@gmail.com>
2024-01-10 18:54:15 +0530

    bump ingestion version 1.2.4.2

--- ingestion/setup.py
+++ ingestion/setup.py
@@ -312,7 +312,7 @@ e2e_test = {
 build_options = {"includes": ["_cffi_backend"]}
 setup(
     name="openmetadata-ingestion",
-    version="1.2.4.1",
+    version="1.2.4.2",
     url="https://open-metadata.org/",
     author="OpenMetadata Committers",
     license="Apache License 2.0",
