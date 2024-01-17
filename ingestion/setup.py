52c40ab65087ec4d43211ad8c7670313247b12d9
Pere Miquel Brull <peremiquelbrull@gmail.com>
2024-01-17 12:38:08 +0100

    MINOR - Bump ingestion to 1.2.4.3

--- ingestion/setup.py
+++ ingestion/setup.py
@@ -311,7 +311,7 @@ e2e_test = {
 build_options = {"includes": ["_cffi_backend"]}
 setup(
     name="openmetadata-ingestion",
-    version="1.2.4.2",
+    version="1.2.4.3",
     url="https://open-metadata.org/",
     author="OpenMetadata Committers",
     license="Apache License 2.0",
