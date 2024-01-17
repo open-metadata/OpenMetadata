1b4c8d7bbec4804c10cf45c584af31647c4e3edd
NiharDoshi99 <51595473+NiharDoshi99@users.noreply.github.com>
2024-01-17 12:38:42 +0530

    #14727 change all all_ query to dba_ query (#14748)

--- ingestion/src/metadata/ingestion/source/database/oracle/queries.py
+++ ingestion/src/metadata/ingestion/source/database/oracle/queries.py
@@ -14,68 +14,80 @@ SQL Queries used during ingestion
 
 import textwrap
 
-ORACLE_ALL_TABLE_COMMENTS = """
-SELECT 
+ORACLE_ALL_TABLE_COMMENTS = textwrap.dedent(
+    """
+SELECT
 	comments table_comment,
 	LOWER(table_name) "table_name",
 	LOWER(owner) "schema" 	
-FROM all_tab_comments
+FROM dba_tab_comments
 where comments is not null and owner not in ('SYSTEM', 'SYS')
 """
+)
 
 
-ORACLE_ALL_VIEW_DEFINITIONS = """
+ORACLE_ALL_VIEW_DEFINITIONS = textwrap.dedent(
+    """
 SELECT
 LOWER(view_name) AS "view_name",
 LOWER(owner) AS "schema",
 DBMS_METADATA.GET_DDL('VIEW', view_name, owner) AS view_def
-FROM all_views
+FROM DBA_VIEWS
 WHERE owner NOT IN ('SYSTEM', 'SYS')
 UNION ALL
 SELECT
 LOWER(mview_name) AS "view_name",
 LOWER(owner) AS "schema",
 DBMS_METADATA.GET_DDL('MATERIALIZED_VIEW', mview_name, owner) AS view_def
-FROM all_mviews
+FROM DBA_MVIEWS
 WHERE owner NOT IN ('SYSTEM', 'SYS')
 """
+)
 
-GET_MATERIALIZED_VIEW_NAMES = """
-SELECT mview_name FROM all_mviews WHERE owner = :owner
+GET_MATERIALIZED_VIEW_NAMES = textwrap.dedent(
+    """
+SELECT mview_name FROM DBA_MVIEWS WHERE owner = :owner
 """
+)
 
-ORACLE_GET_TABLE_NAMES = """
-SELECT table_name FROM all_tables WHERE 
+ORACLE_GET_TABLE_NAMES = textwrap.dedent(
+    """
+SELECT table_name FROM DBA_TABLES WHERE 
 {tablespace}
 OWNER = :owner  
 AND IOT_NAME IS NULL 
 AND DURATION IS NULL
 AND TABLE_NAME NOT IN 
-(SELECT mview_name FROM all_mviews WHERE owner = :owner)
+(SELECT mview_name FROM DBA_MVIEWS WHERE owner = :owner)
 """
+)
 
-ORACLE_IDENTITY_TYPE = """\
+ORACLE_IDENTITY_TYPE = textwrap.dedent(
+    """\
 col.default_on_null,
 (
 	SELECT id.generation_type || ',' || id.IDENTITY_OPTIONS
-	FROM ALL_TAB_IDENTITY_COLS{dblink} id
+	FROM DBA_TAB_IDENTITY_COLS{dblink} id
 	WHERE col.table_name = id.table_name
 	AND col.column_name = id.column_name
 	AND col.owner = id.owner
 ) AS identity_options
 """
+)
 
-ORACLE_GET_STORED_PROCEDURES = """
+ORACLE_GET_STORED_PROCEDURES = textwrap.dedent(
+    """
 SELECT
     OWNER,
     NAME,
     LINE,
     TEXT
 FROM
-    ALL_SOURCE
+    DBA_SOURCE
 WHERE
     type = 'PROCEDURE' and owner = '{schema}'
 """
+)
 
 ORACLE_GET_STORED_PROCEDURE_QUERIES = textwrap.dedent(
     """
@@ -127,7 +139,8 @@ ORDER BY PROCEDURE_START_TIME DESC
 """
 )
 
-ORACLE_GET_COLUMNS = """
+ORACLE_GET_COLUMNS = textwrap.dedent(
+    """
         SELECT
             col.column_name,
             col.data_type,
@@ -139,7 +152,7 @@ ORACLE_GET_COLUMNS = """
             com.comments,
             col.virtual_column,
             {identity_cols}
-        FROM all_tab_cols{dblink} col
+        FROM DBA_TAB_COLS{dblink} col
         LEFT JOIN all_col_comments{dblink} com
         ON col.table_name = com.table_name
         AND col.column_name = com.column_name
@@ -147,8 +160,10 @@ ORACLE_GET_COLUMNS = """
         WHERE col.table_name = CAST(:table_name AS VARCHAR2(128))
         AND col.hidden_column = 'NO'
     """
+)
 
-ORACLE_QUERY_HISTORY_STATEMENT = """
+ORACLE_QUERY_HISTORY_STATEMENT = textwrap.dedent(
+    """
 SELECT 
   NULL AS user_name,
   NULL AS database_name,
@@ -164,7 +179,9 @@ WHERE OBJECT_STATUS = 'VALID'
   AND SQL_FULLTEXT NOT LIKE '/* {{"app": "OpenMetadata", %%}} */%%'
   AND SQL_FULLTEXT NOT LIKE '/* {{"app": "dbt", %%}} */%%'
   AND TO_TIMESTAMP(FIRST_LOAD_TIME, 'yy-MM-dd/HH24:MI:SS') >= TO_TIMESTAMP('{start_time}', 'yy-MM-dd HH24:MI:SS')
-  AND TO_TIMESTAMP(FIRST_LOAD_TIME, 'yy-MM-dd/HH24:MI:SS') + NUMTODSINTERVAL(ELAPSED_TIME / 1000, 'SECOND') < TO_TIMESTAMP('{end_time}', 'yy-MM-dd HH24:MI:SS')
+  AND TO_TIMESTAMP(FIRST_LOAD_TIME, 'yy-MM-dd/HH24:MI:SS') + NUMTODSINTERVAL(ELAPSED_TIME / 1000, 'SECOND') 
+  < TO_TIMESTAMP('{end_time}', 'yy-MM-dd HH24:MI:SS')
 ORDER BY FIRST_LOAD_TIME DESC 
 OFFSET 0 ROWS FETCH NEXT {result_limit} ROWS ONLY
 """
+)
