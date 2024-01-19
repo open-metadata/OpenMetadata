35f3ebccfadb2275b6f2edd7153f89aef0a72c38
Pere Miquel Brull <peremiquelbrull@gmail.com>
2024-01-19 11:04:22 +0100

    format

--- ingestion/src/metadata/workflow/application.py
+++ ingestion/src/metadata/workflow/application.py
@@ -145,7 +145,7 @@ class ApplicationWorkflow(BaseWorkflow, ABC):
             and self.calculate_success() < SUCCESS_THRESHOLD_VALUE
         ):
             raise WorkflowExecutionError(
-                f"{self.source.name} reported errors: {Summary.from_step(self.source)}"
+                f"{self.runner.name} reported errors: {Summary.from_step(self.source)}"
             )
 
         if raise_warnings and self.runner.get_status().warnings:
