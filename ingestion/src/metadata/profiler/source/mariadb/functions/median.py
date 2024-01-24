e5ecbbd5588f861f2b047f1da82b2d602adbcd76
Nihar Doshi <nihardoshi16@gmail.com>
2024-01-24 15:23:50 +0530

    added comment

--- ingestion/src/metadata/profiler/source/mariadb/functions/median.py
+++ ingestion/src/metadata/profiler/source/mariadb/functions/median.py
@@ -14,4 +14,7 @@ class MariaDBMedianFn(FunctionElement):
 def _(elements, compiler, **kwargs):  # pylint: disable=unused-argument
     col = compiler.process(elements.clauses.clauses[0])
     percentile = elements.clauses.clauses[2].value
+    # According to the documentation available at https://mariadb.com/kb/en/median/#description,
+    # the PERCENTILE_CONT function can be utilized to calculate the median. Therefore, it is
+    # being used in this context.
     return f"PERCENTILE_CONT({percentile:.2f}) WITHIN GROUP (ORDER BY {col}) OVER()"
