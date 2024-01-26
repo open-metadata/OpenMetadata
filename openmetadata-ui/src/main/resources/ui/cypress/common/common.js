cff8839cca6463e37a1eaa39856bcb5528fad2ff
Shailesh Parmar <shailesh.parmar.webdev@gmail.com>
2024-01-26 17:09:03 +0530

    #12452  UI - Cypress Test Coverage (#14877)
    * #12452 UI - Cypress Test Coverage
    
    * added cypress for my data assets
    
    * added cypress coverage around entity summary panel
    
    * fixed permission cypress
    
    * revert visitEntityDetailsPage change
    
    * added cypress coverage on activity feed

--- openmetadata-ui/src/main/resources/ui/cypress/common/common.js
+++ openmetadata-ui/src/main/resources/ui/cypress/common/common.js
@@ -683,13 +683,29 @@ export const mySqlConnectionInput = () => {
 
 export const login = (username, password) => {
   cy.visit('/');
+  interceptURL('POST', '/api/v1/users/login', 'loginUser');
   cy.get('[id="email"]').should('be.visible').clear().type(username);
   cy.get('[id="password"]').should('be.visible').clear().type(password);
 
   // Don't want to show any popup in the tests
   cy.setCookie(`STAR_OMD_USER_${username.split('@')[0]}`, 'true');
 
+  // Get version and set cookie to hide version banner
+  cy.request({
+    method: 'GET',
+    url: `api/v1/system/version`,
+  }).then((res) => {
+    const version = res.body.version;
+    const versionCookie = `VERSION_${version
+      .split('-')[0]
+      .replaceAll('.', '_')}`;
+
+    cy.setCookie(versionCookie, 'true');
+    window.localStorage.setItem('loggedInUsers', username.split('@')[0]);
+  });
+
   cy.get('.ant-btn').contains('Login').should('be.visible').click();
+  verifyResponseStatusCode('@loginUser', 200);
 };
 
 export const selectTeamHierarchy = (index) => {
