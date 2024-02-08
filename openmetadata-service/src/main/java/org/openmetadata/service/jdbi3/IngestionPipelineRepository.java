0047d5b85b1166df740d13240f2830c1de3189c1
Pere Miquel Brull <peremiquelbrull@gmail.com>
2024-02-08 10:38:32 +0100

    MINOR - Add service information on Pipeline Status GET

--- openmetadata-service/src/main/java/org/openmetadata/service/jdbi3/IngestionPipelineRepository.java
+++ openmetadata-service/src/main/java/org/openmetadata/service/jdbi3/IngestionPipelineRepository.java
@@ -232,7 +232,7 @@ public class IngestionPipelineRepository extends EntityRepository<IngestionPipel
 
   public ResultList<PipelineStatus> listPipelineStatus(
       String ingestionPipelineFQN, Long startTs, Long endTs) {
-    IngestionPipeline ingestionPipeline = findByName(ingestionPipelineFQN, Include.NON_DELETED);
+    IngestionPipeline ingestionPipeline = getByName(null, ingestionPipelineFQN, getFields("service"));
     List<PipelineStatus> pipelineStatusList =
         JsonUtils.readObjects(
             getResultsFromAndToTimestamps(
