ff60a0a8539b36eaa07f277c048ddea419b15c29
mohitdeuex <mohit.y@deuexsolutions.com>
2024-02-08 13:24:44 +0530

    Add Migrations

--- openmetadata-service/src/main/java/org/openmetadata/service/migration/postgres/v131/Migration.java
+++ openmetadata-service/src/main/java/org/openmetadata/service/migration/postgres/v131/Migration.java
@@ -4,11 +4,12 @@ import static org.openmetadata.service.migration.utils.v131.MigrationUtil.migrat
 
 import lombok.SneakyThrows;
 import org.jdbi.v3.core.Handle;
+import org.openmetadata.service.jdbi3.CollectionDAO;
 import org.openmetadata.service.migration.api.MigrationProcessImpl;
 import org.openmetadata.service.migration.utils.MigrationFile;
 
 public class Migration extends MigrationProcessImpl {
-
+  private CollectionDAO collectionDAO;
   private Handle handle;
 
   public Migration(MigrationFile migrationFile) {
@@ -19,11 +20,12 @@ public class Migration extends MigrationProcessImpl {
   public void initialize(Handle handle) {
     super.initialize(handle);
     this.handle = handle;
+    this.collectionDAO = handle.attach(CollectionDAO.class);
   }
 
   @Override
   @SneakyThrows
   public void runDataMigration() {
-    migrateCronExpression(handle);
+    migrateCronExpression(collectionDAO);
   }
 }
