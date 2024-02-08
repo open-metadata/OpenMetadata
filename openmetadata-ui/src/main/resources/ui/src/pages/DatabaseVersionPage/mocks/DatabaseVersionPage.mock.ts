d13fc5c6b996806771278ebb37163fe162e46c2d
abhishek332 <itsabhiofficial.21@gmail.com>
2024-02-08 12:06:46 +0530

    added unit test for DatabaseVersionPage

---
+++ openmetadata-ui/src/main/resources/ui/src/pages/DatabaseVersionPage/mocks/DatabaseVersionPage.mock.ts
@@ -0,0 +1,32 @@
+/*
+ *  Copyright 2024 Collate.
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+import { EntityTabs } from '../../../enums/entity.enum';
+
+export const MOCK_PARAMS = {
+  version: '1.2',
+  tab: EntityTabs.SCHEMA,
+};
+
+export const LOADER = 'Loader';
+export const ERROR_PLACEHOLDER = 'ErrorPlaceHolder';
+
+export const DATA_ASSET_VERSION_HEADER = 'DataAssetVersionHeader';
+export const CUSTOM_PROPERTY_TABLE = 'CustomPropertyTable';
+export const DATA_PRODUCT_CONTAINER = 'DataProductsContainer';
+export const ENTITY_VERSION_TIMELINE = 'EntityVersionTimeLine';
+export const DESCRIPTION_V1 = 'DescriptionV1';
+export const DATABASE_SCHEMA_TABLE = 'DatabaseSchemaTable';
+export const TAGS_CONTAINER_V2 = 'TagsContainerV2';
+export const MOCK_FQN = 'mockFQN';
+export const DATABASE_ID = 'database id';
+export const CUSTOM_PROPERTY_TAB_NAME = 'label.custom-property-plural';
