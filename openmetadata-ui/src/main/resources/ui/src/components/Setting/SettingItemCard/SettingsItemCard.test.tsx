f23fd9455b934c3cbdc99881a7103bb0ee2764a9
Harsh Vador <harsh.vador@somaiya.edu>
2024-02-12 12:31:56 +0530

    refactor code

--- openmetadata-ui/src/main/resources/ui/src/components/Setting/SettingItemCard/SettingsItemCard.test.tsx
+++ openmetadata-ui/src/main/resources/ui/src/components/Setting/SettingItemCard/SettingsItemCard.test.tsx
@@ -10,7 +10,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-import { fireEvent, render } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import React from 'react';
 import SettingItemCard from './SettingItemCard.component';
 
@@ -26,44 +26,36 @@ const mockOnClick = jest.fn();
 
 describe('SettingItemCard', () => {
   it('should render Setting Item Card', () => {
-    const { getByText } = render(
-      <SettingItemCard data={mockData} onClick={mockOnClick} />
-    );
+    render(<SettingItemCard data={mockData} onClick={mockOnClick} />);
 
-    expect(getByText('Test Category')).toBeInTheDocument();
-    expect(getByText('This is an test description.')).toBeInTheDocument();
+    expect(screen.getByText('Test Category')).toBeInTheDocument();
+    expect(
+      screen.getByText('This is an test description.')
+    ).toBeInTheDocument();
   });
 
   it('should render icon', () => {
-    const { getByText } = render(
-      <SettingItemCard data={mockData} onClick={mockOnClick} />
-    );
+    render(<SettingItemCard data={mockData} onClick={mockOnClick} />);
 
-    expect(getByText('Test Icon')).toBeInTheDocument();
+    expect(screen.getByText('Test Icon')).toBeInTheDocument();
   });
 
   it('should render label in case no category', () => {
-    const mockData = {
-      key: 'test-key',
-      icon: () => <svg>Test Icon</svg>,
-      label: 'Test Label',
-      description: 'This is an test description.',
-    };
-
-    const { getByText, queryByText } = render(
-      <SettingItemCard data={mockData} onClick={mockOnClick} />
+    render(
+      <SettingItemCard
+        data={{ ...mockData, category: undefined }}
+        onClick={mockOnClick}
+      />
     );
 
-    expect(queryByText('Test Category')).not.toBeInTheDocument();
-    expect(getByText('Test Label')).toBeInTheDocument();
+    expect(screen.queryByText('Test Category')).not.toBeInTheDocument();
+    expect(screen.getByText('Test Label')).toBeInTheDocument();
   });
 
   it('should calls onClick when clicked', () => {
-    const { getByTestId } = render(
-      <SettingItemCard data={mockData} onClick={mockOnClick} />
-    );
+    render(<SettingItemCard data={mockData} onClick={mockOnClick} />);
 
-    fireEvent.click(getByTestId('test-key'));
+    fireEvent.click(screen.getByTestId('test-key'));
 
     expect(mockOnClick).toHaveBeenCalledWith('test-key');
   });
