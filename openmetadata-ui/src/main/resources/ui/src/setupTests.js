90314bab286d820169b11694b03db6103a377c9d
Chirag Madlani <12962843+chirag-madlani@users.noreply.github.com>
2024-02-12 23:05:18 +0530

    fix unit tests

--- openmetadata-ui/src/main/resources/ui/src/setupTests.js
+++ openmetadata-ui/src/main/resources/ui/src/setupTests.js
@@ -15,7 +15,8 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom/extend-expect';
+import '@testing-library/jest-dom';
+import '@testing-library/jest-dom/jest-globals';
 
 // Reference: https://github.com/ant-design/ant-design/issues/21096
 Object.defineProperty(window, 'matchMedia', {
