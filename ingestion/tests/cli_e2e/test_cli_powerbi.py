f0db24259ba471046dddae28d18fe6a55ad949e2
Onkar Ravgan <onkar.10r@gmail.com>
2024-02-14 11:03:17 +0530

    fixed e2e counts (#15171)

--- ingestion/tests/cli_e2e/test_cli_powerbi.py
+++ ingestion/tests/cli_e2e/test_cli_powerbi.py
@@ -71,7 +71,7 @@ class PowerBICliTest(CliCommonDashboard.TestSuite):
         return 22
 
     def expected_filtered_sink_mix(self) -> int:
-        return 43
+        return 42
 
     def expected_dashboards_and_charts_after_patch(self) -> int:
-        return 1
+        return 0
