74c2abd3de021f11d8b1630399605288260a007d
sushi30 <imri.paran@gmail.com>
2024-02-15 14:06:45 +0100

    use int_admin_ometa in test setup

--- ingestion/tests/integration/profiler/test_nosql_profiler.py
+++ ingestion/tests/integration/profiler/test_nosql_profiler.py
@@ -32,6 +32,7 @@ from unittest import TestCase
 from pymongo import MongoClient
 from testcontainers.mongodb import MongoDbContainer
 
+from ingestion.tests.integration.integration_base import int_admin_ometa
 from metadata.generated.schema.entity.data.table import ColumnProfile
 from metadata.generated.schema.entity.services.connections.metadata.openMetadataConnection import (
     OpenMetadataConnection,
@@ -105,14 +106,7 @@ class NoSQLProfiler(TestCase):
 
     @classmethod
     def setUpClass(cls) -> None:
-        server_config = OpenMetadataConnection(
-            hostPort="http://localhost:8585/api",
-            authProvider="openmetadata",
-            securityConfig=OpenMetadataJWTClientConfig(
-                jwtToken="eyJraWQiOiJHYjM4OWEtOWY3Ni1nZGpzLWE5MmotMDI0MmJrOTQzNTYiLCJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJhZG1pbiIsImlzQm90IjpmYWxzZSwiaXNzIjoib3Blbi1tZXRhZGF0YS5vcmciLCJpYXQiOjE2NjM5Mzg0NjIsImVtYWlsIjoiYWRtaW5Ab3Blbm1ldGFkYXRhLm9yZyJ9.tS8um_5DKu7HgzGBzS1VTA5uUjKWOCU0B_j08WXBiEC0mr0zNREkqVfwFDD-d24HlNEbrqioLsBuFRiwIWKc1m_ZlVQbG7P36RUxhuv2vbSp80FKyNM-Tj93FDzq91jsyNmsQhyNv_fNr3TXfzzSPjHt8Go0FMMP66weoKMgW2PbXlhVKwEuXUHyakLLzewm9UMeQaEiRzhiTMU3UkLXcKbYEJJvfNFcLwSl9W8JCO_l0Yj3ud-qt_nQYEZwqW6u5nfdQllN133iikV4fM5QZsMCnm8Rq1mvLR0y9bmJiD7fwM1tmJ791TUWqmKaTnP49U493VanKpUAfzIiOiIbhg"
-            ),
-        )  # type: ignore
-        cls.metadata = OpenMetadata(server_config)
+        cls.metadata = int_admin_ometa()
 
     def setUp(self) -> None:
         self.mongo_container = MongoDbContainer("mongo:7.0.5-jammy")
