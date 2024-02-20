f51a98db39dc943556d7d0c13e779ccfc10e584c
sushi30 <imri.paran@gmail.com>
2024-02-20 08:38:12 +0100

    use native migrations instead of flyway

--- bootstrap/sql/migrations/flyway/org.postgresql.Driver/v016__update_db_connection_info.sql
+++
@@ -1,4 +0,0 @@
--- Add the supportsProfiler field to the MongoDB connection configuration
-UPDATE dbservice_entity
-SET json = jsonb_set(json::jsonb, '{connection,config,supportsProfiler}', 'true'::jsonb)
-WHERE serviceType = 'MongoDB';
