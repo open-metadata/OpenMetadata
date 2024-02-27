5164bb07ffc056f6431cb65307049a58b072eb21
Pablo Takara <pablo.takara@getcollate.io>
2024-02-27 18:05:43 +0100

    Add small comment explaining the if

--- ingestion/src/metadata/ingestion/source/dashboard/metabase/client.py
+++ ingestion/src/metadata/ingestion/source/dashboard/metabase/client.py
@@ -124,6 +124,8 @@ class MetabaseClient:
         try:
             resp_dashboard = self.client.get(f"/dashboard/{dashboard_id}")
             if resp_dashboard:
+                # Small hack needed to support Metabase versions older than 0.48
+                # https://www.metabase.com/releases/metabase-48#fyi--breaking-changes
                 if "ordered_cards" in resp_dashboard:
                     resp_dashboard["dashcards"] = resp_dashboard["ordered_cards"]
                 return MetabaseDashboardDetails(**resp_dashboard)
