086c35a0d94ff65e5be9ba2eb659834d2a068643
Teddy Crepineau <teddy.crepineau@gmail.com>
2024-02-28 09:35:02 +0100

    Revert "add migration for MongoDB supportsProfiler = true (#15254)"
    This reverts commit ec3eb297f59996daff9015d475f8ff8a2db0ad7d.

--- bootstrap/sql/migrations/native/1.4.0/mysql/schemaChanges.sql
+++
@@ -1,4 +0,0 @@
--- Add the supportsProfiler field to the MongoDB connection configuration
-UPDATE dbservice_entity
-SET json = JSON_INSERT(json, '$.connection.config.supportsProfiler', TRUE)
-WHERE serviceType = 'MongoDB';
