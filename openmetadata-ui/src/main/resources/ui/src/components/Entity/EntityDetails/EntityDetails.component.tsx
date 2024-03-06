f323e0b4141a39cfa2963bfd37e83eac526a5de3
Chirag Madlani <12962843+chirag-madlani@users.noreply.github.com>
2024-03-06 10:50:44 +0530

    fix paths

--- openmetadata-ui/src/main/resources/ui/src/components/Entity/EntityDetails/EntityDetails.component.tsx
+++ openmetadata-ui/src/main/resources/ui/src/components/Entity/EntityDetails/EntityDetails.component.tsx
@@ -13,103 +13,15 @@
 import React, { useMemo } from 'react';
 import { useParams } from 'react-router-dom';
 import { EntityType } from '../../../enums/entity.enum';
-import withSuspenseFallback from '../../AppRouter/withSuspenseFallback';
-import DataProductsPage from '../../DataProducts/DataProductsPage/DataProductsPage.component';
-
-const TableDetailsPageV1 = withSuspenseFallback(
-  React.lazy(
-    () => import('../../../pages/TableDetailsPageV1/TableDetailsPageV1')
-  )
-);
-
-const PipelineDetailsPage = withSuspenseFallback(
-  React.lazy(
-    () => import('../../../pages/PipelineDetails/PipelineDetailsPage.component')
-  )
-);
-
-const TopicDetailsPage = withSuspenseFallback(
-  React.lazy(
-    () => import('../../../pages/TopicDetails/TopicDetailsPage.component')
-  )
-);
-
-const DashboardDetailsPage = withSuspenseFallback(
-  React.lazy(
-    () =>
-      import(
-        '../../../pages/DashboardDetailsPage/DashboardDetailsPage.component'
-      )
-  )
-);
-const DatabaseDetails = withSuspenseFallback(
-  React.lazy(
-    () => import('../../../pages/DatabaseDetailsPage/DatabaseDetailsPage')
-  )
-);
-const DatabaseSchemaPageComponent = withSuspenseFallback(
-  React.lazy(
-    () =>
-      import('../../../pages/DatabaseSchemaPage/DatabaseSchemaPage.component')
-  )
-);
-
-const DataModelDetailsPage = withSuspenseFallback(
-  React.lazy(
-    () => import('../../../pages/DataModelPage/DataModelPage.component')
-  )
-);
-
-const MlModelPage = withSuspenseFallback(
-  React.lazy(() => import('../../../pages/MlModelPage/MlModelPage.component'))
-);
-
-const ContainerPage = withSuspenseFallback(
-  React.lazy(() => import('../../../pages/ContainerPage/ContainerPage'))
-);
-
-const SearchIndexDetailsPage = withSuspenseFallback(
-  React.lazy(
-    () => import('../../../pages/SearchIndexDetailsPage/SearchIndexDetailsPage')
-  )
-);
-
-const StoredProcedureDetailsPage = withSuspenseFallback(
-  React.lazy(() => import('../../../pages/StoredProcedure/StoredProcedurePage'))
-);
+import entityUtilClassBase from '../../../utils/EntityUtilClassBase';
 
 export const EntityDetail = () => {
   const { entityType } = useParams<{ entityType: EntityType }>();
 
-  const Component = useMemo(() => {
-    switch (entityType) {
-      case EntityType.DATABASE:
-        return DatabaseDetails;
-      case EntityType.DATABASE_SCHEMA:
-        return DatabaseSchemaPageComponent;
-      case EntityType.PIPELINE:
-        return PipelineDetailsPage;
-      case EntityType.TOPIC:
-        return TopicDetailsPage;
-      case EntityType.DASHBOARD:
-        return DashboardDetailsPage;
-      case EntityType.STORED_PROCEDURE:
-        return StoredProcedureDetailsPage;
-      case EntityType.DASHBOARD_DATA_MODEL:
-        return DataModelDetailsPage;
-      case EntityType.MLMODEL:
-        return MlModelPage;
-      case EntityType.CONTAINER:
-        return ContainerPage;
-      case EntityType.SEARCH_INDEX:
-        return SearchIndexDetailsPage;
-      case EntityType.DATA_PRODUCT:
-        return DataProductsPage;
-      case EntityType.TABLE:
-      default:
-        return TableDetailsPageV1;
-    }
-  }, [entityType]);
+  const Component = useMemo(
+    () => entityUtilClassBase.getEntityDetailComponent(entityType),
+    [entityType]
+  );
 
   return <Component />;
 };
