0d6db509f915bfa5bd862128d6314f1976ac58ef
Sriharsha Chintalapani <harsha@getcollate.io>
2024-03-12 10:07:03 -0700

    Fix uniquee constraint for checksum in Postgres

--- bootstrap/sql/migrations/native/1.4.0/postgres/schemaChanges.sql
+++ bootstrap/sql/migrations/native/1.4.0/postgres/schemaChanges.sql
@@ -4,6 +4,6 @@ SET json = jsonb_set(json::jsonb, '{connection,config,supportsProfiler}', 'true'
 WHERE serviceType = 'MongoDB';
 
 ALTER TABLE query_entity ADD COLUMN checksum varchar(32) GENERATED ALWAYS AS (json ->> 'checksum') STORED NOT NULL,
-    ADD UNIQUE(queryHash);
+    ADD UNIQUE(checksum);
 
 UPDATE query_entity SET json = jsonb_set(json::jsonb, '{checksum}', MD5(json->'connection'));
