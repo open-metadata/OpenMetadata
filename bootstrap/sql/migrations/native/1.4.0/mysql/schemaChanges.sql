2d29e71c64939bbb38d717171cb71cfd2d67b501
Sriharsha Chintalapani <harshach@users.noreply.github.com>
2024-03-13 05:02:26 -0700

    Fix #11868: Duplicated queries cannot be created (#15519)
    * Fix #11868: Duplicate query should throw an error of entityExists
    
    * Fix #11868: Duplicate query should throw an error of entityExists
    
    * fix test
    
    * fix test
    
    * Fix uniquee constraint for checksum in Postgres
    
    ---------
    
    Co-authored-by: Pere Miquel Brull <peremiquelbrull@gmail.com>

--- bootstrap/sql/migrations/native/1.4.0/mysql/schemaChanges.sql
+++ bootstrap/sql/migrations/native/1.4.0/mysql/schemaChanges.sql
@@ -3,6 +3,10 @@ UPDATE dbservice_entity
 SET json = JSON_INSERT(json, '$.connection.config.supportsProfiler', TRUE)
 WHERE serviceType = 'MongoDB';
 
+ALTER TABLE query_entity ADD COLUMN checksum VARCHAR(32) GENERATED ALWAYS AS (json ->> '$.checksum') NOT NULL UNIQUE;
+
+UPDATE query_entity SET json = JSON_INSERT(json, '$.checksum', MD5(JSON_UNQUOTE(JSON_EXTRACT(json, '$.checksum'))));
+
 UPDATE installed_apps
 SET json = JSON_INSERT(
         JSON_REMOVE(json, '$.appSchedule.scheduleType'),
