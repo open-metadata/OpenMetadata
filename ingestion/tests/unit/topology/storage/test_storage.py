859b977b398ee258f092dbb2c3cef590b93de0e9
ulixius9 <mayursingal9@gmail.com>
2024-03-13 18:09:20 +0530

    fix tests

--- ingestion/tests/unit/topology/storage/test_storage.py
+++ ingestion/tests/unit/topology/storage/test_storage.py
@@ -281,6 +281,7 @@ class StorageUnitTest(TestCase):
                 sourceUrl=SourceUrl(
                     __root__="https://s3.console.aws.amazon.com/s3/buckets/test_bucket?region=us-east-1&prefix=transactions/&showversions=false"
                 ),
+                fullPath='s3://test_bucket/transactions'
             ),
             self.object_store_source._generate_container_details(
                 S3BucketResponse(
