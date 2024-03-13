d41f8e28091f0e855c7df8e1e3d9ffd552ab3d47
Teddy <teddy.crepineau@gmail.com>
2024-03-13 08:37:57 +0100

    [MINOR] GX logging hierarchy (#15542)
    * fix: GX module logging hierarchy
    
    * style: ran python linting
    
    (cherry picked from commit 93a4453a6fa4797d294eeff52badd5e2a52f54d9)

--- ingestion/src/metadata/great_expectations/action.py
+++ ingestion/src/metadata/great_expectations/action.py
@@ -15,6 +15,7 @@ Open Metadata table quality.
 This subpackage needs to be used in Great Expectations
 checkpoints actions.
 """
+import logging
 import traceback
 from datetime import datetime, timezone
 from typing import Dict, List, Optional, Union, cast
@@ -72,9 +73,10 @@ from metadata.great_expectations.utils.ometa_config_handler import (
 from metadata.ingestion.ometa.ometa_api import OpenMetadata
 from metadata.utils import fqn
 from metadata.utils.entity_link import get_entity_link
-from metadata.utils.logger import great_expectations_logger
 
-logger = great_expectations_logger()
+logger = logging.getLogger(
+    "great_expectations.validation_operators.validation_operators.openmetadata"
+)
 
 
 class OpenMetadataValidationAction(ValidationAction):
@@ -429,7 +431,7 @@ class OpenMetadataValidationAction(ValidationAction):
                 test_case_fqn=test_case.fullyQualifiedName.__root__,
             )
 
-            logger.info(
+            logger.debug(
                 f"Test case result for {test_case.fullyQualifiedName.__root__} successfully ingested"
             )
 
