31d254a0fca70cf8eadeeff8ed6d4a5e141553be
ulixius9 <mayursingal9@gmail.com>
2024-03-13 15:19:07 +0530

    MINOR: Restructure dbServiceName field in dashboard and pipeline

--- bootstrap/sql/migrations/native/1.4.0/mysql/schemaChanges.sql
+++ bootstrap/sql/migrations/native/1.4.0/mysql/schemaChanges.sql
@@ -6,3 +6,17 @@ WHERE serviceType = 'MongoDB';
 ALTER TABLE query_entity ADD COLUMN checksum VARCHAR(32) GENERATED ALWAYS AS (json ->> '$.checksum') NOT NULL UNIQUE;
 
 UPDATE query_entity SET json = JSON_INSERT(json, '$.checksum', MD5(JSON_UNQUOTE(JSON_EXTRACT(json, '$.checksum'))));
+
+-- Restructure dbServiceNames in ingestion_pipeline_entity
+update ingestion_pipeline_entity set json = 
+  JSON_INSERT(
+    JSON_REMOVE(json, '$.sourceConfig.config.dbServiceNames'), 
+    '$.sourceConfig.config.lineageInformation', 
+    JSON_OBJECT(
+      'dbServiceNames', 
+      JSON_EXTRACT(json, '$.sourceConfig.config.dbServiceNames')
+    )
+  )
+where 	
+  JSON_EXTRACT(json, '$.sourceConfig.config.type') in ('DashboardMetadata', 'PipelineMetadata')
+  AND JSON_EXTRACT(json, '$.sourceConfig.config.dbServiceNames') is not null;
--- bootstrap/sql/migrations/native/1.4.0/postgres/schemaChanges.sql
+++ bootstrap/sql/migrations/native/1.4.0/postgres/schemaChanges.sql
@@ -7,3 +7,15 @@ ALTER TABLE query_entity ADD COLUMN checksum varchar(32) GENERATED ALWAYS AS (js
     ADD UNIQUE(checksum);
 
 UPDATE query_entity SET json = jsonb_set(json::jsonb, '{checksum}', MD5(json->'connection'));
+
+-- Restructure dbServiceNames in ingestion_pipeline_entity
+update ingestion_pipeline_entity ipe set json = JSONB_SET(
+    json::jsonb #- '{sourceConfig,config,dbServiceNames}',
+    '{sourceConfig,config,lineageInformation}',
+    jsonb_build_object(
+        'dbServiceNames',
+        json#>'{sourceConfig,config,dbServiceNames}'
+    )
+)
+WHERE (json#>'{sourceConfig,config,type}')::varchar(255) IN ('"DashboardMetadata"', '"PipelineMetadata"') 
+and json#>'{sourceConfig,config,dbServiceNames}' is not null;
