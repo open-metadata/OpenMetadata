4acb567110222a29d0df10d894e896833a75a449
Pere Miquel Brull <peremiquelbrull@gmail.com>
2024-03-18 12:33:06 +0100

    Simplify migration client

--- openmetadata-service/src/main/java/org/openmetadata/service/migration/MigrationConfiguration.java
+++ openmetadata-service/src/main/java/org/openmetadata/service/migration/MigrationConfiguration.java
@@ -8,5 +8,4 @@ public class MigrationConfiguration {
   @NotEmpty @Getter @Setter private String flywayPath;
   @NotEmpty @Getter @Setter private String nativePath;
   @NotEmpty @Getter @Setter private String extensionPath;
-  @NotEmpty @Getter @Setter private String migrationValidationClass;
 }
