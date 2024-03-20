c885717668f2d4b08ef02a25e23a68438ef9304e
Pere Miquel Brull <peremiquelbrull@gmail.com>
2024-03-20 14:43:57 +0100

    remove tests

--- ingestion/tests/unit/metadata/cli/resources/profiler_workflow.py
+++
@@ -1,9 +0,0 @@
-"""
-This file has been generated from dag_runner.j2
-"""
-from openmetadata.workflows import workflow_factory
-
-workflow = workflow_factory.WorkflowFactory.create(
-    "/airflow/dag_generated_configs/local_redshift_profiler_e9AziRXs.json"
-)
-workflow.generate_dag(globals())
