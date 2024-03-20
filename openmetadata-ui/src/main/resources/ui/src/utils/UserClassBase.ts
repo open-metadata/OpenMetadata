bd208371a9969f43c1284427d34417f455763dd4
Karan Hotchandani <33024356+karanh37@users.noreply.github.com>
2024-03-20 15:05:59 +0530

    Query extend (#15595)
    * initial commit
    
    * add profile pic for bot
    
    * logo configurable for collate app
    
    * add query extras
    
    * fix tests
    
    * add tests

--- openmetadata-ui/src/main/resources/ui/src/utils/UserClassBase.ts
+++ openmetadata-ui/src/main/resources/ui/src/utils/UserClassBase.ts
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2023 Collate.
+ *  Copyright 2024 Collate.
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
@@ -10,20 +10,14 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-
-class ApplicationSchemaClassBase {
-  public importSchema(fqn: string) {
-    return import(`../../../../utils/ApplicationSchemas/${fqn}.json`);
-  }
-  public getJSONUISchema() {
-    return {};
-  }
-  public importAppLogo(appName: string) {
-    return import(`../../../../assets/svg/${appName}.svg`);
+class UserClassBase {
+  protected botLogos: Record<string, string> = {};
+  public getBotLogo(botName: string) {
+    return this.botLogos[botName];
   }
 }
 
-const applicationSchemaClassBase = new ApplicationSchemaClassBase();
+const userClassBase = new UserClassBase();
 
-export default applicationSchemaClassBase;
-export { ApplicationSchemaClassBase };
+export default userClassBase;
+export { UserClassBase };
