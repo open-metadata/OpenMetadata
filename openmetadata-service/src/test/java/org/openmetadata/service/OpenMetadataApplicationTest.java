c2ae11fef1889c4d43e99109c64aa218b6b501db
Teddy Crepineau <teddy.crepineau@gmail.com>
2024-03-22 09:57:30 +0100

    fix: Change order of attribute setting for ES container

--- openmetadata-service/src/test/java/org/openmetadata/service/OpenMetadataApplicationTest.java
+++ openmetadata-service/src/test/java/org/openmetadata/service/OpenMetadataApplicationTest.java
@@ -162,12 +162,12 @@ public abstract class OpenMetadataApplicationTest {
     ELASTIC_SEARCH_CONTAINER.withPassword("password");
     ELASTIC_SEARCH_CONTAINER.withEnv("discovery.type", "single-node");
     ELASTIC_SEARCH_CONTAINER.withEnv("xpack.security.enabled", "false");
-    ELASTIC_SEARCH_CONTAINER.start();
     ELASTIC_SEARCH_CONTAINER.withReuse(false);
     ELASTIC_SEARCH_CONTAINER.setWaitStrategy(
-        new LogMessageWaitStrategy()
-            .withRegEx(".*(\"message\":\\s?\"started[\\s?|\"].*|] started\n$)")
-            .withStartupTimeout(Duration.ofMinutes(10)));
+            new LogMessageWaitStrategy()
+                    .withRegEx(".*(\"message\":\\s?\"started[\\s?|\"].*|] started\n$)")
+                    .withStartupTimeout(Duration.ofMinutes(5)));
+    ELASTIC_SEARCH_CONTAINER.start();
     String[] parts = ELASTIC_SEARCH_CONTAINER.getHttpHostAddress().split(":");
     HOST = parts[0];
     PORT = parts[1];
