04b40e569f4f00c3c35db3fa2d012e4cd2155237
Pere Miquel Brull <peremiquelbrull@gmail.com>
2024-03-22 11:30:02 +0100

    #15662 - List All test cases from a table in DQ (#15665)
    * #15662 - List All test cases from a table in DQ
    
    * #15662 - List All test cases from a table in DQ
    
    * #15662 - List All test cases from a table in DQ

--- ingestion/src/metadata/data_quality/source/test_suite.py
+++ ingestion/src/metadata/data_quality/source/test_suite.py
@@ -83,11 +83,11 @@ class TestSuiteSource(Source):
     ) -> Optional[List[TestCase]]:
         """Return test cases if the test suite exists and has them"""
         if test_suite:
-            test_cases = self.metadata.list_entities(
+            test_cases = self.metadata.list_all_entities(
                 entity=TestCase,
                 fields=["testSuite", "entityLink", "testDefinition"],
                 params={"testSuiteId": test_suite.id.__root__},
-            ).entities
+            )
             test_cases = cast(List[TestCase], test_cases)  # satisfy type checker
 
             return test_cases
