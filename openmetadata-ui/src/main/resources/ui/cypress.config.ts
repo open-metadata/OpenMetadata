ba7b2a4000d936be9dcfd227ad298b4c9005ce05
Aniket Katkar <aniketkatkar97@gmail.com>
2024-04-03 19:59:58 +0530

    cypress config reverted

--- openmetadata-ui/src/main/resources/ui/cypress.config.ts
+++ openmetadata-ui/src/main/resources/ui/cypress.config.ts
@@ -34,10 +34,6 @@ export default defineConfig({
 
       return config;
     },
-    specPattern: [
-      'cypress/e2e/Flow/SearchFlow/AdditionalRuleSearch.spec.ts',
-      'cypress/e2e/Flow/SearchFlow/GroupSearch.spec.ts',
-      'cypress/e2e/Flow/SearchFlow/SingleFiledSearch.spec.ts',
-    ],
+    specPattern: ['cypress/e2e/**/*.{js,jsx,ts,tsx}'],
   },
 });
