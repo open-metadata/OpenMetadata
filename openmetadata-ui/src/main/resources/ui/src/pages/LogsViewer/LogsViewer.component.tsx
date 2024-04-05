08c744b9851468a61623bc0ffe6d0fc09540812c
Pere Miquel Brull <peremiquelbrull@gmail.com>
2024-04-05 07:09:59 +0200

    MINOR - Add Application type for logs (#15802)
    * MINOR - Add Application type for logs
    
    * MINOR - Add Application type for logs
    
    * Add application_task to DataInsightLatestRun interface and update LogsViewer component
    
    ---------
    
    Co-authored-by: Sachin Chaurasiya <sachinchaurasiyachotey87@gmail.com>

--- openmetadata-ui/src/main/resources/ui/src/pages/LogsViewer/LogsViewer.component.tsx
+++ openmetadata-ui/src/main/resources/ui/src/pages/LogsViewer/LogsViewer.component.tsx
@@ -91,7 +91,7 @@ const LogsViewer = () => {
 
         const logs = await getLatestApplicationRuns(ingestionName);
         setAppLatestRun(data[0]);
-        setLogs(logs.data_insight_task);
+        setLogs(logs.data_insight_task || logs.application_task);
 
         return;
       }
