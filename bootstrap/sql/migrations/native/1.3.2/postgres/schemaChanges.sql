54a0385435baea0ab5ba2843e3c050fdc02f0346
mohitdeuex <mohit.y@deuexsolutions.com>
2024-04-11 13:33:51 +0530

    Fix SQLs

--- bootstrap/sql/migrations/native/1.3.2/postgres/schemaChanges.sql
+++ bootstrap/sql/migrations/native/1.3.2/postgres/schemaChanges.sql
@@ -4,25 +4,33 @@ ALTER TABLE test_case ADD COLUMN entityLink VARCHAR(512) GENERATED ALWAYS AS (js
 -- change scheduleType to scheduleTimeline
 UPDATE installed_apps
 SET json = jsonb_set(
-        json::jsonb #- '{appSchedule,scheduleType}',
-        '{appSchedule,scheduleTimeline}',
-        (json #> '{appSchedule,scheduleType}')::jsonb,
+        json::jsonb,
+        '{appSchedule}',
+        jsonb_set(
+            json->'appSchedule',
+            '{scheduleTimeline}',
+            json->'appSchedule'->'scheduleType'
+        ) - 'scheduleType',
         true
-    );
+    )
+WHERE json->'appSchedule'->>'scheduleType' IS NOT NULL;
+
 delete from apps_extension_time_series;
 
 -- Change systemApp to system
 UPDATE installed_apps
 SET json = jsonb_set(
-        json::jsonb #- '{systemApp}',
+        json::jsonb,
         '{system}',
-        (json #> '{systemApp}')::jsonb,
-        true
-    );
+        json->'systemApp'
+    ) - 'systemApp'
+WHERE json ? 'systemApp';
+
 UPDATE apps_marketplace
 SET json = jsonb_set(
-        json::jsonb #- '{systemApp}',
+        json::jsonb,
         '{system}',
-        (json #> '{systemApp}')::jsonb,
-        true
-    );
+        json->'systemApp'
+    ) - 'systemApp'
+WHERE json ? 'systemApp';
+
