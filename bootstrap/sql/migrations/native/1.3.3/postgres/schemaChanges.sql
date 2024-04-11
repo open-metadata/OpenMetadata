33f4fb9bb469ccceda6315f84790cf5cc3888709
Pere Miquel Brull <peremiquelbrull@gmail.com>
2024-04-11 10:19:07 +0200

    Update bootstrap/sql/migrations/native/1.3.3/postgres/schemaChanges.sql

--- bootstrap/sql/migrations/native/1.3.3/postgres/schemaChanges.sql
+++ bootstrap/sql/migrations/native/1.3.3/postgres/schemaChanges.sql
@@ -1,4 +1,4 @@
--- change scheduleType to scheduleTimeline, , this was failing earlier in 1.3.2 so updating it here
+-- change scheduleType to scheduleTimeline, this was failing earlier in 1.3.2 so updating it here
 UPDATE installed_apps
 SET json = jsonb_set(
         json::jsonb,
