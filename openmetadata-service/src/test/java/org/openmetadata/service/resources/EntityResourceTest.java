1f10481c097fdca187ef1ba61eb5345f563f50d8
Pablo Takara <pjt1991@gmail.com>
2024-04-12 12:35:08 +0200

    Remove descriptionStatus where it's not needed

--- openmetadata-service/src/test/java/org/openmetadata/service/resources/EntityResourceTest.java
+++ openmetadata-service/src/test/java/org/openmetadata/service/resources/EntityResourceTest.java
@@ -1011,7 +1011,7 @@ public abstract class EntityResourceTest<T extends EntityInterface, K extends Cr
     Assumptions.assumeTrue(
         Arrays.asList(entityClass.getInterfaces()).contains(EntityInterface.class));
     // We can't create a Glossary or a Tag without description
-    Assumptions.assumeTrue(!List.of(GLOSSARY, TAG).contains(entityType));
+    Assumptions.assumeTrue(!List.of(GLOSSARY, TAG, QUERY).contains(entityType));
     // Create an entity without description
     K createWithNullDescription = createRequest(test, 1).withDescription(null);
     T entityWithNullDescription = createEntity(createWithNullDescription, ADMIN_AUTH_HEADERS);
@@ -1054,6 +1054,7 @@ public abstract class EntityResourceTest<T extends EntityInterface, K extends Cr
     Assumptions.assumeTrue(supportsSearchIndex);
     Assumptions.assumeTrue(
         Arrays.asList(entityClass.getInterfaces()).contains(EntityInterface.class));
+    Assumptions.assumeTrue(!List.of(QUERY).contains(entityType));
     // Create an entity with empty description
     K createWithEmptyDescription = createRequest(test, 2);
     T entityWithEmptyDescription = createEntity(createWithEmptyDescription, ADMIN_AUTH_HEADERS);
