b6cfb778d5997bd25a524232ef240387a83b73a8
SumanMaharana <sumanmaharana786@gmail.com>
2024-04-15 15:24:11 +0530

    Added sample_data for DDL statements

--- ingestion/src/metadata/ingestion/source/database/sample_data.py
+++ ingestion/src/metadata/ingestion/source/database/sample_data.py
@@ -705,7 +705,7 @@ class SampleDataSource(
                 tableType=table["tableType"],
                 tableConstraints=table.get("tableConstraints"),
                 tags=table["tags"],
-                dataModels=table.get("dataModels")
+                dataModel=table.get("dataModel")
             )
 
             yield Either(right=table_and_db)
