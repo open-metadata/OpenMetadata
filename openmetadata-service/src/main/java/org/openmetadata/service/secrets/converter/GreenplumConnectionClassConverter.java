dfecc1082b916b039e347cff4e9993917f6b20c0
Ayush Shah <ayush@getcollate.io>
2024-04-16 14:33:35 +0530

    Fix Server Failure

--- openmetadata-service/src/main/java/org/openmetadata/service/secrets/converter/GreenplumConnectionClassConverter.java
+++ openmetadata-service/src/main/java/org/openmetadata/service/secrets/converter/GreenplumConnectionClassConverter.java
@@ -15,31 +15,25 @@ package org.openmetadata.service.secrets.converter;
 
 import java.util.List;
 
-import org.openmetadata.schema.security.credentials.GCPCredentials;
 import org.openmetadata.schema.security.ssl.ValidateSSLClientConfig;
-import org.openmetadata.schema.security.ssl.VerifySSL;
 
-import org.openmetadata.schema.services.connections.database.GreenPlumConnection;
-import org.openmetadata.schema.services.connections.database.common.IamAuthConfig;
-import org.openmetadata.schema.services.connections.database.common.basicAuth;
+import org.openmetadata.schema.services.connections.database.GreenplumConnection;
 import org.openmetadata.service.util.JsonUtils;
 
 /**
- * Converter class to get an `DatalakeConnection` object.
+ * Converter class to get an `GreenplumConnection` object.
  */
-public class GreenPlumConnectionClassConverter extends ClassConverter {
-
-    private static final List<Class<?>> CONFIG_SOURCE_CLASSES = List.of(basicAuth.class, IamAuthConfig.class);
+public class GreenplumConnectionClassConverter extends ClassConverter {
 
     private static final List<Class<?>> SSL_SOURCE_CLASS = List.of(ValidateSSLClientConfig.class);
 
-    public GreenPlumConnectionClassConverter() {
-        super(GreenPlumConnection.class);
+    public GreenplumConnectionClassConverter() {
+        super(GreenplumConnection.class);
     }
 
     @Override
     public Object convert(Object object) {
-        GreenPlumConnection greenplumConnection = (GreenPlumConnection) JsonUtils.convertValue(object, this.clazz);
+        GreenplumConnection greenplumConnection = (GreenplumConnection) JsonUtils.convertValue(object, this.clazz);
         tryToConvert(greenplumConnection.getSslConfig(), SSL_SOURCE_CLASS).ifPresent(obj -> greenplumConnection.setSslConfig((ValidateSSLClientConfig) obj));
         return greenplumConnection;
     }
