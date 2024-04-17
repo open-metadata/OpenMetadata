100c6ecf2134f84bfe1332287e43ffe70fd8c8ab
Imri Paran <imri.paran@gmail.com>
2024-04-17 14:52:10 +0200

    MINOR: add raise_from_status for sql_server test (#15931)
    * Update test_metadata_ingestion.py
    
    * Update test_metadata_ingestion.py
    
    * fixed import

--- ingestion/tests/integration/sql_server/test_metadata_ingestion.py
+++ ingestion/tests/integration/sql_server/test_metadata_ingestion.py
@@ -1,3 +1,4 @@
+import logging
 import sys
 
 import pytest
@@ -83,8 +84,10 @@ def ingest_metadata(db_service, metadata: OpenMetadata):
         ),
         workflowConfig=WorkflowConfig(openMetadataServerConfig=metadata.config),
     )
+    logging.getLogger("sqlfluff").setLevel(logging.CRITICAL)
     metadata_ingestion = MetadataWorkflow.create(workflow_config)
     metadata_ingestion.execute()
+    metadata_ingestion.raise_from_status()
     return
 
 
