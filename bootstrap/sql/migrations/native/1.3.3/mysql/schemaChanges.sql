619cf388f1f38247a7a4170a715f60afc96dfcb7
Mohit Yadav <105265192+mohityadav766@users.noreply.github.com>
2024-04-18 14:26:05 +0530

    Move migration for apps to 1.3.3 all together (#15944)

--- bootstrap/sql/migrations/native/1.3.3/mysql/schemaChanges.sql
+++ bootstrap/sql/migrations/native/1.3.3/mysql/schemaChanges.sql
@@ -1,1 +1,24 @@
--- Nothing to add here. 1.3.3 migrations only needed for postgres
+-- Change scheduleType to scheduleTimeline
+
+UPDATE installed_apps
+SET json = JSON_INSERT(
+        JSON_REMOVE(json, '$.appSchedule.scheduleType'),
+        '$.appSchedule.scheduleTimeline',
+        JSON_EXTRACT(json, '$.appSchedule.scheduleType')
+    );
+delete from apps_extension_time_series;
+
+
+-- Change systemApp to system
+UPDATE installed_apps
+SET json = JSON_INSERT(
+        JSON_REMOVE(json, '$.systemApp'),
+        '$.system',
+        JSON_EXTRACT(json, '$.systemApp')
+    );
+UPDATE apps_marketplace
+SET json = JSON_INSERT(
+        JSON_REMOVE(json, '$.systemApp'),
+        '$.system',
+        JSON_EXTRACT(json, '$.systemApp')
+    );
