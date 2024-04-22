449a5f2de3bc229000d26d536538c01e50026a7f
Teddy <teddy.crepineau@gmail.com>
2024-04-22 22:35:37 +0200

    FIX #11951 - ingestion logic for global profiler config (#15948)
    * feat: add global metric configuration for the profiler
    
    * style: ran java linting
    
    * fix: renamed disable to disabled
    
    * style: ran java linting
    
    * feat: ometa sdk for profiler setting
    
    * test: ingestion profiler global config tests
    
    * fix: update metric name to use MetricType Enum
    
    * fix: allow bot to retrieve settings
    
    * fix: exclude GX artifacts
    
    * feat: implement global profiler setting logic for ingestion side
    
    * fix: exclude metrics if Metric is empty
    
    * style: ran python linting
    
    * style: ran python linting
    
    * fix: skip empty metrics
    
    * style: ran python linting
    
    * fix: moved GET profiler config to seperate endpoint in system resource
    
    * fix: moved compute metric filter to MetricFilter + renamed container
    
    * fix: test failures
    
    * fix: profiler test case

---
+++ ingestion/tests/utils/docker_service_builders/test_container_builder.py
@@ -0,0 +1,39 @@
+#  Copyright 2024 Collate
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#  http://www.apache.org/licenses/LICENSE-2.0
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+"""test container builder class"""
+
+from typing import List
+
+from .abstract_test_container import AbstractTestContainer
+from .database_container.mysql_test_container import MySQLTestContainer
+from .database_container.postgres_test_container import PostgresTestContainer
+
+
+class ContainerBuilder:
+    def __init__(self) -> None:
+        self.containers: List[AbstractTestContainer] = []
+
+    def run_mysql_container(self):
+        """build mysql container"""
+        container = MySQLTestContainer()
+        self.containers.append(container)
+        return container
+
+    def run_postgres_container(self):
+        """build mysql container"""
+        container = PostgresTestContainer()
+        self.containers.append(container)
+        return container
+
+    def stop_all_containers(self):
+        """stop all containers"""
+        for container in self.containers:
+            container.stop()
