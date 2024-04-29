4b6767a133cb392ac5fbf66449d213682e7ee79f
harshsoni2024 <harshsoni2024@gmail.com>
2024-04-29 09:23:07 +0530

    issue-15719: remove more patch code

--- ingestion/tests/unit/topology/dashboard/test_superset.py
+++ ingestion/tests/unit/topology/dashboard/test_superset.py
@@ -16,8 +16,8 @@ import json
 import uuid
 from pathlib import Path
 from unittest import TestCase
-from unittest.mock import patch
 
+import sqlalchemy
 from testcontainers.core.generic import DockerContainer
 from testcontainers.postgres import PostgresContainer
 
@@ -54,20 +54,15 @@ from metadata.generated.schema.type.basic import (
 )
 from metadata.generated.schema.type.entityReference import EntityReference
 from metadata.ingestion.api.steps import InvalidSourceException
-from metadata.ingestion.ometa.mixins.server_mixin import OMetaServerMixin
 from metadata.ingestion.ometa.ometa_api import OpenMetadata
-from metadata.ingestion.source.dashboard.dashboard_service import DashboardServiceSource
 from metadata.ingestion.source.dashboard.superset.api_source import SupersetAPISource
-from metadata.ingestion.source.dashboard.superset.client import SupersetAPIClient
 from metadata.ingestion.source.dashboard.superset.db_source import SupersetDBSource
 from metadata.ingestion.source.dashboard.superset.metadata import SupersetSource
 from metadata.ingestion.source.dashboard.superset.models import (
     FetchChart,
     FetchDashboard,
-    ListDatabaseResult,
     SupersetChart,
     SupersetDashboardCount,
-    SupersetDatasource,
 )
 
 mock_file_path = (
@@ -94,7 +89,7 @@ EXPECTED_DASH_SERVICE = DashboardService(
     connection=DashboardConnection(),
     serviceType=DashboardServiceType.Superset,
 )
-EXPECTED_USER = EntityReference(id="21af89aa-1bab-41aa-a567-5e68f78acdc0", type="user")
+EXPECTED_USER = EntityReference(id="81af89aa-1bab-41aa-a567-5e68f78acdc0", type="user")
 
 MOCK_DB_MYSQL_SERVICE_1 = DatabaseService(
     id="c3eb265f-5445-4ad3-ba5e-797d3a307122",
@@ -220,57 +215,79 @@ EXPECTED_CHART_2 = CreateChartRequest(
 EXPECTED_ALL_CHARTS_DB = {1: MOCK_CHART_DB_2}
 
 NOT_FOUND_RESP = {"message": "Not found"}
-
+EXPECTED_API_DATASET_FQN = None
 EXPECTED_DATASET_FQN = "test_postgres.examples.main.wb_health_population"
 
 
-def perform_sql_queries(superset_db):
-    # [(1, 'Rural', 'desc', 99, 'bar_chart', 'sample_table', 'main', 'test_db', 'postgres://user:pass@localhost:5432/examples')]
-    CREATE_SLICES_TABLE = """
-        CREATE TABLE slices (
-            id INTEGER PRIMARY KEY,
-            slice_name VARCHAR(255),
-            description TEXT,
-            datasource_id INTEGER,
-            viz_type VARCHAR(255),
-            datasource_type VARCHAR(255)
-        )
-    """
-    INSERT_SLICES_DATA = """
-        INSERT INTO slices(id, slice_name, description, datasource_id, viz_type, datasource_type)
-        VALUES (1, 'Rural', 'desc', 99, 'bar_chart', 'table');
-    """
-    CREATE_DBS_TABLE = """
-        CREATE TABLE dbs (
-            id INTEGER PRIMARY KEY,
-            database_name VARCHAR(255),
-            sqlalchemy_uri TEXT
-        )
-    """
-    INSERT_DBS_DATA = """
-        INSERT INTO dbs(id, database_name, sqlalchemy_uri)
-        VALUES (5, 'test_db', 'postgres://user:pass@localhost:5432/examples');
-    """
-    CREATE_TABLES_TABLE = """
-        CREATE TABLE tables (
-            id INTEGER PRIMARY KEY,
-            table_name VARCHAR(255),
-            schema VARCHAR(255),
-            database_id INTEGER
-        );
-    """
-    INSERT_TABLES_DATA = """
-        INSERT INTO tables(id, table_name, schema, database_id)
-        VALUES (99, 'sample_table', 'main', 5);
-    """
-    superset_db.engine.execute(CREATE_SLICES_TABLE)
-    superset_db.engine.execute(INSERT_SLICES_DATA)
-
-    superset_db.engine.execute(CREATE_DBS_TABLE)
-    superset_db.engine.execute(INSERT_DBS_DATA)
-
-    superset_db.engine.execute(CREATE_TABLES_TABLE)
-    superset_db.engine.execute(INSERT_TABLES_DATA)
+def setup_sample_data(postgres_container):
+    engine = sqlalchemy.create_engine(postgres_container.get_connection_url())
+    with engine.begin() as connection:
+        CREATE_TABLE_AB_USER = """
+                CREATE TABLE ab_user (
+                id INT PRIMARY KEY,
+                username VARCHAR(50));
+        """
+        CREATE_TABLE_DASHBOARDS = """
+            CREATE TABLE dashboards (
+            id INT PRIMARY KEY,
+            created_by_fk INT,
+            FOREIGN KEY (created_by_fk) REFERENCES ab_user(id));
+        """
+        INSERT_AB_USER_DATA = """
+            INSERT INTO ab_user (id, username)
+            VALUES (1, 'test_user');
+        """
+        INSERT_DASHBOARDS_DATA = """
+            INSERT INTO dashboards (id, created_by_fk)
+            VALUES (1, 1);
+        """
+        CREATE_SLICES_TABLE = """
+            CREATE TABLE slices (
+                id INTEGER PRIMARY KEY,
+                slice_name VARCHAR(255),
+                description TEXT,
+                datasource_id INTEGER,
+                viz_type VARCHAR(255),
+                datasource_type VARCHAR(255)
+            )
+        """
+        INSERT_SLICES_DATA = """
+            INSERT INTO slices(id, slice_name, description, datasource_id, viz_type, datasource_type)
+            VALUES (1, 'Rural', 'desc', 99, 'bar_chart', 'table');
+        """
+        CREATE_DBS_TABLE = """
+            CREATE TABLE dbs (
+                id INTEGER PRIMARY KEY,
+                database_name VARCHAR(255),
+                sqlalchemy_uri TEXT
+            )
+        """
+        INSERT_DBS_DATA = """
+            INSERT INTO dbs(id, database_name, sqlalchemy_uri)
+            VALUES (5, 'test_db', 'postgres://user:pass@localhost:5432/examples');
+        """
+        CREATE_TABLES_TABLE = """
+            CREATE TABLE tables (
+                id INTEGER PRIMARY KEY,
+                table_name VARCHAR(255),
+                schema VARCHAR(255),
+                database_id INTEGER
+            );
+        """
+        INSERT_TABLES_DATA = """
+            INSERT INTO tables(id, table_name, schema, database_id)
+            VALUES (99, 'sample_table', 'main', 5);
+        """
+        connection.execute(sqlalchemy.text(CREATE_TABLE_AB_USER))
+        connection.execute(sqlalchemy.text(INSERT_AB_USER_DATA))
+        connection.execute(sqlalchemy.text(CREATE_TABLE_DASHBOARDS))
+        connection.execute(sqlalchemy.text(INSERT_DASHBOARDS_DATA))
+        connection.execute(sqlalchemy.text(CREATE_SLICES_TABLE))
+        connection.execute(sqlalchemy.text(INSERT_SLICES_DATA))
+        connection.execute(sqlalchemy.text(CREATE_DBS_TABLE))
+        connection.execute(sqlalchemy.text(INSERT_DBS_DATA))
+        connection.execute(sqlalchemy.text(CREATE_TABLES_TABLE))
+        connection.execute(sqlalchemy.text(INSERT_TABLES_DATA))
 
 
 INITIAL_SETUP = True
@@ -283,7 +300,7 @@ def set_testcontainers():
         # postgres test container
         postgres_container = PostgresContainer("postgres:16-alpine")
         postgres_container.start()
-
+        setup_sample_data(postgres_container)
         # superset testcontainer
         superset_container = DockerContainer(image="apache/superset")
         superset_container.with_env("SUPERSET_SECRET_KEY", "&3brfbcf192T!)$sabqbie")
@@ -399,19 +416,14 @@ class SupersetUnitTest(TestCase):
             "dashboard_service"
         ] = EXPECTED_DASH_SERVICE.fullyQualifiedName.__root__
 
-        with patch.object(
-            DashboardServiceSource, "test_connection", return_value=False
-        ), patch.object(OMetaServerMixin, "validate_versions", return_value=True):
-            # This already validates that the source can be initialized
-            self.superset_db: SupersetSource = SupersetSource.create(
-                MOCK_SUPERSET_DB_CONFIG["source"],
-                OpenMetadata(self.config.workflowConfig.openMetadataServerConfig),
-            )
-
-            self.assertEqual(type(self.superset_db), SupersetDBSource)
-            self.superset_db.context.get().__dict__[
-                "dashboard_service"
-            ] = EXPECTED_DASH_SERVICE.fullyQualifiedName.__root__
+        self.superset_db: SupersetSource = SupersetSource.create(
+            MOCK_SUPERSET_DB_CONFIG["source"],
+            OpenMetadata(self.config.workflowConfig.openMetadataServerConfig),
+        )
+        self.assertEqual(type(self.superset_db), SupersetDBSource)
+        self.superset_db.context.get().__dict__[
+            "dashboard_service"
+        ] = EXPECTED_DASH_SERVICE.fullyQualifiedName.__root__
 
     def test_create(self):
         """
@@ -465,7 +477,6 @@ class SupersetUnitTest(TestCase):
         """
         test fetch chart method of db source
         """
-        perform_sql_queries(self.superset_db)
         self.superset_db.prepare()
         self.assertEqual(EXPECTED_ALL_CHARTS_DB, self.superset_db.all_charts)
 
@@ -489,6 +500,7 @@ class SupersetUnitTest(TestCase):
         EXPECTED_DASH.sourceUrl = SourceUrl(
             __root__=f"http://{superset_container.get_container_host_ip()}:{superset_container.get_exposed_port(8088)}/superset/dashboard/14/"
         )
+        EXPECTED_DASH.owner = dashboard.owner
         self.assertEqual(dashboard, EXPECTED_DASH)
 
     def test_yield_dashboard_chart(self):
@@ -517,21 +529,10 @@ class SupersetUnitTest(TestCase):
         """
         Test generated datasource fqn for api source
         """
-        with patch.object(
-            OpenMetadata, "es_search_from_fqn", return_value=None
-        ), patch.object(
-            SupersetAPIClient,
-            "fetch_datasource",
-            return_value=SupersetDatasource(**mock_data["datasource"]),
-        ), patch.object(
-            SupersetAPIClient,
-            "fetch_database",
-            return_value=ListDatabaseResult(**mock_data["database"]),
-        ):
-            fqn = self.superset_api._get_datasource_fqn(  # pylint: disable=protected-access
-                1, MOCK_DB_POSTGRES_SERVICE
-            )
-            self.assertEqual(fqn, EXPECTED_DATASET_FQN)
+        fqn = self.superset_api._get_datasource_fqn(  # pylint: disable=protected-access
+            1, MOCK_DB_POSTGRES_SERVICE
+        )
+        self.assertEqual(fqn, EXPECTED_API_DATASET_FQN)
 
     def test_db_get_datasource_fqn_for_lineage(self):
         fqn = self.superset_db._get_datasource_fqn_for_lineage(  # pylint: disable=protected-access
