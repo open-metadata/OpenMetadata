b400f0f7c2b263d5bb84abb51bcb349530badf55
Sriharsha Chintalapani <harsha@getcollate.io>
2024-05-07 22:51:34 -0700

    Update Limits feature response

--- openmetadata-service/src/main/java/org/openmetadata/service/limits/DefaultLimits.java
+++ openmetadata-service/src/main/java/org/openmetadata/service/limits/DefaultLimits.java
@@ -1,19 +1,23 @@
 package org.openmetadata.service.limits;
 
+import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
 import org.jdbi.v3.core.Jdbi;
 import org.openmetadata.schema.configuration.LimitsConfiguration;
 import org.openmetadata.schema.system.LimitsConfig;
+import org.openmetadata.service.OpenMetadataApplicationConfig;
 import org.openmetadata.service.security.policyevaluator.OperationContext;
 import org.openmetadata.service.security.policyevaluator.ResourceContextInterface;
 
 public class DefaultLimits implements Limits {
+  private OpenMetadataApplicationConfig serverConfig = null;
   private LimitsConfiguration limitsConfiguration = null;
   private Jdbi jdbi = null;
 
   @Override
-  public void init(LimitsConfiguration limitsConfiguration, Jdbi jdbi) {
-    this.limitsConfiguration = limitsConfiguration;
+  public void init(OpenMetadataApplicationConfig serverConfig, Jdbi jdbi) {
+    this.serverConfig = serverConfig;
+    this.limitsConfiguration = serverConfig.getLimitsConfiguration();
     this.jdbi = jdbi;
   }
 
@@ -30,5 +34,10 @@ public class DefaultLimits implements Limits {
     LimitsConfig limitsConfig = new LimitsConfig();
     limitsConfig.setEnable(limitsConfiguration.getEnable());
     return limitsConfig;
+  }
+
+  @Override
+  public Response getLimitsForaFeature(String name) {
+    return Response.ok().build();
   }
 }
--- openmetadata-service/src/main/java/org/openmetadata/service/limits/Limits.java
+++ openmetadata-service/src/main/java/org/openmetadata/service/limits/Limits.java
@@ -1,14 +1,15 @@
 package org.openmetadata.service.limits;
 
+import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
 import org.jdbi.v3.core.Jdbi;
-import org.openmetadata.schema.configuration.LimitsConfiguration;
 import org.openmetadata.schema.system.LimitsConfig;
+import org.openmetadata.service.OpenMetadataApplicationConfig;
 import org.openmetadata.service.security.policyevaluator.OperationContext;
 import org.openmetadata.service.security.policyevaluator.ResourceContextInterface;
 
 public interface Limits {
-  void init(LimitsConfiguration limitsConfiguration, Jdbi jdbi);
+  void init(OpenMetadataApplicationConfig serverConfig, Jdbi jdbi);
 
   void enforceLimits(
       SecurityContext securityContext,
@@ -16,4 +17,6 @@ public interface Limits {
       ResourceContextInterface resourceContext);
 
   LimitsConfig getLimitsConfig();
+
+  Response getLimitsForaFeature(String name);
 }
