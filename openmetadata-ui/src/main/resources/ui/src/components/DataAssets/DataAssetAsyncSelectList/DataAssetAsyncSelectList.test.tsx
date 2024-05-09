37ef9fbc57e6362ef4b5d738386c6dc136339f62
Sachin Chaurasiya <sachinchaurasiyachotey87@gmail.com>
2024-05-09 14:01:10 +0530

    minor: filter out bots from user list (#16190)

--- openmetadata-ui/src/main/resources/ui/src/components/DataAssets/DataAssetAsyncSelectList/DataAssetAsyncSelectList.test.tsx
+++ openmetadata-ui/src/main/resources/ui/src/components/DataAssets/DataAssetAsyncSelectList/DataAssetAsyncSelectList.test.tsx
@@ -261,4 +261,74 @@ describe('DataAssetAsyncSelectList', () => {
 
     expect(screen.getByText('Test')).toBeInTheDocument();
   });
+
+  it('searchQuery should be called with filter isBot:false when search index is user', async () => {
+    const mockSearchQuery = searchQuery as jest.Mock;
+    mockSearchQuery.mockImplementationOnce((params) => {
+      expect(params).toEqual(
+        expect.objectContaining({ filters: 'isBot:false' })
+      );
+
+      return Promise.resolve(mockUserData.data);
+    });
+
+    const { container } = render(
+      <DataAssetAsyncSelectList
+        mode="multiple"
+        searchIndex={SearchIndex.USER}
+      />
+    );
+
+    await act(async () => {
+      toggleOpen(container);
+    });
+
+    expect(searchQuery).toHaveBeenCalledTimes(1);
+  });
+
+  it('searchQuery should be called with filter isBot:false when search index includes user', async () => {
+    const indexes = ['user', 'team', 'user_search_index'].join(
+      ','
+    ) as SearchIndex;
+    const mockSearchQuery = searchQuery as jest.Mock;
+    mockSearchQuery.mockImplementationOnce((params) => {
+      expect(params).toEqual(
+        expect.objectContaining({ filters: 'isBot:false' })
+      );
+
+      return Promise.resolve(mockUserData.data);
+    });
+
+    const { container } = render(
+      <DataAssetAsyncSelectList mode="multiple" searchIndex={indexes} />
+    );
+
+    await act(async () => {
+      toggleOpen(container);
+    });
+
+    expect(searchQuery).toHaveBeenCalledTimes(1);
+  });
+
+  it("searchQuery should not be called with filter isBot:false when search index doesn't include user", async () => {
+    const indexes = ['team', 'glossary'].join(',') as SearchIndex;
+    const mockSearchQuery = searchQuery as jest.Mock;
+    mockSearchQuery.mockImplementationOnce((params) => {
+      expect(params).not.toEqual(
+        expect.objectContaining({ filters: 'isBot:false' })
+      );
+
+      return Promise.resolve(mockUserData.data);
+    });
+
+    const { container } = render(
+      <DataAssetAsyncSelectList mode="multiple" searchIndex={indexes} />
+    );
+
+    await act(async () => {
+      toggleOpen(container);
+    });
+
+    expect(searchQuery).toHaveBeenCalledTimes(1);
+  });
 });
