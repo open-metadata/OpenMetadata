8fc0b12293a21a97691457070a67c9e9efdce724
sushi30 <imri.paran@gmail.com>
2024-05-15 22:45:33 +0200

    - removed warnings for classes which are not real tests - removed "helpers" as its being used

--- ingestion/tests/integration/conftest.py
+++ ingestion/tests/integration/conftest.py
@@ -6,3 +6,11 @@ from .integration_base import int_admin_ometa
 @pytest.fixture(scope="module")
 def metadata():
     return int_admin_ometa()
+
+
+def pytest_pycollect_makeitem(collector, name, obj):
+    try:
+        if obj.__base__.__name__ in ("BaseModel", "Enum"):
+            return []
+    except AttributeError:
+        pass
