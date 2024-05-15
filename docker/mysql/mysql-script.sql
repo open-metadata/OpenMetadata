93d725d6222f7b5be943b36530e3799601e49a68
Ayush Shah <ayush@getcollate.io>
2024-05-15 16:50:24 +0530

    Add Flush priviledges

--- docker/mysql/mysql-script.sql
+++ docker/mysql/mysql-script.sql
@@ -5,4 +5,5 @@ CREATE USER 'airflow_user'@'%' IDENTIFIED BY 'airflow_pass';
 GRANT ALL PRIVILEGES ON openmetadata_db.* TO 'openmetadata_user'@'%' WITH GRANT OPTION;
 GRANT PROCESS, USAGE ON *.* TO 'openmetadata_user'@'%';
 GRANT ALL PRIVILEGES ON airflow_db.* TO 'airflow_user'@'%' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
 commit;
