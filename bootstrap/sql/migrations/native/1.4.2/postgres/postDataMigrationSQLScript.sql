4b8aaa664c7deb39ec5ed5dfbda2445e06b304aa
Teddy Crepineau <teddy.crepineau@gmail.com>
2024-05-29 09:05:05 +0200

    fix: partition migration issue with redshift servics

---
+++ bootstrap/sql/migrations/native/1.4.2/postgres/postDataMigrationSQLScript.sql
@@ -0,0 +1,5 @@
+-- Fix hangiong partition migration
+-- in 1.4.x `tablePartition.intervalType` should not exists
+UPDATE table_entity
+SET json = json - 'tablePartition'
+WHERE json->'tablePartition'->'intervalType' is not null;
