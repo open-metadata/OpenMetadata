d807f47f4d61da1568155655c48f829b588c84c9
Matthew Chamberlin <mchamberlin@ginkgobioworks.com>
2024-06-10 15:42:23 -0400

    cleanup, add comment

--- ingestion/src/metadata/ingestion/source/database/iceberg/metadata.py
+++ ingestion/src/metadata/ingestion/source/database/iceberg/metadata.py
@@ -181,6 +181,7 @@ class IcebergSource(DatabaseServiceSource):
         for table_identifier in self.iceberg.list_tables(namespace):
             try:
                 table = self.iceberg.load_table(table_identifier)
+                # extract table name from table identifier, which does not include catalog name
                 table_name = get_table_name_as_str(table_identifier)
                 table_fqn = fqn.build(
                     self.metadata,
