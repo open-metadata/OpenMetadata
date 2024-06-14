a4f5769b33b9e3656e43a812961d3b6406ca0966
Shailesh Parmar <shailesh.parmar.webdev@gmail.com>
2024-06-14 15:12:36 +0530

    minor owner fix

--- openmetadata-ui/src/main/resources/ui/cypress/common/Utils/Owner.ts
+++ openmetadata-ui/src/main/resources/ui/cypress/common/Utils/Owner.ts
@@ -82,9 +82,7 @@ export const addOwner = (
   verifyResponseStatusCode('@getUsers', 200);
   interceptURL(
     'GET',
-    `api/v1/search/query?q=${encodeURIComponent(
-      ownerName
-    )}*index=user_search_index*`,
+    `api/v1/search/query?q=*&index=user_search_index*`,
     'searchOwner'
   );
 
