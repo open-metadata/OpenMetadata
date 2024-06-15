cde18a23ff8be6318fecd66044f8f9747bad3b75
ulixius9 <mayursingal9@gmail.com>
2024-06-15 17:57:13 +0530

    append only when col lineage available

--- ingestion/src/metadata/ingestion/ometa/mixins/lineage_mixin.py
+++ ingestion/src/metadata/ingestion/ometa/mixins/lineage_mixin.py
@@ -54,13 +54,15 @@ class OMetaLineageMixin(Generic[T]):
         temp_result = []
         try:
             for column in original or []:
-                temp_result.append((*column.get("fromColumns", []), column.get("toColumn")))
+                if column.get("toColumn") and column.get("fromColumns"):
+                    temp_result.append((*column.get("fromColumns", []), column.get("toColumn")))
             for column in updated or []:
                 if not isinstance(column, dict):
                     data = column.dict()
                 else:
                     data = column
-                temp_result.append((*data.get("fromColumns", []), data.get("toColumn")))
+                if data.get("toColumn") and data.get("fromColumns"):
+                    temp_result.append((*data.get("fromColumns", []), data.get("toColumn")))
         except Exception as exc:
             logger.debug(f"Error while merging column lineage: {exc}")
             logger.debug(traceback.format_exc())
