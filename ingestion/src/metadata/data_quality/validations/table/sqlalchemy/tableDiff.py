abf887bfa5970d013ec6afa2c53c7accb8af4561
sushi30 <imri.paran@gmail.com>
2024-06-21 18:16:13 +0200

    feat(table-diff): added column validation
    added column validation for table diff that will be carried out before running the row level diff. If a diff for the column exists, it will short circuit the test and report.

--- ingestion/src/metadata/data_quality/validations/table/sqlalchemy/tableDiff.py
+++ ingestion/src/metadata/data_quality/validations/table/sqlalchemy/tableDiff.py
@@ -11,11 +11,15 @@
 # pylint: disable=missing-module-docstring
 import traceback
 from itertools import islice
-from typing import Optional
+from typing import Dict, List, Optional
 from urllib.parse import urlparse
 
 import data_diff
+import sqlalchemy.types
 from data_diff.diff_tables import DiffResultWrapper
+from data_diff.errors import DataDiffMismatchingKeyTypesError
+from data_diff.utils import safezip
+from sqlalchemy import Column as SAColumn
 
 from metadata.data_quality.validations.base_test_handler import BaseTestValidator
 from metadata.data_quality.validations.mixins.sqa_validator_mixin import (
@@ -25,6 +29,7 @@ from metadata.data_quality.validations.models import TableDiffRuntimeParameters
 from metadata.data_quality.validations.runtime_param_setter.table_diff_params_setter import (
     TableDiffParamsSetter,
 )
+from metadata.generated.schema.entity.data.table import Column
 from metadata.generated.schema.entity.services.connections.database.sapHanaConnection import (
     SapHanaScheme,
 )
@@ -72,15 +77,12 @@ class TableDiffValidator(BaseTestValidator, SQAValidatorMixin):
         try:
             self._validate_dialects()
             return self._run()
-        except KeyError as e:
+        except DataDiffMismatchingKeyTypesError as e:
             result = TestCaseResult(
                 timestamp=self.execution_date,  # type: ignore
                 testCaseStatus=TestCaseStatus.Failed,
-                result=f"MISMATCHED_COLUMNS: One of the tables is missing the column: '{e}'\n"
-                "Use two tables with the same schema or provide the extraColumns parameter.",
-                testResultValue=[TestResultValue(name="diffCount", value=str(0))],
+                result=str(e),
             )
-            logger.error(result.result)
             return result
         except UnsupportedDialectError as e:
             result = TestCaseResult(
@@ -102,6 +104,9 @@ class TableDiffValidator(BaseTestValidator, SQAValidatorMixin):
             return result
 
     def _run(self) -> TestCaseResult:
+        result = self.get_column_diff()
+        if result:
+            return result
         threshold = self.get_test_case_param_value(
             self.test_case.parameterValues, "threshold", int, default=0
         )
@@ -136,15 +141,69 @@ class TableDiffValidator(BaseTestValidator, SQAValidatorMixin):
             threshold,
         )
 
+    def get_incomparable_columns(self) -> List[str]:
+        """Get the columns that have incompatible types in the two tables"""
+        table1 = data_diff.connect_to_table(
+            self.runtime_params.table1.serviceUrl,
+            self.runtime_params.table1.path,
+            self.runtime_params.keyColumns,
+            extra_columns=self.runtime_params.extraColumns,
+        ).with_schema()
+        table2 = data_diff.connect_to_table(
+            self.runtime_params.table2.serviceUrl,
+            self.runtime_params.table2.path,
+            self.runtime_params.keyColumns,
+            extra_columns=self.runtime_params.extraColumns,
+        ).with_schema()
+        result = []
+        columns1 = table1.key_columns + table1.extra_columns
+        columns2 = table2.key_columns + table2.extra_columns
+        for col1, col2 in safezip(columns1, columns2):
+            col1_type = self.get_column_type(
+                table1._schema[col1]  # pylint: disable=protected-access
+            )
+            col2_type = self.get_column_type(
+                table2._schema[col2]  # pylint: disable=protected-access
+            )
+            if col1_type != col2_type:
+                result.append(col1)
+        return result
+
+    @staticmethod
+    def get_column_type(column: SAColumn):
+        """Try to resolve the python_type of a column by cascading through different SQLAlchemy types.
+        If no type is found, return the name of the column type. This is usually undesirable since it can
+        be very database specific, but it is better than nothing.
+
+        Args:
+            column: An SQLAlchemy column object
+        """
+        try:
+            return column.python_type
+        except AttributeError:
+            pass
+        try:
+            return getattr(sqlalchemy.types, type(column).__name__)().python_type
+        except AttributeError:
+            pass
+        try:
+            column = getattr(sqlalchemy.types, type(column).__name__.upper())()
+        except AttributeError:
+            pass
+        return type(column).__name__
+
     def get_table_diff(self) -> DiffResultWrapper:
         """Calls data_diff.diff_tables with the parameters from the test case."""
         table1 = data_diff.connect_to_table(
-            self.runtime_params.service1Url, self.runtime_params.table1, self.runtime_params.keyColumns  # type: ignore
+            self.runtime_params.table1.serviceUrl,
+            self.runtime_params.table1.path,
+            self.runtime_params.keyColumns,  # type: ignore
         )
         table2 = data_diff.connect_to_table(
-            self.runtime_params.service2Url, self.runtime_params.table2, self.runtime_params.keyColumns  # type: ignore
+            self.runtime_params.table2.serviceUrl,
+            self.runtime_params.table2.path,
+            self.runtime_params.keyColumns,  # type: ignore
         )
-
         data_diff_kwargs = {
             "key_columns": self.runtime_params.keyColumns,
             "extra_columns": self.runtime_params.extraColumns,
@@ -170,10 +229,6 @@ class TableDiffValidator(BaseTestValidator, SQAValidatorMixin):
         )
         runtime_params = TableDiffRuntimeParameters.parse_raw(raw)
         return runtime_params
-
-    @property
-    def _param_name(self):
-        return "forbiddenRegex"
 
     def get_test_case_result(
         self,
@@ -203,7 +258,86 @@ class TableDiffValidator(BaseTestValidator, SQAValidatorMixin):
         )
 
     def _validate_dialects(self):
-        for param in ["service1Url", "service2Url"]:
-            dialect = urlparse(getattr(self.runtime_params, param)).scheme
+        for name, param in [
+            ("table1.serviceUrl", self.runtime_params.table1.serviceUrl),
+            ("table2.serviceUrl", self.runtime_params.table2.serviceUrl),
+        ]:
+            dialect = urlparse(param).scheme
             if dialect not in SUPPORTED_DIALECTS:
-                raise UnsupportedDialectError(param, dialect)
+                raise UnsupportedDialectError(name, dialect)
+
+    def get_column_diff(self) -> Optional[TestCaseResult]:
+        column_diff = self.calculate_column_diff(
+            self.runtime_params.table1.columns, self.runtime_params.table2.columns
+        )
+        changed = self.get_incomparable_columns()
+        if column_diff["removed"] or column_diff["added"] or changed:
+            return self.column_validation_result(
+                column_diff["removed"],
+                column_diff["added"],
+                changed,
+            )
+        return None
+
+    @staticmethod
+    def calculate_column_diff(
+        left: List[Column], right: List[Column]
+    ) -> Dict[str, List[str]]:
+        removed: List[str] = []
+        added: List[str] = []
+        right_columns_dict: Dict[str, Column] = {c.name.root: c for c in right}
+        for column in left:
+            table2_column = right_columns_dict.get(column.name.root)
+            if table2_column is None:
+                removed.append(column.name.root)
+                continue
+            del right_columns_dict[column.name.root]
+        added.extend(right_columns_dict.keys())
+        return {
+            "removed": removed,
+            "added": added,
+        }
+
+    def column_validation_result(
+        self, removed: List[str], added: List[str], changed: List[str]
+    ) -> TestCaseResult:
+        """Build the result for a column validation result. Messages will only be added
+        for non-empty categories. Values will be populated reported for all categories.
+
+        Args:
+            removed: List of removed columns
+            added: List of added columns
+            changed: List of changed columns
+
+        Returns:
+            TestCaseResult: The result of the column validation with a meaningful message
+        """
+        message = (
+            f"Tables have {sum(map(len, [removed, added, changed]))} different columns:"
+        )
+        if removed:
+            message += f"\n  Removed columns: {','.join(removed)}\n"
+        if added:
+            message += f"\n  Added columns: {','.join(added)}\n"
+        if changed:
+            message += "\n  Changed columns:"
+            for col in changed:
+                col1 = next(
+                    c for c in self.runtime_params.table1.columns if c.name.root == col
+                )
+                col2 = next(
+                    c for c in self.runtime_params.table2.columns if c.name.root == col
+                )
+                message += (
+                    f"\n    {col}: {col1.dataType.value} -> {col2.dataType.value}"
+                )
+        return TestCaseResult(
+            timestamp=self.execution_date,  # type: ignore
+            testCaseStatus=TestCaseStatus.Failed,
+            result=message,
+            testResultValue=[
+                TestResultValue(name="removedColumns", value=str(len(removed))),
+                TestResultValue(name="addedColumns", value=str(len(added))),
+                TestResultValue(name="changedColumns", value=str(len(changed))),
+            ],
+        )
