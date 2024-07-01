373a8e7697cc8fbbeb8259f1e72ec770fd2de1ac
Mohit Yadav <105265192+mohityadav766@users.noreply.github.com>
2024-07-01 22:26:56 +0530

    Add Assets field to domain (#16871)

--- openmetadata-service/src/main/java/org/openmetadata/service/jdbi3/DomainRepository.java
+++ openmetadata-service/src/main/java/org/openmetadata/service/jdbi3/DomainRepository.java
@@ -16,7 +16,9 @@ package org.openmetadata.service.jdbi3;
 import static org.openmetadata.common.utils.CommonUtil.listOrEmpty;
 import static org.openmetadata.schema.type.Include.ALL;
 import static org.openmetadata.service.Entity.DOMAIN;
+import static org.openmetadata.service.Entity.FIELD_ASSETS;
 
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.jdbi.v3.sqlobject.transaction.Transaction;
 import org.openmetadata.schema.EntityInterface;
@@ -48,6 +50,7 @@ public class DomainRepository extends EntityRepository<Domain> {
 
   @Override
   public void setFields(Domain entity, Fields fields) {
+    entity.withAssets(fields.contains(FIELD_ASSETS) ? getAssets(entity) : null);
     entity.withParent(getParent(entity));
   }
 
@@ -89,6 +92,10 @@ public class DomainRepository extends EntityRepository<Domain> {
       inheritOwner(domain, fields, parent);
       inheritExperts(domain, fields, parent);
     }
+  }
+
+  private List<EntityReference> getAssets(Domain entity) {
+    return findTo(entity.getId(), DOMAIN, Relationship.HAS, null);
   }
 
   public BulkOperationResult bulkAddAssets(String domainName, BulkAssets request) {
