03ce39241afd9cc2ea57e1acb053dca4155fc392
Shailesh Parmar <shailesh.parmar.webdev@gmail.com>
2024-07-05 16:16:04 +0530

    change in config

--- openmetadata-ui/src/main/resources/ui/playwright.config.ts
+++ openmetadata-ui/src/main/resources/ui/playwright.config.ts
@@ -32,10 +32,11 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 4 : undefined,
+  workers: process.env.CI ? 3 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
-    [process.env.CI ? 'github' : 'list'],
+    [github],
+    ['list'],
     ['html', { outputFolder: './playwright/output/playwright-report' }],
     [
       '@estruyf/github-actions-reporter',
