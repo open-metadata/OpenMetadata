e48ca29582e6251dca00ed7821992db069336bb6
Shailesh Parmar <shailesh.parmar.webdev@gmail.com>
2024-07-09 15:55:33 +0530

    addressing comment

--- openmetadata-ui/src/main/resources/ui/playwright/utils/entity.ts
+++ openmetadata-ui/src/main/resources/ui/playwright/utils/entity.ts
@@ -23,7 +23,7 @@ import {
   LIST_OF_FIELDS_TO_EDIT_TO_BE_DISABLED,
 } from '../constant/delete';
 import { EntityTypeEndpoint } from '../support/entity/Entity.interface';
-import { redirectToHomePage } from './common';
+import { clickOutside, redirectToHomePage } from './common';
 
 export const visitEntityPage = async (data: {
   page: Page;
@@ -130,7 +130,7 @@ export const assignTier = async (page: Page, tier: string) => {
   await page.getByTestId('edit-tier').click();
   await page.waitForSelector('[data-testid="loader"]', { state: 'detached' });
   await page.getByTestId(`radio-btn-${tier}`).click();
-  await page.mouse.click(1280, 0); // click mouse to top right corner
+  await clickOutside(page);
 
   await expect(page.getByTestId('Tier')).toContainText(tier);
 };
@@ -139,7 +139,7 @@ export const removeTier = async (page: Page) => {
   await page.getByTestId('edit-tier').click();
   await page.waitForSelector('[data-testid="loader"]', { state: 'detached' });
   await page.getByTestId('clear-tier').click();
-  await page.mouse.click(1280, 0); // click mouse to top right corner
+  await clickOutside(page);
 
   await expect(page.getByTestId('Tier')).toContainText('No Tier');
 };
