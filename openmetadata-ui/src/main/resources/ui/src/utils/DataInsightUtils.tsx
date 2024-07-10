8967cb2e6bdf54fc15b3d188eda1965e9d03e90a
Chira Madlani <chirag@getcollate.io>
2024-07-10 12:42:01 +0530

    startcase for tooltip

--- openmetadata-ui/src/main/resources/ui/src/utils/DataInsightUtils.tsx
+++ openmetadata-ui/src/main/resources/ui/src/utils/DataInsightUtils.tsx
@@ -173,7 +173,7 @@ export const CustomTooltip = (props: DataInsightChartTooltipProps) => {
                 <Surface className="mr-2" height={12} version="1.1" width={12}>
                   <rect fill={entry.color} height="14" rx="2" width="14" />
                 </Surface>
-                {entry.name ?? startCase(entry.dataKey as string)}
+                {startCase(entry.name ?? (entry.dataKey as string))}
               </span>
               <span className="font-medium">
                 {valueFormatter
