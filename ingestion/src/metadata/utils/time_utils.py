4715051135afe65958ee33d5bf3f403d55e660aa
Imri Paran <imri.paran@gmail.com>
2024-07-11 14:23:07 +0200

    chore: timedelta to string (#16985)
    implemented utils.time_utils..timedelta_to_string

--- ingestion/src/metadata/utils/time_utils.py
+++ ingestion/src/metadata/utils/time_utils.py
@@ -14,6 +14,7 @@ Time utility functions
 """
 
 from datetime import datetime, time, timedelta, timezone
+from math import floor
 from typing import Union
 
 from metadata.utils.helpers import datetime_to_ts
@@ -124,3 +125,42 @@ def convert_timestamp_to_milliseconds(timestamp: Union[int, float]) -> int:
     if len(str(round(timestamp))) == 13:
         return timestamp
     return round(timestamp * 1000)
+
+
+def timedelta_to_string(td: timedelta):
+    """Convert timedelta to human readable string
+
+    Example:
+        >>> timedelta_to_string(timedelta(days=1, hours=2, minutes=3, seconds=4))
+        '1 days 2 hours 3 minutes 4 seconds (total seconds: 93784.0)'
+
+    Args:
+        td (timedelta): timedelta object
+
+    Returns:
+        str: human readable string
+    """
+    res = []
+    current = td
+    if current.days:
+        res.append(f"{floor(td.days)} day")
+        if current.days > 1:
+            res[-1] += "s"
+        current -= timedelta(days=floor(td.days))
+    hours = current.seconds // 3600
+    if hours:
+        res.append(f"{hours} hour")
+        if hours > 1:
+            res[-1] += "s"
+        current -= timedelta(hours=hours)
+    minutes = current.seconds // 60
+    if minutes:
+        res.append(f"{minutes} minute")
+        if minutes > 1:
+            res[-1] += "s"
+        current -= timedelta(minutes=minutes)
+    res.append(f"{current.seconds} second")
+    if current.seconds != 1:
+        res[-1] += "s"
+    total_seconds = "total seconds: " + str(td.total_seconds())
+    return " ".join(res) + f" ({total_seconds})"
