d9b93358c578d4d8c34029718d94394ec8f17f4c
sushi30 <imri.paran@gmail.com>
2024-07-11 15:35:45 +0200

    changed mapping in common type map

--- ingestion/tests/integration/sql_server/test_profiler.py
+++ ingestion/tests/integration/sql_server/test_profiler.py
@@ -10,7 +10,6 @@ if not sys.version_info >= (3, 9):
     pytest.skip("requires python 3.9+", allow_module_level=True)
 
 
-@pytest.mark.xfail("fails due to serialization of Any in TableData", strict=True)
 def test_profiler(
     patch_passwords_for_db_services, run_workflow, ingestion_config, profiler_config
 ):
