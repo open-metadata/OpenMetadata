0fee79b200618dd4311ade81e8de2940a600e571
Imri Paran <imri.paran@gmail.com>
2024-07-17 08:11:34 +0200

    MINOR: fix sample data issue with Pydantic v2 and refactor python integration tests (#16943)
    * tests: refactor
    
    refactor tests and consolidate common functionality in integrations.conftest
    
    this enables writing tests more concisely.
    demonstrated with postgres and mssql.
    will migrate more
    
    * format
    
    * removed helpers
    
    * changed scope of fictures
    
    * changed scope of fixtures
    
    * added profiler test for mssql
    
    * fixed import in data_quality test
    
    * json safe serialization
    
    * format
    
    * set MARS_Connection
    
    * use SerializableTableData instead of TableData
    
    * deleted file test_postgres.py
    
    * fixed tests
    
    * added more test cases
    
    * format
    
    * changed name test_models.py
    
    * removed the logic for serializing table data
    
    * wip
    
    * changed mapping in common type map
    
    * changed mapping in common type map
    
    * reverted TableData imports
    
    * reverted TableData imports
    
    * reverted TableData imports

---
+++ ingestion/tests/integration/trino/test_profiler.py
@@ -0,0 +1,15 @@
+from metadata.ingestion.lineage.sql_lineage import search_cache
+from metadata.workflow.metadata import MetadataWorkflow
+from metadata.workflow.profiler import ProfilerWorkflow
+
+
+def test_profiler(
+    patch_passwords_for_db_services,
+    run_workflow,
+    ingestion_config,
+    profiler_config,
+    create_test_data,
+):
+    search_cache.clear()
+    run_workflow(MetadataWorkflow, ingestion_config)
+    run_workflow(ProfilerWorkflow, profiler_config)
