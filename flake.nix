4d8ff3c1b4a1acd087d16b60815b528f48c4cc80
Nicola Coretti <nicola.coretti@exasol.com>
2024-07-19 15:42:33 +0200

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "A nix flake containing the full Open-Metadata dev stack except python";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.05";
+  };
+
+  outputs = { self, nixpkgs }: 
+    let
+        system = "x86_64-linux";
+        pkgs = nixpkgs.legacyPackages.${system};
+    in 
+    with pkgs;
+    {
+        devShell = {
+            x86_64-linux = pkgs.mkShell {
+              buildInputs = [ 
+                pkgs.yarn 
+                pkgs.jre17_minimal
+                pkgs.nodejs_18
+                pkgs.docker
+                pkgs.maven
+                pkgs.antlr4_9
+                pkgs.jq
+              ];
+            };
+        };
+    };
+}
