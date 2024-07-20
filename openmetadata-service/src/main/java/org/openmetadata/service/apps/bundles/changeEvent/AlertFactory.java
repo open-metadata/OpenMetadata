ca6661b74215ea370d1b4e40d840e344d0151a95
Mohit Yadav <105265192+mohityadav766@users.noreply.github.com>
2024-07-20 18:51:58 +0530

    Add Alert Name to Publishers (#17108)
    * Add Alert Name to Publishers
    
    * Fix Test

--- openmetadata-service/src/main/java/org/openmetadata/service/apps/bundles/changeEvent/AlertFactory.java
+++ openmetadata-service/src/main/java/org/openmetadata/service/apps/bundles/changeEvent/AlertFactory.java
@@ -2,6 +2,7 @@ package org.openmetadata.service.apps.bundles.changeEvent;
 
 import static org.openmetadata.schema.api.events.CreateEventSubscription.AlertType.ACTIVITY_FEED;
 
+import org.openmetadata.schema.entity.events.EventSubscription;
 import org.openmetadata.schema.entity.events.SubscriptionDestination;
 import org.openmetadata.schema.type.ChangeEvent;
 import org.openmetadata.service.apps.bundles.changeEvent.email.EmailPublisher;
@@ -12,14 +13,15 @@ import org.openmetadata.service.apps.bundles.changeEvent.msteams.MSTeamsPublishe
 import org.openmetadata.service.apps.bundles.changeEvent.slack.SlackEventPublisher;
 
 public class AlertFactory {
-  public static Destination<ChangeEvent> getAlert(SubscriptionDestination config) {
+  public static Destination<ChangeEvent> getAlert(
+      EventSubscription subscription, SubscriptionDestination config) {
     return switch (config.getType()) {
-      case SLACK -> new SlackEventPublisher(config);
-      case MS_TEAMS -> new MSTeamsPublisher(config);
-      case G_CHAT -> new GChatPublisher(config);
-      case WEBHOOK -> new GenericPublisher(config);
-      case EMAIL -> new EmailPublisher(config);
-      case ACTIVITY_FEED -> new ActivityFeedPublisher(config);
+      case SLACK -> new SlackEventPublisher(subscription, config);
+      case MS_TEAMS -> new MSTeamsPublisher(subscription, config);
+      case G_CHAT -> new GChatPublisher(subscription, config);
+      case WEBHOOK -> new GenericPublisher(subscription, config);
+      case EMAIL -> new EmailPublisher(subscription, config);
+      case ACTIVITY_FEED -> new ActivityFeedPublisher(subscription, config);
     };
   }
 }
