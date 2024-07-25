d6d083885146140878be9944cf808f72e64ad871
Suman Maharana <sumanmaharana786@gmail.com>
2024-07-25 21:01:04 +0530

    Minor: Fix tableau e2e changed filter count (#17180)

--- ingestion/tests/cli_e2e/test_cli_tableau.py
+++ ingestion/tests/cli_e2e/test_cli_tableau.py
@@ -73,7 +73,7 @@ class TableauCliTest(CliCommonDashboard.TestSuite):
         return 2
 
     def expected_filtered_sink_mix(self) -> int:
-        return 14
+        return 13
 
     def expected_dashboards_and_charts_after_patch(self) -> int:
         return 4
