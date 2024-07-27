769c16eacb4b26b6cbdb0e7f31cc07cefcc2844a
ulixius9 <mayursingal9@gmail.com>
2024-07-27 19:31:52 +0530

    address comments

--- bootstrap/sql/migrations/native/1.5.0/mysql/postDataMigrationSQLScript.sql
+++ bootstrap/sql/migrations/native/1.5.0/mysql/postDataMigrationSQLScript.sql
@@ -24,26 +24,6 @@ WHERE name IN (
     'tableRowCountToBeBetween'
 );
 
--- Update DeltaLake service due to connection schema changes to enable DeltaLake ingestion from Storage
-UPDATE dbservice_entity dbse
-SET
-  dbse.json = JSON_REMOVE(JSON_REMOVE(
-  JSON_MERGE_PATCH(
-    dbse.json,
-    JSON_OBJECT(
-      'connection', JSON_OBJECT(
-        'config', JSON_OBJECT(
-          'configSource', JSON_OBJECT(
-            'connection', JSON_EXTRACT(dbse.json, '$.connection.config.metastoreConnection'),
-            'appName', JSON_UNQUOTE(JSON_EXTRACT(dbse.json, '$.connection.config.appName'))
-          )
-        )
-      )
-    )
-  )
-  , '$.connection.config.appName'), '$.connection.config.metastoreConnection')
-WHERE dbse.serviceType = 'DeltaLake';
-
 
 -- KPI Migrations
 UPDATE entity_relationship
--- bootstrap/sql/migrations/native/1.5.0/postgres/postDataMigrationSQLScript.sql
+++ bootstrap/sql/migrations/native/1.5.0/postgres/postDataMigrationSQLScript.sql
@@ -23,18 +23,6 @@ WHERE name IN (
     'tableRowCountToBeBetween'
 );
 
--- Update DeltaLake service due to connection schema changes to enable DeltaLake ingestion from Storage
-UPDATE dbservice_entity
-SET json = JSONB_SET(
-  JSONB_SET(
-    json,
-    '{connection,config,configSource}',
-    JSONB_BUILD_OBJECT('connection', json->'connection'->'config'->'metastoreConnection')
-  ),
-  '{connection,config,configSource,appName}',
-  json->'connection'->'config'->'appName'
-) #- '{connection,config,metastoreConnection}' #- '{connection,config,appName}'
-WHERE serviceType = 'DeltaLake';
 
 -- KPI Migrations
 UPDATE entity_relationship
