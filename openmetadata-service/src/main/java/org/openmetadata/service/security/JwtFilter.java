66cc8ddd141433b77a2f473279fbe27b91b43d97
Siddhanttimeline <siddhanttimeline9@gmail.com>
2024-08-05 00:27:00 +0530

    slack bolt app integration.

--- openmetadata-service/src/main/java/org/openmetadata/service/security/JwtFilter.java
+++ openmetadata-service/src/main/java/org/openmetadata/service/security/JwtFilter.java
@@ -90,7 +90,9 @@ public class JwtFilter implements ContainerRequestFilter {
           "v1/users/password/reset",
           "v1/users/checkEmailInUse",
           "v1/users/login",
-          "v1/users/refresh");
+          "v1/users/refresh",
+          "v1/collate/apps/slack/callback",
+           "v1/collate/apps/slack/events");
 
   @SuppressWarnings("unused")
   private JwtFilter() {}
