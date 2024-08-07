3c3c0b020ac6189d86e70238b1dd0c9a0b6c21ac
Pablo Takara <pjt1991@gmail.com>
2024-08-07 08:52:34 +0200

    Improve DI Error Log for OpenSearch

--- openmetadata-service/src/main/java/org/openmetadata/service/search/opensearch/OpenSearchIndexSink.java
+++ openmetadata-service/src/main/java/org/openmetadata/service/search/opensearch/OpenSearchIndexSink.java
@@ -95,7 +95,7 @@ public class OpenSearchIndexSink implements Sink<BulkRequest, BulkResponse> {
                 .withSubmittedCount(data.numberOfActions())
                 .withSuccessCount(data.numberOfActions() - entityErrorList.size())
                 .withFailedCount(entityErrorList.size())
-                .withMessage("Issues in Sink To Elastic Search.")
+                .withMessage(String.format("Issues in Sink To Elastic Search: %s", entityErrorList))
                 .withFailedEntities(entityErrorList));
       }
 
