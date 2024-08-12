a80b43bb5d9cf4a82229e56ff83297bacffca512
Chirag Madlani <12962843+chirag-madlani@users.noreply.github.com>
2024-08-12 20:29:36 +0530

    Revert toastNotification changes

--- openmetadata-ui/src/main/resources/ui/playwright/utils/common.ts
+++ openmetadata-ui/src/main/resources/ui/playwright/utils/common.ts
@@ -109,7 +109,7 @@ export const toastNotification = async (
 ) => {
   await expect(page.getByRole('alert').first()).toHaveText(message);
 
-  await page.getByRole('button', { name: 'close' }).first().click();
+  await page.getByLabel('button').first().click();
 };
 
 export const clickOutside = async (page: Page) => {
