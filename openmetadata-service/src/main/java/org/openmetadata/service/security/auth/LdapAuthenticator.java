ce0457a3d4778895c5ca946a0a416311b0f8d303
Siddhant <86899184+Siddhanttimeline@users.noreply.github.com>
2024-08-13 11:36:43 +0530

    added /refresh implementation for LDAP with a refresh token expiry time of 24 hours. (#17410)

--- openmetadata-service/src/main/java/org/openmetadata/service/security/auth/LdapAuthenticator.java
+++ openmetadata-service/src/main/java/org/openmetadata/service/security/auth/LdapAuthenticator.java
@@ -1,14 +1,18 @@
 package org.openmetadata.service.security.auth;
 
+import static javax.ws.rs.core.Response.Status.BAD_REQUEST;
 import static javax.ws.rs.core.Response.Status.FORBIDDEN;
 import static javax.ws.rs.core.Response.Status.INTERNAL_SERVER_ERROR;
 import static javax.ws.rs.core.Response.Status.UNAUTHORIZED;
+import static org.openmetadata.common.utils.CommonUtil.nullOrEmpty;
 import static org.openmetadata.schema.auth.TokenType.REFRESH_TOKEN;
 import static org.openmetadata.service.exception.CatalogExceptionMessage.INVALID_EMAIL_PASSWORD;
 import static org.openmetadata.service.exception.CatalogExceptionMessage.INVALID_USER_OR_PASSWORD;
 import static org.openmetadata.service.exception.CatalogExceptionMessage.LDAP_MISSING_ATTR;
 import static org.openmetadata.service.exception.CatalogExceptionMessage.MAX_FAILED_LOGIN_ATTEMPT;
 import static org.openmetadata.service.exception.CatalogExceptionMessage.MULTIPLE_EMAIL_ENTRIES;
+import static org.openmetadata.service.exception.CatalogExceptionMessage.PASSWORD_RESET_TOKEN_EXPIRED;
+import static org.openmetadata.service.util.UserUtil.getRoleListFromUser;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
@@ -28,15 +32,21 @@ import com.unboundid.util.ssl.SSLUtil;
 import freemarker.template.TemplateException;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
+import java.time.Instant;
 import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import javax.ws.rs.BadRequestException;
 import lombok.extern.slf4j.Slf4j;
 import org.openmetadata.common.utils.CommonUtil;
+import org.openmetadata.schema.TokenInterface;
 import org.openmetadata.schema.api.teams.CreateUser;
+import org.openmetadata.schema.auth.JWTAuthMechanism;
 import org.openmetadata.schema.auth.LdapConfiguration;
 import org.openmetadata.schema.auth.LoginRequest;
 import org.openmetadata.schema.auth.RefreshToken;
+import org.openmetadata.schema.auth.ServiceTokenType;
+import org.openmetadata.schema.auth.TokenRefreshRequest;
 import org.openmetadata.schema.entity.teams.Role;
 import org.openmetadata.schema.entity.teams.User;
 import org.openmetadata.schema.services.connections.metadata.AuthProvider;
@@ -52,6 +62,7 @@ import org.openmetadata.service.jdbi3.TokenRepository;
 import org.openmetadata.service.jdbi3.UserRepository;
 import org.openmetadata.service.security.AuthenticationException;
 import org.openmetadata.service.security.SecurityUtil;
+import org.openmetadata.service.security.jwt.JWTTokenGenerator;
 import org.openmetadata.service.util.EmailUtil;
 import org.openmetadata.service.util.JsonUtils;
 import org.openmetadata.service.util.LdapUtil;
@@ -403,7 +414,63 @@ public class LdapAuthenticator implements AuthenticatorHandler {
   public RefreshToken createRefreshTokenForLogin(UUID currentUserId) {
     // just delete the existing token
     tokenRepository.deleteTokenByUserAndType(currentUserId, REFRESH_TOKEN.toString());
-    RefreshToken newRefreshToken = TokenUtil.getRefreshToken(currentUserId, UUID.randomUUID());
+    RefreshToken newRefreshToken =
+        TokenUtil.getRefreshTokenForLDAP(currentUserId, UUID.randomUUID());
+    // save Refresh Token in Database
+    tokenRepository.insertToken(newRefreshToken);
+
+    return newRefreshToken;
+  }
+
+  @Override
+  public JwtResponse getNewAccessToken(TokenRefreshRequest request) {
+    if (CommonUtil.nullOrEmpty(request.getRefreshToken())) {
+      throw new BadRequestException("Token Cannot be Null or Empty String");
+    }
+    TokenInterface tokenInterface = tokenRepository.findByToken(request.getRefreshToken());
+    User storedUser =
+        userRepository.get(
+            null, tokenInterface.getUserId(), userRepository.getFieldsWithUserAuth("*"));
+    if (storedUser.getIsBot() != null && storedUser.getIsBot()) {
+      throw new IllegalArgumentException("User are only allowed to login");
+    }
+    RefreshToken refreshToken = validateAndReturnNewRefresh(storedUser.getId(), request);
+    JWTAuthMechanism jwtAuthMechanism =
+        JWTTokenGenerator.getInstance()
+            .generateJWTToken(
+                storedUser.getName(),
+                getRoleListFromUser(storedUser),
+                !nullOrEmpty(storedUser.getIsAdmin()) && storedUser.getIsAdmin(),
+                storedUser.getEmail(),
+                SecurityUtil.getLoginConfiguration().getJwtTokenExpiryTime(),
+                false,
+                ServiceTokenType.OM_USER);
+    JwtResponse response = new JwtResponse();
+    response.setTokenType("Bearer");
+    response.setAccessToken(jwtAuthMechanism.getJWTToken());
+    response.setRefreshToken(refreshToken.getToken().toString());
+    response.setExpiryDuration(jwtAuthMechanism.getJWTTokenExpiresAt());
+
+    return response;
+  }
+
+  public RefreshToken validateAndReturnNewRefresh(
+      UUID currentUserId, TokenRefreshRequest tokenRefreshRequest) {
+    String requestRefreshToken = tokenRefreshRequest.getRefreshToken();
+    RefreshToken storedRefreshToken =
+        (RefreshToken) tokenRepository.findByToken(requestRefreshToken);
+    if (storedRefreshToken.getExpiryDate().compareTo(Instant.now().toEpochMilli()) < 0) {
+      throw new CustomExceptionMessage(
+          BAD_REQUEST,
+          PASSWORD_RESET_TOKEN_EXPIRED,
+          "Expired token. Please login again : " + storedRefreshToken.getToken().toString());
+    }
+
+    // just delete the existing token
+    tokenRepository.deleteToken(requestRefreshToken);
+    // we use rotating refresh token , generate new token
+    RefreshToken newRefreshToken =
+        TokenUtil.getRefreshTokenForLDAP(currentUserId, UUID.randomUUID());
     // save Refresh Token in Database
     tokenRepository.insertToken(newRefreshToken);
 
