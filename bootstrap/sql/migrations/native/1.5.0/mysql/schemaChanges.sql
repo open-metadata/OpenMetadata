2a8ee41c38af6f82b80a009db178e307dd9ead5b
Onkar Ravgan <onkar.10r@gmail.com>
2024-08-20 04:31:07 +0530

    Fix entity_ext owner update query (#17493)

--- bootstrap/sql/migrations/native/1.5.0/mysql/schemaChanges.sql
+++ bootstrap/sql/migrations/native/1.5.0/mysql/schemaChanges.sql
@@ -209,16 +209,13 @@ WHERE JSON_CONTAINS_PATH(json, 'one', '$.feedInfo.entitySpecificInfo.updatedOwne
 AND JSON_TYPE(JSON_EXTRACT(json, '$.feedInfo.entitySpecificInfo.updatedOwner')) <> 'ARRAY';
 
 -- Update entity_extension to move owner to array
-UPDATE entity_extension
-SET json = JSON_SET(
-    json,
-    '$.owner',
+update entity_extension set json = JSON_SET(
+    JSON_REMOVE(json, '$.owner'),
+    '$.owners',
     JSON_ARRAY(
         JSON_EXTRACT(json, '$.owner')
     )
-)
-WHERE JSON_CONTAINS_PATH(json, 'one', '$.owner')
-AND JSON_TYPE(JSON_EXTRACT(json, '$.owner')) <> 'ARRAY';
+) where json -> '$.owner' is not null;
 
 ALTER TABLE test_case MODIFY COLUMN `name` VARCHAR(512) GENERATED ALWAYS AS (json ->> '$.name') NOT NULL;
 
