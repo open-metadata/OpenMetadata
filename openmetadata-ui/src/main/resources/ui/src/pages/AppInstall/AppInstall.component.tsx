bee4bda5019ca81bb5c939a3d8e541f8d6075b6e
Chirag Madlani <12962843+chirag-madlani@users.noreply.github.com>
2024-08-20 22:13:11 +0530

    fix(ui): move schedule option logic to class base (#17503)
    * fix(ui): move schedule option logic to class base
    
    * skip datainsight run test covered in playwright

--- openmetadata-ui/src/main/resources/ui/src/pages/AppInstall/AppInstall.component.tsx
+++ openmetadata-ui/src/main/resources/ui/src/pages/AppInstall/AppInstall.component.tsx
@@ -26,7 +26,10 @@ import Loader from '../../components/common/Loader/Loader';
 import { TestSuiteIngestionDataType } from '../../components/DataQuality/AddDataQualityTest/AddDataQualityTest.interface';
 import TestSuiteScheduler from '../../components/DataQuality/AddDataQualityTest/components/TestSuiteScheduler';
 import PageLayoutV1 from '../../components/PageLayoutV1/PageLayoutV1';
-import applicationSchemaClassBase from '../../components/Settings/Applications/AppDetails/ApplicationsClassBase';
+import {
+  default as applicationSchemaClassBase,
+  default as applicationsClassBase,
+} from '../../components/Settings/Applications/AppDetails/ApplicationsClassBase';
 import AppInstallVerifyCard from '../../components/Settings/Applications/AppInstallVerifyCard/AppInstallVerifyCard.component';
 import IngestionStepper from '../../components/Settings/Services/Ingestion/IngestionStepper/IngestionStepper.component';
 import { STEPS_FOR_APP_INSTALL } from '../../constants/Applications.constant';
@@ -50,7 +53,6 @@ import {
   getMarketPlaceAppDetailsPath,
   getSettingPath,
 } from '../../utils/RouterUtils';
-import { getScheduleOptionsFromSchedules } from '../../utils/ScheduleUtils';
 import { showErrorToast, showSuccessToast } from '../../utils/ToastUtils';
 import './app-install.less';
 
@@ -81,15 +83,15 @@ const AppInstall = () => {
   );
 
   const { initialOptions, initialValue } = useMemo(() => {
-    let initialOptions;
-
-    if (appData?.name === 'DataInsightsReportApplication') {
-      initialOptions = ['week'];
-    } else if (appData?.appType === AppType.External) {
-      initialOptions = ['day'];
-    } else if (pipelineSchedules && !isEmpty(pipelineSchedules)) {
-      initialOptions = getScheduleOptionsFromSchedules(pipelineSchedules);
+    if (!appData) {
+      return {};
     }
+
+    const initialOptions = applicationsClassBase.getScheduleOptionsForApp(
+      appData?.name,
+      appData?.appType,
+      pipelineSchedules
+    );
 
     return {
       initialOptions,
