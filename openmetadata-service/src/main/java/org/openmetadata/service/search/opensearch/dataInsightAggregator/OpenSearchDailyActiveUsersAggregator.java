5b3d4056dd9b5f489f8095183be7c3e46c167de3
Pablo Takara <pjt1991@gmail.com>
2024-08-22 10:09:32 +0200

    Handle missing user on Web Analytics. Changed Daily Active Users to epoch timestamp

--- openmetadata-service/src/main/java/org/openmetadata/service/search/opensearch/dataInsightAggregator/OpenSearchDailyActiveUsersAggregator.java
+++ openmetadata-service/src/main/java/org/openmetadata/service/search/opensearch/dataInsightAggregator/OpenSearchDailyActiveUsersAggregator.java
@@ -1,5 +1,6 @@
 package org.openmetadata.service.search.opensearch.dataInsightAggregator;
 
+import java.time.ZonedDateTime;
 import java.util.List;
 import os.org.opensearch.search.aggregations.Aggregations;
 import os.org.opensearch.search.aggregations.bucket.histogram.Histogram;
@@ -22,8 +23,9 @@ public class OpenSearchDailyActiveUsersAggregator
   }
 
   @Override
-  protected String getKeyAsString(Histogram.Bucket bucket) {
-    return bucket.getKeyAsString();
+  protected long getKeyAsEpochTimestamp(Histogram.Bucket bucket) {
+
+    return ((ZonedDateTime) bucket.getKey()).toInstant().toEpochMilli();
   }
 
   @Override
