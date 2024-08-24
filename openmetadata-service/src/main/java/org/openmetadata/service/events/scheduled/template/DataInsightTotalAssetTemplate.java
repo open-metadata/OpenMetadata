248cb739d67017f043e444b6582c8633d6188c8a
Siddhant <86899184+Siddhanttimeline@users.noreply.github.com>
2024-08-24 11:43:54 +0530

    Changed Double to int for the DataAssetCount. Included % for the percentageChangeValue. Added colors to the % values with the digits. Corrected the incorrect placeholders. (#17578)

--- openmetadata-service/src/main/java/org/openmetadata/service/events/scheduled/template/DataInsightTotalAssetTemplate.java
+++ openmetadata-service/src/main/java/org/openmetadata/service/events/scheduled/template/DataInsightTotalAssetTemplate.java
@@ -16,22 +16,25 @@ package org.openmetadata.service.events.scheduled.template;
 import static org.openmetadata.service.events.scheduled.template.DataInsightDescriptionAndOwnerTemplate.getFormattedPercentChangeMessage;
 
 import java.util.Map;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
 @SuppressWarnings("unused")
 public class DataInsightTotalAssetTemplate {
   private String totalDataAssets;
   private String percentChangeTotalAssets;
-  private String percentChangeMessage;
-  private String completeMessage;
-  private int numberOfDaysChange;
-  private Map<String, Integer> dateMap;
+  @Setter private String percentChangeMessage;
+  @Setter private String completeMessage;
+  @Setter private int numberOfDaysChange;
+  @Setter private Map<String, Integer> dateMap;
 
   public DataInsightTotalAssetTemplate(
-      Double totalDataAssets,
+      String totalDataAssets,
       Double percentChangeTotalAssets,
       int numberOfDaysChange,
       Map<String, Integer> dateMap) {
-    this.totalDataAssets = String.format("%.2f", totalDataAssets);
+    this.totalDataAssets = totalDataAssets;
     this.percentChangeTotalAssets = String.format("%.2f", percentChangeTotalAssets);
     this.percentChangeMessage = getFormattedPercentChangeMessage(percentChangeTotalAssets);
     this.numberOfDaysChange = numberOfDaysChange;
@@ -40,57 +43,18 @@ public class DataInsightTotalAssetTemplate {
     if (percentChangeTotalAssets > 0) {
       color = "#008510";
     }
+
     completeMessage =
         String.format(
-            "In the past week, the Total Data Assets changed by <span style=\"color: %s; font-weight: bold;\">%s</span>%%.",
+            "In the past week, the Total Data Assets changed by <span style=\"color: %s; font-weight: bold;\">%s%%</span>.",
             color, this.percentChangeTotalAssets);
-  }
-
-  public String getTotalDataAssets() {
-    return totalDataAssets;
   }
 
   public void setTotalDataAssets(Double totalDataAssets) {
     this.totalDataAssets = String.format("%.2f", totalDataAssets);
   }
 
-  public String getPercentChangeTotalAssets() {
-    return percentChangeTotalAssets;
-  }
-
   public void setPercentChangeTotalAssets(Double percentChangeTotalAssets) {
     this.percentChangeTotalAssets = String.format("%.2f", percentChangeTotalAssets);
-  }
-
-  public String getCompleteMessage() {
-    return completeMessage;
-  }
-
-  public void setCompleteMessage(String completeMessage) {
-    this.completeMessage = completeMessage;
-  }
-
-  public String getPercentChangeMessage() {
-    return percentChangeMessage;
-  }
-
-  public void setPercentChangeMessage(String message) {
-    this.percentChangeMessage = message;
-  }
-
-  public int getNumberOfDaysChange() {
-    return numberOfDaysChange;
-  }
-
-  public void setNumberOfDaysChange(int numberOfDaysChange) {
-    this.numberOfDaysChange = numberOfDaysChange;
-  }
-
-  public Map<String, Integer> getDateMap() {
-    return dateMap;
-  }
-
-  public void setDateMap(Map<String, Integer> dateMap) {
-    this.dateMap = dateMap;
   }
 }
