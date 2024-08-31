123f2220261f5e3c28faf1fde2bc25a53715c3ea
Chirag Madlani <12962843+chirag-madlani@users.noreply.github.com>
2024-08-31 16:35:26 +0530

    test(e2e): service ingestion and schedule options (#16846)
    * test(e2e): service ingestion and schedule options
    
    * fix tests
    
    * minor change
    
    * remove duplicate code
    
    * update specs
    
    * update timeout to 2 mins
    
    * Fix invalid wait issue
    
    * fix page getting closed issue
    
    * fix failing tests
    
    * fix tests
    
    * fix test
    
    * fix schedule fix
    
    * fix status is undefined
    
    * fix more tests
    
    * enable airflow spec
    
    * fix typo in service names
    
    * update details
    
    * minor fix
    
    * fix test and remove cypress reference
    
    * fix failing tests
    
    * revert tags check
    
    * fix tests
    
    * fix redshift tests
    
    * fix failing tests
    
    * revert yaml file changes

--- openmetadata-ui/src/main/resources/ui/playwright/constant/service.ts
+++ openmetadata-ui/src/main/resources/ui/playwright/constant/service.ts
@@ -10,6 +10,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
+import { uuid } from '../utils/common';
 import { GlobalSettingOptions } from './settings';
 
 export const SERVICE_TYPE = {
@@ -23,4 +24,39 @@ export const SERVICE_TYPE = {
   Metadata: GlobalSettingOptions.METADATA,
   StoredProcedure: GlobalSettingOptions.STORED_PROCEDURES,
   ApiService: GlobalSettingOptions.APIS,
+};
+
+const uniqueID = uuid();
+
+export const REDSHIFT = {
+  serviceType: 'Redshift',
+  serviceName: `redshift-ct-test-${uniqueID}`,
+  tableName: 'raw_payments',
+  DBTTable: 'customers',
+  description: `This is Redshift-ct-test-${uniqueID} description`,
+};
+
+export const POSTGRES = {
+  serviceType: 'Postgres',
+  serviceName: `pw-postgres-test-${uuid()}`,
+  tableName: 'order_items',
+};
+
+export const HTTP_CONFIG_SOURCE = {
+  DBT_CATALOG_HTTP_PATH:
+    'https://raw.githubusercontent.com/OnkarVO7/dbt_git_test/dbt_aut/catalog.json',
+  DBT_MANIFEST_HTTP_PATH:
+    'https://raw.githubusercontent.com/OnkarVO7/dbt_git_test/dbt_aut/manifest.json',
+  DBT_RUN_RESULTS_FILE_PATH:
+    'https://raw.githubusercontent.com/OnkarVO7/dbt_git_test/dbt_aut/run_results.json',
+};
+
+export const DBT = {
+  classification: 'dbtTags',
+  tagName: 'model_tag_two',
+  dbtQuery: 'select * from "dev"."dbt_automate_upgrade_tests"."stg_orders"',
+  dbtLineageNodeLabel: 'customers',
+  dbtLineageNode: 'dev.dbt_automate_upgrade_tests.stg_customers',
+  dataQualityTest1: 'unique_customers_customer_id',
+  dataQualityTest2: 'not_null_customers_customer_id',
 };
