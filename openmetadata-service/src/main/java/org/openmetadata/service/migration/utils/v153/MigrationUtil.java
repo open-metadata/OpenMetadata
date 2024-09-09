c1c1a8202b12cd9330048b86322124aa9ebea6b1
Siddhanttimeline <siddhanttimeline9@gmail.com>
2024-09-09 14:30:55 +0530

    migrate userName to emailPrefix for ldap auth provider.

--- openmetadata-service/src/main/java/org/openmetadata/service/migration/utils/v153/MigrationUtil.java
+++ openmetadata-service/src/main/java/org/openmetadata/service/migration/utils/v153/MigrationUtil.java
@@ -1,7 +1,12 @@
 package org.openmetadata.service.migration.utils.v153;
 
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
+import org.openmetadata.schema.api.security.AuthenticationConfiguration;
+import org.openmetadata.schema.entity.teams.User;
+import org.openmetadata.schema.services.connections.metadata.AuthProvider;
 import org.openmetadata.service.jdbi3.CollectionDAO;
+import org.openmetadata.service.util.JsonUtils;
 
 @Slf4j
 public class MigrationUtil {
@@ -10,5 +15,28 @@ public class MigrationUtil {
     CollectionDAO.DocStoreDAO dao = collectionDAO.docStoreDAO();
     // delete emailTemplates, it will be loaded from initSeedData.
     dao.deleteEmailTemplates();
+  }
+
+  public static void updateUserNameToEmailPrefixForLdapAuthProvider(
+      CollectionDAO daoCollection, AuthenticationConfiguration config) {
+    if (config.getProvider().equals(AuthProvider.LDAP)) {
+      LOG.info("Starting migration username -> email prefix");
+      int total = daoCollection.userDAO().listTotalCount();
+      int offset = 0;
+      int limit = 200;
+      while (offset < total) {
+        List<String> userEntities = daoCollection.userDAO().listAfterWithOffset(limit, offset);
+        for (String json : userEntities) {
+          User userEntity = JsonUtils.readValue(json, User.class);
+          String email = userEntity.getEmail();
+          String emailPrefix = email.substring(0, email.indexOf("@"));
+          userEntity.setFullyQualifiedName(emailPrefix);
+
+          daoCollection.userDAO().update(userEntity);
+        }
+        offset = offset + limit;
+      }
+      LOG.info("Completed migrating username -> email prefix");
+    }
   }
 }
