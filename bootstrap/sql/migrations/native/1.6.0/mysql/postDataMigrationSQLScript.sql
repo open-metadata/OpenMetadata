2dffc107d02d00d8d1661e8c923cebebf9f91d50
Teddy Crepineau <teddy.crepineau@gmail.com>
2024-09-11 10:36:31 +0200

    fix: move constraint update from schemaChanges to postDataMigration

--- bootstrap/sql/migrations/native/1.6.0/mysql/postDataMigrationSQLScript.sql
+++ bootstrap/sql/migrations/native/1.6.0/mysql/postDataMigrationSQLScript.sql
@@ -5,3 +5,9 @@ SET dqdts.json = JSON_SET(dqdts.json,
 	'$.testCaseFQN', tc.json->'$.fullyQualifiedName',
 	'$.id', (SELECT UUID())
 );
+
+-- Add id column to data_quality_data_time_series table
+-- after we have added the id values to the records
+ALTER TABLE data_quality_data_time_series
+ADD COLUMN id VARCHAR(36) GENERATED ALWAYS AS (json ->> '$.id') STORED NOT NULL,
+ADD CONSTRAINT UNIQUE (id);
--- bootstrap/sql/migrations/native/1.6.0/postgres/postDataMigrationSQLScript.sql
+++ bootstrap/sql/migrations/native/1.6.0/postgres/postDataMigrationSQLScript.sql
@@ -6,3 +6,9 @@ SET json = jsonb_set(
           )
 FROM test_case tc
 WHERE dqdts.entityfqnHash = tc.fqnHash;
+
+-- Add id column to data_quality_data_time_series table
+-- after we have added the id values to the records
+ALTER TABLE data_quality_data_time_series
+ADD COLUMN id VARCHAR(36) GENERATED ALWAYS AS (json ->> 'id') STORED,
+ADD CONSTRAINT id_unique UNIQUE (id);
