42d922df3c880913ac965c67fe5cda956bcbe340
nakaken <k.nakagaki@churadata.okinawa>
2024-09-13 00:48:25 +0900

    Add test for existing code

---
+++ ingestion/tests/unit/profiler/sqlalchemy/snowflake/test_sampling_method.py
@@ -0,0 +1,67 @@
+from unittest import TestCase
+from unittest.mock import patch
+from uuid import uuid4
+
+from metadata.generated.schema.entity.data.table import Column as EntityColumn
+from metadata.generated.schema.entity.data.table import (
+    ColumnName,
+    DataType,
+    Table,
+)
+from metadata.generated.schema.entity.services.connections.database.snowflakeConnection import (
+    SnowflakeConnection,
+)
+from metadata.profiler.api.models import ProfileSampleConfig
+from metadata.profiler.interface.sqlalchemy.profiler_interface import (
+    SQAProfilerInterface,
+)
+from metadata.profiler.processor.sampler.sqlalchemy.snowflake.sampler import (
+    SnowflakeSampler,
+)
+from sqlalchemy import Column, Integer
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.sql.selectable import CTE
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+
+
+class SampleTest(TestCase):
+    table_entity = Table(
+        id=uuid4(),
+        name="user",
+        columns=[
+            EntityColumn(
+                name=ColumnName("id"),
+                dataType=DataType.INT,
+            ),
+        ],
+    )
+
+    snowflake_conn = SnowflakeConnection(
+        username="myuser", account="myaccount", warehouse="mywarehouse"
+    )
+
+    with patch.object(
+        SQAProfilerInterface, "_convert_table_to_orm_object", return_value=User
+    ):
+        sqa_profiler_interface = SQAProfilerInterface(
+            snowflake_conn, None, table_entity, None, None, None, None, None, 5, 43200
+        )
+    session = sqa_profiler_interface.session
+
+    def test_omit_sampling_method(self):
+        """
+        use BERNOULLI if sampling method is not specified.
+        """
+        sampler = SnowflakeSampler(
+            client=self.session,
+            table=User,
+            profile_sample_config=ProfileSampleConfig(profile_sample=50.0),
+        )
+        query: CTE = sampler.get_sample_query()
+        assert "FROM users SAMPLE BERNOULLI" in str(query)
