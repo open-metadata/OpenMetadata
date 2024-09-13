da2130846260cbe64b2ddb4a08af10c3cbff1210
karanh37 <karanh37@gmail.com>
2024-09-13 17:39:14 +0530

    cleanup

--- openmetadata-ui/src/main/resources/ui/playwright/utils/lineage.ts
+++ openmetadata-ui/src/main/resources/ui/playwright/utils/lineage.ts
@@ -170,18 +170,16 @@ export const setupEntitiesForLineage = async (
   ] as const;
 
   const { apiContext, afterAction } = await getApiContext(page);
-  await Promise.all([
-    ...entities.map((entity) => entity.create(apiContext)),
-    currentEntity.create(apiContext),
-  ]);
-
+  for (const entity of entities) {
+    await entity.create(apiContext);
+  }
   await currentEntity.create(apiContext);
 
   const cleanup = async () => {
-    await Promise.all([
-      currentEntity.delete(apiContext),
-      ...entities.map((entity) => entity.delete(apiContext)),
-    ]);
+    await currentEntity.delete(apiContext);
+    for (const entity of entities) {
+      await entity.delete(apiContext);
+    }
     await afterAction();
   };
 
