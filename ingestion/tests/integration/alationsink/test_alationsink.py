df99ab07b2e31af3a8fe4643c9edc8dcf407ee3b
Onkar Ravgan <onkar.10r@gmail.com>
2024-09-17 14:06:33 +0530

    fixed alationsink test (#17883)

--- ingestion/tests/integration/alationsink/test_alationsink.py
+++ ingestion/tests/integration/alationsink/test_alationsink.py
@@ -487,9 +487,9 @@ class AlationSinkTest(TestCase):
                     om_table=om_table,
                 )
             )
-        print(returned_tables)
-        for _, (expected, original) in enumerate(zip(EXPECTED_TABLES, returned_tables)):
-            self.assertEqual(expected, original)
+        self.assertEqual(len(returned_tables), len(EXPECTED_TABLES))
+        for expected_table in EXPECTED_TABLES:
+            self.assertIn(expected_table, returned_tables)
 
     def test_columns(self):
         """
