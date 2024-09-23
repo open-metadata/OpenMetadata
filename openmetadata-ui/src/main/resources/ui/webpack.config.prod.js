cb2df731b0bc789a66b45672d02d4437f07e6215
Mohit Yadav <105265192+mohityadav766@users.noreply.github.com>
2024-09-23 17:59:01 +0530

    revert for "fork-ts-checker-webpack-plugin": "6.5.0", (#17953)
    * yes filesystem cache in webpack
    
    * typo
    
    * revert for "fork-ts-checker-webpack-plugin": "6.5.0",

--- openmetadata-ui/src/main/resources/ui/webpack.config.prod.js
+++ openmetadata-ui/src/main/resources/ui/webpack.config.prod.js
@@ -31,6 +31,10 @@ module.exports = {
   // Input configuration
   entry: ['@babel/polyfill', path.join(__dirname, 'src/index.tsx')],
 
+  cache: {
+    type: 'filesystem', // Enable caching on filesystem
+  },
+
   // Output configuration
   output: {
     path: outputPath,
