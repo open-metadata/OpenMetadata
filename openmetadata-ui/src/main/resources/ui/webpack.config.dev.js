86514d2e13a3ed5b3c8ae83cdbd148e8d178dffe
Chirag Madlani <12962843+chirag-madlani@users.noreply.github.com>
2024-09-25 15:09:34 +0530

    chore(ui): bump quicktype to resolve vulnerabilities (#17979)
    * chore(ui): bump quicktype to resolve vulnerabilities
    
    * fix test
    
    * lower the version to avoid build issue
    
    * revert service_account type changes
    
    * added node-gyp as global package

--- openmetadata-ui/src/main/resources/ui/webpack.config.dev.js
+++ openmetadata-ui/src/main/resources/ui/webpack.config.dev.js
@@ -30,10 +30,6 @@ module.exports = {
 
   // Input configuration
   entry: ['@babel/polyfill', path.join(__dirname, 'src/index.tsx')],
-  
-  cache: {
-    type: 'filesystem', // Enable caching on filesystem
-  },  
 
   // Output configuration
   output: {
