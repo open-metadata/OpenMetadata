214d5f3fa9b86ab417ca13f130818d109b964996
sushi30 <imri.paran@gmail.com>
2024-09-25 12:38:36 +0200

    add link to logs and commit fixed the dq cli test

--- ingestion/src/metadata/utils/dict.py
+++ ingestion/src/metadata/utils/dict.py
@@ -1,3 +1,8 @@
-class CustomDict(dict):
+"""
+A custom dictionary class that extends functionality.
+"""
+
+
+class ExtendedDict(dict):
     def lower_case_keys(self):
         return {k.lower(): v for k, v in self.items()}
