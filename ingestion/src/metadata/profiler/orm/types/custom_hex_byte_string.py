1260d76624d7c5fba06acecd0b9e8e86eb9f132b
Teddy Crepineau <teddy.crepineau@gmail.com>
2024-09-25 18:09:05 +0200

    style: fix python linting failure

--- ingestion/src/metadata/profiler/orm/types/custom_hex_byte_string.py
+++ ingestion/src/metadata/profiler/orm/types/custom_hex_byte_string.py
@@ -63,8 +63,9 @@ class HexByteString(TypeDecorator):
         detected_encoding = chardet.detect(bytes_value).get("encoding")
         if detected_encoding:
             try:
-                # Decode the bytes value with the detected encoding and replace errors with "?" if bytes cannot be decoded
-                # e.g. b"\x66\x67\x67\x9c", if detected_encoding="utf-8" will result in 'foo�' (instead of failing)
+                # Decode the bytes value with the detected encoding and replace errors with "?"
+                # if bytes cannot be decoded e.g. b"\x66\x67\x67\x9c", if detected_encoding="utf-8"
+                # will result in 'foo�' (instead of failing)
                 str_value = bytes_value.decode(
                     encoding=detected_encoding, errors="replace"
                 )
