f49167e506352f63ab9c8ad3dbea63445295a1d7
Mayur Singal <39544459+ulixius9@users.noreply.github.com>
2024-09-27 16:57:09 +0530

    MINOR: MSTR connector import fix (#18025)

--- ingestion/setup.py
+++ ingestion/setup.py
@@ -299,6 +299,7 @@ plugins: Dict[str, Set[str]] = {
         "psycopg2-binary",
         VERSIONS["geoalchemy2"],
     },
+    "mstr": {"mstr-rest-requests==0.14.1"},
     "sagemaker": {VERSIONS["boto3"]},
     "salesforce": {"simple_salesforce~=1.11"},
     "sample-data": {VERSIONS["avro"], VERSIONS["grpc-tools"]},
