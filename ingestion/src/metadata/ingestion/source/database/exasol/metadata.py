16e457c82907758aedca527483e0e5d84d8040da
Nicola Coretti <nicola.coretti@exasol.com>
2024-10-02 11:01:34 +0200

    Update ExasolSource

--- ingestion/src/metadata/ingestion/source/database/exasol/metadata.py
+++ ingestion/src/metadata/ingestion/source/database/exasol/metadata.py
@@ -1,5 +1,31 @@
 from metadata.ingestion.source.database.common_db_source import CommonDbSourceService
 
+from typing import Dict, Iterable, List, Optional, Tuple, cast
+
+from metadata.generated.schema.entity.services.connections.database.exasolConnection import (
+    ExasolConnection,
+)
+from metadata.generated.schema.metadataIngestion.workflow import (
+    Source as WorkflowSource,
+)
+from metadata.ingestion.api.steps import InvalidSourceException
+from metadata.ingestion.ometa.ometa_api import OpenMetadata
+from metadata.ingestion.source.database.common_db_source import (
+    CommonDbSourceService,
+)
 
 class ExasolSource(CommonDbSourceService):
-    pass
+
+    @classmethod
+    def create(
+            cls, config_dict, metadata: OpenMetadata, pipeline_name: Optional[str] = None
+    ):
+        config: WorkflowSource = WorkflowSource.model_validate(config_dict)
+        if config.serviceConnection is None:
+            raise InvalidSourceException("Missing service connection")
+        connection = cast(ExasolConnection, config.serviceConnection.root.config)
+        if not isinstance(connection, ExasolConnection):
+            raise InvalidSourceException(
+                f"Expected ExasolConnection, but got {connection}"
+            )
+        return cls(config, metadata)
