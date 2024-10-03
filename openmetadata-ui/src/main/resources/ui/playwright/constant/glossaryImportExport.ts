de603aaa0b439e731eb9345dee24d0a9fbb79445
Chirag Madlani <12962843+chirag-madlani@users.noreply.github.com>
2024-10-03 10:27:00 +0530

    minor(ui): react-quill package update to maintained one (#18067)

--- openmetadata-ui/src/main/resources/ui/playwright/constant/glossaryImportExport.ts
+++ openmetadata-ui/src/main/resources/ui/playwright/constant/glossaryImportExport.ts
@@ -1,3 +1,15 @@
+/*
+ *  Copyright 2024 Collate.
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 export const CUSTOM_PROPERTIES_TYPES = {
   STRING: 'String',
   MARKDOWN: 'Markdown',
@@ -7,13 +19,13 @@ export const CUSTOM_PROPERTIES_TYPES = {
 
 export const FIELD_VALUES_CUSTOM_PROPERTIES = {
   STRING: 'This is "testing" string;',
-  MARKDOWN: `## Overview
+  MARKDOWN: `### Overview
 This project is designed to **simplify** and *automate* daily tasks. It aims to:
 - Increase productivity
 - Reduce manual effort
 - Provide real-time data insights
 
-## Features
+### Features
 1. **Task Management**: Organize tasks efficiently with custom tags.
 2. **Real-Time Analytics**: Get up-to-date insights on task progress.
 3. **Automation**: Automate repetitive workflows using custom scripts.`,
