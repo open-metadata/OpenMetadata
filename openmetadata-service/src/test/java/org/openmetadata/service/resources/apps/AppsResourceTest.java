e3da87cdf1d071e837151324bbef17e8b70492b8
Pere Miquel Brull <peremiquelbrull@gmail.com>
2024-10-03 11:00:29 +0200

    MINOR - Add App Limits (#18072)
    * App Limits - Prep extension
    
    * App Limits - Prep extension
    
    * App Limits - Allow to search by name
    
    * App Limits - Allow to search by name
    
    * fix postgres sql
    
    * comments

--- openmetadata-service/src/test/java/org/openmetadata/service/resources/apps/AppsResourceTest.java
+++ openmetadata-service/src/test/java/org/openmetadata/service/resources/apps/AppsResourceTest.java
@@ -36,6 +36,7 @@ import org.openmetadata.schema.analytics.type.WebAnalyticEventType;
 import org.openmetadata.schema.api.data.CreateTableProfile;
 import org.openmetadata.schema.api.services.CreateDatabaseService;
 import org.openmetadata.schema.entity.app.App;
+import org.openmetadata.schema.entity.app.AppExtension;
 import org.openmetadata.schema.entity.app.AppMarketPlaceDefinition;
 import org.openmetadata.schema.entity.app.AppRunRecord;
 import org.openmetadata.schema.entity.app.AppSchedule;
@@ -336,6 +337,7 @@ public class AppsResourceTest extends EntityResourceTest<App, CreateApp> {
     String appName = "SearchIndexingApplication";
     postTriggerApp(appName, ADMIN_AUTH_HEADERS);
     assertAppStatusAvailableAfterTrigger(appName);
+    assertListExtension(appName, AppExtension.ExtensionType.STATUS);
     assertAppRanAfterTriggerWithStatus(appName, AppRunRecord.Status.SUCCESS);
   }
 
@@ -345,6 +347,19 @@ public class AppsResourceTest extends EntityResourceTest<App, CreateApp> {
         () -> {
           try {
             assert Objects.nonNull(getLatestAppRun(appName, ADMIN_AUTH_HEADERS));
+          } catch (HttpResponseException ex) {
+            throw new AssertionError(ex);
+          }
+        },
+        APP_TRIGGER_RETRY);
+  }
+
+  private void assertListExtension(String appName, AppExtension.ExtensionType extensionType) {
+    assertEventually(
+        "appIsRunning",
+        () -> {
+          try {
+            assert Objects.nonNull(listAppExtension(appName, extensionType, ADMIN_AUTH_HEADERS));
           } catch (HttpResponseException ex) {
             throw new AssertionError(ex);
           }
@@ -419,5 +434,14 @@ public class AppsResourceTest extends EntityResourceTest<App, CreateApp> {
       throws HttpResponseException {
     WebTarget target = getResource(String.format("apps/name/%s/runs/latest", appName));
     return TestUtils.get(target, AppRunRecord.class, authHeaders);
+  }
+
+  private AppExtension listAppExtension(
+      String appName, AppExtension.ExtensionType extensionType, Map<String, String> authHeaders)
+      throws HttpResponseException {
+    WebTarget target =
+        getResource(
+            String.format("apps/name/%s/extension?extensionType=%s", appName, extensionType));
+    return TestUtils.get(target, AppExtension.class, authHeaders);
   }
 }
