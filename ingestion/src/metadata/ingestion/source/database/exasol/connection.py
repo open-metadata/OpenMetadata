b601536fe52ac913572edb819989518e8b775bbe
Nicola Coretti <nicola.coretti@exasol.com>
2024-10-07 10:22:54 +0200

    Establish basic connection to Exasol DB from connector

--- ingestion/src/metadata/ingestion/source/database/exasol/connection.py
+++ ingestion/src/metadata/ingestion/source/database/exasol/connection.py
@@ -1,5 +1,6 @@
 from typing import Optional
 
+from pydantic import SecretStr
 from sqlalchemy.engine import Engine
 
 from metadata.generated.schema.entity.automations.workflow import (
@@ -11,7 +12,7 @@ from metadata.generated.schema.entity.services.connections.database.exasolConnec
 from metadata.ingestion.connections.builders import (
     create_generic_db_connection,
     get_connection_args_common,
-    get_connection_url_common,
+    get_connection_options_dict,
 )
 from metadata.ingestion.connections.test_connections import (
     test_query,
@@ -19,9 +20,46 @@ from metadata.ingestion.connections.test_connections import (
 )
 from metadata.ingestion.ometa.ometa_api import OpenMetadata
 from metadata.utils.logger import ingestion_logger
+from urllib.parse import quote_plus
 
 logger = ingestion_logger()
 
+
+def get_connection_url(connection: ExasolConnection) -> str:
+    """
+    Common method for building the source connection urls
+    """
+
+    url = f"{connection.scheme.value}://"
+
+    if connection.username:
+        url += f"{quote_plus(connection.username)}"
+        connection.password = SecretStr("") if not connection.password else connection.password
+        url += (
+            f":{quote_plus(connection.password.get_secret_value())}"
+            if connection
+            else ""
+        )
+        url += "@"
+
+    url += connection.hostPort
+
+    if hasattr(connection, "databaseSchema"):
+        url += f"/{connection.databaseSchema}" if connection.databaseSchema else ""
+
+    # FIXME: Decide based on user provided settings/options
+    # options = get_connection_options_dict(connection)
+    options = {"SSLCertificate":"SSL_VERIFY_NONE"}
+    if options:
+        if (hasattr(connection, "database") and not connection.database) or (
+                hasattr(connection, "databaseSchema") and not connection.databaseSchema
+        ):
+            url += "/"
+        params = "&".join(
+            f"{key}={quote_plus(value)}" for (key, value) in options.items() if value
+        )
+        url = f"{url}?{params}"
+    return url
 
 def get_connection(connection: ExasolConnection) -> Engine:
     """
@@ -29,27 +67,19 @@ def get_connection(connection: ExasolConnection) -> Engine:
     """
     return create_generic_db_connection(
         connection=connection,
-        get_connection_url_fn=get_connection_url_common,
+        get_connection_url_fn=get_connection_url,
         get_connection_args_fn=get_connection_args_common,
     )
 
 
 def test_connection(
-    metadata: OpenMetadata,
-    engine: Engine,
-    service_connection: ExasolConnection,
-    automation_workflow: Optional[AutomationWorkflow] = None,
+        metadata: OpenMetadata,
+        engine: Engine,
+        service_connection: ExasolConnection,
+        automation_workflow: Optional[AutomationWorkflow] = None,
 ) -> None:
     """
     Test connection. This can be executed either as part
     of a metadata workflow or during an Automation Workflow
     """
-    test_query(engine, "SELECT 1;")
-    #queries = {}
-    #test_connection_db_schema_sources(
-    #    metadata=metadata,
-    #    engine=engine,
-    #    service_connection=service_connection,
-    #    automation_workflow=automation_workflow,
-    #    queries=queries,
-    #)
+    test_query(engine, "SELECT 1357;")
