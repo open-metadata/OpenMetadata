49012b90b7b032c9d52b825f2db36bafb0e4d56d
Ashish Gupta <ashish@getcollate.io>
2024-10-07 18:52:19 +0530

    supported TableDetailPage tabs as classBase for supporting collate only tabs

--- openmetadata-ui/src/main/resources/ui/src/pages/TableDetailsPageV1/TableDetailsPageV1.tsx
+++ openmetadata-ui/src/main/resources/ui/src/pages/TableDetailsPageV1/TableDetailsPageV1.tsx
@@ -12,39 +12,29 @@
  *  limitations under the License.
  */
 
-import { Col, Row, Space, Tabs, Typography } from 'antd';
+import { Col, Row, Space, Tabs } from 'antd';
 import { AxiosError } from 'axios';
 import classNames from 'classnames';
 import { compare } from 'fast-json-patch';
-import { get, isEmpty, isEqual, isUndefined } from 'lodash';
+import { isEmpty, isEqual, isUndefined } from 'lodash';
 import { EntityTags } from 'Models';
 import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useHistory, useParams } from 'react-router-dom';
 import { useActivityFeedProvider } from '../../components/ActivityFeed/ActivityFeedProvider/ActivityFeedProvider';
-import { ActivityFeedTab } from '../../components/ActivityFeed/ActivityFeedTab/ActivityFeedTab.component';
 import ActivityThreadPanel from '../../components/ActivityFeed/ActivityThreadPanel/ActivityThreadPanel';
 import { withActivityFeed } from '../../components/AppRouter/withActivityFeed';
 import { withSuggestions } from '../../components/AppRouter/withSuggestions';
-import { CustomPropertyTable } from '../../components/common/CustomPropertyTable/CustomPropertyTable';
 import DescriptionV1 from '../../components/common/EntityDescription/DescriptionV1';
 import ErrorPlaceHolder from '../../components/common/ErrorWithPlaceholder/ErrorPlaceHolder';
 import Loader from '../../components/common/Loader/Loader';
-import QueryViewer from '../../components/common/QueryViewer/QueryViewer.component';
 import ResizablePanels from '../../components/common/ResizablePanels/ResizablePanels';
-import TabsLabel from '../../components/common/TabsLabel/TabsLabel.component';
 import { DataAssetsHeader } from '../../components/DataAssets/DataAssetsHeader/DataAssetsHeader.component';
-import TableProfiler from '../../components/Database/Profiler/TableProfiler/TableProfiler';
-import SampleDataTableComponent from '../../components/Database/SampleDataTable/SampleDataTable.component';
 import SchemaTab from '../../components/Database/SchemaTab/SchemaTab.component';
-import TableQueries from '../../components/Database/TableQueries/TableQueries';
 import { QueryVote } from '../../components/Database/TableQueries/TableQueries.interface';
 import EntityRightPanel from '../../components/Entity/EntityRightPanel/EntityRightPanel';
-import IncidentManager from '../../components/IncidentManager/IncidentManager.component';
-import Lineage from '../../components/Lineage/Lineage.component';
 import { EntityName } from '../../components/Modals/EntityNameModal/EntityNameModal.interface';
 import PageLayoutV1 from '../../components/PageLayoutV1/PageLayoutV1';
-import { SourceType } from '../../components/SearchedData/SearchedData.interface';
 import { FQN_SEPARATOR_CHAR } from '../../constants/char.constants';
 import {
   getEntityDetailsPath,
@@ -54,7 +44,6 @@ import {
 import { FEED_COUNT_INITIAL_DATA } from '../../constants/entity.constants';
 import { mockDatasetData } from '../../constants/mockTourData.constants';
 import { COMMON_RESIZABLE_PANEL_CONFIG } from '../../constants/ResizablePanel.constants';
-import LineageProvider from '../../context/LineageProvider/LineageProvider';
 import { usePermissionProvider } from '../../context/PermissionProvider/PermissionProvider';
 import {
   OperationPermission,
@@ -114,6 +103,7 @@ import { showErrorToast, showSuccessToast } from '../../utils/ToastUtils';
 import { FrequentlyJoinedTables } from './FrequentlyJoinedTables/FrequentlyJoinedTables.component';
 import './table-details-page-v1.less';
 import TableConstraints from './TableConstraints/TableConstraints';
+import tableDetailPageClassBase from './TableDetailPageClassBase';
 
 const TableDetailsPageV1: React.FC = () => {
   const { isTourOpen, activeTabForTourDatasetPage, isTourPage } =
@@ -655,222 +645,51 @@ const TableDetailsPageV1: React.FC = () => {
   );
 
   const tabs = useMemo(() => {
-    const allTabs = [
-      {
-        label: <TabsLabel id={EntityTabs.SCHEMA} name={t('label.schema')} />,
-        key: EntityTabs.SCHEMA,
-        children: schemaTab,
-      },
-      {
-        label: (
-          <TabsLabel
-            count={feedCount.totalCount}
-            id={EntityTabs.ACTIVITY_FEED}
-            isActive={activeTab === EntityTabs.ACTIVITY_FEED}
-            name={t('label.activity-feed-and-task-plural')}
-          />
-        ),
-        key: EntityTabs.ACTIVITY_FEED,
-        children: (
-          <ActivityFeedTab
-            refetchFeed
-            columns={tableDetails?.columns}
-            entityFeedTotalCount={feedCount.totalCount}
-            entityType={EntityType.TABLE}
-            fqn={tableDetails?.fullyQualifiedName ?? ''}
-            owners={tableDetails?.owners}
-            onFeedUpdate={getEntityFeedCount}
-            onUpdateEntityDetails={fetchTableDetails}
-            onUpdateFeedCount={handleFeedCount}
-          />
-        ),
-      },
-      {
-        label: (
-          <TabsLabel
-            id={EntityTabs.SAMPLE_DATA}
-            name={t('label.sample-data')}
-          />
-        ),
-
-        key: EntityTabs.SAMPLE_DATA,
-        children:
-          !isTourOpen && !viewSampleDataPermission ? (
-            <ErrorPlaceHolder type={ERROR_PLACEHOLDER_TYPE.PERMISSION} />
-          ) : (
-            <SampleDataTableComponent
-              isTableDeleted={deleted}
-              owners={tableDetails?.owners ?? []}
-              permissions={tablePermissions}
-              tableId={tableDetails?.id ?? ''}
-            />
-          ),
-      },
-      {
-        label: (
-          <TabsLabel
-            count={queryCount}
-            id={EntityTabs.TABLE_QUERIES}
-            isActive={activeTab === EntityTabs.TABLE_QUERIES}
-            name={t('label.query-plural')}
-          />
-        ),
-        key: EntityTabs.TABLE_QUERIES,
-        children: !viewQueriesPermission ? (
-          <ErrorPlaceHolder type={ERROR_PLACEHOLDER_TYPE.PERMISSION} />
-        ) : (
-          <TableQueries
-            isTableDeleted={deleted}
-            tableId={tableDetails?.id ?? ''}
-          />
-        ),
-      },
-      {
-        label: (
-          <TabsLabel
-            id={EntityTabs.PROFILER}
-            name={t('label.profiler-amp-data-quality')}
-          />
-        ),
-        key: EntityTabs.PROFILER,
-        children:
-          !isTourOpen && !viewProfilerPermission ? (
-            <ErrorPlaceHolder type={ERROR_PLACEHOLDER_TYPE.PERMISSION} />
-          ) : (
-            <TableProfiler
-              permissions={tablePermissions}
-              table={tableDetails}
-              testCaseSummary={testCaseSummary}
-            />
-          ),
-      },
-      {
-        label: (
-          <TabsLabel
-            id={EntityTabs.INCIDENTS}
-            name={t('label.incident-plural')}
-          />
-        ),
-        key: EntityTabs.INCIDENTS,
-        children:
-          tablePermissions.ViewAll || tablePermissions.ViewTests ? (
-            <div className="p-x-lg p-b-lg p-t-md">
-              <IncidentManager
-                isIncidentPage={false}
-                tableDetails={tableDetails}
-              />
-            </div>
-          ) : (
-            <ErrorPlaceHolder type={ERROR_PLACEHOLDER_TYPE.PERMISSION} />
-          ),
-      },
-      {
-        label: <TabsLabel id={EntityTabs.LINEAGE} name={t('label.lineage')} />,
-        key: EntityTabs.LINEAGE,
-        children: (
-          <LineageProvider>
-            <Lineage
-              deleted={deleted}
-              entity={tableDetails as SourceType}
-              entityType={EntityType.TABLE}
-              hasEditAccess={editLineagePermission}
-            />
-          </LineageProvider>
-        ),
-      },
-
-      {
-        label: (
-          <TabsLabel id={EntityTabs.DBT} name={t('label.dbt-lowercase')} />
-        ),
-        isHidden: !(
-          tableDetails?.dataModel?.sql || tableDetails?.dataModel?.rawSql
-        ),
-        key: EntityTabs.DBT,
-        children: (
-          <QueryViewer
-            sqlQuery={
-              get(tableDetails, 'dataModel.sql', '') ||
-              get(tableDetails, 'dataModel.rawSql', '')
-            }
-            title={
-              <Space className="p-y-xss">
-                <Typography.Text className="text-grey-muted">
-                  {`${t('label.path')}:`}
-                </Typography.Text>
-                <Typography.Text>
-                  {tableDetails?.dataModel?.path}
-                </Typography.Text>
-              </Space>
-            }
-          />
-        ),
-      },
-      {
-        label: (
-          <TabsLabel
-            id={
-              isViewTableType
-                ? EntityTabs.VIEW_DEFINITION
-                : EntityTabs.SCHEMA_DEFINITION
-            }
-            name={
-              isViewTableType
-                ? t('label.view-definition')
-                : t('label.schema-definition')
-            }
-          />
-        ),
-        isHidden: isUndefined(tableDetails?.schemaDefinition),
-        key: isViewTableType
-          ? EntityTabs.VIEW_DEFINITION
-          : EntityTabs.SCHEMA_DEFINITION,
-        children: (
-          <QueryViewer sqlQuery={tableDetails?.schemaDefinition ?? ''} />
-        ),
-      },
-      {
-        label: (
-          <TabsLabel
-            id={EntityTabs.CUSTOM_PROPERTIES}
-            name={t('label.custom-property-plural')}
-          />
-        ),
-        key: EntityTabs.CUSTOM_PROPERTIES,
-        children: tableDetails && (
-          <div className="m-sm">
-            <CustomPropertyTable<EntityType.TABLE>
-              entityDetails={tableDetails}
-              entityType={EntityType.TABLE}
-              handleExtensionUpdate={onExtensionUpdate}
-              hasEditAccess={editCustomAttributePermission}
-              hasPermission={viewAllPermission}
-            />
-          </div>
-        ),
-      },
-    ];
-
-    return allTabs.filter((data) => !data.isHidden);
+    return tableDetailPageClassBase
+      .getPageTabs({
+        schemaTab,
+        queryCount,
+        isTourOpen,
+        tablePermissions,
+        activeTab,
+        deleted,
+        tableDetails,
+        totalFeedCount: feedCount.totalCount,
+        onExtensionUpdate,
+        getEntityFeedCount,
+        handleFeedCount,
+        viewAllPermission,
+        editCustomAttributePermission,
+        viewSampleDataPermission,
+        viewQueriesPermission,
+        viewProfilerPermission,
+        editLineagePermission,
+        fetchTableDetails,
+        testCaseSummary,
+        isViewTableType,
+      })
+      .filter((data) => !data.isHidden);
   }, [
     schemaTab,
+    queryCount,
+    isTourOpen,
     tablePermissions,
     activeTab,
-    schemaTab,
     deleted,
     tableDetails,
     feedCount.totalCount,
-    entityName,
     onExtensionUpdate,
     getEntityFeedCount,
     handleFeedCount,
-    tableDetails?.dataModel,
     viewAllPermission,
     editCustomAttributePermission,
     viewSampleDataPermission,
     viewQueriesPermission,
     viewProfilerPermission,
     editLineagePermission,
+    fetchTableDetails,
+    testCaseSummary,
+    isViewTableType,
   ]);
 
   const onTierUpdate = useCallback(
