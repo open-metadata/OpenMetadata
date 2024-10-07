bc65801f1d7c8d42995c831c1a8a0711f8c789f6
sushi30 <imri.paran@gmail.com>
2024-10-07 10:42:42 +0200

    add migrations for table diff

--- bootstrap/sql/migrations/native/1.5.7/postgres/postDataMigrationSQLScript.sql
+++ bootstrap/sql/migrations/native/1.5.7/postgres/postDataMigrationSQLScript.sql
@@ -1,2 +1,17 @@
--- recreate tableDiff with the columnCaseSensitive paramter
-DELETE from test_definition where name = 'tableDiff';
+-- Add caseSensitiveColumns to tableDiff
+UPDATE test_definition
+SET json = jsonb_set(
+ json,
+ '{parameterDefinition}',
+ (json->'parameterDefinition') || jsonb_build_array(
+  jsonb_build_object(
+   'name', 'caseSensitiveColumns',
+   'dataType', 'BOOLEAN',
+   'required', false,
+   'description', 'Use case sensitivity when comparing the columns. (default: true)',
+   'displayName', 'Case sensitive columns'
+   'optionValues', jsonb_build_array()
+  )
+ )
+)
+WHERE name = 'tableDiff';
