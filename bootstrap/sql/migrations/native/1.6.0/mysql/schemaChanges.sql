e469a6fe38dd1e121c6dea4ba7986e8fa271f9af
Mohit Yadav <105265192+mohityadav766@users.noreply.github.com>
2024-10-10 19:25:11 +0530

    [OnBoardingApp] Add apps data store (#18209)
    * fix(ui): pagination issue with search in services list page
    
    * Add Apps Data store
    
    * Revert Services Changes
    
    ---------
    
    Co-authored-by: Chirag Madlani <12962843+chirag-madlani@users.noreply.github.com>

--- bootstrap/sql/migrations/native/1.6.0/mysql/schemaChanges.sql
+++ bootstrap/sql/migrations/native/1.6.0/mysql/schemaChanges.sql
@@ -6,3 +6,10 @@ CREATE INDEX apps_extension_time_series_extension ON apps_extension_time_series(
 
 -- Clean dangling workflows not removed after test connection
 truncate automations_workflow;
+
+-- App Data Store
+CREATE TABLE IF NOT EXISTS apps_data_store (
+    identifier VARCHAR(256) NOT NULL,      
+    type VARCHAR(256) NOT NULL,   
+    json JSON NOT NULL
+);
