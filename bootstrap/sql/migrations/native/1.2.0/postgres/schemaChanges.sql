ded0a57f4252cb2d3771acde4b22445be4b22868
MugeshRajaR <123713667+MugeshRajaR@users.noreply.github.com>
2024-10-10 18:50:04 +0530

    Update schemaChanges.sql
    Wrongly named the hashing method as SHA256 instead of SHA-256

--- bootstrap/sql/migrations/native/1.2.0/postgres/schemaChanges.sql
+++ bootstrap/sql/migrations/native/1.2.0/postgres/schemaChanges.sql
@@ -202,7 +202,7 @@ WHERE json #>> '{pipelineType}' = 'metadata';
 
 
 -- update entityReportData from pascale to camel case
---Modified the MD5 with SHA256 as Postgres FIPS blocking it
+--Modified the MD5 with SHA-256 as Postgres FIPS blocking it
 UPDATE report_data_time_series
 SET json = jsonb_set(
   json::jsonb #- '{reportDataType}',
@@ -210,7 +210,7 @@ SET json = jsonb_set(
   '"entityReportData"',
   true
 ),
-entityFQNHash = SHA256('entityReportData')
+entityFQNHash = SHA-256('entityReportData')
 WHERE json #>> '{reportDataType}' = 'EntityReportData';
 
 -- update webAnalyticEntityViewReportData from pascale to camel case
@@ -221,7 +221,7 @@ SET json = jsonb_set(
   '"webAnalyticEntityViewReportData"',
   true
 ),
-entityFQNHash = SHA256('webAnalyticEntityViewReportData')
+entityFQNHash = SHA-256('webAnalyticEntityViewReportData')
 WHERE json #>> '{reportDataType}' = 'WebAnalyticEntityViewReportData';
 
 -- update webAnalyticUserActivityReportData from pascale to camel case
@@ -232,7 +232,7 @@ SET json = jsonb_set(
   '"webAnalyticUserActivityReportData"',
   true
 ),
-entityFQNHash = SHA256('webAnalyticUserActivityReportData') 
+entityFQNHash = SHA-256('webAnalyticUserActivityReportData') 
 WHERE json #>> '{reportDataType}' = 'WebAnalyticUserActivityReportData';
 
 CREATE TABLE IF NOT EXISTS installed_apps (
