002e2a1878cd30217d088c47716277391758e257
Imri Paran <imri.paran@gmail.com>
2024-10-10 08:32:07 +0200

    GEN-970: Refactor redshift system metrics to support freshness test (#17981)
    * ref(profiler): redshift system metrics
    
    - moved redshift system metrics to the redshift source module
    - use Timestamp in data quality
    - added plugin feature to test utils
    
    * use timezone.utc
    
    * format
    
    * reverted unintended snowflake changes
    
    * fixed import test_system_metrics.py
    
    * revert
    
    * fixed import in tests

--- ingestion/src/metadata/profiler/metrics/system/system.py
+++ ingestion/src/metadata/profiler/metrics/system/system.py
@@ -26,7 +26,7 @@ from metadata.generated.schema.entity.data.table import SystemProfile
 from metadata.generated.schema.entity.services.connections.database.bigQueryConnection import (
     BigQueryConnection,
 )
-from metadata.ingestion.source.database.snowflake.profiler.system_metrics import (
+from metadata.ingestion.source.database.snowflake.profiler.system import (
     build_snowflake_query_results,
 )
 from metadata.profiler.metrics.core import SystemMetric
@@ -38,11 +38,6 @@ from metadata.profiler.metrics.system.queries.bigquery import (
     DML_STAT_TO_DML_STATEMENT_MAPPING,
     JOBS,
     BigQueryQueryResult,
-)
-from metadata.profiler.metrics.system.queries.redshift import (
-    STL_QUERY,
-    get_metric_result,
-    get_query_results,
 )
 from metadata.profiler.orm.registry import Dialects
 from metadata.utils.dispatch import valuedispatch
@@ -87,7 +82,6 @@ def get_system_metrics_for_dialect(
             } else returns None
     """
     logger.debug(f"System metrics not support for {dialect}. Skipping processing.")
-    return None
 
 
 @get_system_metrics_for_dialect.register(Dialects.BigQuery)
@@ -186,105 +180,6 @@ def _(
                     "rowsAffected": rows_affected,
                 }
             )
-
-    return TypeAdapter(List[SystemProfile]).validate_python(metric_results)
-
-
-@get_system_metrics_for_dialect.register(Dialects.Redshift)
-def _(
-    dialect: str,
-    session: Session,
-    table: DeclarativeMeta,
-    *args,
-    **kwargs,
-) -> List[SystemProfile]:
-    """List all the DML operations for reshifts tables
-
-    Args:
-        dialect (str): redshift
-        session (Session): session object
-        table (DeclarativeMeta): orm table
-
-    Returns:
-        List[Dict]:
-    """
-    logger.debug(f"Fetching system metrics for {dialect}")
-    database = session.get_bind().url.database
-    schema = table.__table_args__["schema"]  # type: ignore
-
-    metric_results: List[Dict] = []
-
-    # get inserts ddl queries
-    inserts = get_value_from_cache(
-        SYSTEM_QUERY_RESULT_CACHE, f"{Dialects.Redshift}.{database}.{schema}.inserts"
-    )
-    if not inserts:
-        insert_query = STL_QUERY.format(
-            alias="si",
-            join_type="LEFT",
-            condition="sd.query is null",
-            database=database,
-            schema=schema,
-        )
-        inserts = get_query_results(
-            session,
-            insert_query,
-            DatabaseDMLOperations.INSERT.value,
-        )
-        set_cache(
-            SYSTEM_QUERY_RESULT_CACHE,
-            f"{Dialects.Redshift}.{database}.{schema}.inserts",
-            inserts,
-        )
-    metric_results.extend(get_metric_result(inserts, table.__tablename__))  # type: ignore
-
-    # get deletes ddl queries
-    deletes = get_value_from_cache(
-        SYSTEM_QUERY_RESULT_CACHE, f"{Dialects.Redshift}.{database}.{schema}.deletes"
-    )
-    if not deletes:
-        delete_query = STL_QUERY.format(
-            alias="sd",
-            join_type="RIGHT",
-            condition="si.query is null",
-            database=database,
-            schema=schema,
-        )
-        deletes = get_query_results(
-            session,
-            delete_query,
-            DatabaseDMLOperations.DELETE.value,
-        )
-        set_cache(
-            SYSTEM_QUERY_RESULT_CACHE,
-            f"{Dialects.Redshift}.{database}.{schema}.deletes",
-            deletes,
-        )
-    metric_results.extend(get_metric_result(deletes, table.__tablename__))  # type: ignore
-
-    # get updates ddl queries
-    updates = get_value_from_cache(
-        SYSTEM_QUERY_RESULT_CACHE, f"{Dialects.Redshift}.{database}.{schema}.updates"
-    )
-    if not updates:
-        update_query = STL_QUERY.format(
-            alias="si",
-            join_type="INNER",
-            condition="sd.query is not null",
-            database=database,
-            schema=schema,
-        )
-        updates = get_query_results(
-            session,
-            update_query,
-            DatabaseDMLOperations.UPDATE.value,
-        )
-        set_cache(
-            SYSTEM_QUERY_RESULT_CACHE,
-            f"{Dialects.Redshift}.{database}.{schema}.updates",
-            updates,
-        )
-    metric_results.extend(get_metric_result(updates, table.__tablename__))  # type: ignore
 
     return TypeAdapter(List[SystemProfile]).validate_python(metric_results)
 
