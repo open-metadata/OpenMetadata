de2953274c16cc7f6e59b2089c5911c90c7539c4
sushi30 <imri.paran@gmail.com>
2024-10-14 16:50:58 +0200

    fixed test_get_bool_test_case_param

--- ingestion/tests/unit/data_quality/validations/test_utils.py
+++ ingestion/tests/unit/data_quality/validations/test_utils.py
@@ -5,25 +5,18 @@ from metadata.generated.schema.tests.testCase import TestCaseParameterValue
 
 
 @pytest.mark.parametrize(
-    "test_case_param_vals, name, default, expected",
+    "test_case_param_vals, name, expected",
     [
-        ([TestCaseParameterValue(name="param1", value="true")], "param1", None, True),
-        ([TestCaseParameterValue(name="param1", value="false")], "param1", None, False),
-        ([TestCaseParameterValue(name="param1", value="True")], "param1", None, True),
-        ([TestCaseParameterValue(name="param1", value="False")], "param1", None, False),
-        ([TestCaseParameterValue(name="param1", value="TRUE")], "param1", None, True),
-        ([TestCaseParameterValue(name="param1", value="FALSE")], "param1", None, False),
-        (
-            [TestCaseParameterValue(name="param1", value="invalid")],
-            "param1",
-            None,
-            False,
-        ),
-        ([], "param1", True, True),
-        ([], "param1", False, False),
-        ([TestCaseParameterValue(name="param2", value="true")], "param1", None, None),
-        ([TestCaseParameterValue(name="param2", value="true")], "param1", True, True),
+        ([TestCaseParameterValue(name="param1", value="true")], "param1", True),
+        ([TestCaseParameterValue(name="param1", value="false")], "param1", False),
+        ([TestCaseParameterValue(name="param1", value="True")], "param1", True),
+        ([TestCaseParameterValue(name="param1", value="False")], "param1", False),
+        ([TestCaseParameterValue(name="param1", value="TRUE")], "param1", True),
+        ([TestCaseParameterValue(name="param1", value="FALSE")], "param1", False),
+        ([TestCaseParameterValue(name="param1", value="invalid")], "param1", False),
+        ([], "param1", False),
+        ([TestCaseParameterValue(name="param2", value="true")], "param1", False),
     ],
 )
-def test_get_bool_test_case_param(test_case_param_vals, name, default, expected):
-    assert get_bool_test_case_param(test_case_param_vals, name, default) == expected
+def test_get_bool_test_case_param(test_case_param_vals, name, expected):
+    assert get_bool_test_case_param(test_case_param_vals, name) == expected
