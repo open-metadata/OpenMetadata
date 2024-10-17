30a28fbb301bbc83a37d99f5f95b51058ff01428
gyobycoders <gyowanny.queiroz@bycoders.com.br>
2024-10-17 19:31:37 -0300

    WIP - Fix import error. Fix add webpack for prod packing (not working).

--- openmetadata-webserver-cli/run.py
+++ openmetadata-webserver-cli/run.py
@@ -1,4 +1,7 @@
-from webserver import app
+import os
+from flask import Flask
+
+app = Flask(__name__, static_folder=os.path.join(os.getcwd(), 'ui/dist/assets'), static_url_path='')
 
 if __name__ == "__main__":
     app.run(port=8001, debug=True)
