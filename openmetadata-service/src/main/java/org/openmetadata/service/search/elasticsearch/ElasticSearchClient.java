9d006885d5a17585979c2c0c76cb611ec03d012a
Sriharsha Chintalapani <harshach@users.noreply.github.com>
2024-10-23 02:23:53 -0700

    Rbac setting v2 (#18338)
    * Add Search RBAC config option in settings
    
    * add null check
    
    * review comments
    
    * resolve
    
    * WIP search rbac page
    
    * enable / disable search rbac switch added
    
    * testing search RBAC
    
    * Remove Domain and Ignore Build RBAC Query for Bots
    
    * Fix Domain Conditions
    
    * Use common code from Search Client
    
    * update page to preferences -> search
    
    * add playwright tests
    
    * add playwright for search RBAC
    
    * update RBACConditionEvaluator
    
    * No results check added
    
    ---------
    
    Co-authored-by: mohitdeuex <mohit.y@deuexsolutions.com>
    Co-authored-by: Chirag Madlani <12962843+chirag-madlani@users.noreply.github.com>
    Co-authored-by: Chira Madlani <chirag@getcollate.io>
    Co-authored-by: Mohit Yadav <105265192+mohityadav766@users.noreply.github.com>

--- openmetadata-service/src/main/java/org/openmetadata/service/search/elasticsearch/ElasticSearchClient.java
+++ openmetadata-service/src/main/java/org/openmetadata/service/search/elasticsearch/ElasticSearchClient.java
@@ -77,7 +77,6 @@ import es.org.elasticsearch.index.query.QueryStringQueryBuilder;
 import es.org.elasticsearch.index.query.RangeQueryBuilder;
 import es.org.elasticsearch.index.query.ScriptQueryBuilder;
 import es.org.elasticsearch.index.query.TermQueryBuilder;
-import es.org.elasticsearch.index.query.TermsQueryBuilder;
 import es.org.elasticsearch.index.query.functionscore.FunctionScoreQueryBuilder;
 import es.org.elasticsearch.index.query.functionscore.ScoreFunctionBuilders;
 import es.org.elasticsearch.index.reindex.DeleteByQueryRequest;
@@ -362,22 +361,6 @@ public class ElasticSearchClient implements SearchClient {
     SearchSourceBuilder searchSourceBuilder =
         getSearchSourceBuilder(
             request.getIndex(), request.getQuery(), request.getFrom(), request.getSize());
-
-    // Add Domain filter
-    if (request.isApplyDomainFilter()) {
-      if (!nullOrEmpty(request.getDomains())) {
-        TermsQueryBuilder domainFilter =
-            QueryBuilders.termsQuery("domain.fullyQualifiedName", request.getDomains());
-        searchSourceBuilder.query(
-            QueryBuilders.boolQuery().must(searchSourceBuilder.query()).filter(domainFilter));
-      } else {
-        // Else condition to list entries where domain field is null
-        searchSourceBuilder.query(
-            QueryBuilders.boolQuery()
-                .must(searchSourceBuilder.query())
-                .mustNot(QueryBuilders.existsQuery("domain.fullyQualifiedName")));
-      }
-    }
 
     buildSearchRBACQuery(subjectContext, searchSourceBuilder);
 
@@ -2422,7 +2405,7 @@ public class ElasticSearchClient implements SearchClient {
 
   private void buildSearchRBACQuery(
       SubjectContext subjectContext, SearchSourceBuilder searchSourceBuilder) {
-    if (subjectContext != null && !subjectContext.isAdmin()) {
+    if (SearchClient.shouldApplyRbacConditions(subjectContext, rbacConditionEvaluator)) {
       OMQueryBuilder rbacQuery = rbacConditionEvaluator.evaluateConditions(subjectContext);
       if (rbacQuery != null) {
         searchSourceBuilder.query(
