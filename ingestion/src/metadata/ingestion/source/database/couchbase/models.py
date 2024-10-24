0f71ec0581ee436bd89cd76cf9409f55b082e449
Mayur Singal <39544459+ulixius9@users.noreply.github.com>
2024-10-24 20:34:55 +0530

    MINOR: Couchbase Secondary Index Fix (#18398)

--- ingestion/src/metadata/ingestion/source/database/couchbase/models.py
+++ ingestion/src/metadata/ingestion/source/database/couchbase/models.py
@@ -1,4 +1,4 @@
-#  Copyright 2021 Collate
+#  Copyright 2024 Collate
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
 #  You may obtain a copy of the License at
@@ -9,16 +9,23 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 """
-SQL Queries used during ingestion
+Couchbase source models.
 """
 
-import textwrap
+from typing import List, Optional
 
-COUCHBASE_GET_INDEX_KEYS = textwrap.dedent(
-    """ select * from system:indexes where {condition}"""
-)
+from pydantic import BaseModel
 
 
-COUCHBASE_GET_DATA = textwrap.dedent(
-    """ select * from `{database_name}`.`{schema_name}`.`{table_name}` {condition} limit {sample_size} """
-)
+class IndexKey(BaseModel):
+    """A Bigtable index key."""
+
+    index_key: List[str] = []
+    condition: Optional[str] = None
+    is_primary: Optional[bool] = False
+
+
+class IndexObject(BaseModel):
+    """A Bigtable cell value."""
+
+    indexes: Optional[IndexKey] = None
