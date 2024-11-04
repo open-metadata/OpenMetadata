5251577a089cdd4db10945a79314046bd76e1f7e
Pablo Takara <pjt1991@gmail.com>
2024-11-04 10:15:58 +0100

    Remove duplicated migrations

--- bootstrap/sql/migrations/native/1.6.0/mysql/schemaChanges.sql
+++ bootstrap/sql/migrations/native/1.6.0/mysql/schemaChanges.sql
@@ -1657,19 +1657,13 @@ update ACT_GE_PROPERTY set VALUE_ = '7.0.1.1' where NAME_ = 'schema.version';
 update ACT_ID_PROPERTY set VALUE_ = '7.0.1.1' where NAME_ = 'schema.version';
 -- ------------------------------------
 
--- Extend app extension for limits
-ALTER TABLE apps_extension_time_series ADD COLUMN extension VARCHAR(255);
-UPDATE apps_extension_time_series SET extension = 'status' WHERE extension IS NULL;
-ALTER TABLE apps_extension_time_series MODIFY COLUMN extension VARCHAR(255) NOT NULL;
-CREATE INDEX apps_extension_time_series_extension ON apps_extension_time_series(extension);
-
 -- Clean dangling workflows not removed after test connection
 truncate automations_workflow;
 
 -- App Data Store
 CREATE TABLE IF NOT EXISTS apps_data_store (
-    identifier VARCHAR(256) NOT NULL,      
-    type VARCHAR(256) NOT NULL,   
+    identifier VARCHAR(256) NOT NULL,
+    type VARCHAR(256) NOT NULL,
     json JSON NOT NULL
 );
 
@@ -1678,3 +1672,4 @@ ALTER TABLE consumers_dlq ADD COLUMN source VARCHAR(255);
 
 -- Create an index on the source column in the consumers_dlq table
 CREATE INDEX idx_consumers_dlq_source ON consumers_dlq (source);
+
