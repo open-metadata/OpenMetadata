8e1b8defb610849b195815b39e6880d1153ad482
sonikashah <sonikashah94@gmail.com>
2024-11-05 16:48:12 +0530

    fix failing UserResourceTest

--- openmetadata-service/src/test/java/org/openmetadata/service/resources/EntityResourceTest.java
+++ openmetadata-service/src/test/java/org/openmetadata/service/resources/EntityResourceTest.java
@@ -3972,11 +3972,6 @@ public abstract class EntityResourceTest<T extends EntityInterface, K extends Cr
     return TestUtils.putCsv(target, csv, CsvImportResult.class, Status.OK, ADMIN_AUTH_HEADERS);
   }
 
-  protected String exportCsv(String entityName) throws HttpResponseException {
-    WebTarget target = getResourceByName(entityName + "/export");
-    return TestUtils.get(target, String.class, ADMIN_AUTH_HEADERS);
-  }
-
   private String receiveCsvViaSocketIO(String entityName) throws Exception {
     UUID userId = getAdminUserId();
     String uri = String.format("http://localhost:%d", APP.getLocalPort());
@@ -4056,7 +4051,7 @@ public abstract class EntityResourceTest<T extends EntityInterface, K extends Cr
     return adminUser.getId();
   }
 
-  private String initiateExport(String entityName) throws IOException {
+  protected String initiateExport(String entityName) throws IOException {
     WebTarget target = getResourceByName(entityName + "/export");
     CSVExportResponse response =
         TestUtils.getWithResponse(
