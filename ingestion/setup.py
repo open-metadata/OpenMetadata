e44c1b6459bb850fa105dcd04037afcb59e31605
ulixius9 <mayursingal9@gmail.com>
2024-11-06 14:39:19 +0530

    pyformat

--- ingestion/setup.py
+++ ingestion/setup.py
@@ -146,7 +146,7 @@ base_requirements = {
 plugins: Dict[str, Set[str]] = {
     "airflow": {
         VERSIONS["airflow"],
-        "opentelemetry-api==1.27.0", # internal dependency of airflow, need to restrict version
+        "opentelemetry-api==1.27.0",  # internal dependency of airflow, need to restrict version
         "opentelemetry-exporter-otlp-proto-grpc==1.27.0",
         "protobuf<5",
         "attrs",
