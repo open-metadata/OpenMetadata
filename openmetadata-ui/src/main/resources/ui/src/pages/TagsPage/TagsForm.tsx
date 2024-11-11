608b73031d6f02bb855c7ae1fd1db2d65e70e73f
datapopcorn <yanchengdatalife@gmail.com>
2024-11-12 01:58:22 +0800

    add create owner in tag form back and add disabled field

--- openmetadata-ui/src/main/resources/ui/src/pages/TagsPage/TagsForm.tsx
+++ openmetadata-ui/src/main/resources/ui/src/pages/TagsPage/TagsForm.tsx
@@ -11,9 +11,11 @@
  *  limitations under the License.
  */
 
-import { Form, FormProps, Modal, Typography } from 'antd';
+import { PlusOutlined } from '@ant-design/icons';
+import { Button, Form, FormProps, Modal, Space, Typography } from 'antd';
 import React, { useEffect, useMemo, useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { OwnerLabel } from '../../components/common/OwnerLabel/OwnerLabel.component';
 import { VALIDATION_MESSAGES } from '../../constants/constants';
 import {
   HEX_COLOR_CODE_REGEX,
@@ -25,9 +27,10 @@ import { useApplicationStore } from '../../hooks/useApplicationStore';
 import {
   FieldProp,
   FieldTypes,
+  FormItemLayout,
   HelperTextType,
 } from '../../interface/FormUtils.interface';
-import { generateFormFields } from '../../utils/formUtils';
+import { generateFormFields, getField } from '../../utils/formUtils';
 import { RenameFormProps } from './TagsPage.interface';
 
 const TagsForm = ({
@@ -227,6 +230,31 @@ const TagsForm = ({
       : []),
   ];
 
+  const ownerField: FieldProp = {
+    name: 'owners',
+    id: 'root/owner',
+    required: false,
+    label: t('label.owner-plural'),
+    type: FieldTypes.USER_TEAM_SELECT,
+    props: {
+      hasPermission: true,
+      children: (
+        <Button
+          data-testid="add-owner"
+          icon={<PlusOutlined style={{ color: 'white', fontSize: '12px' }} />}
+          size="small"
+          type="primary"
+        />
+      ),
+      multiple: { user: true, team: false },
+    },
+    formItemLayout: FormItemLayout.HORIZONTAL,
+    formItemProps: {
+      valuePropName: 'owners',
+      trigger: 'onUpdate',
+    },
+  };
+
   /*
   const handleSave = async (data: SubmitProps) => {
     try {
@@ -246,6 +274,7 @@ const TagsForm = ({
       name,
       displayName = '',
       description = '',
+      disabled = false,
       mutuallyExclusive = false,
     } = formObj;
 
@@ -263,6 +292,7 @@ const TagsForm = ({
       name: name.trim(),
       displayName: displayName?.trim(),
       description: description,
+      disabled,
       mutuallyExclusive,
       owners: selectedOwners,
     };
@@ -309,6 +339,16 @@ const TagsForm = ({
         validateMessages={VALIDATION_MESSAGES}
         onFinish={handleSave}>
         {generateFormFields(formFields)}
+
+        <div className="m-t-xss">
+          {isClassification && getField(ownerField)}
+
+          {Boolean(ownersList.length) && (
+            <Space wrap data-testid="owner-container" size={[8, 8]}>
+              <OwnerLabel owners={ownersList} />
+            </Space>
+          )}
+        </div>
       </Form>
     </Modal>
   );
