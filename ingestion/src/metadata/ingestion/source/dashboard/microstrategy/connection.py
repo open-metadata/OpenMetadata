ef0f59566b13f7b41acee67bd765b2b2cd7c4dcd
harshsoni2024 <harshsoni2024@gmail.com>
2024-11-12 17:35:48 +0530

    test connection fix

--- ingestion/src/metadata/ingestion/source/dashboard/microstrategy/connection.py
+++ ingestion/src/metadata/ingestion/source/dashboard/microstrategy/connection.py
@@ -20,6 +20,9 @@ from metadata.generated.schema.entity.automations.workflow import (
 from metadata.generated.schema.entity.services.connections.dashboard.microStrategyConnection import (
     MicroStrategyConnection,
 )
+from metadata.generated.schema.entity.services.connections.testConnectionResult import (
+    TestConnectionResult,
+)
 from metadata.ingestion.connections.test_connections import test_connection_steps
 from metadata.ingestion.ometa.ometa_api import OpenMetadata
 from metadata.ingestion.source.dashboard.microstrategy.client import MicroStrategyClient
@@ -37,7 +40,7 @@ def test_connection(
     client: MicroStrategyClient,
     service_connection: MicroStrategyConnection,
     automation_workflow: Optional[AutomationWorkflow] = None,
-) -> None:
+) -> TestConnectionResult:
     """
     Test connection. This can be executed either as part
     of a metadata workflow or during an Automation Workflow
