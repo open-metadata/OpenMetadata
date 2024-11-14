7884ab9dd8eeb7d2ed34a24138e033e929a44e12
Kenil Shah <32725411+Kenil27@users.noreply.github.com>
2024-11-14 15:38:33 +0530

    Fix: support string value for xAxis apart from date (#18590)
    * Fix: show string value for xAxis apart from date
    
    * update key name

--- openmetadata-ui/src/main/resources/ui/src/utils/DataInsightUtils.tsx
+++ openmetadata-ui/src/main/resources/ui/src/utils/DataInsightUtils.tsx
@@ -152,7 +152,11 @@ export const CustomTooltip = (props: DataInsightChartTooltipProps) => {
   } = props;
 
   if (active && payload && payload.length) {
-    const timestamp = dateTimeFormatter(payload[0].payload[timeStampKey] || 0);
+    // we need to check if the xAxis is a date or not.
+    const timestamp =
+      timeStampKey === 'term'
+        ? payload[0].payload[timeStampKey]
+        : dateTimeFormatter(payload[0].payload[timeStampKey] || 0);
     const payloadValue = uniqBy(payload, 'dataKey');
 
     return (
