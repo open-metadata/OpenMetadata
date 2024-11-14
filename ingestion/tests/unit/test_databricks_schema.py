ede160f5a6bed3bd298552200f76f1a46fd2aa18
Akash Verma <akashverma@Akashs-MacBook-Pro-2.local>
2024-11-14 16:35:38 +0530

    changed method name validate_schema to format_schema_name

--- ingestion/tests/unit/test_databricks_schema.py
+++ ingestion/tests/unit/test_databricks_schema.py
@@ -1,6 +1,6 @@
 import pytest
 
-from metadata.ingestion.source.database.databricks.metadata import validate_schema
+from metadata.ingestion.source.database.databricks.metadata import format_schema_name
 
 
 @pytest.mark.parametrize(
@@ -17,5 +17,5 @@ def test_schema_name_sanitization(input_schema, expected_schema):
     """
     Test sanitization of schema names by adding backticks only around hyphenated names.
     """
-    sanitized_schema = validate_schema(input_schema)
+    sanitized_schema = format_schema_name(input_schema)
     assert sanitized_schema == expected_schema
