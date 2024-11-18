4e8a799d9da8906df216c1442deaa688491414f7
IceS2 <pjt1991@gmail.com>
2024-11-18 08:17:22 +0100

    MINOR: Improve PeriodicBatchEntityTrigger Workflows performance (#18664)
    * Update default configurations
    
    * Refactor how we call the main Workflow in order to increase performance

--- openmetadata-service/src/main/java/org/openmetadata/service/governance/workflows/elements/triggers/impl/FilterEntityImpl.java
+++ openmetadata-service/src/main/java/org/openmetadata/service/governance/workflows/elements/triggers/impl/FilterEntityImpl.java
@@ -1,10 +1,9 @@
 package org.openmetadata.service.governance.workflows.elements.triggers.impl;
 
 import static org.openmetadata.service.governance.workflows.Workflow.RELATED_ENTITY_VARIABLE;
+import static org.openmetadata.service.governance.workflows.elements.triggers.EventBasedEntityTrigger.PASSES_FILTER_VARIABLE;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 import org.flowable.common.engine.api.delegate.Expression;
 import org.flowable.engine.delegate.DelegateExecution;
@@ -14,31 +13,21 @@ import org.openmetadata.schema.type.ChangeDescription;
 import org.openmetadata.schema.type.FieldChange;
 import org.openmetadata.schema.type.Include;
 import org.openmetadata.service.Entity;
-import org.openmetadata.service.governance.workflows.WorkflowHandler;
-import org.openmetadata.service.governance.workflows.WorkflowInstanceListener;
 import org.openmetadata.service.resources.feeds.MessageParser;
 import org.openmetadata.service.util.JsonUtils;
 
-public class TriggerEntityWorkflowImpl implements JavaDelegate {
-  private Expression workflowNameExpr;
+public class FilterEntityImpl implements JavaDelegate {
   private Expression excludedFilterExpr;
 
   @Override
   public void execute(DelegateExecution execution) {
-    String workflowName = (String) workflowNameExpr.getValue(execution);
     List<String> excludedFilter =
         JsonUtils.readOrConvertValue(excludedFilterExpr.getValue(execution), List.class);
 
     String entityLinkStr = (String) execution.getVariable(RELATED_ENTITY_VARIABLE);
 
-    if (passesExcludedFilter(entityLinkStr, excludedFilter)) {
-      WorkflowHandler workflowHandler = WorkflowHandler.getInstance();
-      triggerWorkflow(
-          workflowHandler, execution.getProcessInstanceBusinessKey(), entityLinkStr, workflowName);
-    } else {
-      execution.setEventName("end");
-      new WorkflowInstanceListener().execute(execution);
-    }
+    execution.setVariable(
+        PASSES_FILTER_VARIABLE, passesExcludedFilter(entityLinkStr, excludedFilter));
   }
 
   private boolean passesExcludedFilter(String entityLinkStr, List<String> excludedFilter) {
@@ -59,15 +48,5 @@ public class TriggerEntityWorkflowImpl implements JavaDelegate {
     return changedFields.isEmpty()
         || changedFields.stream()
             .anyMatch(changedField -> !excludedFilter.contains(changedField.getName()));
-  }
-
-  private void triggerWorkflow(
-      WorkflowHandler workflowHandler,
-      String businessKey,
-      String entityLinkStr,
-      String workflowName) {
-    Map<String, Object> variables = new HashMap<>();
-    variables.put(RELATED_ENTITY_VARIABLE, entityLinkStr);
-    workflowHandler.triggerByKey(workflowName, businessKey, variables);
   }
 }
