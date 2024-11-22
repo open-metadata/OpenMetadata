a2bde30af68d7381b52c5de9288aaf1a08cc5c61
Keshav Mohta <keshavmohta09@gmail.com>
2024-11-22 10:54:54 +0530

    feat: mysql view lineage

--- ingestion/src/metadata/ingestion/source/database/mysql/lineage.py
+++ ingestion/src/metadata/ingestion/source/database/mysql/lineage.py
@@ -9,38 +9,79 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 """
-Mysql lineage module
+MySQL lineage module
 """
-from typing import Optional
+from typing import Iterable
 
-from metadata.generated.schema.entity.services.connections.database.mysqlConnection import (
-    MysqlConnection,
-)
-from metadata.generated.schema.metadataIngestion.workflow import (
-    Source as WorkflowSource,
-)
-from metadata.ingestion.api.steps import InvalidSourceException
-from metadata.ingestion.ometa.ometa_api import OpenMetadata
+from metadata.generated.schema.api.lineage.addLineage import AddLineageRequest
+from metadata.ingestion.api.models import Either
 from metadata.ingestion.source.database.lineage_source import LineageSource
+from metadata.ingestion.source.database.mysql.query_parser import MySQLQueryParserSource
+from metadata.ingestion.source.database.postgres.queries import POSTGRES_SQL_STATEMENT
 from metadata.utils.logger import ingestion_logger
 
 logger = ingestion_logger()
 
 
-class MysqlLineageSource(LineageSource):
+class MySQLLineageSource(MySQLQueryParserSource, LineageSource):
     """
-    Mysql lineage source implements view lineage
+    Implements the necessary methods to extract
+    Database lineage from MySQL Source
     """
 
-    @classmethod
-    def create(
-        cls, config_dict, metadata: OpenMetadata, pipeline_name: Optional[str] = None
-    ):
-        """Create class instance"""
-        config: WorkflowSource = WorkflowSource.model_validate(config_dict)
-        connection: MysqlConnection = config.serviceConnection.root.config
-        if not isinstance(connection, MysqlConnection):
-            raise InvalidSourceException(
-                f"Expected MysqlConnection, but got {connection}"
-            )
-        return cls(config, metadata)
+    sql_stmt = POSTGRES_SQL_STATEMENT
+
+    filters = """
+                AND (
+                    s.query ILIKE '%%create table%%as%%select%%'
+                    OR s.query ILIKE '%%insert%%into%%select%%'
+                    OR s.query ILIKE '%%update%%'
+                    OR s.query ILIKE '%%merge%%'
+                )
+            """
+
+    def _iter(self, *_, **__) -> Iterable[Either[AddLineageRequest]]:
+        """
+        Based on the query logs, prepare the lineage
+        and send it to the sink
+        """
+
+        yield from super()._iter()
+
+        # if self.service_connection.scheme == MySQLScheme.pgspider_psycopg2:
+        #     lineages = get_lineage_from_multi_tenant_table(
+        #         self.metadata,
+        #         connection=self.service_connection,
+        #         service_name=self.config.serviceName,
+        #     )
+
+        #     for lineage_request in lineages or []:
+        #         yield lineage_request
+
+    # def process_table_query(self) -> Iterable[TableQuery]:
+    #     """
+    #     Process Query
+    #     """
+    #     try:
+    #         with get_connection(self.service_connection).connect() as conn:
+    #             rows = conn.execute(self.get_sql_statement())
+    #             for row in rows:
+    #                 row = dict(row)
+    #                 try:
+    #                     yield TableQuery(
+    #                         dialect=self.dialect.value,
+    #                         query=row["query_text"],
+    #                         userName=row["usename"],
+    #                         analysisDate=DateTime(datetime.now()),
+    #                         aborted=self.get_aborted_status(row),
+    #                         databaseName=self.get_database_name(row),
+    #                         serviceName=self.config.serviceName,
+    #                         databaseSchema=self.get_schema_name(row),
+    #                         duration=row.get("duration"),
+    #                     )
+    #                 except Exception as err:
+    #                     logger.debug(traceback.format_exc())
+    #                     logger.error(str(err))
+    #     except Exception as err:
+    #         logger.error(f"Source usage processing error - {err}")
+    #         logger.debug(traceback.format_exc())
--- ingestion/src/metadata/ingestion/source/database/mysql/service_spec.py
+++ ingestion/src/metadata/ingestion/source/database/mysql/service_spec.py
@@ -1,4 +1,7 @@
+from metadata.ingestion.source.database.mysql.lineage import MySQLLineageSource
 from metadata.ingestion.source.database.mysql.metadata import MysqlSource
 from metadata.utils.service_spec.default import DefaultDatabaseSpec
 
-ServiceSpec = DefaultDatabaseSpec(metadata_source_class=MysqlSource)
+ServiceSpec = DefaultDatabaseSpec(
+    metadata_source_class=MysqlSource, lineage_source_class=MySQLLineageSource
+)
