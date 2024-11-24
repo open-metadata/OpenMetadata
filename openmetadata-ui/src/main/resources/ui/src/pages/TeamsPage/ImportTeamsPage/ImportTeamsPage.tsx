bcae516a5af88d729066742908aac0c23d116e8a
sonika-shah <58761340+sonika-shah@users.noreply.github.com>
2024-11-24 13:30:31 +0530

    Add async apis for csv import (#18647)
    * Add async apis for import csv
    
    * checkstyle fix
    
    * fix checkstyle after merge from main
    
    * fix auts
    
    * fix backend tests
    
    * ui: add async import support for glossary
    
    * ui: improve the import page layout
    
    * ui: add async import support for teams and users
    
    * show banner at the top
    
    * fix unit test
    
    * update importteams page test
    
    * minor change
    
    ---------
    
    Co-authored-by: Sachin Chaurasiya <sachinchaurasiyachotey87@gmail.com>

--- openmetadata-ui/src/main/resources/ui/src/pages/TeamsPage/ImportTeamsPage/ImportTeamsPage.tsx
+++ openmetadata-ui/src/main/resources/ui/src/pages/TeamsPage/ImportTeamsPage/ImportTeamsPage.tsx
@@ -90,6 +90,10 @@ const ImportTeamsPage = () => {
     return <TeamImportResult csvImportResult={csvImportResult} />;
   }, [csvImportResult, type]);
 
+  const handleCsvImportResultUpdate = (result: CSVImportResult) => {
+    setCsvImportResult(result);
+  };
+
   const fetchPermissions = async (entityFqn: string) => {
     setIsPageLoading(true);
     try {
@@ -130,7 +134,6 @@ const ImportTeamsPage = () => {
     const api = type === ImportType.USERS ? importUserInTeam : importTeam;
     try {
       const response = await api(name, data, dryRun);
-      setCsvImportResult(response);
 
       return response;
     } catch (error) {
@@ -207,6 +210,7 @@ const ImportTeamsPage = () => {
           <EntityImport
             entityName={team.name}
             onCancel={handleViewClick}
+            onCsvResultUpdate={handleCsvImportResultUpdate}
             onImport={handleImportCsv}
             onSuccess={handleViewClick}>
             {importResult}
