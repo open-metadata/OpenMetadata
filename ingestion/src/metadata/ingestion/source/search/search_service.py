375b7c31f560676d363800ff4206770e60d5e817
Keshav Mohta <68001229+keshavmohta09@users.noreply.github.com>
2024-11-26 14:42:43 +0530

    Fixes #15543: Implemented ElasticSearch Index Template Ingestion (#18686)

--- ingestion/src/metadata/ingestion/source/search/search_service.py
+++ ingestion/src/metadata/ingestion/source/search/search_service.py
@@ -83,7 +83,7 @@ class SearchServiceTopology(ServiceTopology):
                 cache_entities=True,
             ),
         ],
-        children=["search_index"],
+        children=["search_index", "search_index_template"],
         post_process=["mark_search_indexes_as_deleted"],
     )
     search_index: Annotated[
@@ -104,6 +104,21 @@ class SearchServiceTopology(ServiceTopology):
                 consumer=["search_service"],
                 nullable=True,
             ),
+        ],
+    )
+
+    search_index_template: Annotated[
+        TopologyNode, Field(description="Search Index Template Processing Node")
+    ] = TopologyNode(
+        producer="get_search_index_template",
+        stages=[
+            NodeStage(
+                type_=SearchIndex,
+                context="search_index_template",
+                processor="yield_search_index_template",
+                consumer=["search_service"],
+                use_cache=True,
+            )
         ],
     )
 
@@ -177,6 +192,34 @@ class SearchServiceSource(TopologyRunnerMixin, Source, ABC):
                 )
                 continue
             yield index_details
+
+    def yield_search_index_template(
+        self, search_index_template_details: Any
+    ) -> Iterable[Either[CreateSearchIndexRequest]]:
+        """Method to Get Search Index Templates"""
+
+    def get_search_index_template_list(self) -> Optional[List[Any]]:
+        """Get list of all search index templates"""
+
+    def get_search_index_template_name(self, search_index_template_details: Any) -> str:
+        """Get Search Index Template Name"""
+
+    def get_search_index_template(self) -> Any:
+        if self.source_config.includeIndexTemplate:
+            for index_template_details in self.get_search_index_template_list():
+                if search_index_template_name := self.get_search_index_template_name(
+                    index_template_details
+                ):
+                    if filter_by_search_index(
+                        self.source_config.searchIndexFilterPattern,
+                        search_index_template_name,
+                    ):
+                        self.status.filter(
+                            search_index_template_name,
+                            "Search Index Template Filtered Out",
+                        )
+                        continue
+                    yield index_template_details
 
     def yield_create_request_search_service(
         self, config: WorkflowSource
