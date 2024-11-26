bea020c9acd1fd7907a43754fe7677f2b6c87dad
Shrushti Polekar <shrushtipolekar@gmail.com>
2024-11-27 01:26:57 +0530

    Feat: Support  for Emoji Option in Feed Editor (#18772)

--- openmetadata-ui/src/main/resources/ui/webpack.config.dev.js
+++ openmetadata-ui/src/main/resources/ui/webpack.config.dev.js
@@ -116,6 +116,7 @@ module.exports = {
     },
     alias: {
       process: 'process/browser',
+      Quill: path.resolve(__dirname, 'node_modules/quill'), // Alias for the 'quill' library in node_modules
     },
   },
 
