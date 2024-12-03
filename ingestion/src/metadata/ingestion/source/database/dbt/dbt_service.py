122a2937ec38ee2a192775de0fcebbf776c2ea71
Suman Maharana <sumanmaharana786@gmail.com>
2024-12-03 21:42:11 +0530

    Minor: Fix dbt source file issue (#18903)

--- ingestion/src/metadata/ingestion/source/database/dbt/dbt_service.py
+++ ingestion/src/metadata/ingestion/source/database/dbt/dbt_service.py
@@ -214,6 +214,9 @@ class DbtServiceSource(TopologyRunnerMixin, Source, ABC):
             if self.context.get().dbt_file.dbt_catalog
             else None,
             dbt_manifest=parse_manifest(self.context.get().dbt_file.dbt_manifest),
+            dbt_sources=parse_sources(self.context.get().dbt_file.dbt_sources)
+            if self.context.get().dbt_file.dbt_sources
+            else None,
             dbt_run_results=[
                 parse_run_results(run_result_file)
                 for run_result_file in self.context.get().dbt_file.dbt_run_results
