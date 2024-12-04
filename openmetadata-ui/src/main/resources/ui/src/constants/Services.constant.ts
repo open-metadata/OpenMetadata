d598008c7cff58a1864455b0bbcfe650923009f9
Sachin Chaurasiya <sachinchaurasiyachotey87@gmail.com>
2024-12-04 10:38:59 +0530

    Minor: improve the service type display name (#18895)
    * Minor: improve the service type display name
    
    * Minor: add display names for new service types in Services.constant.ts
    
    * Minor: add display names for new service types in Services.constant.ts

--- openmetadata-ui/src/main/resources/ui/src/constants/Services.constant.ts
+++ openmetadata-ui/src/main/resources/ui/src/constants/Services.constant.ts
@@ -497,3 +497,22 @@ export const SERVICE_INGESTION_PIPELINE_TYPES = [
   PipelineType.AutoClassification,
   PipelineType.Dbt,
 ];
+
+export const SERVICE_TYPE_WITH_DISPLAY_NAME = new Map<string, string>([
+  [PipelineServiceType.GluePipeline, 'Glue Pipeline'],
+  [DatabaseServiceType.DomoDatabase, 'Domo Database'],
+  [DashboardServiceType.DomoDashboard, 'Domo Dashboard'],
+  [DashboardServiceType.MicroStrategy, 'Micro Strategy'],
+  [DashboardServiceType.PowerBIReportServer, 'PowerBI Report Server'],
+  [PipelineServiceType.DatabricksPipeline, 'Databricks Pipeline'],
+  [PipelineServiceType.DomoPipeline, 'Domo Pipeline'],
+  [PipelineServiceType.KafkaConnect, 'Kafka Connect'],
+  [DatabaseServiceType.SapERP, 'SAP ERP'],
+  [DatabaseServiceType.SapHana, 'SAP HANA'],
+  [DatabaseServiceType.UnityCatalog, 'Unity Catalog'],
+  [PipelineServiceType.DataFactory, 'Data Factory'],
+  [PipelineServiceType.DBTCloud, 'DBT Cloud'],
+  [PipelineServiceType.OpenLineage, 'Open Lineage'],
+  [MetadataServiceType.AlationSink, 'Alation Sink'],
+  [SearchServiceType.ElasticSearch, 'Elasticsearch'],
+]);
