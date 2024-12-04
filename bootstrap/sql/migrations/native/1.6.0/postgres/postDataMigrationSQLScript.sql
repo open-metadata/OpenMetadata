abdd6a5e911d43b829cf05948a63055a5d2f4444
Siddhant <86899184+Siddhanttimeline@users.noreply.github.com>
2024-12-05 04:44:31 +0530

    FIX: Combined index on offset and eventTime in changeEvent table (#18922)
    * fix: combined index on offset and eventTime in changeEvent table
    
    * fix: correct syntax error in Postgres query.
    
    * fix: correct syntax error in Postgres query.
    
    * fix: correct syntax error in Postgres query.

--- bootstrap/sql/migrations/native/1.6.0/postgres/postDataMigrationSQLScript.sql
+++ bootstrap/sql/migrations/native/1.6.0/postgres/postDataMigrationSQLScript.sql
@@ -73,3 +73,7 @@ WHERE serviceType IN ('Snowflake', 'Redshift', 'BigQuery');
 UPDATE consumers_dlq SET source = 'publisher';
 
 DELETE from event_subscription_entity where name = 'ActivityFeedAlert';
+
+DROP INDEX IF EXISTS event_time_index;
+
+CREATE INDEX idx_offset_event_time ON change_event ("offset", eventTime);
