34757be2eeb4c9643b16325d2a48988552dee370
Siddhanttimeline <siddhanttimeline9@gmail.com>
2024-12-04 21:12:51 +0530

    fix: correct syntax error in Postgres query.

--- bootstrap/sql/migrations/native/1.6.0/postgres/postDataMigrationSQLScript.sql
+++ bootstrap/sql/migrations/native/1.6.0/postgres/postDataMigrationSQLScript.sql
@@ -74,6 +74,6 @@ UPDATE consumers_dlq SET source = 'publisher';
 
 DELETE from event_subscription_entity where name = 'ActivityFeedAlert';
 
-DROP INDEX event_time_index;
+DROP INDEX IF EXISTS event_time_index;
 
 CREATE INDEX idx_offset_event_time ON change_event (offset, eventTime);
