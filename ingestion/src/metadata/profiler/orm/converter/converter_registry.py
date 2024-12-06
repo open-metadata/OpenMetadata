610322ffedfd39fe43077576306a372cf7931670
Teddy <teddy.crepineau@gmail.com>
2024-12-06 08:03:42 +0100

    MINOR - MSSQL timestamp type profiler fix (#18935)
    * fix: mssql timestamp processing
    
    * fix: min/max test type on datetime column
    
    * style: fix python format

--- ingestion/src/metadata/profiler/orm/converter/converter_registry.py
+++ ingestion/src/metadata/profiler/orm/converter/converter_registry.py
@@ -19,6 +19,7 @@ from metadata.generated.schema.entity.services.databaseService import (
 )
 from metadata.profiler.orm.converter.bigquery.converter import BigqueryMapTypes
 from metadata.profiler.orm.converter.common import CommonMapTypes
+from metadata.profiler.orm.converter.mssql.converter import MssqlMapTypes
 from metadata.profiler.orm.converter.redshift.converter import RedshiftMapTypes
 from metadata.profiler.orm.converter.snowflake.converter import SnowflakeMapTypes
 
@@ -26,3 +27,4 @@ converter_registry = defaultdict(lambda: CommonMapTypes)
 converter_registry[DatabaseServiceType.BigQuery] = BigqueryMapTypes
 converter_registry[DatabaseServiceType.Snowflake] = SnowflakeMapTypes
 converter_registry[DatabaseServiceType.Redshift] = RedshiftMapTypes
+converter_registry[DatabaseServiceType.Mssql] = MssqlMapTypes
