3b196cdb2c0967a42296e7ec886d4ff229e70741
Keshav Mohta <keshavmohta09@gmail.com>
2025-01-03 16:50:15 +0530

    fix: cassandra helpers file file level comment

--- ingestion/src/metadata/ingestion/source/database/cassandra/helpers.py
+++ ingestion/src/metadata/ingestion/source/database/cassandra/helpers.py
@@ -9,7 +9,7 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 """
-Iceberg source helpers.
+Cassandra source helpers.
 """
 from __future__ import annotations
 
