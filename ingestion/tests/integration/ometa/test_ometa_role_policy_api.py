0e5ae15d187b54af1d6852ad4c9f51868b03d929
Ayush Shah <ayush@getcollate.io>
2025-01-15 23:09:39 +0530

    fix: pytest failures due to User id, modified to fqn

--- ingestion/tests/integration/ometa/test_ometa_role_policy_api.py
+++ ingestion/tests/integration/ometa/test_ometa_role_policy_api.py
@@ -645,8 +645,8 @@ class OMetaRolePolicyTest(TestCase):
             data=CreateTeamRequest(
                 name="test-role-team-1",
                 teamType="Group",
-                users=[user.id],
-                defaultRoles=[role.id],
+                users=[user.fullyQualifiedName.root],
+                defaultRoles=[role.fullyQualifiedName.root],
             )
         )
 
