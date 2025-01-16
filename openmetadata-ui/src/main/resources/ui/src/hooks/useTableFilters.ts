36c732680129305923f029a62b5ce111b395dc47
shrushti2000 <shrushtipolekar@gmail.com>
2025-01-16 19:39:37 +0530

    update useTableFilter to handle multiple query params

--- openmetadata-ui/src/main/resources/ui/src/hooks/useTableFilters.ts
+++ openmetadata-ui/src/main/resources/ui/src/hooks/useTableFilters.ts
@@ -10,47 +10,80 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
+import qs, { ParsedQs } from 'qs';
 import { useMemo } from 'react';
 import { useHistory } from 'react-router-dom';
+import { useLocationSearch } from './LocationSearch/useLocationSearch';
 import useCustomLocation from './useCustomLocation/useCustomLocation';
 
-type FilterState = Record<string, string | boolean>;
+type FilterState = Record<string, string | boolean | string[]>;
+type UpdatedFilters = {
+  [key: string]: string | string[] | boolean;
+};
 
 export const useTableFilters = <T extends FilterState>(initialFilters: T) => {
   const location = useCustomLocation();
   const history = useHistory();
+  const searchQuery = useLocationSearch<ParsedQs>();
 
   const parseFiltersFromUrl = (): T => {
-    const searchParams = new URLSearchParams(location.search);
     const parsedFilters = { ...initialFilters };
 
     for (const key of Object.keys(initialFilters)) {
-      const paramValue = searchParams.get(key);
+      const paramValue = searchQuery[key];
       const initialValue = initialFilters[key as keyof T];
 
-      if (paramValue !== null) {
-        parsedFilters[key as keyof T] =
-          typeof initialValue === 'boolean'
-            ? ((paramValue === 'true') as T[keyof T])
-            : (paramValue as T[keyof T]);
+      if (paramValue !== undefined && paramValue !== null) {
+        // Handle boolean types
+        if (typeof initialValue === 'boolean') {
+          parsedFilters[key as keyof T] = (paramValue === 'true') as T[keyof T];
+        }
+        // Handle array types
+        else if (Array.isArray(initialValue)) {
+          // Check if paramValue is a string before calling split
+          parsedFilters[key as keyof T] =
+            typeof paramValue === 'string'
+              ? (paramValue.split(',').map((val) => val.trim()) as T[keyof T])
+              : (paramValue as T[keyof T]);
+        }
+        // Handle other types
+        else {
+          parsedFilters[key as keyof T] = paramValue as T[keyof T];
+        }
       }
     }
 
     return parsedFilters;
   };
 
-  const updateUrlWithFilters = (updatedFilters: Partial<T>) => {
-    const searchParams = new URLSearchParams(location.search);
-
-    Object.entries(updatedFilters).forEach(([key, value]) => {
-      if (value == null) {
-        searchParams.delete(key);
-      } else {
-        searchParams.set(key, value.toString());
-      }
+  const updateUrlWithFilters = (updatedFilters: {
+    [key: string]: string | boolean | string[] | null;
+  }) => {
+    const currentQueryParams = qs.parse(window.location.search, {
+      ignoreQueryPrefix: true,
     });
 
-    history.replace({ search: searchParams.toString() });
+    // Merge the existing query params with the updated filters
+    const mergedQueryParams = {
+      ...currentQueryParams,
+      ...updatedFilters,
+    };
+
+    Object.keys(mergedQueryParams).forEach((key) => {
+      const value = mergedQueryParams[key];
+
+      // Check if the value is null, undefined, or an empty array
+      if (value == null || (Array.isArray(value) && value.length === 0)) {
+        delete mergedQueryParams[key];
+      } else if (Array.isArray(value)) {
+        mergedQueryParams[key] = value.join(',');
+      }
+    });
+    history.replace({
+      search: qs.stringify(mergedQueryParams, {
+        addQueryPrefix: true,
+      }),
+    });
   };
 
   const filters = useMemo(
@@ -59,12 +92,12 @@ export const useTableFilters = <T extends FilterState>(initialFilters: T) => {
   );
 
   // Update a single filter
-  const setFilter = <K extends keyof T>(key: K, value: T[K]) => {
-    updateUrlWithFilters({ [key]: value } as unknown as Partial<T>);
+  const setFilter = (key: string, value: string | boolean) => {
+    updateUrlWithFilters({ [key]: value });
   };
 
   // Update multiple filters at a time
-  const setFilters = (newFilters: Partial<T>) => {
+  const setFilters = (newFilters: UpdatedFilters) => {
     updateUrlWithFilters(newFilters);
   };
 
