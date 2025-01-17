a65cf28521f505a76824640e50a104c0045c2cf1
mgorsk1 <gorskimariusz13@gmail.com>
2025-01-17 06:34:34 +0100

    :tada: Init (#19417)

--- ingestion/setup.py
+++ ingestion/setup.py
@@ -25,7 +25,7 @@ VERSIONS = {
     "boto3": "boto3>=1.20,<2.0",  # No need to add botocore separately. It's a dep from boto3
     "geoalchemy2": "GeoAlchemy2~=0.12",
     "google-cloud-monitoring": "google-cloud-monitoring>=2.0.0",
-    "google-cloud-storage": "google-cloud-storage==1.43.0",
+    "google-cloud-storage": "google-cloud-storage>=1.43.0",
     "gcsfs": "gcsfs>=2023.1.0",
     "great-expectations": "great-expectations>=0.18.0,<0.18.14",
     "grpc-tools": "grpcio-tools>=1.47.2",
