82da83cd19e8de2ae00e1083216e88886dbe5ffe
Chirag Madlani <12962843+chirag-madlani@users.noreply.github.com>
2025-01-23 14:32:26 +0530

    add tests for refresh logic

--- openmetadata-ui/src/main/resources/ui/playwright/e2e/Pages/Login.spec.ts
+++ openmetadata-ui/src/main/resources/ui/playwright/e2e/Pages/Login.spec.ts
@@ -139,7 +139,7 @@ test.describe('Login flow should work properly', () => {
     await page.locator('[data-testid="go-back-button"]').click();
   });
 
-  test.fixme('Refresh should work', async ({ browser }) => {
+  test('Refresh should work', async ({ browser }) => {
     const browserContext = await browser.newContext();
     const { apiContext, afterAction } = await performAdminLogin(browser);
     const page1 = await browserContext.newPage(),
