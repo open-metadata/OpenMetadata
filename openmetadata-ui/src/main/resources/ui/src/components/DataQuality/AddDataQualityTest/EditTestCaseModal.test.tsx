503e587a13a5d83b1b7933ffd23fd46e0825da66
Pranita <pfulsundar8@gmail.com>
2025-01-24 16:07:08 +0530

    fix: mock implementations for unit test

--- openmetadata-ui/src/main/resources/ui/src/components/DataQuality/AddDataQualityTest/EditTestCaseModal.test.tsx
+++ openmetadata-ui/src/main/resources/ui/src/components/DataQuality/AddDataQualityTest/EditTestCaseModal.test.tsx
@@ -53,12 +53,38 @@ jest.mock('../../../rest/testAPI', () => {
     updateTestCaseById: jest.fn().mockImplementation(() => Promise.resolve()),
   };
 });
+
 jest.mock('../../../utils/EntityUtils', () => ({
-  getColumnNameFromEntityLink: jest
+  getColumnNameFromEntityLink: jest.fn().mockReturnValue('last_name'),
+  getEntityName: jest
     .fn()
-    .mockReturnValue('getColumnNameFromEntityLink'),
-  getEntityName: jest.fn().mockReturnValue('getEntityName'),
+    .mockReturnValue('Column Values To Match Regex Pattern'),
 }));
+
+jest.mock('../../../utils/CommonUtils', () => ({
+  getNameFromFQN: jest.fn().mockReturnValue('dim_address'),
+}));
+
+jest.mock('../../../utils/FeedUtils', () => ({
+  getEntityFQN: jest.fn(),
+}));
+
+jest.mock('../../../utils/ToastUtils', () => ({
+  showErrorToast: jest.fn(),
+  showSuccessToast: jest.fn(),
+}));
+
+jest.mock('../../../utils/StringsUtils', () => ({
+  isValidJSONString: jest.fn(),
+}));
+
+jest.mock('../../../utils/formUtils', () => ({
+  generateFormFields: jest.fn(),
+}));
+
+jest.mock('../../common/Loader/Loader', () =>
+  jest.fn().mockImplementation(() => <div>Loader</div>)
+);
 
 describe('EditTestCaseModal Component', () => {
   it('component should render', async () => {
