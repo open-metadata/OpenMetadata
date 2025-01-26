e6a6e602af4c4db9f6b116175977040cc57f5535
mohitdeuex <mohit.y@deuexsolutions.com>
2025-01-26 22:35:24 +0530

    Update queries and conflict on apps data store

--- bootstrap/sql/migrations/native/1.6.3/mysql/schemaChanges.sql
+++ bootstrap/sql/migrations/native/1.6.3/mysql/schemaChanges.sql
@@ -1,4 +1,4 @@
 -- Add constraint to apps_data_store
-ALTER TABLE apps_data_store ADD CONSTRAINT entity_relationship_pkey PRIMARY KEY (identifier, type);
+ALTER TABLE apps_data_store ADD CONSTRAINT entity_relationship_pky PRIMARY KEY (identifier, type);
 UPDATE user_entity SET json = JSON_SET(json, '$.isBot', false) WHERE JSON_EXTRACT(json, '$.isBot') IS NULL;
 ALTER TABLE user_entity  ADD COLUMN isBot BOOLEAN GENERATED ALWAYS AS (json -> '$.isBot') NOT NULL;
