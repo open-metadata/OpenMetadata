efb519ec73cebd895c9445b626dab12ccf9b2cba
sonika-shah <58761340+sonika-shah@users.noreply.github.com>
2025-01-27 15:43:42 +0530

    support domain-hierarchy

--- openmetadata-ui/src/main/resources/ui/src/utils/GlossaryUtils.tsx
+++ openmetadata-ui/src/main/resources/ui/src/utils/GlossaryUtils.tsx
@@ -22,6 +22,7 @@ import { FQN_SEPARATOR_CHAR } from '../constants/char.constants';
 import { EntityType } from '../enums/entity.enum';
 import { Glossary } from '../generated/entity/data/glossary';
 import { GlossaryTerm, Status } from '../generated/entity/data/glossaryTerm';
+import { Domain } from '../generated/entity/domains/domain';
 import { calculatePercentageFromValue } from './CommonUtils';
 import { getEntityName } from './EntityUtils';
 import Fqn from './Fqn';
@@ -177,23 +178,28 @@ export const updateGlossaryTermByFqn = (
 // This function finds and gives you the glossary term you're looking for.
 // You can then use this term or update its information in the Glossary or Term with it's reference created
 // Reference will only be created if withReference is true
-export const findGlossaryTermByFqn = (
-  list: ModifiedGlossaryTerm[],
+export const findItemByFqn = (
+  list: ModifiedGlossaryTerm[] | Domain[],
   fullyQualifiedName: string,
   withReference = true
 ): GlossaryTerm | Glossary | ModifiedGlossary | null => {
   for (const item of list) {
-    if ((item.fullyQualifiedName ?? item.value) === fullyQualifiedName) {
+    if (
+      (item.fullyQualifiedName ?? (item as ModifiedGlossaryTerm).value) ===
+      fullyQualifiedName
+    ) {
       return withReference
         ? item
         : {
             ...item,
-            fullyQualifiedName: item.fullyQualifiedName ?? item.data?.tagFQN,
-            ...(item.data ?? {}),
+            fullyQualifiedName:
+              item.fullyQualifiedName ??
+              (item as ModifiedGlossaryTerm).data?.tagFQN,
+            ...((item as ModifiedGlossaryTerm).data ?? {}),
           };
     }
     if (item.children) {
-      const found = findGlossaryTermByFqn(
+      const found = findItemByFqn(
         item.children as ModifiedGlossaryTerm[],
         fullyQualifiedName
       );
