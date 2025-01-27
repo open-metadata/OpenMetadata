ff1956521cf67b766f9e2b9f1cf8c07449ec5ded
Imri Paran <imri.paran@gmail.com>
2025-01-27 14:29:15 +0100

    fix(governance-workflows): fix migrations for 1.7.0 (#19533)
    eventBasedEntityWorkflow -> eventBasedEntity
    periodicBatchEntityWorkflow -> periodicBatchEntity

--- bootstrap/sql/migrations/native/1.7.0/mysql/postDataMigrationSQLScript.sql
+++ bootstrap/sql/migrations/native/1.7.0/mysql/postDataMigrationSQLScript.sql
@@ -1,7 +1,7 @@
 UPDATE workflow_definition_entity
 SET json = JSON_SET(json, '$.trigger.type', 'eventBasedEntity')
-WHERE JSON_EXTRACT(json, '$.trigger.type') = 'eventBasedEntityWorkflow';
+WHERE JSON_EXTRACT(json, '$.trigger.type') = 'eventBasedEntityTrigger';
 
 UPDATE workflow_definition_entity
 SET json = JSON_SET(json, '$.trigger.type', 'periodicBatchEntity')
-WHERE JSON_EXTRACT(json, '$.trigger.type') = 'periodicBatchEntityWorkflow';
+WHERE JSON_EXTRACT(json, '$.trigger.type') = 'periodicBatchEntityTrigger';
