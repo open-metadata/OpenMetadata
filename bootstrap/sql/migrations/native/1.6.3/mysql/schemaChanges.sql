47c39e51c95847f5c6e2f278722bccc377149aa8
Mohit Yadav <105265192+mohityadav766@users.noreply.github.com>
2025-01-28 19:22:33 +0530

    Cleanup App data (#19571)

--- bootstrap/sql/migrations/native/1.6.3/mysql/schemaChanges.sql
+++ bootstrap/sql/migrations/native/1.6.3/mysql/schemaChanges.sql
@@ -3,3 +3,4 @@ ALTER TABLE apps_data_store ADD CONSTRAINT entity_relationship_pky PRIMARY KEY (
 UPDATE user_entity SET json = JSON_SET(json, '$.isBot', false) WHERE JSON_EXTRACT(json, '$.isBot') IS NULL;
 ALTER TABLE user_entity  ADD COLUMN isBot BOOLEAN GENERATED ALWAYS AS (json -> '$.isBot') NOT NULL;
 CREATE INDEX idx_isBot ON user_entity (isBot);
+DELETE from apps_data_store;
--- bootstrap/sql/migrations/native/1.6.3/postgres/schemaChanges.sql
+++ bootstrap/sql/migrations/native/1.6.3/postgres/schemaChanges.sql
@@ -3,3 +3,4 @@ ALTER TABLE apps_data_store ADD CONSTRAINT entity_relationship_pky PRIMARY KEY (
 UPDATE user_entity SET json = jsonb_set(json::jsonb, '{isBot}', 'false'::jsonb, true) WHERE NOT (json ?? 'isBot');
 ALTER TABLE user_entity ADD COLUMN isBot BOOLEAN GENERATED ALWAYS AS ((json ->> 'deleted')::boolean) STORED NOT NULL;
 CREATE INDEX idx_isBot ON user_entity (isBot);
+DELETE from apps_data_store;
