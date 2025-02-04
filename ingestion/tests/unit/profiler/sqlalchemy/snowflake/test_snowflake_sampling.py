93b8fc32ef26d722744f9a7adc96d7b2b3926895
Teddy <teddy.crepineau@gmail.com>
2025-02-04 10:40:40 +0100

    MINOR: Wrong attribute name in SampleConfig model (#19641)
    * fix: wrong attribute name in SampleConfig model
    
    * fix: test attribute
    
    * fix: failing tests
    
    * fix: trino filter error + adjust test to take into account null value
    
    * fix: mssql and azuresql tablesample on views
    
    (cherry picked from commit ef131d7e20404531e82fb3c4b34c7771ac035a40)

--- ingestion/tests/unit/profiler/sqlalchemy/snowflake/test_snowflake_sampling.py
+++ ingestion/tests/unit/profiler/sqlalchemy/snowflake/test_snowflake_sampling.py
@@ -80,7 +80,7 @@ class SampleTest(TestCase):
             ometa_client=None,
             entity=self.table_entity,
             sample_config=SampleConfig(
-                profile_sample_type=ProfileSampleType.PERCENTAGE, profile_sample=50.0
+                profileSampleType=ProfileSampleType.PERCENTAGE, profileSample=50.0
             ),
         )
         query: CTE = sampler.get_sample_query()
@@ -107,9 +107,9 @@ class SampleTest(TestCase):
                 ometa_client=None,
                 entity=self.table_entity,
                 sample_config=SampleConfig(
-                    profile_sample_type=ProfileSampleType.PERCENTAGE,
-                    profile_sample=50.0,
-                    sampling_method_type=sampling_method_type,
+                    profileSampleType=ProfileSampleType.PERCENTAGE,
+                    profileSample=50.0,
+                    samplingMethodType=sampling_method_type,
                 ),
             )
             query: CTE = sampler.get_sample_query()
@@ -132,7 +132,7 @@ class SampleTest(TestCase):
             ometa_client=None,
             entity=self.table_entity,
             sample_config=SampleConfig(
-                profile_sample_type=ProfileSampleType.ROWS, profile_sample=50
+                profileSampleType=ProfileSampleType.ROWS, profileSample=50
             ),
         )
         query: CTE = sampler.get_sample_query()
@@ -155,8 +155,8 @@ class SampleTest(TestCase):
             ometa_client=None,
             entity=self.table_entity,
             sample_config=SampleConfig(
-                profile_sample_type=ProfileSampleType.PERCENTAGE,
-                profile_sample=50.0,
+                profileSampleType=ProfileSampleType.PERCENTAGE,
+                profileSample=50.0,
             ),
             partition_details=PartitionProfilerConfig(
                 enablePartitioning=True,
