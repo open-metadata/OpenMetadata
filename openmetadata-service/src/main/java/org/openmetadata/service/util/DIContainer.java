3a22f1b36446341e353176ae051612af9366a8f0
sushi30 <imri.paran@gmail.com>
2025-02-06 14:07:21 +0100

    added docs for DIContainer

--- openmetadata-service/src/main/java/org/openmetadata/service/util/DIContainer.java
+++ openmetadata-service/src/main/java/org/openmetadata/service/util/DIContainer.java
@@ -3,6 +3,22 @@ package org.openmetadata.service.util;
 import java.util.HashMap;
 import java.util.Map;
 
+
+/**
+ * Dependency Injection container to manage resources. This is a simple implementation and does not
+ * support any lifecycle management. It allows registering resources and retrieving them by a
+ * a consuming class. Consuming class might use a subset of the resources registered (or none at all)
+ * Example:
+ *
+ * <pre>
+ *     DIContainer container = new DIContainer();
+ *     container.registerResource(MyService.class, new MyService());
+ *     MyService service = container.getResource(MyService.class);
+ *     service.doSomething();
+ * </pre>
+ *
+ * TODO - Add support for lifecycle management.
+ */
 public class DIContainer {
   private final Map<Class<?>, Object> resources = new HashMap<>();
 
