d87b081fa69df52850b4b319f98431fe76e0e6ab
Akash Verma <138790903+akashverma0786@users.noreply.github.com>
2025-02-07 11:35:02 +0530

    Added redshift external table lineage support (#19665)

--- ingestion/src/metadata/ingestion/source/database/redshift/queries.py
+++ ingestion/src/metadata/ingestion/source/database/redshift/queries.py
@@ -203,6 +203,11 @@ REDSHIFT_GET_SCHEMA_COLUMN_INFO = textwrap.dedent(
             """
 )
 
+REDSHIFT_EXTERNAL_TABLE_LOCATION = """
+  SELECT schemaname, tablename, location
+    FROM svv_external_tables
+    where redshift_database_name='{database_name}'
+"""
 
 REDSHIFT_PARTITION_DETAILS = """
   select "schema", "table", diststyle
