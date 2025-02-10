3bba51575309716f9d0fc46489a2162ae2948b29
Pablo Takara <pjt1991@gmail.com>
2025-02-10 14:43:52 +0100

    Fix db2 license file import

--- ingestion/src/metadata/ingestion/source/database/db2/connection.py
+++ ingestion/src/metadata/ingestion/source/database/db2/connection.py
@@ -50,7 +50,7 @@ def get_connection(connection: Db2Connection) -> Engine:
             "w",
             encoding=UTF_8,
         ) as file:
-            file.write(connection.license)
+            file.write(connection.license.encode("utf-8").decode("unicode-escape"))
 
     return create_generic_db_connection(
         connection=connection,
