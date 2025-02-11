8cd822509fb2a32809cd6f5c14599cca4202a5d4
Pere Miquel Brull <peremiquelbrull@gmail.com>
2025-02-11 12:46:04 +0100

    Unify limits

--- openmetadata-ui/src/main/resources/ui/src/generated/entity/applications/configuration/private/limits.ts
+++ openmetadata-ui/src/main/resources/ui/src/generated/entity/applications/configuration/private/limits.ts
@@ -11,34 +11,9 @@
  *  limitations under the License.
  */
 /**
- * Private Configuration for the CollateAI External Application.
- */
-export interface CollateAIAppPrivateConfig {
-    /**
-     * Collate Server public URL. WAII will use this information to interact with the server.
-     * E.g., https://sandbox.getcollate.io
-     */
-    collateURL: string;
-    /**
-     * Limits for the CollateAI Application.
-     */
-    limits: AppLimitsConfig;
-    /**
-     * WAII API Token
-     */
-    token: string;
-    /**
-     * WAII API host URL
-     */
-    waiiInstance: string;
-}
-
-/**
- * Limits for the CollateAI Application.
- *
  * Private Configuration for the App Limits.
  */
-export interface AppLimitsConfig {
+export interface Limits {
     /**
      * The records of the limits.
      */
