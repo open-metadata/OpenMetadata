7b97834068dba7a18fca8e1e710f69fc2eeb3e86
Mayur Singal <39544459+ulixius9@users.noreply.github.com>
2025-02-12 12:03:18 +0530

    MINOR: Add support for cluster key information - bigquery (#19727)

--- openmetadata-ui/src/main/resources/ui/src/utils/TableUtils.tsx
+++ openmetadata-ui/src/main/resources/ui/src/utils/TableUtils.tsx
@@ -248,6 +248,15 @@ export const getConstraintIcon = ({
 
       break;
     }
+    case ConstraintType.ClusterKey: {
+      title = t('label.entity-key', {
+        entity: t('label.cluster'),
+      });
+      icon = isConstraintDeleted ? IconDistLineThrough : IconDistKey;
+      dataTestId = 'cluster-key';
+
+      break;
+    }
     default:
       return null;
   }
