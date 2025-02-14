740f78559160ae21726e5b3566019a8384aac7d0
Gyowanny P. Queiroz <81326247+gyohub@users.noreply.github.com>
2025-02-14 12:08:37 -0300

    MINOR - Fixes relationship between IngestionPipeline and IngestionAgent entities (#19790)
    * MINOR - Fix relationship between IngestionPipeline and IngestionAgent entities
    
    * Add null check
    
    ---------
    
    Co-authored-by: Pere Miquel Brull <peremiquelbrull@gmail.com>

--- openmetadata-service/src/main/java/org/openmetadata/service/jdbi3/IngestionPipelineRepository.java
+++ openmetadata-service/src/main/java/org/openmetadata/service/jdbi3/IngestionPipelineRepository.java
@@ -153,12 +153,14 @@ public class IngestionPipelineRepository extends EntityRepository<IngestionPipel
   @Override
   public void storeRelationships(IngestionPipeline ingestionPipeline) {
     addServiceRelationship(ingestionPipeline, ingestionPipeline.getService());
-    addRelationship(
-        ingestionPipeline.getService().getId(),
-        ingestionPipeline.getId(),
-        ingestionPipeline.getService().getType(),
-        entityType,
-        Relationship.HAS);
+    if (ingestionPipeline.getIngestionAgent() != null) {
+      addRelationship(
+          ingestionPipeline.getId(),
+          ingestionPipeline.getIngestionAgent().getId(),
+          entityType,
+          ingestionPipeline.getIngestionAgent().getType(),
+          Relationship.HAS);
+    }
   }
 
   @Override
