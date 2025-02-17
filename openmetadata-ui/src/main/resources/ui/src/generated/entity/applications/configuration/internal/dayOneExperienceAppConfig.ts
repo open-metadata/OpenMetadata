6c6f9d410f2effeda72f792e12c88f879f2e7186
Pablo Takara <pjt1991@gmail.com>
2025-02-17 17:47:52 +0100

    Fix JSONSchemas

--- openmetadata-ui/src/main/resources/ui/src/generated/entity/applications/configuration/internal/dayOneExperienceAppConfig.ts
+++ openmetadata-ui/src/main/resources/ui/src/generated/entity/applications/configuration/internal/dayOneExperienceAppConfig.ts
@@ -30,5 +30,5 @@ export interface DayOneExperienceAppConfig {
  * Application type.
  */
 export enum DayOneExperienceAppType {
-    DayOneExperience = "DayOneExperience",
+    DayOneExperienceWorkflow = "DayOneExperienceWorkflow",
 }
