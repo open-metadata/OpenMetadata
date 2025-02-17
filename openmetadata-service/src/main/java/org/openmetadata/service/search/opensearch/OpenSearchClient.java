9c8c0dab08e5636d478b98c18ce6caded23d5656
mohitdeuex <mohit.y@deuexsolutions.com>
2025-02-17 15:58:10 +0530

    Fix Deleted filter

--- openmetadata-service/src/main/java/org/openmetadata/service/search/opensearch/OpenSearchClient.java
+++ openmetadata-service/src/main/java/org/openmetadata/service/search/opensearch/OpenSearchClient.java
@@ -1120,7 +1120,7 @@ public class OpenSearchClient implements SearchClient {
     searchSourceBuilder.query(
         QueryBuilders.boolQuery()
             .must(QueryBuilders.termQuery(direction, FullyQualifiedName.buildHash(fqn))));
-    if (CommonUtil.nullOrEmpty(deleted)) {
+    if (!CommonUtil.nullOrEmpty(deleted)) {
       searchSourceBuilder.query(
           QueryBuilders.boolQuery()
               .must(QueryBuilders.termQuery(direction, FullyQualifiedName.buildHash(fqn)))
@@ -1319,7 +1319,7 @@ public class OpenSearchClient implements SearchClient {
       if (searchAfter != null) {
         searchSourceBuilder.searchAfter(searchAfter);
       }
-      if (CommonUtil.nullOrEmpty(deleted)) {
+      if (!CommonUtil.nullOrEmpty(deleted)) {
         searchSourceBuilder.query(
             QueryBuilders.boolQuery()
                 .must(boolQueryBuilder)
