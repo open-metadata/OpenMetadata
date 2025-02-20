5d825c883da122b35a0c67dd657e4ca7cd597f62
Pere Miquel Brull <peremiquelbrull@gmail.com>
2025-02-20 14:39:05 +0100

    MINOR - Collate AI Tier App Config Spec (#19694)
    * MINOR - Collate AI Tier App Config Spec
    
    * MINOR - Collate AI Tier App Config Spec
    
    * MINOR - Collate AI Tier App Config Spec
    
    * Unify limits
    
    * Unify limits
    
    * Unify limits
    
    * Unify limits
    
    * bump version
    
    * bump version
    
    * map
    
    * map

--- openmetadata-ui/src/main/resources/ui/src/generated/entity/applications/configuration/private/internal/collateAITierAgentAppPrivateConfig.ts
+++ openmetadata-ui/src/main/resources/ui/src/generated/entity/applications/configuration/private/internal/collateAITierAgentAppPrivateConfig.ts
@@ -11,30 +11,17 @@
  *  limitations under the License.
  */
 /**
- * Private Configuration for the CollateAI External Application.
+ * Private Configuration for the CollateAITierAgent Internal Application.
  */
-export interface CollateAIAppPrivateConfig {
+export interface CollateAITierAgentAppPrivateConfig {
     /**
-     * Collate Server public URL. WAII will use this information to interact with the server.
-     * E.g., https://sandbox.getcollate.io
-     */
-    collateURL: string;
-    /**
-     * Limits for the CollateAI Application.
+     * Limits for the CollateAITierAgent Application.
      */
     limits: AppLimitsConfig;
-    /**
-     * WAII API Token
-     */
-    token: string;
-    /**
-     * WAII API host URL
-     */
-    waiiInstance: string;
 }
 
 /**
- * Limits for the CollateAI Application.
+ * Limits for the CollateAITierAgent Application.
  *
  * Private Configuration for the App Limits.
  */
