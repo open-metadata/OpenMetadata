aefc36b5969add77bbcbc1c3e9a586c1f439b8e5
sonika-shah <58761340+sonika-shah@users.noreply.github.com>
2025-02-20 16:43:30 +0530

    GEN 16908 - Support pagination for children field (#19650)
    * GEN 16908 - Support pagination for children field
    
    * Fix tests - Support pagination for children field
    
    * move children pagination listing to separate api
    
    * added pagination support from UI
    
    * added playwright test for the pagination test
    
    ---------
    
    Co-authored-by: Ashish Gupta <ashish@getcollate.io>

--- openmetadata-ui/src/main/resources/ui/playwright/constant/contianer.ts
+++ openmetadata-ui/src/main/resources/ui/playwright/constant/contianer.ts
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2023 Collate.
+ *  Copyright 2025 Collate.
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
@@ -10,16 +10,13 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-import { Include } from '../generated/type/include';
+import { uuid } from '../utils/common';
 
-export type ListParams = {
-  fields?: string | string[];
-  limit?: number;
-  before?: string;
-  after?: string;
-  include?: Include;
-};
+export const CONTAINER_CHILDREN = Array.from({ length: 25 }, (_, i) => {
+  const id = uuid();
 
-export type ListParamsWithOffset = ListParams & {
-  offset?: number;
-};
+  return {
+    name: `pw-container-children${i + 1}-${id}`,
+    displayName: `pw-container-children-${i + 1}-${id}`,
+  };
+});
