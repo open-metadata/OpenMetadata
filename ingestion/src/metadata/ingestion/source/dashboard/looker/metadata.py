14f83d99c90dceb56bca70a1bd6b19d4812d3309
ulixius9 <mayursingal9@gmail.com>
2025-02-26 10:59:06 +0530

    MINOR: Looker case insensitive search for users

--- ingestion/src/metadata/ingestion/source/dashboard/looker/metadata.py
+++ ingestion/src/metadata/ingestion/source/dashboard/looker/metadata.py
@@ -780,7 +780,9 @@ class LookerSource(DashboardServiceSource):
         try:
             if dashboard_details.user_id is not None:
                 dashboard_owner = self.client.user(dashboard_details.user_id)
-                return self.metadata.get_reference_by_email(dashboard_owner.email)
+                return self.metadata.get_reference_by_email(
+                    dashboard_owner.email.lower()
+                )
 
         except Exception as err:
             logger.debug(traceback.format_exc())
