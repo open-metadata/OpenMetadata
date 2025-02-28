1cbbe9a7c0c65a37d071ab51a01d0256639bd7f1
Karan Hotchandani <33024356+karanh37@users.noreply.github.com>
2025-02-28 21:25:40 +0530

    fix generated types (#20033)
    * fix generated types
    
    * fix path
    
    * fix unit tests

--- openmetadata-ui/src/main/resources/ui/src/generated/api/automations/createWorkflow.ts
+++ openmetadata-ui/src/main/resources/ui/src/generated/api/automations/createWorkflow.ts
@@ -33,7 +33,7 @@ export interface CreateWorkflow {
     /**
      * Owners of this workflow.
      */
-    owners?: OwnerElement[];
+    owners?: EntityReference[];
     /**
      * Request body for a specific workflow type
      */
@@ -64,8 +64,10 @@ export interface CreateWorkflow {
  * EntityReference is used for capturing relationships from one entity to another. For
  * example, a table has an attribute called database of type EntityReference that captures
  * the relationship of a table `belongs to a` database.
+ *
+ * Service to be modified
  */
-export interface OwnerElement {
+export interface EntityReference {
     /**
      * If true the entity referred to has been soft-deleted.
      */
@@ -143,7 +145,7 @@ export interface TestServiceConnectionRequest {
     /**
      * Service to be modified
      */
-    service?: ServiceClass;
+    service?: EntityReference;
     /**
      * Pipeline type
      */
@@ -2469,7 +2471,7 @@ export interface GCPCredentialsConfiguration {
     /**
      * Google Cloud Platform account type.
      */
-    type?: string;
+    type?: TypeEnum;
     /**
      * Path of the file containing the GCP credentials info
      */
@@ -2487,7 +2489,7 @@ export interface GCPCredentialsConfiguration {
     /**
      * Google Cloud Platform account type.
      */
-    externalType?: string;
+    externalType?: ExternalTypeEnum;
     /**
      * Google Security Token Service subject token type based on the OAuth 2.0 token exchange
      * spec.
@@ -2498,6 +2500,16 @@ export interface GCPCredentialsConfiguration {
      */
     tokenURL?: string;
     [property: string]: any;
+}
+
+export enum ExternalTypeEnum {
+    ExternalAccount = "external_account",
+}
+
+export enum TypeEnum {
+    ExternalAccount = "external_account",
+    GcpCredentialPath = "gcp_credential_path",
+    ServiceAccount = "service_account",
 }
 
 /**
@@ -3616,11 +3628,11 @@ export interface ReverseIngestionConfig {
     /**
      * Added owners to be applied
      */
-    addedOwners?: OwnerElement[];
+    addedOwners?: EntityReference[];
     /**
      * Removed owners from the entity
      */
-    removedOwners?: OwnerElement[];
+    removedOwners?: EntityReference[];
     /**
      * Added tags to be applied
      */
@@ -3725,57 +3737,6 @@ export enum Type {
     UpdateDescription = "UPDATE_DESCRIPTION",
     UpdateOwner = "UPDATE_OWNER",
     UpdateTags = "UPDATE_TAGS",
-}
-
-/**
- * Service to be modified
- *
- * This schema defines the EntityReference type used for referencing an entity.
- * EntityReference is used for capturing relationships from one entity to another. For
- * example, a table has an attribute called database of type EntityReference that captures
- * the relationship of a table `belongs to a` database.
- */
-export interface ServiceClass {
-    /**
-     * If true the entity referred to has been soft-deleted.
-     */
-    deleted?: boolean;
-    /**
-     * Optional description of entity.
-     */
-    description?: string;
-    /**
-     * Display Name that identifies this entity.
-     */
-    displayName?: string;
-    /**
-     * Fully qualified name of the entity instance. For entities such as tables, databases
-     * fullyQualifiedName is returned in this field. For entities that don't have name hierarchy
-     * such as `user` and `team` this will be same as the `name` field.
-     */
-    fullyQualifiedName?: string;
-    /**
-     * Link to the entity resource.
-     */
-    href?: string;
-    /**
-     * Unique identifier that identifies an entity instance.
-     */
-    id: string;
-    /**
-     * If true the relationship indicated by this entity reference is inherited from the parent
-     * entity.
-     */
-    inherited?: boolean;
-    /**
-     * Name of the entity instance.
-     */
-    name?: string;
-    /**
-     * Entity type/class name - Examples: `database`, `table`, `metrics`, `databaseService`,
-     * `dashboardService`...
-     */
-    type: string;
 }
 
 /**
--- openmetadata-ui/src/main/resources/ui/src/generated/entity/automations/workflow.ts
+++ openmetadata-ui/src/main/resources/ui/src/generated/entity/automations/workflow.ts
@@ -21,7 +21,7 @@ export interface Workflow {
     /**
      * List of data products this entity is part of.
      */
-    dataProducts?: DomainElement[];
+    dataProducts?: EntityReference[];
     /**
      * When `true` indicates the entity has been soft deleted.
      */
@@ -38,7 +38,7 @@ export interface Workflow {
      * Domain the asset belongs to. When not set, the asset inherits the domain from the parent
      * it belongs to.
      */
-    domain?: DomainElement;
+    domain?: EntityReference;
     /**
      * FullyQualifiedName same as `name`.
      */
@@ -63,7 +63,7 @@ export interface Workflow {
     /**
      * Owners of this workflow.
      */
-    owners?: DomainElement[];
+    owners?: EntityReference[];
     /**
      * Request body for a specific workflow type
      */
@@ -151,8 +151,10 @@ export interface FieldChange {
  *
  * Domain the asset belongs to. When not set, the asset inherits the domain from the parent
  * it belongs to.
+ *
+ * Service to be modified
  */
-export interface DomainElement {
+export interface EntityReference {
     /**
      * If true the entity referred to has been soft-deleted.
      */
@@ -483,7 +485,7 @@ export interface TestServiceConnectionRequest {
     /**
      * Service to be modified
      */
-    service?: ServiceClass;
+    service?: EntityReference;
     /**
      * Pipeline type
      */
@@ -2760,7 +2762,7 @@ export interface GCPCredentialsConfiguration {
     /**
      * Google Cloud Platform account type.
      */
-    type?: string;
+    type?: TypeEnum;
     /**
      * Path of the file containing the GCP credentials info
      */
@@ -2778,7 +2780,7 @@ export interface GCPCredentialsConfiguration {
     /**
      * Google Cloud Platform account type.
      */
-    externalType?: string;
+    externalType?: ExternalTypeEnum;
     /**
      * Google Security Token Service subject token type based on the OAuth 2.0 token exchange
      * spec.
@@ -2789,6 +2791,16 @@ export interface GCPCredentialsConfiguration {
      */
     tokenURL?: string;
     [property: string]: any;
+}
+
+export enum ExternalTypeEnum {
+    ExternalAccount = "external_account",
+}
+
+export enum TypeEnum {
+    ExternalAccount = "external_account",
+    GcpCredentialPath = "gcp_credential_path",
+    ServiceAccount = "service_account",
 }
 
 /**
@@ -3852,11 +3864,11 @@ export interface ReverseIngestionConfig {
     /**
      * Added owners to be applied
      */
-    addedOwners?: DomainElement[];
+    addedOwners?: EntityReference[];
     /**
      * Removed owners from the entity
      */
-    removedOwners?: DomainElement[];
+    removedOwners?: EntityReference[];
     /**
      * Added tags to be applied
      */
@@ -3961,57 +3973,6 @@ export enum Type {
     UpdateDescription = "UPDATE_DESCRIPTION",
     UpdateOwner = "UPDATE_OWNER",
     UpdateTags = "UPDATE_TAGS",
-}
-
-/**
- * Service to be modified
- *
- * This schema defines the EntityReference type used for referencing an entity.
- * EntityReference is used for capturing relationships from one entity to another. For
- * example, a table has an attribute called database of type EntityReference that captures
- * the relationship of a table `belongs to a` database.
- */
-export interface ServiceClass {
-    /**
-     * If true the entity referred to has been soft-deleted.
-     */
-    deleted?: boolean;
-    /**
-     * Optional description of entity.
-     */
-    description?: string;
-    /**
-     * Display Name that identifies this entity.
-     */
-    displayName?: string;
-    /**
-     * Fully qualified name of the entity instance. For entities such as tables, databases
-     * fullyQualifiedName is returned in this field. For entities that don't have name hierarchy
-     * such as `user` and `team` this will be same as the `name` field.
-     */
-    fullyQualifiedName?: string;
-    /**
-     * Link to the entity resource.
-     */
-    href?: string;
-    /**
-     * Unique identifier that identifies an entity instance.
-     */
-    id: string;
-    /**
-     * If true the relationship indicated by this entity reference is inherited from the parent
-     * entity.
-     */
-    inherited?: boolean;
-    /**
-     * Name of the entity instance.
-     */
-    name?: string;
-    /**
-     * Entity type/class name - Examples: `database`, `table`, `metrics`, `databaseService`,
-     * `dashboardService`...
-     */
-    type: string;
 }
 
 /**
