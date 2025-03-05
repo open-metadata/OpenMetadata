f004a86127472980c2d069985f07df063d372b57
sushi30 <imri.paran@gmail.com>
2025-03-05 10:56:37 +0100

    - removed `oneOf` from app configuration scheme - generated ts files for apps

--- openmetadata-ui/src/main/resources/ui/src/generated/entity/applications/app.ts
+++ openmetadata-ui/src/main/resources/ui/src/generated/entity/applications/app.ts
@@ -22,7 +22,7 @@ export interface App {
     /**
      * Application Configuration object.
      */
-    appConfiguration?: any[] | boolean | CollateAIAppConfig | number | null | string;
+    appConfiguration?: { [key: string]: any };
     /**
      * Application Logo Url.
      */
@@ -130,7 +130,7 @@ export interface App {
     /**
      * Application Private configuration loaded at runtime.
      */
-    privateConfiguration?: PrivateConfig;
+    privateConfiguration?: { [key: string]: any };
     provider?:             ProviderType;
     /**
      * Execution Configuration.
@@ -172,279 +172,51 @@ export interface App {
     version?: number;
 }
 
-/**
- * Configuration for the CollateAI External Application.
- *
- * Configuration for the Automator External Application.
- *
- * This schema defines the Slack App Token Configuration
- *
- * No configuration needed to instantiate the Data Insights Pipeline. The logic is handled
- * in the backend.
- *
- * Search Indexing App.
- *
- * Configuration for the Collate AI Quality Agent.
- *
- * Configuration for the Day One Experience Flow.
- */
-export interface CollateAIAppConfig {
+export interface AppScheduleClass {
     /**
-     * Query filter to be passed to ES. E.g.,
-     * `{"query":{"bool":{"must":[{"bool":{"should":[{"term":{"domain.displayName.keyword":"DG
-     * Anim"}}]}}]}}}`. This is the same payload as in the Explore page.
+     * Cron Expression in case of Custom scheduled Trigger
      */
-    filter?: string;
-    /**
-     * Patch the description if it is empty, instead of raising a suggestion
-     */
-    patchIfEmpty?: boolean;
-    /**
-     * Application Type
-     */
-    type?: Type;
-    /**
-     * Action to take on those entities. E.g., propagate description through lineage, auto
-     * tagging, etc.
-     */
-    actions?: Action[];
-    /**
-     * Entities selected to run the automation.
-     */
-    resources?: Resource;
-    /**
-     * Bot Token
-     */
-    botToken?: string;
-    /**
-     * User Token
-     */
-    userToken?:             string;
-    backfillConfiguration?: BackfillConfiguration;
-    /**
-     * Maximum number of events processed at a time (Default 100).
-     *
-     * Maximum number of events sent in a batch (Default 100).
-     */
-    batchSize?:           number;
-    moduleConfiguration?: ModuleConfiguration;
-    /**
-     * Recreates the DataAssets index on DataInsights. Useful if you changed a Custom Property
-     * Type and are facing errors. Bear in mind that recreating the index will delete your
-     * DataAssets and a backfill will be needed.
-     */
-    recreateDataAssetsIndex?: boolean;
-    sendToAdmins?:            boolean;
-    sendToTeams?:             boolean;
-    /**
-     * Number of threads to use for reindexing
-     */
-    consumerThreads?: number;
-    /**
-     * List of Entities to Reindex
-     */
-    entities?: string[];
-    /**
-     * Initial backoff time in milliseconds
-     */
-    initialBackoff?: number;
-    /**
-     * Maximum backoff time in milliseconds
-     */
-    maxBackoff?: number;
-    /**
-     * Maximum number of concurrent requests to the search index
-     */
-    maxConcurrentRequests?: number;
-    /**
-     * Maximum number of retries for a failed request
-     */
-    maxRetries?: number;
-    /**
-     * Maximum number of events sent in a batch (Default 100).
-     */
-    payLoadSize?: number;
-    /**
-     * Number of threads to use for reindexing
-     */
-    producerThreads?: number;
-    /**
-     * Queue Size to user internally for reindexing.
-     */
-    queueSize?: number;
-    /**
-     * This schema publisher run modes.
-     */
-    recreateIndex?: boolean;
-    /**
-     * Recreate Indexes with updated Language
-     */
-    searchIndexMappingLanguage?: SearchIndexMappingLanguage;
-    /**
-     * Whether the suggested tests should be active or not upon suggestion
-     *
-     * Whether the Day One Experience flow should be active or not.
-     */
-    active?: boolean;
-    /**
-     * Enter the retention period for change event records in days (e.g., 7 for one week, 30 for
-     * one month).
-     */
-    changeEventRetentionPeriod?: number;
+    cronExpression?:  string;
+    scheduleTimeline: ScheduleTimeline;
 }
 
 /**
- * Action to take on those entities. E.g., propagate description through lineage, auto
- * tagging, etc.
- *
- * Apply Tags to the selected assets.
- *
- * Remove Tags Action Type
- *
- * Add an owner to the selected assets.
- *
- * Remove Owner Action Type
- *
- * Add a Custom Property to the selected assets.
- *
- * Add owners to the selected assets.
- *
- * Remove Custom Properties Action Type
- *
- * Propagate description, tags and glossary terms via lineage
- *
- * ML Tagging action configuration for external automator.
+ * This schema defines the Application ScheduleTimeline Options
  */
-export interface Action {
-    /**
-     * Apply tags to the children of the selected assets that match the criteria. E.g., columns,
-     * tasks, topic fields,...
-     *
-     * Remove tags from the children of the selected assets. E.g., columns, tasks, topic
-     * fields,...
-     *
-     * Apply the description to the children of the selected assets that match the criteria.
-     * E.g., columns, tasks, topic fields,...
-     *
-     * Remove descriptions from the children of the selected assets. E.g., columns, tasks, topic
-     * fields,...
-     */
-    applyToChildren?: string[];
-    /**
-     * Update tags even if they are already defined in the asset. By default, incoming tags are
-     * merged with the existing ones.
-     *
-     * Update the domain even if it is defined in the asset. By default, we will only apply the
-     * domain to assets without domain.
-     *
-     * Update the description even if they are already defined in the asset. By default, we'll
-     * only add the descriptions to assets without the description set.
-     *
-     * Update the Custom Property even if it is defined in the asset. By default, we will only
-     * apply the owners to assets without the given Custom Property informed.
-     *
-     * Update the tier even if it is defined in the asset. By default, we will only apply the
-     * tier to assets without tier.
-     *
-     * Update the owners even if it is defined in the asset. By default, we will only apply the
-     * owners to assets without owner.
-     *
-     * Update descriptions, tags and Glossary Terms via lineage even if they are already defined
-     * in the asset. By default, descriptions are only updated if they are not already defined
-     * in the asset, and incoming tags are merged with the existing ones.
-     */
-    overwriteMetadata?: boolean;
-    /**
-     * Tags to apply
-     *
-     * Tags to remove
-     */
-    tags?: TagLabel[];
-    /**
-     * Application Type
-     */
-    type: ActionType;
-    /**
-     * Remove tags from all the children and parent of the selected assets.
-     *
-     * Remove descriptions from all the children and parent of the selected assets.
-     */
-    applyToAll?: boolean;
-    /**
-     * Remove tags by its label type
-     */
-    labels?: LabelElement[];
-    /**
-     * Domain to apply
-     */
-    domain?: EntityReference;
-    /**
-     * Description to apply
-     */
-    description?: string;
-    /**
-     * Owners to apply
-     *
-     * Custom Properties keys to remove
-     */
-    customProperties?: any;
-    /**
-     * tier to apply
-     */
-    tier?: TagLabel;
-    /**
-     * Owners to apply
-     */
-    owners?: EntityReference[];
-    /**
-     * Propagate the metadata to columns via column-level lineage.
-     */
-    propagateColumnLevel?: boolean;
-    /**
-     * Propagate description through lineage
-     */
-    propagateDescription?: boolean;
-    /**
-     * Propagate glossary terms through lineage
-     */
-    propagateGlossaryTerms?: boolean;
-    /**
-     * Propagate owner from the parent
-     */
-    propagateOwner?: boolean;
-    /**
-     * Propagate the metadata to the parents (e.g., tables) via lineage.
-     */
-    propagateParent?: boolean;
-    /**
-     * Propagate tags through lineage
-     */
-    propagateTags?: boolean;
-    /**
-     * Propagate tier from the parent
-     */
-    propagateTier?: boolean;
+export enum ScheduleTimeline {
+    Custom = "Custom",
+    Daily = " Daily",
+    Hourly = "Hourly",
+    Monthly = "Monthly",
+    None = "None",
+    Weekly = "Weekly",
 }
 
 /**
- * Domain to apply
+ * This schema defines the type of application.
+ */
+export enum AppType {
+    External = "external",
+    Internal = "internal",
+}
+
+/**
+ * Bot User Associated with this application.
  *
  * This schema defines the EntityReference type used for referencing an entity.
  * EntityReference is used for capturing relationships from one entity to another. For
  * example, a table has an attribute called database of type EntityReference that captures
  * the relationship of a table `belongs to a` database.
  *
- * Owners to apply
+ * Domain the asset belongs to. When not set, the asset inherits the domain from the parent
+ * it belongs to.
+ *
+ * Event Subscriptions for the Application.
  *
  * This schema defines the EntityReferenceList type used for referencing an entity.
  * EntityReference is used for capturing relationships from one entity to another. For
  * example, a table has an attribute called database of type EntityReference that captures
  * the relationship of a table `belongs to a` database.
- *
- * Bot User Associated with this application.
- *
- * Domain the asset belongs to. When not set, the asset inherits the domain from the parent
- * it belongs to.
  */
 export interface EntityReference {
     /**
@@ -487,317 +259,6 @@ export interface EntityReference {
      * `dashboardService`...
      */
     type: string;
-}
-
-/**
- * Remove tags by its label type
- */
-export enum LabelElement {
-    Automated = "Automated",
-    Manual = "Manual",
-    Propagated = "Propagated",
-}
-
-/**
- * This schema defines the type for labeling an entity with a Tag.
- *
- * tier to apply
- */
-export interface TagLabel {
-    /**
-     * Description for the tag label.
-     */
-    description?: string;
-    /**
-     * Display Name that identifies this tag.
-     */
-    displayName?: string;
-    /**
-     * Link to the tag resource.
-     */
-    href?: string;
-    /**
-     * Label type describes how a tag label was applied. 'Manual' indicates the tag label was
-     * applied by a person. 'Derived' indicates a tag label was derived using the associated tag
-     * relationship (see Classification.json for more details). 'Propagated` indicates a tag
-     * label was propagated from upstream based on lineage. 'Automated' is used when a tool was
-     * used to determine the tag label.
-     */
-    labelType: LabelTypeEnum;
-    /**
-     * Name of the tag or glossary term.
-     */
-    name?: string;
-    /**
-     * Label is from Tags or Glossary.
-     */
-    source: TagSource;
-    /**
-     * 'Suggested' state is used when a tag label is suggested by users or tools. Owner of the
-     * entity must confirm the suggested labels before it is marked as 'Confirmed'.
-     */
-    state:  State;
-    style?: Style;
-    tagFQN: string;
-}
-
-/**
- * Label type describes how a tag label was applied. 'Manual' indicates the tag label was
- * applied by a person. 'Derived' indicates a tag label was derived using the associated tag
- * relationship (see Classification.json for more details). 'Propagated` indicates a tag
- * label was propagated from upstream based on lineage. 'Automated' is used when a tool was
- * used to determine the tag label.
- */
-export enum LabelTypeEnum {
-    Automated = "Automated",
-    Derived = "Derived",
-    Manual = "Manual",
-    Propagated = "Propagated",
-}
-
-/**
- * Label is from Tags or Glossary.
- */
-export enum TagSource {
-    Classification = "Classification",
-    Glossary = "Glossary",
-}
-
-/**
- * 'Suggested' state is used when a tag label is suggested by users or tools. Owner of the
- * entity must confirm the suggested labels before it is marked as 'Confirmed'.
- */
-export enum State {
-    Confirmed = "Confirmed",
-    Suggested = "Suggested",
-}
-
-/**
- * UI Style is used to associate a color code and/or icon to entity to customize the look of
- * that entity in UI.
- */
-export interface Style {
-    /**
-     * Hex Color Code to mark an entity such as GlossaryTerm, Tag, Domain or Data Product.
-     */
-    color?: string;
-    /**
-     * An icon to associate with GlossaryTerm, Tag, Domain or Data Product.
-     */
-    iconURL?: string;
-}
-
-/**
- * Application Type
- *
- * Add Tags action type.
- *
- * Remove Tags Action Type.
- *
- * Add Owner Action Type.
- *
- * Remove Domain Action Type
- *
- * Add Description Action Type.
- *
- * Add Custom Properties Action Type.
- *
- * Remove Description Action Type
- *
- * Add Tier Action Type.
- *
- * Remove Tier Action Type
- *
- * Remove Owner Action Type
- *
- * Remove Custom Properties Action Type.
- *
- * Lineage propagation action type.
- *
- * ML PII Tagging action type.
- */
-export enum ActionType {
-    AddCustomPropertiesAction = "AddCustomPropertiesAction",
-    AddDescriptionAction = "AddDescriptionAction",
-    AddDomainAction = "AddDomainAction",
-    AddOwnerAction = "AddOwnerAction",
-    AddTagsAction = "AddTagsAction",
-    AddTierAction = "AddTierAction",
-    LineagePropagationAction = "LineagePropagationAction",
-    MLTaggingAction = "MLTaggingAction",
-    RemoveCustomPropertiesAction = "RemoveCustomPropertiesAction",
-    RemoveDescriptionAction = "RemoveDescriptionAction",
-    RemoveDomainAction = "RemoveDomainAction",
-    RemoveOwnerAction = "RemoveOwnerAction",
-    RemoveTagsAction = "RemoveTagsAction",
-    RemoveTierAction = "RemoveTierAction",
-}
-
-/**
- * Backfill Configuration
- */
-export interface BackfillConfiguration {
-    /**
-     * Enable Backfill for the configured dates
-     */
-    enabled?: boolean;
-    /**
-     * Date for which the backfill will end
-     */
-    endDate?: Date;
-    /**
-     * Date from which to start the backfill
-     */
-    startDate?: Date;
-    [property: string]: any;
-}
-
-/**
- * Different Module Configurations
- */
-export interface ModuleConfiguration {
-    /**
-     * App Analytics Module configuration
-     */
-    appAnalytics: AppAnalyticsConfig;
-    /**
-     * Cost Analysis Insights Module configuration
-     */
-    costAnalysis: CostAnalysisConfig;
-    /**
-     * Data Assets Insights Module configuration
-     */
-    dataAssets: DataAssetsConfig;
-    /**
-     * Data Quality Insights Module configuration
-     */
-    dataQuality: DataQualityConfig;
-}
-
-/**
- * App Analytics Module configuration
- */
-export interface AppAnalyticsConfig {
-    /**
-     * If Enabled, App Analytics insights will be populated when the App runs.
-     */
-    enabled: boolean;
-}
-
-/**
- * Cost Analysis Insights Module configuration
- */
-export interface CostAnalysisConfig {
-    /**
-     * If Enabled, Cost Analysis insights will be populated when the App runs.
-     */
-    enabled: boolean;
-}
-
-/**
- * Data Assets Insights Module configuration
- */
-export interface DataAssetsConfig {
-    /**
-     * If Enabled, Data Asset insights will be populated when the App runs.
-     */
-    enabled: boolean;
-    /**
-     * List of Entities to Reindex
-     */
-    entities?: string[];
-    /**
-     * Defines the number of days the Data Assets Insights information will be kept. After it
-     * they will be deleted.
-     */
-    retention?:     number;
-    serviceFilter?: ServiceFilter;
-}
-
-export interface ServiceFilter {
-    serviceName?: string;
-    serviceType?: string;
-}
-
-/**
- * Data Quality Insights Module configuration
- */
-export interface DataQualityConfig {
-    /**
-     * If Enabled, Data Quality insights will be populated when the App runs.
-     */
-    enabled: boolean;
-}
-
-/**
- * Entities selected to run the automation.
- */
-export interface Resource {
-    /**
-     * Query filter to be passed to ES. E.g.,
-     * `{"query":{"bool":{"must":[{"bool":{"should":[{"term":{"domain.displayName.keyword":"DG
-     * Anim"}}]}}]}}}`. This is the same payload as in the Explore page.
-     */
-    queryFilter?: string;
-    /**
-     * Type of the entity. E.g., 'table', 'chart',...
-     */
-    type?: string[];
-    [property: string]: any;
-}
-
-/**
- * Recreate Indexes with updated Language
- *
- * This schema defines the language options available for search index mappings.
- */
-export enum SearchIndexMappingLanguage {
-    En = "EN",
-    Jp = "JP",
-    Zh = "ZH",
-}
-
-/**
- * Application Type
- *
- * Application type.
- */
-export enum Type {
-    Automator = "Automator",
-    CollateAI = "CollateAI",
-    CollateAIQualityAgent = "CollateAIQualityAgent",
-    DataInsights = "DataInsights",
-    DataInsightsReport = "DataInsightsReport",
-    DayOneExperienceWorkflow = "DayOneExperienceWorkflow",
-    SearchIndexing = "SearchIndexing",
-}
-
-export interface AppScheduleClass {
-    /**
-     * Cron Expression in case of Custom scheduled Trigger
-     */
-    cronExpression?:  string;
-    scheduleTimeline: ScheduleTimeline;
-}
-
-/**
- * This schema defines the Application ScheduleTimeline Options
- */
-export enum ScheduleTimeline {
-    Custom = "Custom",
-    Daily = " Daily",
-    Hourly = "Hourly",
-    Monthly = "Monthly",
-    None = "None",
-    Weekly = "Weekly",
-}
-
-/**
- * This schema defines the type of application.
- */
-export enum AppType {
-    External = "external",
-    Internal = "internal",
 }
 
 /**
@@ -1091,47 +552,6 @@ export enum VerifySSL {
  */
 export enum Permissions {
     All = "All",
-}
-
-/**
- * Application Private configuration loaded at runtime.
- *
- * Private Configuration for the CollateAI External Application.
- */
-export interface PrivateConfig {
-    /**
-     * Collate Server public URL. WAII will use this information to interact with the server.
-     * E.g., https://sandbox.getcollate.io
-     */
-    collateURL: string;
-    /**
-     * Limits for the CollateAI Application.
-     */
-    limits: AppLimitsConfig;
-    /**
-     * WAII API Token
-     */
-    token: string;
-    /**
-     * WAII API host URL
-     */
-    waiiInstance: string;
-}
-
-/**
- * Limits for the CollateAI Application.
- *
- * Private Configuration for the App Limits.
- */
-export interface AppLimitsConfig {
-    /**
-     * The records of the limits.
-     */
-    actions: { [key: string]: number };
-    /**
-     * The start of this limit cycle.
-     */
-    billingCycleStart: Date;
 }
 
 /**
