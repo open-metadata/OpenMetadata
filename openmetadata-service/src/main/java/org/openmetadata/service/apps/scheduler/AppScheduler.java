cb44b906b69fc97283649ae6c9d69af6f5c89fad
sushi30 <imri.paran@gmail.com>
2025-03-05 18:29:13 +0100

    format

--- openmetadata-service/src/main/java/org/openmetadata/service/apps/scheduler/AppScheduler.java
+++ openmetadata-service/src/main/java/org/openmetadata/service/apps/scheduler/AppScheduler.java
@@ -70,7 +70,7 @@ public class AppScheduler {
   public static final String APPS_JOB_GROUP = "OMAppsJobGroup";
   public static final String APPS_TRIGGER_GROUP = "OMAppsJobGroup";
   public static final String APP_INFO_KEY = "applicationInfoKey";
-  public static final String APP_NAME ="appName";
+  public static final String APP_NAME = "appName";
   public static String CONFIG_OVERRIDE_KEY = "configOverride";
 
   private static AppScheduler instance;
