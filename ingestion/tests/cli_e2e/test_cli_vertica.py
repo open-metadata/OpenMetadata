5425b12bf9f0f5c7d49691ff5f1c41f9944afc56
Ayush Shah <ayush@getcollate.io>
2025-03-06 12:00:30 +0530

    test: Update Vertica CLI test with IF NOT EXISTS and OR REPLACE clauses

--- ingestion/tests/cli_e2e/test_cli_vertica.py
+++ ingestion/tests/cli_e2e/test_cli_vertica.py
@@ -24,14 +24,14 @@ from .common_e2e_sqa_mixins import SQACommonMethods
 
 class VerticaCliTest(CliCommonDB.TestSuite, SQACommonMethods):
     create_table_query: str = """
-        CREATE TABLE vendor_dimension_new AS
+        CREATE TABLE IF NOT EXISTS vendor_dimension_new AS
             SELECT *
             FROM vendor_dimension
             WHERE 1=0;
     """
 
     create_view_query: str = """
-        CREATE VIEW vendor_dimension_v AS
+        CREATE OR REPLACE VIEW vendor_dimension_v AS
             SELECT vendor_key, vendor_name
             FROM public.vendor_dimension_new;
     """
