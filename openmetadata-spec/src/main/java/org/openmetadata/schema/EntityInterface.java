4b8699265c4dd45cc5112b31d3547aa853c7c4c8
Pere Miquel Brull <peremiquelbrull@gmail.com>
2025-03-07 12:14:52 +0100

    MINOR - Add UsageDetails to EntityInterface (#20111)
    * MINOR - Add UsageDetails to EntityInterface
    
    * fix

--- openmetadata-spec/src/main/java/org/openmetadata/schema/EntityInterface.java
+++ openmetadata-spec/src/main/java/org/openmetadata/schema/EntityInterface.java
@@ -56,6 +56,10 @@ public interface EntityInterface {
 
   ChangeDescription getIncrementalChangeDescription();
 
+  default UsageDetails getUsageSummary() {
+    return null;
+  }
+
   default List<EntityReference> getOwners() {
     return null;
   }
@@ -127,6 +131,8 @@ public interface EntityInterface {
   void setChangeDescription(ChangeDescription changeDescription);
 
   void setIncrementalChangeDescription(ChangeDescription incrementalChangeDescription);
+
+  default void setUsageSummary(UsageDetails usageSummary) {}
 
   void setFullyQualifiedName(String fullyQualifiedName);
 
