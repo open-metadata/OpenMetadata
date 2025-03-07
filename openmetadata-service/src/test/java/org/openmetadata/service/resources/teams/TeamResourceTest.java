192039c0ad985de9fdf38c9908c49a5e349bb379
Zhang, Juntao <juntzhang@ebay.com>
2025-03-07 18:18:04 +0800

    Fix childrenCount of organization in team (#19761)

--- openmetadata-service/src/test/java/org/openmetadata/service/resources/teams/TeamResourceTest.java
+++ openmetadata-service/src/test/java/org/openmetadata/service/resources/teams/TeamResourceTest.java
@@ -510,8 +510,10 @@ public class TeamResourceTest extends EntityResourceTest<Team, CreateTeam> {
         "dep",
         DEPARTMENT,
         div12.getEntityReference(),
-        div21.getEntityReference(),
-        ORG_TEAM.getEntityReference());
+        div21.getEntityReference());
+
+    ORG_TEAM = getEntity(ORG_TEAM.getId(), "children", ADMIN_AUTH_HEADERS);
+    assertEquals(5, ORG_TEAM.getChildrenCount());
 
     //
     // Deletion tests to ensure no dangling parent/children relationship
@@ -520,6 +522,7 @@ public class TeamResourceTest extends EntityResourceTest<Team, CreateTeam> {
     deleteEntity(bu1.getId(), true, true, ADMIN_AUTH_HEADERS);
     ORG_TEAM = getEntity(ORG_TEAM.getId(), "children", ADMIN_AUTH_HEADERS);
     assertEntityReferencesDoesNotContain(ORG_TEAM.getChildren(), bu1.getEntityReference());
+    assertEquals(4, ORG_TEAM.getChildrenCount());
   }
 
   @Test
