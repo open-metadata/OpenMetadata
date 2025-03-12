beded8b6544650f6375039fc798dc79ae54c3e09
Pere Miquel Brull <peremiquelbrull@gmail.com>
2025-03-12 10:32:50 +0100

    simplify tc body for automator

--- openmetadata-ui/src/main/resources/ui/src/generated/entity/applications/configuration/external/automator/addTestCaseAction.ts
+++ openmetadata-ui/src/main/resources/ui/src/generated/entity/applications/configuration/external/automator/addTestCaseAction.ts
@@ -17,7 +17,7 @@ export interface AddTestCaseAction {
     /**
      * Add tests to the selected table columns
      */
-    applyToChildren?: string[];
+    applyToChildren?: TestCaseDefinitions[];
     /**
      * Update the test even if it is defined in the asset. By default, we will only apply the
      * test to assets without the existing test already existing.
@@ -31,6 +31,42 @@ export interface AddTestCaseAction {
      * Application Type
      */
     type: AddTestCaseActionType;
+}
+
+/**
+ * Minimum set of requirements to get a Test Case request ready
+ */
+export interface TestCaseDefinitions {
+    /**
+     * Compute the passed and failed row count for the test case.
+     */
+    computePassedFailedRowCount?: boolean;
+    parameterValues?:             TestCaseParameterValue[];
+    /**
+     * Fully qualified name of the test definition.
+     */
+    testDefinition?: string;
+    /**
+     * If the test definition supports it, use dynamic assertion to evaluate the test case.
+     */
+    useDynamicAssertion?: boolean;
+    [property: string]: any;
+}
+
+/**
+ * This schema defines the parameter values that can be passed for a Test Case.
+ */
+export interface TestCaseParameterValue {
+    /**
+     * name of the parameter. Must match the parameter names in testCaseParameterDefinition
+     */
+    name?: string;
+    /**
+     * value to be passed for the Parameters. These are input from Users. We capture this in
+     * string and convert during the runtime.
+     */
+    value?: string;
+    [property: string]: any;
 }
 
 /**
@@ -127,22 +163,6 @@ export interface EntityReference {
      * `dashboardService`...
      */
     type: string;
-}
-
-/**
- * This schema defines the parameter values that can be passed for a Test Case.
- */
-export interface TestCaseParameterValue {
-    /**
-     * name of the parameter. Must match the parameter names in testCaseParameterDefinition
-     */
-    name?: string;
-    /**
-     * value to be passed for the Parameters. These are input from Users. We capture this in
-     * string and convert during the runtime.
-     */
-    value?: string;
-    [property: string]: any;
 }
 
 /**
