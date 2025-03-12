9a2067cf2175957713f660986af147f400aab1d5
Pere Miquel Brull <peremiquelbrull@gmail.com>
2025-03-12 11:13:19 +0100

    simplify tc body for automator

--- openmetadata-ui/src/main/resources/ui/src/generated/entity/applications/configuration/external/automator/addTestCaseAction.ts
+++ openmetadata-ui/src/main/resources/ui/src/generated/entity/applications/configuration/external/automator/addTestCaseAction.ts
@@ -17,7 +17,7 @@ export interface AddTestCaseAction {
     /**
      * Add tests to the selected table columns
      */
-    applyToChildren?: TestCaseDefinitions[];
+    applyToChildren?: string[];
     /**
      * Update the test even if it is defined in the asset. By default, we will only apply the
      * test to assets without the existing test already existing.
@@ -26,7 +26,7 @@ export interface AddTestCaseAction {
     /**
      * Test Cases to apply
      */
-    testCases: CreateTestCaseRequest[];
+    testCases: TestCaseDefinitions[];
     /**
      * Application Type
      */
@@ -67,102 +67,6 @@ export interface TestCaseParameterValue {
      */
     value?: string;
     [property: string]: any;
-}
-
-/**
- * Test is a test definition to capture data quality tests.
- */
-export interface CreateTestCaseRequest {
-    /**
-     * Compute the passed and failed row count for the test case.
-     */
-    computePassedFailedRowCount?: boolean;
-    /**
-     * Description of the testcase.
-     */
-    description?: string;
-    /**
-     * Display Name that identifies this test.
-     */
-    displayName?: string;
-    entityLink?:  string;
-    /**
-     * Name that identifies this test case.
-     */
-    name: string;
-    /**
-     * Owners of this test
-     */
-    owners?:          EntityReference[];
-    parameterValues?: TestCaseParameterValue[];
-    /**
-     * Fully qualified name of the test definition.
-     */
-    testDefinition: string;
-    /**
-     * Fully qualified name of the testSuite
-     */
-    testSuite?: string;
-    /**
-     * If the test definition supports it, use dynamic assertion to evaluate the test case.
-     */
-    useDynamicAssertion?: boolean;
-}
-
-/**
- * Owners of this test
- *
- * This schema defines the EntityReferenceList type used for referencing an entity.
- * EntityReference is used for capturing relationships from one entity to another. For
- * example, a table has an attribute called database of type EntityReference that captures
- * the relationship of a table `belongs to a` database.
- *
- * This schema defines the EntityReference type used for referencing an entity.
- * EntityReference is used for capturing relationships from one entity to another. For
- * example, a table has an attribute called database of type EntityReference that captures
- * the relationship of a table `belongs to a` database.
- */
-export interface EntityReference {
-    /**
-     * If true the entity referred to has been soft-deleted.
-     */
-    deleted?: boolean;
-    /**
-     * Optional description of entity.
-     */
-    description?: string;
-    /**
-     * Display Name that identifies this entity.
-     */
-    displayName?: string;
-    /**
-     * Fully qualified name of the entity instance. For entities such as tables, databases
-     * fullyQualifiedName is returned in this field. For entities that don't have name hierarchy
-     * such as `user` and `team` this will be same as the `name` field.
-     */
-    fullyQualifiedName?: string;
-    /**
-     * Link to the entity resource.
-     */
-    href?: string;
-    /**
-     * Unique identifier that identifies an entity instance.
-     */
-    id: string;
-    /**
-     * If true the relationship indicated by this entity reference is inherited from the parent
-     * entity.
-     */
-    inherited?: boolean;
-    /**
-     * Name of the entity instance.
-     */
-    name?: string;
-    /**
-     * Entity type/class name - Examples: `database`, `table`, `metrics`, `databaseService`,
-     * `dashboardService`...
-     */
-    type: string;
 }
 
 /**
