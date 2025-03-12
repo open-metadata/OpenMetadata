fe96f8656e908a99e9344b56440198497b178ba1
karanh37 <karanh37@gmail.com>
2025-03-12 12:36:22 +0530

    add custom properties on domains page

--- openmetadata-ui/src/main/resources/ui/src/constants/constants.ts
+++ openmetadata-ui/src/main/resources/ui/src/constants/constants.ts
@@ -546,6 +546,7 @@ export const ENTITY_PATH = {
   apiEndpoints: 'apiEndpoint',
   dataProducts: 'dataProduct',
   metrics: 'metric',
+  domains: 'domain',
 };
 
 export const VALIDATION_MESSAGES = {
