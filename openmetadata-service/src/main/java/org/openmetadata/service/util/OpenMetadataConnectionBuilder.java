3e6bd37b2b29fef768460f01c7dd2128ef0fb61d
Pere Miquel Brull <peremiquelbrull@gmail.com>
2025-03-14 15:49:34 +0100

    FIX #20252 - Add Auto Classification bot (#20254)
    * FIX #20252 - Add Auto Classification bot
    
    * format

--- openmetadata-service/src/main/java/org/openmetadata/service/util/OpenMetadataConnectionBuilder.java
+++ openmetadata-service/src/main/java/org/openmetadata/service/util/OpenMetadataConnectionBuilder.java
@@ -91,10 +91,6 @@ public class OpenMetadataConnectionBuilder {
         String type = IngestionPipelineRepository.getPipelineWorkflowType(ingestionPipeline);
         botName = String.format("%sApplicationBot", type);
       }
-        // TODO: Remove this once we internalize the DataInsights app
-        // For now we need it since DataInsights has its own pipelineType inherited from when it was
-        // a standalone workflow
-      case DATA_INSIGHT -> botName = "DataInsightsApplicationBot";
       default -> botName =
           String.format("%s-bot", ingestionPipeline.getPipelineType().toString().toLowerCase());
     }
@@ -195,7 +191,8 @@ public class OpenMetadataConnectionBuilder {
   private User retrieveBotUser(String botName) {
     User botUser = retrieveIngestionBotUser(botName);
     if (botUser == null) {
-      throw new IllegalArgumentException("Please, verify that the ingestion-bot is present.");
+      throw new IllegalArgumentException(
+          String.format("Please, verify that the bot [%s] is present.", botName));
     }
     return botUser;
   }
