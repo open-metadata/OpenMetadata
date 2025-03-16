658e3bf4a611dc730fc0930b103e269ad84871b5
sonikashah <sonikashah94@gmail.com>
2025-03-16 12:56:11 +0530

    Add ViewCustomFields operation

--- openmetadata-ui/src/main/resources/ui/src/generated/entity/policies/accessControl/resourceDescriptor.ts
+++ openmetadata-ui/src/main/resources/ui/src/generated/entity/policies/accessControl/resourceDescriptor.ts
@@ -65,6 +65,7 @@ export enum Operation {
     Trigger = "Trigger",
     ViewAll = "ViewAll",
     ViewBasic = "ViewBasic",
+    ViewCustomFields = "ViewCustomFields",
     ViewDataProfile = "ViewDataProfile",
     ViewProfilerGlobalConfiguration = "ViewProfilerGlobalConfiguration",
     ViewQueries = "ViewQueries",
