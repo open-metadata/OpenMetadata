2c749b438a703d805a74330cb4c7a6899d3ba7c6
mgorsk1 <gorskimariusz13@gmail.com>
2025-03-18 01:06:32 +0100

    :tada: Init (#19267)

--- ingestion/src/metadata/ingestion/source/database/dbt/dbt_config.py
+++ ingestion/src/metadata/ingestion/source/database/dbt/dbt_config.py
@@ -255,6 +255,7 @@ def get_blobs_grouped_by_dir(blobs: List[str]) -> Dict[str, List[str]]:
             DBT_MANIFEST_FILE_NAME == blob_file_name.lower()
             or DBT_CATALOG_FILE_NAME == blob_file_name.lower()
             or DBT_RUN_RESULTS_FILE_NAME in blob_file_name.lower()
+            or DBT_SOURCES_FILE_NAME == blob_file_name.lower()
         ):
             blob_grouped_by_directory[subdirectory].append(blob)
     return blob_grouped_by_directory
