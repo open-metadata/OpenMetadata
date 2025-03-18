27a87621503dd84327309278e0e2c01645a7c464
KC31 <45307507+kunalchoudhary31@users.noreply.github.com>
2025-03-18 11:43:20 +0530

    Fixes ISSUE-13953: Converted Nifi Client from requests module to OM REST client (#20039)
    * ISSUE-13953 Converted Nifi Client from requests module to OM REST client
    
    * pyformat
    
    * lint
    
    ---------
    
    Co-authored-by: kc <kc@kcs-MacBook-Pro.local>
    Co-authored-by: ulixius9 <mayursingal9@gmail.com>

--- ingestion/src/metadata/ingestion/source/pipeline/nifi/connection.py
+++ ingestion/src/metadata/ingestion/source/pipeline/nifi/connection.py
@@ -17,9 +17,6 @@ from typing import Optional
 from metadata.generated.schema.entity.automations.workflow import (
     Workflow as AutomationWorkflow,
 )
-from metadata.generated.schema.entity.services.connections.pipeline.nifi.basicAuth import (
-    NifiBasicAuth,
-)
 from metadata.generated.schema.entity.services.connections.pipeline.nifiConnection import (
     NifiConnection,
 )
@@ -36,22 +33,8 @@ def get_connection(connection: NifiConnection) -> NifiClient:
     """
     Create connection
     """
-    if isinstance(connection.nifiConfig, NifiBasicAuth):
-        return NifiClient(
-            host_port=connection.hostPort,
-            username=connection.nifiConfig.username,
-            password=connection.nifiConfig.password.get_secret_value()
-            if connection.nifiConfig.password
-            else None,
-            verify=connection.nifiConfig.verifySSL,
-        )
 
-    return NifiClient(
-        host_port=connection.hostPort,
-        ca_file_path=connection.nifiConfig.certificateAuthorityPath,
-        client_cert_path=connection.nifiConfig.clientCertificatePath,
-        client_key_path=connection.nifiConfig.clientkeyPath,
-    )
+    return NifiClient(connection=connection)
 
 
 def test_connection(
