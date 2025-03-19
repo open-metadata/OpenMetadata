252d0329644b265e78ffa87bbf2d80696a4f91c9
Teddy Crepineau <teddy.crepineau@gmail.com>
2025-03-19 10:19:18 +0100

    migration: createdBy sql migration

--- bootstrap/sql/migrations/native/1.7.0/mysql/postDataMigrationSQLScript.sql
+++ bootstrap/sql/migrations/native/1.7.0/mysql/postDataMigrationSQLScript.sql
@@ -5,3 +5,7 @@ WHERE JSON_EXTRACT(json, '$.trigger.type') in ('eventBasedEntityTrigger', 'event
 UPDATE workflow_definition_entity
 SET json = JSON_SET(json, '$.trigger.type', 'periodicBatchEntity')
 WHERE JSON_EXTRACT(json, '$.trigger.type') in ('periodicBatchEntityTrigger', 'periodicBatchEntityWorkflow');
+
+UPDATE test_case
+SET json = json_set(json, '$.createdBy', json->>'$.updatedBy')
+WHERE json->>'$.createdBy' IS NULL;
