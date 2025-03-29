bcbfdb9cc31a3a1b5176daa12d06ea40a4507a88
Shailesh Parmar <shailesh.parmar.webdev@gmail.com>
2025-03-29 13:13:53 +0530

    fixed alert bar issue

--- openmetadata-ui/src/main/resources/ui/playwright/utils/common.ts
+++ openmetadata-ui/src/main/resources/ui/playwright/utils/common.ts
@@ -120,9 +120,7 @@ export const toastNotification = async (
 ) => {
   await expect(page.getByTestId('alert-bar')).toHaveText(message);
 
-  await expect(page.getByTestId('alert-icon')).toBeVisible();
-
-  await expect(page.getByTestId('alert-icon-close')).toBeVisible();
+  await page.getByTestId('alert-icon-close').click();
 };
 
 export const clickOutside = async (page: Page) => {
