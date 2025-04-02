d12d109ca84932a45628e0b726a3116d119342cf
Teddy Crepineau <teddy.crepineau@gmail.com>
2025-04-02 16:17:04 +0200

    fix: allow incident to be set to new from any points

--- openmetadata-service/src/test/java/org/openmetadata/service/resources/dqtests/TestCaseResourceTest.java
+++ openmetadata-service/src/test/java/org/openmetadata/service/resources/dqtests/TestCaseResourceTest.java
@@ -1820,36 +1820,6 @@ public class TestCaseResourceTest extends EntityResourceTest<TestCase, CreateTes
   }
 
   @Test
-  void unauthorizedTestCaseResolutionFlow(TestInfo test)
-      throws HttpResponseException, ParseException {
-    TestCase testCaseEntity = createEntity(createRequest(getEntityName(test)), ADMIN_AUTH_HEADERS);
-    // Add failed test case, which will create a NEW incident
-    postTestCaseResult(
-        testCaseEntity.getFullyQualifiedName(),
-        new CreateTestCaseResult()
-            .withResult("result")
-            .withTestCaseStatus(TestCaseStatus.Failed)
-            .withTimestamp(TestUtils.dateToTimestamp("2024-01-01")),
-        ADMIN_AUTH_HEADERS);
-
-    // Now, we should be good to create an ASSIGNED status
-    CreateTestCaseResolutionStatus createAssignedIncident =
-        new CreateTestCaseResolutionStatus()
-            .withTestCaseReference(testCaseEntity.getFullyQualifiedName())
-            .withTestCaseResolutionStatusType(TestCaseResolutionStatusTypes.Assigned)
-            .withTestCaseResolutionStatusDetails(new Assigned().withAssignee(USER1_REF));
-    createTestCaseFailureStatus(createAssignedIncident);
-
-    assertResponseContains(
-        () ->
-            createTestCaseFailureStatus(
-                createAssignedIncident.withTestCaseResolutionStatusType(
-                    TestCaseResolutionStatusTypes.Ack)),
-        BAD_REQUEST,
-        "Incident with status [Assigned] cannot be moved to [Ack]");
-  }
-
-  @Test
   void testInferSeverity() {
     IncidentSeverityClassifierInterface severityClassifier =
         IncidentSeverityClassifierInterface.getInstance();
