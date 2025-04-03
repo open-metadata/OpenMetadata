054ddf049bd441faf0f3fe4f2ff4d107ae37fecc
Ayush Shah <ayush@getcollate.io>
2025-04-03 14:15:06 +0530

    Enhance Slack Link Monitoring Workflow and Script
    - Updated GitHub Actions workflow to include deployment type as an input for monitoring Slack link status.
    - Modified the slack-link-monitor.py script to accept deployment type as an argument, allowing for dynamic URL selection based on the deployment type.
    - Improved error handling and logging in the monitoring script for better debugging.

--- scripts/slack-link-monitor.py
+++ scripts/slack-link-monitor.py
@@ -11,17 +11,45 @@
 
 import logging
 import requests
+import argparse
 
-try:
-    res = requests.post(
-        "https://linkmonitor.onrender.com/api/v1/validate",
-        headers={"Content-Type": "application/json"},
-        json={"url": "https://slack.open-metadata.org"},
+
+def main():
+    parser = argparse.ArgumentParser(description="Monitor Slack link status")
+    parser.add_argument(
+        "deployment_type",
+        choices=["open-metadata", "free-tier-collate"],
+        help="Deployment type: open-metadata or free-tier-collate",
     )
-    if res.json().get("status") != "active":
-        raise RuntimeError("Expired status!")
-except RuntimeError as err:
-    raise err
-except Exception as err:
-    logging.error(f"Something went wrong fetching the data - [{err}]")
-    raise err
+
+    args = parser.parse_args()
+    slack_url = None
+    if args.deployment_type == "open-metadata":
+        slack_url = "https://slack.open-metadata.org"
+    elif args.deployment_type == "free-tier-support":
+        slack_url = "https://free-tier-support.getcollate.io/"
+
+    if slack_url is None:
+        raise ValueError(f"Invalid deployment type: {args.deployment_type}")
+
+    res = None
+    try:
+        res = requests.post(
+            "https://linkmonitor.onrender.com/api/v1/validate",
+            headers={"Content-Type": "application/json"},
+            json={"url": slack_url},
+        )
+        if res.json().get("status") != "active":
+            raise RuntimeError("Expired status!")
+    except RuntimeError as err:
+        raise err
+    except Exception as err:
+        error_msg = f"Something went wrong fetching the data - [{err}]"
+        if res is not None:
+            error_msg += f" [{res.text}]"
+        logging.error(error_msg)
+        raise err
+
+
+if __name__ == "__main__":
+    main()
