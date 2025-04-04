63fe369a1a94e063c5e07f5c1042e0e09dc847f9
Ashish Gupta <ashish@getcollate.io>
2025-04-04 13:05:49 +0530

    file was not running due to wrong filename

--- openmetadata-ui/src/main/resources/ui/playwright.config.ts
+++ openmetadata-ui/src/main/resources/ui/playwright.config.ts
@@ -74,7 +74,7 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
       // Added admin setup as a dependency. This will authorize the page with an admin user before running the test. doc: https://playwright.dev/docs/auth#multiple-signed-in-roles
       dependencies: ['setup'],
-      testMatch: '**/BulkEditEntity.ts',
+      testMatch: '**/BulkEditEntity.spec.ts',
       grepInvert: /data-insight/,
     },
     {
