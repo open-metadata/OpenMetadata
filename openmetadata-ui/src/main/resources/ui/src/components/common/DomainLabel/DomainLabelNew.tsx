f28e4760eabc4e8c517675b5208f0c2899f26082
shrushti2000 <shrushtipolekar@gmail.com>
2025-04-04 16:31:20 +0530

    inherited domain and no data placeholder size updates

--- openmetadata-ui/src/main/resources/ui/src/components/common/DomainLabel/DomainLabelNew.tsx
+++ openmetadata-ui/src/main/resources/ui/src/components/common/DomainLabel/DomainLabelNew.tsx
@@ -121,10 +121,9 @@ export const DomainLabelNew = ({
                   title={t('label.inherited-entity', {
                     entity: t('label.domain'),
                   })}>
-                  <InheritIcon
-                    className="inherit-icon cursor-pointer"
-                    width={14}
-                  />
+                  <span className="inherit-icon-container d-flex items-center flex-center">
+                    <InheritIcon className="inherit-icon" height={8} />
+                  </span>
                 </Tooltip>
               ) : null;
 
@@ -205,7 +204,7 @@ export const DomainLabelNew = ({
         </div>
 
         <div className="d-flex justify-between w-full">
-          <Typography.Text className="text-sm font-medium">
+          <Typography.Text className="text-sm font-medium p-l-xss">
             {t('label.domain-plural')}
           </Typography.Text>
           {selectableList}
