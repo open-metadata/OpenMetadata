c178aa2d6b0ba3f68be7a02ef245eba1ab0eb713
Dhruv Parmar <83108871+dhruvjsx@users.noreply.github.com>
2025-04-07 22:14:59 +0530

    fixed no owner (#20659)

--- openmetadata-ui/src/main/resources/ui/src/components/common/OwnerLabel/OwnerLabel.component.tsx
+++ openmetadata-ui/src/main/resources/ui/src/components/common/OwnerLabel/OwnerLabel.component.tsx
@@ -51,7 +51,7 @@ export const OwnerLabel = ({
             {showLabel && (
               <Typography.Text
                 className={classNames(
-                  'no-owner font-medium text-sm',
+                  'no-owner-heading font-medium text-sm',
                   className
                 )}>
                 {placeHolder ?? t('label.owner-plural')}
