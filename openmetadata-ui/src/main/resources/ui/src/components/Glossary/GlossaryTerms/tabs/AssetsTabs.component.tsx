bffcab81d089670d27b3c46e7895644dddcae3e4
karanh37 <karanh37@gmail.com>
2025-04-09 21:21:01 +0530

    fix styling

--- openmetadata-ui/src/main/resources/ui/src/components/Glossary/GlossaryTerms/tabs/AssetsTabs.component.tsx
+++ openmetadata-ui/src/main/resources/ui/src/components/Glossary/GlossaryTerms/tabs/AssetsTabs.component.tsx
@@ -812,7 +812,11 @@ const AssetsTabs = forwardRef(
           )}
           data-testid="table-container"
           id="asset-tab">
-          <Row className="h-full filters-row gap-2 " gutter={[0, 20]}>
+          <Row
+            className={classNames('filters-row gap-2 ', {
+              'h-full': assetCount === 0,
+            })}
+            gutter={[0, 20]}>
             {assetCount > 0 && (
               <>
                 <Col className="d-flex items-center gap-3" span={24}>
