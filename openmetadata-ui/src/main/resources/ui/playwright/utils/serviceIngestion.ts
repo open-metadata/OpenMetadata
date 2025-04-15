36ba0cd343ea87adbfa8ea786464e47eb7763d51
Aniket Katkar <aniketkatkar97@gmail.com>
2025-04-15 14:00:33 +0530

    Chore(ui): Increase timeout for delete toast verification (#20827)
    * Increase timeout for delete toast verification for entities which might take more time
    
    * Remove the redundancy

--- openmetadata-ui/src/main/resources/ui/playwright/utils/serviceIngestion.ts
+++ openmetadata-ui/src/main/resources/ui/playwright/utils/serviceIngestion.ts
@@ -12,6 +12,7 @@
  */
 
 import { expect, Page } from '@playwright/test';
+import { BIG_ENTITY_DELETE_TIMEOUT } from '../constant/delete';
 import { GlobalSettingOptions } from '../constant/settings';
 import { EntityTypeEndpoint } from '../support/entity/Entity.interface';
 import { getApiContext, toastNotification } from './common';
@@ -120,7 +121,11 @@ export const deleteService = async (
   await deleteResponse;
 
   // Closing the toast notification
-  await toastNotification(page, /deleted successfully!/, 5 * 60 * 1000); // Wait for up to 5 minutes for the toast notification to appear
+  await toastNotification(
+    page,
+    /deleted successfully!/,
+    BIG_ENTITY_DELETE_TIMEOUT
+  ); // Wait for up to 5 minutes for the toast notification to appear
 
   await page.reload();
   await page.waitForLoadState('networkidle');
