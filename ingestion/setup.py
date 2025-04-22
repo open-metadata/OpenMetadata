29579a28f27344ed5e533207efcf73a1b14d6cd9
Elay Gelbart <elaygelbart@gmail.com>
2025-04-22 16:34:29 +0300

    upgrade openmetadata-ingestion dependency google-cloud-secret-manager version to 2.23.3 with ~

--- ingestion/setup.py
+++ ingestion/setup.py
@@ -131,7 +131,7 @@ base_requirements = {
     "cached-property==1.5.2",  # LineageParser
     "chardet==4.0.0",  # Used in the profiler
     "cryptography>=42.0.0",
-    "google-cloud-secret-manager==2.23.3",
+    "google-cloud-secret-manager~=2.23.3",
     "google-crc32c",
     "email-validator>=2.0",  # For the pydantic generated models for Email
     "importlib-metadata>=4.13.0",  # From airflow constraints
@@ -456,7 +456,7 @@ playwright_dependencies = {
     *plugins["airflow"],
     *plugins["datalake-s3"],
     *plugins["dbt"],
-    *e2e_test
+    *e2e_test,
     # Add other plugins as needed for Playwright tests
 }
 
