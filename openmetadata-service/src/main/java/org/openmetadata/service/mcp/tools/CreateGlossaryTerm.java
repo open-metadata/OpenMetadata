0911388b2a05a0d512bfdc14d4823d49cdab5895
mohitdeuex <mohit.y@deuexsolutions.com>
2025-05-03 13:44:56 +0530

    Use prepare

--- openmetadata-service/src/main/java/org/openmetadata/service/mcp/tools/CreateGlossaryTerm.java
+++ openmetadata-service/src/main/java/org/openmetadata/service/mcp/tools/CreateGlossaryTerm.java
@@ -1,17 +1,20 @@
 package org.openmetadata.service.mcp.tools;
 
+import static org.openmetadata.common.utils.CommonUtil.nullOrEmpty;
 import static org.openmetadata.service.Entity.ADMIN_USER_NAME;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
+import org.openmetadata.schema.entity.data.Glossary;
 import org.openmetadata.schema.entity.data.GlossaryTerm;
 import org.openmetadata.schema.entity.teams.User;
 import org.openmetadata.schema.type.EntityReference;
 import org.openmetadata.schema.type.Include;
 import org.openmetadata.service.Entity;
 import org.openmetadata.service.exception.EntityNotFoundException;
+import org.openmetadata.service.jdbi3.GlossaryRepository;
 import org.openmetadata.service.jdbi3.GlossaryTermRepository;
 import org.openmetadata.service.jdbi3.UserRepository;
 import org.openmetadata.service.resources.glossary.GlossaryTermMapper;
@@ -73,11 +76,16 @@ public class CreateGlossaryTerm {
     }
 
     try {
+      GlossaryRepository glossaryRepository =
+          (GlossaryRepository) Entity.getEntityRepository(Entity.GLOSSARY);
+      Glossary glossary =
+          glossaryRepository.findByNameOrNull(createGlossaryTerm.getGlossary(), Include.ALL);
       GlossaryTerm glossaryTerm =
           glossaryTermMapper.createToEntity(createGlossaryTerm, ADMIN_USER_NAME);
       GlossaryTermRepository glossaryTermRepository =
           (GlossaryTermRepository) Entity.getEntityRepository(Entity.GLOSSARY_TERM);
       // TODO: Get the updatedBy from the tool request.
+      glossaryTermRepository.prepare(glossaryTerm, nullOrEmpty(glossary));
       glossaryTermRepository.setFullyQualifiedName(glossaryTerm);
       RestUtil.PutResponse<GlossaryTerm> response =
           glossaryTermRepository.createOrUpdate(null, glossaryTerm, "admin");
