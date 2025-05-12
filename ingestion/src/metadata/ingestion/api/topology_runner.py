6c38d35f445d7c2344614e1131befaffff6813c1
SumanMaharana <sumanmaharana786@gmail.com>
2025-05-12 16:51:29 +0530

    fix overwrite metadata

--- ingestion/src/metadata/ingestion/api/topology_runner.py
+++ ingestion/src/metadata/ingestion/api/topology_runner.py
@@ -376,11 +376,7 @@ class TopologyRunnerMixin(Generic[C]):
             entity_source_hash = self.cache[stage.type_].get(entity_fqn)
             if entity_source_hash:
                 # if the source hash is present, compare it with new hash
-                # if overrideMetadata is true, we will always update the entity
-                if (
-                    entity_source_hash != create_entity_request_hash
-                    or self.source_config.overrideMetadata
-                ):
+                if entity_source_hash != create_entity_request_hash:
                     # the entity has changed, get the entity from server and make a patch request
                     entity = self.metadata.get_by_name(
                         entity=stage.type_,
