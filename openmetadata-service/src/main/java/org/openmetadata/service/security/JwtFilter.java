27ca6a25a28d134d424aad242ada8c1fbf3ce643
sushi30 <imri.paran@gmail.com>
2025-05-14 14:43:32 +0200

    fixed whitelist links

--- openmetadata-service/src/main/java/org/openmetadata/service/security/JwtFilter.java
+++ openmetadata-service/src/main/java/org/openmetadata/service/security/JwtFilter.java
@@ -93,7 +93,7 @@ public class JwtFilter implements ContainerRequestFilter {
           "v1/users/password/reset",
           "v1/users/login",
           "v1/users/refresh",
-          "v1/collate/apps/support/magicLink");
+          "v1/collate/apps/support/login");
 
   @SuppressWarnings("unused")
   private JwtFilter() {}
