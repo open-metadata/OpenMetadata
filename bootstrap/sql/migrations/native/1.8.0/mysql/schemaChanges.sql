73e12d1abd6d96bb395d491b1eec2abb3c5809d8
sushi30 <imri.paran@gmail.com>
2025-05-14 15:58:50 +0200

    added indexes for runAt

--- bootstrap/sql/migrations/native/1.8.0/mysql/schemaChanges.sql
+++ bootstrap/sql/migrations/native/1.8.0/mysql/schemaChanges.sql
@@ -1,6 +1,8 @@
 ALTER TABLE background_jobs
 ADD COLUMN runAt BIGINT;
 
+ALTER TABLE `background_jobs` ADD INDEX `background_jobs_run_at_index` (`runAt`);
+
 -- Add runtime: enabled for AutoPilot
 UPDATE apps_marketplace
 SET json = JSON_SET(
