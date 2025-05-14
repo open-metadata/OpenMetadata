7941a9eaf2f6d1b3e04081400b4a760d10bd895c
karanh37 <karanh37@gmail.com>
2025-05-14 13:29:04 +0530

    add logs for internal applications

--- openmetadata-ui/src/main/resources/ui/src/utils/RouterUtils.ts
+++ openmetadata-ui/src/main/resources/ui/src/utils/RouterUtils.ts
@@ -19,6 +19,7 @@ import {
   IN_PAGE_SEARCH_ROUTES,
   LOG_ENTITY_NAME,
   LOG_ENTITY_TYPE,
+  LOG_RUN_ID,
   PLACEHOLDER_ACTION,
   PLACEHOLDER_DASHBOARD_TYPE,
   PLACEHOLDER_ROUTE_ENTITY_TYPE,
@@ -409,13 +410,18 @@ export const getTestSuiteIngestionPath = (
 export const getLogsViewerPath = (
   logEntityType: string,
   logEntityName: string,
-  ingestionName: string
+  ingestionName: string,
+  logRunId?: string
 ) => {
   let path = ROUTES.LOGS;
 
   path = path.replace(LOG_ENTITY_TYPE, logEntityType);
   path = path.replace(LOG_ENTITY_NAME, logEntityName);
   path = path.replace(PLACEHOLDER_ROUTE_FQN, getEncodedFqn(ingestionName));
+
+  if (logRunId) {
+    path = path.replace(LOG_RUN_ID, logRunId);
+  }
 
   return path;
 };
