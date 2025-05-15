22bb266bc2195688e18fac10a4a671ef8349003a
Dhruv Parmar <83108871+dhruvjsx@users.noreply.github.com>
2025-05-15 19:32:40 +0530

    removed changes that does not belongs to me in pr

--- openmetadata-ui/src/main/resources/ui/src/generated/entity/services/ingestionPipelines/ingestionPipeline.ts
+++ openmetadata-ui/src/main/resources/ui/src/generated/entity/services/ingestionPipelines/ingestionPipeline.ts
@@ -1236,7 +1236,7 @@ export interface Pipeline {
     /**
      * Application configuration
      */
-    appConfig?: any[] | boolean | number | null | CollateAIAppConfig | string;
+    appConfig?: any[] | boolean | CollateAIAppConfig | number | null | string;
     /**
      * Application private configuration
      */
@@ -1392,7 +1392,6 @@ export interface CollateAIAppConfig {
      * Service Entity Link for which to trigger the application.
      */
     entityLink?: string;
-    [property: string]: any;
 }
 
 /**
@@ -1906,20 +1905,18 @@ export interface PrivateConfig {
      * Collate Server public URL. WAII will use this information to interact with the server.
      * E.g., https://sandbox.getcollate.io
      */
-    collateURL?: string;
+    collateURL: string;
     /**
      * Limits for the CollateAI Application.
      */
-    limits?: AppLimitsConfig;
+    limits: AppLimitsConfig;
     /**
      * WAII API Token
      */
-    token?: string;
+    token: string;
     /**
      * WAII API host URL
      */
-    waiiInstance?: string;
-    [property: string]: any;
 }
 
 /**
