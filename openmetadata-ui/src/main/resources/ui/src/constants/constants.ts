a6dddde94da83e3fea528c323423326724eec8d2
Sweta Agarwalla <105535990+sweta1308@users.noreply.github.com>
2025-05-16 23:11:50 +0530

    Update ingestion runner fetch condition (#21222)
    * update ingestion runner fetch condition
    
    * update condition in service details page
    
    * add test for runner fetching
    
    * fix sonar failure
    
    * fix failures
    
    * mock config data function
    
    * add condition for ip address alert
    
    * update test condition

--- openmetadata-ui/src/main/resources/ui/src/constants/constants.ts
+++ openmetadata-ui/src/main/resources/ui/src/constants/constants.ts
@@ -84,6 +84,10 @@ export const NO_DATA_PLACEHOLDER = '--';
 export const PIPE_SYMBOL = '|';
 export const NO_DATA = '-';
 export const STAR_OMD_USER = 'STAR_OMD_USER';
+export const AIRFLOW_HYBRID = 'Hybrid';
+export const COLLATE_SAAS = 'CollateSaaS';
+export const COLLATE_SAAS_RUNNER = 'Collate SaaS Runner';
+export const RUNNER = 'ingestionRunner';
 
 export const TOUR_SEARCH_TERM = 'dim_a';
 export const ERROR500 = t('message.something-went-wrong');
