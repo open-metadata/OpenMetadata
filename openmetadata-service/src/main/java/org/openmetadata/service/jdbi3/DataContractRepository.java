3dd7257e6fb0a8eb6b1e61633269fcc4fa74da2e
Sriharsha Chintalapani <harsha@getcollate.io>
2025-05-16 16:30:28 -0700

    Add @Repository

--- openmetadata-service/src/main/java/org/openmetadata/service/jdbi3/DataContractRepository.java
+++ openmetadata-service/src/main/java/org/openmetadata/service/jdbi3/DataContractRepository.java
@@ -31,9 +31,9 @@ import org.openmetadata.service.util.EntityUtil.Fields;
 public class DataContractRepository extends EntityRepository<DataContract> {
 
   private static final String DATA_CONTRACT_UPDATE_FIELDS =
-      "entity,owners,tags,status,schema,qualityExpectations,contractUpdates";
+      "entity,owners,status,schema,qualityExpectations,contractUpdates";
   private static final String DATA_CONTRACT_PATCH_FIELDS =
-      "entity,owners,tags,status,schema,qualityExpectations,contractUpdates";
+      "entity,owners,status,schema,qualityExpectations,contractUpdates";
 
   public DataContractRepository() {
     super(
