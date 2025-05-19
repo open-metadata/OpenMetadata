9d25764e119772d357e957762999cb2bdac970f5
shrushti2000 <shrushtipolekar@gmail.com>
2025-05-19 13:38:27 +0530

    fix announcement failing test

--- openmetadata-ui/src/main/resources/ui/playwright/utils/entity.ts
+++ openmetadata-ui/src/main/resources/ui/playwright/utils/entity.ts
@@ -847,6 +847,10 @@ export const createAnnouncement = async (
   await announcementForm(page, { ...data, startDate, endDate });
 
   await page.reload();
+  await page.waitForResponse(
+    '/api/v1/feed?type=Announcement&activeAnnouncement=true&entityLink=*'
+  );
+  await page.waitForLoadState('networkidle');
   await page.getByTestId('announcement-card').isVisible();
 
   await expect(page.getByTestId('announcement-title')).toHaveText(data.title);
