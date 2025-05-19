3aef305ae4cba12356c343df180aa8da9f9fe9f6
shrushti2000 <shrushtipolekar@gmail.com>
2025-05-19 23:09:09 +0530

    remove api call wait

--- openmetadata-ui/src/main/resources/ui/playwright/utils/entity.ts
+++ openmetadata-ui/src/main/resources/ui/playwright/utils/entity.ts
@@ -847,9 +847,6 @@ export const createAnnouncement = async (
   await announcementForm(page, { ...data, startDate, endDate });
   await page.reload();
   await page.waitForLoadState('networkidle');
-  await page.waitForResponse(
-    '/api/v1/feed?type=Announcement&activeAnnouncement=true&entityLink=*'
-  );
   await page.getByTestId('announcement-card').isVisible();
 
   await expect(page.getByTestId('announcement-title')).toHaveText(data.title);
