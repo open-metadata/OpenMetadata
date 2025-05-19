71c6af4edfa286da5640e88ad5d5a5b20f8d0647
Pranita <pfulsundar8@gmail.com>
2025-05-19 18:30:03 +0530

    fix test

--- openmetadata-ui/src/main/resources/ui/playwright/e2e/VersionPages/GlossaryVersionPage.spec.ts
+++ openmetadata-ui/src/main/resources/ui/playwright/e2e/VersionPages/GlossaryVersionPage.spec.ts
@@ -224,17 +224,13 @@ test('GlossaryTerm', async ({ page }) => {
 
     await page.waitForLoadState('networkidle');
 
-    await page
-      .locator(
-        '[data-testid="glossary-reviewer"] [data-testid="glossary-reviewer-name"]'
-      )
-      .scrollIntoViewIfNeeded();
+    const diffLocator = page.locator(
+      '[data-testid="glossary-reviewer"] [data-testid="diff-added"]'
+    );
 
-    await expect(
-      page.locator(
-        '[data-testid="glossary-reviewer"] [data-testid="glossary-reviewer-name"]'
-      )
-    ).toBeVisible();
+    await diffLocator.waitFor({ state: 'attached' });
+
+    await expect(diffLocator).toBeVisible();
   });
 
   await cleanup();
