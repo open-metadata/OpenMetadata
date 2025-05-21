b8c8c2e068a6c48c9a5e5b87dde03432f636a60a
gpby <gpby@users.noreply.github.com>
2025-05-21 10:12:15 +0300

    Fixes #20956: Teradata profiler (#21292)
    * add teradata functions
    
    * fix teradata schema
    
    * reformat code
    
    * change random approach for teradata
    
    ---------
    
    Co-authored-by: Teddy <teddy.crepineau@gmail.com>

--- ingestion/src/metadata/profiler/orm/functions/modulo.py
+++ ingestion/src/metadata/profiler/orm/functions/modulo.py
@@ -61,6 +61,7 @@ def _(element, compiler, **kw):
 @compiles(ModuloFn, Dialects.Vertica)
 @compiles(ModuloFn, Dialects.Hana)
 @compiles(ModuloFn, Dialects.Cockroach)
+@compiles(ModuloFn, Dialects.Teradata)
 def _(element, compiler, **kw):
     """Modulo function for specific dialect"""
     value, base = validate_and_compile(element, compiler, **kw)
