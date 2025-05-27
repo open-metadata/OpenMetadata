ad2661dcfe13dd272ffe87b274835cbc70b262c2
Ayush Shah <ayush@getcollate.io>
2025-05-27 11:49:16 +0530

    Fix: Update Snowflake ingestion tests and configuration
    - Refactored import path for TestCaseDefinition in test_cli_snowflake.py.
    - Updated snowflake.yaml to use privateKey and snowflakePrivatekeyPassphrase for authentication instead of password.

--- ingestion/tests/cli_e2e/test_cli_snowflake.py
+++ ingestion/tests/cli_e2e/test_cli_snowflake.py
@@ -24,7 +24,7 @@ from metadata.generated.schema.tests.testCase import TestCaseParameterValue
 from metadata.generated.schema.type.basic import Timestamp
 from metadata.ingestion.api.status import Status
 
-from ...src.metadata.data_quality.api.models import TestCaseDefinition
+from metadata.data_quality.api.models import TestCaseDefinition
 from .base.e2e_types import E2EType
 from .common.test_cli_db import CliCommonDB
 from .common_e2e_sqa_mixins import SQACommonMethods
