b6fbf43f304cd7fb0f5f0f01d08a466c75f30f3a
Shailesh Parmar <shailesh.parmar.webdev@gmail.com>
2025-06-03 19:18:50 +0530

    Improvement: #21455 Improve renderning time for profiler chart for large amount of data (#21525)
    * Improvement: #21455 Improve renderning time for profiler chart for large amount of data
    
    * Update Profiler and Chart components to handle larger datasets by increasing data size limit and removing unnecessary dataKey prop from Brush component.
    
    * addressing comment

--- openmetadata-ui/src/main/resources/ui/src/test/unit/mocks/recharts.mock.js
+++ openmetadata-ui/src/main/resources/ui/src/test/unit/mocks/recharts.mock.js
@@ -14,6 +14,8 @@
 import React from 'react';
 jest.mock('recharts', () => ({
   Bar: jest.fn().mockImplementation(() => <div>Bar</div>),
+  Line: jest.fn().mockImplementation(() => <div>Line</div>),
+  Brush: jest.fn().mockImplementation(() => <div>Brush</div>),
   Area: jest.fn().mockImplementation(() => <div>Area</div>),
   Scatter: jest.fn().mockImplementation(() => <div>Scatter</div>),
   CartesianGrid: jest.fn().mockImplementation(() => <div>CartesianGrid</div>),
@@ -27,6 +29,9 @@ jest.mock('recharts', () => ({
   AreaChart: jest
     .fn()
     .mockImplementation(({ children }) => <div>{children}</div>),
+  LineChart: jest
+    .fn()
+    .mockImplementation(({ children }) => <div>{children}</div>),
   ComposedChart: jest
     .fn()
     .mockImplementation(({ children }) => <div>{children}</div>),
@@ -35,9 +40,9 @@ jest.mock('recharts', () => ({
     .mockImplementation(({ children, ...rest }) => (
       <div {...rest}>{children}</div>
     )),
-  ResponsiveContainer: jest
-    .fn()
-    .mockImplementation(({ children }) => (
-      <div data-testid="responsive-container">{children}</div>
-    )),
+  ResponsiveContainer: jest.fn().mockImplementation(({ children, ...rest }) => (
+    <div data-testid="responsive-container" {...rest}>
+      {children}
+    </div>
+  )),
 }));
