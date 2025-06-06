ff62c93963c000efcde5d8c35a86f9959a43da0d
Ram Narayan Balaji <ramnarayanb3005@gmail.com>
2025-06-06 12:24:40 +0530

    typescript files for edit_certification

--- openmetadata-ui/src/main/resources/ui/src/generated/entity/policies/accessControl/rule.ts
+++ openmetadata-ui/src/main/resources/ui/src/generated/entity/policies/accessControl/rule.ts
@@ -61,6 +61,7 @@ export enum Operation {
     DeleteTestCaseFailedRowsSample = "DeleteTestCaseFailedRowsSample",
     Deploy = "Deploy",
     EditAll = "EditAll",
+    EditCertification = "EditCertification",
     EditCustomFields = "EditCustomFields",
     EditDataProfile = "EditDataProfile",
     EditDescription = "EditDescription",
