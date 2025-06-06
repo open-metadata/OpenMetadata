fc78dfd574b53364fc40e1ce44f20d1c1200b237
Ashish Gupta <ashish@getcollate.io>
2025-06-06 10:31:13 +0530

    fix the playwright flaky test (#21573)

--- openmetadata-ui/src/main/resources/ui/src/components/Dashboard/DataModel/DataModels/DataModelDetails.component.tsx
+++ openmetadata-ui/src/main/resources/ui/src/components/Dashboard/DataModel/DataModels/DataModelDetails.component.tsx
@@ -136,8 +136,7 @@ const DataModelDetails = ({
       showSuccessToast(
         t('message.restore-entities-success', {
           entity: t('label.data-model'),
-        }),
-        2000
+        })
       );
       handleToggleDelete(newVersion);
     } catch (error) {
