19ea74948e1120d1bca55a82e5fc481f475c63ac
harshsoni2024 <harshsoni2024@gmail.com>
2025-06-08 18:43:35 +0530

    db2 custom driver installation

--- ingestion/src/metadata/ingestion/source/database/db2/utils.py
+++ ingestion/src/metadata/ingestion/source/database/db2/utils.py
@@ -12,8 +12,24 @@
 """
 Module to define overriden dialect methods
 """
+from enum import Enum
+
 from sqlalchemy import and_, join, sql
 from sqlalchemy.engine import reflection
+
+from metadata.utils.logger import ingestion_logger
+
+logger = ingestion_logger()
+
+
+class DB2CLIDriverVersions(Enum):
+    v11_1_4 = "11.1.4"
+    v11_5_4 = "11.5.4"
+    v11_5_5 = "11.5.5"
+    v11_5_6 = "11.5.6"
+    v11_5_8 = "11.5.8"
+    v11_5_9 = "11.5.9"
+    v12_1_0 = "12.1.0"
 
 
 @reflection.cache
@@ -61,3 +77,99 @@ def get_unique_constraints(
                 }
             )
     return unique_consts
+
+
+def check_clidriver_version(clidriver_version: str):
+    """
+    Check if the CLI Driver version is valid
+    """
+    if clidriver_version not in [v.value for v in DB2CLIDriverVersions]:
+        logger.warning(f"Invalid CLI Driver version provided: {clidriver_version}")
+        return None
+    return DB2CLIDriverVersions(clidriver_version)
+
+
+def install_clidriver(clidriver_version: str) -> None:
+    """
+    Install the CLI Driver for DB2
+    """
+    # pylint: disable=import-outside-toplevel
+    import os
+    import platform
+    import subprocess
+    import sys
+    from urllib.request import URLError, urlopen
+
+    import pkg_resources
+
+    clidriver_version = f"v{clidriver_version}"
+    system = platform.system().lower()
+    is_64bits = platform.architecture()[0] == "64bit"
+    clidriver_url = None
+    default_clidriver_url = None
+
+    def is_valid_url(url: str) -> bool:
+        """Check if the URL is valid and accessible"""
+        try:
+            urlopen(url)
+            return True
+        except URLError:
+            return False
+
+    if system == "darwin":  # macOS
+        machine = platform.machine().lower()
+        if machine == "arm64":  # Apple Silicon
+            default_clidriver_url = "https://public.dhe.ibm.com/ibmdl/export/pub/software/data/db2/drivers/odbc_cli/macarm64_odbc_cli.tar.gz"
+            clidriver_url = "https://public.dhe.ibm.com/ibmdl/export/pub/software/data/db2/drivers/odbc_cli/macarm64_odbc_cli.tar.gz"
+        elif machine == "x86_64":  # Intel
+            default_clidriver_url = "https://public.dhe.ibm.com/ibmdl/export/pub/software/data/db2/drivers/odbc_cli/macos64_odbc_cli.tar.gz"
+            clidriver_url = f"https://public.dhe.ibm.com/ibmdl/export/pub/software/data/db2/drivers/odbc_cli/{str(clidriver_version)}/macos64_odbc_cli.tar.gz"
+    elif system == "linux":
+        if is_64bits:
+            default_clidriver_url = "https://public.dhe.ibm.com/ibmdl/export/pub/software/data/db2/drivers/odbc_cli/linuxx64_odbc_cli.tar.gz"
+            clidriver_url = f"https://public.dhe.ibm.com/ibmdl/export/pub/software/data/db2/drivers/odbc_cli/{str(clidriver_version)}/linuxx64_odbc_cli.tar.gz"
+        else:
+            default_clidriver_url = "https://public.dhe.ibm.com/ibmdl/export/pub/software/data/db2/drivers/odbc_cli/linuxia32_odbc_cli.tar.gz"
+            clidriver_url = f"https://public.dhe.ibm.com/ibmdl/export/pub/software/data/db2/drivers/odbc_cli/{str(clidriver_version)}/linuxia32_odbc_cli.tar.gz"
+    elif system == "windows":
+        if is_64bits:
+            default_clidriver_url = "https://public.dhe.ibm.com/ibmdl/export/pub/software/data/db2/drivers/odbc_cli/ntx64_odbc_cli.zip"
+            clidriver_url = f"https://public.dhe.ibm.com/ibmdl/export/pub/software/data/db2/drivers/odbc_cli/{str(clidriver_version)}/ntx64_odbc_cli.zip"
+        else:
+            default_clidriver_url = "https://public.dhe.ibm.com/ibmdl/export/pub/software/data/db2/drivers/odbc_cli/nt32_odbc_cli.zip"
+            clidriver_url = f"https://public.dhe.ibm.com/ibmdl/export/pub/software/data/db2/drivers/odbc_cli/{str(clidriver_version)}/nt32_odbc_cli.zip"
+    else:
+        logger.error(
+            f"Unsupported operating system for db2 driver installation: {system}"
+        )
+        return
+
+    # set env variables for CLIDRIVER_VERSION and IBM_DB_INSTALLER_URL
+    os.environ["CLIDRIVER_VERSION"] = clidriver_version
+    if is_valid_url(clidriver_url):
+        os.environ["IBM_DB_INSTALLER_URL"] = clidriver_url
+    else:
+        os.environ["IBM_DB_INSTALLER_URL"] = default_clidriver_url
+    logger.info(f"Set IBM_DB_INSTALLER_URL to {os.environ['IBM_DB_INSTALLER_URL']}")
+    logger.info(f"Set CLIDRIVER_VERSION to {os.environ['CLIDRIVER_VERSION']}")
+    # Uninstall ibm_db if it is already installed
+    try:
+        pkg_resources.get_distribution("ibm_db")
+        # If we get here, ibm_db is installed, so uninstall it first
+        subprocess.check_call(
+            [sys.executable, "-m", "pip", "uninstall", "-y", "ibm_db"]
+        )
+    except pkg_resources.DistributionNotFound:
+        # ibm_db is not installed, proceed with installation
+        pass
+    # Install ibm_db with specific flags
+    subprocess.check_call(
+        [
+            sys.executable,
+            "-m",
+            "pip",
+            "install",
+            "ibm_db~=3.2.6",
+        ]
+    )
+    return None
