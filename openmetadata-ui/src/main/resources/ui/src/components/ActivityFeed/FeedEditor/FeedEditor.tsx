4bbde8f9254cd374ec040bc5ae48c880db0cf9a3
shrushti2000 <shrushtipolekar@gmail.com>
2025-06-11 11:44:19 +0530

    handle outside click for emoji feed editor

--- openmetadata-ui/src/main/resources/ui/src/components/ActivityFeed/FeedEditor/FeedEditor.tsx
+++ openmetadata-ui/src/main/resources/ui/src/components/ActivityFeed/FeedEditor/FeedEditor.tsx
@@ -90,6 +90,32 @@ export const FeedEditor = forwardRef<editorRef, FeedEditorProp>(
 
     const { userProfilePics } = useApplicationStore();
 
+    // handle click outside of emoji container
+    useEffect(() => {
+      const handleClickOutside = (event: MouseEvent) => {
+        const emojiContainer = document.querySelector(
+          '#om-quill-editor #textarea-emoji'
+        ) as HTMLElement;
+        const emojiToggleButton = document.querySelector(
+          '#om-quill-editor .textarea-emoji-control.ql-list'
+        ) as HTMLElement;
+
+        if (
+          emojiContainer &&
+          !emojiContainer.contains(event.target as Node) &&
+          emojiToggleButton &&
+          !emojiToggleButton.contains(event.target as Node)
+        ) {
+          emojiToggleButton.click(); // Simulates a click to close the emoji container
+        }
+      };
+
+      document.addEventListener('mousedown', handleClickOutside);
+
+      return () => {
+        document.removeEventListener('mousedown', handleClickOutside);
+      };
+    }, []);
     const userSuggestionRenderer = async (
       searchTerm: string,
       renderList: (matches: MentionSuggestionsItem[], search: string) => void,
