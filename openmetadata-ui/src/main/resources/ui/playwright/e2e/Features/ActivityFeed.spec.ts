a1e08820b6f63d84927cb9bb5a9bf0cb1eef142d
shrushti2000 <shrushtipolekar@gmail.com>
2025-06-12 00:13:13 +0530

    added  test

--- openmetadata-ui/src/main/resources/ui/playwright/e2e/Features/ActivityFeed.spec.ts
+++ openmetadata-ui/src/main/resources/ui/playwright/e2e/Features/ActivityFeed.spec.ts
@@ -230,6 +230,20 @@ test.describe('Activity feed', () => {
     const commentInput = page.locator('[data-testid="comments-input-field"]');
     commentInput.click();
 
+    await test.step(
+      'Should be able to open and close emoji container',
+      async () => {
+        await page.locator('.textarea-emoji-control').click();
+
+        await expect(page.locator('#textarea-emoji')).toBeVisible();
+
+        // Click on the main content area which is outside the emoji container
+        await page.locator('.center-container').click();
+
+        await expect(page.locator('#textarea-emoji')).not.toBeVisible();
+      }
+    );
+
     await page.fill(
       '[data-testid="editor-wrapper"] .ql-editor',
       `Reply message`
