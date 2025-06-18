88a0219b35742c23da05f2597ea2d45a9c2dadf9
harshsoni2024 <64592571+harshsoni2024@users.noreply.github.com>
2025-06-18 10:52:08 +0530

    MINOR: Tableau proxy url for sourceurl (#21799)

--- ingestion/tests/unit/topology/dashboard/test_tableau.py
+++ ingestion/tests/unit/topology/dashboard/test_tableau.py
@@ -456,3 +456,16 @@ class TableauUnitTest(TestCase):
             ):
                 dashboards = list(self.tableau.get_dashboard())
                 self.assertEqual(len(dashboards), 0)
+
+    def test_generate_dashboard_url(self):
+        """
+        Test that the dashboard url is generated correctly with proxyURL
+        """
+        self.tableau.config.serviceConnection.root.config.proxyURL = (
+            "http://mockTableauServer.com"
+        )
+        result = list(self.tableau.yield_dashboard(MOCK_DASHBOARD))
+        self.assertEqual(
+            result[0].right.sourceUrl.root,
+            "http://mockTableauServer.com/#/site/hidarsite/workbooks/897790/views",
+        )
