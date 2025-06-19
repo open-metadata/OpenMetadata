392ab1e20770f09d1e8814d61ac2caef7a7330f2
Suman Maharana <sumanmaharana786@gmail.com>
2025-06-19 11:18:52 +0530

    Add: Tableau Hierarchy project filter (#21811)

--- ingestion/src/metadata/ingestion/source/dashboard/dashboard_service.py
+++ ingestion/src/metadata/ingestion/source/dashboard/dashboard_service.py
@@ -550,15 +550,25 @@ class DashboardServiceSource(TopologyRunnerMixin, Source, ABC):
                 self.context.get().project_name = (  # pylint: disable=E1128
                     self.get_project_name(dashboard_details=dashboard_details)
                 )
+
+                # Get both single project name and list of project names
+                project_name = self.context.get().project_name
+                project_names = (
+                    self.get_project_names(dashboard_details=dashboard_details) or []
+                )
+                if project_names:
+                    project_name = project_names
+
                 if filter_by_project(
                     self.source_config.projectFilterPattern,
-                    self.context.get().project_name,
+                    project_name,
                 ):
                     self.status.filter(
-                        self.context.get().project_name,
+                        project_name,
                         "Project / Workspace Filtered Out",
                     )
                     continue
+
             except Exception as exc:
                 logger.debug(traceback.format_exc())
                 logger.warning(
@@ -601,7 +611,18 @@ class DashboardServiceSource(TopologyRunnerMixin, Source, ABC):
         Get the project / workspace / folder / collection name of the dashboard
         """
         logger.debug(
-            f"Projects are not supported for {self.service_connection.type.name}"
+            f"Project name is not supported for {self.service_connection.type.name}"
+        )
+        return None
+
+    def get_project_names(  # pylint: disable=unused-argument, useless-return
+        self, dashboard_details: Any
+    ) -> Optional[str]:
+        """
+        Get the project / workspace / folder / collection names of the dashboard
+        """
+        logger.debug(
+            f"Project names are not supported for {self.service_connection.type.name}"
         )
         return None
 
