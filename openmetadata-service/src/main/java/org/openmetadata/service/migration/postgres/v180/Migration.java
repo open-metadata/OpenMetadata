586169905ce2fc060a5cbc9042255e21e0fb12f7
Ajith Prasad <37380177+aji-aju@users.noreply.github.com>
2025-06-23 06:14:46 +0530

    Scim Bot can update display name (#21879)
    * Scim Bot can update display name
    
    * Updated by Bot checked fir editdisplayname
    
    * format
    
    * Fixed TC
    
    * Fixed policy tests
    
    * comment
    
    ---------
    
    Co-authored-by: Sriharsha Chintalapani <harshach@users.noreply.github.com>

--- openmetadata-service/src/main/java/org/openmetadata/service/migration/postgres/v180/Migration.java
+++ openmetadata-service/src/main/java/org/openmetadata/service/migration/postgres/v180/Migration.java
@@ -1,6 +1,7 @@
 package org.openmetadata.service.migration.postgres.v180;
 
 import static org.openmetadata.service.migration.utils.v180.MigrationUtil.addCertificationOperationsToPolicy;
+import static org.openmetadata.service.migration.utils.v180.MigrationUtil.addDenyDisplayNameRuleToBotPolicies;
 
 import lombok.SneakyThrows;
 import org.openmetadata.service.migration.api.MigrationProcessImpl;
@@ -16,5 +17,6 @@ public class Migration extends MigrationProcessImpl {
   @SneakyThrows
   public void runDataMigration() {
     addCertificationOperationsToPolicy(collectionDAO);
+    addDenyDisplayNameRuleToBotPolicies(collectionDAO);
   }
 }
