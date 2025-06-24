084b474a86a146cf44c1de13562ee7e5516c10e7
Sriharsha Chintalapani <harsha@getcollate.io>
2025-06-23 20:29:10 -0700

    Fix #20621: User Status Tracking in the System

--- bootstrap/sql/migrations/native/1.8.1/mysql/schemaChanges.sql
+++ bootstrap/sql/migrations/native/1.8.1/mysql/schemaChanges.sql
@@ -5,14 +5,7 @@ ADD COLUMN lastLoginTime BIGINT UNSIGNED GENERATED ALWAYS AS (json_unquote(json_
 ALTER TABLE user_entity 
 ADD COLUMN lastActivityTime BIGINT UNSIGNED GENERATED ALWAYS AS (json_unquote(json_extract(`json`, '$.lastActivityTime'))) VIRTUAL;
 
--- Create index on lastLoginTime for fast queries
 CREATE INDEX idx_user_entity_last_login_time ON user_entity(lastLoginTime);
-
--- Create index on lastActivityTime for fast queries
 CREATE INDEX idx_user_entity_last_activity_time ON user_entity(lastActivityTime);
-
--- Create composite index for efficient online user queries with deleted flag
 CREATE INDEX idx_user_entity_last_login_deleted ON user_entity(lastLoginTime, deleted);
-
--- Create composite index for efficient active user queries with deleted flag
 CREATE INDEX idx_user_entity_last_activity_deleted ON user_entity(lastActivityTime, deleted);
--- bootstrap/sql/migrations/native/1.8.1/postgres/schemaChanges.sql
+++ bootstrap/sql/migrations/native/1.8.1/postgres/schemaChanges.sql
@@ -1,18 +1,11 @@
--- Add generated columns for lastLoginTime and lastActivityTime to user_entity table for efficient querying of online users
-ALTER TABLE user_entity 
+ALTER TABLE user_entity
 ADD COLUMN lastLoginTime BIGINT GENERATED ALWAYS AS ((json->>'lastLoginTime')::bigint) STORED;
 
 ALTER TABLE user_entity 
 ADD COLUMN lastActivityTime BIGINT GENERATED ALWAYS AS ((json->>'lastActivityTime')::bigint) STORED;
 
--- Create index on lastLoginTime for fast queries
+
 CREATE INDEX idx_user_entity_last_login_time ON user_entity(lastLoginTime);
-
--- Create index on lastActivityTime for fast queries
 CREATE INDEX idx_user_entity_last_activity_time ON user_entity(lastActivityTime);
-
--- Create composite index for efficient online user queries with deleted flag
 CREATE INDEX idx_user_entity_last_login_deleted ON user_entity(lastLoginTime, deleted);
-
--- Create composite index for efficient active user queries with deleted flag
 CREATE INDEX idx_user_entity_last_activity_deleted ON user_entity(lastActivityTime, deleted);
