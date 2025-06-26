7be5c4d63476c25e5aca4cc17822707f9f569a74
Ashish Gupta <ashish@getcollate.io>
2025-06-26 14:23:36 +0530

    fix sonar failure

--- openmetadata-ui/src/main/resources/ui/src/components/NavBar/NavBar.test.tsx
+++ openmetadata-ui/src/main/resources/ui/src/components/NavBar/NavBar.test.tsx
@@ -11,7 +11,6 @@
  *  limitations under the License.
  */
 import { act, render, screen } from '@testing-library/react';
-import React from 'react';
 import {
   LAST_VERSION_FETCH_TIME_KEY,
   ONE_HOUR_MS,
