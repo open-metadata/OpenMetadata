ace3602c3f916f4e479c50f5ec07b5172b44491a
sonikashah <sonikashah94@gmail.com>
2025-06-30 15:51:24 +0530

    fix import

--- openmetadata-service/src/main/java/org/openmetadata/service/jdbi3/UserRepository.java
+++ openmetadata-service/src/main/java/org/openmetadata/service/jdbi3/UserRepository.java
@@ -82,7 +82,6 @@ import org.openmetadata.service.security.policyevaluator.SubjectContext;
 import org.openmetadata.service.util.EntityUtil;
 import org.openmetadata.service.util.EntityUtil.Fields;
 import org.openmetadata.service.util.FullyQualifiedName;
-import org.openmetadata.service.util.JsonUtils;
 import org.openmetadata.service.util.UserUtil;
 
 @Slf4j
