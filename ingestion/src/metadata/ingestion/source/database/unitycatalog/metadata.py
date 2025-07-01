b13139a8edeeb692ef54294bcd4b90e71f913609
Keshav Mohta <keshavmohta09@gmail.com>
2025-07-01 14:16:10 +0530

    fix: checkstyle

--- ingestion/src/metadata/ingestion/source/database/unitycatalog/metadata.py
+++ ingestion/src/metadata/ingestion/source/database/unitycatalog/metadata.py
@@ -334,9 +334,9 @@ class UnitycatalogSource(
         schema_name = self.context.get().database_schema
         db_name = self.context.get().database
         if table.storage_location and not table.storage_location.startswith("dbfs"):
-            self.external_location_map[(db_name, schema_name, table_name)] = (
-                table.storage_location
-            )
+            self.external_location_map[
+                (db_name, schema_name, table_name)
+            ] = table.storage_location
         try:
             columns = list(self.get_columns(table_name, table.columns))
             (
