866ccbfa6864fa1a647928fbeceb5a3eb856c02f
Satish <satish@Satishs-MacBook-Pro.local>
2025-07-04 09:59:11 +0530

    FocusTrap common and first element focus

--- openmetadata-ui/src/main/resources/ui/src/components/Certification/Certification.component.tsx
+++ openmetadata-ui/src/main/resources/ui/src/components/Certification/Certification.component.tsx
@@ -13,7 +13,6 @@
 import { CheckOutlined, CloseOutlined } from '@ant-design/icons';
 import { Button, Card, Popover, Radio, Space, Spin, Typography } from 'antd';
 import { AxiosError } from 'axios';
-import { FocusTrap } from 'focus-trap-react';
 import { useEffect, useMemo, useRef, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { ReactComponent as CertificationIcon } from '../../assets/svg/ic-certification.svg';
@@ -23,9 +22,11 @@ import { getEntityName } from '../../utils/EntityUtils';
 import { stringToHTML } from '../../utils/StringsUtils';
 import { getTagImageSrc } from '../../utils/TagsUtils';
 import { showErrorToast } from '../../utils/ToastUtils';
+import { FocusTrapWithContainer } from '../common/FocusTrap/FocusTrapWithContainer';
 import Loader from '../common/Loader/Loader';
 import { CertificationProps } from './Certification.interface';
 import './certification.less';
+
 const Certification = ({
   currentCertificate = '',
   children,
@@ -159,67 +160,59 @@ const Certification = ({
     <Popover
       className="p-0"
       content={
-        <FocusTrap
-          focusTrapOptions={{
-            fallbackFocus: () =>
-              (document.querySelector(
-                '#certification-card-container'
-              ) as HTMLElement) || document.body,
-          }}>
-          <div id="certification-card-container">
-            <Card
-              bordered={false}
-              className="certification-card"
-              data-testid="certification-cards"
-              title={
-                <Space className="w-full justify-between">
-                  <div className="flex gap-2 items-center w-full">
-                    <CertificationIcon height={18} width={18} />
-                    <Typography.Text className="m-b-0 font-semibold text-sm">
-                      {t('label.edit-entity', {
-                        entity: t('label.certification'),
-                      })}
-                    </Typography.Text>
-                  </div>
-                  <Typography.Text
-                    className="m-b-0 font-semibold text-primary text-sm cursor-pointer"
-                    data-testid="clear-certification"
-                    tabIndex={0}
-                    onClick={() => updateCertificationData()}
-                    onKeyDown={(e) => {
-                      if (e.key === 'Enter' || e.key === ' ') {
-                        e.preventDefault();
-                        updateCertificationData();
-                      }
-                    }}>
-                    {t('label.clear')}
+        <FocusTrapWithContainer>
+          <Card
+            bordered={false}
+            className="certification-card"
+            data-testid="certification-cards"
+            title={
+              <Space className="w-full justify-between">
+                <div className="flex gap-2 items-center w-full">
+                  <CertificationIcon height={18} width={18} />
+                  <Typography.Text className="m-b-0 font-semibold text-sm">
+                    {t('label.edit-entity', {
+                      entity: t('label.certification'),
+                    })}
                   </Typography.Text>
-                </Space>
-              }>
-              <Spin
-                indicator={<Loader size="small" />}
-                spinning={isLoadingCertificationData}>
-                {certificationCardData}
-                <div className="flex justify-end text-lg gap-2 mt-4">
-                  <Button
-                    data-testid="close-certification"
-                    type="default"
-                    onClick={handleCloseCertification}>
-                    <CloseOutlined />
-                  </Button>
-                  <Button
-                    data-testid="update-certification"
-                    type="primary"
-                    onClick={() =>
-                      updateCertificationData(selectedCertification)
-                    }>
-                    <CheckOutlined />
-                  </Button>
                 </div>
-              </Spin>
-            </Card>
-          </div>
-        </FocusTrap>
+                <Typography.Text
+                  className="m-b-0 font-semibold text-primary text-sm cursor-pointer"
+                  data-testid="clear-certification"
+                  tabIndex={0}
+                  onClick={() => updateCertificationData()}
+                  onKeyDown={(e) => {
+                    if (e.key === 'Enter' || e.key === ' ') {
+                      e.preventDefault();
+                      updateCertificationData();
+                    }
+                  }}>
+                  {t('label.clear')}
+                </Typography.Text>
+              </Space>
+            }>
+            <Spin
+              indicator={<Loader size="small" />}
+              spinning={isLoadingCertificationData}>
+              {certificationCardData}
+              <div className="flex justify-end text-lg gap-2 mt-4">
+                <Button
+                  data-testid="close-certification"
+                  type="default"
+                  onClick={handleCloseCertification}>
+                  <CloseOutlined />
+                </Button>
+                <Button
+                  data-testid="update-certification"
+                  type="primary"
+                  onClick={() =>
+                    updateCertificationData(selectedCertification)
+                  }>
+                  <CheckOutlined />
+                </Button>
+              </div>
+            </Spin>
+          </Card>
+        </FocusTrapWithContainer>
       }
       overlayClassName="certification-card-popover"
       placement="bottomRight"
---
+++ openmetadata-ui/src/main/resources/ui/src/components/common/FocusTrap/FocusTrapWithContainer.tsx
@@ -0,0 +1,36 @@
+/*
+ *  Copyright 2023 Collate.
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+import { FocusTrap } from 'focus-trap-react';
+import { useRef } from 'react';
+
+export const FocusTrapWithContainer = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const containerRef = useRef<HTMLDivElement | null>(null);
+
+  return (
+    <FocusTrap
+      focusTrapOptions={{
+        fallbackFocus: () => containerRef.current || document.body,
+        initialFocus: () =>
+          (containerRef.current?.querySelector(
+            'button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])'
+          ) as HTMLElement) || containerRef.current,
+      }}>
+      <div ref={containerRef}>{children}</div>
+    </FocusTrap>
+  );
+};
