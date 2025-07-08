d00a17f922b4ed1693f640d550998f71072be0cd
Harshit Shah <dinkushah169@gmail.com>
2025-07-08 11:49:25 +0530

    Add support for My Task widget (#22161)
    * Add support for My Task widget
    
    * address comments
    
    * minor fix
    
    * fix failing tests

---
+++ openmetadata-ui/src/main/resources/ui/src/components/MyData/Widgets/Common/WidgetFooter/WidgetFooter.tsx
@@ -0,0 +1,58 @@
+/*
+ *  Copyright 2025 Collate.
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+import { ArrowRightOutlined } from '@ant-design/icons';
+import { Button, Divider, Row } from 'antd';
+import { useTranslation } from 'react-i18next';
+import { useApplicationStore } from '../../../../../hooks/useApplicationStore';
+import './widget-footer.less';
+
+export interface WidgetFooterProps {
+  showMoreButton?: boolean;
+  moreButtonText?: string;
+  onMoreClick?: () => void;
+  className?: string;
+}
+
+const WidgetFooter = ({
+  showMoreButton = false,
+  moreButtonText,
+  onMoreClick,
+  className = '',
+}: WidgetFooterProps) => {
+  const { t } = useTranslation();
+  const { currentUser } = useApplicationStore();
+  if (!showMoreButton) {
+    return null;
+  }
+
+  return (
+    <div className={`widget-footer ${className}`} data-testid="widget-footer">
+      {showMoreButton && onMoreClick && (
+        <Row className="widget-footer">
+          <Divider className="mb-0 mt-0" />
+          <Button
+            className="text-primary hover:underline w-full footer-view-more-button"
+            href={`users/${currentUser?.name}/task`}
+            target="_blank"
+            type="link">
+            {moreButtonText || t('label.view-more')}
+            <ArrowRightOutlined data-testid="arrow-right-icon" />
+          </Button>
+        </Row>
+      )}
+    </div>
+  );
+};
+
+export default WidgetFooter;
