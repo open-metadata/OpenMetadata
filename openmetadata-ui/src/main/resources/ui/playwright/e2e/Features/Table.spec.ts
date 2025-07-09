3667d46bfab5bca3095102dad51f5949ad5df8a2
Dhruv Parmar <83108871+dhruvjsx@users.noreply.github.com>
2025-07-09 12:03:03 +0530

    addressed comments

--- openmetadata-ui/src/main/resources/ui/playwright/e2e/Features/Table.spec.ts
+++ openmetadata-ui/src/main/resources/ui/playwright/e2e/Features/Table.spec.ts
@@ -91,7 +91,7 @@ test.describe('Table pagination sorting search scenarios ', () => {
   test('Table page should show schema tab with count', async ({ page }) => {
     await table1.visitEntityPage(page);
 
-    await expect(page.getByRole('tab', { name: 'Schema' })).toContainText('4');
+    await expect(page.getByRole('tab', { name: 'Columns' })).toContainText('4');
   });
 
   test('should persist current page', async ({ page }) => {
