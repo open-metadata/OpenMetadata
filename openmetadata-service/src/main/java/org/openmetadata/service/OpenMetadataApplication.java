fe5451300abcf554ad5ae42da08f0eb4e14fde5d
Sriharsha Chintalapani <harshach@users.noreply.github.com>
2025-07-09 15:36:16 -0700

    Fix Promtheus endpoint (#22233)
    (cherry picked from commit 91ae7c488257b0d17540e4f849478ea961cabfd0)

--- openmetadata-service/src/main/java/org/openmetadata/service/OpenMetadataApplication.java
+++ openmetadata-service/src/main/java/org/openmetadata/service/OpenMetadataApplication.java
@@ -529,6 +529,10 @@ public class OpenMetadataApplication extends Application<OpenMetadataApplication
             return configuration.getWebConfiguration();
           }
         });
+
+    // Add Micrometer bundle for Prometheus metrics
+    bootstrap.addBundle(MicrometerBundleSingleton.getInstance());
+
     super.initialize(bootstrap);
   }
 
