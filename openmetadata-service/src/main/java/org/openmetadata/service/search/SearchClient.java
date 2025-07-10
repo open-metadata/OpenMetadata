ea1b325d4a3929d485233d3f0e5bcaace45a9665
Pere Miquel Brull <peremiquelbrull@gmail.com>
2025-07-10 20:45:46 +0200

    fix indexing updates

--- openmetadata-service/src/main/java/org/openmetadata/service/search/SearchClient.java
+++ openmetadata-service/src/main/java/org/openmetadata/service/search/SearchClient.java
@@ -120,12 +120,24 @@ public interface SearchClient {
           + "ctx._source.owners = params.updatedOwners; "
           + "}";
 
+  String ADD_DOMAINS_SCRIPT =
+      "if (ctx._source.domains == null || ctx._source.domains.isEmpty() || "
+          + "(ctx._source.domains.size() > 0 && ctx._source.domains[0] != null && ctx._source.domains[0].inherited == true)) { "
+          + "ctx._source.domains = params.updatedDomains; "
+          + "}";
+
   String PROPAGATE_TEST_SUITES_SCRIPT = "ctx._source.testSuites = params.testSuites";
 
   String REMOVE_OWNERS_SCRIPT =
       "if (ctx._source.owners != null) { "
           + "ctx._source.owners.removeIf(owner -> owner.inherited == true); "
           + "ctx._source.owners.addAll(params.deletedOwners); "
+          + "}";
+
+  String REMOVE_DOMAINS_SCRIPT =
+      "if (ctx._source.domains != null) { "
+          + "ctx._source.domains.removeIf(domain -> domain.inherited == true); "
+          + "ctx._source.domains.addAll(params.deletedDomains); "
           + "}";
 
   String UPDATE_TAGS_FIELD_SCRIPT =
