8e0db6452b90d019537e42408e662bb6e60830cc
Pere Miquel Brull <peremiquelbrull@gmail.com>
2025-07-14 11:55:02 +0200

    MINOR - Keep presidio logger at ERROR (#22124)
    * MINOR - Keep presidio debug at ERROR
    
    * test
    
    ---------
    
    Co-authored-by: Keshav Mohta <68001229+keshavmohta09@users.noreply.github.com>
    Co-authored-by: Sriharsha Chintalapani <harshach@users.noreply.github.com>

--- ingestion/src/metadata/pii/algorithms/presidio_utils.py
+++ ingestion/src/metadata/pii/algorithms/presidio_utils.py
@@ -13,7 +13,7 @@ Utilities for working with the Presidio Library.
 """
 import inspect
 import logging
-from typing import Iterable, Optional, Type
+from typing import Iterable, Type, Union
 
 import spacy
 from presidio_analyzer import (
@@ -26,7 +26,7 @@ from presidio_analyzer.nlp_engine import SpacyNlpEngine
 from spacy.cli.download import download  # pyright: ignore[reportUnknownVariableType]
 
 from metadata.pii.constants import PRESIDIO_LOGGER, SPACY_EN_MODEL, SUPPORTED_LANG
-from metadata.utils.logger import METADATA_LOGGER, pii_logger
+from metadata.utils.logger import pii_logger
 
 logger = pii_logger()
 
@@ -64,17 +64,10 @@ def build_analyzer_engine(
     return analyzer_engine
 
 
-def set_presidio_logger_level(log_level: Optional[int] = None) -> None:
+def set_presidio_logger_level(log_level: Union[int, str] = logging.ERROR) -> None:
     """
     Set the presidio logger to talk less about internal entities unless we are debugging.
     """
-    if log_level is None:
-        log_level = (
-            logging.INFO
-            if logging.getLogger(METADATA_LOGGER).level == logging.DEBUG
-            else logging.ERROR
-        )
-
     logging.getLogger(PRESIDIO_LOGGER).setLevel(log_level)
 
 
@@ -87,7 +80,6 @@ def _load_spacy_model(model_name: str) -> None:
     try:
         _ = spacy.load(model_name)
     except OSError:
-
         logger.warning(f"Downloading {model_name} language model for the spaCy")
         download(model_name)
         _ = spacy.load(model_name)
