8833ad72f709a50803b7305b2883a7fc1563893e
Mohit Yadav <105265192+mohityadav766@users.noreply.github.com>
2025-07-17 02:56:38 +0530

    Fix Entity Relationship Cleanup to not deleted entities without repo… (#22400)
    * - Fix Entity Relationship Cleanup to not delted entities without repositories
    
    * - Add Migration from Test Case to Test Case Resolution
    
    * - Typo should be resolution id
    
    * Add Tests and Fix Condition
    
    * Update BatchSize

---
+++ openmetadata-service/src/main/java/org/openmetadata/service/migration/mysql/v185/Migration.java
@@ -0,0 +1,20 @@
+package org.openmetadata.service.migration.mysql.v185;
+
+import lombok.SneakyThrows;
+import org.openmetadata.service.migration.api.MigrationProcessImpl;
+import org.openmetadata.service.migration.utils.MigrationFile;
+import org.openmetadata.service.migration.utils.v185.MigrationUtil;
+
+public class Migration extends MigrationProcessImpl {
+
+  public Migration(MigrationFile migrationFile) {
+    super(migrationFile);
+  }
+
+  @Override
+  @SneakyThrows
+  public void runDataMigration() {
+    MigrationUtil migrationUtil = new MigrationUtil(collectionDAO);
+    migrationUtil.createTestCaseToTestCaseResolutionRelation();
+  }
+}
