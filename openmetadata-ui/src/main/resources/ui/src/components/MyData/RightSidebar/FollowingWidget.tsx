787d86eb8a8602c260cf4616bb1e7465d8b1ea3c
Pranita Fulsundar <pfulsundar8@gmail.com>
2025-07-21 20:41:11 +0530

    fix type errors (#22486)
    * fix ts errors
    
    * fix ts error
    
    * fix localisation

--- openmetadata-ui/src/main/resources/ui/src/components/MyData/RightSidebar/FollowingWidget.tsx
+++ openmetadata-ui/src/main/resources/ui/src/components/MyData/RightSidebar/FollowingWidget.tsx
@@ -181,7 +181,7 @@ function FollowingWidget({
   );
 
   const showMoreCount = useMemo(() => {
-    return String(followedData.length > 0 ? followedData.length : '');
+    return followedData.length > 0 ? followedData.length.toString() : '';
   }, [followedData]);
 
   const followingContent = useMemo(() => {
@@ -276,7 +276,7 @@ function FollowingWidget({
               EntityTabs.ACTIVITY_FEED
             )}
             moreButtonText={t('label.view-more-count', {
-              count: showMoreCount,
+              countValue: showMoreCount,
             })}
             showMoreButton={
               Boolean(!isLoadingOwnedData) && !isEmpty(followedData)
