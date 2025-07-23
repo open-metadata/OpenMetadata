2d11e48ecd9066bf2103867cf561e00afbdb38aa
“maulikkotak98” <maulik.k@deuexsolutions.com>
2025-07-23 19:29:41 +0530

    cleanup localstorage utils

--- openmetadata-ui/src/main/resources/ui/src/utils/LocalStorageUtils.ts
+++ openmetadata-ui/src/main/resources/ui/src/utils/LocalStorageUtils.ts
@@ -10,31 +10,3 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-import { OM_SESSION_KEY } from '../hooks/useApplicationStore';
-
-export const getOidcToken = (): string => {
-  return (
-    JSON.parse(localStorage.getItem(OM_SESSION_KEY) ?? '{}')?.oidcIdToken ?? ''
-  );
-};
-
-export const setOidcToken = (token: string) => {
-  const session = JSON.parse(localStorage.getItem(OM_SESSION_KEY) ?? '{}');
-
-  session.oidcIdToken = token;
-  localStorage.setItem(OM_SESSION_KEY, JSON.stringify(session));
-};
-
-export const getRefreshToken = (): string => {
-  return (
-    JSON.parse(localStorage.getItem(OM_SESSION_KEY) ?? '{}')?.refreshTokenKey ??
-    ''
-  );
-};
-
-export const setRefreshToken = (token: string) => {
-  const session = JSON.parse(localStorage.getItem(OM_SESSION_KEY) ?? '{}');
-
-  session.refreshTokenKey = token;
-  localStorage.setItem(OM_SESSION_KEY, JSON.stringify(session));
-};
