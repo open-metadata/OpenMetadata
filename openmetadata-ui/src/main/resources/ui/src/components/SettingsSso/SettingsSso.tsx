5219329c2a421edde82d015cd38504d139391541
shrushti2000 <shrushtipolekar@gmail.com>
2025-07-28 12:58:51 +0530

    update scim component

--- openmetadata-ui/src/main/resources/ui/src/components/SettingsSso/SettingsSso.tsx
+++ openmetadata-ui/src/main/resources/ui/src/components/SettingsSso/SettingsSso.tsx
@@ -10,90 +10,35 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-import { AxiosError } from 'axios';
-import { useEffect, useMemo, useState } from 'react';
+import { useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { GlobalSettingsMenuCategory } from '../../constants/GlobalSettings.constants';
-import { TabSpecificField } from '../../enums/entity.enum';
-import { Bot } from '../../generated/entity/bot';
-import { User } from '../../generated/entity/teams/user';
-import { Include } from '../../generated/type/include';
-import {
-  getBotByName,
-  getUserByName,
-  revokeUserToken,
-} from '../../rest/userAPI';
 import '../../styles/variables.less';
 import { getSettingPageEntityBreadCrumb } from '../../utils/GlobalSettingsUtils';
 import ssoUtilClassBase from '../../utils/SSOUtilClassBase';
-import { showErrorToast } from '../../utils/ToastUtils';
 import TitleBreadcrumb from '../common/TitleBreadcrumb/TitleBreadcrumb.component';
 import PageLayoutV1 from '../PageLayoutV1/PageLayoutV1';
-import AccessTokenCard from '../Settings/Users/AccessTokenCard/AccessTokenCard.component';
 import './SettingsSso.less';
 import SSOConfigurationFormRJSF from './SSOConfigurationFormRJSF';
 
 const SettingsSso = () => {
   const { t } = useTranslation();
-  const [botUserData, setBotUserData] = useState<User | null>(null);
-  const [botData, setBotData] = useState<Bot | null>(null);
 
   const breadcrumb = useMemo(
     () => getSettingPageEntityBreadCrumb(GlobalSettingsMenuCategory.SSO),
     []
   );
 
-  // Get AccessTokenCard component using the base SSO utility class (OSS)
-  const isAccessTokenCardEnabled = ssoUtilClassBase.isAccessTokenCardEnabled();
-
-  const fetchBotsData = async () => {
-    try {
-      const botResponse = await getBotByName('scim-bot', {
-        include: Include.All,
-      });
-
-      const botUserResponse = await getUserByName(
-        botResponse.botUser.fullyQualifiedName || '',
-        {
-          fields: [TabSpecificField.ROLES, TabSpecificField.PROFILE],
-          include: Include.All,
-        }
-      );
-      setBotUserData(botUserResponse);
-      setBotData(botResponse);
-    } catch (error) {
-      showErrorToast(error as AxiosError);
-    }
-  };
-
-  const handleRevokeToken = async () => {
-    try {
-      await revokeUserToken(botData?.botUser.id as string);
-    } catch (error) {
-      showErrorToast(error as AxiosError);
-    }
-  };
-
-  useEffect(() => {
-    fetchBotsData();
-  }, []);
-
-  // Render AccessTokenCard if enabled (same as Collate)
   const renderAccessTokenCard = () => {
-    if (!isAccessTokenCardEnabled) {
+    const AccessTokenCardComponent =
+      ssoUtilClassBase.getAccessTokenCardComponent();
+    if (!AccessTokenCardComponent) {
       return null;
     }
 
     return (
       <div className="m-t-md m-b-md">
-        <AccessTokenCard
-          isBot
-          isSCIMBot
-          botData={botData ?? undefined}
-          botUserData={botUserData ?? undefined}
-          disabled={false}
-          revokeTokenHandlerBot={handleRevokeToken}
-        />
+        <AccessTokenCardComponent />
       </div>
     );
   };
--- openmetadata-ui/src/main/resources/ui/src/utils/SSOUtilClassBase.ts
+++ openmetadata-ui/src/main/resources/ui/src/utils/SSOUtilClassBase.ts
@@ -10,10 +10,15 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
+import { ComponentType } from 'react';
 
 class SSOUtilClassBase {
   public isAccessTokenCardEnabled(): boolean {
-    return false; // Not available in OSS
+    return false;
+  }
+
+  public getAccessTokenCardComponent(): ComponentType<any> | null {
+    return null;
   }
 }
 
