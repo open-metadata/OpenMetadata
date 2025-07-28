1beec621d60392f34df83fd9ed88768bdfd1143d
Keshav Mohta <keshavmohta09@gmail.com>
2025-07-28 13:43:24 +0530

    fix: deleted __init__ file and added SSISConnection again in the workflowclassconverter.java

--- openmetadata-service/src/main/java/org/openmetadata/service/secrets/converter/ClassConverterFactory.java
+++ openmetadata-service/src/main/java/org/openmetadata/service/secrets/converter/ClassConverterFactory.java
@@ -47,6 +47,7 @@ import org.openmetadata.schema.services.connections.mlmodel.VertexAIConnection;
 import org.openmetadata.schema.services.connections.pipeline.AirflowConnection;
 import org.openmetadata.schema.services.connections.pipeline.MatillionConnection;
 import org.openmetadata.schema.services.connections.pipeline.NifiConnection;
+import org.openmetadata.schema.services.connections.pipeline.SSISConnection;
 import org.openmetadata.schema.services.connections.search.ElasticSearchConnection;
 import org.openmetadata.schema.services.connections.security.RangerConnection;
 import org.openmetadata.schema.services.connections.storage.GCSConnection;
@@ -99,6 +100,7 @@ public final class ClassConverterFactory {
             Map.entry(VertexAIConnection.class, new VertexAIConnectionClassConverter()));
     Map.entry(Workflow.class, new WorkflowClassConverter());
     Map.entry(CassandraConnection.class, new CassandraConnectionClassConverter());
+    Map.entry(SSISConnection.class, new SsisConnectionClassConverter());
     Map.entry(RangerConnection.class, new RangerConnectionClassConverter());
   }
 
