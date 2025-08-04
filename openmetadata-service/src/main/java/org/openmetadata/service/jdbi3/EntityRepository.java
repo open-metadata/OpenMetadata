c7257912477a6ec8bdcd9670bb707d0927b6fa80
Ram Narayan Balaji <81347100+yan-3005@users.noreply.github.com>
2025-08-04 18:11:39 +0530

    Fix #22623 Fetch entities for import csv should exclude only the fields that are coming from the CSV (#22663)
    * Fetch entities for import csv should exclude only the fields that are coming from the CSV
    
    * Updated DocStrings with Comments!
    
    (cherry picked from commit 8d7db583049d8f93e0c1330255a2c43588f6eaff)

--- openmetadata-service/src/main/java/org/openmetadata/service/jdbi3/EntityRepository.java
+++ openmetadata-service/src/main/java/org/openmetadata/service/jdbi3/EntityRepository.java
@@ -787,6 +787,19 @@ public abstract class EntityRepository<T extends EntityInterface> {
     return entities;
   }
 
+  // Get Entity By Name excluding certain fields
+  public final T getByNameWithExcludedFields(
+      UriInfo uriInfo, String fqn, String excludeFields, Include include) {
+    return getByNameWithExcludedFields(uriInfo, fqn, excludeFields, include, false);
+  }
+
+  // Form the Field Object by excluding certain fields and get entity by name
+  public final T getByNameWithExcludedFields(
+      UriInfo uriInfo, String fqn, String excludeFields, Include include, boolean fromCache) {
+    Fields fields = EntityUtil.Fields.createWithExcludedFields(allowedFields, excludeFields);
+    return getByName(uriInfo, fqn, fields, include, fromCache);
+  }
+
   public final T findByNameOrNull(String fqn, Include include) {
     try {
       return findByName(fqn, include);
