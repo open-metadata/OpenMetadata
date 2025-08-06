a403879f868199729a632164d0c017afdfe91a38
Akash Verma <138790903+akashverma0786@users.noreply.github.com>
2025-08-06 22:01:41 +0530

    feature: googlesheet connector (#22464)
    * feature: googlesheet connector
    
    * updates
    
    * minor change
    
    * java checkstyle
    
    * googledrive files
    
    * fix
    
    * make the google sheets work
    
    * fix directory and files
    
    * remove ui
    
    * update generated types
    
    ---------
    
    Co-authored-by: Akash Verma <akashverma@Akashs-MacBook-Pro-2.local>
    Co-authored-by: sonika-shah <58761340+sonika-shah@users.noreply.github.com>
    Co-authored-by: Akash Verma <akashverma@Mac.lan>
    Co-authored-by: ulixius9 <mayursingal9@gmail.com>

--- ingestion/src/metadata/ingestion/ometa/routes.py
+++ ingestion/src/metadata/ingestion/ometa/routes.py
@@ -41,6 +41,8 @@ from metadata.generated.schema.api.data.createDatabaseSchema import (
 from metadata.generated.schema.api.data.createDataContract import (
     CreateDataContractRequest,
 )
+from metadata.generated.schema.api.data.createDirectory import CreateDirectoryRequest
+from metadata.generated.schema.api.data.createFile import CreateFileRequest
 from metadata.generated.schema.api.data.createGlossary import CreateGlossaryRequest
 from metadata.generated.schema.api.data.createGlossaryTerm import (
     CreateGlossaryTermRequest,
@@ -55,11 +57,15 @@ from metadata.generated.schema.api.data.createQueryCostRecord import (
 from metadata.generated.schema.api.data.createSearchIndex import (
     CreateSearchIndexRequest,
 )
+from metadata.generated.schema.api.data.createSpreadsheet import (
+    CreateSpreadsheetRequest,
+)
 from metadata.generated.schema.api.data.createStoredProcedure import (
     CreateStoredProcedureRequest,
 )
 from metadata.generated.schema.api.data.createTable import CreateTableRequest
 from metadata.generated.schema.api.data.createTopic import CreateTopicRequest
+from metadata.generated.schema.api.data.createWorksheet import CreateWorksheetRequest
 from metadata.generated.schema.api.docStore.createDocument import CreateDocumentRequest
 from metadata.generated.schema.api.domains.createDataProduct import (
     CreateDataProductRequest,
@@ -137,6 +143,8 @@ from metadata.generated.schema.entity.data.dashboardDataModel import DashboardDa
 from metadata.generated.schema.entity.data.database import Database
 from metadata.generated.schema.entity.data.databaseSchema import DatabaseSchema
 from metadata.generated.schema.entity.data.dataContract import DataContract
+from metadata.generated.schema.entity.data.directory import Directory
+from metadata.generated.schema.entity.data.file import File
 from metadata.generated.schema.entity.data.glossary import Glossary
 from metadata.generated.schema.entity.data.glossaryTerm import GlossaryTerm
 from metadata.generated.schema.entity.data.metric import Metric
@@ -146,9 +154,11 @@ from metadata.generated.schema.entity.data.query import Query
 from metadata.generated.schema.entity.data.queryCostRecord import QueryCostRecord
 from metadata.generated.schema.entity.data.report import Report
 from metadata.generated.schema.entity.data.searchIndex import SearchIndex
+from metadata.generated.schema.entity.data.spreadsheet import Spreadsheet
 from metadata.generated.schema.entity.data.storedProcedure import StoredProcedure
 from metadata.generated.schema.entity.data.table import Table
 from metadata.generated.schema.entity.data.topic import Topic
+from metadata.generated.schema.entity.data.worksheet import Worksheet
 from metadata.generated.schema.entity.docStore.document import Document
 from metadata.generated.schema.entity.domains.dataProduct import DataProduct
 from metadata.generated.schema.entity.domains.domain import Domain
@@ -217,6 +227,15 @@ ROUTES = {
     CreateAPICollectionRequest.__name__: "/apiCollections",
     Document.__name__: "/docStore",
     CreateDocumentRequest.__name__: "/docStore",
+    # Drive
+    Directory.__name__: "/drives/directories",
+    CreateDirectoryRequest.__name__: "/drives/directories",
+    Spreadsheet.__name__: "/drives/spreadsheets",
+    CreateSpreadsheetRequest.__name__: "/drives/spreadsheets",
+    File.__name__: "/drives/files",
+    CreateFileRequest.__name__: "/drives/files",
+    Worksheet.__name__: "/drives/worksheets",
+    CreateWorksheetRequest.__name__: "/drives/worksheets",
     # Classifications
     Tag.__name__: "/tags",
     CreateTagRequest.__name__: "/tags",
@@ -246,6 +265,8 @@ ROUTES = {
     Workflow.__name__: "/automations/workflows",
     CreateWorkflowRequest.__name__: "/automations/workflows",
     # Services
+    DriveService.__name__: "/services/driveServices",
+    CreateDriveServiceRequest.__name__: "/services/driveServices",
     ApiService.__name__: "/services/apiServices",
     CreateApiServiceRequest.__name__: "/services/apiServices",
     DatabaseService.__name__: "/services/databaseServices",
