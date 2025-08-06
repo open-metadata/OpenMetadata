d31e2d8ba0adc1ad732dca31c59aae040d31628d
Ariel Schulz <43442541+ArBridgeman@users.noreply.github.com>
2025-08-06 20:19:38 +0200

    Feature/1 fix and add lineage to exasol connector (#21399)
    * Add lineage to Exasol connector
    
    * Update test_connection to return TestConnectionResult
    
    * Add exasol tests & dependencies to tests in setup.py
    
    * Opensearch is required for testing, so add it there
    
    * Modify metadata
    
    * Update documentation for lineage
    
    * Apply formatting changes to code
    
    * Apply make py_format

--- ingestion/src/metadata/ingestion/source/database/exasol/query_parser.py
+++ ingestion/src/metadata/ingestion/source/database/exasol/query_parser.py
@@ -1,6 +1,5 @@
-from typing import Optional, cast
-
-from sqlalchemy.engine.reflection import Inspector
+from abc import ABC
+from typing import Optional
 
 from metadata.generated.schema.entity.services.connections.database.exasolConnection import (
     ExasolConnection,
@@ -10,22 +9,23 @@ from metadata.generated.schema.metadataIngestion.workflow import (
 )
 from metadata.ingestion.api.steps import InvalidSourceException
 from metadata.ingestion.ometa.ometa_api import OpenMetadata
-from metadata.ingestion.source.database.common_db_source import CommonDbSourceService
-from metadata.utils.sqlalchemy_utils import get_all_table_ddls, get_table_ddl
+from metadata.ingestion.source.database.query_parser_source import QueryParserSource
+from metadata.utils.logger import ingestion_logger
 
-Inspector.get_all_table_ddls = get_all_table_ddls
-Inspector.get_table_ddl = get_table_ddl
+logger = ingestion_logger()
 
 
-class ExasolSource(CommonDbSourceService):
+class ExasolQueryParserSource(QueryParserSource, ABC):
+    """
+    Exasol base for Usage and Lineage
+    """
+
     @classmethod
     def create(
         cls, config_dict, metadata: OpenMetadata, pipeline_name: Optional[str] = None
     ):
         config: WorkflowSource = WorkflowSource.model_validate(config_dict)
-        if config.serviceConnection is None:
-            raise InvalidSourceException("Missing service connection")
-        connection = cast(ExasolConnection, config.serviceConnection.root.config)
+        connection: ExasolConnection = config.serviceConnection.root.config
         if not isinstance(connection, ExasolConnection):
             raise InvalidSourceException(
                 f"Expected ExasolConnection, but got {connection}"
