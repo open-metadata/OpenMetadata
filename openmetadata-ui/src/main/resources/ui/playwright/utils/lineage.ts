5e7e518f305b0291eddb4fc8cca4505b511e82cf
Shailesh Parmar <shailesh.parmar.webdev@gmail.com>
2025-08-18 14:41:01 +0530

    Upgrade playwright version to 1.51.1 (#22884)
    * Upgrade playwright version to 1.54.2 (latest) to support indexedDb storageState (#22879)
    
    * bump playwright version to latest
    
    * update playwright version in workflow files
    
    * update playwright installation step in workflow files to ensure consistent working directory
    
    ---------
    
    Co-authored-by: “maulikkotak98” <“maulik.k@deuexsolutions.com”>
    
    * update playwright version to 1.51.1
    
    * fix tests for lineage
    
    * minor refactor and cleanup
    
    * fix lineage tests
    
    ---------
    
    Co-authored-by: Maulik Kotak <72746599+maulikkotak98@users.noreply.github.com>
    Co-authored-by: “maulikkotak98” <“maulik.k@deuexsolutions.com”>
    Co-authored-by: Aniket Katkar <aniketkatkar97@gmail.com>
    (cherry picked from commit bb9b395243fe166c37d6c7083a1fe01f9223c4b4)

--- openmetadata-ui/src/main/resources/ui/playwright/utils/lineage.ts
+++ openmetadata-ui/src/main/resources/ui/playwright/utils/lineage.ts
@@ -123,7 +123,7 @@ export const deleteEdge = async (
 
   await page.locator('[data-testid="add-pipeline"]').dispatchEvent('click');
 
-  await expect(page.locator('[role="dialog"]')).toBeVisible();
+  await expect(page.locator('[role="dialog"]').first()).toBeVisible();
 
   await page
     .locator(
@@ -131,7 +131,7 @@ export const deleteEdge = async (
     )
     .dispatchEvent('click');
 
-  await expect(page.locator('[role="dialog"]')).toBeVisible();
+  await expect(page.locator('[role="dialog"]').first()).toBeVisible();
 
   const deleteRes = page.waitForResponse('/api/v1/lineage/**');
   await page
