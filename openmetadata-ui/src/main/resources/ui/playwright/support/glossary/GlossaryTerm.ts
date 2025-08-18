634d8c3b6ec35717f9f24769cebcc8a3c7a65fc4
Dhruv Parmar <83108871+dhruvjsx@users.noreply.github.com>
2025-08-18 19:40:06 +0530

    fixed glossary fialing playwright

--- openmetadata-ui/src/main/resources/ui/playwright/support/glossary/GlossaryTerm.ts
+++ openmetadata-ui/src/main/resources/ui/playwright/support/glossary/GlossaryTerm.ts
@@ -43,7 +43,8 @@ export class GlossaryTerm extends EntityClass {
     if (parent) {
       this.data.parent = parent;
     }
-
+    // eslint-disable-next-line no-useless-escape
+    this.data.fullyQualifiedName = `\"${this.data.glossary}\".\"${this.data.name}\"`;
     this.data.name = name ?? this.data.name;
     this.data.displayName = name ?? this.data.displayName;
     this.data.reviewers = glossary.data.reviewers;
