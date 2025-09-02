682aa635e9d63ff5b38f7098ddc8af2ccde44510
Teddy Crepineau <teddy.crepineau@gmail.com>
2025-09-02 16:14:19 +0200

    chore: added migrations to 1.9.7

--- bootstrap/sql/migrations/native/1.9.6/mysql/schemaChanges.sql
+++ bootstrap/sql/migrations/native/1.9.6/mysql/schemaChanges.sql
@@ -1,7 +1,3 @@
--- Modify the path to the auto-generated operation column to extract from the JSON field
-ALTER TABLE profiler_data_time_series
-MODIFY COLUMN operation VARCHAR(256) GENERATED ALWAYS AS (json ->> '$.profileData.operation') NULL;
-
 -- Add displayName virtual column to glossary_term_entity for efficient search
 ALTER TABLE glossary_term_entity ADD COLUMN displayName VARCHAR(256) GENERATED ALWAYS AS (json ->> '$.displayName') STORED;
 
--- bootstrap/sql/migrations/native/1.9.6/postgres/schemaChanges.sql
+++ bootstrap/sql/migrations/native/1.9.6/postgres/schemaChanges.sql
@@ -1,14 +1,3 @@
--- Modify the path to the auto-generated operation column to extract from the JSON field
--- 1. Drop the unique constraint first
-ALTER TABLE profiler_data_time_series
-DROP CONSTRAINT IF EXISTS profiler_data_time_series_unique_hash_extension_ts;
--- 2. Drop the generated column
-ALTER TABLE profiler_data_time_series
-DROP COLUMN operation;
--- 3. Add the column back with new expression
-ALTER TABLE profiler_data_time_series
-ADD COLUMN operation VARCHAR(256) GENERATED ALWAYS AS (json -> 'profileData' ->> 'operation') STORED;
-
 -- Add displayName virtual column to glossary_term_entity for efficient search
 ALTER TABLE glossary_term_entity ADD COLUMN IF NOT EXISTS displayName VARCHAR(256) GENERATED ALWAYS AS (json ->> 'displayName') STORED;
 
