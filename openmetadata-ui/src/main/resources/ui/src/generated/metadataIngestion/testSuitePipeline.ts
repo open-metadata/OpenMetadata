9303602dfca6349a2155aae7273bd12a15c09fe8
Akash Verma <138790903+akashverma0786@users.noreply.github.com>
2025-09-02 11:00:23 +0530

    Feature: ServiceNow Connector (#23093)
    * service now connector files
    
    * updated schema and ui file
    
    ---------
    
    Co-authored-by: Akash Verma <akashverma@Mac.lan>
    (cherry picked from commit a41d0404c4cba560dd7bca4e75a4507499777fad)

--- openmetadata-ui/src/main/resources/ui/src/generated/metadataIngestion/testSuitePipeline.ts
+++ openmetadata-ui/src/main/resources/ui/src/generated/metadataIngestion/testSuitePipeline.ts
@@ -227,6 +227,8 @@ export interface ServiceConnection {
  *
  * Epic FHIR Connection Config
  *
+ * ServiceNow Connection Config
+ *
  * Kafka Connection Config
  *
  * Redpanda Connection Config
@@ -507,6 +509,8 @@ export interface ConfigObject {
      *
      * Host and port of the Cockrooach service.
      *
+     * ServiceNow instance URL (e.g., https://your-instance.service-now.com)
+     *
      * Host and port of the Amundsen Neo4j Connection. This expect a URI format like:
      * bolt://localhost:7687.
      *
@@ -593,6 +597,8 @@ export interface ConfigObject {
      * Password to connect to Exasol.
      *
      * Password
+     *
+     * Password to connect to ServiceNow.
      *
      * password to connect to the Amundsen Neo4j Connection.
      *
@@ -700,6 +706,9 @@ export interface ConfigObject {
      * metadata in Cockroach.
      *
      * Username
+     *
+     * Username to connect to ServiceNow. This user should have read access to sys_db_object and
+     * sys_dictionary tables.
      *
      * username to connect to the Amundsen Neo4j Connection.
      *
@@ -1383,6 +1392,16 @@ export interface ConfigObject {
      * FHIR specification version (R4, STU3, DSTU2)
      */
     fhirVersion?: FHIRVersion;
+    /**
+     * If true, ServiceNow application scopes will be imported as database schemas. Otherwise, a
+     * single default schema will be used.
+     */
+    includeScopes?: boolean;
+    /**
+     * If true, both admin and system tables (sys_* tables) will be fetched. If false, only
+     * admin tables will be fetched.
+     */
+    includeSystemTables?: boolean;
     /**
      * basic.auth.user.info schema registry config property, Client HTTP credentials in the form
      * of username:password.
@@ -4025,6 +4044,7 @@ export enum ConfigType {
     Salesforce = "Salesforce",
     SapERP = "SapErp",
     SapHana = "SapHana",
+    ServiceNow = "ServiceNow",
     SharePoint = "SharePoint",
     Sigma = "Sigma",
     SingleStore = "SingleStore",
