2b74e50ca09e232931d8a09c4e308d8fd73def02
SumanMaharana <sumanmaharana786@gmail.com>
2025-09-04 02:52:50 +0530

    Fix Kafkaconnect lineage & descriptions

--- ingestion/src/metadata/ingestion/source/pipeline/kafkaconnect/client.py
+++ ingestion/src/metadata/ingestion/source/pipeline/kafkaconnect/client.py
@@ -23,7 +23,6 @@ from metadata.generated.schema.entity.services.connections.pipeline.kafkaConnect
 from metadata.ingestion.source.pipeline.kafkaconnect.models import (
     KafkaConnectDatasetDetails,
     KafkaConnectPipelineDetails,
-    KafkaConnectTasks,
     KafkaConnectTopics,
 )
 from metadata.utils.helpers import clean_uri
@@ -56,6 +55,20 @@ class KafkaConnectClient:
         if config.KafkaConnectConfig:
             auth = f"{config.KafkaConnectConfig.username}:{config.KafkaConnectConfig.password.get_secret_value()}"
         self.client = KafkaConnect(url=url, auth=auth, ssl_verify=ssl_verify)
+
+    def _enrich_connector_details(
+        self, connector_details: KafkaConnectPipelineDetails, connector_name: str
+    ) -> None:
+        """Helper method to enrich connector details with additional information."""
+        connector_details.topics = self.get_connector_topics(connector=connector_name)
+        connector_details.config = self.get_connector_config(connector=connector_name)
+        if connector_details.config:
+            connector_details.description = connector_details.config.get(
+                "description", None
+            )
+            connector_details.dataset = self.get_connector_dataset_info(
+                connector_details.config
+            )
 
     def get_cluster_info(self) -> Optional[dict]:
         """
@@ -101,13 +114,10 @@ class KafkaConnectClient:
         Get the list of connector plugins.
         """
         try:
-            result = self.client.list_connector_plugins()
-            return result
+            return self.client.list_connector_plugins()
         except Exception as exc:
             logger.debug(traceback.format_exc())
             logger.warning(f"Unable to get connector plugins  {exc}")
-
-        return None
 
     def get_connector_config(self, connector: str) -> Optional[dict]:
         """
@@ -125,7 +135,9 @@ class KafkaConnectClient:
 
         return None
 
-    def get_connector_dataset_info(self, connector: str) -> Optional[dict]:
+    def get_connector_dataset_info(
+        self, connector_config: dict
+    ) -> Optional[KafkaConnectDatasetDetails]:
         """
         Get the details of dataset of connector if there is any.
         Checks in the connector configurations for dataset fields
@@ -139,64 +151,19 @@ class KafkaConnectClient:
                         is not found, has no dataset, or an error occurs.
         """
         try:
-            conn_config = self.get_connector_config(connector=connector)
-
-            if not conn_config:
+            if not connector_config:
                 return None
 
             result = {}
             for dataset in SUPPORTED_DATASETS or []:
                 for key in SUPPORTED_DATASETS[dataset] or []:
-                    if conn_config.get(key):
-                        result[dataset] = conn_config[key]
+                    if connector_config.get(key):
+                        result[dataset] = connector_config[key]
                         return KafkaConnectDatasetDetails(**result)
 
         except Exception as exc:
             logger.debug(traceback.format_exc())
             logger.warning(f"Unable to get connector dataset details {exc}")
-
-        return None
-
-    def get_connector_tasks(self, connector: str) -> Optional[List[KafkaConnectTasks]]:
-        """
-        Get the list of tasks for a connector.
-        Args:
-            connector (str): The name of the connector.
-        Returns:
-            Optional[List[KafkaConnectTasks]]: A list of KafkaConnectTasks objects
-                                            representing the connector's tasks,
-                                            or None if the connector is not found
-                                            or an error occurs.
-        """
-        try:
-            result = self.client.get_connector_status(connector=connector)
-            tasks = [KafkaConnectTasks(**task) for task in result.get("tasks") or []]
-            return tasks
-        except Exception as exc:
-            logger.debug(traceback.format_exc())
-            logger.error(f"Unable to get connector tasks list {exc}")
-
-        return None
-
-    def get_connector_task_status(self, connector: str, task_id: int) -> Optional[dict]:
-        """
-        Get the status of a specific task for a connector.
-        Args:
-            connector (str): The name of the connector.
-            task_id (int): The ID of the task.
-        Returns:
-            Optional[Dict]: A dictionary containing the task status information,
-                        or None if the connector or task is not found
-                        or an error occurs.
-        """
-        try:
-            result = self.client.get_connector_task_status(
-                connector=connector, task_id=task_id
-            )
-            return result
-        except Exception as exc:
-            logger.debug(traceback.format_exc())
-            logger.error(f"Unable to get connector tasks status {exc}")
 
         return None
 
@@ -231,45 +198,27 @@ class KafkaConnectClient:
 
         return None
 
-    def get_connector_state(self, connector: str) -> Optional[str]:
-        """
-        Get the status of a single connector.
-        Args:
-            connector (str): The name of the connector.
-        """
-        try:
-            result = self.client.get_connector_status(connector=connector)
-            if result.get("connector"):
-                return result["connector"].get("state")
-        except Exception as exc:
-            logger.debug(traceback.format_exc())
-            logger.warning(f"Unable to get connector state due to {exc}")
-
-        return None
-
     def get_connector_list(self) -> Optional[List[KafkaConnectPipelineDetails]]:
         """
-        Get the information of a single connector.
-        Args:
-            connector (str): The name of the connector.
+        Get the information of all connectors.
         Returns:
-            Optional[KafkaConnectPipelineDetails]: A KafkaConnectPipelineDetails
-                                            object containing connector information,
-                                            or None if the connector is not found
-                                            or an error occurs.
+            Optional[List[KafkaConnectPipelineDetails]]: A list of KafkaConnectPipelineDetails
+                                            objects containing connector information,
+                                            or None if an error occurs.
         """
         try:
-            connectors = []
-            for connector in self.get_connectors() or []:
-                result = self.client.get_connector_status(connector=connector)
-                connector_details = KafkaConnectPipelineDetails(**result)
-                connector_details.status = self.get_connector_state(connector=connector)
-                connector_details.tasks = self.get_connector_tasks(connector=connector)
-                connector_details.topics = self.get_connector_topics(
-                    connector=connector
-                )
-                connectors.append(connector_details)
-            return connectors
+            connector_data = self.get_connectors(expand="status") or {}
+
+            for connector_name, connector_info in connector_data.items():
+                if isinstance(connector_info, dict) and "status" in connector_info:
+                    status_info = connector_info["status"]
+                    connector_details = KafkaConnectPipelineDetails(**status_info)
+                    connector_details.status = status_info.get("connector", {}).get(
+                        "state", "UNASSIGNED"
+                    )
+                    self._enrich_connector_details(connector_details, connector_name)
+                if connector_details:
+                    yield connector_details
         except Exception as exc:
             logger.debug(traceback.format_exc())
             logger.error(f"Unable to get connector information {exc}")
