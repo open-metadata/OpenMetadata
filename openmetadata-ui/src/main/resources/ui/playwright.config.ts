ed98b572fe96f9606cd8a1933cd7efc8b1b61270
Shailesh Parmar <shailesh.parmar.webdev@gmail.com>
2025-09-11 18:05:20 +0530

    adding debug log

--- openmetadata-ui/src/main/resources/ui/playwright.config.ts
+++ openmetadata-ui/src/main/resources/ui/playwright.config.ts
@@ -53,7 +53,7 @@ export default defineConfig({
     baseURL: process.env.PLAYWRIGHT_TEST_BASE_URL || 'http://localhost:8585',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'retain-on-failure',
     /* Screenshot on failure. */
     screenshot: 'only-on-failure',
   },
@@ -72,6 +72,7 @@ export default defineConfig({
       dependencies: ['setup'],
       grepInvert: /data-insight/,
       testIgnore: ['**/nightly/**'],
+      testMatch: ['**/IncidentManager.spec.ts'],
     },
     {
       name: 'data-insight-application',
