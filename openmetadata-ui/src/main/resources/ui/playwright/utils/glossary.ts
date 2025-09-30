acf16595c657dd328ecc68b9f6e4ca14494cb249
Dhruv Parmar <83108871+dhruvjsx@users.noreply.github.com>
2025-09-30 13:22:12 +0530

    Fix(ui): glossary pagination issue (#23438)
    * fixed glossary term pagination issues
    
    * fixed expand all recursive api calls
    
    * removed unused code
    
    * fixed expand all button
    
    * fixed glossary tests
    
    * fixed glossary review issues
    
    * fixed search issue
    
    * fixed playwright failure
    
    * addressed comments
    
    (cherry picked from commit 98c937ddde738992862644e2e8e3e19c0031cf24)

--- openmetadata-ui/src/main/resources/ui/playwright/utils/glossary.ts
+++ openmetadata-ui/src/main/resources/ui/playwright/utils/glossary.ts
@@ -1684,9 +1684,7 @@ export const setupGlossaryDenyPermissionTest = async (
 };
 
 export const performExpandAll = async (page: Page) => {
-  const termRes = page.waitForResponse(
-    '/api/v1/glossaryTerms?directChildrenOf=*&fields=childrenCount%2Cowners%2Creviewers*'
-  );
+  const termRes = page.waitForResponse('/api/v1/glossaryTerms?*');
   await page.getByTestId('expand-collapse-all-button').click();
   await termRes;
 
