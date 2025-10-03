7a258a7b040d805de8d9d1fd108ae5ae380790c9
Keshav Mohta <keshavmohta09@gmail.com>
2025-10-03 12:53:48 +0530

    fix: test unity catalog tags as None

--- ingestion/tests/unit/topology/database/test_unity_catalog.py
+++ ingestion/tests/unit/topology/database/test_unity_catalog.py
@@ -483,6 +483,7 @@ EXPTECTED_TABLE = [
                     dataType=DataType.INT,
                     dataLength=1,
                     dataTypeDisplay="int",
+                    tags=None,
                 ),
                 Column(
                     name=ColumnName(root="array_data"),
@@ -490,18 +491,21 @@ EXPTECTED_TABLE = [
                     arrayDataType=DataType.INT,
                     dataLength=1,
                     dataTypeDisplay="array<int>",
+                    tags=None,
                 ),
                 Column(
                     name=ColumnName(root="map_data"),
                     dataType=DataType.MAP,
                     dataLength=1,
                     dataTypeDisplay="map<string,int>",
+                    tags=None,
                 ),
                 Column(
                     name=ColumnName(root="struct_data"),
                     dataType=DataType.STRUCT,
                     dataLength=1,
                     dataTypeDisplay="struct<a:int,b:string,c:array<string>,d:struct<abc:int>>",
+                    tags=None,
                     children=[
                         Column(
                             name=ColumnName(root="a"),
