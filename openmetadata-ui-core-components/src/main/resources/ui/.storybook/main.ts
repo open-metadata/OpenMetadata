9258c3b1bdeb9422b0f0e6b59e8d5a3fd80c8b59
Ushran Gouhar <ushran987@gmail.com>
2025-10-06 19:37:05 +0530

    Initialize storybook

---
+++ openmetadata-ui-core-components/src/main/resources/ui/.storybook/main.ts
@@ -0,0 +1,17 @@
+import type { StorybookConfig } from '@storybook/react-vite';
+
+const config: StorybookConfig = {
+  "stories": [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-docs",
+    "@storybook/addon-onboarding"
+  ],
+  "framework": {
+    "name": "@storybook/react-vite",
+    "options": {}
+  }
+};
+export default config;
