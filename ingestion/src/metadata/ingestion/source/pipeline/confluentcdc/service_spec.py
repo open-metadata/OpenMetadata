3ac7e13eb651956f94a7dff4b0f1a3c28f8d4b4a
Sriharsha Chintalapani <harsha@getcollate.io>
2025-10-07 22:34:37 -0400

    Kafka Connect: Support Confluent Cloud

--- ingestion/src/metadata/ingestion/source/pipeline/confluentcdc/service_spec.py
+++
@@ -1,4 +0,0 @@
-from metadata.ingestion.source.pipeline.confluentcdc.metadata import ConfluentcdcSource
-from metadata.utils.service_spec import BaseSpec
-
-ServiceSpec = BaseSpec(metadata_source_class=ConfluentcdcSource)
