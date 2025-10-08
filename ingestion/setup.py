3d015d8c142ca3efb19c9d3ec631d78e3768ca45
Mohit Tilala <tilalamohit123@gmail.com>
2025-10-08 10:47:51 +0530

    Revert "Bump datamodel-code-generator to 0.34.0"
    This reverts commit c69116d2935eea49e9c78b2607f2fea94bc44738.

--- ingestion/setup.py
+++ ingestion/setup.py
@@ -372,7 +372,7 @@ plugins: Dict[str, Set[str]] = {
 
 dev = {
     "black==22.3.0",
-    "datamodel-code-generator==0.34.0",
+    "datamodel-code-generator==0.25.6",
     "boto3-stubs",
     "mypy-boto3-glue",
     "isort",
