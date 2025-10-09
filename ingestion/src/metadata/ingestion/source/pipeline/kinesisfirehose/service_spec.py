7aefad937964ad702f980994cac15b3433e2a86a
harshsoni2024 <harshsoni2024@gmail.com>
2025-10-09 13:22:17 +0530

    add kinesis fireshose support

--- ingestion/src/metadata/ingestion/source/pipeline/kinesisfirehose/service_spec.py
+++
@@ -1,4 +0,0 @@
-from metadata.ingestion.source.pipeline.kinesisfirehose.metadata import FirehoseSource
-from metadata.utils.service_spec import BaseSpec
-
-ServiceSpec = BaseSpec(metadata_source_class=FirehoseSource)
