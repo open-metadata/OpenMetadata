3321bbf3a5ed05796669522ea87f2992ae43a6f0
Keshav Mohta <68001229+keshavmohta09@users.noreply.github.com>
2025-10-20 09:52:45 +0530

    deps: upgraded sqlalchemy-bigquery to 1.15.0 (#23909)

--- ingestion/setup.py
+++ ingestion/setup.py
@@ -67,7 +67,7 @@ VERSIONS = {
     "pyiceberg": "pyiceberg==0.5.1",
     "google-cloud-bigtable": "google-cloud-bigtable>=2.0.0",
     "pyathena": "pyathena~=3.0",
-    "sqlalchemy-bigquery": "sqlalchemy-bigquery>=1.2.2",
+    "sqlalchemy-bigquery": "sqlalchemy-bigquery~=1.15.0",
     "presidio-analyzer": "presidio-analyzer==2.2.358",
     "asammdf": "asammdf~=7.4.5",
 }
@@ -188,7 +188,7 @@ plugins: Dict[str, Set[str]] = {
         "google-cloud-logging",
         VERSIONS["pyarrow"],
         VERSIONS["numpy"],
-        "sqlalchemy-bigquery>=1.2.2",
+        "sqlalchemy-bigquery~=1.15.0",
     },
     "bigtable": {
         VERSIONS["google-cloud-bigtable"],
