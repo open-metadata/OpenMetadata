124f6729f616ac8f4216c595ea4156f42749b0b9
myt <745985789@qq.com>
2025-10-21 23:49:44 +0800

    refactor code

--- ingestion/src/metadata/great_expectations/action1xx.py
+++ ingestion/src/metadata/great_expectations/action1xx.py
@@ -86,7 +86,7 @@ class OpenMetadataValidationAction1xx(ValidationAction):
     # This will be initialized in the run method
     ometa_conn: Optional[OpenMetadata] = None
 
-    # pylint: disable=unused-argument
+    # pylint: disable=unused-argument,arguments-differ
     def run(
         self,
         checkpoint_result: CheckpointResult,
