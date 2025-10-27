4fa691bf8c5cef2f06176be3161478041591b1b0
mohitdeuex <mohit.y@deuexsolutions.com>
2025-10-27 21:11:47 +0530

    Fix Retryable Exception

--- openmetadata-service/src/test/java/org/openmetadata/service/util/RetryableAssertionError.java
+++ openmetadata-service/src/test/java/org/openmetadata/service/util/RetryableAssertionError.java
@@ -5,7 +5,7 @@ package org.openmetadata.service.util;
  * This is used by test infrastructure to distinguish between errors that
  * should trigger a retry vs those that should fail immediately.
  */
-public class RetryableAssertionError extends AssertionError {
+public class RetryableAssertionError extends Exception {
 
   public RetryableAssertionError(String message) {
     super(message);
