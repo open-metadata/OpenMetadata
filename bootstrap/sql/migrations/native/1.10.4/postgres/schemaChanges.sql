bd1a90a7538466bb3fc505fd1335ccf3ab3f7ae5
Sriharsha Chintalapani <harsha@getcollate.io>
2025-10-27 19:22:43 -0700

    Fix: Search Slowness when painless scripts aggregates for terms and classifications

--- bootstrap/sql/migrations/native/1.10.4/postgres/schemaChanges.sql
+++ bootstrap/sql/migrations/native/1.10.4/postgres/schemaChanges.sql
@@ -9,3 +9,6 @@ SET json = jsonb_set(
     )::jsonb
 )::json
 WHERE json::jsonb -> 'testPlatforms' @> '"DBT"'::jsonb;
+
+-- Delete searchSettings to force reload from packaged searchSettings.json with field-based aggregations
+DELETE FROM openmetadata_settings WHERE "configType"='searchSettings';
