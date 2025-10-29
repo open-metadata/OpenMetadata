528eb8df741eb3550a099f7da1b872b94c27c1a8
Ram Narayan Balaji <ramnarayanb3005@gmail.com>
2025-10-29 10:18:40 +0530

    Migration moved to 1104 from 1101

--- openmetadata-service/src/main/java/org/openmetadata/service/migration/mysql/v1104/Migration.java
+++ openmetadata-service/src/main/java/org/openmetadata/service/migration/mysql/v1104/Migration.java
@@ -1,4 +1,4 @@
-package org.openmetadata.service.migration.mysql.v1100;
+package org.openmetadata.service.migration.mysql.v1104;
 
 import lombok.SneakyThrows;
 import org.openmetadata.service.jdbi3.locator.ConnectionType;
