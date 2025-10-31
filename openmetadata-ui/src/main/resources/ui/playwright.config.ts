7ee9eb3b0dc4a542b4d6ad7d1df5c7f80e418e9d
Ashish Gupta <ashish@getcollate.io>
2025-10-31 18:40:49 +0530

    modify the playwrigth config so dataAssetRules should be run after the chromium is finish executing so the global run changes won't interrupt other test

--- openmetadata-ui/src/main/resources/ui/playwright.config.ts
+++ openmetadata-ui/src/main/resources/ui/playwright.config.ts
@@ -71,7 +71,11 @@ export default defineConfig({
       // Added admin setup as a dependency. This will authorize the page with an admin user before running the test. doc: https://playwright.dev/docs/auth#multiple-signed-in-roles
       dependencies: ['setup'],
       grepInvert: /data-insight/,
-      testIgnore: ['**/nightly/**'],
+      testIgnore: [
+        '**/nightly/**',
+        '**/DataAssetRulesEnabled.spec.ts',
+        '**/DataAssetRulesDisabled.spec.ts', // exclude both from chromium
+      ],
     },
     {
       name: 'data-insight-application',
@@ -86,12 +90,13 @@ export default defineConfig({
     },
     {
       name: 'DataAssetRulesEnabled',
-      testMatch: /DataAssetRulesEnabled\.spec\.ts/,
+      testMatch: '**/DataAssetRulesEnabled.spec.ts',
+      dependencies: ['chromium'], //  ensures it runs only after chromium is done
     },
     {
       name: 'DataAssetRulesDisabled',
-      testMatch: /DataAssetRulesDisabled\.spec\.ts/,
-      dependencies: ['DataAssetRulesEnabled'], // ensures serial execution between groups
+      testMatch: '**/DataAssetRulesDisabled.spec.ts',
+      dependencies: ['DataAssetRulesEnabled'], // ensures serial order runs after DataAssetRulesEnabled
     },
   ],
 
