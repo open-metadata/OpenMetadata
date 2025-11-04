e4a10f3c89a2aa39ff09cb11f6c3c5929dbbf649
Ajith Prasad <37380177+aji-aju@users.noreply.github.com>
2025-11-04 15:36:38 +0530

    install app ops command bug fixed (#24139)

--- openmetadata-service/src/main/java/org/openmetadata/service/util/OpenMetadataOperations.java
+++ openmetadata-service/src/main/java/org/openmetadata/service/util/OpenMetadataOperations.java
@@ -77,6 +77,7 @@ import org.openmetadata.service.events.AuditExcludeFilterFactory;
 import org.openmetadata.service.events.AuditOnlyFilterFactory;
 import org.openmetadata.service.exception.EntityNotFoundException;
 import org.openmetadata.service.fernet.Fernet;
+import org.openmetadata.service.governance.workflows.WorkflowHandler;
 import org.openmetadata.service.jdbi3.AppMarketPlaceRepository;
 import org.openmetadata.service.jdbi3.AppRepository;
 import org.openmetadata.service.jdbi3.CollectionDAO;
@@ -97,6 +98,7 @@ import org.openmetadata.service.resources.CollectionRegistry;
 import org.openmetadata.service.resources.apps.AppMapper;
 import org.openmetadata.service.resources.apps.AppMarketPlaceMapper;
 import org.openmetadata.service.resources.databases.DatasourceConfig;
+import org.openmetadata.service.resources.settings.SettingsCache;
 import org.openmetadata.service.search.IndexMappingVersionTracker;
 import org.openmetadata.service.search.SearchClient;
 import org.openmetadata.service.search.SearchRepository;
@@ -313,6 +315,8 @@ public class OpenMetadataOperations implements Callable<Integer> {
     try {
       parseConfig();
       initializeCollectionRegistry();
+      WorkflowHandler.initialize(config);
+      SettingsCache.initialize(config);
       initializeSecurityConfig();
       AppRepository appRepository = (AppRepository) Entity.getEntityRepository(Entity.APPLICATION);
 
@@ -345,6 +349,7 @@ public class OpenMetadataOperations implements Callable<Integer> {
     try {
       parseConfig();
       initializeCollectionRegistry();
+      SettingsCache.initialize(config);
       initializeSecurityConfig();
       AppRepository appRepository = (AppRepository) Entity.getEntityRepository(Entity.APPLICATION);
       if (deleteApplication(appRepository, appName)) {
@@ -389,6 +394,7 @@ public class OpenMetadataOperations implements Callable<Integer> {
       }
       parseConfig();
       initializeCollectionRegistry();
+      SettingsCache.initialize(config);
       initializeSecurityConfig();
       AuthProvider authProvider = SecurityConfigurationManager.getCurrentAuthConfig().getProvider();
       if (!authProvider.equals(AuthProvider.BASIC)) {
@@ -566,6 +572,7 @@ public class OpenMetadataOperations implements Callable<Integer> {
       }
       parseConfig();
       CollectionRegistry.initialize();
+      SettingsCache.initialize(config);
       initializeSecurityConfig();
 
       AuthProvider authProvider = SecurityConfigurationManager.getCurrentAuthConfig().getProvider();
