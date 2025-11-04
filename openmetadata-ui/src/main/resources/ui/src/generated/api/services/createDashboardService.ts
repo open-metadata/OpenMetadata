495f1e842f805ca6b74df8897e1e4815412b15a2
harshsoni2024 <64592571+harshsoni2024@users.noreply.github.com>
2025-11-04 21:05:19 +0530

    Fix: PowerBI connector to support custom API URL (#24158)
    (cherry picked from commit 62cf67d15cdc534ace1d96a18ad20d49ef8091c9)

--- openmetadata-ui/src/main/resources/ui/src/generated/api/services/createDashboardService.ts
+++ openmetadata-ui/src/main/resources/ui/src/generated/api/services/createDashboardService.ts
@@ -215,6 +215,11 @@ export interface Connection {
      */
     username?: string;
     /**
+     * API URL to call powerbi rest apis to extract metadata. Default to
+     * `https://api.powerbi.com`. You can provide youw own in case of different environment
+     */
+    apiURL?: string;
+    /**
      * Authority URI for the PowerBI service.
      */
     authorityURI?: string;
