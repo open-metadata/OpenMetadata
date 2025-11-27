40bc696b26201af80d201b290eeeada2feeaac2c
Ushran Gouhar <ushran987@gmail.com>
2025-11-27 16:15:23 +0530

    Toggle search bar also using cols-dropdown button

--- openmetadata-ui/src/main/resources/ui/src/components/Entity/EntityLineage/NodeChildren/NodeChildren.component.tsx
+++ openmetadata-ui/src/main/resources/ui/src/components/Entity/EntityLineage/NodeChildren/NodeChildren.component.tsx
@@ -433,16 +433,18 @@ const NodeChildren = ({
           )}
           data-testid="column-container">
           <div className="search-box">
-            <Input
-              data-testid="search-column-input"
-              placeholder={t('label.search-entity', {
-                entity: childrenHeading,
-              })}
-              suffix={<SearchOutlined color={BORDER_COLOR} />}
-              value={searchValue}
-              onChange={handleSearchChange}
-              onClick={(e) => e.stopPropagation()}
-            />
+            {isChildrenListExpanded && (
+              <Input
+                data-testid="search-column-input"
+                placeholder={t('label.search-entity', {
+                  entity: childrenHeading,
+                })}
+                suffix={<SearchOutlined color={BORDER_COLOR} />}
+                value={searchValue}
+                onChange={handleSearchChange}
+                onClick={(e) => e.stopPropagation()}
+              />
+            )}
 
             {isChildrenListExpanded && !isEmpty(renderedColumns) && (
               <section className="m-t-md" id="table-columns">
