896c16219bace59d664a8aef3b3acda531628c6d
Ashish Gupta <ashish@getcollate.io>
2025-11-27 17:41:00 +0530

    fix custom property failure around data and time formats (#24560)
    * fix custom property failure around data and time formats
    
    * fix the playwright value
    
    * fix the flakyness of serviceForm.spec
    
    (cherry picked from commit ba7f5ebb87374d953ab9f3c55d89dad6507902f9)

--- openmetadata-ui/src/main/resources/ui/playwright/utils/customProperty.ts
+++ openmetadata-ui/src/main/resources/ui/playwright/utils/customProperty.ts
@@ -116,7 +116,7 @@ export const setValueForProperty = async (data: {
   await editButton.scrollIntoViewIfNeeded();
   await editButton.click({ force: true });
 
-  const patchRequest = page.waitForResponse(`/api/v1/${endpoint}/*`);
+  const patchRequestPromise = page.waitForResponse(`/api/v1/${endpoint}/*`);
   switch (propertyType) {
     case 'markdown':
       await page.locator(descriptionBox).isVisible();
@@ -247,7 +247,9 @@ export const setValueForProperty = async (data: {
       break;
     }
   }
-  await patchRequest;
+  const patchRequest = await patchRequestPromise;
+
+  expect(patchRequest.status()).toBe(200);
 };
 
 export const validateValueForProperty = async (data: {
@@ -305,6 +307,21 @@ export const validateValueForProperty = async (data: {
   ) {
     // For other types (string, integer, number, duration), match exact value without transformation
     await expect(container.getByTestId('value')).toContainText(value);
+  } else if ('entityReferenceList' === propertyType) {
+    const refValues = value.split(',');
+
+    for (const val of refValues) {
+      await expect(container.getByTestId(val)).toBeVisible();
+      await expect(container.getByTestId('no-data')).not.toBeVisible();
+    }
+  } else if ('entityReference' === propertyType) {
+    await expect(container.getByTestId('entityReference-value')).toContainText(
+      value
+    );
+    await expect(container.getByTestId('no-data')).not.toBeVisible();
+  } else {
+    await expect(container.getByTestId('value')).toBeVisible();
+    await expect(container.getByTestId('no-data')).not.toBeVisible();
   }
 };
 
