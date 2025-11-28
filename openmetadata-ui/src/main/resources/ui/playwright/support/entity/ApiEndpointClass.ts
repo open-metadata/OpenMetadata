3acb011fe7ac63520df221e065f198f0d671b085
Sriharsha Chintalapani <harshach@users.noreply.github.com>
2025-11-27 22:21:54 -0800

    Cache User policies; Improve Performance (#24501)
    * Cache User policies; Improve Performance
    
    * added test around user performance check when visiting difference entities pages when the user inherit multiple roles from different team assign to him
    
    ---------
    
    Co-authored-by: Ashish Gupta <ashish@getcollate.io>
    (cherry picked from commit 463d057ea51f3e7b655ff92fad5c767ba1198389)

--- openmetadata-ui/src/main/resources/ui/playwright/support/entity/ApiEndpointClass.ts
+++ openmetadata-ui/src/main/resources/ui/playwright/support/entity/ApiEndpointClass.ts
@@ -302,7 +302,9 @@ export class ApiEndpointClass extends EntityClass {
     await visitEntityPage({
       page,
       searchTerm: this.entityResponseData?.['fullyQualifiedName'],
-      dataTestId: `${this.service.name}-${this.entity.name}`,
+      dataTestId: `${
+        this.entityResponseData.service.name ?? this.service.name
+      }-${this.entityResponseData.name ?? this.entity.name}`,
     });
   }
 
