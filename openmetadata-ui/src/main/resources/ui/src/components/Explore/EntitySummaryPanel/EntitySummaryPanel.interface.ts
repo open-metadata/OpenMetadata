d9f399b260729d87f03695d11d1b463ce71866c3
Harsh Vador <58542468+harsh-vador@users.noreply.github.com>
2025-11-28 17:56:00 +0530

    feat(ui): Add sidebar for nodes in lineage view (#24574)
    * feat(ui): Add Lineage sidebar
    
    * fix lineage spec
    
    * custom property style changes
    
    * remove unwanted files
    
    * refactor code
    
    * fix incident manager
    
    * fix code smells
    
    * add opening and closing animation to sidebar
    
    * fix failing playwright
    
    * fix failing tests
    
    * address comments
    
    (cherry picked from commit 39edaff0d32c33e5b9a640a0c1123a9288d9e9a0)

--- openmetadata-ui/src/main/resources/ui/src/components/Explore/EntitySummaryPanel/EntitySummaryPanel.interface.ts
+++ openmetadata-ui/src/main/resources/ui/src/components/Explore/EntitySummaryPanel/EntitySummaryPanel.interface.ts
@@ -11,6 +11,7 @@
  *  limitations under the License.
  */
 
+import { PipelineViewMode } from '../../../generated/settings/settings';
 import { SearchedDataProps } from '../../SearchedData/SearchedData.interface';
 import { EntityDetailsObjectInterface } from '../ExplorePage.interface';
 
@@ -19,4 +20,9 @@ export interface EntitySummaryPanelProps {
   handleClosePanel: () => void;
   highlights?: SearchedDataProps['data'][number]['highlight'];
   panelPath?: string;
+  isSideDrawer?: boolean;
+  upstreamDepth?: number;
+  pipelineViewMode?: PipelineViewMode;
+  downstreamDepth?: number;
+  nodesPerLayer?: number;
 }
