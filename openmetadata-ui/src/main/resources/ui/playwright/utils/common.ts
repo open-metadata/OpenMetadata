075e11d045a41cb1186fae1eb055aada8d90ddf3
Shailesh Parmar <shailesh.parmar.webdev@gmail.com>
2025-12-01 17:26:22 +0530

    playwright: fixed test for incidentmanager and testSuite

--- openmetadata-ui/src/main/resources/ui/playwright/utils/common.ts
+++ openmetadata-ui/src/main/resources/ui/playwright/utils/common.ts
@@ -319,16 +319,10 @@ export const removeDomain = async (
   await page.getByTestId('add-domain').click();
   await page.waitForSelector('[data-testid="loader"]', { state: 'detached' });
 
-  const searchDomain = page.waitForResponse(
-    (response) =>
-      response.url().includes('/api/v1/search/query') &&
-      response.url().includes(encodeURIComponent(domain.name))
-  );
   await page
     .getByTestId('domain-selectable-tree')
     .getByTestId('searchbar')
     .fill(domain.name);
-  await searchDomain;
 
   const tagSelector = page.getByTestId(`tag-${domain.fullyQualifiedName}`);
   await tagSelector.waitFor({ state: 'visible' });
