4641d7d77751c07fbe077e9a3a1efe3d44fad8f4
Harsh Vador <harsh.vador@somaiya.edu>
2025-12-01 19:39:53 +0530

    fix failing spec

--- openmetadata-ui/src/main/resources/ui/playwright/e2e/Features/IncidentManager.spec.ts
+++ openmetadata-ui/src/main/resources/ui/playwright/e2e/Features/IncidentManager.spec.ts
@@ -489,16 +489,14 @@ test.describe('Incident Manager', PLAYWRIGHT_INGESTION_TAG_OBJ, () => {
     await expect(page.getByTestId('Incidents-label')).toBeVisible();
     await expect(page.getByTestId('Incidents-value')).toContainText('3');
 
-    const incidentResponse = page.waitForResponse(
-      `/api/v1/dataQuality/testCases?*entityLink=*${encodeURIComponent(
-        table1.entityResponseData?.['fullyQualifiedName'] ?? ''
-      )}*includeAllTests=true*limit=100*fields=testCaseResult*incidentId*`
-    );
+    await page.getByTestId('Incidents-value').click();
 
-    await Promise.all([
-      page.getByTestId('Incidents-value').click(),
-      incidentResponse,
-    ]);
+    await page.waitForURL('**/profiler/incidents**');
+
+    const incidentTabResponse = page.waitForResponse(
+      `/api/v1/dataQuality/testCases/testCaseIncidentStatus/search/list?*originEntityFQN=${table1.entityResponseData?.['fullyQualifiedName']}*`
+    );
+    await incidentTabResponse;
 
     for (const testCase of testCases) {
       await expect(
