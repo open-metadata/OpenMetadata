b7ad9d0f5f5deacfef4451d9f2b76126df506bae
Chirag Madlani <12962843+chirag-madlani@users.noreply.github.com>
2025-12-09 18:13:20 +0530

    add playwright test for the impact analysis

--- openmetadata-ui/src/main/resources/ui/playwright/e2e/Features/ImpactAnalysis.spec.ts
+++ openmetadata-ui/src/main/resources/ui/playwright/e2e/Features/ImpactAnalysis.spec.ts
@@ -11,14 +11,17 @@
  *  limitations under the License.
  */
 import { expect } from '@playwright/test';
+import { get } from 'lodash';
 import { DashboardClass } from '../../support/entity/DashboardClass';
 import { DashboardDataModelClass } from '../../support/entity/DashboardDataModelClass';
+import { EntityDataClass } from '../../support/entity/EntityDataClass';
 import { MlModelClass } from '../../support/entity/MlModelClass';
 import { PipelineClass } from '../../support/entity/PipelineClass';
 import { TableClass } from '../../support/entity/TableClass';
 import { TopicClass } from '../../support/entity/TopicClass';
 import { performAdminLogin } from '../../utils/admin';
 import { redirectToHomePage } from '../../utils/common';
+import { waitForAllLoadersToDisappear } from '../../utils/entity';
 import {
   connectEdgeBetweenNodesViaAPI,
   visitLineageTab,
@@ -34,6 +37,9 @@ const pipeline = new PipelineClass();
 const mlModel = new MlModelClass();
 
 test.describe('Impact Analysis', () => {
+  let tableColumns: string[] = [];
+  let table2Columns: string[] = [];
+
   test.beforeAll(async ({ browser }) => {
     const { apiContext, afterAction } = await performAdminLogin(browser);
 
@@ -47,30 +53,43 @@ test.describe('Impact Analysis', () => {
       mlModel.create(apiContext),
     ]);
 
+    tableColumns = get(table, 'entityResponseData.columns', []).map(
+      (col) => col.fullyQualifiedName
+    );
+    table2Columns = get(table2, 'entityResponseData.columns', []).map(
+      (col) => col.fullyQualifiedName
+    );
+
+    // connect table and table2 with column lineage
     await connectEdgeBetweenNodesViaAPI(
       apiContext,
       { id: table.entityResponseData.id, type: 'table' },
-      { id: topic.entityResponseData.id, type: 'topic' },
+      { id: table2.entityResponseData.id, type: 'table' },
       [
         {
-          fromColumns: [
-            table.columnsName[0],
-            table.columnsName[1],
-            table.columnsName[2],
-          ],
-          toColumn: table2.columnsName[0],
+          fromColumns: [tableColumns[0], tableColumns[1], tableColumns[2]],
+          toColumn: table2Columns[0],
         },
         {
-          fromColumns: [table.columnsName[3]],
-          toColumn: table2.columnsName[1],
+          fromColumns: [tableColumns[3]],
+          toColumn: table2Columns[1],
         },
         {
-          fromColumns: [table.columnsName[4]],
-          toColumn: table2.columnsName[1],
+          fromColumns: [tableColumns[2]],
+          toColumn: table2Columns[1],
         },
       ]
     );
 
+    // connect table to Topic
+    await connectEdgeBetweenNodesViaAPI(
+      apiContext,
+      { id: table.entityResponseData.id, type: 'table' },
+      { id: topic.entityResponseData.id, type: 'topic' },
+      []
+    );
+
+    // connect Dashboard to table for upstream
     await connectEdgeBetweenNodesViaAPI(
       apiContext,
       { id: dashboard.entityResponseData.id, type: 'dashboard' },
@@ -78,22 +97,116 @@ test.describe('Impact Analysis', () => {
       []
     );
 
+    // connect table to dashboardDataModel
+    await connectEdgeBetweenNodesViaAPI(
+      apiContext,
+      { id: table.entityResponseData.id, type: 'table' },
+      { id: dataModel.entityResponseData.id, type: 'dashboardDataModel' },
+      []
+    );
+
+    await connectEdgeBetweenNodesViaAPI(
+      apiContext,
+      { id: table.entityResponseData.id, type: 'table' },
+      { id: pipeline.entityResponseData.id, type: 'pipeline' },
+      []
+    );
+
+    await connectEdgeBetweenNodesViaAPI(
+      apiContext,
+      { id: table.entityResponseData.id, type: 'table' },
+      { id: mlModel.entityResponseData.id, type: 'mlmodel' },
+      []
+    );
+
+    await table2.patch({
+      apiContext,
+      patchData: [
+        {
+          op: 'add',
+          path: '/tags/0',
+          value: {
+            name: EntityDataClass.tierTag1.responseData.name,
+            tagFQN: EntityDataClass.tierTag1.responseData.fullyQualifiedName,
+            labelType: 'Manual',
+            state: 'Confirmed',
+          },
+        },
+      ],
+    });
+
+    await pipeline.patch({
+      apiContext,
+      patchData: [
+        {
+          op: 'add',
+          value: {
+            type: 'user',
+            id: EntityDataClass.user1.responseData.id,
+          },
+          path: '/owners/0',
+        },
+      ],
+    });
+
+    await topic.patch({
+      apiContext,
+      patchData: [
+        {
+          op: 'add',
+          value: {
+            type: 'domain',
+            id: EntityDataClass.domain1.responseData.id,
+          },
+          path: '/domains/0',
+        },
+      ],
+    });
+
+    await afterAction();
+  });
+
+  test.afterAll(async ({ browser }) => {
+    const { apiContext, afterAction } = await performAdminLogin(browser);
+    await Promise.all([
+      table.delete(apiContext),
+      table2.delete(apiContext),
+      topic.delete(apiContext),
+      dashboard.delete(apiContext),
+      dataModel.delete(apiContext),
+      pipeline.delete(apiContext),
+      mlModel.delete(apiContext),
+    ]);
     await afterAction();
   });
 
   test.beforeEach(async ({ page }) => {
     await redirectToHomePage(page);
+    await table.visitEntityPage(page);
+    await visitLineageTab(page);
+    const lineageResponse = page.waitForResponse(
+      `/api/v1/lineage/getLineageByEntityCount?*`
+    );
+    await page.getByRole('button', { name: 'Impact Analysis' }).click();
+    await lineageResponse;
+    await waitForAllLoadersToDisappear(page);
+  });
+
+  test('validate upstream/ downstream counts', async ({ page }) => {
+    expect(page.getByRole('button', { name: 'Downstream 5' })).toBeVisible();
+    expect(page.getByRole('button', { name: 'Upstream 1' })).toBeVisible();
   });
 
   test('Verify Downstream connections', async ({ page }) => {
-    await table.visitEntityPage(page);
-    await visitLineageTab(page);
-    await page.getByRole('button', { name: 'Impact Analysis' }).click();
-
-    // Verify Topic is visible in Impact Analysis for Downstream
-    await expect(
-      page.getByText(topic.entityResponseData.displayName)
-    ).toBeVisible();
+    const tableDownstreamNodes = [
+      pipeline.entityResponseData.displayName,
+      dataModel.entityResponseData.displayName,
+      topic.entityResponseData.displayName,
+      table2.entityResponseData.displayName,
+    ];
+    for (const node of tableDownstreamNodes) {
+      await expect(page.getByText(node)).toBeVisible();
+    }
 
     // Verify Dashboard is visible in Impact Analysis for Upstream
     await page.getByText('Upstream').click();
@@ -106,17 +219,20 @@ test.describe('Impact Analysis', () => {
     await visitLineageTab(page);
     await page.getByRole('button', { name: 'Impact Analysis' }).click();
 
-    // Verify Table is visible in Impact Analysis for Downstream of Dashboard
-    await expect(
-      page.getByText(table.entityResponseData.displayName)
-    ).toBeVisible();
+    const dashboardDownstreamNodes = [
+      dataModel.entityResponseData.displayName,
+      table.entityResponseData.displayName,
+      pipeline.entityResponseData.displayName,
+      mlModel.entityResponseData.displayName,
+      table2.entityResponseData.displayName,
+    ];
+
+    for (const node of dashboardDownstreamNodes) {
+      await expect(page.getByText(node)).toBeVisible();
+    }
   });
 
   test('Verify Upstream connections', async ({ page }) => {
-    await table.visitEntityPage(page);
-    await visitLineageTab(page);
-    await page.getByRole('button', { name: 'Impact Analysis' }).click();
-
     // Verify Dashboard is visible in Impact Analysis for Upstream
     await page.getByText('Upstream').click();
 
@@ -134,13 +250,117 @@ test.describe('Impact Analysis', () => {
     await expect(
       page.getByText(table.entityResponseData.displayName)
     ).toBeVisible();
+    await expect(
+      page.getByText(dashboard.entityResponseData.displayName)
+    ).toBeVisible();
   });
 
-  test('Verify column level downstream connections', async ({ page }) => {
-    await table.visitEntityPage(page);
+  test('verify owner filter for Asset level impact analysis', async ({
+    page,
+  }) => {
+    await page.getByRole('button', { name: 'Filters' }).click();
+    await page.getByTestId('search-dropdown-Owners').click();
+
+    await expect(
+      page.getByTitle(EntityDataClass.user1.responseData.name)
+    ).toBeVisible();
+
+    await page.getByTitle(EntityDataClass.user1.responseData.name).click();
+    await page.getByRole('button', { name: 'Update' }).click();
+    await waitForAllLoadersToDisappear(page);
+
+    await expect(page.locator('[data-row-key]')).toHaveCount(1);
+
+    await expect(
+      page.locator(
+        `[data-row-key="${pipeline.entityResponseData.fullyQualifiedName}"]`
+      )
+    ).toBeVisible();
+  });
+
+  // we are not getting domain in aggregation response that's why this test is marked as fixme
+  test.fixme(
+    'verify domain for Asset level impact analysis',
+    async ({ page }) => {
+      await page.getByRole('button', { name: 'Filters' }).click();
+      await page.getByTestId('search-dropdown-Domains').click();
+
+      await expect(
+        page.getByTitle(EntityDataClass.domain1.responseData.name)
+      ).toBeVisible();
+
+      await page.getByTitle(EntityDataClass.domain1.responseData.name).click();
+      await page.getByRole('button', { name: 'Update' }).click();
+      await waitForAllLoadersToDisappear(page);
+
+      await expect(page.locator('[data-row-key]')).toHaveCount(1);
+
+      await expect(
+        page.locator(
+          `[data-row-key="${topic.entityResponseData.fullyQualifiedName}"]`
+        )
+      ).toBeVisible();
+    }
+  );
+
+  test('verify tier for Asset level impact analysis', async ({ page }) => {
+    await page.getByRole('button', { name: 'Filters' }).click();
+    await page.getByTestId('search-dropdown-Tier').click();
+
+    await expect(
+      page.getByTitle(EntityDataClass.tierTag1.responseData.displayName)
+    ).toBeVisible();
+
+    await page
+      .getByTitle(EntityDataClass.tierTag1.responseData.displayName)
+      .click();
+    await page.getByRole('button', { name: 'Update' }).click();
+    await waitForAllLoadersToDisappear(page);
+
+    await expect(page.locator('[data-row-key]')).toHaveCount(1);
+
+    await expect(
+      page.locator(
+        `[data-row-key="${table2.entityResponseData.fullyQualifiedName}"]`
+      )
+    ).toBeVisible();
+  });
+
+  test('Verify upstream/downstream counts for column level', async ({
+    page,
+  }) => {
+    await page.getByRole('button', { name: 'Impact On: Table' }).click();
+    const columnLineageResponse = page.waitForResponse(
+      `/api/v1/lineage/getLineage?fqn=${table.entityResponseData.fullyQualifiedName}&type=table&upstreamDepth=2&downstreamDepth=2&includeDeleted=false&size=50`
+    );
+    await page.getByText('Column level').click();
+    await columnLineageResponse;
+
+    // Column level data take time to reflect due to UI processing
+    // hence had to add static wait
+    await page.waitForTimeout(1000);
+
+    expect(page.getByRole('button', { name: 'Downstream 5' })).toBeVisible();
+    expect(page.getByRole('button', { name: 'Upstream 0' })).toBeVisible();
+
+    await table2.visitEntityPage(page);
     await visitLineageTab(page);
     await page.getByRole('button', { name: 'Impact Analysis' }).click();
 
+    await page.getByRole('button', { name: 'Impact On: Table' }).click();
+
+    await page.getByText('Column level').click();
+    await columnLineageResponse;
+
+    // Column level data take time to reflect due to UI processing
+    // hence had to add static wait
+    await page.waitForTimeout(1000);
+
+    expect(page.getByRole('button', { name: 'Downstream 0' })).toBeVisible();
+    expect(page.getByRole('button', { name: 'Upstream 5' })).toBeVisible();
+  });
+
+  test('Verify column level downstream connections', async ({ page }) => {
     await page.getByRole('button', { name: 'Impact On: Table' }).click();
     const columnLineageResponse = page.waitForResponse(
       `/api/v1/lineage/getLineage?fqn=${table.entityResponseData.fullyQualifiedName}&type=table&upstreamDepth=2&downstreamDepth=2&includeDeleted=false&size=50`
@@ -150,14 +370,129 @@ test.describe('Impact Analysis', () => {
 
     // Verify Topic is visible in Impact Analysis for Downstream
     const fromColumns = [
-      table.columnsName[0],
-      table.columnsName[1],
-      table.columnsName[2],
+      {
+        rowKey: tableColumns[0] + '->' + table2Columns[0],
+        sourceColumn: table.columnsName[0],
+        targetColumn: table2.columnsName[0],
+      },
+      {
+        rowKey: tableColumns[0] + '->' + table2Columns[0],
+        sourceColumn: table.columnsName[0],
+        targetColumn: table2.columnsName[0],
+      },
+      {
+        rowKey: tableColumns[1] + '->' + table2Columns[0],
+        sourceColumn: table.columnsName[1],
+        targetColumn: table2.columnsName[0],
+      },
+      {
+        rowKey: tableColumns[2] + '->' + table2Columns[0],
+        sourceColumn: table.columnsName[2],
+        targetColumn: table2.columnsName[0],
+      },
+      {
+        rowKey: tableColumns[3] + '->' + table2Columns[1],
+        sourceColumn: table.columnsName[7],
+        targetColumn: table2.columnsName[1],
+      },
+      {
+        rowKey: tableColumns[2] + '->' + table2Columns[1],
+        sourceColumn: table.columnsName[2],
+        targetColumn: table2.columnsName[1],
+      },
     ];
 
     // Verify columns are visible in Impact Analysis for Downstream Topic
     for (const col of fromColumns) {
-      await expect(page.getByText(col)).toBeVisible();
+      // Assert row with column connection
+      await expect(
+        page.locator(`[data-row-key="${col.rowKey}"]`)
+      ).toBeVisible();
+
+      // assert source column
+      await expect(
+        page
+          .locator(`[data-row-key="${col.rowKey}"]`)
+          .getByRole('cell', { name: col.sourceColumn })
+      ).toBeVisible();
+
+      // assert target column
+      await expect(
+        page
+          .locator(`[data-row-key="${col.rowKey}"]`)
+          .getByRole('cell', { name: col.targetColumn })
+      ).toBeVisible();
+    }
+  });
+
+  test('Verify column level upstream connections', async ({ page }) => {
+    await table2.visitEntityPage(page);
+    await visitLineageTab(page);
+    await page.getByRole('button', { name: 'Impact Analysis' }).click();
+
+    await page.getByRole('button', { name: 'Impact On: Table' }).click();
+    const columnLineageResponse = page.waitForResponse(
+      `/api/v1/lineage/getLineage?fqn=${table2.entityResponseData.fullyQualifiedName}&type=table&upstreamDepth=2&downstreamDepth=2&includeDeleted=false&size=50`
+    );
+    await page.getByText('Column level').click();
+    await columnLineageResponse;
+
+    await page.getByRole('button', { name: 'Upstream' }).click();
+
+    // Verify Topic is visible in Impact Analysis for Upstream
+    const fromColumns = [
+      {
+        rowKey: tableColumns[0] + '->' + table2Columns[0],
+        sourceColumn: table.columnsName[0],
+        targetColumn: table2.columnsName[0],
+      },
+      {
+        rowKey: tableColumns[0] + '->' + table2Columns[0],
+        sourceColumn: table.columnsName[0],
+        targetColumn: table2.columnsName[0],
+      },
+      {
+        rowKey: tableColumns[1] + '->' + table2Columns[0],
+        sourceColumn: table.columnsName[1],
+        targetColumn: table2.columnsName[0],
+      },
+      {
+        rowKey: tableColumns[2] + '->' + table2Columns[0],
+        sourceColumn: table.columnsName[2],
+        targetColumn: table2.columnsName[0],
+      },
+      {
+        rowKey: tableColumns[3] + '->' + table2Columns[1],
+        sourceColumn: table.columnsName[7],
+        targetColumn: table2.columnsName[1],
+      },
+      {
+        rowKey: tableColumns[2] + '->' + table2Columns[1],
+        sourceColumn: table.columnsName[2],
+        targetColumn: table2.columnsName[1],
+      },
+    ];
+
+    // Verify columns are visible in Impact Analysis for Upstream Topic
+    for (const col of fromColumns) {
+      // Assert row with column connection
+      await expect(
+        page.locator(`[data-row-key="${col.rowKey}"]`)
+      ).toBeVisible();
+
+      // assert source column
+      await expect(
+        page
+          .locator(`[data-row-key="${col.rowKey}"]`)
+          .getByRole('cell', { name: col.sourceColumn })
+      ).toBeVisible();
+
+      // assert target column
+      await expect(
+        page
+          .locator(`[data-row-key="${col.rowKey}"]`)
+          .getByRole('cell', { name: col.targetColumn })
+      ).toBeVisible();
     }
   });
 });
