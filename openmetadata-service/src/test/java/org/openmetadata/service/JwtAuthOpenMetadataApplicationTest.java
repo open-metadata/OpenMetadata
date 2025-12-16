c1cffd06677b481c5d7f179381f7337e8be95bf0
Sriharsha Chintalapani <harsha@getcollate.io>
2025-12-15 21:56:40 -0800

    Use Java 11+ HttpClient connector for tests (jersey-jnh-connector)
    Switch from the broken jersey-jetty-connector (incompatible with Jetty 12.1.x)
    to jersey-jnh-connector which uses Java's built-in java.net.http.HttpClient.
    This connector:
    - Natively supports all HTTP methods including PATCH
    - Works with Java 21
    - No external dependencies required
    - Avoids compatibility issues with Jetty versions

--- openmetadata-service/src/test/java/org/openmetadata/service/JwtAuthOpenMetadataApplicationTest.java
+++ openmetadata-service/src/test/java/org/openmetadata/service/JwtAuthOpenMetadataApplicationTest.java
@@ -47,13 +47,10 @@ import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.impl.client.BasicCredentialsProvider;
-import org.eclipse.jetty.client.HttpClient;
 import org.flywaydb.core.Flyway;
 import org.glassfish.jersey.client.ClientConfig;
 import org.glassfish.jersey.client.ClientProperties;
-import org.glassfish.jersey.jetty.connector.JettyClientProperties;
-import org.glassfish.jersey.jetty.connector.JettyConnectorProvider;
-import org.glassfish.jersey.jetty.connector.JettyHttpClientSupplier;
+import org.glassfish.jersey.jnh.connector.JavaNetHttpConnectorProvider;
 import org.jdbi.v3.core.Jdbi;
 import org.jdbi.v3.sqlobject.SqlObjectPlugin;
 import org.jdbi.v3.sqlobject.SqlObjects;
@@ -348,23 +345,13 @@ public abstract class JwtAuthOpenMetadataApplicationTest {
   }
 
   private static void createClient() {
-    // Use Jetty HTTP client connector - Jersey 3.1.4+ jersey-jetty-connector supports Jetty 12
-    HttpClient httpClient = new HttpClient();
-    httpClient.setIdleTimeout(0);
-    try {
-      httpClient.start(); // Jetty 12 HttpClient must be explicitly started
-    } catch (Exception e) {
-      throw new RuntimeException("Failed to start Jetty HttpClient", e);
-    }
+    // Use JNH connector (Java 11+ HttpClient) - simple, reliable, supports PATCH natively
     ClientConfig config = new ClientConfig();
-    config.connectorProvider(new JettyConnectorProvider());
-    config.register(new JettyHttpClientSupplier(httpClient));
+    config.connectorProvider(new JavaNetHttpConnectorProvider());
     config.register(new JacksonFeature(APP.getObjectMapper()));
-    // Set reasonable timeouts to prevent indefinite hangs in CI
+    // Set reasonable timeouts to prevent indefinite hangs
     config.property(ClientProperties.CONNECT_TIMEOUT, 30_000); // 30 seconds
     config.property(ClientProperties.READ_TIMEOUT, 120_000); // 2 minutes
-    config.property(ClientProperties.SUPPRESS_HTTP_COMPLIANCE_VALIDATION, true);
-    config.property(JettyClientProperties.SYNC_LISTENER_RESPONSE_MAX_SIZE, 10 * 1024 * 1024);
     client = ClientBuilder.newClient(config);
   }
 
