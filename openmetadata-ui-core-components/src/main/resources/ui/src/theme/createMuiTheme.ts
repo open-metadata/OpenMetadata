355e466bf5e27908a6b74a169f1dd75246b4038b
Dhruv Parmar <83108871+dhruvjsx@users.noreply.github.com>
2026-01-04 17:56:16 +0530

    updated MUI colors

--- openmetadata-ui-core-components/src/main/resources/ui/src/theme/createMuiTheme.ts
+++ openmetadata-ui-core-components/src/main/resources/ui/src/theme/createMuiTheme.ts
@@ -41,6 +41,8 @@ export const createMuiTheme = (
   defaultTheme?: {
     primaryColor: string;
     infoColor: string;
+    hoverColor:string;
+    selectedColor:string;
     successColor: string;
     warningColor: string;
     errorColor: string;
