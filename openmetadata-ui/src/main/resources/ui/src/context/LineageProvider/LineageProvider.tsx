ecfd3aa68e672bae65e20902e68a2bc8aa875da8
Ushran Gouhar <ushran987@gmail.com>
2026-01-09 00:57:50 +0530

    Using ref to track previous layer

--- openmetadata-ui/src/main/resources/ui/src/context/LineageProvider/LineageProvider.tsx
+++ openmetadata-ui/src/main/resources/ui/src/context/LineageProvider/LineageProvider.tsx
@@ -172,6 +172,7 @@ const LineageProvider = ({ children }: LineageProviderProps) => {
     {} as SourceType
   );
   const [activeLayer, setActiveLayer] = useState<LineageLayer[]>([]);
+  const previousLayerRef = useRef(activeLayer);
 
   // Added this ref to compare the previous active layer with the current active layer.
   // We need to redraw the lineage if the column level lineage is added or removed.
@@ -729,27 +730,25 @@ const LineageProvider = ({ children }: LineageProviderProps) => {
     [nodes, edges, activeNode, tracedNodes]
   );
 
-  const onUpdateLayerView = useCallback(
-    (layers: LineageLayer[]) => {
-      // Check if column layer is being removed
-      if (
-        activeLayer.includes(LineageLayer.ColumnLevelLineage) &&
-        !layers.includes(LineageLayer.ColumnLevelLineage)
-      ) {
-        setTracedColumns([]);
-      }
+  const onUpdateLayerView = useCallback((layers: LineageLayer[]) => {
+    // Check if column layer is being removed
+    if (
+      previousLayerRef.current.includes(LineageLayer.ColumnLevelLineage) &&
+      !layers.includes(LineageLayer.ColumnLevelLineage)
+    ) {
+      setTracedColumns([]);
+    }
 
-      setActiveLayer(layers);
+    setActiveLayer(layers);
+    previousLayerRef.current = layers;
 
-      if (
-        layers.includes(LineageLayer.ColumnLevelLineage) ||
-        layers.includes(LineageLayer.DataObservability)
-      ) {
-        setPlatformView(LineagePlatformView.None);
-      }
-    },
-    [activeLayer]
-  );
+    if (
+      layers.includes(LineageLayer.ColumnLevelLineage) ||
+      layers.includes(LineageLayer.DataObservability)
+    ) {
+      setPlatformView(LineagePlatformView.None);
+    }
+  }, []);
 
   const updateEntityData = useCallback(
     (
