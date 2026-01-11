7e6b70946e4fe4405fab552983d66773513bbb36
Rohit0301 <rj03012002@gmail.com>
2026-01-11 16:57:57 +0530

    fixed failing tests

--- openmetadata-ui/src/main/resources/ui/src/components/Database/DatabaseSchema/DatabaseSchemaTable/DatabaseSchemaTable.tsx
+++ openmetadata-ui/src/main/resources/ui/src/components/Database/DatabaseSchema/DatabaseSchemaTable/DatabaseSchemaTable.tsx
@@ -293,7 +293,7 @@ export const DatabaseSchemaTable = ({
     if (searchValue) {
       searchSchema(searchValue, currentPage);
     }
-  }, [searchValue, currentPage]);
+  }, [searchValue, currentPage, searchSchema]);
 
   useEffect(() => {
     if (searchValue) {
