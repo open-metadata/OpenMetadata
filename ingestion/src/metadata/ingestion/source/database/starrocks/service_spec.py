87be4586a6e0a08f319648a3ec126962201a9839
TeddyCr <teddy.crepineau@gmail.com>
2026-01-12 11:31:52 +0100

    chore: remove unecessary profiler files

--- ingestion/src/metadata/ingestion/source/database/starrocks/service_spec.py
+++ ingestion/src/metadata/ingestion/source/database/starrocks/service_spec.py
@@ -1,14 +1,11 @@
 from metadata.ingestion.source.database.starrocks.lineage import StarRocksLineageSource
 from metadata.ingestion.source.database.starrocks.metadata import StarRocksSource
 from metadata.ingestion.source.database.starrocks.usage import StarRocksUsageSource
-from metadata.profiler.interface.sqlalchemy.starrocks.profiler_interface import (
-    StarRocksProfilerInterface,
-)
+
 from metadata.utils.service_spec.default import DefaultDatabaseSpec
 
 ServiceSpec = DefaultDatabaseSpec(
     metadata_source_class=StarRocksSource,
     lineage_source_class=StarRocksLineageSource,
     usage_source_class=StarRocksUsageSource,
-    profiler_class=StarRocksProfilerInterface,
 )
