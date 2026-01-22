323248e6b421bf5b0140eb0fa211a3fa5a03319e
Aniket Katkar <aniketkatkar97@gmail.com>
2026-01-22 22:11:02 +0530

    Add the invalid-malformed.json to the exclude list for json/yaml check

--- scripts/validate_json_yaml.sh
+++ scripts/validate_json_yaml.sh
@@ -4,7 +4,8 @@ set -eup pipefail
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 EXCLUDED_DIRS=".vscode|great_expectations/resources"
+EXCLUDED_FILES="openmetadata-ui/src/main/resources/ui/playwright/test-data/odcs-examples/invalid-malformed.json"
 echo "Validating JSON files..."
-git ls-files | grep "\.json$" | grep -vE "/($EXCLUDED_DIRS)/" | while read file; do jq . "$file" >/dev/null 2>&1 || { echo "Invalid JSON in $file"; exit 1; }; done
+git ls-files | grep "\.json$" | grep -vE "/($EXCLUDED_DIRS)/" | grep -v "$EXCLUDED_FILES" | while read file; do jq . "$file" >/dev/null 2>&1 || { echo "Invalid JSON in $file"; exit 1; }; done
 echo "Validating YAML files..."
 git ls-files | grep -E "\.ya?ml$" | grep -vE "/($EXCLUDED_DIRS)/" | while read file; do python ${SCRIPT_DIR}/validate_yaml.py "$file" >/dev/null 2>&1 || { echo "Invalid YAML in $file"; exit 1; }; done
