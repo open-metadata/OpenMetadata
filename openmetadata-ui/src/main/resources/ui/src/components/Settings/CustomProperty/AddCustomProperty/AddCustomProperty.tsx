0db1c3258d8f1147cd793bfcc7585b9c57ec4111
Shrabanti Paul <shrabantipaul@Shrabantis-MacBook-Pro.local>
2026-01-30 12:47:34 +0530

    Update language files for new key table-column

--- openmetadata-ui/src/main/resources/ui/src/components/Settings/CustomProperty/AddCustomProperty/AddCustomProperty.tsx
+++ openmetadata-ui/src/main/resources/ui/src/components/Settings/CustomProperty/AddCustomProperty/AddCustomProperty.tsx
@@ -14,7 +14,7 @@
 import { Chip, Typography, useTheme } from '@mui/material';
 import { Button, Col, Form, FormInstance, Row } from 'antd';
 import { AxiosError } from 'axios';
-import { ReactComponent as TableIcon } from '../../../../assets/svg/table-icon.svg';
+import { ReactComponent as ColumnIcon } from '../../../../assets/svg/ic-column.svg';
 import MuiDrawer from '../../../common/MuiDrawer/MuiDrawer';
 
 import { isArray, isUndefined, map, omit, omitBy, startCase } from 'lodash';
@@ -236,12 +236,12 @@ const AddCustomProperty = ({
   const getAppliesToField = useCallback(() => {
     return (
       <Chip
-        icon={<TableIcon />}
+        icon={<ColumnIcon height={14} width={14} />}
         label={
           <Typography
             color={theme.palette.allShades.gray['700']}
             variant="body2">
-            {t('label.table')}
+            {t('label.table-column')}
           </Typography>
         }
         sx={{
