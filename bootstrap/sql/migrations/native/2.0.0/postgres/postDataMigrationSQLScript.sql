3fc4a188002923ce1d035a23adcb3eb39996b011
TeddyCr <teddy.crepineau@gmail.com>
2026-03-02 11:46:34 -0800

    fix psql migration query

--- bootstrap/sql/migrations/native/2.0.0/postgres/postDataMigrationSQLScript.sql
+++ bootstrap/sql/migrations/native/2.0.0/postgres/postDataMigrationSQLScript.sql
@@ -1,4 +1,4 @@
 UPDATE ingestion_pipeline_entity
 SET json = json #- '{sourceConfig,config,computeMetrics}'
-WHERE json -> 'sourceConfig' -> 'config' -> 'computeMetrics'
+WHERE json -> 'sourceConfig' -> 'config' -> 'computeMetrics' IS NOT NULL
 AND pipelineType = 'profiler';
