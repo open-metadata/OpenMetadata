6a7d0d33071f1333989a03d63c9ecbc150b475ae
Aniket Katkar <aniketkatkar97@gmail.com>
2026-03-06 17:18:24 +0530

    Fix the teams spec failure

--- openmetadata-ui/src/main/resources/ui/playwright/utils/table.ts
+++ openmetadata-ui/src/main/resources/ui/playwright/utils/table.ts
@@ -11,6 +11,7 @@
  *  limitations under the License.
  */
 import { expect, Page } from '@playwright/test';
+import { TableClass } from '../support/entity/TableClass';
 
 // Pagination is performed for "performance_test_table" Table Entity
 export const columnPaginationTable = async (page: Page) => {
@@ -92,4 +93,17 @@ export const columnPaginationTable = async (page: Page) => {
 
   // 25 Row + 1 Header row
   expect(page.getByTestId('entity-table').getByRole('row')).toHaveCount(26);
+};
+
+export const getTableColumnsCount = (columns: TableClass['children']) => {
+  let columnsCount = 0;
+
+  columns.forEach((column) => {
+    columnsCount += 1;
+    if (column.children) {
+      columnsCount += getTableColumnsCount(column.children);
+    }
+  });
+
+  return columnsCount;
 };
