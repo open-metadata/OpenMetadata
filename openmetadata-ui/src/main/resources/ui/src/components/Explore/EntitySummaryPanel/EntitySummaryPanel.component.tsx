d47ed014abeae57b6300c3e4e0cc20f065e0e07a
Rohit0301 <rj03012002@gmail.com>
2026-03-09 13:30:35 +0530

    minor fix

--- openmetadata-ui/src/main/resources/ui/src/components/Explore/EntitySummaryPanel/EntitySummaryPanel.component.tsx
+++ openmetadata-ui/src/main/resources/ui/src/components/Explore/EntitySummaryPanel/EntitySummaryPanel.component.tsx
@@ -996,7 +996,7 @@ export default function EntitySummaryPanel({
       {isSideDrawer && (
         <div className="d-flex items-center justify-between">
           <EntityTitleSection
-            className="tw:bg-transparent"
+            className="tw:bg-transparent!"
             entityDetails={entityDetails.details}
             entityDisplayName={entityData?.displayName}
             entityLink={entityLink}
