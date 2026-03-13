da6d07fb14f6c1010aa24b2693f1e083980e30d0
Chirag Madlani <12962843+chirag-madlani@users.noreply.github.com>
2026-03-13 17:36:41 +0530

    fix redshift with dbt ingestion failure

--- openmetadata-ui/src/main/resources/ui/playwright/support/entity/ingestion/RedshiftWithDBTIngestionClass.ts
+++ openmetadata-ui/src/main/resources/ui/playwright/support/entity/ingestion/RedshiftWithDBTIngestionClass.ts
@@ -244,15 +244,6 @@ class RedshiftWithDBTIngestionClass extends ServiceBaseClass {
 
       expect(codeMirrorText).toContain(DBT.dbtQuery);
 
-      await page.click('[data-testid="lineage"]');
-
-      await page.waitForSelector('[data-testid="entity-header-display-name"]');
-      const entityHeaderDisplayName = await page.textContent(
-        '[data-testid="entity-header-display-name"]'
-      );
-
-      expect(entityHeaderDisplayName).toContain(DBT.dbtLineageNodeLabel);
-
       // Verify Data Quality
       await page.click('[data-testid="profiler"]');
 
@@ -269,6 +260,14 @@ class RedshiftWithDBTIngestionClass extends ServiceBaseClass {
 
     await test.step('validate DBT icon should be show to lineage node', async () => {
       await visitLineageTab(page);
+
+      // Verify entity header display name
+      await page.waitForSelector('[data-testid="entity-header-display-name"]');
+      const entityHeaderDisplayName = await page.textContent(
+        '[data-testid="entity-header-display-name"]'
+      );
+
+      expect(entityHeaderDisplayName).toContain(DBT.dbtLineageNodeLabel);
 
       await expect(
         page.getByTestId(`lineage-node-${this.dbtEntityFqn}`)
