1c8582c967eb34e8c36dfd0e3bafeaa995bbec80
anuj-kumary <anujf0510@gmail.com>
2026-03-26 16:19:02 +0530

    nit

--- openmetadata-ui-core-components/src/main/resources/ui/src/components/base/badges/badges.tsx
+++ openmetadata-ui-core-components/src/main/resources/ui/src/components/base/badges/badges.tsx
@@ -92,10 +92,10 @@ export const filledColors: Record<
    * Used for tag badges.
    */
   'blue-dark': {
-    root: 'tw:bg-utility-blue-dark-50 tw:text-[rgba(64,64,64,1)] tw:ring-utility-gray-blue-200',
-    addon: 'tw:text-[rgba(64,64,64,1)]',
+    root: 'tw:bg-utility-blue-dark-50 tw:text-utility-gray-700 tw:ring-utility-gray-blue-200',
+    addon: 'tw:text-utility-gray-700',
     addonButton:
-      'tw:hover:bg-utility-blue-dark-100 tw:text-[rgba(64,64,64,1)] tw:hover:text-utility-blue-dark-500',
+      'tw:hover:bg-utility-blue-dark-100 tw:text-utility-gray-700 tw:hover:text-utility-blue-dark-500',
   },
 };
 
