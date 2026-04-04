d5e5f58fc9ff1bf1137ba1be1e699285addf1e50
Siddhi Gupta <abhayakg123@gmail.com>
2026-04-04 12:20:46 +0530

    Add recursive LDAP group membership support

--- openmetadata-ui/src/main/resources/ui/src/generated/configuration/authenticationConfiguration.ts
+++ openmetadata-ui/src/main/resources/ui/src/generated/configuration/authenticationConfiguration.ts
@@ -161,6 +161,11 @@ export interface LDAPConfiguration {
      */
     port: number;
     /**
+     * Enable transitive group membership resolution for Active Directory nested groups using
+     * LDAP_MATCHING_RULE_IN_CHAIN.
+     */
+    recursiveGroupMembership?: boolean;
+    /**
      * Admin role name
      */
     roleAdminName?: string;
