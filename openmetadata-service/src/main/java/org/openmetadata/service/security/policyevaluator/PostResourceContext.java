49db2aaea919279efb607b8695826c336c74cd80
atharvasingh7007 <singhatharva7007@gmail.com>
2026-04-13 19:57:26 +0530

    fix: restore feed delete domain authorization

--- openmetadata-service/src/main/java/org/openmetadata/service/security/policyevaluator/PostResourceContext.java
+++ openmetadata-service/src/main/java/org/openmetadata/service/security/policyevaluator/PostResourceContext.java
@@ -3,13 +3,19 @@ package org.openmetadata.service.security.policyevaluator;
 import java.util.ArrayList;
 import java.util.List;
 import org.openmetadata.schema.EntityInterface;
+import org.openmetadata.schema.entity.feed.Thread;
 import org.openmetadata.schema.type.EntityReference;
 import org.openmetadata.schema.type.Include;
 import org.openmetadata.schema.type.TagLabel;
 import org.openmetadata.service.Entity;
 
 /** Posts that are part of conversation threads require special handling */
-public record PostResourceContext(String postedBy) implements ResourceContextInterface {
+public record PostResourceContext(String postedBy, Thread thread)
+    implements ResourceContextInterface {
+  public PostResourceContext(String postedBy) {
+    this(postedBy, null);
+  }
+
   @Override
   public String getResource() {
     return Entity.THREAD;
@@ -17,6 +23,9 @@ public record PostResourceContext(String postedBy) implements ResourceContextInt
 
   @Override
   public List<EntityReference> getOwners() {
+    if (postedBy == null) {
+      return null;
+    }
     List<EntityReference> owners = new ArrayList<>();
     owners.add(Entity.getEntityReferenceByName(Entity.USER, postedBy, Include.NON_DELETED));
     return owners;
@@ -29,11 +38,11 @@ public record PostResourceContext(String postedBy) implements ResourceContextInt
 
   @Override
   public EntityInterface getEntity() {
-    return null;
+    return ThreadResourceContext.resolveAboutEntity(thread);
   }
 
   @Override
   public List<EntityReference> getDomains() {
-    return null;
+    return ThreadResourceContext.resolveDomains(thread);
   }
 }
--- openmetadata-service/src/main/java/org/openmetadata/service/security/policyevaluator/ThreadResourceContext.java
+++ openmetadata-service/src/main/java/org/openmetadata/service/security/policyevaluator/ThreadResourceContext.java
@@ -1,15 +1,21 @@
 package org.openmetadata.service.security.policyevaluator;
 
+import static org.openmetadata.common.utils.CommonUtil.nullOrEmpty;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 import org.openmetadata.schema.EntityInterface;
+import org.openmetadata.schema.entity.feed.Thread;
 import org.openmetadata.schema.type.EntityReference;
 import org.openmetadata.schema.type.Include;
 import org.openmetadata.schema.type.TagLabel;
 import org.openmetadata.service.Entity;
+import org.openmetadata.service.resources.feeds.MessageParser.EntityLink;
+import org.openmetadata.service.util.EntityUtil;
 
 /** Conversation threads require special handling */
-public record ThreadResourceContext(String createdBy) implements ResourceContextInterface {
+public record ThreadResourceContext(Thread thread) implements ResourceContextInterface {
   @Override
   public String getResource() {
     return Entity.THREAD;
@@ -17,8 +23,12 @@ public record ThreadResourceContext(String createdBy) implements ResourceContext
 
   @Override
   public List<EntityReference> getOwners() {
+    if (thread == null || thread.getCreatedBy() == null) {
+      return null;
+    }
     List<EntityReference> owners = new ArrayList<>();
-    owners.add(Entity.getEntityReferenceByName(Entity.USER, createdBy, Include.NON_DELETED));
+    owners.add(
+        Entity.getEntityReferenceByName(Entity.USER, thread.getCreatedBy(), Include.NON_DELETED));
     return owners;
   }
 
@@ -29,12 +39,41 @@ public record ThreadResourceContext(String createdBy) implements ResourceContext
 
   @Override
   public EntityInterface getEntity() {
+    return resolveAboutEntity(thread);
+  }
+
+  @Override
+  public List<EntityReference> getDomains() {
+    return resolveDomains(thread);
+  }
+
+  static List<EntityReference> resolveDomains(Thread thread) {
+    if (thread == null) {
+      return null;
+    }
+
+    EntityInterface aboutEntity = resolveAboutEntity(thread);
+    if (aboutEntity != null && !nullOrEmpty(aboutEntity.getDomains())) {
+      return aboutEntity.getDomains();
+    }
+
+    List<UUID> domainIds = thread.getDomains();
+    if (!nullOrEmpty(domainIds)) {
+      return EntityUtil.getEntityReferencesById(Entity.DOMAIN, domainIds);
+    }
+
     return null;
   }
 
-  // TODO: Fix this this should be thread.getEntity().getDomain()
-  @Override
-  public List<EntityReference> getDomains() {
-    return null;
+  static EntityInterface resolveAboutEntity(Thread thread) {
+    if (thread == null || thread.getAbout() == null) {
+      return null;
+    }
+    try {
+      EntityLink about = EntityLink.parse(thread.getAbout());
+      return Entity.getEntity(about, Entity.FIELD_DOMAINS, Include.ALL);
+    } catch (Exception ignored) {
+      return null;
+    }
   }
 }
