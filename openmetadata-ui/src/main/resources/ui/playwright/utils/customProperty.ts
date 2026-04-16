42e2bc1bb6d4364b90d32e6797d7b4a3ac6cd2c7
Aniket Katkar <aniketkatkar97@gmail.com>
2026-04-16 17:20:12 +0530

    check

--- openmetadata-ui/src/main/resources/ui/playwright/utils/customProperty.ts
+++ openmetadata-ui/src/main/resources/ui/playwright/utils/customProperty.ts
@@ -18,8 +18,8 @@ import {
 } from '../constant/customProperty';
 import { SidebarItem } from '../constant/sidebar';
 import {
-  EntityTypeEndpoint,
   ENTITY_PATH,
+  EntityTypeEndpoint,
 } from '../support/entity/Entity.interface';
 import { UserClass } from '../support/user/UserClass';
 import { selectOption, showAdvancedSearchDialog } from './advancedSearch';
