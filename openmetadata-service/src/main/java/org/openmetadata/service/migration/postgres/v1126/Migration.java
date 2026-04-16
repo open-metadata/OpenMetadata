60eb1642e6b043ebb261fd58f9b5717207fb063b
Ram Narayan Balaji <81347100+yan-3005@users.noreply.github.com>
2026-04-16 19:33:08 +0530

    fix(migration): revert webhook authType back to secretKey in v1126 and remove broken v1125 migration (#27427)
    * fix(migration): add v1126 reverse migration to revert webhook authType back to secretKey
    
    * fix(migration): remove migrateWebhookSecretKeyToAuthType from v1125 migration
    
    * fix(test): remove migrateWebhookSecretKeyToAuthType references from v1125 migration tests
    
    * fix(migration): address copilot review comments on v1126 migration
    
    * fix(migration): case-insensitive bearer check and verify JSON content in v1126 tests
    
    * fix(migration): remove unused constants from v1125 and add postgres path + SQL verification to v1126 tests
    
    (cherry picked from commit 35ede8fe5fc43d6bf8a908a1920e72e082e96b3d)

---
+++ openmetadata-service/src/main/java/org/openmetadata/service/migration/postgres/v1126/Migration.java
@@ -0,0 +1,25 @@
+package org.openmetadata.service.migration.postgres.v1126;
+
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.openmetadata.service.migration.api.MigrationProcessImpl;
+import org.openmetadata.service.migration.utils.MigrationFile;
+import org.openmetadata.service.migration.utils.v1126.MigrationUtil;
+
+@Slf4j
+public class Migration extends MigrationProcessImpl {
+
+  public Migration(MigrationFile migrationFile) {
+    super(migrationFile);
+  }
+
+  @Override
+  @SneakyThrows
+  public void runDataMigration() {
+    try {
+      MigrationUtil.revertWebhookAuthTypeToSecretKey(handle);
+    } catch (Exception e) {
+      LOG.error("Failed to revert webhook authType to secretKey in v1126 migration.", e);
+    }
+  }
+}
