5ee5b74efdc5ef246f0d07df0e86d0356c8acc17
Aniket Katkar <aniketkatkar97@gmail.com>
2026-04-22 10:27:25 +0530

    Fix checkstyle

--- openmetadata-ui/src/main/resources/ui/playwright/e2e/Pages/Entity.spec.ts
+++ openmetadata-ui/src/main/resources/ui/playwright/e2e/Pages/Entity.spec.ts
@@ -10,7 +10,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-import { test as base, expect, Page } from '@playwright/test';
+import { expect, Page, test as base } from '@playwright/test';
 import { isUndefined } from 'lodash';
 import { Column, Table } from '../../../src/generated/entity/data/table';
 import { COMMON_TIER_TAG, KEY_PROFILE_METRICS } from '../../constant/common';
