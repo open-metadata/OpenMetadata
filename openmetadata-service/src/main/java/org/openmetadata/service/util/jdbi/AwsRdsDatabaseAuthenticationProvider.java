01c987e557edfc0077a7dfb763be25883b7998ac
Aniruddha Adak <aniruddhaadak80@users.noreply.github.com>
2026-04-22 19:42:50 +0530

    Fix RDS IAM Cross Account Auth and Clarify Dev Container Docs

--- openmetadata-service/src/main/java/org/openmetadata/service/util/jdbi/AwsRdsDatabaseAuthenticationProvider.java
+++ openmetadata-service/src/main/java/org/openmetadata/service/util/jdbi/AwsRdsDatabaseAuthenticationProvider.java
@@ -4,10 +4,14 @@ import java.net.MalformedURLException;
 import java.net.URI;
 import java.util.Map;
 import java.util.Objects;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.rds.RdsUtilities;
 import software.amazon.awssdk.services.rds.model.GenerateAuthenticationTokenRequest;
+import software.amazon.awssdk.services.sts.StsClient;
+import software.amazon.awssdk.services.sts.auth.StsAssumeRoleCredentialsProvider;
+import software.amazon.awssdk.services.sts.model.AssumeRoleRequest;
 
 /**
  * {@link DatabaseAuthenticationProvider} implementation for AWS RDS IAM Auth.
@@ -18,6 +22,7 @@ public class AwsRdsDatabaseAuthenticationProvider implements DatabaseAuthenticat
 
   public static final String AWS_REGION = "awsRegion";
   public static final String ALLOW_PUBLIC_KEY_RETRIEVAL = "allowPublicKeyRetrieval";
+  public static final String ASSUME_ROLE_ARN = "assumeRoleArn";
   public static final String PROTOCOL = "https://";
 
   @Override
@@ -30,6 +35,7 @@ public class AwsRdsDatabaseAuthenticationProvider implements DatabaseAuthenticat
       // Set
       String awsRegion = queryParams.get(AWS_REGION);
       String allowPublicKeyRetrieval = queryParams.get(ALLOW_PUBLIC_KEY_RETRIEVAL);
+      String assumeRoleArn = queryParams.get(ASSUME_ROLE_ARN);
 
       // Validate
       Objects.requireNonNull(awsRegion, "Parameter `awsRegion` shall be provided in the jdbc url.");
@@ -37,10 +43,32 @@ public class AwsRdsDatabaseAuthenticationProvider implements DatabaseAuthenticat
           allowPublicKeyRetrieval,
           "Parameter `allowPublicKeyRetrieval` shall be provided in the jdbc url.");
 
+      AwsCredentialsProvider credentialsProvider = DefaultCredentialsProvider.create();
+
+      if (assumeRoleArn != null) {
+        StsClient stsClient =
+            StsClient.builder()
+                .region(Region.of(awsRegion))
+                .credentialsProvider(credentialsProvider)
+                .build();
+
+        AssumeRoleRequest assumeRoleRequest =
+            AssumeRoleRequest.builder()
+                .roleArn(assumeRoleArn)
+                .roleSessionName("OpenMetadata-RDS-IAM-Auth")
+                .build();
+
+        credentialsProvider =
+            StsAssumeRoleCredentialsProvider.builder()
+                .stsClient(stsClient)
+                .refreshRequest(assumeRoleRequest)
+                .build();
+      }
+
       // Prepare request
       GenerateAuthenticationTokenRequest request =
           GenerateAuthenticationTokenRequest.builder()
-              .credentialsProvider(DefaultCredentialsProvider.create())
+              .credentialsProvider(credentialsProvider)
               .hostname(uri.getHost())
               .port(uri.getPort())
               .username(username)
