690e02d5885be292d4c5e6370dc712b7ddf67e5a
anuj-kumary <anujf0510@gmail.com>
2026-04-24 13:57:31 +0530

    fix lint issue

--- openmetadata-ui/src/main/resources/ui/src/utils/GlobalSettingsClassBase.ts
+++ openmetadata-ui/src/main/resources/ui/src/utils/GlobalSettingsClassBase.ts
@@ -69,7 +69,6 @@ import { ReactComponent as WorksheetIcon } from '../assets/svg/worksheet-colored
 import { SettingMenuItem } from './GlobalSettingsUtils';
 
 import { ReactComponent as GovernanceIcon } from '../assets/svg/governance.svg';
-import { ReactComponent as WorkflowsSettingsIcon } from '../assets/svg/ic-workflows.svg';
 import { ReactComponent as PreferencesSearchIcon } from '../assets/svg/preferences-search.svg';
 import {
   GlobalSettingOptions,
