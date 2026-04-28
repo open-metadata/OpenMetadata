4ea87025e233cd15d22abde72012bb10e547e066
JAYA DUBEY <jayadubey6402@gmail.com>
2026-04-28 22:06:53 +0530

    chore: fix license headers and playwright linting

--- openmetadata-ui/src/main/resources/ui/playwright/e2e/Pages/PipelineValidation.spec.ts
+++ openmetadata-ui/src/main/resources/ui/playwright/e2e/Pages/PipelineValidation.spec.ts
@@ -1,9 +1,9 @@
 /*
- *  Copyright 2026 Collate.
+ *  Copyright 2022 Collate.
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
- *
+ *  You may obtain a copy of the License at
+ *  http://apache.org
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
