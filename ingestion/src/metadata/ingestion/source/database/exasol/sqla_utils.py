7f1da794792fe2e7b714ec92d36ed91dd3fb99ed
Ariel Schulz <ariel.schulz@exasol.com>
2026-05-04 09:19:30 +0200

    Add type hints to maintain signature

--- ingestion/src/metadata/ingestion/source/database/exasol/sqla_utils.py
+++ ingestion/src/metadata/ingestion/source/database/exasol/sqla_utils.py
@@ -1,11 +1,13 @@
-from sqlalchemy.engine import reflection
+from typing import Any
+
+from sqlalchemy.engine import Connection, reflection
 
 from metadata.ingestion.source.database.exasol.queries import EXASOL_GET_TABLE_COMMENTS
 from metadata.utils.sqlalchemy_utils import get_table_comment_wrapper
 
 
 @reflection.cache
-def get_table_comment(self, connection, table_name, schema=None, **kw):
+def get_table_comment(self, connection: Connection, table_name: str, schema: str | None = None, **kw: Any):  # pylint: disable=unused-argument
     return get_table_comment_wrapper(
         self,
         connection,
