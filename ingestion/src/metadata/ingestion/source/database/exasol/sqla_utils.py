3b3fcbe482042e67926bb968a908524056e2bd1a
Ariel Schulz <ariel.schulz@exasol.com>
2026-05-04 09:28:06 +0200

    Add return type too

--- ingestion/src/metadata/ingestion/source/database/exasol/sqla_utils.py
+++ ingestion/src/metadata/ingestion/source/database/exasol/sqla_utils.py
@@ -1,17 +1,25 @@
 from typing import Any
 
 from sqlalchemy.engine import Connection, reflection
+from sqlalchemy.engine.interfaces import ReflectedTableComment
 
 from metadata.ingestion.source.database.exasol.queries import EXASOL_GET_TABLE_COMMENTS
 from metadata.utils.sqlalchemy_utils import get_table_comment_wrapper
 
 
 @reflection.cache
-def get_table_comment(self, connection: Connection, table_name: str, schema: str | None = None, **kw: Any):  # pylint: disable=unused-argument
-    return get_table_comment_wrapper(
+def get_table_comment(
+    self,
+    connection: Connection,
+    table_name: str,
+    schema: str | None = None,
+    **kw: Any,  # pylint: disable=unused-argument
+) -> ReflectedTableComment:
+    table_comment = get_table_comment_wrapper(
         self,
         connection,
         query=EXASOL_GET_TABLE_COMMENTS,
         table_name=table_name.upper(),
         schema=schema.upper() if schema else None,
     )
+    return ReflectedTableComment(text=table_comment.get("text"))
