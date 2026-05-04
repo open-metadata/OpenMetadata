bca0a467904e4e509baca3db47836ff879c19e46
JAYA DUBEY <jayadubey6402@gmail.com>
2026-05-04 15:09:07 +0530

    fix: update test assertion and EntityCsv string to match updated entity name validation regex

--- openmetadata-service/src/test/java/org/openmetadata/service/util/ValidatorUtilTest.java
+++ openmetadata-service/src/test/java/org/openmetadata/service/util/ValidatorUtilTest.java
@@ -22,7 +22,7 @@ class ValidatorUtilTest {
 
     // Invalid name
     glossary.withName("invalid::Name").withDescription("description");
-    assertEquals("[name must match \"^((?!::).)*$\"]", ValidatorUtil.validate(glossary));
+    assertEquals("[name must match \"^((?!::)[^><\"|\\x00-\\x1f])*$\"]", ValidatorUtil.validate(glossary));
 
     // No error
     glossary.withName("validName").withId(UUID.randomUUID()).withDescription("description");
