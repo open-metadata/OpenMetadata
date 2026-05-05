2e2fdad7ef8b7874e7b78750ae82f03c46bba4ec
Harsh Vador <harsh.vador@somaiya.edu>
2026-05-05 13:03:48 +0530

    add import

--- openmetadata-service/src/main/java/org/openmetadata/service/jdbi3/UserRepository.java
+++ openmetadata-service/src/main/java/org/openmetadata/service/jdbi3/UserRepository.java
@@ -39,6 +39,7 @@ import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
