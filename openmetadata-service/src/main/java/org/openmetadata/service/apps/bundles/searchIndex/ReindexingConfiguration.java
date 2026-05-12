b6b88dd9bdbe74a591550197ba0330da0330e9eb
mohitdeuex <mohit.y@deuexsolutions.com>
2026-05-12 22:35:30 +0530

    Spotless

--- openmetadata-service/src/main/java/org/openmetadata/service/apps/bundles/searchIndex/ReindexingConfiguration.java
+++ openmetadata-service/src/main/java/org/openmetadata/service/apps/bundles/searchIndex/ReindexingConfiguration.java
@@ -1,7 +1,6 @@
 package org.openmetadata.service.apps.bundles.searchIndex;
 
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 import org.openmetadata.schema.system.EventPublisherJob;
