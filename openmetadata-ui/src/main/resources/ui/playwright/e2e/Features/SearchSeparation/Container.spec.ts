d3d869795dae34d76cfb1eced23db1211268091d
Anujkumar Yadav <anujf0510@gmail.com>
2026-05-19 14:13:05 +0530

    test:fix SearchSeparation failure (#28239)
    * test:fix SearchSeparation failure
    
    * fix lint issue
    
    * nit
    
    * nit

--- openmetadata-ui/src/main/resources/ui/playwright/e2e/Features/SearchSeparation/Container.spec.ts
+++ openmetadata-ui/src/main/resources/ui/playwright/e2e/Features/SearchSeparation/Container.spec.ts
@@ -13,7 +13,7 @@
 
 import { test } from '@playwright/test';
 import { ContainerClass } from '../../../support/entity/ContainerClass';
-import { registerFilterSeparationSuite } from './searchSeparationSuite';
+import { registerFilterSeparationSuite } from './SearchSeparationSuite';
 
 test.use({ storageState: 'playwright/.auth/admin.json' });
 
