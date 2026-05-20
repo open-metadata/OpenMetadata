ebd317f212c702605769915ce8e9c8218683c6c1
PRADDZY <64481960+PRADDZY@users.noreply.github.com>
2026-05-20 21:31:22 +0530

    fix kafka avro sample decode for pre-deserialized values
    (cherry picked from commit 288ea29b9fe896f0b101be6347625a116686e1a9)

--- ingestion/src/metadata/ingestion/source/messaging/common_broker_source.py
+++ ingestion/src/metadata/ingestion/source/messaging/common_broker_source.py
@@ -17,7 +17,7 @@ import concurrent.futures
 import time
 import traceback
 from abc import ABC
-from typing import Iterable, Optional
+from typing import Any, Iterable, Optional
 
 import confluent_kafka
 from confluent_kafka import KafkaError, KafkaException
@@ -360,8 +360,11 @@ class CommonBrokerSource(MessagingServiceSource, ABC):
                 )
             )
 
-    def decode_message(self, record: bytes, schema: str, schema_type: SchemaType):
+    def decode_message(self, record: Any, schema: str, schema_type: SchemaType):
         if schema_type == SchemaType.Avro:
+            # DeserializingConsumer may already return decoded dict/object values.
+            if not isinstance(record, (bytes, bytearray, memoryview)):
+                return str(record)
             deserializer = AvroDeserializer(
                 schema_str=schema, schema_registry_client=self.schema_registry_client
             )
@@ -369,7 +372,9 @@ class CommonBrokerSource(MessagingServiceSource, ABC):
         if schema_type == SchemaType.Protobuf:
             logger.debug("Protobuf deserializing sample data is not supported")
             return ""
-        return str(record.decode("utf-8"))
+        if isinstance(record, (bytes, bytearray, memoryview)):
+            return bytes(record).decode("utf-8")
+        return str(record)
 
     def close(self):
         if self.generate_sample_data and self.consumer_client:
