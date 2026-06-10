cc4dca14d105d3d41244700f8528ffb2e35e3446
Ariel Schulz <ariel.schulz@exasol.com>
2026-06-10 15:52:58 +0200

    Add schema names to make fully qualified names

--- ingestion/src/metadata/ingestion/source/database/exasol/queries.py
+++ ingestion/src/metadata/ingestion/source/database/exasol/queries.py
@@ -9,8 +9,8 @@ EXASOL_SQL_STATEMENT = textwrap.dedent(
       s.start_time "start_time",
       s.stop_time "end_time",
       s.duration "duration"
-    FROM EXA_DBA_AUDIT_SQL s
-    JOIN EXA_DBA_AUDIT_SESSIONS se
+    FROM EXA_STATISTICS.EXA_DBA_AUDIT_SQL s
+    JOIN EXA_STATISTICS.EXA_DBA_AUDIT_SESSIONS se
     ON s.SESSION_ID = se.SESSION_ID
     WHERE s.sql_text NOT LIKE '/* {{"app": "OpenMetadata", %%}} */%%'
     AND s.sql_text NOT LIKE '/* {{"app": "dbt", %%}} */%%'
@@ -29,8 +29,8 @@ EXASOL_TEST_GET_QUERIES = textwrap.dedent(
       s.start_time,
       s.stop_time,
       s.duration
-    FROM EXA_DBA_AUDIT_SQL s
-    JOIN EXA_DBA_AUDIT_SESSIONS se
+    FROM EXA_STATISTICS.EXA_DBA_AUDIT_SQL s
+    JOIN EXA_STATISTICS.EXA_DBA_AUDIT_SESSIONS se
     ON s.SESSION_ID = se.SESSION_ID
     LIMIT 1
     """
@@ -44,7 +44,7 @@ EXASOL_SYSTEM_METRICS_QUERY = textwrap.dedent(
       :table AS "table",
       s.start_time AS "starttime",
       s.row_count AS "rows"
-    FROM EXA_DBA_AUDIT_SQL s
+    FROM EXA_STATISTICS.EXA_DBA_AUDIT_SQL s
     WHERE s.command_name = :operation
       AND s.success = TRUE
       AND s.start_time >= ADD_DAYS(SYSTIMESTAMP, -1)
