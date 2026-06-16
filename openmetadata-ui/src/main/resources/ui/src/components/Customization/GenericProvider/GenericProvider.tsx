c02ab0ceb820fa00d2cb7d8ef79757eebef65a4e
Harshit Shah <dinkushah169@gmail.com>
2026-06-17 01:06:39 +0530

    fix checkstyle

--- openmetadata-ui/src/main/resources/ui/src/components/Customization/GenericProvider/GenericProvider.tsx
+++ openmetadata-ui/src/main/resources/ui/src/components/Customization/GenericProvider/GenericProvider.tsx
@@ -12,14 +12,7 @@
  */
 import { AxiosError } from 'axios';
 import { isEmpty, omit } from 'lodash';
-import {
-  lazy,
-  useCallback,
-  useEffect,
-  useMemo,
-  useRef,
-  useState,
-} from 'react';
+import { lazy, useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { ENTITY_PAGE_TYPE_MAP } from '../../../constants/Customize.constants';
@@ -54,8 +47,8 @@ import {
   ColumnFieldUpdate,
   ColumnOrTask,
 } from '../../Database/ColumnDetailPanel/ColumnDetailPanel.interface';
-import { GenericProviderProps } from './GenericProvider.interface';
 import { createGenericContext } from './GenericContext';
+import { GenericProviderProps } from './GenericProvider.interface';
 
 const ColumnDetailPanel = withSuspenseFallback(
   lazy(() =>
