9db5942b0cb5a14dfeb97d413303ae3e75eb6055
Akash Verma <akashverma@Akashs-MacBook-Pro-2.local>
2026-06-17 18:35:13 +0530

    fix(sapbw4hana): clean up connection schema and register pipeline service type

--- openmetadata-ui/src/main/resources/ui/src/utils/ServiceUtilClassBase.ts
+++ openmetadata-ui/src/main/resources/ui/src/utils/ServiceUtilClassBase.ts
@@ -116,6 +116,8 @@ class ServiceUtilClassBase {
     DatabaseServiceType.MicrosoftAccess,
     DashboardServiceType.SapS4Hana,
     DatabaseServiceType.SapSuccessFactors,
+    DatabaseServiceType.SapBw4Hana,
+    PipelineServiceType.SapBw4HanaPipeline
   ];
 
   DatabaseServiceTypeSmallCase = this.convertEnumToLowerCase<
