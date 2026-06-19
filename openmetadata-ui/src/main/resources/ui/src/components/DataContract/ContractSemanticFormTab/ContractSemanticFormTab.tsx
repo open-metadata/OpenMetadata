92f450b40d179af31179499419e5980636d0bf16
Harshit Shah <dinkushah169@gmail.com>
2026-06-19 14:29:46 +0530

    fix(ui): remove duplicate query builder css import

--- openmetadata-ui/src/main/resources/ui/src/components/DataContract/ContractSemanticFormTab/ContractSemanticFormTab.tsx
+++ openmetadata-ui/src/main/resources/ui/src/components/DataContract/ContractSemanticFormTab/ContractSemanticFormTab.tsx
@@ -13,7 +13,6 @@
 
 import Icon from '@ant-design/icons';
 import { Actions, JsonTree } from '@react-awesome-query-builder/antd';
-import '@react-awesome-query-builder/antd/css/styles.css';
 import {
   Button,
   Col,
