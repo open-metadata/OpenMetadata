9719d43abf742b004507fe50520d27776f078fac
Rohit0301 <rj03012002@gmail.com>
2026-06-20 13:09:09 +0530

    DataAssetFilterPopover component for improved UI/UX.

---
+++ openmetadata-ui/src/main/resources/ui/src/components/DataAssets/DataAssetPicker/DataAssetPickerRow.tsx
@@ -0,0 +1,88 @@
+/*
+ *  Copyright 2026 Collate.
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+import { Badge, Box, Typography } from '@openmetadata/ui-core-components';
+import { Check } from '@untitledui/icons';
+import { FC } from 'react';
+import { getEntityIconWithBg } from 'src/utils/Assets/AssetsUtils';
+import { DataAssetPickerOption } from './DataAssetPicker.interface';
+
+interface DataAssetPickerRowProps {
+  option: DataAssetPickerOption;
+  isSelected: boolean;
+  onSelect: (option: DataAssetPickerOption) => void;
+}
+
+const DataAssetPickerRow: FC<DataAssetPickerRowProps> = ({
+  option,
+  isSelected,
+  onSelect,
+}) => {
+  const { label, displayName, name, type, id } = option;
+  const title = displayName || name || label;
+
+  return (
+    <button
+      className={[
+        'tw:w-full tw:flex tw:items-center tw:gap-2 tw:px-2.5 tw:py-2 tw:rounded-md tw:mb-1 tw:justify-between',
+        'tw:cursor-pointer tw:text-left tw:transition tw:duration-100',
+        'tw:hover:bg-utility-gray-blue-50 tw:outline-hidden',
+        isSelected ? 'tw:bg-brand-primary' : '',
+      ].join(' ')}
+      type="button"
+      onClick={() => onSelect(option)}>
+      <Box align="center" className="tw:min-w-0 tw:flex-1" gap={2}>
+        {type && getEntityIconWithBg(type)}
+
+        <Box
+          align="center"
+          className="tw:min-w-0 tw:flex-1"
+          gap={3}
+          justify="between">
+          <Box
+            className="tw:min-w-0 tw:flex-1 tw:[&_.prose]:leading-tight"
+            direction="col">
+            <Typography
+              ellipsis
+              className="tw:truncate tw:leading-tight"
+              size="text-xs"
+              weight="medium">
+              {title}
+            </Typography>
+            {id && (
+              <Typography
+                ellipsis
+                className="tw:text-tertiary tw:truncate tw:leading-tight"
+                size="text-xs">
+                {id}
+              </Typography>
+            )}
+          </Box>
+          {type && (
+            <Badge
+              className="tw:shrink-0 tw:uppercase tw:font-medium"
+              color="gray"
+              size="xs"
+              type="color">
+              {type}
+            </Badge>
+          )}
+        </Box>
+      </Box>
+      {isSelected && (
+        <Check className="tw:shrink-0" size={16} strokeWidth={1.5} />
+      )}
+    </button>
+  );
+};
+
+export default DataAssetPickerRow;
