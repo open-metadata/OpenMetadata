dff407a0f95650544279dfb3b85244830b1722e0
Teddy <teddy.crepineau@gmail.com>
2026-06-24 14:48:59 -0700

    ISSUE #27923 -  fixed shadowing datetime serialization from jackson (#27925)
    * chore: fixed shadowing datetime serialization from jackson
    
    * fix: add lenient date serializer
    
    ---------
    
    Co-authored-by: IceS2 <pablo.takara@getcollate.io>
    Co-authored-by: Pere Miquel Brull <peremiquelbrull@gmail.com>
    (cherry picked from commit eb259b110b9548664ede4b12ada5c3cbe49f551c)

--- openmetadata-service/src/main/java/org/openmetadata/service/OpenMetadataApplication.java
+++ openmetadata-service/src/main/java/org/openmetadata/service/OpenMetadataApplication.java
@@ -15,6 +15,7 @@ package org.openmetadata.service;
 
 import static org.openmetadata.service.util.jdbi.JdbiUtils.createAndSetupJDBI;
 
+import com.fasterxml.jackson.databind.SerializationFeature;
 import io.dropwizard.configuration.EnvironmentVariableSubstitutor;
 import io.dropwizard.configuration.SubstitutingSourceProvider;
 import io.dropwizard.core.Application;
@@ -80,6 +81,7 @@ import org.openmetadata.schema.api.security.AuthorizerConfiguration;
 import org.openmetadata.schema.configuration.LimitsConfiguration;
 import org.openmetadata.schema.services.connections.metadata.AuthProvider;
 import org.openmetadata.schema.type.MetadataOperation;
+import org.openmetadata.schema.utils.JsonUtils;
 import org.openmetadata.search.IndexMappingLoader;
 import org.openmetadata.service.apps.ApplicationContext;
 import org.openmetadata.service.apps.ApplicationHandler;
@@ -779,6 +781,11 @@ public class OpenMetadataApplication extends Application<OpenMetadataApplication
             org.openmetadata.service.events.AuditExcludeFilterFactory.class,
             SwitchableEventLayoutFactory.class,
             SwitchableAccessLayoutFactory.class);
+    bootstrap
+        .getObjectMapper()
+        .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+        .setDateFormat(JsonUtils.DATE_TIME_FORMAT)
+        .registerModule(JsonUtils.lenientDateModule());
 
     bootstrap.addBundle(
         new SwaggerBundle<>() {
