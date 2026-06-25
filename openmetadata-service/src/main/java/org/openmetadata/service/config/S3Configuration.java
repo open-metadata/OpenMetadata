c7798a18849c6b1ead99a89ae245db2b9eb9c117
Ayush Shah <ayush@getcollate.io>
2026-06-25 17:47:15 +0530

    fix: remove stale S3 validation annotations

--- openmetadata-service/src/main/java/org/openmetadata/service/config/S3Configuration.java
+++ openmetadata-service/src/main/java/org/openmetadata/service/config/S3Configuration.java
@@ -1,8 +1,6 @@
 package org.openmetadata.service.config;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.validation.constraints.AssertTrue;
-import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 import org.apache.commons.lang3.StringUtils;
@@ -12,11 +10,9 @@ import org.apache.commons.lang3.StringUtils;
 public class S3Configuration {
 
   @JsonProperty("bucketName")
-  @NotBlank(message = "Bucket name must be provided")
   private String bucketName;
 
   @JsonProperty("region")
-  @NotBlank(message = "Region must be provided")
   private String region;
 
   @JsonProperty("accessKey")
@@ -52,8 +48,6 @@ public class S3Configuration {
   @JsonProperty("kmsKeyId")
   private String kmsKeyId;
 
-  @AssertTrue(
-      message = "Either useIamRole must be true or both accessKey and secretKey must be provided")
   public boolean isValidCredentials() {
     if (useIamRole) {
       return true;
