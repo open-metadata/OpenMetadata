f9ddd6ffab059ae499dfa70881bdf7e46612340b
Chirag Madlani <12962843+chirag-madlani@users.noreply.github.com>
2026-07-01 00:52:42 +0530

    fix styles

--- openmetadata-ui/src/main/resources/ui/src/components/common/OwnerLabel/OwnerAvatarStack.component.tsx
+++ openmetadata-ui/src/main/resources/ui/src/components/common/OwnerLabel/OwnerAvatarStack.component.tsx
@@ -58,7 +58,7 @@ export const OwnerAvatarStack: React.FC<OwnerAvatarStackProps> = ({
 
     return (
       <span
-        className="owner-avatar-stack-team tw:bg-utility-gray-100 tw:text-utility-gray-600"
+        className="owner-avatar-stack-team"
         data-testid={entityName}
         style={{ width: avatarSize, height: avatarSize }}>
         <TeamsIcons
