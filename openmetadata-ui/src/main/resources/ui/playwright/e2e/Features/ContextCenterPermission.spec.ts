30d1f8c890f10c6bb4fd3e5199d0d05025bf8275
Rohit0301 <rj03012002@gmail.com>
2026-07-11 08:45:00 +0530

    fix playwright

--- openmetadata-ui/src/main/resources/ui/playwright/e2e/Features/ContextCenterPermission.spec.ts
+++ openmetadata-ui/src/main/resources/ui/playwright/e2e/Features/ContextCenterPermission.spec.ts
@@ -13,9 +13,9 @@
 
 import {
   APIRequestContext,
+  test as base,
   expect,
   Page,
-  test as base,
 } from '@playwright/test';
 import { KnowledgeCenterClass } from '../../support/entity/KnowledgeCenterClass';
 import { UserClass } from '../../support/user/UserClass';
@@ -2191,12 +2191,12 @@ test.describe('Context Center Permissions', () => {
         await expect(
           tagsContainer
             .getByTestId('add-tag')
-            .or(tagsContainer.getByTestId('edit-tag'))
+            .or(tagsContainer.getByTestId('edit-button'))
         ).toBeVisible();
         await expect(
           glossaryContainer
             .getByTestId('add-tag')
-            .or(glossaryContainer.getByTestId('edit-tag'))
+            .or(glossaryContainer.getByTestId('edit-button'))
         ).toBeVisible();
         await expect(
           dataStewardPage.getByTestId('add-domain')
