60b7e0c0c80a3bf468fe62cae0bded045a8f9a97
Mohit Tilala <tilalamohit123@gmail.com>
2026-07-17 14:08:00 +0530

    Add skops to test deps for mlflow 3.14 sklearn default

--- ingestion/setup.py
+++ ingestion/setup.py
@@ -507,6 +507,7 @@ test = {
     "testcontainers~=4.8.0",
     "minio==7.2.5",
     *plugins["mlflow"],
+    "skops",  # mlflow 3.14 switched the mlflow.sklearn serialization default to skops, which mlflow-skinny does not pull in
     *plugins["datalake-s3"],
     *plugins["kafka"],
     "kafka-python==2.0.2",
