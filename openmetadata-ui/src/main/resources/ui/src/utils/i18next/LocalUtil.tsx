62700f0d435df1da8d7d3d6c13fde0b31577895e
Chirag Madlani <12962843+chirag-madlani@users.noreply.github.com>
2026-07-30 17:50:46 +0530

    feat(app): attach core-components i18n namespace to app i18next instance

--- openmetadata-ui/src/main/resources/ui/src/utils/i18next/LocalUtil.tsx
+++ openmetadata-ui/src/main/resources/ui/src/utils/i18next/LocalUtil.tsx
@@ -11,6 +11,7 @@
  *  limitations under the License.
  */
 
+import { initCoreI18n, loadCoreLocale } from '@openmetadata/ui-core-components';
 import i18next, { t as i18nextT } from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import { ReactNode } from 'react';
@@ -23,6 +24,7 @@ i18next
   .use(initReactI18next)
   .init(getInitOptions())
   .then(async () => {
+    initCoreI18n(i18next);
     if (i18next.language !== i18next.resolvedLanguage) {
       await i18next.changeLanguage(i18next.language);
     }
@@ -30,6 +32,7 @@ i18next
 
 i18next.on('languageChanged', async (lng) => {
   await localUtilClassBase.loadLocales(lng);
+  await loadCoreLocale(i18next, lng);
 });
 
 export const t = (key: string, options?: Record<string, unknown>): string => {
