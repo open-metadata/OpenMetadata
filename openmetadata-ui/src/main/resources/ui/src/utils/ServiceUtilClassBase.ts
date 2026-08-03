8e614f6521f67936777767f51fe2ccc9b8327e2e
Harshit Shah <dinkushah169@gmail.com>
2026-08-03 16:42:39 +0530

    Remove unsupported SAP BW service enum references

--- openmetadata-ui/src/main/resources/ui/src/utils/ServiceUtilClassBase.ts
+++ openmetadata-ui/src/main/resources/ui/src/utils/ServiceUtilClassBase.ts
@@ -110,8 +110,6 @@ class ServiceUtilClassBase {
     DatabaseServiceType.MicrosoftAccess,
     DashboardServiceType.SapS4Hana,
     DatabaseServiceType.SapSuccessFactors,
-    DatabaseServiceType.SapBw4Hana,
-    PipelineServiceType.SapBw4HanaPipeline,
   ];
 
   DatabaseServiceTypeSmallCase = this.convertEnumToLowerCase<
