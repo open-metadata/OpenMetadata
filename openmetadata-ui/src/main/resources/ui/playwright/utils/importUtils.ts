c6a6f3a5775dd6b1262700c87945e9875d6e6698
Rohit0301 <rj03012002@gmail.com>
2026-08-17 23:13:58 +0530

    fix(playwright): Bulk import playwright fix, user tab selection in owner popover

--- openmetadata-ui/src/main/resources/ui/playwright/utils/importUtils.ts
+++ openmetadata-ui/src/main/resources/ui/playwright/utils/importUtils.ts
@@ -26,8 +26,8 @@ import {
 } from '../constant/glossaryImportExport';
 import { GlobalSettingOptions } from '../constant/settings';
 import {
-  EntityTypeEndpoint,
   ENTITY_PATH,
+  EntityTypeEndpoint,
 } from '../support/entity/Entity.interface';
 import {
   clickOutside,
@@ -507,9 +507,12 @@ export const fillOwnerDetails = async (page: Page, owners: string[]) => {
   ).toBeVisible();
 
   await waitForAllLoadersToDisappear(page);
+  await page.waitForLoadState('domcontentloaded');
 
   const userListResponse = page.waitForResponse(
-    '/api/v1/search/query?q=&index=user&*'
+    (response) =>
+      response.url().includes('/api/v1/search/query?q=') &&
+      response.url().includes('index=user')
   );
   await page.getByRole('tab', { name: 'Users' }).click();
   await userListResponse;
