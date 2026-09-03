612470370875809015aac2a1d2f7120e89973a57
Ram Narayan Balaji <ramnarayanb3005@gmail.com>
2026-09-03 18:23:00 +0530

    test(teams): account for update error wrapper

--- openmetadata-integration-tests/src/test/java/org/openmetadata/it/tests/TeamResourceIT.java
+++ openmetadata-integration-tests/src/test/java/org/openmetadata/it/tests/TeamResourceIT.java
@@ -71,6 +71,8 @@ public class TeamResourceIT extends BaseEntityIT<Team, CreateTeam> {
 
   private static final String DIRECT_USER_ASSIGNMENT_ERROR =
       "Team is of type Department. Direct users can only be assigned to teams of type Group.";
+  private static final String UPDATE_DIRECT_USER_ASSIGNMENT_ERROR =
+      "Failed to update entity: " + DIRECT_USER_ASSIGNMENT_ERROR;
 
   {
     supportsImportExport = true;
@@ -313,7 +315,7 @@ public class TeamResourceIT extends BaseEntityIT<Team, CreateTeam> {
     Exception exception =
         assertThrows(Exception.class, () -> patchEntity(update.getId().toString(), update));
 
-    assertEquals(DIRECT_USER_ASSIGNMENT_ERROR, exception.getMessage());
+    assertEquals(UPDATE_DIRECT_USER_ASSIGNMENT_ERROR, exception.getMessage());
   }
 
   @Test
@@ -388,7 +390,7 @@ public class TeamResourceIT extends BaseEntityIT<Team, CreateTeam> {
             Exception.class,
             () -> SdkClients.adminClient().users().update(user.getId().toString(), update));
 
-    assertEquals(DIRECT_USER_ASSIGNMENT_ERROR, exception.getMessage());
+    assertEquals(UPDATE_DIRECT_USER_ASSIGNMENT_ERROR, exception.getMessage());
   }
 
   @Test
