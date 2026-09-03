5280f0316d6dc53ffc89ff7a3fe479ef58854cce
anuj-kumary <anujf0510@gmail.com>
2026-09-03 20:03:00 +0530

    fix: footer spacing issue for ontology studio

--- openmetadata-ui/src/main/resources/ui/src/pages/OntologyExplorerPage/OntologyExplorerPage.tsx
+++ openmetadata-ui/src/main/resources/ui/src/pages/OntologyExplorerPage/OntologyExplorerPage.tsx
@@ -44,6 +44,7 @@ import { RelationshipType } from '../../generated/entity/data/relationshipType';
 import { Operation } from '../../generated/entity/policies/policy';
 import { useAuth } from '../../hooks/authHooks';
 import { useApplicationStore } from '../../hooks/useApplicationStore';
+import { useIsAiMode } from '../../hooks/useAppMode';
 import { checkPermission } from '../../utils/PermissionsUtils';
 import { generateUUID } from '../../utils/StringUtils';
 
@@ -127,6 +128,7 @@ function RdfDisabledNotice() {
 
 const OntologyExplorerPage: React.FC = () => {
   const { t } = useTranslation();
+  const isAiMode = useIsAiMode();
   const { isAdminUser } = useAuth();
   const { permissions } = usePermissionProvider();
   const { currentUser } = useApplicationStore();
@@ -355,8 +357,12 @@ const OntologyExplorerPage: React.FC = () => {
   return (
     <PageLayoutV1
       fullHeight
-      className="tw:p-0!"
-      mainContainerClassName="ontology-studio-page-layout"
+      className={classNames('tw:p-0!', {
+        'tw:h-full!': isAiMode,
+      })}
+      mainContainerClassName={classNames('ontology-studio-page-layout', {
+        'tw:h-full!': isAiMode,
+      })}
       pageTitle={t('label.ontology-studio')}>
       <main
         className="tw:flex tw:h-full tw:min-h-0 tw:flex-col tw:overflow-hidden tw:bg-tertiary tw:font-body tw:antialiased"
