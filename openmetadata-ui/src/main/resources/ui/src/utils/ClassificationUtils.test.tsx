fd9125a92b11c1e41dda52afbf2d2d1922a2e037
anuj-kumary <anujf0510@gmail.com>
2026-09-15 15:31:52 +0530

    fix(checkstyle): remove unused OperationPermission import

--- openmetadata-ui/src/main/resources/ui/src/utils/ClassificationUtils.test.tsx
+++ openmetadata-ui/src/main/resources/ui/src/utils/ClassificationUtils.test.tsx
@@ -12,7 +12,6 @@
  */
 
 import { EntityField } from '../constants/Feeds.constants';
-import { OperationPermission } from '../context/PermissionProvider/PermissionProvider.interface';
 import { ProviderType } from '../generated/entity/bot';
 import { Classification } from '../generated/entity/classification/classification';
 import { ChangeDescription } from '../generated/entity/type';
