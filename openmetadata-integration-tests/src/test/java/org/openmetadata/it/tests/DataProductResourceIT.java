13e60f490d4f738cf1b6714e7efaaf707a12a892
sonika-shah <sonika.s@deuexsolutions.com>
2026-09-17 15:38:47 +0530

    Add missing java.util.ArrayList import to fix 1.13 compile

--- openmetadata-integration-tests/src/test/java/org/openmetadata/it/tests/DataProductResourceIT.java
+++ openmetadata-integration-tests/src/test/java/org/openmetadata/it/tests/DataProductResourceIT.java
@@ -11,6 +11,7 @@ import static org.openmetadata.it.bootstrap.SharedEntities.*;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
